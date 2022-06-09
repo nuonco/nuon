472eb71a00a767b126b51b8207585be13cc7ab35
Jason Tarasovic <JTarasovic@users.noreply.github.com>
2022-06-09 18:40:03 -0500

    add shortid package (#12)

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/powertoolsdev/go-common
 go 1.18
 
 require (
+	github.com/google/uuid v1.1.2
 	github.com/mitchellh/mapstructure v1.5.0
 	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.12.0
---
+++ shortid/shortid.go
@@ -0,0 +1,64 @@
+package shortid
+
+import (
+	"encoding/binary"
+	"fmt"
+	"strconv"
+
+	"github.com/google/uuid"
+)
+
+const (
+	base     = 36
+	intBytes = 8
+)
+
+type shortid string
+
+func (s shortid) String() string {
+	return string(s)
+}
+
+func (s shortid) UUID() (uuid.UUID, error) {
+	str := s.String()
+	var u uuid.UUID
+	if len(str) < 26 {
+		return u, fmt.Errorf("invalid shortid length")
+	}
+
+	i1, err := strconv.ParseUint(str[:13], base, 64)
+	if err != nil {
+		return u, err
+	}
+
+	i2, err := strconv.ParseUint(str[13:], base, 64)
+	if err != nil {
+		return u, err
+	}
+
+	bs := [16]byte{}
+	binary.BigEndian.PutUint64(bs[:intBytes], i1)
+	binary.BigEndian.PutUint64(bs[intBytes:], i2)
+
+	u = uuid.UUID(bs)
+
+	return u, nil
+}
+
+func ParseUUID(u uuid.UUID) (shortid, error) {
+	msi := binary.BigEndian.Uint64(u[:intBytes])
+	lsi := binary.BigEndian.Uint64(u[intBytes:])
+
+	mss := strconv.FormatUint(msi, base)
+	lss := strconv.FormatUint(lsi, base)
+
+	return shortid(fmt.Sprintf("%026s", mss+lss)), nil
+}
+
+func ParseString(s string) (shortid, error) {
+	u, err := uuid.Parse(s)
+	if err != nil {
+		return "", err
+	}
+	return ParseUUID(u)
+}
