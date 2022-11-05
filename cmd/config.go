d605a9cab5e2f39ccc252567223dc7953ee2dbd0
Jon Morehouse <MorehouseJ09@gmail.com>
2022-11-05 14:10:10 -0700

    chore: init from template (#1)
    * chore: init from template
    
    * chore: import new template
    
    * chore: update infra vals
    
    * chore: disable update status checks

---
+++ cmd/config.go
@@ -0,0 +1,21 @@
+package cmd
+
+import (
+	"github.com/powertoolsdev/go-common/config"
+	domain "github.com/powertoolsdev/template-go-workers/internal"
+)
+
+//nolint:gochecknoinits
+func init() {
+	config.RegisterDefault("temporal_host", "localhost:7233")
+	config.RegisterDefault("temporal_namespace", "default")
+
+}
+
+type Config struct {
+	config.Base       `config:",squash"`
+	TemporalHost      string `config:"temporal_host"`
+	TemporalNamespace string `config:"temporal_namespace"`
+
+	Cfg domain.Config `config:"org"`
+}
