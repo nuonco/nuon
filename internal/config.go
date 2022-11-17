3f6ee01f2552266d901e9f822f0ba1dbc91668c9
Jon Morehouse <MorehouseJ09@gmail.com>
2022-11-16 17:30:17 -0800

    feat: add waypoint project into project config (#25)

--- internal/config.go
+++ internal/config.go
@@ -3,7 +3,9 @@ package workers
 import "github.com/go-playground/validator/v10"
 
 type Config struct {
-	OrgsIamRoleArn string `config:"orgs_account_iam_role_arn" validate:"required"`
+	OrgsIamRoleArn           string `config:"orgs_account_iam_role_arn" validate:"required" json:"orgs_iam_role_arn"`
+	WaypointTokenNamespace   string `config:"waypoint_token_namespace" json:"waypoint_token_namespace" validate:"required"`
+	WaypointServerRootDomain string `config:"waypoint_server_root_domain" json:"waypoint_server_root_domain" validate:"required"`
 }
 
 func (c Config) Validate() error {
