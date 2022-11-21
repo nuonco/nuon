80e98db697fe46b86425abd9b7c0f5fe10dc86b2
Jon Morehouse <MorehouseJ09@gmail.com>
2022-11-21 14:20:09 -0800

    feat: add iam role for creating IAM roles in org account (#33)
    * feat: add iam role for creating IAM roles in org account
    
    * add create role

---
+++ infra/orgs.tf
@@ -0,0 +1,53 @@
+data "aws_iam_policy_document" "orgs_account_access" {
+  provider = aws.orgs
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "iam:CreatePolicy",
+      "iam:CreateRole",
+      "iam:GetRole",
+    ]
+    resources = ["*", ]
+  }
+}
+
+data "aws_iam_policy_document" "orgs_account_access_trust" {
+  provider = aws.orgs
+
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole", ]
+
+    principals {
+      type = "AWS"
+      identifiers = [
+        module.iam_eks_role.iam_role_arn,
+      ]
+    }
+  }
+}
+
+resource "aws_iam_policy" "orgs_account_access_policy" {
+  provider = aws.orgs
+
+  name   = "${local.name}-orgs-account-access"
+  policy = data.aws_iam_policy_document.orgs_account_access.json
+}
+
+module "orgs_account_access_role" {
+  providers = {
+    aws = aws.orgs
+  }
+
+
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
+  version = ">= 5.1.0"
+
+  create_role       = true
+  role_requires_mfa = false
+
+  role_name                = "${local.name}-orgs-account-access"
+  custom_role_trust_policy = data.aws_iam_policy_document.orgs_account_access_trust.json
+  custom_role_policy_arns  = [aws_iam_policy.orgs_account_access_policy.arn, ]
+}
