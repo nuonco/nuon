b30b173f1fa39e53eaebf685fda252139b6b3e83
Jon Morehouse <MorehouseJ09@gmail.com>
2022-11-22 10:12:49 -0800

    feat: add registry output (#41)
    feat: add registry id output

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -74,4 +74,7 @@ output "orgs_iam_oidc_provider_arn" {
   value = nonsensitive(data.tfe_outputs.infra-eks-orgs.values.oidc_provider_arn)
 }
 
-#output "orgs_ecr_registry_id" {}
+output "orgs_ecr_registry_id" {
+  description = "Registry id for orgs - this is where org repos will live."
+  value = "https://${local.accounts["orgs-${var.env}"].id}.dkr.ecr.region.amazonaws.com"
+}
