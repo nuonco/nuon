7c6cd7fe986e3f06096dc71284a1978bbfd6a8c6
Jon Morehouse <MorehouseJ09@gmail.com>
2022-11-23 15:15:29 -0800

    chore: cleanup configs (#32)
    Consolidate configs, remove unused values and move everything into
    `internal/`

--- internal/config.go
+++ internal/config.go
@@ -2,15 +2,32 @@ package deployment
 
 import (
 	"github.com/go-playground/validator/v10"
+	"github.com/powertoolsdev/go-common/config"
 )
 
+//nolint:gochecknoinits
+func init() {
+	config.RegisterDefault("temporal_host", "localhost:7233")
+	config.RegisterDefault("temporal_namespace", "default")
+
+	// deployment defaults
+	config.RegisterDefault("waypoint_token_secret_namespace", "default")
+	config.RegisterDefault("bucket", "nuon-installations-stage")
+	config.RegisterDefault("bucket_region", "us-west-2")
+	config.RegisterDefault("role_arn", "arn:aws:iam::618886478608:role/install-k8s-admin-stage")
+	config.RegisterDefault("waypoint_org_server_root_domain", "orgs-stage.nuon.co")
+	config.RegisterDefault("temporal_host", "localhost:7233")
+	config.RegisterDefault("temporal_namespace", "default")
+}
+
 type Config struct {
+	config.Base       `config:",squash"`
+	TemporalHost      string `config:"temporal_host"`
+	TemporalNamespace string `config:"temporal_namespace"`
+
 	Bucket       string `config:"bucket" validate:"required"`
 	BucketRegion string `config:"bucket_region" validate:"required"`
 	RoleArn      string `config:"role_arn" validate:"required"`
-
-	TemporalHost      string `config:"temporal_host" validate:"required"`
-	TemporalNamespace string `config:"temporal_namespace" validate:"required"`
 
 	WaypointTokenSecretNamespace string `config:"waypoint_token_secret_namespace" validate:"required"`
 	WaypointOrgServerRootDomain  string `config:"waypoint_org_server_root_domain" validate:"required"`
