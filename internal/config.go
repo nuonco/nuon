66a5192f9dc2c77a5f30619c306255aeda58e03d
Jon Morehouse <MorehouseJ09@gmail.com>
2022-11-23 12:00:05 -0800

    chore: refactor configs + set defaults (#48)
    chore: refactor config + set defaults

--- internal/config.go
+++ internal/config.go
@@ -1,11 +1,26 @@
 package workers
 
-import "github.com/go-playground/validator/v10"
+import (
+	"github.com/go-playground/validator/v10"
+	"github.com/powertoolsdev/go-common/config"
+)
+
+//nolint:gochecknoinits
+func init() {
+	config.RegisterDefault("temporal_host", "localhost:7233")
+	config.RegisterDefault("temporal_namespace", "default")
+	config.RegisterDefault("waypoint_token_secret_namespace", "default")
+	config.RegisterDefault("waypoint_server_root_domain", "orgs-stage.nuon.co")
+}
 
 type Config struct {
+	config.Base `config:",squash"`
+
 	OrgsEcrAccessIamRoleArn  string `config:"orgs_ecr_access_iam_role_arn" validate:"required" json:"orgs_ecr_access_iam_role_arn"`
 	WaypointTokenNamespace   string `config:"waypoint_token_namespace" json:"waypoint_token_namespace" validate:"required"`
 	WaypointServerRootDomain string `config:"waypoint_server_root_domain" json:"waypoint_server_root_domain" validate:"required"`
+	TemporalHost             string `config:"temporal_host"`
+	TemporalNamespace        string `config:"temporal_namespace"`
 }
 
 func (c Config) Validate() error {
