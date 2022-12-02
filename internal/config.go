4ac45ea6afc40336af52a176f38d62cf6060d86d
Jon Morehouse <MorehouseJ09@gmail.com>
2022-12-02 12:48:16 -0800

    fix: default config vals (#39)

--- internal/config.go
+++ internal/config.go
@@ -11,11 +11,11 @@ func init() {
 	config.RegisterDefault("temporal_namespace", "default")
 
 	// instance defaults
-	config.RegisterDefault("instance.bucket", "nuon-installations-stage")
-	config.RegisterDefault("instance.bucket_region", "us-west-2")
-	config.RegisterDefault("instance.role_arn", "arn:aws:iam::618886478608:role/install-k8s-admin-stage")
-	config.RegisterDefault("instance.waypoint_token_secret_namespace", "default")
-	config.RegisterDefault("instance.waypoint_server_root_domain", "orgs-stage.nuon.co")
+	config.RegisterDefault("bucket", "nuon-installations-stage")
+	config.RegisterDefault("bucket_region", "us-west-2")
+	config.RegisterDefault("role_arn", "arn:aws:iam::618886478608:role/install-k8s-admin-stage")
+	config.RegisterDefault("waypoint_token_secret_namespace", "default")
+	config.RegisterDefault("waypoint_server_root_domain", "orgs-stage.nuon.co")
 }
 
 type Config struct {
