04af8de62d7bf2f03fcc918c113c44487fbb4d58
Jon Morehouse <MorehouseJ09@gmail.com>
2022-12-16 12:32:18 -0800

    feat: add provision response (#27)
    * feat: add provision response
    
    * generate

--- workflows/generated/types/orgs/v1/server/v1/provision.pb.validate.go
+++ workflows/generated/types/orgs/v1/server/v1/provision.pb.validate.go
@@ -161,3 +161,105 @@ var _ interface {
 var _ProvisionRequest_Region_InLookup = map[string]struct{}{
 	"us-west-2": {},
 }
+
+// Validate checks the field values on ProvisionResponse with the rules defined
+// in the proto definition for this message. If any rules are violated, the
+// first error encountered is returned, or nil if there are no violations.
+func (m *ProvisionResponse) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on ProvisionResponse with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, the result is a list of violation errors wrapped in
+// ProvisionResponseMultiError, or nil if none found.
+func (m *ProvisionResponse) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *ProvisionResponse) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	if len(errors) > 0 {
+		return ProvisionResponseMultiError(errors)
+	}
+
+	return nil
+}
+
+// ProvisionResponseMultiError is an error wrapping multiple validation errors
+// returned by ProvisionResponse.ValidateAll() if the designated constraints
+// aren't met.
+type ProvisionResponseMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m ProvisionResponseMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m ProvisionResponseMultiError) AllErrors() []error { return m }
+
+// ProvisionResponseValidationError is the validation error returned by
+// ProvisionResponse.Validate if the designated constraints aren't met.
+type ProvisionResponseValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e ProvisionResponseValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e ProvisionResponseValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e ProvisionResponseValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e ProvisionResponseValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e ProvisionResponseValidationError) ErrorName() string {
+	return "ProvisionResponseValidationError"
+}
+
+// Error satisfies the builtin error interface
+func (e ProvisionResponseValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sProvisionResponse.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = ProvisionResponseValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = ProvisionResponseValidationError{}
