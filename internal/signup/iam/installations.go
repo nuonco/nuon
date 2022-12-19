09fad1b9495f7900fc52ba4fe37de280cbe3233d
Jon Morehouse <MorehouseJ09@gmail.com>
2022-12-18 18:19:34 -0800

    feat: create installations IAM role (#105)
    feat: add installations iam role
    
    The installations IAM role functionality is almost identical to the
    deployments, so this is mainly a copy/paste.

---
+++ internal/signup/iam/installations.go
@@ -0,0 +1,65 @@
+package iam
+
+import (
+	"encoding/json"
+	"fmt"
+
+	workers "github.com/powertoolsdev/workers-orgs/internal"
+)
+
+func installationsIAMName(orgID string) string {
+	return fmt.Sprintf("org-installations-access-%s", orgID)
+}
+
+func installationsIAMPolicy(bucketName string, orgID string) ([]byte, error) {
+	policy := iamRolePolicy{
+		Version: defaultIAMPolicyVersion,
+		Statement: []iamRoleStatement{
+			{
+				Effect: "Allow",
+				Action: []string{
+					"s3:*",
+				},
+				Resource: fmt.Sprintf("arn:aws:s3:::%s/orgID=%s/*", bucketName, orgID),
+			},
+		},
+	}
+
+	byts, err := json.Marshal(policy)
+	if err != nil {
+		return nil, fmt.Errorf("unable to convert policy to json: %w", err)
+	}
+	return byts, nil
+}
+
+func installationsIAMTrustPolicy(cfg workers.Config) ([]byte, error) {
+	trustPolicy := iamRoleTrustPolicy{
+		Version: defaultIAMPolicyVersion,
+		Statement: []iamRoleTrustStatement{
+			{
+				Action: "sts:AssumeRoleWithWebIdentity",
+				Effect: "Allow",
+				Sid:    "",
+				Principal: struct {
+					Federated string `json:"Federated,omitempty"`
+				}{
+					Federated: cfg.OrgsIAMOidcProviderArn,
+				},
+				Condition: struct {
+					StringEquals map[string]string `json:"StringEquals"`
+				}{
+					StringEquals: map[string]string{
+						fmt.Sprintf("%s:sub", cfg.OrgsIAMOidcProviderURL): "system:serviceaccount:default:*",
+					},
+				},
+			},
+		},
+	}
+
+	byts, err := json.Marshal(trustPolicy)
+	if err != nil {
+		return nil, fmt.Errorf("unable to create trust policy: %w", err)
+	}
+
+	return byts, nil
+}
