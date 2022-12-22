532bfc51e61e09d496f0946edc3dc112abac62d5
Jon Morehouse <MorehouseJ09@gmail.com>
2022-12-22 12:52:45 -0800

    chore: rewire service outputs (#65)
    * chore: rewire service outputs
    
    * remove account id

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -29,33 +29,3 @@ output "cluster_name" {
 output "cluster_gh_role_arn" {
   value = nonsensitive(data.tfe_outputs.infra-eks-nuon.values.github_action_role_arn)
 }
-
-output "installations_bucket_name" {
-  value = nonsensitive(data.tfe_outputs.infra-orgs.values.installations_bucket_name)
-}
-
-output "installations_k8s_role_arn" {
-  value = nonsensitive(data.tfe_outputs.infra-orgs.values.install_k8s_role_arn)
-}
-
-output "orgs_k8s_role_arn" {
-  # NOTE: you need to update `infra-eks` to add your service into the auth map
-  value = nonsensitive(data.tfe_outputs.infra-eks-orgs.values.auth_map_additional_role_arns["eks-workers-installs"])
-}
-
-output "orgs_k8s_cluster_id" {
-  value = nonsensitive(data.tfe_outputs.infra-eks-orgs.values.cluster_id)
-}
-
-output "orgs_k8s_ca_data" {
-  value = nonsensitive(data.tfe_outputs.infra-eks-orgs.values.cluster_certificate_authority_data)
-}
-
-output "orgs_k8s_public_endpoint" {
-  value = nonsensitive(data.tfe_outputs.infra-eks-orgs.values.cluster_endpoint)
-}
-
-output "orgs_account_id" {
-  value = local.accounts[format("orgs-%s", var.env)].id
-
-}
---
+++ infra/outputs_service.tf
@@ -0,0 +1,24 @@
+output "installations_bucket_name" {
+  value = nonsensitive(data.tfe_outputs.infra-orgs.values.buckets.installations.name)
+}
+
+output "installations_k8s_role_arn" {
+  value = nonsensitive(data.tfe_outputs.infra-orgs.values.installations.k8s_access_role_arn)
+}
+
+output "orgs_k8s_role_arn" {
+  # NOTE: you need to update `infra-eks` to add your service into the auth map
+  value = nonsensitive(data.tfe_outputs.infra-eks-orgs.values.auth_map_additional_role_arns["eks-workers-installs"])
+}
+
+output "orgs_k8s_cluster_id" {
+  value = nonsensitive(data.tfe_outputs.infra-orgs.values.k8s.cluster_id)
+}
+
+output "orgs_k8s_ca_data" {
+  value = nonsensitive(data.tfe_outputs.infra-orgs.values.k8s.ca_data)
+}
+
+output "orgs_k8s_public_endpoint" {
+  value = nonsensitive(data.tfe_outputs.infra-orgs.values.k8s.public_endpoint)
+}
