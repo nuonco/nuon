84e3764227bc824408a86b4d13369ac1dd2f5749
Chris Potter <chris@powertools.dev>
2022-12-22 14:05:25 -0500

    style: move get_org to separate file (#27)
    Signed-off-by: Chris Potter <chris@powertools.dev>
    
    Signed-off-by: Chris Potter <chris@powertools.dev>

--- internal/server/orgs/server.go
+++ internal/server/orgs/server.go
@@ -4,7 +4,6 @@ import (
 	"context"
 
 	"github.com/powertoolsdev/api/internal/repos"
-	"github.com/powertoolsdev/api/internal/request"
 	orgv1 "github.com/powertoolsdev/protos/api/generated/types/org/v1"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
@@ -23,26 +22,6 @@ func NewOrgServer(db *gorm.DB) server {
 
 var _ orgv1.OrgsServiceServer
 
-func (s *server) GetOrg(ctx context.Context, req *orgv1.GetOrgRequest) (*orgv1.GetOrgResponse, error) {
-	orgID, err := request.ParseID(req.OrgId)
-	if err != nil {
-		return nil, err
-	}
-
-	org, err := s.repo.Get(ctx, orgID)
-	if err != nil {
-		return nil, err
-	}
-
-	orgProto, err := orgModelToProto(org)
-	if err != nil {
-		return nil, err
-	}
-
-	return &orgv1.GetOrgResponse{
-		Org: orgProto,
-	}, nil
-}
 func (s *server) GetOrgsByUser(context.Context, *orgv1.GetOrgsByUserRequest) (*orgv1.GetOrgsByUserResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetOrgsByUser not implemented")
 }
