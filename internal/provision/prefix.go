160940f82baa1edd772d54631cd1b8c580030d8b
Jon Morehouse <MorehouseJ09@gmail.com>
2022-12-22 12:24:25 -0800

    fix: remove references to installations prefix (#64)

--- internal/provision/prefix.go
+++ internal/provision/prefix.go
@@ -5,9 +5,9 @@ import (
 )
 
 func getInstallationPrefix(orgShortID, appShortID, installShortID string) string {
-	return fmt.Sprintf("installations/org=%s/app=%s/install=%s", orgShortID, appShortID, installShortID)
+	return fmt.Sprintf("org=%s/app=%s/install=%s", orgShortID, appShortID, installShortID)
 }
 
 func getS3Prefix(bucket, orgID, appID, installID string) string {
-	return fmt.Sprintf("s3://%s/installations/org=%s/app=%s/install=%s", bucket, orgID, appID, installID)
+	return fmt.Sprintf("s3://%s/org=%s/app=%s/install=%s", bucket, orgID, appID, installID)
 }
