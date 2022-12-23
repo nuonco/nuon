806ba894baca2324b437745c7ed55c2a2d1f9ab9
Jon Morehouse <MorehouseJ09@gmail.com>
2022-12-23 15:31:33 -0800

    fix: set correct region values (#74)

--- infra/outputs_service.tf
+++ infra/outputs_service.tf
@@ -2,10 +2,16 @@ output "installations_bucket_name" {
   value = nonsensitive(data.tfe_outputs.infra-orgs.values.buckets.installations.name)
 }
 
+output "installations_bucket_region" {
+  value = nonsensitive(data.tfe_outputs.infra-orgs.values.buckets.installations.region)
+}
+
+# this role is added to the sandbox, and grants Nuon access to do things in it
 output "installations_k8s_role_arn" {
   value = nonsensitive(data.tfe_outputs.infra-orgs.values.installations.k8s_access_role_arn)
 }
 
+# the following values are for interacting with the orgs k8s cluster
 output "orgs_k8s_role_arn" {
   # NOTE: you need to update `infra-eks` to add your service into the auth map
   value = nonsensitive(data.tfe_outputs.infra-eks-orgs.values.auth_map_additional_role_arns["eks-workers-installs"])
@@ -23,6 +29,7 @@ output "orgs_k8s_public_endpoint" {
   value = nonsensitive(data.tfe_outputs.infra-orgs.values.k8s.public_endpoint)
 }
 
+# the following values are for using IAM roles related to an org
 output "orgs_instance_role_template" {
   value = nonsensitive(data.tfe_outputs.infra-orgs.values.org_iam_role_name_templates.instances_access)
 }
