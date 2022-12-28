e65e32d0da6e75da8a56ed36138d5f805cfe0e34
Jon Morehouse <MorehouseJ09@gmail.com>
2022-12-28 15:56:17 -0800

    feat: integrate connect-go  (#7)
    Setup connect-go and a basic handler for ping / healthchecks.

---
+++ cmd/server.go
@@ -0,0 +1,81 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/powertoolsdev/go-common/config"
+	"github.com/powertoolsdev/orgs-api/internal"
+	statusserver "github.com/powertoolsdev/orgs-api/internal/servers/status"
+	"github.com/powertoolsdev/protos/orgs-api/generated/types/status/v1/statusv1connect"
+	"github.com/spf13/cobra"
+	"go.uber.org/zap"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+)
+
+var runServerCmd = &cobra.Command{
+	Use:   "server",
+	Short: "run server",
+	Run:   runServer,
+}
+
+//nolint:gochecknoinits
+func init() {
+	rootCmd.AddCommand(runServerCmd)
+}
+
+// registerStatusServer registers the status service handler on the provided mux
+func registerStatusServer(mux *http.ServeMux, cfg *internal.Config) error {
+	srv, err := statusserver.New(statusserver.WithConfig(cfg))
+	if err != nil {
+		return fmt.Errorf("unable to initialize status server: %w", err)
+	}
+
+	path, handler := statusv1connect.NewStatusServiceHandler(srv)
+	mux.Handle(path, handler)
+	return nil
+}
+
+//nolint:all
+func runServer(cmd *cobra.Command, args []string) {
+	var cfg internal.Config
+
+	if err := config.LoadInto(cmd.Flags(), &cfg); err != nil {
+		log.Fatalf("failed to load config: %s", err)
+	}
+
+	var (
+		l   *zap.Logger
+		err error
+	)
+	switch cfg.Env {
+	case config.Development:
+		l, err = zap.NewDevelopment()
+	default:
+		l, err = zap.NewProduction()
+	}
+	zap.ReplaceGlobals(l)
+	if err != nil {
+		fmt.Printf("failed to instantiate logger: %v\n", err)
+	}
+
+	mux := http.NewServeMux()
+	if err := registerStatusServer(mux, &cfg); err != nil {
+		l.Fatal("unable to register status server:", zap.Error(err))
+	}
+
+	l.Info("server starting: ",
+		zap.String("host", cfg.HTTPAddress),
+		zap.String("port", cfg.HTTPPort))
+
+	if err := http.ListenAndServe(
+		fmt.Sprintf("%s:%s", cfg.HTTPAddress, cfg.HTTPPort),
+
+		// Use h2c so we can serve HTTP/2 without TLS.
+		h2c.NewHandler(mux, &http2.Server{}),
+	); err != nil {
+		l.Fatal("error on listen and server", zap.Error(err))
+	}
+}
