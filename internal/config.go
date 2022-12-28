006727fc3d0c0fd1f3b0464e0d20cfb395ab1bc6
Jon Morehouse <MorehouseJ09@gmail.com>
2022-12-28 15:56:17 -0800

    feat: integrate connect-go  (#7)
    Setup connect-go and a basic handler for ping / healthchecks.

--- internal/config.go
+++ internal/config.go
@@ -7,30 +7,32 @@ import (
 
 //nolint:gochecknoinits
 func init() {
-	config.RegisterDefault("temporal_host", "localhost:7233")
-	config.RegisterDefault("temporal_namespace", "default")
-
 	// deployment defaults
 	config.RegisterDefault("waypoint_token_secret_namespace", "default")
 	config.RegisterDefault("waypoint_org_server_root_domain", "orgs-stage.nuon.co")
+
+	config.RegisterDefault("http_port", "8080")
+	config.RegisterDefault("http_address", "0.0.0.0")
 }
 
 type Config struct {
-	config.Base       `config:",squash"`
-	TemporalHost      string `config:"temporal_host" validate:"required"`
-	TemporalNamespace string `config:"temporal_namespace" validate:"required"`
+	config.Base `config:",squash"`
 
-	DeploymentsBucket   string `config:"deployments_bucket" validate:"required"`
-	InstallationsBucket string `config:"installations_bucket" validate:"required"`
+	// configs for starting and introspecting service
+	GitRef      string `config:"git_ref" validate:"required"`
+	HTTPPort    string `config:"http_port" validate:"required"`
+	HTTPAddress string `config:"http_address" validate:"required"`
 
 	// waypoint configuration
 	WaypointTokenSecretNamespace string `config:"waypoint_token_secret_namespace" validate:"required"`
 	WaypointTokenSecretTemplate  string `config:"waypoint_token_secret_template" validate:"required"`
 	WaypointServerRootDomain     string `config:"waypoint_server_root_domain" validate:"required"`
 
-	// org IAM role template names
+	// org IAM role template names and buckets for accessing state
+	DeploymentsBucket             string `config:"deployments_bucket" validate:"required"`
 	OrgsDeploymentsRoleTemplate   string `config:"orgs_deployments_role_template" validate:"required"`
 	OrgsInstallationsRoleTemplate string `config:"orgs_installations_role_template" validate:"required"`
+	InstallationsBucket           string `config:"installations_bucket" validate:"required"`
 
 	// configs needed to access the orgs cluster, which runs all org runner/servers
 	OrgsK8sClusterID      string `config:"orgs_k8s_cluster_id" json:"orgs_k8s_cluster_id" validate:"required"`
