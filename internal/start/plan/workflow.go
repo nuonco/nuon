5dff819d64835be5f12c0461084cf0d79e2f1f3a
Jon Morehouse <MorehouseJ09@gmail.com>
2022-12-27 16:14:20 -0800

    feat: add basic plan functionality (#71)
    * feat: add plan creator
    
    * feat: build out structure of plan generation
    
    * build out creator
    
    * feat add static config builder
    
    * fixes for linting
    
    * fix ci app tokens for new go-uploader version

--- internal/start/plan/workflow.go
+++ internal/start/plan/workflow.go
@@ -1,6 +1,7 @@
 package plan
 
 import (
+	"fmt"
 	"time"
 
 	"go.temporal.io/sdk/workflow"
@@ -13,7 +14,6 @@ const (
 	defaultActivityTimeout = time.Second * 5
 )
 
-//nolint:all
 func configureActivityOptions(ctx workflow.Context) workflow.Context {
 	activityOpts := workflow.ActivityOptions{
 		ScheduleToCloseTimeout: defaultActivityTimeout,
@@ -34,13 +34,45 @@ func NewWorkflow(cfg workers.Config) *wkflow {
 func (w *wkflow) Plan(ctx workflow.Context, req *planv1.PlanRequest) (*planv1.PlanResponse, error) {
 	resp := &planv1.PlanResponse{}
 	l := workflow.GetLogger(ctx)
-	//ctx = configureActivityOptions(ctx)
-	//act := NewActivities()
+	ctx = configureActivityOptions(ctx)
+	act := NewActivities()
 
 	if err := req.Validate(); err != nil {
 		return resp, err
 	}
 
+	cpReq := CreatePlanRequest{
+		OrgID:                          req.OrgId,
+		AppID:                          req.AppId,
+		DeploymentID:                   req.DeploymentId,
+		DeploymentsBucketPrefix:        getS3Prefix(req),
+		DeploymentsBucketAssumeRoleARN: fmt.Sprintf(w.cfg.OrgsDeploymentsRoleTemplate, req.OrgId),
+		Component:                      req.Component,
+		Config:                         w.cfg,
+	}
+	cpResp, err := execCreatePlan(ctx, act, cpReq)
+	if err != nil {
+		return resp, fmt.Errorf("unable to create plan: %w", err)
+	}
+	resp.Plan = cpResp.Plan
+
 	l.Debug("successfully created plan for build")
+	return resp, nil
+}
+
+func execCreatePlan(
+	ctx workflow.Context,
+	act *Activities,
+	req CreatePlanRequest,
+) (CreatePlanResponse, error) {
+	l := workflow.GetLogger(ctx)
+	var resp CreatePlanResponse
+
+	l.Debug("executing create plan activity", "request", req)
+	fut := workflow.ExecuteActivity(ctx, act.CreatePlan, req)
+	if err := fut.Get(ctx, &resp); err != nil {
+		return resp, err
+	}
+
 	return resp, nil
 }
