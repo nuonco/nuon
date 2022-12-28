c4a8fe6ada4147ac225eac6c86e17cd2866f914f
Jon Morehouse <MorehouseJ09@gmail.com>
2022-12-28 14:47:43 -0800

    feat: add connect-go generation (#65)

--- tools.go
+++ tools.go
@@ -4,5 +4,8 @@
 package tools
 
 import (
+	_ "github.com/bufbuild/buf/cmd/buf"
+	_ "github.com/bufbuild/connect-go/cmd/protoc-gen-connect-go"
 	_ "github.com/srikrsna/protoc-gen-gotag"
+	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
 )
