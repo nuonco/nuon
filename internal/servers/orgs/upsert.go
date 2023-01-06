40eecec0a7fab5523aca9711362d684005ba5846
Jon Morehouse <MorehouseJ09@gmail.com>
2023-01-06 12:47:13 -0800

    feat: add upsert orgs handler (#52)
    feat: add upsert org

--- internal/servers/orgs/upsert.go
+++ internal/servers/orgs/upsert.go
@@ -2,8 +2,12 @@ package orgs
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/bufbuild/connect-go"
+	"github.com/powertoolsdev/api/internal/models"
+	"github.com/powertoolsdev/api/internal/servers"
+	"github.com/powertoolsdev/go-generics"
 	orgv1 "github.com/powertoolsdev/protos/api/generated/types/org/v1"
 )
 
@@ -11,6 +15,23 @@ func (s *server) UpsertOrg(
 	ctx context.Context,
 	req *connect.Request[orgv1.UpsertOrgRequest],
 ) (*connect.Response[orgv1.UpsertOrgResponse], error) {
-	res := connect.NewResponse(&orgv1.UpsertOrgResponse{})
+	org, err := s.Svc.UpsertOrg(ctx, models.OrgInput{
+		ID:      generics.ToPtr(req.Msg.Id),
+		Name:    req.Msg.Name,
+		OwnerID: req.Msg.OwnerId,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("unable to upsert org: %w", err)
+	}
+
+	res := connect.NewResponse(&orgv1.UpsertOrgResponse{
+		Org: &orgv1.Org{
+			Id:        org.ID.String(),
+			Name:      org.Name,
+			OwnerId:   org.CreatedByID,
+			UpdatedAt: servers.TimeToDatetime(org.UpdatedAt),
+			CreatedAt: servers.TimeToDatetime(org.CreatedAt),
+		},
+	})
 	return res, nil
 }
---
+++ internal/servers/timestamp.go
@@ -0,0 +1,20 @@
+package servers
+
+import (
+	"time"
+
+	"google.golang.org/genproto/googleapis/type/datetime"
+)
+
+func TimeToDatetime(ts time.Time) *datetime.DateTime {
+	return &datetime.DateTime{
+		Year:       int32(ts.Year()),
+		Month:      int32(ts.Month()),
+		Day:        int32(ts.Day()),
+		Hours:      int32(ts.Hour()),
+		Minutes:    int32(ts.Minute()),
+		Seconds:    int32(ts.Second()),
+		Nanos:      int32(ts.Nanosecond()),
+		TimeOffset: &datetime.DateTime_UtcOffset{},
+	}
+}
