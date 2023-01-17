92d55b6306967c8092eb5052cdcff277ecbf2e71
Jon Morehouse <MorehouseJ09@gmail.com>
2023-01-17 10:55:36 -0800

    chore: add provision-psql script (#62)
    * chore: add provision-psql script
    
    * update

--- infra/outputs_service.tf
+++ infra/outputs_service.tf
@@ -6,22 +6,31 @@ output "db_instance_host" {
   value = module.primary.db_instance_address
 }
 
-output "db_instance_name" {
+output "db_instance_admin_name" {
   value = module.primary.db_instance_name
+}
+
+output "db_instance_name" {
+  value = "api"
 }
 
 output "db_instance_port" {
   value = module.primary.db_instance_port
 }
 
-output "db_instance_username" {
+output "db_instance_admin_username" {
   sensitive = true
   value     = module.primary.db_instance_username
 }
 
-output "db_instance_password" {
+output "db_instance_admin_password" {
   sensitive = true
   value     = module.primary.db_instance_password
+}
+
+output "db_instance_username" {
+  sensitive = true
+  value     = "api"
 }
 
 output "certificate_arn" {
