5cccd2573286bf2e226de4a3ed533bd38f9d4c1b
Maria Paktiti <maria@powertools.dev>
2023-01-25 12:10:32 -0500

    fix: upsertComponent and githubConfig (#93)
    * fix: upsertComponent should work with or without githubConfig
    
    Signed-off-by: Maria Paktiti <maria@powertools.dev>

--- internal/models/models_gen.go
+++ internal/models/models_gen.go
@@ -111,21 +111,13 @@ type ComponentEdge struct {
 }
 
 type ComponentInput struct {
-	AppID       string        `json:"appId" faker:"uuid_hyphenated"`
-	ID          *string       `json:"id" faker:"uuid_hyphenated"`
-	Name        string        `json:"name"`
-	BuildImage  string        `json:"buildImage"`
-	CreatedByID string        `json:"created_by_id"`
-	Type        ComponentType `json:"type"`
-	// Github repository is for this component
-	GithubRepo *string `json:"githubRepo"`
-	// Directory path to the container file
-	GithubDir *string `json:"githubDir"`
-	// The Github repository branch for this component
-	GithubBranch *string `json:"githubBranch"`
-	// The owner of the Github repository for this component
-	GithubRepoOwner *string            `json:"githubRepoOwner"`
-	GithubConfig    *GithubConfigInput `json:"githubConfig"`
+	AppID        string             `json:"appId" faker:"uuid_hyphenated"`
+	ID           *string            `json:"id" faker:"uuid_hyphenated"`
+	Name         string             `json:"name"`
+	BuildImage   string             `json:"buildImage"`
+	CreatedByID  string             `json:"created_by_id"`
+	Type         ComponentType      `json:"type"`
+	GithubConfig *GithubConfigInput `json:"githubConfig"`
 }
 
 // Filters returned Connection results
