477bbdfd06a83ff4aa8e2054c1211a09d35c37a2
Maria Paktiti <maria@powertools.dev>
2023-01-30 19:35:52 -0500

    build: bump protos to 0.52.0 (#111)
    * build: bump protos to 0.52.0
    * fix: add news endpoints at deployment server
    
    Signed-off-by: Maria Paktiti <maria@powertools.dev>

--- internal/servers/deployments/get.go
+++ internal/servers/deployments/get.go
@@ -29,6 +29,26 @@ func (s *server) GetDeployment(
 	}), nil
 }
 
+func (s *server) GetDeploymentsByInstalls(
+	ctx context.Context,
+	req *connect.Request[deploymentv1.GetDeploymentsByInstallsRequest],
+) (*connect.Response[deploymentv1.GetDeploymentsByInstallsResponse], error) {
+	// run protobuf validations
+	if err := req.Msg.Validate(); err != nil {
+		return nil, fmt.Errorf("input validation failed: %w", err)
+	}
+
+	// TODO: add new service to retrieve deployments by install IDs
+	deployments, _, err := s.Svc.GetComponentDeployments(ctx, req.Msg.InstallIds, &models.ConnectionOptions{})
+	if err != nil {
+		return nil, fmt.Errorf("unable to get install deployments: %w", err)
+	}
+
+	return connect.NewResponse(&deploymentv1.GetDeploymentsByInstallsResponse{
+		Deployments: converters.DeploymentModelsToProtos(deployments),
+	}), nil
+}
+
 func (s *server) GetDeploymentsByComponents(
 	ctx context.Context,
 	req *connect.Request[deploymentv1.GetDeploymentsByComponentsRequest],
@@ -44,6 +64,26 @@ func (s *server) GetDeploymentsByComponents(
 	}
 
 	return connect.NewResponse(&deploymentv1.GetDeploymentsByComponentsResponse{
+		Deployments: converters.DeploymentModelsToProtos(deployments),
+	}), nil
+}
+
+func (s *server) GetDeploymentsByApps(
+	ctx context.Context,
+	req *connect.Request[deploymentv1.GetDeploymentsByAppsRequest],
+) (*connect.Response[deploymentv1.GetDeploymentsByAppsResponse], error) {
+	// run protobuf validations
+	if err := req.Msg.Validate(); err != nil {
+		return nil, fmt.Errorf("input validation failed: %w", err)
+	}
+
+	// TODO: add new service to retrieve deployments by app IDs
+	deployments, _, err := s.Svc.GetComponentDeployments(ctx, req.Msg.AppIds, &models.ConnectionOptions{})
+	if err != nil {
+		return nil, fmt.Errorf("unable to get install deployments: %w", err)
+	}
+
+	return connect.NewResponse(&deploymentv1.GetDeploymentsByAppsResponse{
 		Deployments: converters.DeploymentModelsToProtos(deployments),
 	}), nil
 }
