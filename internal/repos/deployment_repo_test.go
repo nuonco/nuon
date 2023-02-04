a5ec035d7363116340ae25c3c5b422bc517a2e1a
Maria Paktiti <maria@powertools.dev>
2023-02-03 19:32:20 -0500

    test: add repo tests for GetAppDeployments & GetInstallDeployments (#139)
    * test: add unit tests for GetInstallDeployments
    * chore: fix log message
    
    Signed-off-by: Maria Paktiti <maria@powertools.dev>

--- internal/repos/deployment_repo_test.go
+++ internal/repos/deployment_repo_test.go
@@ -20,6 +20,7 @@ func createDeployment(ctx context.Context, t *testing.T, state repoTestState) *m
 	assert.NotNil(t, deployment)
 	return deployment
 }
+
 func TestGetDeployment(t *testing.T) {
 	execRepoTests(t, []repoTest{
 		{
@@ -78,6 +79,88 @@ func TestListByComponents(t *testing.T) {
 
 				state.ctxCloseFn()
 				fetchedDeployment, page, err := state.deploymentRepo.ListByComponents(ctx, []uuid.UUID{deployment.ComponentID}, &models.ConnectionOptions{})
+				assert.Error(t, err)
+				assert.Nil(t, fetchedDeployment)
+				assert.Nil(t, page)
+			},
+		},
+	})
+}
+
+func TestListByApps(t *testing.T) {
+	execRepoTests(t, []repoTest{
+		{
+			desc: "should get a list of deployments successfully",
+			fn: func(ctx context.Context, state repoTestState) {
+				origDeployment := createDeployment(ctx, t, state)
+				origDeployment2 := createDeployment(ctx, t, state)
+
+				app := createApp(ctx, t, state)
+				origDeployment.Component.AppID = app.ID
+				origDeployment2.Component.AppID = app.ID
+
+				uuids := []uuid.UUID{origDeployment.Component.App.ID, origDeployment2.Component.App.ID}
+
+				deployments, page, err := state.deploymentRepo.ListByApps(ctx, uuids, &models.ConnectionOptions{})
+				assert.NoError(t, err)
+				assert.NotNil(t, deployments)
+				assert.NotNil(t, page)
+				assert.Equal(t, 2, len(deployments))
+			},
+		},
+		{
+			desc: "should error with canceled context",
+			fn: func(ctx context.Context, state repoTestState) {
+				deployment := createDeployment(ctx, t, state)
+				app := createApp(ctx, t, state)
+				deployment.Component.AppID = app.ID
+
+				state.ctxCloseFn()
+				fetchedDeployment, page, err := state.deploymentRepo.ListByApps(ctx, []uuid.UUID{deployment.Component.AppID}, &models.ConnectionOptions{})
+				assert.Error(t, err)
+				assert.Nil(t, fetchedDeployment)
+				assert.Nil(t, page)
+			},
+		},
+	})
+}
+
+func TestListByInstalls(t *testing.T) {
+	execRepoTests(t, []repoTest{
+		{
+			desc: "should get a list of deployments successfully",
+			fn: func(ctx context.Context, state repoTestState) {
+				install := createInstall(ctx, t, state)
+				install2 := createInstall(ctx, t, state)
+				origDeployment := createDeployment(ctx, t, state)
+				origDeployment2 := createDeployment(ctx, t, state)
+
+				// update Component.AppID so it's the same as the Install's
+				origDeployment.Component.AppID = install.AppID
+				origDeployment2.Component.AppID = install2.AppID
+				origDeployment.Component.App = install.App
+				origDeployment2.Component.App = install2.App
+				_, _ = state.componentRepo.Update(ctx, &origDeployment.Component)
+				_, _ = state.componentRepo.Update(ctx, &origDeployment2.Component)
+
+				uuids := []uuid.UUID{install.ID, install2.ID}
+
+				deployments, page, err := state.deploymentRepo.ListByInstalls(ctx, uuids, &models.ConnectionOptions{})
+				assert.NoError(t, err)
+				assert.NotNil(t, deployments)
+				assert.NotNil(t, page)
+				assert.Equal(t, 2, len(deployments))
+			},
+		},
+		{
+			desc: "should error with canceled context",
+			fn: func(ctx context.Context, state repoTestState) {
+				deployment := createDeployment(ctx, t, state)
+				install := createInstall(ctx, t, state)
+				deployment.Component.AppID = install.AppID
+
+				state.ctxCloseFn()
+				fetchedDeployment, page, err := state.deploymentRepo.ListByInstalls(ctx, []uuid.UUID{install.ID}, &models.ConnectionOptions{})
 				assert.Error(t, err)
 				assert.Nil(t, fetchedDeployment)
 				assert.Nil(t, page)
