4798931afcc08bf2fec6e01199fc162448008a79
Jon Morehouse <MorehouseJ09@gmail.com>
2023-02-04 23:21:04 -0600

    chore: bump sandbox version (#142)

--- internal/workflows/installs.go
+++ internal/workflows/installs.go
@@ -12,7 +12,7 @@ import (
 //go:generate -command mockgen go run github.com/golang/mock/mockgen
 //go:generate mockgen -destination=mock_installs.go -source=installs.go -package=workflows
 const (
-	sandboxVersion string = "0.10.2"
+	sandboxVersion string = "0.10.3"
 	sandboxName    string = "aws-eks"
 )
 
