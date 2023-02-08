f0b29babd99cc91c0465c90e8b9d3080bb37bfba
Chris Potter <chris.potter.3d+github@gmail.com>
2023-02-08 17:06:21 -0500

    feat: adds go-workers-meta (#122)
    * feat: adds go-workers-meta
    
    Signed-off-by: Chris Potter <chris@powertools.dev>
    
    * test: update tests for new activities
    
    Signed-off-by: Chris Potter <chris@powertools.dev>
    
    * fix: add assume role repo to dependency
    
    Signed-off-by: Chris Potter <chris@powertools.dev>
    
    * style: linter
    
    Signed-off-by: Chris Potter <chris@powertools.dev>
    
    * style: trailing space
    
    Signed-off-by: Chris Potter <chris@powertools.dev>
    
    * style: linting
    
    Signed-off-by: Chris Potter <chris@powertools.dev>
    
    * feat: format error
    
    Signed-off-by: Chris Potter <chris@powertools.dev>
    
    ---------
    
    Signed-off-by: Chris Potter <chris@powertools.dev>

--- internal/config.go
+++ internal/config.go
@@ -11,6 +11,9 @@ func init() {
 	config.RegisterDefault("temporal_namespace", "default")
 	config.RegisterDefault("waypoint_token_namespace", "default")
 	config.RegisterDefault("waypoint_server_root_domain", "orgs-stage.nuon.co")
+
+	//org bucket default
+	config.RegisterDefault("org_orgs_bucket_name", "nuon-orgs-stage")
 }
 
 type Config struct {
@@ -20,6 +23,8 @@ type Config struct {
 	TemporalNamespace string `config:"temporal_namespace"`
 
 	OrgsEcrAccessRoleArn string `config:"orgs_ecr_access_role_arn" validate:"required" json:"orgs_ecr_access_iam_role_arn"`
+	OrgsBucketName       string `config:"org_orgs_bucket_name" json:"org_orgs_bucket_name" validate:"required"`
+	OrgsRoleTemplate     string `config:"orgs_role_template" validate:"required"`
 
 	WaypointTokenNamespace   string `config:"waypoint_token_namespace" json:"waypoint_token_namespace" validate:"required"`
 	WaypointServerRootDomain string `config:"waypoint_server_root_domain" json:"waypoint_server_root_domain" validate:"required"`
