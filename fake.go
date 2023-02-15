1b526e5cf62292e10e08d998d07ae7357da1cd0b
Jon Morehouse <MorehouseJ09@gmail.com>
2023-02-15 17:15:29 -0600

    feat: add built in support for shortID (#3)
    * feat: add built in support for shortID
    
    * fix

--- fake.go
+++ fake.go
@@ -1,13 +1,31 @@
 package generics
 
 import (
+	"reflect"
+
 	"github.com/go-faker/faker/v4"
+	"github.com/google/uuid"
+	"github.com/powertoolsdev/go-common/shortid"
 )
+
+func registerFakerProviders() {
+	_ = faker.AddProvider("shortID", func(v reflect.Value) (interface{}, error) {
+		uid := uuid.New()
+		shortID, err := shortid.ParseString(uid.String())
+		if err != nil {
+			panic(err)
+		}
+
+		return shortID, nil
+	})
+}
 
 // GetFakeObj returns a faked instance of type T
 // will panic on error
 // meant exclusively for testing
 func GetFakeObj[T any]() T {
+	registerFakerProviders()
+
 	var obj T
 	err := faker.FakeData(&obj)
 	if err != nil {
