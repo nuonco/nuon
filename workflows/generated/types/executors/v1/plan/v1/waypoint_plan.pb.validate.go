013e9ec329d384cf646862010677aecbc898f62b
Jon Morehouse <MorehouseJ09@gmail.com>
2023-02-20 18:24:46 -0600

    feat: add git source into waypoint plan (#177)

--- workflows/generated/types/executors/v1/plan/v1/waypoint_plan.pb.validate.go
+++ workflows/generated/types/executors/v1/plan/v1/waypoint_plan.pb.validate.go
@@ -231,6 +231,35 @@ func (m *WaypointPlan) validate(all bool) error {
 		}
 	}
 
+	if all {
+		switch v := interface{}(m.GetGitSource()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, WaypointPlanValidationError{
+					field:  "GitSource",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, WaypointPlanValidationError{
+					field:  "GitSource",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetGitSource()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return WaypointPlanValidationError{
+				field:  "GitSource",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
 	if len(errors) > 0 {
 		return WaypointPlanMultiError(errors)
 	}
