02704b83fa55ed6f3ae8564c8ae8051faf36d98b
Jon Morehouse <MorehouseJ09@gmail.com>
2023-02-24 13:42:28 -0800

    feat: add repo response to app (#186)
    chore: add response

--- workflows/generated/types/apps/v1/provision.pb.validate.go
+++ workflows/generated/types/apps/v1/provision.pb.validate.go
@@ -183,6 +183,35 @@ func (m *ProvisionResponse) validate(all bool) error {
 
 	var errors []error
 
+	if all {
+		switch v := interface{}(m.GetRepository()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, ProvisionResponseValidationError{
+					field:  "Repository",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, ProvisionResponseValidationError{
+					field:  "Repository",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetRepository()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return ProvisionResponseValidationError{
+				field:  "Repository",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
 	if len(errors) > 0 {
 		return ProvisionResponseMultiError(errors)
 	}
