5c634df0a13ac5c18d4919b21a73b08cedc5d222
Maria Paktiti <maria@powertools.dev>
2023-03-01 12:47:48 -0500

    refactor: remove component build image, type, vcs_config (#182)
    * refactor: bump protos + remove redundant component fields
    * refactor: use component config object for deployments
    * refactor: use main as default branch
    * refactor: add migration file to drop columns
    * refactor: retrieve github commit for both public and private repos
    * build: bump protos version to 0.76.2
    * style: update pending todo comments
    
    Signed-off-by: Maria Paktiti <maria@powertools.dev>

--- internal/servers/converters/component.go
+++ internal/servers/converters/component.go
@@ -9,36 +9,6 @@ import (
 	"google.golang.org/protobuf/encoding/protojson"
 )
 
-func ComponentTypeToProto(input models.ComponentType) componentv1.ComponentType {
-	switch input {
-	case models.ComponentTypeGithubRepo:
-		return componentv1.ComponentType_COMPONENT_TYPE_GITHUB_REPO
-	case models.ComponentTypePublicImage:
-		return componentv1.ComponentType_COMPONENT_TYPE_PUBLIC_IMAGE
-	case models.ComponentTypeHelm:
-		return componentv1.ComponentType_COMPONENT_TYPE_HELM
-	case models.ComponentTypeTerraform:
-		return componentv1.ComponentType_COMPONENT_TYPE_TERRAFORM
-	}
-
-	return componentv1.ComponentType_COMPONENT_TYPE_UNSPECIFIED
-}
-
-func ProtoToComponentType(input componentv1.ComponentType) models.ComponentType {
-	switch input {
-	case componentv1.ComponentType_COMPONENT_TYPE_GITHUB_REPO:
-		return models.ComponentTypeGithubRepo
-	case componentv1.ComponentType_COMPONENT_TYPE_PUBLIC_IMAGE:
-		return models.ComponentTypePublicImage
-	case componentv1.ComponentType_COMPONENT_TYPE_HELM:
-		return models.ComponentTypeHelm
-	case componentv1.ComponentType_COMPONENT_TYPE_TERRAFORM:
-		return models.ComponentTypeTerraform
-	}
-
-	return ""
-}
-
 // Component model to proto converts component domain model into component proto message
 func ComponentModelToProto(component *models.Component) (*componentv1.ComponentRef, error) {
 	config := &componentConfig.Component{}
@@ -50,22 +20,9 @@ func ComponentModelToProto(component *models.Component) (*componentv1.ComponentR
 		Id:              component.ID.String(),
 		Name:            component.Name,
 		CreatedById:     component.CreatedByID,
-		BuildImage:      component.BuildImage,
 		ComponentConfig: config,
-		Type:            ComponentTypeToProto(models.ComponentType(component.Type)),
 		UpdatedAt:       TimeToDatetime(component.UpdatedAt),
 		CreatedAt:       TimeToDatetime(component.CreatedAt),
-	}
-
-	if component.GithubConfig != nil {
-		res.VcsConfig = &componentv1.ComponentRef_GithubConfig{
-			GithubConfig: &componentv1.GithubConfig{
-				Branch:    component.GithubConfig.Branch,
-				Directory: component.GithubConfig.Directory,
-				Repo:      component.GithubConfig.Repo,
-				RepoOwner: component.GithubConfig.RepoOwner,
-			},
-		}
 	}
 
 	return res, nil
