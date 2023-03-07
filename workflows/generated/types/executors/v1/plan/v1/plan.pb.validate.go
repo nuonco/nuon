2dcdf1f63610ded388388de9e954555e7db066ee
Chris Potter <chris.potter.3d+github@gmail.com>
2023-03-07 12:29:58 -0500

    fix: update plan proto (#197)
    fix: update plan proto with new RunnerPlan
    
    Signed-off-by: Chris Potter <chris@powertools.dev>

--- workflows/generated/types/executors/v1/plan/v1/plan.pb.validate.go
+++ workflows/generated/types/executors/v1/plan/v1/plan.pb.validate.go
@@ -139,6 +139,47 @@ func (m *Plan) validate(all bool) error {
 			}
 		}
 
+	case *Plan_RunnerPlan:
+		if v == nil {
+			err := PlanValidationError{
+				field:  "Actual",
+				reason: "oneof value cannot be a typed-nil",
+			}
+			if !all {
+				return err
+			}
+			errors = append(errors, err)
+		}
+
+		if all {
+			switch v := interface{}(m.GetRunnerPlan()).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, PlanValidationError{
+						field:  "RunnerPlan",
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, PlanValidationError{
+						field:  "RunnerPlan",
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(m.GetRunnerPlan()).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return PlanValidationError{
+					field:  "RunnerPlan",
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
 	default:
 		_ = v // ensures v is used
 	}
