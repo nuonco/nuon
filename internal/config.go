f9f663c90d6aad9abf74f2700567f246fcccb70a
Jon Morehouse <MorehouseJ09@gmail.com>
2023-03-07 23:24:50 -0600

    chore: update installs (#124)

--- internal/config.go
+++ internal/config.go
@@ -8,7 +8,7 @@ import (
 //nolint:gochecknoinits
 func init() {
 	config.RegisterDefault("temporal_host", "localhost:7233")
-	config.RegisterDefault("temporal_namespace", "default")
+	config.RegisterDefault("temporal_namespace", "deployments")
 	config.RegisterDefault("temporal_task_queue", "deployment")
 	config.RegisterDefault("temporal_max_concurrent_activities", 1)
 
