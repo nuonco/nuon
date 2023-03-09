bf6ea71e0e0267e0a6b96f5cbc39878045a09843
Jon Morehouse <MorehouseJ09@gmail.com>
2023-03-09 13:39:58 -0600

    feat: support both shortID and longIDs (#45)

--- internal/servers/instances/info.go
+++ internal/servers/instances/info.go
@@ -6,13 +6,52 @@ import (
 
 	"github.com/bufbuild/connect-go"
 	"github.com/powertoolsdev/orgs-api/internal/repos/workflows"
+	"github.com/powertoolsdev/orgs-api/internal/servers"
 	instancesv1 "github.com/powertoolsdev/protos/orgs-api/generated/types/instances/v1"
 )
+
+func ensureShortIDsGetInfoRequest(msg *instancesv1.GetInfoRequest) error {
+	orgID, err := servers.EnsureShortID(msg.OrgId)
+	if err != nil {
+		return fmt.Errorf("invalid orgID: %w", err)
+	}
+	msg.OrgId = orgID
+
+	appID, err := servers.EnsureShortID(msg.AppId)
+	if err != nil {
+		return fmt.Errorf("invalid appID: %w", err)
+	}
+	msg.AppId = appID
+
+	componentID, err := servers.EnsureShortID(msg.ComponentId)
+	if err != nil {
+		return fmt.Errorf("invalid componentID: %w", err)
+	}
+	msg.ComponentId = componentID
+
+	deploymentID, err := servers.EnsureShortID(msg.DeploymentId)
+	if err != nil {
+		return fmt.Errorf("invalid deploymentID: %w", err)
+	}
+	msg.DeploymentId = deploymentID
+
+	installID, err := servers.EnsureShortID(msg.InstallId)
+	if err != nil {
+		return fmt.Errorf("invalid installID: %w", err)
+	}
+	msg.InstallId = installID
+
+	return nil
+}
 
 func (s *server) GetInfo(
 	ctx context.Context,
 	req *connect.Request[instancesv1.GetInfoRequest],
 ) (*connect.Response[instancesv1.GetInfoResponse], error) {
+	if err := ensureShortIDsGetInfoRequest(req.Msg); err != nil {
+		return nil, fmt.Errorf("unable to ensure ids: %w", err)
+	}
+
 	wkflowsRepo, err := s.WorkflowsRepo(ctx, req.Msg.OrgId)
 	if err != nil {
 		return nil, fmt.Errorf("unable to get workflows repo: %w", err)
