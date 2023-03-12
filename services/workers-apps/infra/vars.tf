df06171e89a0cf0b60a601062934ad7be6354176
Jon Morehouse <MorehouseJ09@gmail.com>
2023-03-12 16:57:52 -0600

    chore: update repos for permissions (#22)

--- services/workers-apps/infra/vars.tf
+++ services/workers-apps/infra/vars.tf
@@ -4,7 +4,7 @@ locals {
   }
 
   name                   = "workers-apps"
-  github_repository      = local.name
+  github_repository      = "mono"
   github_organization    = "powertoolsdev"
   terraform_organization = "launchpaddev"
 
