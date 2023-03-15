1825fff398d56f1e21cb56604cab5614f4177ad5
Jon Morehouse <MorehouseJ09@gmail.com>
2023-03-15 10:41:43 -0600

    chore: add workers-orgs kms access (#94)

---
+++ services/workers-orgs/infra/orgs_kms_access.tf
@@ -0,0 +1,71 @@
+data "aws_iam_policy_document" "orgs_account_kms_access" {
+  provider = aws.orgs
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "kms:DescribeKey",
+      "kms:DisableKey",
+      "kms:CreateKey",
+      "kms:CreateAlias",
+      "kms:CreateGrant",
+      "kms:EnableKeyRotation",
+      "kms:PutKeyPolicy",
+      "kms:TagResource",
+      "kms:UntagResource",
+    ]
+    resources = ["*", ]
+  }
+}
+
+data "aws_iam_policy_document" "orgs_account_kms_access_trust" {
+  provider = aws.orgs
+
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole", ]
+
+    principals {
+      type = "AWS"
+      identifiers = [
+        module.iam_eks_role.iam_role_arn,
+      ]
+    }
+  }
+
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole", ]
+
+    principals {
+      type = "AWS"
+      identifiers = [
+        data.tfe_outputs.infra-orgs.values.iam_roles.support.arn,
+      ]
+    }
+  }
+}
+
+resource "aws_iam_policy" "orgs_account_kms_access_policy" {
+  provider = aws.orgs
+
+  name   = "${local.name}-orgs-account-kms-access"
+  policy = data.aws_iam_policy_document.orgs_account_kms_access.json
+}
+
+module "orgs_account_kms_access_role" {
+  providers = {
+    aws = aws.orgs
+  }
+
+
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
+  version = ">= 5.1.0"
+
+  create_role       = true
+  role_requires_mfa = false
+
+  role_name                = "${local.name}-orgs-account-kms-access"
+  custom_role_trust_policy = data.aws_iam_policy_document.orgs_account_kms_access_trust.json
+  custom_role_policy_arns  = [aws_iam_policy.orgs_account_kms_access_policy.arn, ]
+}
