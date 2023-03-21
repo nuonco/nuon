50981c9d8fbd125a13930a6e96a486cd419627d0
Peter Lyons <pete@powertools.dev>
2023-03-21 14:12:27 -0600

    feat: nuonctl ids new (#129)
    * feat: nuonctl ids new
    
    * Add a new command for general utility
    * Print out in long, short form
    * Does not need error handling during init like most subcommands
    
    ---------
    
    Co-authored-by: Jon Morehouse <MorehouseJ09@gmail.com>

---
+++ services/nuonctl/cmd/ids.go
@@ -0,0 +1,34 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/powertoolsdev/mono/pkg/common/shortid"
+	"github.com/spf13/cobra"
+)
+
+func (c *cli) registerIDs(ctx context.Context, rootCmd *cobra.Command) {
+
+	var idsCmd = &cobra.Command{
+		Use:     "ids",
+		Aliases: []string{"s"},
+		Short:   "commands for working with UUIDs and shortids",
+	}
+	rootCmd.AddCommand(idsCmd)
+
+	idsCmd.AddCommand(&cobra.Command{
+		Use:   "new",
+		Short: "generate and print a new random unique id in long and short formats",
+
+		RunE: func(cmd *cobra.Command, args []string) error {
+			short := shortid.New()
+			long, err := shortid.ToUUID(short)
+			if err != nil {
+				return err
+			}
+			fmt.Printf("%s %s", long, short)
+			return nil
+		},
+	})
+}
