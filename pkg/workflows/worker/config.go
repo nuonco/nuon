70ce14ff445b5b4532866c6a542d1bd9c00f465a
Jon Morehouse <MorehouseJ09@gmail.com>
2023-03-25 15:38:50 -0600

    chore: migrate workers-executors (#224)
    * chore: use worker package
    
    * fix: remove extra config + default task queue
    
    * chore: fix
    
    * chore: make default workflow task queue
    
    * chore: remove extra config
    
    ---------
    
    Co-authored-by: nuonbot <128654611+nuonbot@users.noreply.github.com>

--- pkg/workflows/worker/config.go
+++ pkg/workflows/worker/config.go
@@ -4,8 +4,14 @@ import (
 	"github.com/powertoolsdev/mono/pkg/config"
 )
 
+// each domain has it's own namespace, so we no longer need to split work by domain at the task queue level. By having a
+// single queue in each namespace, we can more easily understand queue depth + have headroom to have a
+// high-priority/low-priority queue in the future.
+const DefaultTaskQueue string = "main"
+
 //nolint:gochecknoinits
 func init() {
+	config.RegisterDefault("temporal_task_queue", DefaultTaskQueue)
 	config.RegisterDefault("temporal_host", "localhost:7233")
 	config.RegisterDefault("temporal_max_concurrent_activities", 1)
 }
@@ -19,7 +25,6 @@ type Config struct {
 	// temporal configuration
 	TemporalHost                    string `config:"temporal_host" validate:"required"`
 	TemporalNamespace               string `config:"temporal_namespace" validate:"required"`
-	InstancesTemporalNamespace      string `config:"instances_temporal_namespace" validate:"required"`
 	TemporalTaskQueue               string `config:"temporal_task_queue" validate:"required"`
 	TemporalMaxConcurrentActivities int    `config:"temporal_max_concurrent_activities" validate:"required"`
 
