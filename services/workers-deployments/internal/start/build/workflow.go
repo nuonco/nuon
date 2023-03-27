eaea7406a9ca9c65a0cb8bdb2a358b14de730e79
Jon Morehouse <MorehouseJ09@gmail.com>
2023-03-27 16:49:49 -0600

    chore: move executors to own queue (#269)

--- services/workers-deployments/internal/start/build/workflow.go
+++ services/workers-deployments/internal/start/build/workflow.go
@@ -78,7 +78,7 @@ func execCreatePlan(
 	cwo := workflow.ChildWorkflowOptions{
 		WorkflowExecutionTimeout: time.Minute * 20,
 		WorkflowTaskTimeout:      time.Minute * 10,
-		TaskQueue:                workflows.DefaultTaskQueue,
+		TaskQueue:                workflows.ExecutorsTaskQueue,
 	}
 	ctx = workflow.WithChildOptions(ctx, cwo)
 
@@ -101,7 +101,7 @@ func execExecutePlan(
 	cwo := workflow.ChildWorkflowOptions{
 		WorkflowExecutionTimeout: time.Minute * 20,
 		WorkflowTaskTimeout:      time.Minute * 10,
-		TaskQueue:                workflows.DefaultTaskQueue,
+		TaskQueue:                workflows.ExecutorsTaskQueue,
 	}
 	ctx = workflow.WithChildOptions(ctx, cwo)
 
