7d161c1d9f50e9a4fb12f5bef0d023f13f77c52e
Jon Morehouse <MorehouseJ09@gmail.com>
2023-03-28 09:59:30 -0600

    feat(orgs-api): integrate metrics interceptor (#284)
    * chore: cleanup
    
    * feat: integrate metrics writer interceptor
    
    * chore: support tags + init logger

--- pkg/metrics/metrics.go
+++ pkg/metrics/metrics.go
@@ -18,9 +18,9 @@ const (
 //go:generate mockgen -destination=mock_writer.go -source=metrics.go -package=metrics
 type Writer interface {
 	// dogstatsd metrics
-	Incr(string, int)
-	Decr(string, int)
-	Timing(string, time.Duration)
+	Incr(string, int, []string)
+	Decr(string, int, []string)
+	Timing(string, time.Duration, []string)
 
 	// datadog specific
 	Event(e *statsd.Event)
@@ -32,7 +32,7 @@ type writer struct {
 	Address string `validate:"required"`
 	Disable bool
 	Tags    []string
-	Log     *zap.Logger
+	Log     *zap.Logger `validate:"required"`
 
 	// internal
 	client dogstatsdClient
@@ -42,10 +42,15 @@ var _ Writer = (*writer)(nil)
 
 // New returns a default writer, which emits metrics to statsd by default
 func New(v *validator.Validate, opts ...writerOption) (*writer, error) {
+	l, err := zap.NewDevelopment()
+	if err != nil {
+		return nil, fmt.Errorf("unable to get logger: %w", err)
+	}
 	r := &writer{
 		v:       v,
 		Address: defaultAddress,
 		Tags:    make([]string, 0),
+		Log:     l,
 	}
 	for idx, opt := range opts {
 		if err := opt(r); err != nil {
