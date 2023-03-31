8156848be9cffe5fd52a7070493a567c4f7bb13d
Jon Morehouse <MorehouseJ09@gmail.com>
2023-03-31 13:49:00 -0600

    chore: move commands into general + start services (#310)
    * chore: switch back to use go template
    
    * fix
    
    * chore: new nuonctl commands
    
    ---------
    
    Co-authored-by: nuonbot <128654611+nuonbot@users.noreply.github.com>

---
+++ services/nuonctl/cmd/services.go
@@ -0,0 +1,35 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/powertoolsdev/mono/services/nuonctl/internal/commands/services"
+	"github.com/spf13/cobra"
+)
+
+func (c *cli) registerServices(ctx context.Context, rootCmd *cobra.Command) error {
+	cmds, err := services.New(c.v)
+	if err != nil {
+		return fmt.Errorf("unable to initialize service commands: %w", err)
+	}
+
+	var generalCmd = &cobra.Command{
+		Use:     "service",
+		Aliases: []string{"s"},
+		Short:   "commands for developing and interacting with services",
+	}
+	rootCmd.AddCommand(generalCmd)
+
+	var name string
+	generalCmd.PersistentFlags().StringVar(&name, "name", "", "service name")
+	generalCmd.AddCommand(&cobra.Command{
+		Use:   "run",
+		Short: "run a service locally",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return cmds.Run(ctx, name)
+		},
+	})
+
+	return nil
+}
