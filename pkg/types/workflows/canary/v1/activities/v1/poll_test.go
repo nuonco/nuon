8042a5a737cdc4abe4549bb36735ad0816558146
Jon Morehouse <MorehouseJ09@gmail.com>
2023-04-05 17:56:20 -0600

    feat: implement canary workflow structure (#383)
    * chore: add activities for canary
    
    * feat: implement out workflows + new structure
    
    * chore: format

---
+++ pkg/types/workflows/canary/v1/activities/v1/poll_test.go
@@ -0,0 +1,49 @@
+package activitiesv1
+
+import (
+	"testing"
+
+	"github.com/powertoolsdev/mono/pkg/common/shortid"
+	canaryv1 "github.com/powertoolsdev/mono/pkg/types/workflows/canary/v1"
+	"github.com/stretchr/testify/assert"
+	anypb "google.golang.org/protobuf/types/known/anypb"
+)
+
+func Test_PollWorkflowRequest(t *testing.T) {
+	t.Run("test a poll workflow request", func(t *testing.T) {
+		req := &PollWorkflowRequest{
+			Namespace:    "orgs",
+			WorkflowName: "Provision",
+			WorkflowId:   shortid.New(),
+		}
+		err := req.Validate()
+		assert.NoError(t, err)
+	})
+}
+
+func Test_PollWorkflowResponse(t *testing.T) {
+	t.Run("a valid poll workflow response", func(t *testing.T) {
+		resp := &canaryv1.ProvisionResponse{}
+		any, err := anypb.New(resp)
+		assert.NoError(t, err)
+
+		req := &PollWorkflowResponse{
+			Step:     &canaryv1.Step{},
+			Response: any,
+		}
+		err = req.Validate()
+		assert.NoError(t, err)
+	})
+
+	t.Run("invalid poll workflow response", func(t *testing.T) {
+		resp := &canaryv1.ProvisionResponse{}
+		any, err := anypb.New(resp)
+		assert.NoError(t, err)
+
+		req := &PollWorkflowResponse{
+			Response: any,
+		}
+		err = req.Validate()
+		assert.Error(t, err)
+	})
+}
