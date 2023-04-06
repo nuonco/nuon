04bbcd8af03abc5f1cbcc3f37cf65fda9b3df0f3
Maria Paktiti <maria@powertools.dev>
2023-04-06 09:01:52 -0600

    feat: add resolver for upsertSecrets mutation (#358)
    Signed-off-by: Maria Paktiti <maria@powertools.dev>

---
+++ services/api-gateway/src/gql-resolvers/secret/upsert-secrets.ts
@@ -0,0 +1,49 @@
+import {
+  Secret,
+  UpsertSecretsRequest,
+} from "@buf/nuon_orgs-api.grpc_node/instances/v1/secrets_pb";
+import { GraphQLError } from "graphql";
+import type {
+  Mutation,
+  MutationUpsertSecretsArgs,
+  SecretsInput,
+  TResolverFn,
+} from "../../types";
+
+export function parseSecretInput(input: SecretsInput[]) {
+  return input.flatMap(({ appId, componentId, installId, orgId, secrets }) => {
+    return secrets.map(({ id, key, value }) => {
+      const secret = new Secret()
+        .setAppId(appId)
+        .setOrgId(orgId)
+        .setComponentId(componentId)
+        .setInstallId(installId)
+        .setId(id)
+        .setKey(key)
+        .setValue(value);
+      return secret;
+    });
+  });
+}
+
+export const upsertSecrets: TResolverFn<
+  MutationUpsertSecretsArgs,
+  Mutation["upsertSecrets"]
+> = (_, { input }, { clients }) =>
+  new Promise((resolve, reject) => {
+    if (clients.instance) {
+      const request = new UpsertSecretsRequest().setSecretsList(
+        parseSecretInput(input)
+      );
+
+      clients.instance.upsertSecrets(request, (err) => {
+        if (err) {
+          reject(new GraphQLError(err?.message));
+        } else {
+          resolve(true);
+        }
+      });
+    } else {
+      reject(new GraphQLError("Service isn't available"));
+    }
+  });
