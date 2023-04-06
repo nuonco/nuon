600a52c73a9d0aa01075136a47dd27a84abfd1b9
Maria Paktiti <maria@powertools.dev>
2023-04-06 09:01:52 -0600

    feat: add resolver for upsertSecrets mutation (#358)
    Signed-off-by: Maria Paktiti <maria@powertools.dev>

--- services/api-gateway/src/gql-resolvers/secret/index.ts
+++ services/api-gateway/src/gql-resolvers/secret/index.ts
@@ -1,6 +1,10 @@
 import { secrets } from "./secrets";
+import { upsertSecrets } from "./upsert-secrets";
 
 export const secretResolvers = {
+  Mutation: {
+    upsertSecrets,
+  },
   Query: {
     secrets,
   },
