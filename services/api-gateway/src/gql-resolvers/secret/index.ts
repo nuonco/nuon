b6d8a83fd7a11e293f5d08d11b973513577a3fae
Maria Paktiti <maria@powertools.dev>
2023-04-06 15:37:18 -0600

    feat: add resolver for deleteSecrets mutation (#386)
    Signed-off-by: Maria Paktiti <maria@powertools.dev>

--- services/api-gateway/src/gql-resolvers/secret/index.ts
+++ services/api-gateway/src/gql-resolvers/secret/index.ts
@@ -1,8 +1,10 @@
+import { deleteSecrets } from "./delete-secrets";
 import { secrets } from "./secrets";
 import { upsertSecrets } from "./upsert-secrets";
 
 export const secretResolvers = {
   Mutation: {
+    deleteSecrets,
     upsertSecrets,
   },
   Query: {
