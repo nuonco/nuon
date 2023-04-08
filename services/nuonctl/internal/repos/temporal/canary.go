b13d4bf5c5db7a66162915da71320434b6841109
Jon Morehouse <MorehouseJ09@gmail.com>
2023-04-08 11:08:46 -0600

    chore: add deprovision canary command (#401)

--- services/nuonctl/internal/repos/temporal/canary.go
+++ services/nuonctl/internal/repos/temporal/canary.go
@@ -25,3 +25,20 @@ func (r *repo) TriggerCanaryProvision(ctx context.Context, req *canaryv1.Provisi
 
 	return nil
 }
+
+func (r *repo) TriggerCanaryDeprovision(ctx context.Context, req *canaryv1.DeprovisionRequest) error {
+	opts := tclient.StartWorkflowOptions{
+		TaskQueue: workflows.DefaultTaskQueue,
+		Memo: map[string]interface{}{
+			"canary-id":  req.CanaryId,
+			"started-by": "nuonctl",
+		},
+	}
+
+	_, err := r.Client.ExecuteWorkflowInNamespace(ctx, "canary", opts, "Deprovision", req)
+	if err != nil {
+		return fmt.Errorf("unable to provision install: %w", err)
+	}
+
+	return nil
+}
