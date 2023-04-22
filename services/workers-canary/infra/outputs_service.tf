b7bf7a193caf978c1c2ad9de911d408191f6a141
Jon Morehouse <MorehouseJ09@gmail.com>
2023-04-21 18:01:13 -0600

    chore: disable non-sensitive (#576)

--- services/workers-canary/infra/outputs_service.tf
+++ services/workers-canary/infra/outputs_service.tf
@@ -1,5 +1,5 @@
 output "canary_slack_webhooks_url" {
-  value = nonsensitive(var.slack_webhook_url_canary_bots)
+  value = var.slack_webhook_url_canary_bots
 }
 
 output "canary_install_iam_role_arn" {
