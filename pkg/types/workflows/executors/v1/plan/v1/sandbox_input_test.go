27e80198b44d03befca70c3d4766c3c0060021b5
Jon Morehouse <MorehouseJ09@gmail.com>
2023-04-28 09:53:59 -0600

    feat: add new org server input (#642)
    * feat: add new org server input
    
    * chore: change input types
    
    * chore: refactor plan paths
    
    * feat: refactor plan activity executors + protos
    
    * feat: update protos to remove unused type field
    
    * fix: get executors compiling again
    
    * goimports cleanup
    
    * chore: progress being made on the waypoint planner
    
    * chore: updates
    
    * chore: remove comment
    
    * chore: implement server base plan (almost)
    
    * chore: add runner and server plans
    
    * chore: update waypoint runner
    
    * chore: add commands for org server + add dry-run
    
    * feat: get nuonctl plans for testing working
    
    * chore: add helm repo values
    
    * chore: add values
    
    * golangcilint changes
    
    ---------
    
    Co-authored-by: Jordan Acosta <jordanjacosta@gmail.com>

---
+++ pkg/types/workflows/executors/v1/plan/v1/sandbox_input_test.go
@@ -0,0 +1,18 @@
+package planv1
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSandboxInputType_ToRunType(t *testing.T) {
+	assert.Equal(t,
+		SandboxInputType_SANDBOX_INPUT_TYPE_DEPROVISION.ToRunType(),
+		TerraformRunType_TERRAFORM_RUN_TYPE_DESTROY)
+
+	assert.Equal(t,
+		SandboxInputType_SANDBOX_INPUT_TYPE_PROVISION.ToRunType(),
+		TerraformRunType_TERRAFORM_RUN_TYPE_APPLY)
+
+}
--- services/workers-installs/internal/deprovision/workflow.go
+++ services/workers-installs/internal/deprovision/workflow.go
@@ -67,17 +67,13 @@ func (w wkflow) Deprovision(ctx workflow.Context, req *installsv1.DeprovisionReq
 	}
 
 	cpReq := planv1.CreatePlanRequest{
-		Type: planv1.PlanType_PLAN_TYPE_TERRAFORM_SANDBOX,
 		Input: &planv1.CreatePlanRequest_Sandbox{
 			Sandbox: &planv1.SandboxInput{
 				OrgId:           req.OrgId,
 				AppId:           req.AppId,
 				InstallId:       req.InstallId,
 				SandboxSettings: &planv1.SandboxSettings{Name: req.SandboxSettings.Name, Version: req.SandboxSettings.Version},
-				// TODO(jdt): accept this from the API and set it here?
-				// it's defaulted in workers-executors so no need to double hard-code
-				// TerraformVersion: new(string),
-				RunType: planv1.TerraformRunType_TERRAFORM_RUN_TYPE_DESTROY,
+				Type:            planv1.SandboxInputType_SANDBOX_INPUT_TYPE_DEPROVISION,
 				AccountSettings: &planv1.SandboxInput_Aws{
 					Aws: &planv1.AWSSettings{
 						Region:    req.AccountSettings.Region,
