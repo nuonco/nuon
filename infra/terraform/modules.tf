18e01ee7f43a34c8c5980c2886735aca85d8dfc8
Jon Morehouse <MorehouseJ09@gmail.com>
2023-04-30 16:06:04 -0600

    chore: add echo module (#713)

--- infra/terraform/modules.tf
+++ infra/terraform/modules.tf
@@ -1,10 +1,10 @@
-#resource "tfe_registry_module" "echo-module" {
-#name         = "echo"
-#organization = data.tfe_organization.main.name
+resource "tfe_registry_module" "echo-module" {
+  name         = "echo"
+  organization = data.tfe_organization.main.name
 
-#vcs_repo {
-#display_identifier = "powertoolsdev/mono"
-#identifier         = "powertoolsdev/mono"
-#oauth_token_id     = data.tfe_oauth_client.github.oauth_token_id
-#}
-#}
+vcs_repo {
+    display_identifier = "powertoolsdev/terraform-provider-echo"
+    identifier         = "powertoolsdev/terraform-provider-echo"
+    oauth_token_id     = data.tfe_oauth_client.github.oauth_token_id
+  }
+}
