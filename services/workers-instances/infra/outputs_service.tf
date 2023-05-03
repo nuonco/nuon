6ee2a987b477d34c13777e601af4145bbd757ca7
Jon Morehouse <MorehouseJ09@gmail.com>
2023-05-03 17:20:53 -0600

    feat: add orgs k8s outputs for workers-instances (#769)
    Co-authored-by: nuonbot <128654611+nuonbot@users.noreply.github.com>

--- services/workers-instances/infra/outputs_service.tf
+++ services/workers-instances/infra/outputs_service.tf
@@ -9,3 +9,13 @@ output "orgs_deployments_role_template" {
 output "waypoint_server" {
   value = nonsensitive(data.tfe_outputs.infra-orgs.values.waypoint)
 }
+
+output "orgs_k8s" {
+  sensitive = true
+  value = {
+    ca_data         = data.tfe_outputs.infra-orgs.values.k8s.ca_data
+    public_endpoint = data.tfe_outputs.infra-orgs.values.k8s.public_endpoint
+    cluster_id      = data.tfe_outputs.infra-orgs.values.k8s.cluster_id
+    role_arn        = data.tfe_outputs.infra-orgs.values.k8s.access_role_arns["eks-workers-orgs"]
+  }
+}
