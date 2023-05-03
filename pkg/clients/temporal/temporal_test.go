525cb2752886e4636a783e2d172e1de21a9c18be
Jon Morehouse <MorehouseJ09@gmail.com>
2023-05-02 23:13:15 -0600

    feat: lazy load temporal client for `nuonctl` (#751)
    * feat: allow lazy loading temporal client
    
    * chore: more updates
    
    * chore: fix executors `service.yml`

--- pkg/clients/temporal/temporal_test.go
+++ pkg/clients/temporal/temporal_test.go
@@ -13,11 +13,48 @@ import (
 func TestNew(t *testing.T) {
 	namespace := generics.GetFakeObj[string]()
 	logger := zaptest.NewLogger(t)
+	address := generics.GetFakeObj[string]()
 
 	tests := map[string]struct {
 		optFns      func() []temporalOption
 		errExpected error
+		assertFn    func(*testing.T, Client)
 	}{
+		// NOTE(jm): we can only load this with lazy load on and assert, otherwise the connection will be
+		// attempted and fail during testing
+		"happy path": {
+			optFns: func() []temporalOption {
+				return []temporalOption{
+					WithNamespace(namespace),
+					WithLogger(logger),
+					WithLazyLoad(true),
+					WithAddr(address),
+				}
+			},
+			assertFn: func(t *testing.T, client Client) {
+				tClient, ok := client.(*temporal)
+				assert.True(t, ok)
+				assert.True(t, tClient.LazyLoad)
+				assert.Equal(t, tClient.Addr, address)
+				assert.Equal(t, tClient.Namespace, namespace)
+				assert.Equal(t, tClient.Logger, logger)
+			},
+		},
+		"sets lazy load": {
+			optFns: func() []temporalOption {
+				return []temporalOption{
+					WithNamespace(namespace),
+					WithLogger(logger),
+					WithAddr(address),
+					WithLazyLoad(true),
+				}
+			},
+			assertFn: func(t *testing.T, client Client) {
+				tClient, ok := client.(*temporal)
+				assert.True(t, ok)
+				assert.True(t, tClient.LazyLoad)
+			},
+		},
 		"missing address": {
 			optFns: func() []temporalOption {
 				return []temporalOption{
