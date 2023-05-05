abc307706a420ed82a75f3ff98bb89948d6e4f20
Chris Potter <chris@powertools.dev>
2023-05-04 22:42:50 -0400

    feat: working delete and create install pollers (#779)
    feat: working delete and create install poillers
    
    Signed-off-by: Chris Potter <chris@powertools.dev>

--- services/api/internal/jobs/createinstall/workflow.go
+++ services/api/internal/jobs/createinstall/workflow.go
@@ -1,10 +1,14 @@
 package createinstall
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/go-playground/validator/v10"
 	jobsv1 "github.com/powertoolsdev/mono/pkg/types/api/jobs/v1"
+	activitiesv1 "github.com/powertoolsdev/mono/pkg/types/workflows/shared/v1/activities/v1"
+	sharedactivities "github.com/powertoolsdev/mono/pkg/workflows/activities"
+	"go.temporal.io/sdk/temporal"
 	"go.temporal.io/sdk/workflow"
 )
 
@@ -29,5 +33,30 @@ func (w *wkflow) CreateInstall(ctx workflow.Context, req *jobsv1.CreateInstallRe
 	if err := fut.Get(ctx, &resp); err != nil {
 		return nil, err
 	}
+
+	shrdAct := &sharedactivities.Activities{}
+
+	pollRequest := &activitiesv1.PollWorkflowRequest{
+		Namespace:    "installs",
+		WorkflowName: "Provision",
+		WorkflowId:   resp.WorkflowID,
+	}
+
+	// set poll timeout
+	ctx = workflow.WithActivityOptions(ctx, workflow.ActivityOptions{
+		ScheduleToCloseTimeout: sharedactivities.PollActivityTimeout * sharedactivities.MaxActivityRetries,
+		StartToCloseTimeout:    sharedactivities.PollActivityTimeout,
+		RetryPolicy: &temporal.RetryPolicy{
+			MaximumAttempts: sharedactivities.MaxActivityRetries,
+		},
+	})
+
+	var pollResp activitiesv1.PollWorkflowResponse
+	fut = workflow.ExecuteActivity(ctx, shrdAct.PollWorkflow, pollRequest)
+	err := fut.Get(ctx, &pollResp)
+	if err != nil {
+		return nil, fmt.Errorf("unable to poll for workflow response: %w", err)
+	}
+
 	return &jobsv1.CreateInstallResponse{}, nil
 }
--- services/api/internal/jobs/deleteinstall/workflow.go
+++ services/api/internal/jobs/deleteinstall/workflow.go
@@ -1,10 +1,14 @@
 package deleteinstall
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/go-playground/validator/v10"
 	jobsv1 "github.com/powertoolsdev/mono/pkg/types/api/jobs/v1"
+	activitiesv1 "github.com/powertoolsdev/mono/pkg/types/workflows/shared/v1/activities/v1"
+	sharedactivities "github.com/powertoolsdev/mono/pkg/workflows/activities"
+	"go.temporal.io/sdk/temporal"
 	"go.temporal.io/sdk/workflow"
 )
 
@@ -28,6 +32,29 @@ func (w *wkflow) DeleteInstall(ctx workflow.Context, req *jobsv1.DeleteInstallRe
 	fut := workflow.ExecuteActivity(ctx, act.TriggerInstallDeprovJob, req.InstallId)
 	if err := fut.Get(ctx, &resp); err != nil {
 		return nil, err
+	}
+	shrdAct := &sharedactivities.Activities{}
+
+	pollRequest := &activitiesv1.PollWorkflowRequest{
+		Namespace:    "installs",
+		WorkflowName: "Provision",
+		WorkflowId:   resp.WorkflowID,
+	}
+
+	// set poll timeout
+	ctx = workflow.WithActivityOptions(ctx, workflow.ActivityOptions{
+		ScheduleToCloseTimeout: sharedactivities.PollActivityTimeout * sharedactivities.MaxActivityRetries,
+		StartToCloseTimeout:    sharedactivities.PollActivityTimeout,
+		RetryPolicy: &temporal.RetryPolicy{
+			MaximumAttempts: sharedactivities.MaxActivityRetries,
+		},
+	})
+
+	var pollResp activitiesv1.PollWorkflowResponse
+	fut = workflow.ExecuteActivity(ctx, shrdAct.PollWorkflow, pollRequest)
+	err := fut.Get(ctx, &pollResp)
+	if err != nil {
+		return nil, fmt.Errorf("unable to poll for workflow response: %w", err)
 	}
 
 	return &jobsv1.DeleteInstallResponse{}, nil
