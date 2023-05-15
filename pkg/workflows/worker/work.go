565675e8b2b193980e26fcefcf1ed83023e13c8b
Jon Morehouse <MorehouseJ09@gmail.com>
2023-05-15 16:23:15 -0600

    feat: executor changes (#754)
    * chore: remove otel
    
    * feat: add new preset
    
    * chore: update namespace
    
    * fix: lots of updates and changes
    
    ---------
    
    Co-authored-by: nuonbot <128654611+nuonbot@users.noreply.github.com>

--- pkg/workflows/worker/work.go
+++ pkg/workflows/worker/work.go
@@ -1,30 +1,15 @@
 package worker
 
 import (
-	"fmt"
-
 	"go.temporal.io/sdk/client"
-	"go.temporal.io/sdk/contrib/opentelemetry"
 	"go.temporal.io/sdk/interceptor"
 	tworker "go.temporal.io/sdk/worker"
 )
 
 func (w *worker) getWorker(c client.Client) (tworker.Worker, error) {
-	t, err := w.getTracer()
-	if err != nil {
-		return nil, fmt.Errorf("unable to get tracer: %w", err)
-	}
-
-	traceIntercepter, err := opentelemetry.NewTracingInterceptor(opentelemetry.TracerOptions{
-		Tracer: t,
-	})
-	if err != nil {
-		return nil, fmt.Errorf("unable to get tracing interceptor: %w", err)
-	}
-
 	return tworker.New(c, w.Config.TemporalTaskQueue, tworker.Options{
 		MaxConcurrentActivityExecutionSize: w.Config.TemporalMaxConcurrentActivities,
-		Interceptors:                       []interceptor.WorkerInterceptor{traceIntercepter},
+		Interceptors:                       []interceptor.WorkerInterceptor{},
 	}), nil
 }
 
