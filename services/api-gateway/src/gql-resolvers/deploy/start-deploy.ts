1d31d01e1630490a953d1ff42eaa425220c2ce97
Maria Paktiti <maria@powertools.dev>
2023-05-19 21:00:49 +0300

    feat: add resolvers for deploys (#903)

---
+++ services/api-gateway/src/gql-resolvers/deploy/start-deploy.ts
@@ -0,0 +1,29 @@
+import { StartDeployRequest } from "@buf/nuon_apis.grpc_node/deploy/v1/messages_pb";
+import { GraphQLError } from "graphql";
+import type {
+  Mutation,
+  MutationStartDeployArgs,
+  TResolverFn,
+} from "../../types";
+
+export const startDeploy: TResolverFn<
+  MutationStartDeployArgs,
+  Mutation["startDeploy"]
+> = (_, { input }, { clients }) =>
+  new Promise((resolve, reject) => {
+    if (clients.deploy) {
+      const request = new StartDeployRequest()
+        .setBuildId(input.buildId)
+        .setInstallId(input.installId);
+
+      clients.deploy.startDeploy(request, (err, res) => {
+        if (err) {
+          reject(new GraphQLError(err?.message));
+        } else {
+          resolve(res.toObject().deploy);
+        }
+      });
+    } else {
+      reject(new GraphQLError("Service isn't available"));
+    }
+  });
