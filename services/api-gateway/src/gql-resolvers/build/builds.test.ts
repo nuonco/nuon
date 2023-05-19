3d734e68daf6b6fe41aca1e02f90474ae37bda30
Maria Paktiti <maria@powertools.dev>
2023-05-19 10:45:48 +0300

    feat: add resolvers for builds (#897)

---
+++ services/api-gateway/src/gql-resolvers/build/builds.test.ts
@@ -0,0 +1,76 @@
+import { builds } from "./builds";
+
+const mockBuild = {
+  componentId: "test-component-id",
+  createdAt: "1999-12-31T08:15:30.000",
+  gitRef: "test-git-ref",
+  id: "test-id",
+  updatedAt: "1999-12-31T08:15:30.000",
+};
+
+const mockBuildServiceClient = {
+  getBuilds: jest
+    .fn()
+    .mockImplementationOnce((req, cb) => {
+      cb(undefined, {
+        toObject: jest.fn().mockReturnValue({
+          buildsList: [mockBuild],
+        }),
+      });
+    })
+    .mockImplementationOnce((req, cb) => {
+      cb(undefined, {
+        toObject: jest.fn().mockReturnValue({
+          buildsList: [],
+        }),
+      });
+    })
+    .mockImplementationOnce((req, cb) => {
+      const err = new Error("some error");
+      cb(err);
+    }),
+};
+
+const mockClients = {
+  build: mockBuildServiceClient,
+};
+
+test("builds resolver should return list of builds", async () => {
+  const spec = await builds(
+    undefined,
+    { componentId: "123" },
+    { clients: mockClients }
+  );
+
+  expect(spec).toEqual([
+    {
+      componentId: "test-component-id",
+      createdAt: "1999-12-31T08:15:30.000",
+      gitRef: "test-git-ref",
+      id: "test-id",
+      updatedAt: "1999-12-31T08:15:30.000",
+    },
+  ]);
+});
+
+test("builds resolver should return empty array", async () => {
+  const spec = await builds(
+    undefined,
+    { componentId: "123" },
+    { clients: mockClients }
+  );
+
+  expect(spec).toEqual([]);
+});
+
+test("builds resolver should return error on failed query", async () => {
+  await expect(
+    builds(undefined, { componentId: "123" }, { clients: mockClients })
+  ).rejects.toThrowErrorMatchingInlineSnapshot(`"some error"`);
+});
+
+test("builds resolver should return error if service client doesn't exist", async () => {
+  await expect(
+    builds(undefined, { componentId: "123" }, { clients: {} })
+  ).rejects.toThrowErrorMatchingInlineSnapshot(`"Service isn't available"`);
+});
