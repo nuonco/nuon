7a580c88700f8dac89e9c9ac8ea29ddbceb2ffe5
Jordan Acosta <jordan@nuon.co>
2023-05-22 15:21:09 -0700

    fix: add in the github info we were missing (#906)
    * fix: add in the github info we were missing
    
    This was previously being handled in the deployments service. This
    probably isn't what we want to do long-term, but for now we just need
    this info appended to each plan request.
    
    * fix: copy deployments service code we need for builds
    
    My first attempt at this didn't work. I thought I was simply updating
    the build config with github info. Turns out the build config was nil,
    and needed to be constructed.
    
    I stepped backwards through the service code path, until I understood
    exactly what it was doing, then copied over the minimum code I needed to
    get it working.
    
    This needs some cleanup, but it has least has all the business logic.
    
    * fix: update build job tests with extra params

--- services/api/internal/jobs/build/activities/create_plan_request.go
+++ services/api/internal/jobs/build/activities/create_plan_request.go
@@ -3,12 +3,18 @@ package activities
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	apibuildv1 "github.com/powertoolsdev/mono/pkg/types/api/build/v1"
+	componentConfig "github.com/powertoolsdev/mono/pkg/types/components/component/v1"
 	componentv1 "github.com/powertoolsdev/mono/pkg/types/components/component/v1"
+	vcsv1 "github.com/powertoolsdev/mono/pkg/types/components/vcs/v1"
+	"github.com/powertoolsdev/mono/services/api/internal/models"
 	"github.com/powertoolsdev/mono/services/api/internal/servers/converters"
+	"google.golang.org/protobuf/encoding/protojson"
 
 	planv1 "github.com/powertoolsdev/mono/pkg/types/workflows/executors/v1/plan/v1"
+	"github.com/powertoolsdev/mono/services/api/internal/repos"
 	"github.com/powertoolsdev/mono/services/api/internal/services"
 	"go.uber.org/zap"
 
@@ -16,20 +22,51 @@ import (
 )
 
 type activities struct {
-	db *gorm.DB
+	db                     *gorm.DB
+	githubAppID            string
+	githubAppKeySecretName string
+	componentService       services.ComponentService
+	componentRepo          repos.ComponentRepo
 }
 
-func New(db *gorm.DB) *activities {
-	return &activities{db: db}
+func New(db *gorm.DB, githubAppID, githubAppKeySecretName string) *activities {
+	// TODO fix up logger here
+	componentService := services.NewComponentService(db, zap.L())
+
+	// The component service doesn't allow updating an existing componet,
+	// so we need to create an instance of the component repo.
+	// The service has one, but doesn't expose it.
+	componentRepo := repos.NewComponentRepo(db)
+
+	return &activities{
+		db:                     db,
+		githubAppID:            githubAppID,
+		githubAppKeySecretName: githubAppKeySecretName,
+		componentService:       componentService,
+		componentRepo:          componentRepo,
+	}
 }
 
 func (a *activities) CreatePlanRequest(ctx context.Context, req *apibuildv1.StartBuildRequest) (*planv1.CreatePlanRequest, error) {
 	// TODO fix up logger here
-	componentService := services.NewComponentService(a.db, zap.L())
-	component, err := componentService.GetComponent(ctx, req.ComponentId)
-
+	component, err := a.componentService.GetComponent(ctx, req.ComponentId)
 	if err != nil {
 		return nil, fmt.Errorf("error during StartBuild.CreatePlan loading component: %w", err)
+	}
+
+	// retrieve commits if the component configuration contains github info
+	dbConfig := &componentConfig.Component{}
+	if err = protojson.Unmarshal([]byte(component.Config.String()), dbConfig); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal DB JSON: %w", err)
+	}
+	if dbConfig.BuildCfg != nil {
+		dockerCfg := dbConfig.BuildCfg.GetDockerCfg()
+		if dockerCfg != nil {
+			err = a.getGithubInfo(ctx, dockerCfg.GetVcsCfg(), component, req.GitRef, a.githubAppID, a.githubAppKeySecretName)
+			if err != nil {
+				return nil, fmt.Errorf("failed to process github repo: %w", err)
+			}
+		}
 	}
 
 	componentProto, err := converters.ComponentModelToProto(component)
@@ -55,4 +92,68 @@ func (a *activities) CreatePlanRequest(ctx context.Context, req *apibuildv1.Star
 	}
 
 	return planReq, nil
+}
+
+func (a *activities) getGithubInfo(ctx context.Context, vcsCfg *vcsv1.Config, component *models.Component, gitRef, githubAppID, githubAppKeySecretName string) error {
+	var repo string
+	connectedGithubConfig := vcsCfg.GetConnectedGithubConfig()
+	githubInstallID := "" // start with empty string which also means public git repo
+
+	// if the component config is for a connected github repo,
+	// then we have a githubInstallID and should make github API calls using it
+	isPrivate := connectedGithubConfig != nil
+	if isPrivate {
+		// Use the githubInstallID for github API calls
+		repo = connectedGithubConfig.Repo
+		githubInstallID = component.App.Org.GithubInstallID
+	} else {
+		publicGitConfig := vcsCfg.GetPublicGitConfig()
+		// TODO: temporary workaround, will refactor after the component config retro
+		repo = strings.ReplaceAll(publicGitConfig.Repo, "https://github.com/", "")
+		repo = strings.ReplaceAll(repo, ".git", "")
+	}
+
+	// update component config with github info
+	err := a.updateComponentConfig(ctx, githubInstallID, isPrivate, component, gitRef, githubAppID, githubAppKeySecretName)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (a *activities) updateComponentConfig(ctx context.Context, ghInstallID string, isPrivate bool, component *models.Component, gitRef, githubAppID, githubAppKeySecretName string) error {
+	// unmarshal existing JSON
+	dbConfig, _ := component.Config.MarshalJSON()
+	componentConfiguration := &componentConfig.Component{}
+	if err := protojson.Unmarshal(dbConfig, componentConfiguration); err != nil {
+		return err
+	}
+
+	if isPrivate {
+		// for private repos set: git_ref, github_app_key_id, github_app_key_secret_name, github_install_id
+		componentConfiguration.BuildCfg.GetDockerCfg().VcsCfg.GetConnectedGithubConfig().GitRef = gitRef
+		componentConfiguration.BuildCfg.GetDockerCfg().VcsCfg.GetConnectedGithubConfig().GithubAppKeyId = githubAppID
+		componentConfiguration.BuildCfg.GetDockerCfg().VcsCfg.GetConnectedGithubConfig().GithubAppKeySecretName = githubAppKeySecretName
+		componentConfiguration.BuildCfg.GetDockerCfg().VcsCfg.GetConnectedGithubConfig().GithubInstallId = ghInstallID
+	} else {
+		// for public repos set only the git_ref
+		componentConfiguration.BuildCfg.GetDockerCfg().VcsCfg.GetPublicGitConfig().GitRef = gitRef
+	}
+
+	//marshal JSON
+	var updatedConfig []byte
+	updatedConfig, err := protojson.Marshal(componentConfiguration)
+	if err != nil {
+		return err
+	}
+	component.Config = updatedConfig
+
+	// update component record with new configuration
+	component, err = a.componentRepo.Update(ctx, component)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
