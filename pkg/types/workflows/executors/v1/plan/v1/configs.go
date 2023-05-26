4193777a1401f8bfa7b23f478758a2b2e5ea695e
Jon Morehouse <MorehouseJ09@gmail.com>
2023-05-26 17:33:00 -0600

    chore: refactor waypoint plans + setup runner values (#970)
    * feat: refactor waypoint plan + add runner settings
    
    This change refactors the waypoint plan to split it out into sub proto
    messages (and each in their own file).
    
    While in here, we've added a `WaypointVariable` config type, which is
    tied to a waypoint job, as well as added new fields for configuring
    runners `WaypointRunner` message.
    
    * feat: add `envVar` faker
    
    * feat: add waypointVariables type
    
    * feat: waypoint job adds variables for faker
    
    * feat: add helper method on config sets
    
    Adding a `WaypointVariables` and `EnvVars` set.
    
    * chore: some cleanup of config types
    
    * feat: update `workers-executors` to new plan
    
    * update faker
    
    * chore: buf format

---
+++ pkg/types/workflows/executors/v1/plan/v1/configs.go
@@ -0,0 +1,21 @@
+package planv1
+
+// ToMap converts a map of env vars into a mapping
+func (e *EnvVars) ToMap() map[string]string {
+	vals := make(map[string]string, 0)
+	for _, envVar := range e.Env {
+		vals[envVar.Name] = envVar.Value
+	}
+
+	return vals
+}
+
+// ToMap converts a map of waypoint variables into a mapping
+func (w *WaypointVariables) ToMap() map[string]string {
+	vals := make(map[string]string, 0)
+	for _, wpVar := range w.Variables {
+		vals[wpVar.Name] = wpVar.Value
+	}
+
+	return vals
+}
