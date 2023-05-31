dbdb9437d3104ac0afaafa92303f08e237995876
Jon Morehouse <MorehouseJ09@gmail.com>
2023-05-31 15:29:58 -0600

    feat: implement oci archive (#1020)
    * plugin: implement archive functionality
    
    * feat: implement out oci image pulling
    
    * chore: more tests for archive implementation
    
    * chore: fix tests
    
    * moar tests

---
+++ pkg/terraform/archive/oci/image.go
@@ -0,0 +1,13 @@
+package oci
+
+import "fmt"
+
+type Image struct {
+	Registry string
+	Repo     string
+	Tag      string
+}
+
+func (i Image) RepoURL() string {
+	return fmt.Sprintf("%s/%s", i.Registry, i.Tag)
+}
