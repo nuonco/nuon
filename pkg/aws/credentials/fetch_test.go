decce5d0acbe874e6ae4e5feba31bcf6bff4fee7
Jon Morehouse <MorehouseJ09@gmail.com>
2023-06-01 08:47:43 -0600

    feat: add `pkg/aws/credentials` (#1024)
    Add a package that exposes an idiomatic way to get/share credentials,
    using `context.Context` and supports either static or assumed
    credentials.
    
    Co-authored-by: nuonbot <128654611+nuonbot@users.noreply.github.com>

---
+++ pkg/aws/credentials/fetch_test.go
@@ -0,0 +1,77 @@
+package credentials
+
+import (
+	"context"
+	"testing"
+
+	aws "github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/powertoolsdev/mono/pkg/generics"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFetch(t *testing.T) {
+	staticCreds := generics.GetFakeObj[StaticCredentials]()
+	cacheID := generics.GetFakeObj[string]()
+
+	tests := map[string]struct {
+		configFn    func() *Config
+		ctxFn       func(*testing.T, context.Context, *Config) context.Context
+		assertFn    func(*testing.T, *aws.Credentials)
+		errExpected error
+	}{
+		"happy path - creds in context": {
+			configFn: func() *Config {
+				return &Config{
+					StaticCredentials: staticCreds,
+					CacheID:           cacheID,
+				}
+			},
+			ctxFn: func(t *testing.T, ctx context.Context, cfg *Config) context.Context {
+				ctx, err := EnsureContext(ctx, cfg)
+				assert.NoError(t, err)
+				return ctx
+			},
+			assertFn: func(t *testing.T, creds *aws.Credentials) {
+				assert.Equal(t, staticCreds.AccessKeyID, creds.AccessKeyID)
+				assert.Equal(t, staticCreds.SecretAccessKey, creds.SecretAccessKey)
+				assert.Equal(t, staticCreds.SessionToken, creds.SessionToken)
+			},
+		},
+		"happy path - no creds in context": {
+			configFn: func() *Config {
+				return &Config{
+					StaticCredentials: staticCreds,
+				}
+			},
+			ctxFn: func(t *testing.T, ctx context.Context, cfg *Config) context.Context {
+				return ctx
+			},
+			assertFn: func(t *testing.T, creds *aws.Credentials) {
+				assert.Equal(t, staticCreds.AccessKeyID, creds.AccessKeyID)
+				assert.Equal(t, staticCreds.SecretAccessKey, creds.SecretAccessKey)
+				assert.Equal(t, staticCreds.SessionToken, creds.SessionToken)
+			},
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			ctx := context.Background()
+			ctx, cancelFn := context.WithCancel(ctx)
+			defer cancelFn()
+
+			cfg := test.configFn()
+			ctx = test.ctxFn(t, ctx, cfg)
+
+			awsCfg, err := Fetch(ctx, cfg)
+			if test.errExpected != nil {
+				assert.ErrorContains(t, err, test.errExpected.Error())
+				return
+			}
+
+			creds, err := awsCfg.Credentials.Retrieve(ctx)
+			assert.NoError(t, err)
+			test.assertFn(t, &creds)
+		})
+	}
+}
