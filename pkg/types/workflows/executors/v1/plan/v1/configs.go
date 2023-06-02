40992e1b1cdbc9d8dea9e6370d58a7e27e934b42
Jon Morehouse <MorehouseJ09@gmail.com>
2023-06-02 14:42:49 -0600

    feat: fix nil env issue (#1055)
    fix: handle nil values better for some types

--- pkg/types/workflows/executors/v1/plan/v1/configs.go
+++ pkg/types/workflows/executors/v1/plan/v1/configs.go
@@ -2,6 +2,10 @@ package planv1
 
 // ToMap converts a map of env vars into a mapping
 func (e *EnvVars) ToMap() map[string]string {
+	if e == nil {
+		return nil
+	}
+
 	vals := make(map[string]string, 0)
 	for _, envVar := range e.Env {
 		vals[envVar.Name] = envVar.Value
@@ -12,6 +16,10 @@ func (e *EnvVars) ToMap() map[string]string {
 
 // ToMap converts a map of waypoint variables into a mapping
 func (w *WaypointVariables) ToMap() map[string]string {
+	if w == nil {
+		return nil
+	}
+
 	vals := make(map[string]string, 0)
 	for _, wpVar := range w.Variables {
 		vals[wpVar.Name] = wpVar.Value
