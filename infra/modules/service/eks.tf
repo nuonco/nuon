b502fee38f5aa10d09ffd1c38196e9cb9cf0d60d
Jon Morehouse <MorehouseJ09@gmail.com>
2023-06-01 23:05:31 -0600

    fix: executors s3 permissions (#1045)

--- infra/modules/service/eks.tf
+++ infra/modules/service/eks.tf
@@ -1,4 +1,36 @@
 data "aws_iam_policy_document" "service" {
+  // NOTE(jm): the following policies are essentially a giant hack so I can fix installs.
+  //
+  // TLDR -- we've been migrating all services to use this module, and are hitting some limits where a few services need
+  // custom permissions, beyond the standard. Ideally, we could just have the services define their own policy to pass
+  // in, and then everything else here would just work. For now, we can just hard code the permissions, though.
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:CreateBucket",
+      "s3:ListBucket",
+    ]
+    resources = ["*", ]
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:*Object",
+    ]
+    resources = ["*", ]
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:DescribeKey"
+    ]
+    resources = ["*", ]
+  }
+
   statement {
     effect = "Allow"
     actions = [
