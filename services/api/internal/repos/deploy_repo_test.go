f40de0bab13f3647df9381022d9987da70bc2c63
Chris Potter <chris@powertools.dev>
2023-06-09 11:19:56 -0400

    test: Cp/tests deploy repo test (#1145)
    * test:deploy repo tests
    
    Signed-off-by: Chris Potter <chris@powertools.dev>
    
    * test: update test for deploy
    
    Signed-off-by: Chris Potter <chris@powertools.dev>
    
    * fix: missing helper file
    
    Signed-off-by: Chris Potter <chris@powertools.dev>
    
    ---------
    
    Signed-off-by: Chris Potter <chris@powertools.dev>

---
+++ services/api/internal/repos/deploy_repo_test.go
@@ -0,0 +1,114 @@
+package repos
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"github.com/powertoolsdev/mono/pkg/common/shortid"
+	"github.com/powertoolsdev/mono/pkg/common/shortid/domains"
+	"github.com/powertoolsdev/mono/services/api/internal/models"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+// create build for repo tests here since Builds follows different paradigm
+func createBuild(ctx context.Context, t *testing.T, state repoTestState) *models.Build {
+	component := createComponent(ctx, t, state)
+	buildID := domains.NewBuildID()
+	build := models.Build{
+		Model: models.Model{
+			ID: buildID,
+		},
+		GitRef:      "git-ref",
+		ComponentID: component.ID,
+		CreatedByID: "created-by-id",
+	}
+	err := state.db.WithContext(ctx).Create(&build).Error
+	require.NoError(t, err)
+	assert.NotNil(t, build)
+	return &build
+}
+
+func createDeploy(ctx context.Context, t *testing.T, state repoTestState) *models.Deploy {
+	build := createBuild(ctx, t, state)
+	install := createInstall(ctx, t, state)
+
+	deployID := shortid.NewNanoID("dpl")
+	deploy, err := state.deployRepo.Create(ctx, &models.Deploy{
+		BuildID:   build.ID,
+		InstallID: install.ID,
+		Model:     models.Model{ID: deployID},
+	})
+	require.NoError(t, err)
+	assert.NotNil(t, deploy)
+	return deploy
+}
+
+func TestGetDeploy(t *testing.T) {
+	integration := os.Getenv("INTEGRATION")
+	if integration == "" {
+		t.Skip("INTEGRATION=true must be set in environment to run.")
+	}
+
+	execRepoTests(t, []repoTest{
+		{
+			desc: "should get a deploy successfully",
+			fn: func(ctx context.Context, state repoTestState) {
+				origDeploy := createDeploy(ctx, t, state)
+
+				deploy, err := state.deployRepo.Get(ctx, origDeploy.ID)
+				assert.NoError(t, err)
+				assert.NotNil(t, deploy)
+			},
+		},
+		{
+			desc: "should error with canceled context",
+			fn: func(ctx context.Context, state repoTestState) {
+				deploy := createDeploy(ctx, t, state)
+
+				state.ctxCloseFn()
+				fetchedDeploy, err := state.deployRepo.Get(ctx, deploy.ID)
+				assert.Error(t, err)
+				assert.Nil(t, fetchedDeploy)
+			},
+		},
+		{
+			desc: "should error with not found",
+			fn: func(ctx context.Context, state repoTestState) {
+				deployID := shortid.NewNanoID("dpl")
+				fetchedDeploy, err := state.deployRepo.Get(ctx, deployID)
+				assert.Error(t, err)
+				assert.Nil(t, fetchedDeploy)
+			},
+		},
+	})
+}
+
+func TestUpdateDeploy(t *testing.T) {
+	integration := os.Getenv("INTEGRATION")
+	if integration == "" {
+		t.Skip("INTEGRATION=true must be set in environment to run.")
+	}
+
+	execRepoTests(t, []repoTest{
+		{
+			desc: "should get a deploy successfully",
+			fn: func(ctx context.Context, state repoTestState) {
+				origDeploy := createDeploy(ctx, t, state)
+
+				install := createInstall(ctx, t, state)
+
+				origDeploy.InstallID = install.ID
+
+				deploy, err := state.deployRepo.Update(ctx, origDeploy)
+				assert.NoError(t, err)
+				assert.NotNil(t, deploy)
+
+				dbDeploy, err := state.deployRepo.Get(ctx, origDeploy.ID)
+				assert.NoError(t, err)
+				assert.NotNil(t, deploy, dbDeploy)
+			},
+		},
+	})
+}
