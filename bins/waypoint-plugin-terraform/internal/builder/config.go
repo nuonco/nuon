34cbc70314b88cd10d9851be6dceae385a71a73f
Jon Morehouse <MorehouseJ09@gmail.com>
2023-06-10 14:58:05 -0600

    fix: plugin change + added odr-debug script (#1173)
    * feat: add command for debugging odr
    
    * chore: script for debugging waypoint odr jobs
    
    * chore: update values
    
    * reset to main

--- bins/waypoint-plugin-terraform/internal/builder/config.go
+++ bins/waypoint-plugin-terraform/internal/builder/config.go
@@ -2,12 +2,10 @@ package builder
 
 import (
 	"fmt"
-	"os"
 )
 
 type BuildConfig struct {
 	OutputName string `hcl:"output_name,optional"`
-	Source     string `hcl:"source,optional"`
 
 	Labels map[string]string `hacl:"labels,optional"`
 }
@@ -19,14 +17,9 @@ func (b *Builder) Config() (interface{}, error) {
 
 // Implement ConfigurableNotify
 func (b *Builder) ConfigSet(config interface{}) error {
-	c, ok := config.(*BuildConfig)
+	_, ok := config.(*BuildConfig)
 	if !ok {
 		return fmt.Errorf("expected type BuildConfig")
-	}
-
-	_, err := os.Stat(c.Source)
-	if err != nil {
-		return fmt.Errorf("source folder does not exist")
 	}
 
 	return nil
