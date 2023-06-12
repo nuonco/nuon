48f3600234ca1e6c419c3ae7918b7fbbe5ecd9e1
Nat Hamilton <info@nathamilton.com>
2023-06-12 16:21:01 -0400

    feat: added instance field resolvers with integration test (#1211)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/api-gateway/src/gql-resolvers/instance/index.ts
+++ services/api-gateway/src/gql-resolvers/instance/index.ts
@@ -1,6 +1,15 @@
+import { build } from "../build/build";
+import { component } from "../component/component";
+import { deploy } from "../deploy/deploy";
 import { instances } from "./instances";
 
 export const instanceResolvers = {
+  Instance: {
+    build: (parent, _, ctx) => build(undefined, { id: parent.buildId }, ctx),
+    component: (parent, _, ctx) =>
+      component(undefined, { id: parent.componentId }, ctx),
+    deploy: (parent, _, ctx) => deploy(undefined, { id: parent.deployId }, ctx),
+  },
   Query: {
     instances,
   },
