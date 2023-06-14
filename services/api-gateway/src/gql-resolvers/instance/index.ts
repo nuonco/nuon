71a85fa9f08fb546115880d17af8687f7c137534
Nat Hamilton <info@nathamilton.com>
2023-06-14 18:02:02 -0400

    refactor: removed build from instance (#1243)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/api-gateway/src/gql-resolvers/instance/index.ts
+++ services/api-gateway/src/gql-resolvers/instance/index.ts
@@ -1,11 +1,9 @@
-import { build } from "../build/build";
 import { component } from "../component/component";
 import { deploy } from "../deploy/deploy";
 import { instances } from "./instances";
 
 export const instanceResolvers = {
   Instance: {
-    build: (parent, _, ctx) => build(undefined, { instanceId: parent.id }, ctx),
     component: (parent, _, ctx) =>
       component(undefined, { id: parent.componentId }, ctx),
     deploy: (parent, _, ctx) =>
