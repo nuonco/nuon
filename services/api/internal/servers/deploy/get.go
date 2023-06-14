6adddc39ebeb8bded467f0ba103d90f20446649d
Chris Potter <chris@powertools.dev>
2023-06-14 10:28:33 -0400

    feat: instance improvements (#1230)
    * feat: update to remove ids from Instances and add Endpoints for listing by instances
    
    Signed-off-by: Chris Potter <chris@powertools.dev>
    
    * feat: list deploys by instance
    
    Signed-off-by: Chris Potter <chris@powertools.dev>
    
    ---------
    
    Signed-off-by: Chris Potter <chris@powertools.dev>
    Co-authored-by: nuonbot <128654611+nuonbot@users.noreply.github.com>

--- services/api/internal/servers/deploy/get.go
+++ services/api/internal/servers/deploy/get.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/bufbuild/connect-go"
 	deployv1 "github.com/powertoolsdev/mono/pkg/types/api/deploy/v1"
+	"github.com/powertoolsdev/mono/services/api/internal/models"
 )
 
 func (s *server) GetDeploy(
@@ -24,5 +25,33 @@ func (s *server) GetDeploy(
 
 	return connect.NewResponse(&deployv1.GetDeployResponse{
 		Deploy: deploy.ToProto(),
+	}), nil
+}
+
+func (s *server) GetDeploysByInstance(
+	ctx context.Context,
+	req *connect.Request[deployv1.GetDeploysByInstanceRequest],
+) (*connect.Response[deployv1.GetDeploysByInstanceResponse], error) {
+	// run protobuf validations
+	if err := req.Msg.Validate(); err != nil {
+		return nil, fmt.Errorf("input validation failed: %w", err)
+	}
+
+	deploys, _, err := s.repo.ListByInstance(ctx, req.Msg.InstanceId, &models.ConnectionOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	protos := make([]*deployv1.Deploy, len(deploys))
+	for idx, deploy := range deploys {
+		protoComponent := deploy.ToProto()
+		if err != nil {
+			return nil, err
+		}
+		protos[idx] = protoComponent
+	}
+
+	return connect.NewResponse(&deployv1.GetDeploysByInstanceResponse{
+		Deploys: protos,
 	}), nil
 }
