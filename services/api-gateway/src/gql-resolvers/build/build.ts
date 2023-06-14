a2cc330aa4b2923b391d7d94c9d439a51a21a004
Nat Hamilton <info@nathamilton.com>
2023-06-14 18:02:02 -0400

    refactor: removed build from instance (#1243)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/api-gateway/src/gql-resolvers/build/build.ts
+++ services/api-gateway/src/gql-resolvers/build/build.ts
@@ -1,53 +1,24 @@
-import {
-  GetBuildRequest,
-  ListBuildsByInstanceRequest,
-} from "@buf/nuon_apis.grpc_node/build/v1/messages_pb";
+import { GetBuildRequest } from "@buf/nuon_apis.grpc_node/build/v1/messages_pb";
 import { GraphQLError } from "graphql";
 import type { Build, Query, QueryBuildArgs, TResolverFn } from "../../types";
 import { getNodeFields } from "../../utils";
 
 export const build: TResolverFn<QueryBuildArgs, Query["build"]> = (
   _,
-  { id, instanceId },
+  { id },
   { clients }
 ) =>
   new Promise((resolve, reject) => {
     if (clients.build) {
-      if (!id && !instanceId) {
-        reject(
-          new GraphQLError(
-            "Invalid query parameters, please provide either a build ID or instance ID"
-          )
-        );
-      }
+      const request = new GetBuildRequest().setId(id);
 
-      if (id) {
-        const request = new GetBuildRequest().setId(id);
-
-        clients.build.getBuild(request, (err, res) => {
-          if (err) {
-            reject(new GraphQLError(err?.message));
-          } else {
-            resolve(getNodeFields<Build>(res.toObject().build));
-          }
-        });
-      }
-
-      if (instanceId) {
-        const request = new ListBuildsByInstanceRequest().setInstanceId(
-          instanceId
-        );
-
-        clients.build.listBuildsByInstance(request, (err, res) => {
-          if (err) {
-            reject(new GraphQLError(err?.message));
-          } else {
-            const { buildsList } = res.toObject();
-            const recentBuild = buildsList.reverse()[0];
-            resolve(getNodeFields(recentBuild));
-          }
-        });
-      }
+      clients.build.getBuild(request, (err, res) => {
+        if (err) {
+          reject(new GraphQLError(err?.message));
+        } else {
+          resolve(getNodeFields<Build>(res.toObject().build));
+        }
+      });
     } else {
       reject(new GraphQLError("Service isn't available"));
     }
