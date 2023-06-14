ccd3203b333389a6dcaf8c85b7055dfe35e9e1f9
Chris Potter <chris@powertools.dev>
2023-06-14 16:28:19 -0400

    fix: add instance_id to build table (#1241)
    Signed-off-by: Chris Potter <chris@powertools.dev>

--- services/api/migrations/00028_instance_table_component.sql
+++ services/api/migrations/00028_instance_table_component.sql
@@ -6,12 +6,7 @@ ALTER TABLE deploys ADD CONSTRAINT fk_instances_deploys FOREIGN KEY (instance_id
 
 -- +goose Down
 -- +goose StatementBegin
-ALTER TABLE instances DROP CONSTRAINT fk_components_instances;
 ALTER TABLE deploys DROP CONSTRAINT fk_instances_deploys;
-
-
-ALTER TABLE instances DROP COLUMN component_id;
-
 
 ALTER TABLE deploys DROP COLUMN instance_id;
 -- +goose StatementEnd
