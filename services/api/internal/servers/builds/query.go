ce6a04e846577f49e44ff3b1ffee0dea40dd08a8
Chris Potter <chris@powertools.dev>
2023-06-15 11:49:41 -0400

    fix: remove listbyintance from builds (#1246)
    Signed-off-by: Chris Potter <chris@powertools.dev>

--- services/api/internal/servers/builds/query.go
+++ services/api/internal/servers/builds/query.go
@@ -57,32 +57,3 @@ func (s *server) QueryBuilds(
 		Builds: builds,
 	}), nil
 }
-
-func (s *server) ListBuildsByInstance(
-	ctx context.Context,
-	req *connect.Request[buildv1.ListBuildsByInstanceRequest],
-) (*connect.Response[buildv1.ListBuildsByInstanceResponse], error) {
-	// run protobuf validations
-	if err := req.Msg.Validate(); err != nil {
-		return nil, fmt.Errorf("input validation failed: %w", err)
-	}
-
-	// use gorm model to retrieve builds
-	rows, err := s.db.Model(&models.Build{}).Where("instance_id = ?", req.Msg.InstanceId).Rows()
-	if err != nil {
-		return nil, fmt.Errorf("retrieving builds failed: %w", err)
-	}
-	defer rows.Close()
-
-	// iterate through rows and convert to proto
-	builds := []*buildv1.Build{}
-	for rows.Next() {
-		var build *models.Build
-		s.db.ScanRows(rows, &build)
-		builds = append(builds, build.ToProto())
-	}
-
-	return connect.NewResponse(&buildv1.ListBuildsByInstanceResponse{
-		Builds: builds,
-	}), nil
-}
