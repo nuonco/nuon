4d94ca5f47524fec8efea28b3885f041e2a4d616
Jon Morehouse <MorehouseJ09@gmail.com>
2023-06-17 16:45:23 -0600

    feat: new approach to plugin config (#1265)
    * feat: create `pkg/plugins`
    
    * chore: add some scaffolding for other config types

---
+++ pkg/plugins/configs/build_terraform.go
@@ -0,0 +1,11 @@
+package configs
+
+// TerraformBuild is used by the terraform plugin to create an OCI archive with the build parameters.
+type TerraformBuild struct {
+	Name string `hcl:"name,label"`
+
+	OutputName string `hcl:"output_name,optional"`
+
+	Labels    map[string]string `hcl:"labels,optional"`
+	Variables map[string]string `hcl:"variables,optional"`
+}
