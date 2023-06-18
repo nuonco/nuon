9513803f526ee146cc2602f5f1e22c84fd0b68d5
Jon Morehouse <MorehouseJ09@gmail.com>
2023-06-17 18:23:20 -0600

    feat: some nuonctl plugin helpers (#1267)
    * chore: move plugin catalog into `pkg/plugins`
    
    * chore: update path
    
    * feat: add `nuonctl print-latest` command
    
    * remove multi version command
    
    * feat: add list-plugins command
    
    * update

---
+++ pkg/plugins/catalog/get.go
@@ -0,0 +1,61 @@
+package catalog
+
+import (
+	"context"
+	"fmt"
+	"sort"
+
+	"github.com/aws/aws-sdk-go-v2/service/ecrpublic"
+	ecrpublic_types "github.com/aws/aws-sdk-go-v2/service/ecrpublic/types"
+	"github.com/powertoolsdev/mono/pkg/generics"
+)
+
+// GetLatest returns the latest plugin, based on a type
+func (c *catalog) GetLatest(ctx context.Context, typ PluginType) (*Plugin, error) {
+	client, err := c.getClient(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("unable to get ecrpublic client: %w", err)
+	}
+
+	if c.DevOverride {
+		typ = PluginTypeDev
+	}
+
+	plugin, err := c.getLatest(ctx, client, typ)
+	if err != nil {
+		return nil, fmt.Errorf("unable to get latest: %w", err)
+	}
+
+	return plugin, nil
+}
+
+func (c *catalog) getLatest(ctx context.Context, client ecrpublicClient, typ PluginType) (*Plugin, error) {
+	images := make([]ecrpublic_types.ImageTagDetail, 0)
+
+	input := &ecrpublic.DescribeImageTagsInput{
+		RepositoryName: generics.ToPtr(typ.RepositoryName()),
+	}
+	for {
+		resp, err := client.DescribeImageTags(ctx, input)
+		if err != nil {
+			return nil, fmt.Errorf("unable to describe image tags: %w", err)
+		}
+
+		images = append(images, resp.ImageTagDetails...)
+		if resp.NextToken == nil {
+			break
+		}
+		input.NextToken = resp.NextToken
+	}
+	sort.Slice(images, func(i, j int) bool {
+		return images[j].CreatedAt.Before(*images[i].CreatedAt)
+	})
+
+	img := images[0]
+	return &Plugin{
+		Tag:            *img.ImageTag,
+		ImageURL:       typ.ImageURL(),
+		RepositoryName: typ.RepositoryName(),
+		CreatedAt:      *img.CreatedAt,
+	}, nil
+}
---
+++ pkg/plugins/catalog/plugin.go
@@ -0,0 +1,89 @@
+package catalog
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/go-playground/validator/v10"
+)
+
+const (
+	imageURLTemplate string = "public.ecr.aws/p7e3r5y0/%s"
+)
+
+func ToPluginType(val string) (PluginType, error) {
+	for k, v := range pluginTypeNames {
+		if val == v {
+			return k, nil
+		}
+	}
+
+	return PluginTypeUnknown, fmt.Errorf("unable to find plugin type for %s", val)
+}
+
+var pluginTypeNames map[PluginType]string = map[PluginType]string{
+	PluginTypeDefault:   "default",
+	PluginTypeTerraform: "terraform",
+	PluginTypeHelm:      "helm",
+	PluginTypeExp:       "exp",
+	PluginTypeDev:       "dev",
+}
+
+type PluginType int
+
+const (
+	PluginTypeDefault PluginType = iota + 1
+	PluginTypeTerraform
+	PluginTypeExp
+	PluginTypeDev
+	PluginTypeUnknown
+
+	// NOTE(jm): we currently do not publish a helm plugin, but will be doing so once terraform is	finished, so
+	// just adding for now to help drive this architecturally.
+	PluginTypeHelm
+)
+
+// ImageURL returns the image url to access this using our default, public ecr repo
+func (p PluginType) ImageURL() string {
+	return fmt.Sprintf(imageURLTemplate, p.RepositoryName())
+}
+
+func (p PluginType) String() string {
+	return pluginTypeNames[p]
+}
+
+// RepositoryName is used to return the name of a repository. We hard code these here, to prevent having to configure
+// them and/or describe the repository in the public ecr.
+func (p PluginType) RepositoryName() string {
+	switch p {
+	case PluginTypeDefault:
+		return "waypoint-odr"
+	case PluginTypeTerraform:
+		return "waypoint-plugin-terraform"
+	case PluginTypeDev:
+		// TODO(jm): rename this
+		return "dev-public"
+	case PluginTypeExp:
+		return "waypoint-plugin-exp"
+	case PluginTypeHelm:
+		return "waypoint-plugin-helm"
+	default:
+	}
+	return ""
+}
+
+// Plugin is used to return the information needed to use a plugin for an ODR
+type Plugin struct {
+	ImageURL       string    `validate:"required"`
+	Tag            string    `validate:"required"`
+	RepositoryName string    `validate:"required"`
+	CreatedAt      time.Time `validate:"required"`
+}
+
+func (p Plugin) Validate(v validator.Validate) error {
+	if err := v.Struct(p); err != nil {
+		return fmt.Errorf("unable to validate plugin: %w", err)
+	}
+
+	return nil
+}
