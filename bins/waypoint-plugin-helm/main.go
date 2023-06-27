2d3f4b285278fff54e663787d12ab3b40dc05d0f
Jon Morehouse <MorehouseJ09@gmail.com>
2023-06-27 12:26:14 -0600

    chore: initial commit of helm/oci plugin code (#1389)
    * chore: initial commit of helm/oci plugin code
    
    * chore: update imports
    
    * chore: remove LFS
    
    ---------
    
    Co-authored-by: nuonbot <128654611+nuonbot@users.noreply.github.com>

---
+++ bins/waypoint-plugin-helm/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+
+	"github.com/go-playground/validator/v10"
+	sdk "github.com/hashicorp/waypoint-plugin-sdk"
+	"github.com/powertoolsdev/mono/bins/waypoint-plugin-helm/internal/builder"
+	"github.com/powertoolsdev/mono/bins/waypoint-plugin-helm/internal/registry"
+)
+
+func main() {
+	v := validator.New()
+
+	buildPlugin, err := builder.New(v)
+	if err != nil {
+		log.Fatalf("unable to initialize build plugin: %s", err)
+	}
+
+	registryPlugin, err := registry.New(v)
+	if err != nil {
+		log.Fatalf("unable to initialize registry plugin: %s", err)
+	}
+
+	sdk.Main(sdk.WithComponents(
+		buildPlugin,
+		registryPlugin,
+	),
+	// NOTE(jm): eventually, we are going to move the oci plugin stuff into it's own, and expose an OCI artifact
+	// properly.
+	//sdk.WithMappers(registry.ImageMapper),
+	)
+}
