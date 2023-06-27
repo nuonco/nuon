a3552cf3358a96846037877327d164797086857a
Jon Morehouse <MorehouseJ09@gmail.com>
2023-06-27 12:03:10 -0600

    feat: set up executors/components for helm charts (#1380)
    * chore: initialize `helm` plugin infra
    
    * chore: update helm chart
    
    * feat: add private helm chart seed
    
    * chore: add helm chart proto buf types
    
    * chore: add backend for helm chart builds
    
    * fix: helm chart seed + backend import
    
    * chore: remove runner from helm chart build
    
    * chore: import backend for syncing images
    
    * chore: rename `noopimagesync` to `noopsync`
    
    * feat: add noop config + normalize naming
    
    * fixes from renaming oneOf
    
    * feat: add job config for archive syncing
    
    * chore: rename backend + re-integrate into plan
    
    * chore: more renaming + setup plans for oci sync
    
    * feat: integrate new helmchartdeploy backend
    
    * more job config changes
    
    * chore: buf format issues
    
    * fix: don't try to push `plugins`
    
    * chore: update buf dependencies
    
    * api-gateway: update dependencies
    
    * chore: update config protos
    
    * chore: revert suffix on terraform module
    
    * chore: update deps
    
    * chore: revert changes
    
    * chore: re-update npm packages
    
    * chore: update tests with new component configs
    
    * chore: update
    
    ---------
    
    Co-authored-by: nuonbot <128654611+nuonbot@users.noreply.github.com>

--- pkg/types/components/build/v1/build.go
+++ pkg/types/components/build/v1/build.go
@@ -10,6 +10,8 @@ func (c *Config) GetVcsCfg() *vcsv1.Config {
 		return cfg.DockerCfg.GetVcsCfg()
 	case *Config_TerraformModuleCfg:
 		return cfg.TerraformModuleCfg.GetVcsCfg()
+	case *Config_HelmChartCfg:
+		return cfg.HelmChartCfg.GetVcsCfg()
 	case *Config_ExternalImageCfg:
 	case *Config_Noop:
 	default:
