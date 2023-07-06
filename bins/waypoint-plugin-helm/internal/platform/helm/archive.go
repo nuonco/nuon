7a65cfc04b15731a92dca373aab975bb57af52f7
Jon Morehouse <MorehouseJ09@gmail.com>
2023-07-06 11:29:58 -0600

    fix: get helm chart deploys working end to end (#1468)
    * feat: refactor helm chart config for plugin
    
    * chore: update job configs
    
    * fix job config
    
    * chore: add addtl plugin binaries
    
    * fix: bad earthly cp
    
    * chore: update
    
    * fix: archive path
    
    * chore: fix knobs on helm deploy

--- bins/waypoint-plugin-helm/internal/platform/helm/archive.go
+++ bins/waypoint-plugin-helm/internal/platform/helm/archive.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"path/filepath"
+	"strings"
 
 	"github.com/powertoolsdev/mono/pkg/aws/credentials"
 	ecrauthorization "github.com/powertoolsdev/mono/pkg/aws/ecr-authorization"
@@ -12,6 +13,10 @@ import (
 	"oras.land/oras-go/v2/registry/remote"
 	"oras.land/oras-go/v2/registry/remote/auth"
 	"oras.land/oras-go/v2/registry/remote/retry"
+)
+
+const (
+	defaultChartPackageFilename string = "chart.tgz"
 )
 
 func (o *Platform) initECRAuth(ctx context.Context) error {
@@ -34,8 +39,9 @@ func (o *Platform) initECRAuth(ctx context.Context) error {
 	return nil
 }
 
-func (o *Platform) getSrc() (oras.ReadOnlyTarget, error) {
-	repositoryURL := filepath.Join(o.auth.ServerAddress, o.config.Archive.Repo)
+func (o *Platform) getSrcRepo() (oras.ReadOnlyTarget, error) {
+	baseURL := strings.TrimPrefix(o.auth.ServerAddress, "https://")
+	repositoryURL := filepath.Join(baseURL, o.config.Archive.Image)
 	repo, err := remote.NewRepository(repositoryURL)
 	if err != nil {
 		return nil, fmt.Errorf("unable to get repository: %w", err)
@@ -44,7 +50,7 @@ func (o *Platform) getSrc() (oras.ReadOnlyTarget, error) {
 	repo.Client = &auth.Client{
 		Client: retry.DefaultClient,
 		Cache:  auth.DefaultCache,
-		Credential: auth.StaticCredential(o.auth.ServerAddress, auth.Credential{
+		Credential: auth.StaticCredential(baseURL, auth.Credential{
 			Username: o.auth.Username,
 			Password: o.auth.RegistryToken,
 		}),
@@ -54,20 +60,28 @@ func (o *Platform) getSrc() (oras.ReadOnlyTarget, error) {
 }
 
 func (p *Platform) unpackArchive(ctx context.Context) (string, error) {
-	src, err := p.getSrc()
+	if err := p.initECRAuth(ctx); err != nil {
+		return "", fmt.Errorf("unable initialize ecr auth: %w", err)
+	}
+	p.log.Info("successfully initialized ECR auth")
+
+	src, err := p.getSrcRepo()
 	if err != nil {
 		return "", fmt.Errorf("unable to get repo client: %w", err)
 	}
+	p.log.Info("successfully fetched source repo")
 
 	manifest, err := oras.Copy(ctx, src, p.config.Archive.Tag, p.store, p.config.Archive.Tag, oras.DefaultCopyOptions)
 	if err != nil {
 		return "", fmt.Errorf("unable to copy image: %w", err)
 	}
+	p.log.Info("successfully copied manifest ")
 
 	_, err = content.FetchAll(ctx, p.store, manifest)
 	if err != nil {
 		return "", fmt.Errorf("unable to fetch content: %w", err)
 	}
+	p.log.Info("successfully fetched manifest")
 
-	return filepath.Join(p.tmpDir, p.config.Archive.Filename), nil
+	return filepath.Join(p.tmpDir, "store", defaultChartPackageFilename), nil
 }
