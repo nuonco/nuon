053f335513c5d148208d9f63ec062f8393b7e725
Jon Morehouse <MorehouseJ09@gmail.com>
2023-07-08 17:26:24 -0600

    chore: enable ci for terraform provider (#1490)

---
+++ pkg/api/gqlclient/deps.go
@@ -0,0 +1,8 @@
+package gqlclient
+
+import (
+	_ "github.com/alexflint/go-arg"
+	_ "github.com/vektah/gqlparser/v2/validator"
+)
+
+// NOTE: this dependency is used by the generated code, which we don't check in.
