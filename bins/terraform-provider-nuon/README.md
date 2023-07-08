59f6b621ac6836c009ab01b29146ded2dd09e3cc
Jon Morehouse <MorehouseJ09@gmail.com>
2023-07-08 15:17:31 -0600

    feat: create initial scaffolding for terraform provider (#1489)
    * feat: initial commit of scaffolding
    
    * feat: initialize infra for provider
    
    * update earthfile
    
    * disable CI until artifacts are provisioned
    
    * remove
    
    * more deps

---
+++ bins/terraform-provider-nuon/README.md
@@ -0,0 +1,3 @@
+# Terraform Provider Nuon
+
+A terraform provider for interacting with Nuon.
---
+++ bins/terraform-provider-nuon/docs.go
@@ -0,0 +1,6 @@
+package main
+
+// Generate docs
+//
+//go:generate -command tfplugindocs go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
+//go:generate go run tfplugindocs
