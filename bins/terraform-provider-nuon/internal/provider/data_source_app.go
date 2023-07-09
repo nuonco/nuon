61cb9d62c1e00c3f14819d2b0c69df67f2b185f0
Jon Morehouse <MorehouseJ09@gmail.com>
2023-07-09 17:44:41 -0600

    feat: implement tf provider datasources (#1523)
    * feat: implement both org and app datasources
    
    * chore
    
    * chore: initialize demo org
    
    * feat: add install script for terraform provider
    
    * working demo-org terraform

---
+++ bins/terraform-provider-nuon/internal/provider/data_source_app.go
@@ -0,0 +1,92 @@
+package provider
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+	"github.com/powertoolsdev/mono/pkg/api/gqlclient"
+)
+
+var _ datasource.DataSource = &AppDataSource{}
+
+func NewAppDataSource() datasource.DataSource {
+	return &AppDataSource{}
+}
+
+// AppDataSource defines the data source implementation.
+type AppDataSource struct {
+	client gqlclient.Client
+}
+
+// AppDataSourceModel describes the data source data model.
+type AppDataSourceModel struct {
+	Name types.String `tfsdk:"name"`
+	Id   types.String `tfsdk:"id"`
+}
+
+func (d *AppDataSource) Metadata(ctx context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_app"
+}
+
+func (d *AppDataSource) Schema(ctx context.Context, req datasource.SchemaRequest, resp *datasource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		Description:         "`nuon_app` provides information about a Nuon app.\nThis data source can be useful when adding components and installs to an app created in the UI.",
+		MarkdownDescription: "`nuon_app` provides information about a Nuon app.\nThis data source can be useful when adding components and installs to an app created in the UI.",
+
+		Attributes: map[string]schema.Attribute{
+			"name": schema.StringAttribute{
+				Description:         "app name",
+				MarkdownDescription: "app name",
+				Computed:            true,
+			},
+			"id": schema.StringAttribute{
+				Description:         "App id.",
+				MarkdownDescription: "App id.",
+				Required:            true,
+			},
+		},
+	}
+}
+
+func (d *AppDataSource) Configure(ctx context.Context, req datasource.ConfigureRequest, resp *datasource.ConfigureResponse) {
+	if req.ProviderData == nil {
+		return
+	}
+
+	client, ok := req.ProviderData.(gqlclient.Client)
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Data Source Configure Type",
+			fmt.Sprintf("Expected gqlclient.Client, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+		return
+	}
+
+	d.client = client
+}
+
+func (d *AppDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	var data AppDataSourceModel
+	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	tflog.Trace(ctx, "fetching app by id")
+	appResp, err := d.client.GetApp(ctx, data.Id.ValueString())
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Unable to get app",
+			fmt.Sprintf("Please make sure your app_id (%s) is correct, and that the auth token has permissions for this org. ", data.Id.String()),
+		)
+		return
+	}
+	data.Name = types.StringValue(appResp.Name)
+
+	// Save data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
