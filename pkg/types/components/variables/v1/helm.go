31654037768880a7ae286225148f4e4d3886c0ba
Jon Morehouse <MorehouseJ09@gmail.com>
2023-07-11 22:08:58 -0600

    feat: add variables backend (#1539)
    * chore: create variables backend
    
    * chore: outline set concept
    
    * feat: introduce `orgsclient`
    
    Create an api client for interacting with the orgs-api.
    
    * refactor variable helpers
    
    * feat: implement variables backend
    
    * chore: remove terraform deploy
    
    * update terraform component with inputs
    
    * fixes to variable backend

---
+++ pkg/types/components/variables/v1/helm.go
@@ -0,0 +1,37 @@
+package variablesv1
+
+func (e *HelmValue) GetValueNeedEd() string {
+	return e.Value
+}
+
+func (e *HelmValue) SetValue(val string) {
+	e.Value = val
+}
+
+func (e *HelmValues) ToMap() map[string]string {
+	vals := make(map[string]string)
+	for _, hv := range e.Values {
+		vals[hv.Name] = hv.Value
+	}
+
+	return vals
+}
+
+// ToVariables converts a list env vars into a variables array
+func (e *HelmValues) ToVariables() []*Variable {
+	if e == nil {
+		return nil
+	}
+
+	vars := make([]*Variable, 0)
+	for _, envVar := range e.Values {
+		ev := envVar
+		vars = append(vars, &Variable{
+			Actual: &Variable_HelmValue{
+				HelmValue: ev,
+			},
+		})
+	}
+
+	return vars
+}
