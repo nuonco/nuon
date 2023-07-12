f53bf3820e0a019fe4ade975533eacf1a642d7af
Chris Potter <chris@powertools.dev>
2023-07-12 18:14:33 -0400

    feat: add all instance connections to current plan deployment (#1558)
    * feat: add all instance connections to current plan deployment
    
    Signed-off-by: Chris Potter <chris@powertools.dev>
    
    * feat: update with new fields from proto
    
    Signed-off-by: Chris Potter <chris@powertools.dev>
    
    ---------
    
    Signed-off-by: Chris Potter <chris@powertools.dev>

--- services/api/internal/jobs/startdeploy/workflow.go
+++ services/api/internal/jobs/startdeploy/workflow.go
@@ -90,6 +90,11 @@ func (w *wkflow) StartDeploy(ctx workflow.Context, req *jobsv1.StartDeployReques
 		return nil, fmt.Errorf("unable to trigger workflow response: %w", err)
 	}
 
+	fut = workflow.ExecuteActivity(ctx, act.AddConnectionsToPlan, plan)
+	if err = fut.Get(ctx, &plan); err != nil {
+		return nil, fmt.Errorf("unable to add connections to plan: %w", err)
+	}
+
 	//we need to add the installId to the plan
 	switch plan := plan.Actual.(type) {
 	case *planv1.Plan_WaypointPlan:
