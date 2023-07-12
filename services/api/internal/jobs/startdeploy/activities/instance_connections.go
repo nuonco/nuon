8eacbc48ce69b9ad5f2c77d2d3b1f6ffd2eca261
Chris Potter <chris@powertools.dev>
2023-07-12 18:14:33 -0400

    feat: add all instance connections to current plan deployment (#1558)
    * feat: add all instance connections to current plan deployment
    
    Signed-off-by: Chris Potter <chris@powertools.dev>
    
    * feat: update with new fields from proto
    
    Signed-off-by: Chris Potter <chris@powertools.dev>
    
    ---------
    
    Signed-off-by: Chris Potter <chris@powertools.dev>

---
+++ services/api/internal/jobs/startdeploy/activities/instance_connections.go
@@ -0,0 +1,38 @@
+package activities
+
+import (
+	"context"
+
+	connectionsv1 "github.com/powertoolsdev/mono/pkg/types/components/connections/v1"
+	planv1 "github.com/powertoolsdev/mono/pkg/types/workflows/executors/v1/plan/v1"
+)
+
+func (a *activities) AddConnectionsToPlan(ctx context.Context, buildPlan *planv1.Plan) (*planv1.Plan, error) {
+	waypointPlan := buildPlan.GetWaypointPlan()
+
+	instances, err := a.instanceRepo.ListByComponent(ctx, waypointPlan.Component.Id)
+	if err != nil {
+		return nil, err
+	}
+
+	instanceConnections := []*connectionsv1.InstanceConnection{}
+	for _, instance := range instances {
+		connection := &connectionsv1.InstanceConnection{
+			DeployId:      waypointPlan.Metadata.DeploymentId,
+			ComponentId:   instance.ComponentID,
+			ComponentName: instance.Component.Name,
+			InstallId:     instance.InstallID,
+		}
+		instanceConnections = append(instanceConnections, connection)
+	}
+
+	waypointPlan.Component.Connections = &connectionsv1.Connections{
+		Instances: instanceConnections,
+	}
+
+	buildPlan.Actual = &planv1.Plan_WaypointPlan{
+		WaypointPlan: waypointPlan,
+	}
+
+	return buildPlan, nil
+}
