24bad6efbd59cefd3e81b86bf21052eefb1a930d
Nat Hamilton <info@nathamilton.com>
2023-07-14 17:22:12 -0400

    test: better test converage of upsert component resolver (#1613)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/api-gateway/src/gql-resolvers/component/upsert-component/upsert.test.ts
+++ services/api-gateway/src/gql-resolvers/component/upsert-component/upsert.test.ts
@@ -1,306 +1,41 @@
-import { AwsRegion, TerraformVersion } from "../../../types";
-import {
-  parseBuildConfigInput,
-  parseConfigInput,
-  parseDeployConfigInput,
-  upsertComponent,
-} from "./upsert";
+import { parseConfigInput, upsertComponent } from "./upsert";
 
-test("parseBuildConfigInput should return a build config for an external image", () => {
-  const spec = parseBuildConfigInput({
-    externalImageConfig: {
-      ociImageUrl: "some-place.io/test/image",
-      tag: "latest",
-    },
-  });
-  expect(spec.toObject()).toMatchInlineSnapshot(`
-    {
-      "dockerCfg": undefined,
-      "externalImageCfg": {
-        "authCfg": {
-          "awsIamAuthCfg": undefined,
-          "publicAuthCfg": {},
-        },
-        "ociImageUrl": "some-place.io/test/image",
-        "tag": "latest",
-      },
-      "helmChartCfg": undefined,
-      "noop": undefined,
-      "terraformModuleCfg": undefined,
-      "timeout": undefined,
-    }
-  `);
-});
+const mockDateTimeObject = {
+  day: 31,
+  hours: 8,
+  minutes: 15,
+  month: 12,
+  seconds: 30,
+  year: 1999,
+};
 
-test("parseBuildConfigInput should return a build config for a private ECR external image", () => {
-  const spec = parseBuildConfigInput({
-    externalImageConfig: {
-      authConfig: {
-        region: AwsRegion.UsEast_1,
-        role: "test",
-      },
-      ociImageUrl: "some-place.io/test/image",
-      tag: "latest",
-    },
-  });
-  expect(spec.toObject()).toMatchInlineSnapshot(`
-    {
-      "dockerCfg": undefined,
-      "externalImageCfg": {
-        "authCfg": {
-          "awsIamAuthCfg": {
-            "awsRegion": "US_EAST_1",
-            "iamRoleArn": "test",
-          },
-          "publicAuthCfg": undefined,
-        },
-        "ociImageUrl": "some-place.io/test/image",
-        "tag": "latest",
-      },
-      "helmChartCfg": undefined,
-      "noop": undefined,
-      "terraformModuleCfg": undefined,
-      "timeout": undefined,
-    }
-  `);
-});
+const mockComponent = {
+  appId: "app-id",
+  createdAt: mockDateTimeObject,
+  id: "test-id",
+  name: "test-node",
+  updatedAt: mockDateTimeObject,
+};
 
-test("parseBuildConfigInput should return a build config for a docker build with connected github repo", () => {
-  const spec = parseBuildConfigInput({
-    dockerBuildConfig: {
-      dockerfile: "Dockerfile",
-      vcsConfig: {
-        connectedGithub: {
-          branch: "main",
-          directory: "/",
-          repo: "org/repo",
-        },
-      },
-    },
-  });
-  expect(spec.toObject()).toMatchInlineSnapshot(`
-    {
-      "dockerCfg": {
-        "buildArgsList": [],
-        "dockerfile": "Dockerfile",
-        "envVars": undefined,
-        "target": "",
-        "vcsCfg": {
-          "connectedGithubConfig": {
-            "directory": "/",
-            "gitRef": "",
-            "githubAppKeyId": "",
-            "githubAppKeySecretName": "",
-            "githubInstallId": "",
-            "repo": "org/repo",
-          },
-          "publicGitConfig": undefined,
-        },
-      },
-      "externalImageCfg": undefined,
-      "helmChartCfg": undefined,
-      "noop": undefined,
-      "terraformModuleCfg": undefined,
-      "timeout": undefined,
-    }
-  `);
-});
+const mockComponentServiceClient = {
+  upsertComponent: jest
+    .fn()
+    .mockImplementationOnce((req, cb) => {
+      cb(undefined, {
+        toObject: jest.fn().mockReturnValue({
+          component: mockComponent,
+        }),
+      });
+    })
+    .mockImplementationOnce((req, cb) => {
+      const err = new Error("some error");
+      cb(err);
+    }),
+};
 
-test("parseBuildConfigInput should return a build config for a docker build with public git repo and one environment variable", () => {
-  const spec = parseBuildConfigInput({
-    dockerBuildConfig: {
-      dockerfile: "Dockerfile",
-      vcsConfig: {
-        publicGit: {
-          directory: "/",
-          repo: "org/repo",
-        },
-      },
-    },
-  });
-  expect(spec.toObject()).toMatchInlineSnapshot(`
-    {
-      "dockerCfg": {
-        "buildArgsList": [],
-        "dockerfile": "Dockerfile",
-        "envVars": undefined,
-        "target": "",
-        "vcsCfg": {
-          "connectedGithubConfig": undefined,
-          "publicGitConfig": {
-            "directory": "/",
-            "gitRef": "",
-            "repo": "org/repo",
-          },
-        },
-      },
-      "externalImageCfg": undefined,
-      "helmChartCfg": undefined,
-      "noop": undefined,
-      "terraformModuleCfg": undefined,
-      "timeout": undefined,
-    }
-  `);
-});
-
-test("parseBuildConfigInput should return a build config for a helm chart build with public git repo", () => {
-  const spec = parseBuildConfigInput({
-    helmBuildConfig: {
-      chartName: "test-chart",
-      vcsConfig: {
-        publicGit: {
-          directory: "/",
-          repo: "org/repo",
-        },
-      },
-    },
-  });
-  expect(spec.toObject()).toMatchInlineSnapshot(`
-    {
-      "dockerCfg": undefined,
-      "externalImageCfg": undefined,
-      "helmChartCfg": {
-        "chartName": "test-chart",
-        "vcsCfg": {
-          "connectedGithubConfig": undefined,
-          "publicGitConfig": {
-            "directory": "/",
-            "gitRef": "",
-            "repo": "org/repo",
-          },
-        },
-      },
-      "noop": undefined,
-      "terraformModuleCfg": undefined,
-      "timeout": undefined,
-    }
-  `);
-});
-
-test("parseDeployConfigInput should return a deploy config for a basic k8s deployment", () => {
-  const spec = parseDeployConfigInput({
-    basicDeployConfig: {
-      healthCheckPath: "/test",
-      instanceCount: 1,
-      port: 3000,
-    },
-  });
-
-  expect(spec.toObject()).toMatchInlineSnapshot(`
-    {
-      "basic": {
-        "argsList": [],
-        "cpuLimit": "",
-        "cpuRequest": "",
-        "envVars": undefined,
-        "instanceCount": 1,
-        "listenerCfg": {
-          "healthCheckPath": "/test",
-          "listenPort": 3000,
-        },
-        "memLimit": "",
-        "memRequest": "",
-      },
-      "helmChart": undefined,
-      "helmRepo": undefined,
-      "noop": undefined,
-      "terraformModuleConfig": undefined,
-      "timeout": undefined,
-    }
-  `);
-});
-
-test("parseDeployConfigInput should return a deploy config for a helm chart deployment", () => {
-  const spec = parseDeployConfigInput({
-    helmDeployConfig: {
-      noop: true,
-      values: [
-        {
-          key: "key",
-          sensitive: false,
-          value: "value",
-        },
-        {
-          key: "key",
-          sensitive: true,
-          value: "value",
-        },
-      ],
-    },
-  });
-
-  expect(spec.toObject()).toMatchInlineSnapshot(`
-    {
-      "basic": undefined,
-      "helmChart": {
-        "values": {
-          "valuesList": [
-            {
-              "name": "key",
-              "sensitive": false,
-              "value": "value",
-            },
-            {
-              "name": "key",
-              "sensitive": true,
-              "value": "value",
-            },
-          ],
-        },
-      },
-      "helmRepo": undefined,
-      "noop": undefined,
-      "terraformModuleConfig": undefined,
-      "timeout": undefined,
-    }
-  `);
-});
-
-test("parseDeployConfigInput should return a deploy config for a terraform deployment", () => {
-  const spec = parseDeployConfigInput({
-    terraformDeployConfig: {
-      terraformVersion: TerraformVersion.TerraformVersionLatest,
-      vars: [
-        {
-          key: "TEST",
-          value: "test",
-        },
-        {
-          key: "TEST",
-          sensitive: true,
-          value: "test",
-        },
-      ],
-    },
-  });
-
-  expect(spec.toObject()).toMatchInlineSnapshot(`
-    {
-      "basic": undefined,
-      "helmChart": undefined,
-      "helmRepo": undefined,
-      "noop": undefined,
-      "terraformModuleConfig": {
-        "terraformVersion": 1,
-        "vars": {
-          "variablesList": [
-            {
-              "name": "TEST",
-              "sensitive": false,
-              "value": "test",
-            },
-            {
-              "name": "TEST",
-              "sensitive": true,
-              "value": "test",
-            },
-          ],
-        },
-      },
-      "timeout": undefined,
-    }
-  `);
-});
+const mockClients = {
+  component: mockComponentServiceClient,
+};
 
 test("parseConfigInput should return a component config", () => {
   const spec = parseConfigInput({
@@ -359,6 +94,44 @@ test("parseConfigInput should return a component config", () => {
       "id": "",
     }
   `);
+});
+
+test("upsertComponent resolver should return a basic component", async () => {
+  await expect(
+    upsertComponent(
+      undefined,
+      {
+        input: {
+          appId: "app-id",
+          config: { buildConfig: { noop: true }, deployConfig: { noop: true } },
+          id: "test-id",
+          name: "test-node",
+        },
+      },
+      { clients: mockClients }
+    )
+  ).resolves.toEqual({
+    appId: "app-id",
+    config: {
+      __typename: "ComponentConfig",
+      buildConfig: null,
+      deployConfig: null,
+    },
+    createdAt: "1999-12-31T08:15:30.000Z",
+    id: "test-id",
+    name: "test-node",
+    updatedAt: "1999-12-31T08:15:30.000Z",
+  });
+});
+
+test("upsertComponent resolver should return error on failed mutation", async () => {
+  await expect(
+    upsertComponent(
+      undefined,
+      { input: { appId: "error", id: "error", name: "error" } },
+      { clients: mockClients, user: { id: "user-id" } }
+    )
+  ).rejects.toThrowErrorMatchingInlineSnapshot(`"some error"`);
 });
 
 test("upsertComponent resolver should return error if service client doesn't exist", async () => {
