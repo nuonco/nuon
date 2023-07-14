77648d8ef3c25f72976124272e6d27962579c650
Nat Hamilton <info@nathamilton.com>
2023-07-14 17:22:12 -0400

    test: better test converage of upsert component resolver (#1613)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

---
+++ services/api-gateway/src/gql-resolvers/component/upsert-component/parse-deploy-config.ts
@@ -0,0 +1,44 @@
+import { Config as DeployConfig } from "@buf/nuon_components.grpc_node/deploy/v1/config_pb";
+import type { DeployConfigInput, TgRPCMessage } from "../../../types";
+import { initBasicDeployConfig } from "./basic-deploy-config";
+import { initHelmDeployConfig } from "./helm-deploy-config";
+import { initNoopDeployConfig } from "./noop-deploy-config";
+import { initTerraformDeployConfig } from "./terraform-deploy-config";
+
+export function parseDeployConfigInput(
+  deployConfigInput: DeployConfigInput
+): TgRPCMessage {
+  const deployCfg = new DeployConfig();
+
+  if (deployConfigInput?.noop) {
+    const noopCfg = initNoopDeployConfig();
+
+    deployCfg.setNoop(noopCfg);
+  }
+
+  if (deployConfigInput?.basicDeployConfig) {
+    const basicDeployCfg = initBasicDeployConfig(
+      deployConfigInput.basicDeployConfig
+    );
+
+    deployCfg.setBasic(basicDeployCfg);
+  }
+
+  if (deployConfigInput?.helmDeployConfig) {
+    const helmDeployCfg = initHelmDeployConfig(
+      deployConfigInput.helmDeployConfig
+    );
+
+    deployCfg.setHelmChart(helmDeployCfg);
+  }
+
+  if (deployConfigInput?.terraformDeployConfig) {
+    const terraformDeployCfg = initTerraformDeployConfig(
+      deployConfigInput.terraformDeployConfig
+    );
+
+    deployCfg.setTerraformModuleConfig(terraformDeployCfg);
+  }
+
+  return deployCfg;
+}
