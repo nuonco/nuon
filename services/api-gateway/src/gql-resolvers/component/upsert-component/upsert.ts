753cb5809a8ee6247a4585720e7152ec6083500d
Nat Hamilton <info@nathamilton.com>
2023-07-14 17:22:12 -0400

    test: better test converage of upsert component resolver (#1613)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/api-gateway/src/gql-resolvers/component/upsert-component/upsert.ts
+++ services/api-gateway/src/gql-resolvers/component/upsert-component/upsert.ts
@@ -1,116 +1,26 @@
 import { UpsertComponentRequest } from "@buf/nuon_apis.grpc_node/component/v1/messages_pb";
-import { Config as BuildConfig } from "@buf/nuon_components.grpc_node/build/v1/build_pb";
 import { Component } from "@buf/nuon_components.grpc_node/component/v1/component_pb";
-import { Config as DeployConfig } from "@buf/nuon_components.grpc_node/deploy/v1/config_pb";
 import { GraphQLError } from "graphql";
 import type {
-  BuildConfigInput,
   ComponentConfigInput,
-  DeployConfigInput,
   Mutation,
   MutationUpsertComponentArgs,
   TResolverFn,
+  TgRPCMessage,
 } from "../../../types";
 import { formatComponent } from "../utils";
-import { initBasicDeployConfig } from "./basic-deploy-config";
-import { initDockerBuildConfig } from "./docker-build-config";
-import { initExternalImageConfig } from "./external-image-config";
-import { initHelmBuildConfig } from "./helm-build-config";
-import { initHelmDeployConfig } from "./helm-deploy-config";
-import { initNoopBuildConfig } from "./noop-build-config";
-import { initNoopDeployConfig } from "./noop-deploy-config";
-import { initTerraformBuildConfig } from "./terraform-build-config";
-import { initTerraformDeployConfig } from "./terraform-deploy-config";
+import { parseBuildConfigInput } from "./parse-build-config";
+import { parseDeployConfigInput } from "./parse-deploy-config";
 
-export function parseBuildConfigInput(
-  buildConfig: BuildConfigInput
-): Record<string, unknown> | any {
-  const buildCfg = new BuildConfig();
-
-  if (buildConfig?.noop) {
-    const noopCfg = initNoopBuildConfig();
-
-    buildCfg.setNoop(noopCfg);
-  }
-
-  if (buildConfig?.externalImageConfig) {
-    const externalImageCfg = initExternalImageConfig(
-      buildConfig?.externalImageConfig
-    );
-
-    buildCfg.setExternalImageCfg(externalImageCfg);
-  }
-
-  if (buildConfig?.dockerBuildConfig) {
-    const dockerCfg = initDockerBuildConfig(buildConfig?.dockerBuildConfig);
-
-    buildCfg.setDockerCfg(dockerCfg);
-  }
-
-  if (buildConfig?.helmBuildConfig) {
-    const helmBuildCfg = initHelmBuildConfig(buildConfig?.helmBuildConfig);
-
-    buildCfg.setHelmChartCfg(helmBuildCfg);
-  }
-
-  if (buildConfig?.terraformBuildConfig) {
-    const terraformBuildCfg = initTerraformBuildConfig(
-      buildConfig?.terraformBuildConfig
-    );
-
-    buildCfg.setTerraformModuleCfg(terraformBuildCfg);
-  }
-
-  return buildCfg;
-}
-
-export function parseDeployConfigInput(
-  deployConfig: DeployConfigInput
-): Record<string, unknown> | any {
-  const deployCfg = new DeployConfig();
-
-  if (deployConfig?.noop) {
-    const noopCfg = initNoopDeployConfig();
-
-    deployCfg.setNoop(noopCfg);
-  }
-
-  if (deployConfig?.basicDeployConfig) {
-    const basicDeployCfg = initBasicDeployConfig(
-      deployConfig?.basicDeployConfig
-    );
-
-    deployCfg.setBasic(basicDeployCfg);
-  }
-
-  if (deployConfig?.helmDeployConfig) {
-    const helmDeployCfg = initHelmDeployConfig(deployConfig?.helmDeployConfig);
-
-    deployCfg.setHelmChart(helmDeployCfg);
-  }
-
-  if (deployConfig?.terraformDeployConfig) {
-    const terraformDeployCfg = initTerraformDeployConfig(
-      deployConfig?.terraformDeployConfig
-    );
-
-    deployCfg.setTerraformModuleConfig(terraformDeployCfg);
-  }
-
-  return deployCfg;
-}
-
-export function parseConfigInput(
-  config: ComponentConfigInput
-): Record<string, unknown> | any {
+export function parseConfigInput(config: ComponentConfigInput): TgRPCMessage {
   const componentConfig = new Component();
 
   if (config?.buildConfig) {
-    componentConfig.setBuildCfg(parseBuildConfigInput(config?.buildConfig));
+    componentConfig.setBuildCfg(parseBuildConfigInput(config.buildConfig));
   }
 
   if (config?.deployConfig) {
-    componentConfig.setDeployCfg(parseDeployConfigInput(config?.deployConfig));
+    componentConfig.setDeployCfg(parseDeployConfigInput(config.deployConfig));
   }
 
   return componentConfig;
