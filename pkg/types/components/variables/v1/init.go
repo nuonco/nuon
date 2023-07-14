edf9dbeec5df6d640daba85e77615fe055a3c917
Jon Morehouse <MorehouseJ09@gmail.com>
2023-07-13 18:58:46 -0600

    feat: add inputs + intermediate to variables (#1593)
    * feat: add inputs + intermediate to variables
    
    * add additional data into rendered
    
    * update format

---
+++ pkg/types/components/variables/v1/init.go
@@ -0,0 +1,14 @@
+package variablesv1
+
+import (
+	"github.com/go-faker/faker/v4"
+)
+
+func init() {
+	_ = faker.AddProvider("variables", fakeVariables)
+	_ = faker.AddProvider("envVars", fakeEnvVars)
+	_ = faker.AddProvider("helmValues", fakeHelmValues)
+	_ = faker.AddProvider("terraformVariables", fakeTerraformVariables)
+	_ = faker.AddProvider("waypointVariables", fakeWaypointVariables)
+	_ = faker.AddProvider("intermediateData", fakeIntermediateData)
+}
