295bf5e9df8588f9f319ea4466fbfe4170be4519
Jon Morehouse <MorehouseJ09@gmail.com>
2023-07-22 11:28:42 -0600

    feat: add public dns iam role (#1726)
    * feat: add IAM role for public dns + wiki 'bout it
    
    * chore: add k8s values + change output nesting
    
    * docs: developing terraform

---
+++ services/workers-installs/infra/domains_iam.tf
@@ -0,0 +1,54 @@
+data "aws_iam_policy_document" "public_dns_access" {
+  provider = aws.public
+
+  statement {
+    effect = "Allow"
+    // TODO: lock these down once we know what permissions are needed
+    actions = [
+      "route53:*",
+    ]
+    // TODO: lock these down once we know what permissions are needed
+    resources = ["*", ]
+  }
+}
+
+resource "aws_iam_policy" "public_dns_access_policy" {
+  provider = aws.public
+
+  name   = "${local.name}-public-account-dns-access"
+  policy = data.aws_iam_policy_document.public_dns_access.json
+}
+
+data "aws_iam_policy_document" "public_dns_access_trust" {
+  provider = aws.public
+
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole", ]
+
+    principals {
+      type = "AWS"
+      identifiers = [
+        module.iam_eks_role.iam_role_arn,
+        data.tfe_outputs.infra-orgs.values.iam_roles.support.arn,
+      ]
+    }
+  }
+}
+
+module "public_dns_access_role" {
+  providers = {
+    aws = aws.public
+  }
+
+
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
+  version = ">= 5.1.0"
+
+  create_role       = true
+  role_requires_mfa = false
+
+  role_name                = "${local.name}-public-dns-access"
+  custom_role_trust_policy = data.aws_iam_policy_document.public_dns_access_trust.json
+  custom_role_policy_arns  = [aws_iam_policy.public_dns_access_policy.arn, ]
+}
