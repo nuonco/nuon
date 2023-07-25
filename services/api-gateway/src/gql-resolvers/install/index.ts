dfe4d86d6f647072b1d455804c657fd525570240
Nat Hamilton <info@nathamilton.com>
2023-07-25 14:12:26 -0400

    feat: add install.app and app.org field resolver (#1795)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/api-gateway/src/gql-resolvers/install/index.ts
+++ services/api-gateway/src/gql-resolvers/install/index.ts
@@ -1,3 +1,4 @@
+import { app } from "../app/app";
 import { components } from "../component/components";
 import { deployments } from "../deployment/deployments";
 import { deleteInstall } from "./delete-install";
@@ -8,6 +9,7 @@ import { upsertInstall } from "./upsert-install";
 
 export const installResolvers = {
   Install: {
+    app: (parent, _, ctx) => app(parent, { id: parent.appId }, ctx),
     components: (parent, { options }, ctx) =>
       components(parent, { appId: parent.appId, options }, ctx),
     deployments: (parent, { options }, ctx) =>
