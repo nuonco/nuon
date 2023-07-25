b11de1eff3154450199b194f3035347738b72727
Nat Hamilton <info@nathamilton.com>
2023-07-25 16:49:19 -0400

    feat: added deploy log resolver (#1798)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/api-gateway/src/gql-resolvers/deploy/index.ts
+++ services/api-gateway/src/gql-resolvers/deploy/index.ts
@@ -1,4 +1,5 @@
 import { deploy } from "./deploy";
+import { deployLog } from "./deploy-log";
 import { startDeploy } from "./start-deploy";
 
 export const deployResolvers = {
@@ -7,5 +8,6 @@ export const deployResolvers = {
   },
   Query: {
     deploy,
+    deployLog,
   },
 };
