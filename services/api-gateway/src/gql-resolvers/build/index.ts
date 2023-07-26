ca4c73f3cac0104022156bd9ab526293d137f0cc
Nat Hamilton <info@nathamilton.com>
2023-07-26 08:42:15 -0400

    feat: more field resolvers (#1804)
    * feat: added deploy field resolvers for build and install
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat: added build field resolvers for component
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat: added builds resolver to component
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/api-gateway/src/gql-resolvers/build/index.ts
+++ services/api-gateway/src/gql-resolvers/build/index.ts
@@ -1,3 +1,4 @@
+import { component } from "../component/component";
 import { build } from "./build";
 import { buildLog } from "./build-log";
 import { buildStatus } from "./build-status";
@@ -6,6 +7,10 @@ import { cancelBuild } from "./cancel-build";
 import { startBuild } from "./start-build";
 
 export const buildResolvers = {
+  Build: {
+    component: (parent, _, ctx) =>
+      component(parent, { id: parent.componentId }, ctx),
+  },
   Mutation: {
     cancelBuild,
     startBuild,
