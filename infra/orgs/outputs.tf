9d8566501cd88cb97fd165958fb4889df4b125e2
Jon Morehouse <MorehouseJ09@gmail.com>
2023-07-27 09:59:29 -0600

    fix: url needs http (#1824)

--- infra/orgs/outputs.tf
+++ infra/orgs/outputs.tf
@@ -34,7 +34,7 @@ output "org_iam_role_name_templates" {
 
 output "api" {
   value = {
-    url = nonsensitive("orgs-api.${data.tfe_outputs.infra-eks.values.private_zone}"),
+    url = nonsensitive("http://orgs-api.${data.tfe_outputs.infra-eks.values.private_zone}"),
   }
 }
 
