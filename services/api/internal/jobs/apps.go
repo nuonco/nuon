d9d87d6f12c73a42437c64adf1e825aa0b262cbf
Chris Potter <chris@powertools.dev>
2023-07-31 15:03:07 -0400

    feat: update Workflow retry policy (#1873)
    Signed-off-by: Chris Potter <chris@powertools.dev>

--- services/api/internal/jobs/apps.go
+++ services/api/internal/jobs/apps.go
@@ -5,10 +5,13 @@ import (
 	"fmt"
 
 	jobsv1 "github.com/powertoolsdev/mono/pkg/types/api/jobs/v1"
+	enumspb "go.temporal.io/api/enums/v1"
 )
 
 func (m *manager) CreateApp(ctx context.Context, appID string) error {
 	m.Opts.ID = appID
+	m.Opts.WorkflowIDReusePolicy = enumspb.WORKFLOW_ID_REUSE_POLICY_TERMINATE_IF_RUNNING
+
 	_, err := m.Client.ExecuteWorkflowInNamespace(ctx, "apps", m.Opts, "CreateApp", &jobsv1.CreateAppRequest{
 		AppId: appID,
 	})
