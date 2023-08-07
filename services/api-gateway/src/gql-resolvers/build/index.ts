9e3bae29b708f95ac9b1a1854e1e4d49f141ea08
Nat Hamilton <info@nathamilton.com>
2023-08-07 14:25:52 -0400

    refactor: removed logs from gateway api (#1977)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/api-gateway/src/gql-resolvers/build/index.ts
+++ services/api-gateway/src/gql-resolvers/build/index.ts
@@ -1,6 +1,5 @@
 import { component } from "../component/component";
 import { build } from "./build";
-import { buildLog } from "./build-log";
 import { buildStatus } from "./build-status";
 import { builds } from "./builds";
 import { cancelBuild } from "./cancel-build";
@@ -17,7 +16,6 @@ export const buildResolvers = {
   },
   Query: {
     build,
-    buildLog,
     builds,
     buildStatus,
   },
