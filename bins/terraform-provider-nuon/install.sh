ce76b037671e2fb9dde9e2330c4c139d6eef5b08
Jon Morehouse <MorehouseJ09@gmail.com>
2023-08-09 19:18:59 -0600

    chore: add provider install + DEBUG (#2015)
    * fix: actions git short
    
    * add install scripts
    
    ---------
    
    Co-authored-by: nuonbot <128654611+nuonbot@users.noreply.github.com>

--- bins/terraform-provider-nuon/install.sh
+++ bins/terraform-provider-nuon/install.sh
@@ -9,26 +9,9 @@ set -e
 set -o pipefail
 set -u
 
-BASE_URL=https://nuon-artifacts.s3.us-west-2.amazonaws.com/cli
-NAME=nuon
-
-DIR=$HOME/bin
-if [ ! -d "$DIR" ]; then
-  DIR=/usr/local/bin
-
-  # fall back to /usr/local/bin
-  if [ ! -d $DIR ]; then
-    # fall back to /bin
-    DIR=/bin
-  fi
-fi
-
-read -ep "Installing nuon cli into $DIR, would you like to proceed? " -n 1 -r
-echo
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-    exit 1
-fi
+NAME=terraform-provider-nuon
+BASE_URL=https://nuon-artifacts.s3.us-west-2.amazonaws.com/$NAME
+VERSION=v0.0.1
 
 echo "checking OS and Architecture..."
 set +e
@@ -43,16 +26,27 @@ fi
 OS=$(uname -s |  awk '{print tolower($0)}')
 echo "✅ using version ${OS}_${ARCH}..."
 
+DIR=~/.terraform.d/plugins/terraform.local/local/nuon/0.0.1/${OS}_${ARCH}
+read -ep "Installing nuon terraform provider into $DIR, would you like to proceed? " -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 1
+fi
+
 echo "calculating latest version..."
 VERSION=$(curl -s $BASE_URL/latest.txt)
 echo "✅ using version ${VERSION}..."
 
+echo "ensuring directories exist"
+mkdir -p $DIR
+
 echo "fetching binary for ${OS} ${ARCH}..."
-curl -s -o $DIR/$NAME $BASE_URL/$VERSION/${NAME}_${OS}_${ARCH}
+curl -s -o $DIR/${NAME}_${VERSION} $BASE_URL/$VERSION/${NAME}_${OS}_${ARCH}
 echo "✅ fetching binary for ${OS} ${ARCH}..."
 
 echo "making binary executable..."
-chmod +x $DIR/$NAME
+chmod +x $DIR/${NAME}_${VERSION}
 echo "✅ nuon should be ready to use"
 
 echo "🚀 To get started, please run - nuon login"
