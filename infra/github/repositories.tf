3e0ba01dcea1eae2fb53538ef93f24237a014d08
Jordan Acosta <jordan@nuon.co>
2023-08-10 15:21:04 -0700

    feat: add quickstart repo (#2024)

--- infra/github/repositories.tf
+++ infra/github/repositories.tf
@@ -98,3 +98,10 @@ module "waypoint" {
   description = "Our internal fork of hashicorp/waypoint."
   topics      = ["terraform"]
 }
+
+module "quickstart" {
+  source          = "./modules/repository"
+  name            = "quickstart"
+  description     = "A simple example project to easily get up and running with Nuon."
+  required_checks = []
+}
