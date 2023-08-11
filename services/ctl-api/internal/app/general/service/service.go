9034ecdf198e9c95776055d23d840b1f0a80b81e
Jon Morehouse <MorehouseJ09@gmail.com>
2023-08-11 17:54:00 -0600

    feat: add metrics endpoint (#2031)

---
+++ services/ctl-api/internal/app/general/service/service.go
@@ -0,0 +1,33 @@
+package service
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"github.com/powertoolsdev/mono/pkg/metrics"
+	"go.uber.org/zap"
+	"gorm.io/gorm"
+)
+
+type service struct {
+	v  *validator.Validate
+	l  *zap.Logger
+	db *gorm.DB
+	mw metrics.Writer
+}
+
+func (s *service) RegisterRoutes(api *gin.Engine) error {
+	api.POST("/v1/general/metrics", s.PublishMetrics)
+	return nil
+}
+
+func (s *service) RegisterInternalRoutes(api *gin.Engine) error {
+	return nil
+}
+
+func New(v *validator.Validate, db *gorm.DB, mw metrics.Writer, l *zap.Logger) *service {
+	return &service{
+		l:  l,
+		v:  v,
+		mw: mw,
+	}
+}
