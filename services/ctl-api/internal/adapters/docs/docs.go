c017a4d35d32de01daf9ef8360314c9b6403e4b3
Nat Hamilton <info@nathamilton.com>
2023-08-15 09:45:09 -0400

    fix: correct base path for swagger docs (#2091)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/ctl-api/internal/adapters/docs/docs.go
+++ services/ctl-api/internal/adapters/docs/docs.go
@@ -2,7 +2,7 @@ package docs
 
 import (
 	"github.com/gin-gonic/gin"
-	docs "github.com/powertoolsdev/mono/services/ctl-api/docs"
+	"github.com/powertoolsdev/mono/services/ctl-api/docs"
 	swaggerfiles "github.com/swaggo/files"
 	swagger "github.com/swaggo/gin-swagger"
 )
@@ -14,7 +14,7 @@ func (r *Route) RegisterRoutes(g *gin.Engine) error {
 }
 
 func (r *Route) RegisterInternalRoutes(g *gin.Engine) error {
-	docs.SwaggerInfo.BasePath = "/api/v1"
+	docs.SwaggerInfo.BasePath = ""
 	g.GET("/docs/*any", swagger.WrapHandler(swaggerfiles.Handler))
 	return nil
 }
