f5c8d88e15e74480d0484a6a386968d1de14b52f
Jon Morehouse <MorehouseJ09@gmail.com>
2023-08-20 16:28:27 -0600

    feat: create app background job + event loop (#2161)
    * kickoff apps event loop
    
    * update org hooks
    
    * update app model to add status
    
    * update
    
    * chore: remove noisy middleware debug logs
    
    * chore: initialize apps
    
    * updates
    
    * fix local env
    
    * update logic to add app
    
    * fixes and getting app workers running
    
    * fix: get app event loop properly working
    
    * cleanup seed script
    
    * fix: bug where fresh db integration fails

---
+++ services/ctl-api/internal/app/apps/worker/provision.go
@@ -0,0 +1,45 @@
+package worker
+
+import (
+	"fmt"
+
+	appsv1 "github.com/powertoolsdev/mono/pkg/types/workflows/apps/v1"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app/apps/worker/activities"
+	"go.temporal.io/sdk/workflow"
+)
+
+func (w *Workflows) provision(ctx workflow.Context, appID string, dryRun bool) error {
+	var currentApp app.App
+	if err := w.defaultExecGetActivity(ctx, w.acts.Get, activities.GetRequest{
+		AppID: appID,
+	}, &currentApp); err != nil {
+		return fmt.Errorf("unable to get app: %w", err)
+	}
+
+	if err := w.defaultExecErrorActivity(ctx, w.acts.UpdateStatus, activities.UpdateStatusRequest{
+		AppID:             appID,
+		Status:            "provisioning",
+		StatusDescription: "creating app resources - this should just take a few seconds",
+	}); err != nil {
+		return fmt.Errorf("unable to update app status: %w", err)
+	}
+
+	_, err := w.execProvisionWorkflow(ctx, dryRun, &appsv1.ProvisionRequest{
+		OrgId: currentApp.OrgID,
+		AppId: appID,
+	})
+	if err != nil {
+		return fmt.Errorf("unable to provision app: %w", err)
+	}
+
+	// update status with response
+	if err := w.defaultExecErrorActivity(ctx, w.acts.UpdateStatus, activities.UpdateStatusRequest{
+		AppID:             appID,
+		Status:            "active",
+		StatusDescription: "active",
+	}); err != nil {
+		return fmt.Errorf("unable to update app status: %w", err)
+	}
+	return nil
+}
