34bb5bfdacd3d066dea18cf6cfd1f6ed26f0e2ac
Jon Morehouse <MorehouseJ09@gmail.com>
2023-08-20 16:28:27 -0600

    feat: create app background job + event loop (#2161)
    * kickoff apps event loop
    
    * update org hooks
    
    * update app model to add status
    
    * update
    
    * chore: remove noisy middleware debug logs
    
    * chore: initialize apps
    
    * updates
    
    * fix local env
    
    * update logic to add app
    
    * fixes and getting app workers running
    
    * fix: get app event loop properly working
    
    * cleanup seed script
    
    * fix: bug where fresh db integration fails

--- services/ctl-api/internal/app/apps/hooks/created.go
+++ services/ctl-api/internal/app/apps/hooks/created.go
@@ -2,10 +2,45 @@ package hooks
 
 import (
 	"context"
+	"log"
 
+	"github.com/powertoolsdev/mono/pkg/workflows"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app/apps/worker"
+	enumsv1 "go.temporal.io/api/enums/v1"
+	tclient "go.temporal.io/sdk/client"
 	"go.uber.org/zap"
 )
 
-func (a *Hooks) Created(ctx context.Context, id string) {
-	a.l.Info("new app created", zap.String("id", id))
+func (a *Hooks) Created(ctx context.Context, appID string) {
+	workflowID := worker.EventLoopWorkflowID(appID)
+	opts := tclient.StartWorkflowOptions{
+		ID:        workflowID,
+		TaskQueue: workflows.APITaskQueue,
+		// Memo is non-indexed metadata available when listing workflows
+		Memo: map[string]interface{}{
+			"app-id":     appID,
+			"started-by": "api",
+		},
+		WorkflowIDReusePolicy: enumsv1.WORKFLOW_ID_REUSE_POLICY_TERMINATE_IF_RUNNING,
+	}
+	wkflowRun, err := a.client.ExecuteWorkflowInNamespace(ctx,
+		defaultNamespace,
+		opts,
+		worker.EventLoopWorkflowName,
+		appID)
+	if err != nil {
+		log.Fatalln("error creating app event loop", err)
+		return
+	}
+	a.l.Debug("started app event loop",
+		zap.String("workflow-id", wkflowRun.GetID()),
+		zap.String("run-id", wkflowRun.GetID()),
+		zap.String("app-id", appID),
+		zap.Error(err),
+	)
+
+	a.sendSignal(ctx, appID, worker.Signal{
+		DryRun:    a.cfg.DevEnableWorkersDryRun,
+		Operation: worker.OperationProvision,
+	})
 }
