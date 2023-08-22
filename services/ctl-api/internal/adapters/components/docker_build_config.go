a63d6568407d243354bbb623778cf2933c751d00
Jon Morehouse <MorehouseJ09@gmail.com>
2023-08-21 18:09:35 -0600

    feat: map component models to protos + execute (#2169)
    * initial commit
    
    * chore: add component adapter
    
    * update
    
    * update in flight
    
    * chore: implement plan+exec in the component build loop
    
    * fix get workflow response

---
+++ services/ctl-api/internal/adapters/components/docker_build_config.go
@@ -0,0 +1,80 @@
+package components
+
+import (
+	"fmt"
+	"strings"
+
+	buildv1 "github.com/powertoolsdev/mono/pkg/types/components/build/v1"
+	componentv1 "github.com/powertoolsdev/mono/pkg/types/components/component/v1"
+	variablesv1 "github.com/powertoolsdev/mono/pkg/types/components/variables/v1"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
+	"google.golang.org/protobuf/types/known/durationpb"
+)
+
+func (c *Adapter) toEnvVars(inputVals map[string]*string) *variablesv1.EnvVars {
+	vals := make([]*variablesv1.EnvVar, 0)
+	for k, v := range inputVals {
+		if v == nil {
+			continue
+		}
+
+		vals = append(vals, &variablesv1.EnvVar{
+			Name:      k,
+			Value:     *v,
+			Sensitive: true,
+		})
+	}
+
+	return &variablesv1.EnvVars{
+		Env: vals,
+	}
+}
+
+func (c *Adapter) toBuildArgs(inputArgs []string) ([]*buildv1.DockerBuildArg, error) {
+	args := make([]*buildv1.DockerBuildArg, 0)
+	for _, arg := range inputArgs {
+		pieces := strings.SplitN(arg, "=", 2)
+		if len(pieces) != 2 {
+			return nil, fmt.Errorf("invalid docker build arg: %s", arg)
+		}
+
+		args = append(args, &buildv1.DockerBuildArg{
+			Key:   pieces[0],
+			Value: pieces[1],
+		})
+	}
+
+	return args, nil
+}
+
+func (c *Adapter) ToDockerBuildConfig(cfg *app.DockerBuildComponentConfig, connections []app.InstallDeploy, gitRef string) (*componentv1.Component, error) {
+	vcsCfg, err := c.ToVCSConfig(gitRef, cfg.PublicGitVCSConfig, cfg.ConnectedGithubVCSConfig)
+	if err != nil {
+		return nil, fmt.Errorf("unable to get vcs config: %w", err)
+	}
+
+	buildArgs, err := c.toBuildArgs(cfg.BuildArgs)
+	if err != nil {
+		return nil, fmt.Errorf("invalid build args: %w", err)
+	}
+
+	return &componentv1.Component{
+		Id: cfg.ID,
+		BuildCfg: &buildv1.Config{
+			Timeout: durationpb.New(defaultBuildTimeout),
+			Cfg: &buildv1.Config_DockerCfg{
+				DockerCfg: &buildv1.DockerConfig{
+					VcsCfg:     vcsCfg,
+					Dockerfile: cfg.Dockerfile,
+					Target:     cfg.Target,
+					BuildArgs:  buildArgs,
+					EnvVars: &variablesv1.EnvVars{
+						Env: []*variablesv1.EnvVar{},
+					},
+				},
+			},
+		},
+		DeployCfg:   c.toBasicDeployConfig(cfg.SyncOnly, cfg.BasicDeployConfig),
+		Connections: c.toConnections(connections),
+	}, nil
+}
---
+++ services/ctl-api/internal/adapters/components/external_image_config.go
@@ -0,0 +1,45 @@
+package components
+
+import (
+	buildv1 "github.com/powertoolsdev/mono/pkg/types/components/build/v1"
+	componentv1 "github.com/powertoolsdev/mono/pkg/types/components/component/v1"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
+	"google.golang.org/protobuf/types/known/durationpb"
+)
+
+func (c *Adapter) toExternalImageAuthConfig(cfg *app.ExternalImageComponentConfig) *buildv1.ExternalImageAuthConfig {
+	if cfg.AWSECRImageConfig == nil {
+		return &buildv1.ExternalImageAuthConfig{
+			Cfg: &buildv1.ExternalImageAuthConfig_PublicAuthCfg{
+				PublicAuthCfg: &buildv1.PublicAuthCfg{},
+			},
+		}
+	}
+
+	return &buildv1.ExternalImageAuthConfig{
+		Cfg: &buildv1.ExternalImageAuthConfig_AwsIamAuthCfg{
+			AwsIamAuthCfg: &buildv1.AWSIAMAuthCfg{
+				IamRoleArn: cfg.AWSECRImageConfig.IAMRoleARN,
+				AwsRegion:  cfg.AWSECRImageConfig.AWSRegion,
+			},
+		},
+	}
+}
+
+func (c *Adapter) ToExternalImageConfig(cfg *app.ExternalImageComponentConfig, connections []app.InstallDeploy) (*componentv1.Component, error) {
+	return &componentv1.Component{
+		Id: cfg.ID,
+		BuildCfg: &buildv1.Config{
+			Timeout: durationpb.New(defaultBuildTimeout),
+			Cfg: &buildv1.Config_ExternalImageCfg{
+				ExternalImageCfg: &buildv1.ExternalImageConfig{
+					Tag:         cfg.Tag,
+					OciImageUrl: cfg.ImageURL,
+					AuthCfg:     c.toExternalImageAuthConfig(cfg),
+				},
+			},
+		},
+		DeployCfg:   c.toBasicDeployConfig(cfg.SyncOnly, cfg.BasicDeployConfig),
+		Connections: c.toConnections(connections),
+	}, nil
+}
