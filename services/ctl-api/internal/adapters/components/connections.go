d796e99317118714d9031aeab16b889f7331a05d
Jon Morehouse <MorehouseJ09@gmail.com>
2023-08-21 18:09:35 -0600

    feat: map component models to protos + execute (#2169)
    * initial commit
    
    * chore: add component adapter
    
    * update
    
    * update in flight
    
    * chore: implement plan+exec in the component build loop
    
    * fix get workflow response

---
+++ services/ctl-api/internal/adapters/components/connections.go
@@ -0,0 +1,22 @@
+package components
+
+import (
+	connectionsv1 "github.com/powertoolsdev/mono/pkg/types/components/connections/v1"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
+)
+
+func (c *Adapter) toConnections(installDeploys []app.InstallDeploy) *connectionsv1.Connections {
+	instances := make([]*connectionsv1.InstanceConnection, 0)
+	for _, deploy := range installDeploys {
+		instances = append(instances, &connectionsv1.InstanceConnection{
+			ComponentId:   deploy.InstallComponent.ComponentID,
+			InstallId:     deploy.InstallComponent.InstallID,
+			DeployId:      deploy.ID,
+			ComponentName: deploy.InstallComponent.Component.Name,
+		})
+	}
+
+	return &connectionsv1.Connections{
+		Instances: instances,
+	}
+}
--- services/ctl-api/internal/app/components/worker/activities/activities.go
+++ services/ctl-api/internal/app/components/worker/activities/activities.go
@@ -2,14 +2,17 @@ package activities
 
 import (
 	"github.com/powertoolsdev/mono/services/ctl-api/internal"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/adapters/components"
 	"gorm.io/gorm"
 )
 
 type Activities struct {
-	db *gorm.DB
+	db         *gorm.DB
+	components *components.Adapter
 }
 
 func New(cfg *internal.Config,
+	components *components.Adapter,
 	db *gorm.DB) (*Activities, error) {
 	return &Activities{
 		db: db,
