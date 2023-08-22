05dc24ab3223c149d5625cd9feec027ab78cde9e
Jon Morehouse <MorehouseJ09@gmail.com>
2023-08-21 18:09:35 -0600

    feat: map component models to protos + execute (#2169)
    * initial commit
    
    * chore: add component adapter
    
    * update
    
    * update in flight
    
    * chore: implement plan+exec in the component build loop
    
    * fix get workflow response

---
+++ services/ctl-api/internal/app/components/worker/activities/get_component_config.go
@@ -0,0 +1,54 @@
+package activities
+
+import (
+	"context"
+	"fmt"
+
+	componentsv1 "github.com/powertoolsdev/mono/pkg/types/components/component/v1"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
+)
+
+type GetRequest struct {
+	BuildID string `validate:"required"`
+}
+
+func (a *Activities) GetComponentConfig(ctx context.Context, req GetRequest) (*componentsv1.Component, error) {
+	bld := app.ComponentBuild{}
+	res := a.db.WithContext(ctx).
+		Preload("VCSConnectionCommit").
+		Preload("ComponentConfigConnection").
+		Preload("ComponentConfigConnection.Component").
+
+		// preload all terraform configs
+		Preload("ComponentConfigConnection.TerraformModuleComponentConfig").
+		Preload("ComponentConfigConnection.TerraformModuleComponentConfig.PublicGitVCSConfig").
+		Preload("ComponentConfigConnection.TerraformModuleComponentConfig.ConnectedGithubVCSConfig").
+
+		// preload all helm configs
+		Preload("ComponentConfigConnection.HelmComponentConfig").
+		Preload("ComponentConfigConnection.HelmComponentConfig.PublicGitVCSConfig").
+		Preload("ComponentConfigConnection.HelmComponentConfig.ConnectedGithubVCSConfig").
+
+		// preload all docker configs
+		Preload("ComponentConfigConnection.DockerBuildComponentConfig").
+		Preload("ComponentConfigConnection.DockerBuildComponentConfig.PublicGitVCSConfig").
+		Preload("ComponentConfigConnection.DockerBuildComponentConfig.ConnectedGithubVCSConfig").
+		Preload("ComponentConfigConnection.DockerBuildComponentConfig.BasicDeployConfig").
+
+		// preload all external image configs
+		Preload("ComponentConfigConnection.ExternalImageComponentConfig").
+		Preload("ComponentConfigConnection.ExternalImageComponentConfig.PublicGitVCSConfig").
+		Preload("ComponentConfigConnection.ExternalImageComponentConfig.ConnectedGithubVCSConfig").
+		Preload("ComponentConfigConnection.ExternalImageComponentConfig.BasicDeployConfig").
+		First(&bld, "id = ?", req.BuildID)
+	if res.Error != nil {
+		return nil, fmt.Errorf("unable to get build: %w", res.Error)
+	}
+
+	compCfg, err := a.components.FromBuild(&bld)
+	if err != nil {
+		return nil, fmt.Errorf("unable to convert build to component config: %w", err)
+	}
+
+	return compCfg, nil
+}
--- services/ctl-api/internal/app/components/worker/build.go
+++ services/ctl-api/internal/app/components/worker/build.go
@@ -3,6 +3,10 @@ package worker
 import (
 	"fmt"
 
+	componentsv1 "github.com/powertoolsdev/mono/pkg/types/components/component/v1"
+	execv1 "github.com/powertoolsdev/mono/pkg/types/workflows/executors/v1/execute/v1"
+	planv1 "github.com/powertoolsdev/mono/pkg/types/workflows/executors/v1/plan/v1"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/app/components/worker/activities"
 	"go.temporal.io/sdk/workflow"
 )
@@ -16,9 +20,35 @@ func (w *Workflows) build(ctx workflow.Context, cmpID, buildID string, dryRun bo
 		return fmt.Errorf("unable to update build status: %w", err)
 	}
 
+	var app app.App
+	if err := w.defaultExecGetActivity(ctx, w.acts.GetComponentApp, activities.GetComponentAppRequest{
+		ComponentID: cmpID,
+	}, &app); err != nil {
+		return fmt.Errorf("unable to get component app: %w", err)
+	}
+
+	var buildCfg componentsv1.Component
+	if err := w.defaultExecGetActivity(ctx, w.acts.GetComponentConfig, activities.GetRequest{
+		BuildID: buildID,
+	}, &buildCfg); err != nil {
+		return fmt.Errorf("unable to get build component config: %w", err)
+	}
+
 	// execute the plan phase here
-	if dryRun {
-		workflow.Sleep(ctx, w.cfg.DevDryRunSleep)
+	buildPlanWorkflowID := fmt.Sprintf("%s-build-plan-%s", cmpID, buildID)
+	planResp, err := w.execCreatePlanWorkflow(ctx, dryRun, buildPlanWorkflowID, &planv1.CreatePlanRequest{
+		Input: &planv1.CreatePlanRequest_Component{
+			Component: &planv1.ComponentInput{
+				OrgId:     app.OrgID,
+				AppId:     app.ID,
+				BuildId:   buildID,
+				Component: &buildCfg,
+				Type:      planv1.ComponentInputType_COMPONENT_INPUT_TYPE_WAYPOINT_BUILD,
+			},
+		},
+	})
+	if err != nil {
+		return fmt.Errorf("unable to create build plan: %w", err)
 	}
 
 	// update status with response
@@ -31,8 +61,12 @@ func (w *Workflows) build(ctx workflow.Context, cmpID, buildID string, dryRun bo
 	}
 
 	// execute the exec phase here
-	if dryRun {
-		workflow.Sleep(ctx, w.cfg.DevDryRunSleep)
+	buildExecuteWorkflowID := fmt.Sprintf("%s-build-execute-%s", cmpID, buildID)
+	_, err = w.execExecPlanWorkflow(ctx, dryRun, buildExecuteWorkflowID, &execv1.ExecutePlanRequest{
+		Plan: planResp.Plan,
+	})
+	if err != nil {
+		return fmt.Errorf("unable to execute build plan: %w", err)
 	}
 
 	if err := w.defaultExecErrorActivity(ctx, w.acts.UpdateBuildStatus, activities.UpdateBuildStatus{
