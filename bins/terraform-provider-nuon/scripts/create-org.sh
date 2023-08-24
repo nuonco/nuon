c3dda0de3272a3027ac1fd3c78b3b1c2de95fd90
Jon Morehouse <MorehouseJ09@gmail.com>
2023-08-24 00:07:44 -0600

    chore: enable middlewares in stage/prod (#2214)
    * chore: enable middlewares in stage/prod
    
    * disable generate_token
    
    * fix: mark health checks as public + provider script
    
    ---------
    
    Co-authored-by: nuonbot <128654611+nuonbot@users.noreply.github.com>

--- bins/terraform-provider-nuon/scripts/create-org.sh
+++ bins/terraform-provider-nuon/scripts/create-org.sh
@@ -1,1 +1,7 @@
-curl -X POST --data '{"name": "My Org"}' localhost:8081/v1/orgs
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+set -u
+
+curl -vvv -H "Authorization: Bearer $NUON_API_TOKEN" -X POST --data '{"name": "My Org"}' localhost:8081/v1/orgs
