d23d9f9a28cefefdd62507895c86754e28ddaaed
Jon Morehouse <MorehouseJ09@gmail.com>
2023-08-24 22:02:22 -0600

    feat: support seeding api locally from terraform (#2227)
    * feat: support seeding from terraform
    
    * chore: update terraform for local seeding
    
    * fix: get working with existing code bases
    
    * chore: update to new provider
    
    * chore: update to new provider
    
    * feat: get seeding with terraform working
    
    * tf to stderr

--- bins/terraform-provider-nuon/internal/provider/api_conversion.go
+++ bins/terraform-provider-nuon/internal/provider/api_conversion.go
@@ -1,26 +1,5 @@
 package provider
 
-import (
-	"fmt"
-
-	"github.com/powertoolsdev/mono/pkg/deprecated/api/gqlclient"
-)
-
-func stringToAPIRegion(val string) (gqlclient.AWSRegion, error) {
-	switch val {
-	case "us-east-1":
-		return gqlclient.AWSRegionUsEast1, nil
-	case "us-east-2":
-		return gqlclient.AWSRegionUsEast2, nil
-	case "us-west-1":
-		return gqlclient.AWSRegionUsWest1, nil
-	case "us-west-2":
-		return gqlclient.AWSRegionUsWest2, nil
-	}
-
-	return gqlclient.AWSRegion(""), fmt.Errorf("region not supported: %s", val)
-}
-
 const (
 	// orgs, apps & installs:
 	statusQueued         = "queued"
