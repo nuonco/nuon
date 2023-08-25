83ada5446efb0f9b613e2199df3a3901df555601
Jon Morehouse <MorehouseJ09@gmail.com>
2023-08-24 19:33:04 -0600

    feat: cleaning up some seeding to simplify things (#2226)
    * chore: update db
    
    * remove seed script
    
    * chore: remove a whole bunch of legacy stuff
    
    * chore: update namespaces
    
    * fix provision script
    
    * update

--- services/ctl-api/internal/adapters/db/auto_migrate.go
+++ services/ctl-api/internal/adapters/db/auto_migrate.go
@@ -2,58 +2,40 @@ package db
 
 import (
 	"context"
+	"fmt"
 
-	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal"
 	"go.uber.org/fx"
 	"go.uber.org/zap"
 	"gorm.io/gorm"
 )
 
-type AutoMigrate struct{}
+type AutoMigrate struct {
+	db  *gorm.DB
+	l   *zap.Logger
+	cfg *internal.Config
+}
 
-func NewAutoMigrate(db *gorm.DB, l *zap.Logger, lc fx.Lifecycle, shutdowner fx.Shutdowner) *AutoMigrate {
+func NewAutoMigrate(db *gorm.DB, cfg *internal.Config, l *zap.Logger, lc fx.Lifecycle, shutdowner fx.Shutdowner) *AutoMigrate {
+	a := AutoMigrate{
+		db:  db,
+		l:   l,
+		cfg: cfg,
+	}
+
 	lc.Append(fx.Hook{
-		OnStart: func(_ context.Context) error {
-			l.Info("running auto migrate")
+		OnStart: func(ctx context.Context) error {
+			if err := a.migrateModels(ctx); err != nil {
+				return fmt.Errorf("unable to migrate models: %w", err)
+			}
 
-			// org basics
-			db.AutoMigrate(&app.Org{})
-			db.AutoMigrate(&app.UserOrg{})
-			db.AutoMigrate(&app.UserToken{})
+			if err := a.seedModels(ctx); err != nil {
+				return fmt.Errorf("unable to seed models: %w", err)
+			}
 
-			// vcs basics
-			db.AutoMigrate(&app.VCSConnection{})
-			db.AutoMigrate(&app.VCSConnectionCommit{})
-
-			// apps
-			db.AutoMigrate(&app.App{})
-			db.AutoMigrate(&app.Sandbox{})
-			db.AutoMigrate(&app.SandboxRelease{})
-
-			// installs
-			db.AutoMigrate(&app.AWSAccount{})
-			db.AutoMigrate(&app.Install{})
-
-			// component configuration
-			db.AutoMigrate(&app.Component{})
-			db.AutoMigrate(&app.ComponentConfigConnection{})
-			db.AutoMigrate(&app.HelmComponentConfig{})
-			db.AutoMigrate(&app.TerraformModuleComponentConfig{})
-			db.AutoMigrate(&app.DockerBuildComponentConfig{})
-			db.AutoMigrate(&app.ExternalImageComponentConfig{})
-			db.AutoMigrate(&app.ConnectedGithubVCSConfig{})
-			db.AutoMigrate(&app.PublicGitVCSConfig{})
-			db.AutoMigrate(&app.BasicDeployConfig{})
-			db.AutoMigrate(&app.AWSECRImageConfig{})
-
-			// component management
-			db.AutoMigrate(&app.ComponentBuild{})
-
-			// install management
-			db.AutoMigrate(&app.InstallDeploy{})
-			db.AutoMigrate(&app.InstallComponent{})
-
-			shutdowner.Shutdown()
+			if err := shutdowner.Shutdown(); err != nil {
+				return fmt.Errorf("unable to shut down: %w", err)
+			}
 			return nil
 		},
 		OnStop: func(_ context.Context) error {
@@ -61,5 +43,5 @@ func NewAutoMigrate(db *gorm.DB, l *zap.Logger, lc fx.Lifecycle, shutdowner fx.S
 		},
 	})
 
-	return &AutoMigrate{}
+	return &a
 }
