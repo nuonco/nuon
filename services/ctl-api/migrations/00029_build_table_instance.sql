fff9580ad553edb5d6b035117985b27d2ba85d2b
Jon Morehouse <MorehouseJ09@gmail.com>
2023-08-25 18:50:39 -0600

    feat: improve automation of api (#2238)
    * fix: key
    
    * chore: api shuts down properly
    
    * delete old migrations
    
    * remove unneeded nuonctl config
    
    * feat: kickoff
    
    * remove goose
    
    * update wiki
    
    * fix: nuonctl handles startup
    
    * update
    
    * update compose + directions
    
    * update

--- services/ctl-api/migrations/00029_build_table_instance.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-ALTER TABLE builds ADD COLUMN instance_id text NULL;
--- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
-ALTER TABLE builds DROP COLUMN instance_id;
--- +goose StatementEnd
