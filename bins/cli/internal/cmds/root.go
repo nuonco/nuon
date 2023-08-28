68ecf2f3701914e2441a3216194e7b10d0298ec3
Nat Hamilton <info@nathamilton.com>
2023-08-28 16:24:22 -0400

    feat: comp build release cmds (#2276)
    * fix: builds get correct ID format
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * refactor: removed top level releases cmds
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat: added build and release cmds for components
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- bins/cli/internal/cmds/root.go
+++ bins/cli/internal/cmds/root.go
@@ -36,7 +36,6 @@ func Execute() {
 		"all":        c.registerCtl,
 		"components": c.registerComponents,
 		"installls":  c.registerInstalls,
-		"releases":   c.registerReleases,
 		"version":    c.registerVersion,
 	}
 	for ns, fn := range namespaces {
