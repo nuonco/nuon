54d88c89d344c6e04f9af42e11f0d2697a384188
Jordan Acosta <jordan@nuon.co>
2023-09-02 09:19:54 -0700

    feat: cli builds commands (#2324)
    I tried adding these under the components command, but it was awkward to
    implement and use, so I created a top-level builds command.
    
    I noticed that we need the component ID to get a build, even when we
    already have the build ID. Are build IDs not unique? Feels wrong.
    
    I also realized that the "register" functions used for the commands are
    actually constructors, so I'll be renaming them. This also made me
    realize I could refactor the root command to further simplify the code
    and clarify some boundaries in the code. I'll have a follow-up PR for
    that soon.

---
+++ bins/cli/cmd/builds.go
@@ -0,0 +1,64 @@
+package cmd
+
+import (
+	"github.com/powertoolsdev/mono/bins/cli/internal/builds"
+	"github.com/spf13/cobra"
+)
+
+// newBuildsCmd constructs a new builds command
+func newBuildsCmd(buildsService *builds.Service) cobra.Command {
+	var (
+		buildID string
+		compID  string
+	)
+
+	buildsCmd := &cobra.Command{
+		Use:   "builds",
+		Short: "Manage component builds",
+		Long:  "Manage builds of your app components",
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			return bindConfig(cmd)
+		},
+	}
+
+	listCmd := &cobra.Command{
+		Use:     "list",
+		Aliases: []string{"ls"},
+		Short:   "List builds",
+		Long:    "List your app's builds",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return buildsService.List(cmd.Context(), compID)
+		},
+	}
+	listCmd.Flags().StringVarP(&compID, "component-id", "c", "", "The ID of a component to filter builds by")
+	listCmd.MarkFlagRequired("component-id")
+	buildsCmd.AddCommand(listCmd)
+
+	getCmd := &cobra.Command{
+		Use:   "get",
+		Short: "Get component",
+		Long:  "Get app component by ID",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return buildsService.Get(cmd.Context(), compID, buildID)
+		},
+	}
+	getCmd.Flags().StringVarP(&compID, "component-id", "c", "", "The ID of the component whose build you want to view")
+	getCmd.MarkFlagRequired("component-id")
+	getCmd.Flags().StringVarP(&buildID, "build-id", "b", "", "The ID of the build you want to view")
+	getCmd.MarkFlagRequired("build-id")
+	buildsCmd.AddCommand(getCmd)
+
+	createCmd := &cobra.Command{
+		Use:   "create",
+		Short: "Create a build",
+		Long:  "Create a build of an app component",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return buildsService.Create(cmd.Context(), compID)
+		},
+	}
+	createCmd.Flags().StringVarP(&compID, "component-id", "c", "", "The ID of the component you want to create a build for")
+	createCmd.MarkFlagRequired("component-id")
+	buildsCmd.AddCommand(createCmd)
+
+	return *buildsCmd
+}
