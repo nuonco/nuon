f3ab83f29ed433041c91c7b02bc2c74045967a4d
Jordan Acosta <jordan@nuon.co>
2023-09-02 11:08:30 -0700

    feat: cli release commands (#2326)
    The CLI now has basic commands for managing releases. The output isn't
    supper useful yet, especially for release steps, but everything is wired
    up.
    
    I left the logs command commented out for this PR. That will require
    some more intensive UI work, which will come in another PR.
    
    I also noticed that the cobra MarkFlagsOneRequired isn't available,
    which would be great for the `releases list` command. Maybe we need to
    update our version of cobra.

---
+++ bins/cli/internal/releases/service.go
@@ -0,0 +1,13 @@
+package releases
+
+import "github.com/powertoolsdev/mono/pkg/api/client"
+
+type Service struct {
+	api client.Client
+}
+
+func New(apiClient client.Client) *Service {
+	return &Service{
+		api: apiClient,
+	}
+}
