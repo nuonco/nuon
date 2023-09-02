99365251dd91d4edba561c7a4b7b7257a508ca52
Jordan Acosta <jordan@nuon.co>
2023-09-02 10:07:18 -0700

    refactor: decouple cli command constructors from init code (#2325)
    Previously, the Execute function was acting as both init code for the
    executable -- what you expect in the cmd directory -- as well as a
    constructor the root command. This PR pulls those two roles apart, and
    treats all the commands as modular components with their own
    constructors, that are then composed together by the Execute function.
    
    It should be possible now to pull the commands themselves out of cmd,
    maybe into something like `/internal/commands`. I'm not sure about that
    yet, though, so I'll leave them where they are for now.

--- bins/cli/cmd/root.go
+++ bins/cli/cmd/root.go
@@ -1,75 +1,27 @@
 package cmd
 
 import (
-	"context"
-	"log"
-	"os"
-
-	"github.com/go-playground/validator/v10"
-	"github.com/powertoolsdev/mono/bins/cli/internal/apps"
-	"github.com/powertoolsdev/mono/bins/cli/internal/builds"
-	"github.com/powertoolsdev/mono/bins/cli/internal/components"
-	"github.com/powertoolsdev/mono/bins/cli/internal/installs"
-	"github.com/powertoolsdev/mono/bins/cli/internal/orgs"
-	"github.com/powertoolsdev/mono/bins/cli/internal/version"
-	"github.com/powertoolsdev/mono/pkg/ui"
 	"github.com/spf13/cobra"
 )
 
-// Execute is essentially the init method of the CLI. It initializes all the components and composes them together.
-func Execute() {
-	// Create the root command that all other commands will be nested under.
-	// If there are any flags or other settings that we want to be "global",
-	// they should be configured on this command.
+// newRootCmd constructs a new root cobra command, which all other commands will be nested under. If there are any flags or other settings that we want to be "global", they should be configured on this command.
+func newRootCmd(
+	bindConfig bindConfigFunc,
+	cmds ...*cobra.Command,
+) *cobra.Command {
 	rootCmd := &cobra.Command{
 		Use:          "nuonctl",
 		SilenceUsage: true,
-		// TODO(ja): PersistentPreRunE is only inherited by immediate child commands,
-		// so we still have to set this on each subcommand, so that it's children
-		// will inherit it. There are a couple ways we can refactor things to avoid
-		// this, but for now I'm just copy/pasting.
+		// PersistentPreRunE is only inherited by immediate child commands.
+		// We have to copy/paste this on each subcommand, so that it's children will inherit it.
 		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
 			return bindConfig(cmd)
 		},
 	}
 
-	// Construct a validator for the API client and the UI logger.
-	vld := validator.New()
-
-	// Construct an API client for the services to use.
-	api, err := newAPI(vld)
-	if err != nil {
-		log.Fatal(err)
+	for _, cmd := range cmds {
+		rootCmd.AddCommand(cmd)
 	}
 
-	// Construct the domain services, and pass them to the CLI commands that use them.
-	appsCmd := registerApps(apps.New(api))
-	rootCmd.AddCommand(&appsCmd)
-
-	componentsCmd := registerComponents(components.New(api))
-	rootCmd.AddCommand(&componentsCmd)
-
-	installsCmd := registerInstalls(installs.New(api))
-	rootCmd.AddCommand(&installsCmd)
-
-	orgsCmd := registerOrgs(orgs.New(api))
-	rootCmd.AddCommand(&orgsCmd)
-
-	versionCmd := registerVersion(version.New())
-	rootCmd.AddCommand(&versionCmd)
-
-	buildsCmd := newBuildsCmd(builds.New(api))
-	rootCmd.AddCommand(&buildsCmd)
-
-	// Create a context to pass down the code path.
-	ctx := context.Background()
-	uiLog, err := ui.New(vld)
-	if err != nil {
-		log.Fatalf("unable to initialize ui: %s", err)
-	}
-	ctx = ui.WithContext(ctx, uiLog)
-
-	if err := rootCmd.ExecuteContext(ctx); err != nil {
-		os.Exit(2)
-	}
+	return rootCmd
 }
