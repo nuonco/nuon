172adfb11d30af7b6e2bd78eda6d43d7d5a649d5
Jordan Acosta <jordan@nuon.co>
2023-09-05 17:22:36 -0700

    refactor: replace pkg/ui with pterm for cli builds (#2349)
    This swaps in pterm for `pkg/ui`. I'm wrapping pterm's printers in our
    own types, in an internal package, so we can configure printer behavior,
    override methods, set up a custom theme, etc. This also keeps all pterm
    references out of non-UI code, so changes should be easier.
    
    I tried a couple different ways of integrating pterm. I'm not happy with
    what I have now, but the commands work, so I'm merging this now, and
    then will iterate as I add more commands. Right now, the printers are
    constructed within the service methods, when we should be injecting
    them. I'll need to refactor `cmd/execute.go` to set this up properly.

--- bins/cli/internal/builds/create.go
+++ bins/cli/internal/builds/create.go
@@ -2,16 +2,17 @@ package builds
 
 import (
 	"context"
-	"fmt"
 
+	"github.com/powertoolsdev/mono/bins/cli/internal/ui"
 	"github.com/powertoolsdev/mono/pkg/api/client/models"
-	"github.com/pterm/pterm"
 )
 
-func (s *Service) Create(ctx context.Context, compID string) error {
-	pterm.DefaultSpinner.Start()
+func (s *Service) Create(ctx context.Context, compID string) {
+	spinner := ui.NewSpinner()
+	spinner.Start()
+	basicText := ui.NewBasicText()
 
-	pterm.DefaultSpinner.UpdateText(fmt.Sprintf("Starting build for component %s", compID))
+	spinner.UpdateText(basicText.Sprintfln("Starting build for component %s", compID))
 	build, err := s.api.CreateComponentBuild(
 		ctx,
 		compID,
@@ -20,12 +21,10 @@ func (s *Service) Create(ctx context.Context, compID string) error {
 		},
 	)
 	if err != nil {
-		pterm.DefaultSpinner.Fail(fmt.Sprintf("build failed: %s", err))
-		return nil
+		spinner.Fail(basicText.Sprintfln("build failed: %s", err))
+		return
 	}
 
-	pterm.DefaultSpinner.Success(fmt.Sprintf("build completed: %s", build.ID))
-
-	pterm.DefaultSpinner.Stop()
-	return nil
+	spinner.Success(basicText.Sprintfln("build completed: %s", build.ID))
+	spinner.Stop()
 }
