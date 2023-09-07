7de11b2abacf804ce6f409d368e79a42950c5076
Jordan Acosta <jordan@nuon.co>
2023-09-07 14:45:04 -0700

    feat: Create a public repo for our terraform provider (#2366)
    We need it to be open source so we can publish it Hashicorp's official
    registry.

--- infra/github/repos_public.tf
+++ infra/github/repos_public.tf
@@ -5,3 +5,19 @@ module "quickstart" {
   required_checks = []
   is_public       = true
 }
+
+module "terraform-provider-nuon" {
+  source          = "./modules/repository"
+  name            = "terraform-provider-nuon"
+  description     = "A Terraform provider for managing applications in Nuon."
+  required_checks = []
+  is_public       = true
+}
+
+module "nuon-sdk" {
+  source          = "./modules/repository"
+  name            = "nuon-sdk"
+  description     = "An SDK for interacting with the Nuon platform."
+  required_checks = []
+  is_public       = true
+}
