6ccad93f8593b1ec6b52f80cadc067c2fbe50c3d
Jordan Acosta <jordan@nuon.co>
2023-09-12 21:06:49 -0700

    feat: always update release status (#2439)
    The ctl-api should now update the status of a release in all cases.

--- services/ctl-api/internal/app/releases/worker/provision.go
+++ services/ctl-api/internal/app/releases/worker/provision.go
@@ -6,23 +6,36 @@ import (
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/app/releases/worker/activities"
 	"go.temporal.io/sdk/workflow"
+	"go.uber.org/zap"
 )
 
+func (w *Workflows) updateStatus(ctx workflow.Context, releaseID, status, statusDescription string) {
+	err := w.defaultExecErrorActivity(ctx, w.acts.UpdateStatus, activities.UpdateStatusRequest{
+		ReleaseID:         releaseID,
+		Status:            status,
+		StatusDescription: statusDescription,
+	})
+	if err == nil {
+		return
+	}
+
+	w.l.Error("unable to update release status",
+		zap.String("release-id", releaseID),
+		zap.Error(err))
+}
+
 func (w *Workflows) provision(ctx workflow.Context, releaseID string, dryRun bool) error {
+	w.updateStatus(ctx, releaseID, "provisioning", "provisioning release")
+
 	var release app.ComponentRelease
 	if err := w.defaultExecGetActivity(ctx, w.acts.Get, activities.GetRequest{
 		ReleaseID: releaseID,
 	}, &release); err != nil {
-		return fmt.Errorf("unable to get release: %w", err)
+		w.updateStatus(ctx, releaseID, "failed", "unable to read release record from database")
+		return fmt.Errorf("unable to read release record from database: %w", err)
 	}
 
-	if err := w.defaultExecErrorActivity(ctx, w.acts.UpdateStatus, activities.UpdateStatusRequest{
-		ReleaseID:         releaseID,
-		Status:            "executing_release_steps",
-		StatusDescription: fmt.Sprintf("executing %d release steps", len(release.ComponentReleaseSteps)),
-	}); err != nil {
-		return fmt.Errorf("unable to update release status: %w", err)
-	}
+	w.updateStatus(ctx, releaseID, "executing_release_steps", fmt.Sprintf("executing %d release steps", len(release.ComponentReleaseSteps)))
 
 	// now trigger each step of the release
 	for _, step := range release.ComponentReleaseSteps {
@@ -35,25 +48,12 @@ func (w *Workflows) provision(ctx workflow.Context, releaseID string, dryRun boo
 			stepWorkflowID,
 			req,
 		); err != nil {
-			if updateErr := w.defaultExecErrorActivity(ctx, w.acts.UpdateStatus, activities.UpdateStatusRequest{
-				ReleaseID:         releaseID,
-				Status:            "failed",
-				StatusDescription: "release step failed",
-			}); updateErr != nil {
-				return fmt.Errorf("unable to update release status: %w", err)
-			}
-
+			w.updateStatus(ctx, releaseID, "failed", "release step failed")
 			return fmt.Errorf("release failed: %w", err)
 		}
 	}
 
 	// update release status
-	if err := w.defaultExecErrorActivity(ctx, w.acts.UpdateStatus, activities.UpdateStatusRequest{
-		ReleaseID:         releaseID,
-		Status:            "active",
-		StatusDescription: "release succeeded",
-	}); err != nil {
-		return fmt.Errorf("unable to update release status: %w", err)
-	}
+	w.updateStatus(ctx, releaseID, "active", "release succeeded")
 	return nil
 }
