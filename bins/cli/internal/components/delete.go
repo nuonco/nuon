e4a4c7adf367f743a72e59ee9b8510180de12564
Nat Hamilton <info@nathamilton.com>
2023-09-14 13:58:48 -0400

    feat: json flag cli delete cmds (#2483)
    * feat: installs delete cmd supports --json flag
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat: components delete cmd supports --json flag
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- bins/cli/internal/components/delete.go
+++ bins/cli/internal/components/delete.go
@@ -2,18 +2,36 @@ package components
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 
 	"github.com/powertoolsdev/mono/bins/cli/internal/ui"
 )
 
-func (s *Service) Delete(ctx context.Context, compID string) {
-	view := ui.NewDeleteView("component", compID)
+func (s *Service) Delete(ctx context.Context, compID string, asJSON bool) {
+	if asJSON == true {
+		res, err := s.api.DeleteComponent(ctx, compID)
+		if err != nil {
+			fmt.Printf("failed to delete component: %s", err)
+			return
+		}
 
-	view.Start()
-	_, err := s.api.DeleteComponent(ctx, compID)
-	if err != nil {
-		view.Fail(err)
-		return
+		type response struct {
+			ID      string `json:"id"`
+			Deleted bool   `json:"deleted"`
+		}
+		r := response{ID: compID, Deleted: res}
+		j, _ := json.Marshal(r)
+		fmt.Println(string(j))
+	} else {
+		view := ui.NewDeleteView("component", compID)
+
+		view.Start()
+		_, err := s.api.DeleteComponent(ctx, compID)
+		if err != nil {
+			view.Fail(err)
+			return
+		}
+		view.Success()
 	}
-	view.Success()
 }
