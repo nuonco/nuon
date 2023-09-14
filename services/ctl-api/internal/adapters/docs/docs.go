418dfd4fe7734ebda9e053bdb3f3acd30a45cd59
Jon Morehouse <MorehouseJ09@gmail.com>
2023-09-13 19:03:02 -0600

    chore: move docs to public api (#2473)
    * chore: move docs to public api
    
    * update
    
    ---------
    
    Co-authored-by: nuonbot <128654611+nuonbot@users.noreply.github.com>

--- services/ctl-api/internal/adapters/docs/docs.go
+++ services/ctl-api/internal/adapters/docs/docs.go
@@ -3,22 +3,35 @@ package docs
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/powertoolsdev/mono/services/ctl-api/docs"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal"
 	swaggerfiles "github.com/swaggo/files"
 	swagger "github.com/swaggo/gin-swagger"
 )
 
-type Route struct{}
-
-func (r *Route) RegisterRoutes(g *gin.Engine) error {
-	return nil
+type Route struct {
+	cfg *internal.Config
 }
 
-func (r *Route) RegisterInternalRoutes(g *gin.Engine) error {
-	docs.SwaggerInfo.Host = "localhost:8081"
+func (r *Route) RegisterRoutes(g *gin.Engine) error {
+	switch r.cfg.Env {
+	case "developnent":
+		docs.SwaggerInfo.Host = "localhost:8081"
+	case "prod":
+		docs.SwaggerInfo.Host = "ctl.prod.nuon.co"
+	case "stage":
+		docs.SwaggerInfo.Host = "ctl.stage.nuon.co"
+	}
+
 	g.GET("/docs/*any", swagger.WrapHandler(swaggerfiles.Handler))
 	return nil
 }
 
-func New() *Route {
-	return &Route{}
+func (r *Route) RegisterInternalRoutes(g *gin.Engine) error {
+	return nil
+}
+
+func New(cfg *internal.Config) *Route {
+	return &Route{
+		cfg: cfg,
+	}
 }
