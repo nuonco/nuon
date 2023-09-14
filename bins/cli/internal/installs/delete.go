db2d12a8a1abe89fdd97df3ba79fee91b41f7087
Nat Hamilton <info@nathamilton.com>
2023-09-14 13:58:48 -0400

    feat: json flag cli delete cmds (#2483)
    * feat: installs delete cmd supports --json flag
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat: components delete cmd supports --json flag
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- bins/cli/internal/installs/delete.go
+++ bins/cli/internal/installs/delete.go
@@ -2,19 +2,35 @@ package installs
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 
 	"github.com/powertoolsdev/mono/bins/cli/internal/ui"
 )
 
-func (s *Service) Delete(ctx context.Context, id string) {
-	view := ui.NewDeleteView("install", id)
+func (s *Service) Delete(ctx context.Context, id string, asJSON bool) {
+	if asJSON == true {
+		res, err := s.api.DeleteInstall(ctx, id)
+		if err != nil {
+			fmt.Printf("failed to delete install: %s", err)
+			return
+		}
+		type response struct {
+			ID      string `json:"id"`
+			Deleted bool   `json:"deleted"`
+		}
+		r := response{ID: id, Deleted: res}
+		j, _ := json.Marshal(r)
+		fmt.Println(string(j))
+	} else {
+		view := ui.NewDeleteView("install", id)
 
-	view.Start()
-	_, err := s.api.DeleteInstall(ctx, id)
-	if err != nil {
-		view.Fail(err)
-		return
+		view.Start()
+		_, err := s.api.DeleteInstall(ctx, id)
+		if err != nil {
+			view.Fail(err)
+			return
+		}
+		view.Success()
 	}
-
-	view.Success()
 }
