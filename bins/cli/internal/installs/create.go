65c3ae49252ff4de85d00f2b69f98fbd7eedabe5
Nat Hamilton <info@nathamilton.com>
2023-09-14 10:12:37 -0400

    feat: create cmd has --json support (#2465)
    * feat: builds create cmd has --json support
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat: installs create cmd supports --json flag
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat: releases create cmd supports --json flag
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- bins/cli/internal/installs/create.go
+++ bins/cli/internal/installs/create.go
@@ -2,6 +2,7 @@ package installs
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"time"
 
@@ -14,37 +15,53 @@ const (
 	statusActive = "active"
 )
 
-func (s *Service) Create(ctx context.Context, appID, name, region, arn string) {
-	view, _ := pterm.DefaultSpinner.Start("creating install")
+func (s *Service) Create(ctx context.Context, appID, name, region, arn string, asJSON bool) {
+	if asJSON == true {
+		install, err := s.api.CreateInstall(ctx, appID, &models.ServiceCreateInstallRequest{
+			Name: &name,
+			AwsAccount: &models.ServiceCreateInstallRequestAwsAccount{
+				Region:     region,
+				IamRoleArn: &arn,
+			},
+		})
+		if err != nil {
+			fmt.Printf("failed to create install: %s", err)
+			return
+		}
+		j, _ := json.Marshal(install)
+		fmt.Println(string(j))
+	} else {
+		view, _ := pterm.DefaultSpinner.Start("creating install")
 
-	view.Start()
-	install, err := s.api.CreateInstall(ctx, appID, &models.ServiceCreateInstallRequest{
-		Name: &name,
-		AwsAccount: &models.ServiceCreateInstallRequestAwsAccount{
-			Region:     region,
-			IamRoleArn: &arn,
-		},
-	})
-	if err != nil {
-		view.Fail(err.Error() + "\n")
-		return
-	}
-
-	for {
-		ins, err := s.api.GetInstall(ctx, install.ID)
-		switch {
-		case err != nil:
+		view.Start()
+		install, err := s.api.CreateInstall(ctx, appID, &models.ServiceCreateInstallRequest{
+			Name: &name,
+			AwsAccount: &models.ServiceCreateInstallRequestAwsAccount{
+				Region:     region,
+				IamRoleArn: &arn,
+			},
+		})
+		if err != nil {
 			view.Fail(err.Error() + "\n")
-		case ins.Status == statusError:
-			view.Fail(fmt.Errorf("failed to create install: %s", ins.StatusDescription))
 			return
-		case ins.Status == statusActive:
-			view.Success(fmt.Sprintf("successfully created install %s", ins.ID))
-			return
-		default:
-			view.UpdateText(fmt.Sprintf("%s install", ins.Status))
 		}
 
-		time.Sleep(5 * time.Second)
+		for {
+			ins, err := s.api.GetInstall(ctx, install.ID)
+			switch {
+			case err != nil:
+				view.Fail(err.Error() + "\n")
+			case ins.Status == statusError:
+				view.Fail(fmt.Errorf("failed to create install: %s", ins.StatusDescription))
+				return
+			case ins.Status == statusActive:
+				view.Success(fmt.Sprintf("successfully created install %s", ins.ID))
+				return
+			default:
+				view.UpdateText(fmt.Sprintf("%s install", ins.Status))
+			}
+
+			time.Sleep(5 * time.Second)
+		}
 	}
 }
