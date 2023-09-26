2a8cdf20bcf9ad8a1dfc7d60c0e164f83ff8d2a1
Jordan Acosta <jordan@nuon.co>
2023-09-26 13:03:46 -0700

    feat: api endpoint for creating release with a build ID (#2629)
    Our current endpoint for creating a release is nested under
    `/components/:component_id`, we would like to allow users to create a
    release using just a build ID.
    
    This PR adds a new `POST /v1/releases` endpoint that only requires a
    build ID.
    
    I initially tried getting the component ID from the API in the CLI, but
    the build endpoints are also nested under `/component/:component_id`, so
    you need the component ID up front. There's no way, afaik, to fetch a
    comoponent using the ID of a build, config, or release.
    
    I also tried adding integration test, but those rely on nuon-go. Since
    that's no longer in the monorepo, I need to merge the API changes first,
    then publish the nuon-go changes that depend on it, then I can add the
    integration test.

---
+++ services/ctl-api/internal/app/releases/service/create_release.go
@@ -0,0 +1,69 @@
+package service
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
+)
+
+// @BasePath /v1/releases
+// Create a release from a build
+// @Summary create a release
+// @Schemes
+// @Description create a release for a build
+// @Tags releases
+// @Accept json
+// @Param req body CreateComponentReleaseRequest true "Input"
+// @Produce json
+// @Success 201 {object} app.ComponentRelease
+// @Router /v1/releases [post]
+func (s *service) CreateRelease(ctx *gin.Context) {
+	var req CreateComponentReleaseRequest
+	if err := ctx.BindJSON(&req); err != nil {
+		ctx.Error(fmt.Errorf("unable to parse request: %w", err))
+		return
+	}
+	if err := req.Validate(s.v); err != nil {
+		ctx.Error(fmt.Errorf("invalid request: %w", err))
+		return
+	}
+
+	cmp, err := s.getBuildComponent(ctx, req.BuildID)
+	if err != nil {
+		ctx.Error(fmt.Errorf("unable to create app: %w", err))
+		return
+	}
+
+	app, err := s.createRelease(ctx, cmp.ID, &req)
+	if err != nil {
+		ctx.Error(fmt.Errorf("unable to create app: %w", err))
+		return
+	}
+
+	s.hooks.Created(ctx, app.ID)
+	ctx.JSON(http.StatusCreated, app)
+}
+
+func (s *service) getBuildComponent(ctx context.Context, buildID string) (*app.Component, error) {
+	component := app.Component{}
+	res := s.db.WithContext(ctx).
+		Raw(`
+select cmp.*
+from components cmp
+join component_config_connections cfg on cmp.id = cfg.component_id
+join component_builds bld on cfg.id = bld.component_config_connection_id
+where bld.id = @buildID
+`, sql.Named("buildID", buildID)).
+		First(&component)
+	if res.Error != nil {
+		return nil, fmt.Errorf("unable to get component: %w", res.Error)
+	}
+
+	component.ConfigVersions = len(component.ComponentConfigs)
+
+	return &component, nil
+}
