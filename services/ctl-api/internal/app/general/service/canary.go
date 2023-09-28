d0a07ee5bd49f3ba0e268905e46afb485d4a332d
Jon Morehouse <MorehouseJ09@gmail.com>
2023-09-28 00:19:01 -0600

    chore: add endpoint to provision a canary (#2646)

---
+++ services/ctl-api/internal/app/general/service/canary.go
@@ -0,0 +1,50 @@
+package service
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/powertoolsdev/mono/pkg/shortid/domains"
+	canaryv1 "github.com/powertoolsdev/mono/pkg/types/workflows/canary/v1"
+	"github.com/powertoolsdev/mono/pkg/workflows"
+	tclient "go.temporal.io/sdk/client"
+)
+
+type ProvisionCanaryRequest struct{}
+
+// @BasePath /v1/general
+// Provision a canary
+// @Summary provision a canary
+// @Schemes
+// @Description provision a canary
+// @Param req body ProvisionCanaryRequest true "Input"
+// @Tags general/internal
+// @Accept json
+// @Produce json
+// @Success 201 {string} ok
+// @Router /v1/general/provision-canary [post]
+func (c *service) ProvisionCanary(ctx *gin.Context) {
+	req := &canaryv1.ProvisionRequest{
+		CanaryId:    domains.NewCanaryID(),
+		Deprovision: true,
+	}
+
+	opts := tclient.StartWorkflowOptions{
+		TaskQueue: workflows.DefaultTaskQueue,
+		Memo: map[string]interface{}{
+			"canary-id":  req.CanaryId,
+			"started-by": "ctl-api",
+		},
+	}
+
+	_, err := c.temporalClient.ExecuteWorkflowInNamespace(ctx, "canary", opts, "Provision", req)
+	if err != nil {
+		ctx.Error(fmt.Errorf("unable to provision canary: %w", err))
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, map[string]string{
+		"status": "ok",
+	})
+}
