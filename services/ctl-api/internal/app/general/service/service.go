fac423998973ae0a41380381dcbb05d5a2d29895
Jon Morehouse <MorehouseJ09@gmail.com>
2023-09-28 00:19:01 -0600

    chore: add endpoint to provision a canary (#2646)

--- services/ctl-api/internal/app/general/service/service.go
+++ services/ctl-api/internal/app/general/service/service.go
@@ -4,15 +4,17 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 	"github.com/powertoolsdev/mono/pkg/metrics"
+	temporal "github.com/powertoolsdev/mono/pkg/temporal/client"
 	"go.uber.org/zap"
 	"gorm.io/gorm"
 )
 
 type service struct {
-	v  *validator.Validate
-	l  *zap.Logger
-	db *gorm.DB
-	mw metrics.Writer
+	v              *validator.Validate
+	l              *zap.Logger
+	db             *gorm.DB
+	mw             metrics.Writer
+	temporalClient temporal.Client
 }
 
 func (s *service) RegisterRoutes(api *gin.Engine) error {
@@ -22,15 +24,16 @@ func (s *service) RegisterRoutes(api *gin.Engine) error {
 }
 
 func (s *service) RegisterInternalRoutes(api *gin.Engine) error {
-	api.GET("/v1/general/migrate-noteable", s.MigrateNoteable)
+	api.POST("/v1/general/provision-canary", s.ProvisionCanary)
 	return nil
 }
 
-func New(v *validator.Validate, db *gorm.DB, mw metrics.Writer, l *zap.Logger) *service {
+func New(v *validator.Validate, db *gorm.DB, mw metrics.Writer, l *zap.Logger, temporalClient temporal.Client) *service {
 	return &service{
-		l:  l,
-		v:  v,
-		mw: mw,
-		db: db,
+		l:              l,
+		v:              v,
+		mw:             mw,
+		db:             db,
+		temporalClient: temporalClient,
 	}
 }
