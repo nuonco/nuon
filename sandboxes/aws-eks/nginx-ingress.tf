5ae72443b01050a5e0626c70215bec15dc448aa0
Jon Morehouse <MorehouseJ09@gmail.com>
2023-10-03 11:57:59 -0600

    chore: sandbox upgrades, fixes and more (#2690)
    * chore: fix executors locally
    
    * chore: update various sandbox versions and more
    
    * chore: delete policy version permission

--- sandboxes/aws-eks/nginx-ingress.tf
+++ sandboxes/aws-eks/nginx-ingress.tf
@@ -1,20 +1,20 @@
 # NOTE(jm): creating this may lead to an SG getting deleted, which breaks deprovisioning. Trying without this.
-#resource "helm_release" "nginx-ingress-controller" {
-  #namespace        = "kube-system"
-  #create_namespace = true
+resource "helm_release" "nginx-ingress-controller" {
+  namespace        = "nginx-ingress"
+  create_namespace = true
 
-  #name       = "nginx-ingress-controller"
-  #repository = "https://helm.nginx.com/stable"
-  #chart      = "nginx-ingress"
-  #version    = "0.18.0"
+  name       = "nginx-ingress-controller"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+  version    = "4.8.0"
 
-  #set {
-    #name  = "rbac.create"
-    #value = "true"
-  #}
+  set {
+    name  = "rbac.create"
+    value = "true"
+  }
 
-  #depends_on = [
-    #helm_release.cert_manager,
-    #helm_release.alb-ingress-controller
-  #]
-#}
+  depends_on = [
+    helm_release.cert_manager,
+    helm_release.alb-ingress-controller
+  ]
+}
