73c1e594231e42c6a787c2c05de1d2ab61369201
Jon Morehouse <MorehouseJ09@gmail.com>
2023-10-04 13:19:05 -0600

    chore: ability to use demo org locally (#2701)
    chore: pass workspaces into demo org

--- infra/demo-org/versions.tf
+++ infra/demo-org/versions.tf
@@ -1,6 +1,14 @@
 terraform {
   required_version = ">= 1.3.7"
 
+  backend "remote" {
+    organization = "nuonco"
+
+    workspaces {
+      prefix = "demo-org-"
+    }
+  }
+
   required_providers {
     nuon = {
       source  = "nuonco/nuon"
