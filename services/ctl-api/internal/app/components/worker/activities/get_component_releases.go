df7d93ae06af3c2b817d8f88045a6cec51f329df
Jon Morehouse <MorehouseJ09@gmail.com>
2023-10-04 19:34:29 -0600

    chore: cleanup and fine tuning of event loops (#2710)

---
+++ services/ctl-api/internal/app/components/worker/activities/get_component_releases.go
@@ -0,0 +1,26 @@
+package activities
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
+)
+
+type GetComponentRequest struct {
+	ComponentID string `validate:"required"`
+}
+
+func (a *Activities) GetComponent(ctx context.Context, req GetComponentRequest) (*app.Component, error) {
+	cmp := app.Component{}
+	res := a.db.WithContext(ctx).
+		Preload("ComponentConfigs").
+		Preload("ComponentConfigs.ComponentBuilds").
+		Preload("ComponentConfigs.ComponentBuilds.ComponentReleases").
+		First(&cmp, "id = ?", req.ComponentID)
+	if res.Error != nil {
+		return nil, fmt.Errorf("unable to get component: %w", res.Error)
+	}
+
+	return &cmp, nil
+}
