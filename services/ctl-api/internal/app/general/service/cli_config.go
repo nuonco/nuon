5287adfc2ed17d131e67aac2c2cbc6258cd5226f
Jon Morehouse <MorehouseJ09@gmail.com>
2023-10-06 11:00:44 -0600

    chore: add endpoint for cli config (#2731)
    * chore: add endpoint for cli-config
    
    * chore: commit values

---
+++ services/ctl-api/internal/app/general/service/cli_config.go
@@ -0,0 +1,39 @@
+package service
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type CLIConfig struct {
+	AuthDomain   string `json:"auth_domain"`
+	AuthClientID string `json:"auth_client_id"`
+	AuthAudience string `json:"auth_audience"`
+}
+
+//	@BasePath	/v1/general
+
+// Get config for cli
+//
+//	@Summary	Get config for cli
+//	@Schemes
+//	@Description	get config for cli
+//	@Tags			general
+//	@Accept			json
+//	@Produce		json
+//	@Failure		400				{object}	stderr.ErrResponse
+//	@Failure		401				{object}	stderr.ErrResponse
+//	@Failure		403				{object}	stderr.ErrResponse
+//	@Failure		404				{object}	stderr.ErrResponse
+//	@Failure		500				{object}	stderr.ErrResponse
+//	@Success		200				{object}	CLIConfig
+//
+// @Router				/v1/general/cli-config [GET]
+func (s *service) GetCLIConfig(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, &CLIConfig{
+		AuthDomain:   s.cfg.Auth0IssuerURL,
+		AuthClientID: s.cfg.Auth0ClientID,
+		AuthAudience: s.cfg.Auth0Audience,
+	})
+}
