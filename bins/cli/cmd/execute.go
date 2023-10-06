f10ebb21ef8c4242b04fd8d8686c231c534f1ac4
Jon Morehouse <MorehouseJ09@gmail.com>
2023-10-05 21:27:14 -0600

    feat: refactor root command, support custom config + more (#2726)
    * chore: handle more edge cases in error handling
    
    * chore: cleanup config parsing

--- bins/cli/cmd/execute.go
+++ bins/cli/cmd/execute.go
@@ -2,64 +2,33 @@ package cmd
 
 import (
 	"context"
-	"log"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/go-playground/validator/v10"
-	"github.com/powertoolsdev/mono/bins/cli/internal/apps"
-	"github.com/powertoolsdev/mono/bins/cli/internal/builds"
-	"github.com/powertoolsdev/mono/bins/cli/internal/components"
-	"github.com/powertoolsdev/mono/bins/cli/internal/config"
-	"github.com/powertoolsdev/mono/bins/cli/internal/installs"
-	"github.com/powertoolsdev/mono/bins/cli/internal/orgs"
-	"github.com/powertoolsdev/mono/bins/cli/internal/releases"
-	"github.com/powertoolsdev/mono/bins/cli/internal/version"
-	"github.com/powertoolsdev/mono/pkg/ui"
-	"github.com/spf13/cobra"
 )
 
 // Execute is essentially the init method of the CLI. It initializes all the components and composes them together.
-// TODO(ja): one could argue this and config.go should be the only files in cmd, and the commands, now that they're
-// decoupled from this init code, could go in their own internal package. That's not the convention cobra promotes,
-// but it could make sense since we're using constructors for all the commands.
 func Execute() {
 	// Construct a validator for the API client and the UI logger.
-	vld := validator.New()
-
-	// Construct a config instance
-	cfg, err := config.NewConfig()
-	if err != nil {
-		log.Fatal(err)
+	v := validator.New()
+	c := &cli{
+		v:   v,
+		ctx: context.Background(),
 	}
 
-	// Construct an API client for the services to use.
-	api, err := newAPI(vld, cfg)
-	if err != nil {
-		log.Fatal(err)
-	}
+	// Kill CLI immediately when user types Ctrl-C.
+	// Including SIGTERM to ensure consistent behavior.
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-sigChan
+		os.Exit(1)
+	}()
 
-	// Construct the domain services, and pass them to the CLI commands that use them.
-	cmds := []*cobra.Command{
-		newAppsCmd(cfg.BindCobraFlags, apps.New(api)),
-		newComponentsCmd(cfg.BindCobraFlags, components.New(api)),
-		newInstallsCmd(cfg.BindCobraFlags, installs.New(api)),
-		newOrgsCmd(cfg.BindCobraFlags, orgs.New(api)),
-		newVersionCmd(cfg.BindCobraFlags, version.New()),
-		newBuildsCmd(cfg.BindCobraFlags, builds.New(api)),
-		newReleasesCmd(cfg.BindCobraFlags, releases.New(api)),
-	}
-
-	// Create a context to pass down the code path.
-	ctx := context.Background()
-	uiLog, err := ui.New(vld)
-	if err != nil {
-		log.Fatalf("unable to initialize ui: %s", err)
-	}
-	ctx = ui.WithContext(ctx, uiLog)
-
-	// Construct and init the root command.
-	rootCmd := newRootCmd(cfg.BindCobraFlags, cmds...)
-	if err := rootCmd.ExecuteContext(ctx); err != nil {
+	rootCmd := c.rootCmd()
+	if err := rootCmd.ExecuteContext(c.ctx); err != nil {
 		os.Exit(2)
 	}
 }
