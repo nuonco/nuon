25d529e33864c832a2c053887550cbb1005ded25
Jon Morehouse <MorehouseJ09@gmail.com>
2023-10-05 21:27:14 -0600

    feat: refactor root command, support custom config + more (#2726)
    * chore: handle more edge cases in error handling
    
    * chore: cleanup config parsing

--- bins/cli/cmd/root.go
+++ bins/cli/cmd/root.go
@@ -1,42 +1,38 @@
 package cmd
 
 import (
-	"os"
-	"os/signal"
-	"syscall"
-
-	"github.com/powertoolsdev/mono/bins/cli/internal/config"
 	"github.com/spf13/cobra"
 )
 
-var PrintJSON bool = false
+var (
+	PrintJSON  bool = false
+	ConfigFile string
+)
 
-// newRootCmd constructs a new root cobra command, which all other commands will be nested under. If there are any flags or other settings that we want to be "global", they should be configured on this command.
-func newRootCmd(bindConfig config.BindCobraFunc, cmds ...*cobra.Command) *cobra.Command {
+// newRootCmd constructs a new root cobra command, which all other commands will be nested under. If there are any flags
+// or other settings that we want to be "global", they should be configured on this command.
+func (c *cli) rootCmd() *cobra.Command {
 	rootCmd := &cobra.Command{
-		Use:          "nuon",
-		SilenceUsage: true,
-		// PersistentPreRunE is only inherited by immediate child commands.
-		// We have to copy/paste this on each subcommand, so that it's children will inherit it.
-		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
-			return bindConfig(cmd)
-		},
+		Use:               "nuon",
+		SilenceUsage:      true,
+		PersistentPreRunE: c.persistentPreRunE,
 	}
 
 	rootCmd.PersistentFlags().BoolVarP(&PrintJSON, "json", "j", false, "print output as json")
+	rootCmd.PersistentFlags().StringVarP(&ConfigFile, "config", "f", "~/.nuon", "path to custom config file (defaults to $HOME/.nuon if exists)")
 
+	cmds := []*cobra.Command{
+		c.appsCmd(),
+		c.buildsCmd(),
+		c.componentsCmd(),
+		c.installsCmd(),
+		c.releasesCmd(),
+		c.buildsCmd(),
+		c.versionCmd(),
+	}
 	for _, cmd := range cmds {
 		rootCmd.AddCommand(cmd)
 	}
-
-	// Kill CLI immediately when user types Ctrl-C.
-	// Including SIGTERM to ensure consistent behavior.
-	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
-	go func() {
-		<-sigChan
-		os.Exit(1)
-	}()
 
 	return rootCmd
 }
