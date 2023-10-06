2b7d27a26ec9d2d0d55f0300e941c85d3f578f4b
Jon Morehouse <MorehouseJ09@gmail.com>
2023-10-05 21:27:14 -0600

    feat: refactor root command, support custom config + more (#2726)
    * chore: handle more edge cases in error handling
    
    * chore: cleanup config parsing

--- bins/cli/cmd/apps.go
+++ bins/cli/cmd/apps.go
@@ -2,26 +2,23 @@ package cmd
 
 import (
 	"github.com/powertoolsdev/mono/bins/cli/internal/apps"
-	"github.com/powertoolsdev/mono/bins/cli/internal/config"
 	"github.com/spf13/cobra"
 )
 
-func newAppsCmd(bindConfig config.BindCobraFunc, appsService *apps.Service) *cobra.Command {
-
+func (c *cli) appsCmd() *cobra.Command {
 	appsCmd := &cobra.Command{
-		Use:   "apps",
-		Short: "View the apps in your org",
-		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
-			return bindConfig(cmd)
-		},
+		Use:               "apps",
+		Short:             "View the apps in your org",
+		PersistentPreRunE: c.persistentPreRunE,
 	}
 
 	listCmd := &cobra.Command{
 		Use:     "list",
 		Aliases: []string{"ls"},
 		Short:   "List all your apps",
-		Run: func(cmd *cobra.Command, args []string) {
-			appsService.List(cmd.Context(), PrintJSON)
+		Run: func(cmd *cobra.Command, _ []string) {
+			svc := apps.New(c.apiClient)
+			svc.List(cmd.Context(), PrintJSON)
 		},
 	}
 
@@ -31,8 +28,9 @@ func newAppsCmd(bindConfig config.BindCobraFunc, appsService *apps.Service) *cob
 	getCmd := &cobra.Command{
 		Use:   "get",
 		Short: "Get the current app",
-		Run: func(cmd *cobra.Command, args []string) {
-			appsService.Get(cmd.Context(), appID, PrintJSON)
+		Run: func(cmd *cobra.Command, _ []string) {
+			svc := apps.New(c.apiClient)
+			svc.Get(cmd.Context(), appID, PrintJSON)
 		},
 	}
 	// TODO: Update API to support getting app by name and add a flag for that.
