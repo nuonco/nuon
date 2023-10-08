652f155b9cf9873d7872920115a5cbae9657e856
Jon Morehouse <MorehouseJ09@gmail.com>
2023-10-07 22:09:25 -0600

    feat: get wild card certs working (#2738)
    * chore: update custom cert
    
    * chore: update wild cards

--- services/ctl-api/internal/app/orgs/worker/reprovision.go
+++ services/ctl-api/internal/app/orgs/worker/reprovision.go
@@ -4,16 +4,27 @@ import (
 	"fmt"
 
 	orgsv1 "github.com/powertoolsdev/mono/pkg/types/workflows/orgs/v1"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app/orgs/worker/activities"
 	"go.temporal.io/sdk/workflow"
 )
 
 func (w *Workflows) reprovision(ctx workflow.Context, orgID string, dryRun bool) error {
 	w.updateStatus(ctx, orgID, StatusProvisioning, "reprovisioning organization resources")
 
+	var org app.Org
+	if err := w.defaultExecGetActivity(ctx, w.acts.Get, activities.GetRequest{
+		OrgID: orgID,
+	}, &org); err != nil {
+		w.updateStatus(ctx, orgID, StatusError, "unable to get org from database")
+		return fmt.Errorf("unable to get install: %w", err)
+	}
+
 	_, err := w.execProvisionWorkflow(ctx, dryRun, &orgsv1.ProvisionRequest{
 		OrgId:       orgID,
 		Region:      defaultOrgRegion,
 		Reprovision: true,
+		CustomCert:  org.CustomCert,
 	})
 	if err != nil {
 		w.updateStatus(ctx, orgID, StatusError, "unable to reprovision organization resources")
