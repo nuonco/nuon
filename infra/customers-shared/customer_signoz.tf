3581ede9163a519041317d5a8a2fd4fd54787c4e
Jon Morehouse <MorehouseJ09@gmail.com>
2023-10-16 15:46:15 -0600

    chore: fix admin app (#2810)
    * fix: spec does not match endpoint
    
    * chore: create install for testing signoz

--- infra/customers-shared/customer_signoz.tf
+++ infra/customers-shared/customer_signoz.tf
@@ -14,3 +14,11 @@ resource "nuon_helm_chart_component" "signoz" {
     value = "nlb.{{ .nuon.install.public_domain }}"
   }
 }
+
+resource "nuon_install" "signoz_install" {
+  app_id = nuon_app.real["signoz"].id
+
+  name         = "signoz-demo"
+  region       = "us-east-1"
+  iam_role_arn = "arn:aws:iam::949309607565:role/nuon-demo-install-access"
+}
