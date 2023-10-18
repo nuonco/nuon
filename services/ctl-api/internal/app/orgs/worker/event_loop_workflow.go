6d7a6decc4396e5acc3a72e755e525d0ac11da49
Jon Morehouse <MorehouseJ09@gmail.com>
2023-10-18 17:44:03 -0600

    chore: experiment to simulate policies (#2847)
    * chore: prevent disabling event loops
    
    * fix: publish iam policies on promote
    
    * feat: experiment to test policies

--- services/ctl-api/internal/app/orgs/worker/event_loop_workflow.go
+++ services/ctl-api/internal/app/orgs/worker/event_loop_workflow.go
@@ -56,6 +56,7 @@ func (w *Workflows) OrgEventLoop(ctx workflow.Context, req OrgEventLoopRequest) 
 		case OperationDelete:
 			if err := w.delete(ctx, req.OrgID, req.SandboxMode); err != nil {
 				l.Info("unable to delete org: %w", zap.Error(err))
+				return
 			}
 			finished = true
 		}
