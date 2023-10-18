5273bb4a628dfdf1bc0a1dd9ace3201ee12785c4
Jon Morehouse <MorehouseJ09@gmail.com>
2023-10-17 18:29:33 -0600

    fix: releases only release for active installs (#2830)
    Co-authored-by: nuonbot <128654611+nuonbot@users.noreply.github.com>

--- services/ctl-api/internal/app/releases/service/create_component_release.go
+++ services/ctl-api/internal/app/releases/service/create_component_release.go
@@ -113,7 +113,7 @@ func (s *service) createRelease(ctx context.Context, cmpID string, req *CreateCo
 	cmp := app.Component{}
 	res := s.db.WithContext(ctx).
 		Preload("App").
-		Preload("App.Installs").
+		Preload("App.Installs", "status = ?", "active").
 		First(&cmp, "id = ?", cmpID)
 	if res.Error != nil {
 		return nil, fmt.Errorf("unable to get component: %w", res.Error)
