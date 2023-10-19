cc5bcb2826663020e07de385104ae60be2d2adce
Nat Hamilton <info@nathamilton.com>
2023-10-19 11:20:28 -0400

    feat: cli now displays the latest component config (#2850)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

---
+++ bins/cli/internal/components/latest_config.go
@@ -0,0 +1,31 @@
+package components
+
+import (
+	"context"
+
+	"github.com/powertoolsdev/mono/bins/cli/internal/lookup"
+	"github.com/powertoolsdev/mono/bins/cli/internal/ui"
+)
+
+func (s *Service) LatestConfig(ctx context.Context, compID string, asJSON bool) {
+	compID, err := lookup.ComponentID(ctx, s.api, compID)
+	if err != nil {
+		ui.PrintError(err)
+		return
+	}
+
+	view := ui.NewGetView()
+
+	config, err := s.api.GetComponentLatestConfig(ctx, compID)
+	if err != nil {
+		view.Error(err)
+		return
+	}
+
+	if asJSON {
+		ui.PrintJSON(config)
+		return
+	}
+
+	ui.PrintJSON(config)
+}
