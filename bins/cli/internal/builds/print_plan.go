106b1e39f3b2e8767994e6dcc96b5f87476cf455
Nat Hamilton <info@nathamilton.com>
2023-10-20 22:49:45 -0400

    feat: cli now prints build plans (#2869)
    * feat: cli now prints build plans
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * chore: update go version
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    Co-authored-by: nuonbot <128654611+nuonbot@users.noreply.github.com>
    Co-authored-by: Jon Morehouse <morehousej09@gmail.com>

---
+++ bins/cli/internal/builds/print_plan.go
@@ -0,0 +1,31 @@
+package builds
+
+import (
+	"context"
+
+	"github.com/powertoolsdev/mono/bins/cli/internal/lookup"
+	"github.com/powertoolsdev/mono/bins/cli/internal/ui"
+)
+
+func (s *Service) PrintPlan(ctx context.Context, compID, buildID string, asJSON bool) {
+	compID, err := lookup.ComponentID(ctx, s.api, compID)
+	if err != nil {
+		ui.PrintError(err)
+		return
+	}
+
+	view := ui.NewGetView()
+
+	build, err := s.api.GetComponentBuildPlan(ctx, compID, buildID)
+	if err != nil {
+		view.Error(err)
+		return
+	}
+
+	if asJSON {
+		ui.PrintJSON(build)
+		return
+	}
+
+	ui.PrintJSON(build)
+}
