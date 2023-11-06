58ee210c94e12425e2eed0e3bb5ab0d5e2ce2d50
Nat Hamilton <info@nathamilton.com>
2023-11-06 16:29:28 -0500

    feat: adds cli installs list-deploys cmd (#2953)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

---
+++ bins/cli/internal/installs/list_deploys.go
@@ -0,0 +1,47 @@
+package installs
+
+import (
+	"context"
+
+	"github.com/powertoolsdev/mono/bins/cli/internal/lookup"
+	"github.com/powertoolsdev/mono/bins/cli/internal/ui"
+)
+
+func (s *Service) ListDeploys(ctx context.Context, installID string, asJSON bool) {
+	installID, err := lookup.InstallID(ctx, s.api, installID)
+	if err != nil {
+		ui.PrintError(err)
+		return
+	}
+
+	view := ui.NewGetView()
+
+	deploys, err := s.api.GetInstallDeploys(ctx, installID)
+	if err != nil {
+		view.Error(err)
+		return
+	}
+
+	if asJSON {
+		ui.PrintJSON(deploys)
+		return
+	}
+
+	data := [][]string{
+		{
+			"id",
+			"status",
+			"build id",
+			"created at",
+		},
+	}
+	for _, deploy := range deploys {
+		data = append(data, []string{
+			deploy.ID,
+			deploy.Status,
+			deploy.BuildID,
+			deploy.CreatedAt,
+		})
+	}
+	view.Render(data)
+}
