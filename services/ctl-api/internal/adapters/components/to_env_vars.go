2787b9d2e2cfd8373a425d5bd44bc4bc422eecfa
Jordan Acosta <jordan@nuon.co>
2023-11-07 23:18:37 -0800

    feat: job components have env vars (#2975)

---
+++ services/ctl-api/internal/adapters/components/to_env_vars.go
@@ -0,0 +1,22 @@
+package components
+
+import variablesv1 "github.com/powertoolsdev/mono/pkg/types/components/variables/v1"
+
+func (c *Adapter) toEnvVars(inputVals map[string]*string) *variablesv1.EnvVars {
+	vals := make([]*variablesv1.EnvVar, 0)
+	for k, v := range inputVals {
+		if v == nil {
+			continue
+		}
+
+		vals = append(vals, &variablesv1.EnvVar{
+			Name:      k,
+			Value:     *v,
+			Sensitive: true,
+		})
+	}
+
+	return &variablesv1.EnvVars{
+		Env: vals,
+	}
+}
