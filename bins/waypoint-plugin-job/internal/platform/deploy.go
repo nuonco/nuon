ec8d2661c2c2babe4f8522644957bd689e0d0e0b
Jordan Acosta <jordan@nuon.co>
2023-11-15 07:53:27 -0800

    feat: stream job logs (#3043)
    * feat: init and use logger
    
    * feat: stream job logs

--- bins/waypoint-plugin-job/internal/platform/deploy.go
+++ bins/waypoint-plugin-job/internal/platform/deploy.go
@@ -3,7 +3,6 @@ package platform
 import (
 	"context"
 
-	"github.com/hashicorp/waypoint-plugin-sdk/component"
 	"github.com/hashicorp/waypoint-plugin-sdk/terminal"
 	jobv1 "github.com/powertoolsdev/mono/pkg/types/plugins/job/v1"
 )
@@ -14,33 +13,42 @@ func (p *Platform) DeployFunc() interface{} {
 
 func (p *Platform) deploy(
 	ctx context.Context,
-	ji *component.JobInfo,
 	ui terminal.UI,
 ) (*jobv1.Deployment, error) {
 
-	// TODO: uncomment this when the logging bug is fixed.
-	// Will use ui.Output for now.
-	// stdout, _, err := ui.OutputWriters()
-	// if err != nil {
-	// 	return nil, fmt.Errorf("unable to get output writers")
-	// }
-	// logger := hclog.New(&hclog.LoggerOptions{
-	// 	Name:   "job",
-	// 	Output: stdout,
-	// })
-	// logger.Error("LOGGER TEST")
+	// init logger
+	err := p.initLogger(ui)
+	if err != nil {
+		return nil, err
+	}
+	p.logger.Debug("initialized logger")
 
 	// init k8s client
+	p.logger.Debug("getting k8s client set")
 	clientset, err := p.getClientset()
 	if err != nil {
+		p.logger.Error(err.Error())
 		return nil, err
 	}
+	p.logger.Debug("got k8s client set")
 
 	// start k8s job
-	_, err = p.startJob(ctx, clientset, ji)
+	p.logger.Debug("starting k8s job")
+	job, err := p.startJob(ctx, clientset)
 	if err != nil {
+		p.logger.Error(err.Error())
 		return nil, err
 	}
+	p.logger.Debug("started k8s job")
+
+	// monitor job
+	p.logger.Debug("monitoring k8s job")
+	err = p.monitorJob(ctx, clientset, job)
+	if err != nil {
+		p.logger.Error(err.Error())
+		return nil, err
+	}
+	p.logger.Debug("done monitoring k8s job")
 
 	return &jobv1.Deployment{}, nil
 }
