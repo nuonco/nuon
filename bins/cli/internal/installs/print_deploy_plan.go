7e047eac111b98f9e77349cd4f437b67e005ef8d
Nat Hamilton <info@nathamilton.com>
2023-11-17 08:00:00 -0500

    feat: cli deploy plan flags (#3066)
    * fix: cli log view decoding ignore log order
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat: cli print-deploy-plan optional flags
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * refactor: cli print-deploy-plan --print-job-config prints only job conf string
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * refactor: cli print-deploy-plan --rendered-vars displays helm values
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- bins/cli/internal/installs/print_deploy_plan.go
+++ bins/cli/internal/installs/print_deploy_plan.go
@@ -2,12 +2,37 @@ package installs
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/mitchellh/mapstructure"
 	"github.com/powertoolsdev/mono/bins/cli/internal/lookup"
 	"github.com/powertoolsdev/mono/bins/cli/internal/ui"
 )
 
-func (s *Service) PrintDeployPlan(ctx context.Context, installID string, deployID string, asJSON bool) {
+type DeployPlanActual struct {
+	Waypoint_Plan struct {
+		Variables struct {
+			Intermediate_Data interface{}
+			Variables         []struct {
+				Actual *struct {
+					TerraformVariable *struct {
+						Name  string
+						Value string
+					}
+					HelmValue *struct {
+						Name  string
+						Value string
+					}
+				}
+			}
+		}
+		Waypoint_Job struct {
+			Hcl_Config string
+		}
+	}
+}
+
+func (s *Service) PrintDeployPlan(ctx context.Context, installID, deployID string, asJSON, renderedVars, intermediateOnly, jobConfig bool) {
 	installID, err := lookup.InstallID(ctx, s.api, installID)
 	if err != nil {
 		ui.PrintError(err)
@@ -22,9 +47,62 @@ func (s *Service) PrintDeployPlan(ctx context.Context, installID string, deployI
 		return
 	}
 
-	if asJSON {
-		ui.PrintJSON(plan)
-		return
+	if renderedVars || intermediateOnly || jobConfig {
+		var p DeployPlanActual
+		err = mapstructure.Decode(plan.Actual, &p)
+		if err != nil {
+			ui.PrintError(err)
+			return
+		}
+
+		if renderedVars {
+			if asJSON {
+				ui.PrintJSON(p.Waypoint_Plan.Variables.Variables)
+				return
+			}
+
+			data := [][]string{{
+				"type",
+				"name",
+				"value",
+			}}
+
+			for _, v := range p.Waypoint_Plan.Variables.Variables {
+				if v.Actual.TerraformVariable != nil {
+					data = append(data, []string{
+						"Terraform",
+						v.Actual.TerraformVariable.Name,
+						v.Actual.TerraformVariable.Value,
+					})
+				}
+
+				if v.Actual.HelmValue != nil {
+					data = append(data, []string{
+						"Helm",
+						v.Actual.HelmValue.Name,
+						v.Actual.HelmValue.Value,
+					})
+				}
+			}
+
+			view.Render(data)
+			return
+		}
+
+		if intermediateOnly {
+			ui.PrintJSON(p.Waypoint_Plan.Variables.Intermediate_Data)
+			return
+		}
+
+		if jobConfig {
+			if asJSON {
+				ui.PrintJSON(p.Waypoint_Plan.Waypoint_Job)
+				return
+			}
+
+			fmt.Printf("%s", p.Waypoint_Plan.Waypoint_Job.Hcl_Config)
+			return
+		}
 	}
 
 	ui.PrintJSON(plan)
