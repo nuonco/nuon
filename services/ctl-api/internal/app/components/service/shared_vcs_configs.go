4f4afcbb29bfa6306de59d41204a0e34b6fe8410
Jon Morehouse <MorehouseJ09@gmail.com>
2023-11-17 14:04:02 -0600

    chore: fix multiple vcs connections + dupes (#3078)
    * fix: support multiple VCS connections + dupes
    
    * chore: fix migration

--- services/ctl-api/internal/app/components/service/shared_vcs_configs.go
+++ services/ctl-api/internal/app/components/service/shared_vcs_configs.go
@@ -1,10 +1,14 @@
 package service
 
 import (
+	"context"
 	"fmt"
+	"strconv"
 	"strings"
 
+	"github.com/google/go-github/v50/github"
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
+	"golang.org/x/oauth2"
 )
 
 type PublicGitVCSConfigRequest struct {
@@ -26,7 +30,45 @@ type basicVCSConfigRequest struct {
 	ConnectedGithubVCSConfig *ConnectedGithubVCSConfigRequest `json:"connected_github_vcs_config" `
 }
 
-func (b *basicVCSConfigRequest) connectedGithubVCSConfig(parentCmp *app.Component) (*app.ConnectedGithubVCSConfig, error) {
+func (b *basicVCSConfigRequest) lookupVCSConnection(ctx context.Context,
+	ghClient *github.Client,
+	owner, name string,
+	vcsConnections []app.VCSConnection) (string, error) {
+	if len(vcsConnections) < 1 {
+		return "", fmt.Errorf("no vcs connections on org")
+	}
+
+	for _, vcsConn := range vcsConnections {
+		installID, err := strconv.ParseInt(vcsConn.GithubInstallID, 10, 64)
+		if err != nil {
+			return "", fmt.Errorf("unable to get install ID: %w", err)
+		}
+
+		resp, _, err := ghClient.Apps.CreateInstallationToken(ctx, installID, &github.InstallationTokenOptions{})
+		if err != nil {
+			return "", fmt.Errorf("unable to get installation token: %w", err)
+		}
+
+		// get a client with the github install token
+		ts := oauth2.StaticTokenSource(
+			&oauth2.Token{AccessToken: *resp.Token},
+		)
+		tc := oauth2.NewClient(ctx, ts)
+		client := github.NewClient(tc)
+
+		_, _, err = client.Repositories.Get(ctx, owner, name)
+		// TODO(jm): better parsing here
+		if err != nil {
+			continue
+		}
+
+		return vcsConn.ID, nil
+	}
+
+	return "", fmt.Errorf("no vcs connection found with access to %s/%s", owner, name)
+}
+
+func (b *basicVCSConfigRequest) connectedGithubVCSConfig(ctx context.Context, parentCmp *app.Component, ghClient *github.Client) (*app.ConnectedGithubVCSConfig, error) {
 	if b.ConnectedGithubVCSConfig == nil {
 		return nil, nil
 	}
@@ -36,8 +78,9 @@ func (b *basicVCSConfigRequest) connectedGithubVCSConfig(parentCmp *app.Componen
 		return nil, fmt.Errorf("invalid repo, must be of the format <user-name>/<repo-name>")
 	}
 
-	if len(parentCmp.App.Org.VCSConnections) < 1 {
-		return nil, fmt.Errorf("org must have at least one vcs connection")
+	vcsConnID, err := b.lookupVCSConnection(ctx, ghClient, pieces[0], pieces[1], parentCmp.App.Org.VCSConnections)
+	if err != nil {
+		return nil, err
 	}
 
 	return &app.ConnectedGithubVCSConfig{
@@ -46,7 +89,7 @@ func (b *basicVCSConfigRequest) connectedGithubVCSConfig(parentCmp *app.Componen
 		RepoOwner:       pieces[0],
 		Directory:       b.ConnectedGithubVCSConfig.Directory,
 		Branch:          b.ConnectedGithubVCSConfig.Branch,
-		VCSConnectionID: parentCmp.App.Org.VCSConnections[0].ID,
+		VCSConnectionID: vcsConnID,
 	}, nil
 }
 
