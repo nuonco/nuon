255f94a154a68ca6b16a6b1545c184dc3cd27c49
Jon Morehouse <MorehouseJ09@gmail.com>
2023-11-17 14:48:13 -0600

    chore: push actual migration (#3080)
    chore: vcs configs

--- services/ctl-api/migrations/drop-vcs-configs.sql
+++ services/ctl-api/migrations/drop-vcs-configs.sql
@@ -1,17 +1,7 @@
-ALTER TABLE apps ENABLE ROW LEVEL SECURITY;
-CREATE POLICY apps_isolation_policy ON apps FOR select
-USING (org_id = current_setting('app.org_id') OR org_id = 'admin');
-
-ALTER TABLE components ENABLE ROW LEVEL SECURITY;
-CREATE POLICY components_isolation_policy ON components
-USING (org_id = current_setting('app.org_id'));
-
-ALTER TABLE installs ENABLE ROW LEVEL SECURITY;
-CREATE POLICY installs_isolation_policy ON installs
-USING (org_id = current_setting('app.org_id'));
-
-CREATE OR REPLACE FUNCTION set_org(org_id text) RETURNS void AS $$
-BEGIN
-    PERFORM set_config('app.org_id', org_id, false);
-END;
-$$ LANGUAGE plpgsql;
+DELETE FROM vcs_connections
+WHERE id NOT IN
+(
+SELECT MIN(id)
+FROM vcs_connections
+GROUP BY org_id, github_install_id
+)
