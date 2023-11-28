f2dd7871663143d424bec09f440b8d27b4d356c0
Nat Hamilton <info@nathamilton.com>
2023-11-28 14:46:18 -0500

    feat(cli): installs sandbox-runs (#3183)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

---
+++ bins/cli/internal/installs/sandbox_runs.go
@@ -0,0 +1,69 @@
+package installs
+
+import (
+	"context"
+	"time"
+
+	"github.com/powertoolsdev/mono/bins/cli/internal/lookup"
+	"github.com/powertoolsdev/mono/bins/cli/internal/ui"
+)
+
+func (s *Service) SandboxRuns(ctx context.Context, installID string, asJSON bool) {
+	installID, err := lookup.InstallID(ctx, s.api, installID)
+	if err != nil {
+		ui.PrintError(err)
+		return
+	}
+
+	view := ui.NewGetView()
+
+	runs, err := s.api.GetInstallSandboxRuns(ctx, installID)
+	if err != nil {
+		view.Error(err)
+		return
+	}
+
+	if asJSON {
+		ui.PrintJSON(runs)
+		return
+	}
+
+	data := [][]string{
+		{
+			"id",
+			"run type",
+			"status",
+			"app sandbox",
+			"sandbox config type",
+			"sandbox repo",
+			"updated at",
+		},
+	}
+	for _, run := range runs {
+		var cfgType string
+		var repo string
+
+		if run.AppSandboxConfig.PublicGitVcsConfig != nil {
+			cfgType = "public git"
+			repo = run.AppSandboxConfig.PublicGitVcsConfig.Repo
+		}
+
+		if run.AppSandboxConfig.ConnectedGithubVcsConfig != nil {
+			cfgType = "conntected github"
+			repo = run.AppSandboxConfig.ConnectedGithubVcsConfig.Repo
+		}
+
+		updatedAt, _ := time.Parse(time.RFC3339Nano, run.UpdatedAt)
+
+		data = append(data, []string{
+			run.ID,
+			string(run.RunType),
+			run.StatusDescription,
+			run.AppSandboxConfig.AppSandbox,
+			cfgType,
+			repo,
+			updatedAt.Format(time.Stamp),
+		})
+	}
+	view.Render(data)
+}
