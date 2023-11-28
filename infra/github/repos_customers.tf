7dfb416e8ec43a0a2700844592f27eba6f6dfa6a
Jon Morehouse <MorehouseJ09@gmail.com>
2023-11-27 19:31:25 -0600

    chore: add github repos for customers (#3170)
    Co-authored-by: nuonbot <128654611+nuonbot@users.noreply.github.com>

--- infra/github/repos_customers.tf
+++ infra/github/repos_customers.tf
@@ -1,23 +1,35 @@
-#module "customer-meroxa" {
-  #source = "./modules/repository"
+module "customer-meroxa" {
+  source = "./modules/repository"
 
-  #name            = "customer-meroxa"
-  #description     = "Nuon configuration + demo for Meroxa."
-  #required_checks = []
-#}
+  name            = "customer-meroxa"
+  description     = "Nuon configuration + demo for Meroxa."
+  required_checks = []
 
-#module "customer-weaviate" {
-  #source = "./modules/repository"
+  providers = {
+    github = github.nuon
+  }
+}
 
-  #name            = "customer-weaviate"
-  #description     = "Nuon configuration + demo for Weaviate."
-  #required_checks = []
-#}
+module "customer-weaviate" {
+  source = "./modules/repository"
 
-#module "customer-flipt" {
-  #source = "./modules/repository"
+  name            = "customer-weaviate"
+  description     = "Nuon configuration + demo for Weaviate."
+  required_checks = []
 
-  #name            = "customer-flipt"
-  #description     = "Nuon configuration + demo for Flipt."
-  #required_checks = []
-#}
+  providers = {
+    github = github.nuon
+  }
+}
+
+module "customer-flipt" {
+  source = "./modules/repository"
+
+  name            = "customer-flipt"
+  description     = "Nuon configuration + demo for Flipt."
+  required_checks = []
+
+  providers = {
+    github = github.nuon
+  }
+}
