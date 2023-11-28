7ebe33aa58405a26cbc6f4532c079b7b60fd0dba
Jon Morehouse <MorehouseJ09@gmail.com>
2023-11-27 21:44:21 -0600

    feat: add `orgs id` command (#3171)

---
+++ bins/cli/internal/orgs/id.go
@@ -0,0 +1,20 @@
+package orgs
+
+import (
+	"context"
+
+	"github.com/powertoolsdev/mono/bins/cli/internal/ui"
+)
+
+func (s *Service) ID(ctx context.Context, asJSON bool) {
+	view := ui.NewGetView()
+
+	if asJSON {
+		ui.PrintJSON(s.cfg.OrgID)
+		return
+	}
+
+	view.Render([][]string{
+		{"api-token", s.cfg.OrgID},
+	})
+}
