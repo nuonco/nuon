1a621128b58390c8bd651419c916cd7506f832b7
Jon Morehouse <MorehouseJ09@gmail.com>
2023-11-28 18:05:52 -0600

    fix: admin delete + seed (#3190)
    * fix: admin delete
    
    * fix: admin delete + seed

--- services/ctl-api/internal/app/apps/service/admin_delete.go
+++ services/ctl-api/internal/app/apps/service/admin_delete.go
@@ -1,9 +1,12 @@
 package service
 
 import (
+	"context"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
 )
 
 type AdminDeleteAppRequest struct{}
@@ -25,6 +28,35 @@ type AdminDeleteAppRequest struct{}
 //	@Router			/v1/apps/{app_id}/admin-delete [POST]
 func (s *service) AdminDeleteApp(ctx *gin.Context) {
 	appID := ctx.Param("app_id")
+
+	currentApp, err := s.getAppAndDependencies(ctx, appID)
+	if err != nil {
+		ctx.Error(fmt.Errorf("unable to get app: %w", err))
+		return
+	}
+
+	for _, install := range currentApp.Installs {
+		s.installHooks.Deleted(ctx, install.ID)
+		s.installHooks.Forgotten(ctx, install.ID)
+	}
+
+	for _, comp := range currentApp.Components {
+		s.componentHooks.Deleted(ctx, comp.ID)
+	}
 	s.hooks.Deleted(ctx, appID)
+
 	ctx.JSON(http.StatusOK, true)
+}
+
+func (s *service) getAppAndDependencies(ctx context.Context, appID string) (*app.App, error) {
+	currentApp := app.App{}
+	res := s.db.WithContext(ctx).
+		Preload("Installs").
+		Preload("Components").
+		First(&currentApp, "id = ?", appID)
+	if res.Error != nil {
+		return nil, fmt.Errorf("unable to get app %s: %w", appID, res.Error)
+	}
+
+	return &currentApp, nil
 }
