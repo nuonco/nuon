8dea880931223408e0c309108c9ce2ddbc113986
Jordan Acosta <jordan@nuon.co>
2023-11-28 20:05:32 -0800

    feat: test updated sandbox with e2e service (#3199)

--- services/e2e/nuon/app_sandbox.tf
+++ services/e2e/nuon/app_sandbox.tf
@@ -3,17 +3,22 @@ data "nuon_builtin_sandbox" "main" {
 }
 
 resource "nuon_app_sandbox" "main" {
-  app_id = nuon_app.main.id
+  app_id            = nuon_app.main.id
   terraform_version = "v1.6.3"
 
   public_repo = {
-    repo = "nuonco/sandboxes"
-    branch = "main"
+    repo      = "nuonco/sandboxes"
+    branch    = "ja/3024-aws-eks-admin-access"
     directory = "aws-eks"
   }
 
   input {
-    name = "eks_version"
+    name  = "eks_version"
     value = "v1.27.8"
+  }
+
+  input {
+    name  = "admin_access_role_arn"
+    value = "arn:aws:iam::676549690856:role/aws-reserved/sso.amazonaws.com/us-east-2/AWSReservedSSO_NuonAdmin_b8aea3365312317b"
   }
 }
