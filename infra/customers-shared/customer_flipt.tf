10092ff813eff7d00331662e987f570cc65698a8
Jon Morehouse <MorehouseJ09@gmail.com>
2023-12-04 17:36:50 -0600

    feat: add flipt setup (#3237)
    * feat: add flipt setup
    
    * chore: update

---
+++ infra/customers-shared/customer_flipt.tf
@@ -0,0 +1,46 @@
+resource "nuon_docker_build_component" "flipt" {
+  name   = "image"
+  app_id     = nuon_app.sandbox["flipt"].id
+
+  dockerfile = "Dockerfile"
+  public_repo = {
+    directory = "."
+    repo      = "flipt-io/flipt"
+    branch    = "main"
+  }
+}
+
+resource "nuon_helm_chart_component" "flipt" {
+  provider = nuon.sandbox
+
+  name       = "Flipt"
+  app_id     = nuon_app.sandbox["flipt"].id
+  chart_name = "flipt"
+
+  public_repo = {
+    directory = "charts/flipt"
+    repo      = "https://github.com/flipt-io/helm-charts"
+    branch    = "main"
+  }
+
+  value {
+    name = "image.repository"
+    value             = "{{.nuon.components.image.image.tag}}"
+  }
+
+  value {
+    name = "image.tag"
+    value             = "{{.nuon.components.image.image.tag}}"
+  }
+}
+
+resource "nuon_install" "flipt_install" {
+  provider = nuon.sandbox
+
+  app_id = nuon_app.sandbox["flipt"].id
+
+  name         = "flipt-demo"
+  region       = "us-east-1"
+  iam_role_arn = "arn:aws:iam::949309607565:role/nuon-demo-install-access"
+}
+
