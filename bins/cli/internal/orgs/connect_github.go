37a87e5d53487702ff9836ef8c35c0f7e0fe1c08
Nat Hamilton <info@nathamilton.com>
2023-12-05 13:09:18 -0500

    feat(cli): orgs connect-github (#3222)
    * feat(cli): orgs connect-github
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * style(cli): orgs connect-github cleanup
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat(cli): passing org id as url state
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat(cli): connect-github using new ui /connect flow
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat(cli): github app name in config with prod as default
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    Co-authored-by: nuonbot <128654611+nuonbot@users.noreply.github.com>

---
+++ bins/cli/internal/orgs/connect_github.go
@@ -0,0 +1,72 @@
+package orgs
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/nuonco/nuon-go/models"
+	"github.com/pkg/browser"
+	"github.com/powertoolsdev/mono/bins/cli/internal/ui"
+)
+
+func (s *Service) ConnectGithub(ctx context.Context) {
+	var v *models.AppVCSConnection
+	var connection = "started"
+	view := ui.NewGetView()
+
+	ogVcs, err := s.api.GetVCSConnections(ctx)
+	if err != nil {
+		view.Error(err)
+		connection = "failed"
+		return
+	}
+
+	timeout := 1
+	connected := false
+	for connected == false {
+		fmt.Printf("connection: %s\n", connection)
+
+		if connection == "started" {
+			connection = "pending"
+			browser.OpenURL("https://github.com/apps/" + s.cfg.GitHubAppName + "/installations/new?state=" + s.cfg.OrgID)
+		}
+
+		vcs, err := s.api.GetVCSConnections(ctx)
+		if err != nil {
+			view.Error(err)
+			connection = "failed"
+			return
+		}
+
+		if len(vcs) > len(ogVcs) || connection == "failed" {
+			v = vcs[len(vcs)-1]
+			fmt.Println("connection: connected")
+			connection = "connected"
+			connected = true
+		} else {
+			time.Sleep(5 * time.Second)
+		}
+
+		if timeout == 18 {
+			fmt.Println("connection: time out")
+			connection = "time out"
+			connected = true
+		}
+
+		timeout++
+	}
+
+	if connection == "connected" {
+		fmt.Println("")
+		fmt.Println("new vcs connection")
+		fmt.Println("-------------------------------------------------")
+		view.Render([][]string{
+			{"id", v.ID},
+			{"github install id", v.GithubInstallID},
+		})
+	} else {
+		fmt.Println("")
+		fmt.Println("please try again")
+	}
+}
