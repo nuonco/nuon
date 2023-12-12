d850a975cd6ff0a15d6ece8ef99674a46b9cf57c
Nat Hamilton <info@nathamilton.com>
2023-12-12 07:57:13 -0500

    refactor(cli): internal/ui print error now exits with code 1 (#3344)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- bins/cli/internal/ui/print.go
+++ bins/cli/internal/ui/print.go
@@ -1,6 +1,8 @@
 package ui
 
 import (
+	"os"
+
 	"github.com/nuonco/nuon-go"
 	"github.com/pterm/pterm"
 )
@@ -14,13 +16,16 @@ func PrintError(err error) {
 	userErr, ok := nuon.ToUserError(err)
 	if ok {
 		pterm.Error.Println(userErr.Description)
+		os.Exit(1)
 		return
 	}
 
 	if nuon.IsServerError(err) {
 		pterm.Error.Println(defaultServerErrorMessage)
+		os.Exit(1)
 		return
 	}
 
 	pterm.Error.Println(defaultUnknownErrorMessage)
+	os.Exit(1)
 }
