ec6321c626d419e9bfb194d197bb3e06376aa3a0
Jon Morehouse <MorehouseJ09@gmail.com>
2023-12-19 12:48:10 -0600

    chore: add experimental endpoint for oapi3spec (#3473)
    Co-authored-by: nuonbot <128654611+nuonbot@users.noreply.github.com>

--- services/ctl-api/internal/app/general/service/service.go
+++ services/ctl-api/internal/app/general/service/service.go
@@ -34,6 +34,7 @@ func (s *service) RegisterInternalRoutes(api *gin.Engine) error {
 
 	api.POST("/v1/general/integration-user", s.CreateIntegrationUser)
 	api.POST("/v1/general/canary-user", s.CreateCanaryUser)
+	api.GET("/v1/general/openapi3-spec", s.GetOpenAPI3Spec)
 
 	return nil
 }
