b3f5d587b9087525be67717e8da4ea26c338588a
Jon Morehouse <MorehouseJ09@gmail.com>
2023-12-21 10:41:34 -0600

    feat: point docs to mintlify site (#3494)

--- infra/dns/docs.tf
+++ infra/dns/docs.tf
@@ -5,6 +5,16 @@ resource "aws_route53_record" "docs" {
   type    = "CNAME"
   ttl     = 300
   records = [
-    "50ae19cad5-hosting.gitbook.io"
+    "cname.vercel-dns.com."
+  ]
+}
+
+resource "aws_route53_record" "docs-txt" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = "_vercel"
+  type    = "TXT"
+  ttl     = 300
+  records = [
+    "vc-domain-verify=docs.nuon.co,d405ffa1f9db2909d3bd"
   ]
 }
