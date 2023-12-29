34d9f551f7c86879511515004dc97be416dd0344
Nat Hamilton <info@nathamilton.com>
2023-12-29 11:29:29 -0500

    feat: cli org select (#3523)
    * refactor(cli): set-current svc refactor, added some helpers
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat(cli): added orgs select cmd
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * refactor(cli): better not found err check when setting an org
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- bins/cli/internal/orgs/service.go
+++ bins/cli/internal/orgs/service.go
@@ -1,8 +1,13 @@
 package orgs
 
 import (
+	"context"
+	"errors"
+	"fmt"
+
 	"github.com/nuonco/nuon-go"
 	"github.com/powertoolsdev/mono/bins/cli/internal/config"
+	"github.com/pterm/pterm"
 )
 
 type Service struct {
@@ -15,4 +20,25 @@ func New(apiClient nuon.Client, cfg *config.Config) *Service {
 		api: apiClient,
 		cfg: cfg,
 	}
+}
+
+func (s *Service) setOrgInConfig(ctx context.Context, orgID string) error {
+	s.cfg.Set("org_id", orgID)
+	return s.cfg.WriteConfig()
+}
+
+func (s *Service) printOrgSetMsg(name, id string) {
+	pterm.Info.Printfln("current org is now %s: %s", pterm.Green(name), pterm.Green(id))
+}
+
+func (s *Service) printNoOrgsMsg() {
+	pterm.DefaultBasicText.Printfln("you don't have any orgs, create one using %s", pterm.LightMagenta("orgs create"))
+}
+
+func (s *Service) printOrgNotFoundMsg(id string) {
+	pterm.DefaultBasicText.Printfln("can't find org %s, use %s to view all orgs or create one using %s", pterm.Green(id), pterm.LightMagenta("orgs list"), pterm.LightMagenta("orgs create"))
+}
+
+func (s *Service) notFoundErr(id string) error {
+	return errors.New(fmt.Sprintf("org %s was not found", id))
 }
