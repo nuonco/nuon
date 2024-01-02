eb5e5757b038bef4505565d3086e4c9424756cc9
Nat Hamilton <info@nathamilton.com>
2024-01-02 14:19:28 -0500

    feat(cli): adds apps select cmd (#3543)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- bins/cli/internal/apps/service.go
+++ bins/cli/internal/apps/service.go
@@ -1,8 +1,13 @@
 package apps
 
 import (
+	"context"
+	"errors"
+	"fmt"
+
 	"github.com/nuonco/nuon-go"
 	"github.com/powertoolsdev/mono/bins/cli/internal/config"
+	"github.com/pterm/pterm"
 )
 
 type Service struct {
@@ -15,4 +20,25 @@ func New(apiClient nuon.Client, cfg *config.Config) *Service {
 		api: apiClient,
 		cfg: cfg,
 	}
+}
+
+func (s *Service) setAppInConfig(ctx context.Context, appID string) error {
+	s.cfg.Set("app_id", appID)
+	return s.cfg.WriteConfig()
+}
+
+func (s *Service) printAppSetMsg(name, id string) {
+	pterm.Info.Printfln("current app is now %s: %s", pterm.Green(name), pterm.Green(id))
+}
+
+func (s *Service) printNoAppsMsg() {
+	pterm.DefaultBasicText.Printfln("you don't have any apps, create one using %s", pterm.LightMagenta("apps create"))
+}
+
+func (s *Service) printAppNotFoundMsg(id string) {
+	pterm.DefaultBasicText.Printfln("can't find app %s, use %s to view all apps", pterm.Green(id), pterm.LightMagenta("apps list"))
+}
+
+func (s *Service) notFoundErr(id string) error {
+	return errors.New(fmt.Sprintf("app %s was not found", id))
 }
