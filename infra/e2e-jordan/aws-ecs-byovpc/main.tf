c309c3c159d51f086cda2d5fafc3516c3b60fee2
Jordan Acosta <jordan@nuon.co>
2024-01-25 11:13:45 -0800

    feat(infra/e2e-jordan): create fresh installs for ecs (#3752)
    Testing both aws-ecs and aws-ecs-byovpc sandboxes.

--- infra/e2e-jordan/aws-ecs-byovpc/main.tf
+++ infra/e2e-jordan/aws-ecs-byovpc/main.tf
@@ -1,47 +1,47 @@
 locals {
-  name                  = "e2e-jordan"
-  sandbox_name          = "aws-ecs-byovpc"
-  sandboxes_repo        = "nuonco/sandboxes"
-  sandbox_branch        = "ja/3705-aws-ecs-sandbox"
-  ecs_artifact_base_url = "https://raw.githubusercontent.com/nuonco/sandboxes/${local.sandbox_branch}/${local.sandbox_name}/artifacts"
+  name              = "e2e-jordan"
+  sandbox_name      = "aws-ecs-byovpc"
+  sandboxes_repo    = "nuonco/sandboxes"
+  sandbox_branch    = "ja/3705-aws-ecs-sandbox"
+  artifact_base_url = "https://raw.githubusercontent.com/nuonco/sandboxes/${local.sandbox_branch}/${local.sandbox_name}/artifacts"
 }
 
-data "http" "ecs_sandbox_trust_policy" {
-  url = "${local.ecs_artifact_base_url}/trust.json"
+data "http" "sandbox_trust_policy" {
+  url = "${local.artifact_base_url}/trust.json"
 }
 
-data "http" "ecs_sandbox_provision_policy" {
-  url = "${local.ecs_artifact_base_url}/provision.json"
+data "http" "sandbox_provision_policy" {
+  url = "${local.artifact_base_url}/provision.json"
 }
 
-data "http" "ecs_sandbox_deprovision_policy" {
-  url = "${local.ecs_artifact_base_url}/deprovision.json"
+data "http" "sandbox_deprovision_policy" {
+  url = "${local.artifact_base_url}/deprovision.json"
 }
 
-resource "aws_iam_policy" "ecs_deprovision" {
-  name   = "nuon-${local.name}-install-deprovision-access-ecs"
-  policy = data.http.ecs_sandbox_deprovision_policy.response_body
+resource "aws_iam_policy" "deprovision" {
+  name   = "nuon-${local.name}-${local.sandbox_name}-install-deprovision-access"
+  policy = data.http.sandbox_deprovision_policy.response_body
 }
 
-resource "aws_iam_policy" "ecs_provision" {
-  name   = "nuon-${local.name}-install-provision-access-ecs"
-  policy = data.http.ecs_sandbox_provision_policy.response_body
+resource "aws_iam_policy" "provision" {
+  name   = "nuon-${local.name}-${local.sandbox_name}-install-provision-access"
+  policy = data.http.sandbox_provision_policy.response_body
 }
 
-module "ecs_access" {
+module "access" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
   version = ">= 5.1.0"
 
   create_role = true
 
-  role_name = "${local.name}-customer-iam-role-ecs"
+  role_name = "${local.name}-${local.sandbox_name}-customer-iam-role"
 
   allow_self_assume_role          = true
-  custom_role_trust_policy        = data.http.ecs_sandbox_trust_policy.response_body
+  custom_role_trust_policy        = data.http.sandbox_trust_policy.response_body
   create_custom_role_trust_policy = true
   custom_role_policy_arns = [
-    aws_iam_policy.ecs_deprovision.arn,
-    aws_iam_policy.ecs_provision.arn
+    aws_iam_policy.deprovision.arn,
+    aws_iam_policy.provision.arn
   ]
 }
 
@@ -55,11 +55,11 @@ module "e2e" {
   sandbox_dir     = local.sandbox_name
   app_runner_type = "aws-ecs"
 
-  east_1_count = 0
+  east_1_count = 1
   east_2_count = 0
   west_2_count = 0
 
-  install_role_arn = module.ecs_access.iam_role_arn
+  install_role_arn = module.access.iam_role_arn
   install_inputs = [
     {
       name          = "vpc_id"
