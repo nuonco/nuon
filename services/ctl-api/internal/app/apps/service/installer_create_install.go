202e9fe7c758919c89ee0cc2f3ed615b0147c44f
Nat Hamilton <info@nathamilton.com>
2024-01-26 11:05:11 -0500

    refactor(ctl-api): updated installer create install to support app in… (#3743)
    refactor(ctl-api): updated installer create install to support app inputs, sandbox & runner configs
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/ctl-api/internal/app/apps/service/installer_create_install.go
+++ services/ctl-api/internal/app/apps/service/installer_create_install.go
@@ -8,6 +8,8 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/middlewares/stderr"
+	"gorm.io/gorm"
 )
 
 type InstallerCreateInstallRequest struct {
@@ -26,7 +28,7 @@ func (c *InstallerCreateInstallRequest) Validate(v *validator.Validate) error {
 	return nil
 }
 
-// @ID InstallerCreateInstaller
+// @ID InstallerCreateInstall
 // @Summary	create an app install from an installer
 // @Description.markdown installer_create_install.md
 // @Param			req	body	InstallerCreateInstallRequest	true	"Input"
@@ -73,9 +75,29 @@ func (s *service) CreateInstallerInstall(ctx *gin.Context) {
 
 func (s *service) createInstall(ctx context.Context, appID string, req *InstallerCreateInstallRequest) (*app.Install, error) {
 	parentApp := app.App{}
-	res := s.db.WithContext(ctx).Preload("Components").Preload("SandboxRelease").First(&parentApp, "id = ?", appID)
+	res := s.db.WithContext(ctx).Preload("Components").
+		Preload("AppSandboxConfigs", func(db *gorm.DB) *gorm.DB {
+			return db.Order("app_sandbox_configs.created_at DESC")
+		}).
+		Preload("AppRunnerConfigs", func(db *gorm.DB) *gorm.DB {
+			return db.Order("app_runner_configs.created_at DESC")
+		}).First(&parentApp, "id = ?", appID)
+
 	if res.Error != nil {
 		return nil, fmt.Errorf("unable to get install: %w", res.Error)
+	}
+
+	if len(parentApp.AppSandboxConfigs) < 1 {
+		return nil, stderr.ErrUser{
+			Err:         fmt.Errorf("app does not have any sandbox configs"),
+			Description: "please make create at least one app sandbox config first",
+		}
+	}
+	if len(parentApp.AppRunnerConfigs) < 1 {
+		return nil, stderr.ErrUser{
+			Err:         fmt.Errorf("app does not have any runner configs"),
+			Description: "please make create at least one app runner config first",
+		}
 	}
 
 	installCmps := make([]app.InstallComponent, 0)
@@ -93,8 +115,9 @@ func (s *service) createInstall(ctx context.Context, appID string, req *Installe
 			Region:     req.AWSAccount.Region,
 			IAMRoleARN: req.AWSAccount.IAMRoleARN,
 		},
-		//SandboxReleaseID:  parentApp.SandboxRelease.ID,
-		InstallComponents: installCmps,
+		AppSandboxConfigID: parentApp.AppSandboxConfigs[0].ID,
+		AppRunnerConfigID:  parentApp.AppRunnerConfigs[0].ID,
+		InstallComponents:  installCmps,
 	}
 
 	res = s.db.WithContext(ctx).Create(&install)
