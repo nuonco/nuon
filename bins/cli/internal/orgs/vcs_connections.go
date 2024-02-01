1cb6316def44d89da9106e7abaf700267ad8e3ab
Nat Hamilton <info@nathamilton.com>
2024-02-01 12:02:36 -0500

    feat(cli): added orgs list-vcs-connections cmd (#3819)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

---
+++ bins/cli/internal/orgs/vcs_connections.go
@@ -0,0 +1,36 @@
+package orgs
+
+import (
+	"context"
+
+	"github.com/powertoolsdev/mono/bins/cli/internal/ui"
+)
+
+func (s *Service) VCSConnections(ctx context.Context, asJSON bool) {
+	view := ui.NewGetView()
+
+	vcs, err := s.api.GetVCSConnections(ctx)
+	if err != nil {
+		view.Error(err)
+		return
+	}
+
+	if asJSON {
+		ui.PrintJSON(vcs)
+		return
+	}
+
+	data := [][]string{
+		{
+			"github install id",
+		},
+	}
+
+	for _, v := range vcs {
+		data = append(data, []string{
+			*&v.GithubInstallID,
+		})
+	}
+
+	view.Render(data)
+}
