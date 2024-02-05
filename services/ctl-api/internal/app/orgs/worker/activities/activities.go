12cd2c6b6a1d246e28f40bf4e5db1f5ad33de469
Jon Morehouse <MorehouseJ09@gmail.com>
2024-02-05 14:11:49 -0800

    feat: automatically reprovision apps when reprovisioning an org (#3879)
    * feat: automatically reprovision apps when reprovisioning an org
    
    * format

--- services/ctl-api/internal/app/orgs/worker/activities/activities.go
+++ services/ctl-api/internal/app/orgs/worker/activities/activities.go
@@ -2,16 +2,20 @@ package activities
 
 import (
 	"github.com/powertoolsdev/mono/services/ctl-api/internal"
+	apphooks "github.com/powertoolsdev/mono/services/ctl-api/internal/app/apps/hooks"
 	"gorm.io/gorm"
 )
 
 type Activities struct {
-	db *gorm.DB
+	db       *gorm.DB
+	appHooks *apphooks.Hooks
 }
 
 func New(cfg *internal.Config,
+	appHooks *apphooks.Hooks,
 	db *gorm.DB) (*Activities, error) {
 	return &Activities{
-		db: db,
+		db:       db,
+		appHooks: appHooks,
 	}, nil
 }
