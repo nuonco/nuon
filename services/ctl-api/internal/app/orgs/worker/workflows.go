b37fa93429c752c4699c85967c11ebb47d5f7aa6
Jon Morehouse <MorehouseJ09@gmail.com>
2024-02-06 14:14:39 -0800

    feat: add metrics into org event loop (#3903)

--- services/ctl-api/internal/app/orgs/worker/workflows.go
+++ services/ctl-api/internal/app/orgs/worker/workflows.go
@@ -2,23 +2,30 @@ package worker
 
 import (
 	"github.com/go-playground/validator/v10"
+	"github.com/powertoolsdev/mono/pkg/metrics"
 	"github.com/powertoolsdev/mono/services/ctl-api/internal"
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/app/orgs/worker/activities"
 	"go.uber.org/zap"
 )
 
 type Workflows struct {
-	cfg  *internal.Config
-	v    *validator.Validate
-	acts activities.Activities
-	l    *zap.Logger
+	cfg           *internal.Config
+	v             *validator.Validate
+	acts          activities.Activities
+	l             *zap.Logger
+	metricsWriter metrics.Writer
 }
 
-func NewWorkflows(v *validator.Validate, cfg *internal.Config, l *zap.Logger) *Workflows {
+func NewWorkflows(v *validator.Validate,
+	cfg *internal.Config,
+	l *zap.Logger,
+	metricsWriter metrics.Writer,
+) *Workflows {
 	return &Workflows{
-		cfg: cfg,
-		v:   v,
-		l:   l,
+		cfg:           cfg,
+		v:             v,
+		l:             l,
+		metricsWriter: metricsWriter,
 		//  NOTE: this field is only used to be able to fetch activity methods
 		acts: activities.Activities{},
 	}
