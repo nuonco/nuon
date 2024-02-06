b790e457e3e4ac2809acb551d336053220a10ee2
Jon Morehouse <MorehouseJ09@gmail.com>
2024-02-06 09:41:53 -0800

    fix: dependency issue / provision orgs (#3894)
    * fix: bug recreating dependencies
    
    * fix: automatically reprovision all child projects

--- services/ctl-api/internal/app/orgs/worker/activities/activities.go
+++ services/ctl-api/internal/app/orgs/worker/activities/activities.go
@@ -1,21 +1,21 @@
 package activities
 
 import (
+	"github.com/powertoolsdev/mono/pkg/waypoint/client/multi"
 	"github.com/powertoolsdev/mono/services/ctl-api/internal"
-	apphooks "github.com/powertoolsdev/mono/services/ctl-api/internal/app/apps/hooks"
 	"gorm.io/gorm"
 )
 
 type Activities struct {
 	db       *gorm.DB
-	appHooks *apphooks.Hooks
+	wpClient multi.Client
 }
 
 func New(cfg *internal.Config,
-	appHooks *apphooks.Hooks,
+	wpClient multi.Client,
 	db *gorm.DB) (*Activities, error) {
 	return &Activities{
 		db:       db,
-		appHooks: appHooks,
+		wpClient: wpClient,
 	}, nil
 }
