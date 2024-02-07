df4f97c08d0c1e788a01851e0fff3ad4043f2fca
Jon Morehouse <MorehouseJ09@gmail.com>
2024-02-07 08:37:33 -0800

    feat: installers list + orgs health check + install script improvements (#3908)
    * feat: get installers endpoint
    
    * added installer command
    
    * fix installer
    
    * chore: update

--- bins/cli/internal/version/version.go
+++ bins/cli/internal/version/version.go
@@ -2,15 +2,22 @@ package version
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/powertoolsdev/mono/bins/cli/internal/ui"
 )
 
 var Version string = "development"
 
-func (s *Service) Version(ctx context.Context) {
+func (s *Service) Version(ctx context.Context, asJSON bool) {
+	if asJSON {
+		fmt.Printf("%s\n", Version)
+		return
+	}
+
 	view := ui.NewGetView()
 	view.Render([][]string{
 		{"version", Version},
 	})
+
 }
