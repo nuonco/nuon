e632ea16ed21ffb468a72e37965028afe7e0b472
Jon Morehouse <MorehouseJ09@gmail.com>
2024-02-07 09:02:14 -0800

    feat: add `components list-configs` (#3910)

---
+++ bins/cli/internal/components/list_configs.go
@@ -0,0 +1,31 @@
+package components
+
+import (
+	"context"
+
+	"github.com/powertoolsdev/mono/bins/cli/internal/lookup"
+	"github.com/powertoolsdev/mono/bins/cli/internal/ui"
+)
+
+func (s *Service) ListConfigs(ctx context.Context, compID string, asJSON bool) {
+	compID, err := lookup.ComponentID(ctx, s.api, compID)
+	if err != nil {
+		ui.PrintError(err)
+		return
+	}
+
+	view := ui.NewGetView()
+
+	configs, err := s.api.GetComponentConfigs(ctx, compID)
+	if err != nil {
+		view.Error(err)
+		return
+	}
+
+	if asJSON {
+		ui.PrintJSON(configs)
+		return
+	}
+
+	ui.PrintJSON(configs)
+}
