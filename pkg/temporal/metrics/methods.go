e9f007cc18b3d6cd455396a22b33886767f381e7
Jon Morehouse <MorehouseJ09@gmail.com>
2024-02-14 17:00:40 -0800

    feat: new temporal metrics writer + canary metric (#4042)
    * chore: update
    
    * update
    
    * fix: update migrations
    
    * chore: update

---
+++ pkg/temporal/metrics/methods.go
@@ -0,0 +1,37 @@
+package metrics
+
+import (
+	"time"
+
+	"github.com/DataDog/datadog-go/v5/statsd"
+	"github.com/powertoolsdev/mono/pkg/metrics"
+	"go.temporal.io/sdk/workflow"
+)
+
+func (w *writer) Incr(ctx workflow.Context, name string, value int, tags ...string) {
+	workflow.SideEffect(ctx, func(workflow.Context) interface{} {
+		w.MetricsWriter.Incr(name, value, metrics.ToTags(w.Tags, tags...))
+		return nil
+	})
+}
+
+func (w *writer) Decr(ctx workflow.Context, name string, value int, tags ...string) {
+	workflow.SideEffect(ctx, func(workflow.Context) interface{} {
+		w.MetricsWriter.Decr(name, value, metrics.ToTags(w.Tags, tags...))
+		return nil
+	})
+}
+
+func (w *writer) Timing(ctx workflow.Context, name string, dur time.Duration, tags ...string) {
+	workflow.SideEffect(ctx, func(workflow.Context) interface{} {
+		w.MetricsWriter.Timing(name, dur, metrics.ToTags(w.Tags, tags...))
+		return nil
+	})
+}
+
+func (w *writer) Event(ctx workflow.Context, e *statsd.Event) {
+	workflow.SideEffect(ctx, func(workflow.Context) interface{} {
+		w.MetricsWriter.Event(e)
+		return nil
+	})
+}
