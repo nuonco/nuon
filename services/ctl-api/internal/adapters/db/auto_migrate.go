5f54cd37b4e49ae7c80af3a675582a60f9f69111
Jon Morehouse <MorehouseJ09@gmail.com>
2024-02-16 13:07:58 -0600

    feat: add data dog metric for migrations (#4069)
    * feat: write events to datadog when migrations applied
    
    * chore: update migration to run

--- services/ctl-api/internal/adapters/db/auto_migrate.go
+++ services/ctl-api/internal/adapters/db/auto_migrate.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/powertoolsdev/mono/pkg/metrics"
 	"github.com/powertoolsdev/mono/services/ctl-api/internal"
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/adapters/db/migrations"
 	"go.uber.org/fx"
@@ -12,23 +13,26 @@ import (
 )
 
 type AutoMigrate struct {
-	db         *gorm.DB
-	l          *zap.Logger
-	cfg        *internal.Config
-	migrations *migrations.Migrations
+	db            *gorm.DB
+	l             *zap.Logger
+	cfg           *internal.Config
+	migrations    *migrations.Migrations
+	metricsWriter metrics.Writer
 }
 
 func NewAutoMigrate(db *gorm.DB,
 	cfg *internal.Config,
 	l *zap.Logger,
 	migrations *migrations.Migrations,
+	metricsWriter metrics.Writer,
 	lc fx.Lifecycle,
 	shutdowner fx.Shutdowner) *AutoMigrate {
 	a := AutoMigrate{
-		db:         db,
-		l:          l,
-		cfg:        cfg,
-		migrations: migrations,
+		db:            db,
+		l:             l,
+		cfg:           cfg,
+		migrations:    migrations,
+		metricsWriter: metricsWriter,
 	}
 
 	lc.Append(fx.Hook{
