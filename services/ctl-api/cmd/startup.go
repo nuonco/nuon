2c7b9e0b6836c1b1a0707207e077d5a5659e086a
Jon Morehouse <MorehouseJ09@gmail.com>
2024-02-18 14:21:10 -0600

    chore: have container hang (#4093)
    * chore: have container hang
    
    * chore: fix up metrics
    
    * chore: rename migration to force re-running

--- services/ctl-api/cmd/startup.go
+++ services/ctl-api/cmd/startup.go
@@ -1,9 +1,13 @@
 package cmd
 
 import (
+	"os"
+	"time"
+
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/adapters/db"
 	"github.com/spf13/cobra"
 	"go.uber.org/fx"
+	"go.uber.org/zap"
 )
 
 func (c *cli) registerStartup() error {
@@ -24,4 +28,10 @@ func (c *cli) runStartup(cmd *cobra.Command, _ []string) {
 	}
 	providers = append(providers, c.providers()...)
 	fx.New(providers...).Run()
+
+	if os.Getenv("ENV") == "prod" || os.Getenv("ENV") == "stage" {
+		l := zap.L()
+		l.Info("sleeping for 1 minute to ensure data dog metrics are flushed")
+		time.Sleep(time.Minute)
+	}
 }
