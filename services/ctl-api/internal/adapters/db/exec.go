f2780e36e5ca40b15e13bd1152d37dd6951e00f1
Jon Morehouse <MorehouseJ09@gmail.com>
2024-02-19 12:45:19 -0600

    chore: run migrations first (#4106)

--- services/ctl-api/internal/adapters/db/exec.go
+++ services/ctl-api/internal/adapters/db/exec.go
@@ -6,12 +6,13 @@ import (
 )
 
 func (a *AutoMigrate) Execute(ctx context.Context) error {
-	if err := a.migrateModels(ctx); err != nil {
-		return fmt.Errorf("unable to migrate models: %w", err)
-	}
-
+	// NOTE(jm): this is a temporary change, and needs to be reverted
 	if err := a.execMigrations(ctx); err != nil {
 		return fmt.Errorf("unable to execute migrations: %w", err)
+	}
+
+	if err := a.migrateModels(ctx); err != nil {
+		return fmt.Errorf("unable to migrate models: %w", err)
 	}
 
 	return nil
