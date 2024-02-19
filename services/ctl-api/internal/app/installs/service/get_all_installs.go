59ea5b2745a6e4acad4362b22d7a473e21a34958
Jon Morehouse <MorehouseJ09@gmail.com>
2024-02-18 19:32:05 -0600

    feat: introduce created by relationships (#4099)
    * chore: add additional endpoints to help fetch installs
    
    * chore: update reference to user token for `created_by`
    
    * feat: add user_tokens created by relationship
    
    * chore: update
    
    * chore: make all fields consistent
    
    * fix: properly set install deploy created by id
    
    * chore cleanup endpoints
    
    * chore: update
    
    * chore: update created by ids

--- services/ctl-api/internal/app/installs/service/get_all_installs.go
+++ services/ctl-api/internal/app/installs/service/get_all_installs.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/middlewares/stderr"
 )
 
 // @ID GetAllInstalls
@@ -14,26 +16,44 @@ import (
 // @Description.markdown	get_all_installs.md
 // @Tags			installs/admin
 // @Accept			json
+// @Param   limit  query int	 false	"limit of installs to return"	     Default(60)
 // @Produce		json
 // @Success		200	{array}	app.Install
 // @Router			/v1/installs [get]
 func (s *service) GetAllInstalls(ctx *gin.Context) {
-	installs, err := s.getAllInstalls(ctx)
+	limitStr := ctx.DefaultQuery("limit", "60")
+	limitVal, err := strconv.Atoi(limitStr)
+	if err != nil {
+		ctx.Error(stderr.ErrUser{
+			Err:         fmt.Errorf("invalid limit %s: %w", limitStr, err),
+			Description: "invalid limit",
+		})
+		return
+	}
+
+	installs, err := s.getAllInstalls(ctx, limitVal)
 	if err != nil {
 		ctx.Error(fmt.Errorf("unable to get installs for: %w", err))
 		return
 	}
+
 	ctx.JSON(http.StatusOK, installs)
 }
 
-func (s *service) getAllInstalls(ctx context.Context) ([]*app.Install, error) {
+func (s *service) getAllInstalls(ctx context.Context, limitVal int) ([]*app.Install, error) {
 	var installs []*app.Install
 	res := s.db.WithContext(ctx).
 		Preload("AppSandboxConfig").
+		Preload("CreatedBy").
 		Preload("AWSAccount").
 		Preload("App").
+		Preload("App.Org").
 		Preload("App.AppSandboxConfigs").
+		Joins("JOIN apps ON apps.id=installs.app_id").
+		Joins("JOIN orgs ON orgs.id=apps.org_id").
+		Where("sandbox_mode = ?", false).
 		Order("created_at desc").
+		Limit(limitVal).
 		Find(&installs)
 	if res.Error != nil {
 		return nil, fmt.Errorf("unable to get all installs: %w", res.Error)
