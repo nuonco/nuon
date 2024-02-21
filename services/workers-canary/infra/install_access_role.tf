6c11f38fd73290ef4b8d061a8b0677829875fc96
Jon Morehouse <MorehouseJ09@gmail.com>
2024-02-21 16:00:41 -0600

    fix: set canary provider correclty (#4157)
    chore: set canary provider correctly

--- services/workers-canary/infra/install_access_role.tf
+++ services/workers-canary/infra/install_access_role.tf
@@ -2,10 +2,18 @@ module "eks_access" {
   source = "github.com/nuonco/sandboxes//iam-role"
   sandbox = "aws-eks"
   prefix = "workers-canary-${var.env}"
+
+  providers = {
+    aws = aws.canary
+  }
 }
 
 module "ecs_access" {
   source = "github.com/nuonco/sandboxes//iam-role"
   sandbox = "aws-ecs"
   prefix = "workers-canary-${var.env}"
+
+  providers = {
+    aws = aws.canary
+  }
 }
