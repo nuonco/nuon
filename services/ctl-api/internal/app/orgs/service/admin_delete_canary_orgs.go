5478eb07af2071aafb9ddc8eb0431e411267d556
Jon Morehouse <MorehouseJ09@gmail.com>
2024-02-22 17:05:51 -0600

    chore: more robustness for canaries (#4165)
    * chore: alphabetize orgs
    
    * fix: various cleanup/others for canaries

---
+++ services/ctl-api/internal/app/orgs/service/admin_delete_canary_orgs.go
@@ -0,0 +1,48 @@
+package service
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
+)
+
+type AdminDeleteCanaryOrgsRequest struct{}
+
+// @ID AdminDeleteCanaryOrgs
+// @Summary delete canary orgs
+// @Description.markdown delete_org.md
+// @Tags			orgs/admin
+// @Accept			json
+// @Param			req	body	AdminDeleteCanaryOrgsRequest	true	"Input"
+// @Produce		json
+// @Success		201	{string}	ok
+// @Router			/v1/orgs/admin-delete-canarys [POST]
+func (s *service) AdminDeleteCanaryOrgs(ctx *gin.Context) {
+	orgs, err := s.getCanaryOrgs(ctx)
+	if err != nil {
+		ctx.Error(err)
+		return
+	}
+
+	for _, org := range orgs {
+		s.hooks.ForceDelete(ctx, org.ID)
+	}
+
+	ctx.JSON(http.StatusOK, true)
+}
+
+func (s *service) getCanaryOrgs(ctx context.Context) ([]app.Org, error) {
+	var orgs []app.Org
+	res := s.db.WithContext(ctx).
+		Joins("JOIN user_tokens on orgs.created_by_id=user_tokens.subject").
+		Where("user_tokens.token_type = ?", "canary").
+		Find(&orgs)
+	if res.Error != nil {
+		return nil, fmt.Errorf("unable to get canary orgs: %w", res.Error)
+	}
+
+	return orgs, nil
+}
---
+++ services/ctl-api/internal/app/orgs/service/admin_delete_integration_orgs.go
@@ -0,0 +1,66 @@
+package service
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
+	"gorm.io/gorm"
+)
+
+type AdminDeleteIntegrationOrgsRequest struct{}
+
+// @ID AdminDeleteIntegrationOrgs
+// @Summary delete leaked integration orgs
+// @Description.markdown delete_org.md
+// @Tags			orgs/admin
+// @Accept			json
+// @Param			req	body	AdminDeleteIntegrationOrgsRequest	true	"Input"
+// @Produce		json
+// @Success		201	{string}	ok
+// @Router			/v1/orgs/admin-delete-integrations [POST]
+func (s *service) AdminDeleteIntegrationOrgs(ctx *gin.Context) {
+	orgs, err := s.getIntegrationOrgs(ctx)
+	if err != nil {
+		ctx.Error(err)
+		return
+	}
+
+	for _, org := range orgs {
+		if err := s.hardDeleteOrg(ctx, org.ID); err != nil {
+			ctx.Error(err)
+			return
+		}
+	}
+
+	ctx.JSON(http.StatusOK, true)
+}
+
+func (s *service) hardDeleteOrg(ctx context.Context, orgID string) error {
+	res := s.db.WithContext(ctx).Unscoped().Delete(&app.Org{
+		ID: orgID,
+	})
+	if res.Error != nil {
+		return fmt.Errorf("unable to delete org: %w", res.Error)
+	}
+	if res.RowsAffected != 1 {
+		return fmt.Errorf("org not found %w", gorm.ErrRecordNotFound)
+	}
+
+	return nil
+}
+
+func (s *service) getIntegrationOrgs(ctx context.Context) ([]app.Org, error) {
+	var orgs []app.Org
+	res := s.db.WithContext(ctx).
+		Joins("JOIN user_tokens on orgs.created_by_id=user_tokens.subject").
+		Where("user_tokens.token_type = ?", "integration").
+		Find(&orgs)
+	if res.Error != nil {
+		return nil, fmt.Errorf("unable to get canary orgs: %w", res.Error)
+	}
+
+	return orgs, nil
+}
