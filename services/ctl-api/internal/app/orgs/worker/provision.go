c3a98a637ebe3213b07809d92ca0d314cf6544b8
Jordan Acosta <jordan@nuon.co>
2024-02-21 16:12:29 -0800

    feat(services/ctl-api): event for failed org provision (#4159)
    * feat(services/ctl-api): event for failed org provision
    
    * docs(wiki): update canary debug steps

--- services/ctl-api/internal/app/orgs/worker/provision.go
+++ services/ctl-api/internal/app/orgs/worker/provision.go
@@ -3,6 +3,8 @@ package worker
 import (
 	"fmt"
 
+	"github.com/DataDog/datadog-go/v5/statsd"
+	"github.com/powertoolsdev/mono/pkg/metrics"
 	orgsv1 "github.com/powertoolsdev/mono/pkg/types/workflows/orgs/v1"
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/app/orgs/worker/activities"
@@ -27,6 +29,14 @@ func (w *Workflows) provision(ctx workflow.Context, orgID string, sandboxMode bo
 		CustomCert:  org.CustomCert,
 	})
 	if err != nil {
+		w.mw.Event(ctx, &statsd.Event{
+			Title: "org failed to provision",
+			Text:  fmt.Sprintf("org %s failed to provision\ncreated by %s\nerror: %s", org.ID, org.CreatedBy.Email, err.Error()),
+			Tags: metrics.ToTags(map[string]string{
+				"status":             "error",
+				"status_description": "failed to provision",
+			}),
+		})
 		w.updateStatus(ctx, orgID, StatusError, "unable to provision organization resources")
 		return fmt.Errorf("unable to provision org: %w", err)
 	}
