96ce3ec2ae34cd8c51fcb16a78e8f4c4048c3f65
Jon Morehouse <MorehouseJ09@gmail.com>
2024-02-23 16:04:46 -0600

    chore: forget by aws account id (#4176)
    * chore: add endpoint to delete installs by account
    
    * fix: endpoint
    
    * chore: add migration
    
    * chore: update migration

--- services/ctl-api/internal/adapters/db/migrations/migration_032_sensitive_inputs.go
+++ services/ctl-api/internal/adapters/db/migrations/migration_032_sensitive_inputs.go
@@ -2,19 +2,16 @@ package migrations
 
 import (
 	"context"
-
-	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
 )
 
 func (a *Migrations) migration033SensitiveInputs(ctx context.Context) error {
-	res := a.db.Unscoped().WithContext(ctx).
-		Model(&app.AppInput{}).
-		Updates(app.AppInput{
-			Sensitive: false,
-		})
-	if res.Error != nil {
+	sql := `
+update app_inputs set sensitive=false where sensitive is null;
+`
+	if res := a.db.WithContext(ctx).Exec(sql); res.Error != nil {
 		return res.Error
 	}
 
 	return nil
+
 }
