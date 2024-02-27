f05e61a059b7a3171918bfda57648d81f8420637
Jon Morehouse <MorehouseJ09@gmail.com>
2024-02-27 15:27:53 -0600

    chore: add metrics for db queries, broken down by endpoint (#4205)
    * chore: add metrics for db queries, broken down by endpoint
    
    * chore: add metrics context tag
    
    * chore: update

---
+++ services/ctl-api/internal/adapters/db/metrics_plugin.go
@@ -0,0 +1,71 @@
+package db
+
+import (
+	"context"
+	"time"
+
+	"github.com/powertoolsdev/mono/pkg/metrics"
+	metrics_middleware "github.com/powertoolsdev/mono/services/ctl-api/internal/middlewares/metrics"
+	"gorm.io/gorm"
+)
+
+type contextKey string
+
+const (
+	defaultContextKey contextKey = "gorm_metrics_plugin"
+)
+
+// This is a plugin that emits well-formed metrics to datadog based on queries/operations performed by gorm.
+//
+// It is semi-inspired by https://github.com/go-gorm/prometheus/blob/master/prometheus.go which takes this a step
+// further by pulling in database metrics and emitting them via prometheus, however prometheus is lower level than what
+// we have here.
+type metricsWriterPlugin struct {
+	metricsWriter metrics.Writer
+}
+
+func (m *metricsWriterPlugin) Name() string {
+	return "metrics-writer"
+}
+
+func (m *metricsWriterPlugin) Initialize(db *gorm.DB) error {
+	db.Callback().Create().Before("*").Register("before_all", m.beforeAll)
+	db.Callback().Create().After("*").Register("after_all", m.afterAll)
+
+	return nil
+}
+
+func (m *metricsWriterPlugin) beforeAll(tx *gorm.DB) {
+	ctx := tx.Statement.Context
+	ts := time.Now()
+
+	ctx = context.WithValue(ctx, defaultContextKey, ts)
+	tx.Statement.Context = ctx
+}
+
+func (m *metricsWriterPlugin) afterAll(tx *gorm.DB) {
+	ctx := tx.Statement.Context
+	schema := tx.Statement.Schema
+
+	val := ctx.Value(defaultContextKey)
+	if val == nil {
+		return
+	}
+	startTS := val.(time.Time)
+
+	tags := []string{
+		"table:" + schema.Table,
+	}
+	metricCtx, err := metrics_middleware.FromContext(ctx)
+	if err == nil {
+		tags = append(tags, []string{
+			"endpoint:" + metricCtx.Endpoint,
+			"context:" + metricCtx.Context,
+			"method:" + metricCtx.Method,
+			"org_id:" + metricCtx.OrgID,
+		}...)
+	}
+
+	m.metricsWriter.Incr("gorm_operation", 1, tags)
+	m.metricsWriter.Timing("gorm_operation_latency", time.Since(startTS), tags)
+}
