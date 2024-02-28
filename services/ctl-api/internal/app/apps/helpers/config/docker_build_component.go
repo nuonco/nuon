da6ab344b512ba0d4beb8c1aa11541c2c7939d23
Jon Morehouse <MorehouseJ09@gmail.com>
2024-02-27 23:20:49 -0600

    fix: document config files, fix values (#4215)

--- services/ctl-api/internal/app/apps/helpers/config/docker_build_component.go
+++ services/ctl-api/internal/app/apps/helpers/config/docker_build_component.go
@@ -1,14 +1,15 @@
 package config
 
+// NOTE(jm): components are parsed using mapstructure. Please refer to the wiki entry for more.
 type DockerBuildComponentConfig struct {
-	Name         string   `mapstructure:"name" toml:"name"`
-	Dependencies []string `mapstructure:"dependencies" toml:"-"`
-	Dockerfile   string   `mapstructure:"dockerfile" toml:"dockerfile"`
+	Name         string   `mapstructure:"name"`
+	Dependencies []string `mapstructure:"dependencies"`
+	Dockerfile   string   `mapstructure:"dockerfile"`
 
-	EnvVars []EnvironmentVariable `mapstructure:"env_vars" toml:"env_vars"`
+	EnvVars []EnvironmentVariable `mapstructure:"env_var"`
 
-	PublicRepo    *PublicRepoConfig    `mapstructure:"public_repo" toml:"public_repo"`
-	ConnectedRepo *ConnectedRepoConfig `mapstructure:"connected_repo" toml:"connected_repo"`
+	PublicRepo    *PublicRepoConfig    `mapstructure:"public_repo"`
+	ConnectedRepo *ConnectedRepoConfig `mapstructure:"connected_repo"`
 
 	// NOTE: the following parameters are not supported in the provider
 	//Target	     string		   `mapstructure:"target" toml:"target"`
@@ -21,10 +22,6 @@ func (t *DockerBuildComponentConfig) ToResource() (map[string]interface{}, error
 		return nil, err
 	}
 	resource["app_id"] = "${var.app_id}"
-
-	// blocks in terraform are singular, and repeated but in our config are plural.
-	resource["env_var"] = resource["env_vars"]
-	delete(resource, "env_vars")
 
 	return resource, nil
 }
