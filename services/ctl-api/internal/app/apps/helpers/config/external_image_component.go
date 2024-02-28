68f8af63e7cc7e3321536ac50d512f54658c9dca
Jon Morehouse <MorehouseJ09@gmail.com>
2024-02-27 23:20:49 -0600

    fix: document config files, fix values (#4215)

--- services/ctl-api/internal/app/apps/helpers/config/external_image_component.go
+++ services/ctl-api/internal/app/apps/helpers/config/external_image_component.go
@@ -12,12 +12,13 @@ type PublicImageConfig struct {
 	Tag      string `mapstructure:"tag" toml:"tag"`
 }
 
+// NOTE(jm): components are parsed using mapstructure. Please refer to the wiki entry for more.
 type ExternalImageComponentConfig struct {
-	Name         string   `mapstructure:"name" toml:"name"`
-	Dependencies []string `mapstructure:"dependencies" toml:"-"`
+	Name         string   `mapstructure:"name"`
+	Dependencies []string `mapstructure:"dependencies"`
 
-	AWSECRImageConfig *AWSECRConfig      `mapstructure:"aws_ecr" toml:"aws_ecr"`
-	PublicImageConfig *PublicImageConfig `mapstructure:"public" toml:"public"`
+	AWSECRImageConfig *AWSECRConfig      `mapstructure:"aws_ecr"`
+	PublicImageConfig *PublicImageConfig `mapstructure:"public"`
 }
 
 func (t *ExternalImageComponentConfig) ToResource() (map[string]interface{}, error) {
