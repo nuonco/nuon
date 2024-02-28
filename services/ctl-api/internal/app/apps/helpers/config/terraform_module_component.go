a2ad393adeb2a09e20646625e168460d4c3f26c8
Jon Morehouse <MorehouseJ09@gmail.com>
2024-02-28 12:14:00 -0600

    feat: apps sync polls for completion (#4217)
    * feat: add get-app-config endpoint
    
    * feat: update sync command to poll
    
    * feat: improve UX of `apps push`
    
    * chore: update
    
    ---------
    
    Co-authored-by: nuonbot <bot@nuon.co>

--- services/ctl-api/internal/app/apps/helpers/config/terraform_module_component.go
+++ services/ctl-api/internal/app/apps/helpers/config/terraform_module_component.go
@@ -1,21 +1,18 @@
 package config
 
-import "fmt"
-
 // NOTE(jm): components are parsed using mapstructure. Please refer to the wiki entry for more.
 type TerraformModuleComponentConfig struct {
-	Name             string                `mapstructure:"name"`
-	TerraformVersion string                `mapstructure:"terraform_version"`
-	Dependencies     []string              `mapstructure:"dependencies"`
-	Variables        []TerraformVariable   `mapstructure:"var"`
-	EnvVars          []EnvironmentVariable `mapstructure:"env_var"`
+	Name		 string		       `mapstructure:"name"`
+	TerraformVersion string		       `mapstructure:"terraform_version"`
+	Dependencies	 []string	       `mapstructure:"dependencies"`
+	Variables	 []TerraformVariable   `mapstructure:"var"`
+	EnvVars		 []EnvironmentVariable `mapstructure:"env_var"`
 
 	PublicRepo    *PublicRepoConfig    `mapstructure:"public_repo" toml:"public_repo"`
 	ConnectedRepo *ConnectedRepoConfig `mapstructure:"connected_repo" toml:"connected_repo"`
 }
 
 func (t *TerraformModuleComponentConfig) ToResource() (map[string]interface{}, error) {
-	fmt.Println(t.Variables)
 	resource, err := toMapStructure(t)
 	if err != nil {
 		return nil, err
