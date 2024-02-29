d8111ca402649652bc2b357345b64210853b58e0
Jon Morehouse <MorehouseJ09@gmail.com>
2024-02-29 16:57:55 -0600

    fix: actually fix installers (#4237)

--- services/ctl-api/internal/app/app_installer_metadata.go
+++ services/ctl-api/internal/app/app_installer_metadata.go
@@ -41,17 +41,25 @@ type AppInstallerMetadata struct {
 func (a *AppInstallerMetadata) AfterQuery(tx *gorm.DB) error {
 	a.FormattedDemoURL = a.DemoURL
 	if !strings.HasPrefix(a.DemoURL, "https://www.youtube.com") {
+		fmt.Println("a")
 		return nil
 	}
 	if strings.HasPrefix(a.DemoURL, "https://www.youtube.com/embed") {
 		return nil
 	}
 
-	params, err := url.ParseQuery(a.DemoURL)
+	u, err := url.Parse(a.DemoURL)
 	if err != nil {
 		return nil
 	}
+
+	params, err := url.ParseQuery(u.RawQuery)
+	if err != nil {
+		return nil
+	}
+
 	ytID := params.Get("v")
+	fmt.Println(params)
 	if ytID == "" {
 		return nil
 	}
