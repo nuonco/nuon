05973284ab87236579dc75cad607dc7e6dcf29be
Jon Morehouse <MorehouseJ09@gmail.com>
2024-03-20 18:20:21 -0600

    feat: use s3 for backend (#4329)
    * chore: update to use s3 state
    
    * chore: update logic for retrying/heartbeats
    
    * chore: add cascading for install events
    
    * chore: update
    
    * chore: update support access role with additional permissions

--- services/ctl-api/internal/app/install.go
+++ services/ctl-api/internal/app/install.go
@@ -35,6 +35,7 @@ type Install struct {
 	InstallComponents  []InstallComponent  `json:"install_components,omitempty" gorm:"constraint:OnDelete:CASCADE;"`
 	InstallSandboxRuns []InstallSandboxRun `json:"install_sandbox_runs,omitempty" gorm:"constraint:OnDelete:CASCADE;"`
 	InstallInputs      []InstallInputs     `json:"install_inputs" gorm:"constraint:OnDelete:CASCADE;"`
+	InstallEvents      []InstallEvent      `json:"install_events" gorm:"constraint:OnDelete:CASCADE;"`
 
 	AWSAccount AWSAccount `json:"aws_account" gorm:"constraint:OnDelete:CASCADE;"`
 
