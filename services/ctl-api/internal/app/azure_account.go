e67cff1cbedd60b8b969396dbb77a01cd19df160
Jon Morehouse <MorehouseJ09@gmail.com>
2024-03-21 17:30:12 -0600

    feat: create AzureAccount object (#4274)
    * create: azure account table
    
    * chore: update create install params
    
    * update
    
    * save
    
    * feat: update all preloads for azure accounts
    
    * chore: update runner types
    
    * chore: update protos
    
    * fix: update

---
+++ services/ctl-api/internal/app/azure_account.go
@@ -0,0 +1,38 @@
+package app
+
+import (
+	"time"
+
+	"github.com/powertoolsdev/mono/pkg/shortid/domains"
+	"gorm.io/gorm"
+	"gorm.io/plugin/soft_delete"
+)
+
+type AzureAccount struct {
+	ID          string                `gorm:"primary_key;check:id_checker,char_length(id)=26" json:"id"`
+	CreatedByID string                `json:"created_by_id" gorm:"not null;default:null"`
+	CreatedBy   UserToken             `json:"created_by" gorm:"references:Subject"`
+	CreatedAt   time.Time             `json:"created_at" gorm:"notnull"`
+	UpdatedAt   time.Time             `json:"updated_at" gorm:"notnull"`
+	DeletedAt   soft_delete.DeletedAt `gorm:"index" json:"-"`
+
+	// used for RLS
+	OrgID string `json:"org_id" gorm:"notnull" swaggerignore:"true"`
+	Org   Org    `faker:"-" json:"-"`
+
+	InstallID string `json:"-" gorm:"notnull"`
+	Install   Install
+
+	Location                 string `json:"location" gorm:"notnull"`
+	SubscriptionID           string `json:"subscription_id" gorm:"not null;default null"`
+	SubscriptionTenantID     string `json:"subscription_tenant_id" gorm:"not null;default null"`
+	ServicePrincipalAppID    string `json:"service_principal_app_id" gorm:"not null;default null"`
+	ServicePrincipalPassword string `json:"service_principal_password" gorm:"not null;default null"`
+}
+
+func (a *AzureAccount) BeforeCreate(tx *gorm.DB) error {
+	a.ID = domains.NewAzureAccountID()
+	a.CreatedByID = createdByIDFromContext(tx.Statement.Context)
+	a.OrgID = orgIDFromContext(tx.Statement.Context)
+	return nil
+}
--- services/ctl-api/internal/app/installs/service/admin_restart_install.go
+++ services/ctl-api/internal/app/installs/service/admin_restart_install.go
@@ -44,6 +44,7 @@ func (s *service) getInstall(ctx context.Context, installID string) (*app.Instal
 	install := app.Install{}
 	res := s.db.WithContext(ctx).
 		Preload("AWSAccount").
+		Preload("AzureAccount").
 		Preload("App").
 		Preload("App.AppInputConfigs", func(db *gorm.DB) *gorm.DB {
 			return db.Order("app_input_configs.created_at DESC")
