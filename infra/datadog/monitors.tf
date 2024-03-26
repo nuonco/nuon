7cd853cc94be43a3db4db5456ab9f2f4c191b0ae
Jordan Acosta <jordan@nuon.co>
2024-03-26 01:27:37 -0700

    feat(infra/datadog): keep monitors separate from other config (#4422)

--- infra/datadog/monitors.tf
+++ infra/datadog/monitors.tf
@@ -1,5 +1,5 @@
 resource "datadog_monitor_json" "kubernetes_container_waiting" {
-  for_each = { for monitor in local.values.datadog.monitors : monitor.json => monitor }
+  for_each = { for monitor in local.vars.monitors : monitor.json => monitor }
 
   monitor = each.value.json
 }
