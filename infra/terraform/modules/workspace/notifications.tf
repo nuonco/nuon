b8016c565e18e833a0b333dd2fd6561f9fef5933
Jon Morehouse <MorehouseJ09@gmail.com>
2024-04-13 19:47:20 -0600

    fix: infra terraform properly disables notifs (#4557)

--- infra/terraform/modules/workspace/notifications.tf
+++ infra/terraform/modules/workspace/notifications.tf
@@ -1,4 +1,5 @@
 resource "tfe_notification_configuration" "slack-alerts" {
+  count = var.slack_notifications_webhook_url != "" ? 1 : 0
   name             = "${var.name}-slack-alerts"
   enabled          = true
   destination_type = "slack"
