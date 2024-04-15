a8c7aadc32dbbd4b9008965939f05871180e427f
Jon Morehouse <MorehouseJ09@gmail.com>
2024-04-14 18:49:56 -0600

    feat: add dashboard terraform (#4558)
    * fix: infra terraform properly disables notifs
    
    * feat: add terraform for dashboard

---
+++ services/dashboard-ui/infra/certificates.tf
@@ -0,0 +1,16 @@
+locals {
+  root_domain = "${var.env}.${local.vars.public_root_domain}"
+}
+
+data "aws_route53_zone" "public" {
+  name = local.root_domain
+}
+
+module "certificate" {
+  source  = "terraform-aws-modules/acm/aws"
+  version = "~> 4.0"
+
+  domain_name         = "dashboard.${local.root_domain}"
+  zone_id             = data.aws_route53_zone.public.zone_id
+  wait_for_validation = true
+}
