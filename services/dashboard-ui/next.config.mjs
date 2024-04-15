bc8b4a80aa947e446069c8b8d90129b0879ec5ca
Jon Morehouse <MorehouseJ09@gmail.com>
2024-04-14 23:53:03 -0600

    fix: production values (#4565)
    * fix: add prod client id
    
    * fix: add cors
    
    * fix: set dev client id
    
    * chore: update next config

--- services/dashboard-ui/next.config.mjs
+++ services/dashboard-ui/next.config.mjs
@@ -1,7 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-  ignoreBuildErrors: true,
+    ignoreBuildErrors: true,
+  },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/(.*?)",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "Authorization, X-Nuon-Org-ID X-Nuon-Api-Token, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
   }
 };
 
