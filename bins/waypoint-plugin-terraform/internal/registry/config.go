6102987cd94ca78e2781ab452f22f73c7b530a85
Jon Morehouse <MorehouseJ09@gmail.com>
2024-04-18 20:18:15 -0600

    fix: terraform builds (#4595)
    * fix: update credentials
    
    * chore: use custom terraform build registry
    
    * update
    
    * fix: use same config
    
    * fix: update

--- bins/waypoint-plugin-terraform/internal/registry/config.go
+++ bins/waypoint-plugin-terraform/internal/registry/config.go
@@ -1,17 +1,5 @@
 package registry
 
-import "github.com/powertoolsdev/mono/pkg/aws/credentials"
-
-type Config struct {
-	// settings to assume a role and access ECR
-	Region string             `hcl:"region,optional"`
-	Auth   credentials.Config `hcl:"auth,block"`
-
-	// repository
-	Repository string `hcl:"repository,optional"`
-	Tag        string `hcl:"tag,attr"`
-}
-
 func (r *Registry) Config() (interface{}, error) {
 	return &r.config, nil
 }
