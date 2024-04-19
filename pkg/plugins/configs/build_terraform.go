75b07723e2c1756676bfdf82370e9092552bae54
Jon Morehouse <MorehouseJ09@gmail.com>
2024-04-18 20:18:15 -0600

    fix: terraform builds (#4595)
    * fix: update credentials
    
    * chore: use custom terraform build registry
    
    * update
    
    * fix: use same config
    
    * fix: update

--- pkg/plugins/configs/build_terraform.go
+++ pkg/plugins/configs/build_terraform.go
@@ -1,5 +1,7 @@
 package configs
 
+import "github.com/powertoolsdev/mono/pkg/aws/credentials"
+
 // TerraformBuild is used by the terraform plugin to create an OCI archive with the build parameters.
 type TerraformBuild struct {
 	Plugin string `hcl:"plugin,label"`
@@ -8,4 +10,14 @@ type TerraformBuild struct {
 
 	Labels    map[string]string `hcl:"labels,optional"`
 	Variables map[string]string `hcl:"variables,optional"`
+}
+
+type TerraformBuildAWSECRRegistry struct {
+	Plugin string `hcl:"plugin,label"`
+
+	Repository string `hcl:"repository"`
+	Tag        string `hcl:"tag,attr"`
+	Region     string `hcl:"region,optional"`
+
+	Auth *credentials.Config `hcl:"auth,block"`
 }
