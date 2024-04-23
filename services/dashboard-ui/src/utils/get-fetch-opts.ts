14c494d286b62830692de278f1024b24d42c7873
Nat Hamilton <info@nathamilton.com>
2024-04-23 09:13:09 -0400

    refactor(ui):  better error handling for proxy api (#4639)
    refactor(ui): update proxy api with better error handling, updated polling duration to 30s
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/utils/get-fetch-opts.ts
+++ services/dashboard-ui/src/utils/get-fetch-opts.ts
@@ -3,11 +3,11 @@ import { getSession } from '@auth0/nextjs-auth0'
 export async function getFetchOpts(orgId = ""): Promise<RequestInit> {
   const session = await getSession()
   return {
-    cache: 'no-store',
+    cache: "no-store",
     headers: {
       Authorization: `Bearer ${session?.accessToken}`,
       'Content-Type': 'application/json',
       'X-Nuon-Org-ID': orgId,
-    },
+    },    
   }
 }
