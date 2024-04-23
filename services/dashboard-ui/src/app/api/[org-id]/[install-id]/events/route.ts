b9665020ab39a6dc7b5b8e18d49a55128f6f927e
Nat Hamilton <info@nathamilton.com>
2024-04-23 09:13:09 -0400

    refactor(ui):  better error handling for proxy api (#4639)
    refactor(ui): update proxy api with better error handling, updated polling duration to 30s
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/app/api/[org-id]/[install-id]/events/route.ts
+++ services/dashboard-ui/src/app/api/[org-id]/[install-id]/events/route.ts
@@ -4,10 +4,17 @@ import { API_URL, getFetchOpts } from '@/utils'
 
 export const GET = withApiAuthRequired(async (req: NextRequest) => {
   const [orgId, installId] = req.url.split('/').slice(4, 6)
-  const data = await fetch(
-    `${API_URL}/v1/installs/${installId}/events`,
-    await getFetchOpts(orgId)
-  )
 
-  return NextResponse.json(await data.json())
+  let events = []
+  try {
+    const data = await fetch(
+      `${API_URL}/v1/installs/${installId}/events`,
+      await getFetchOpts(orgId)
+    )
+    events = await data.json()
+  } catch (error) {
+    console.error(error)
+  }
+
+  return NextResponse.json(events)
 })
--- services/dashboard-ui/src/app/api/[org-id]/status/route.ts
+++ services/dashboard-ui/src/app/api/[org-id]/status/route.ts
@@ -5,22 +5,41 @@ import { API_URL, getFetchOpts } from '@/utils'
 export const GET = withApiAuthRequired(async (req: NextRequest) => {
   const [orgId] = req.url.split('/').slice(4, 5)
 
-  const [org, health] = await Promise.all([fetch(
-    `${API_URL}/v1/orgs/current`,
-    await getFetchOpts(orgId)
-  ).then(d => d?.json()), fetch(
-    `${API_URL}/v1/orgs/current/health-checks`,
-    await getFetchOpts(orgId)
-  ).then(d => d?.json())])
-  
-  return NextResponse.json({
+  let status = {
     orgStatus: {
-      status: org?.status,
-      status_description: org?.status_description,
+      status: 'error',
+      status_description: 'Failed to get org status',
     },
     orgHealth: {
-      status:  health?.[0]?.status,
-      status_description:  health?.[0]?.status_description
+      status: 'error',
+      status_description: 'Failed to get org health',
+    },
+  }
+
+  try {
+    const [org, health] = await Promise.all([
+      fetch(`${API_URL}/v1/orgs/current`, await getFetchOpts(orgId)).then((d) =>
+        d?.json()
+      ),
+      fetch(
+        `${API_URL}/v1/orgs/current/health-checks`,
+        await getFetchOpts(orgId)
+      ).then((d) => d?.json()),
+    ])
+
+    status = {
+      orgStatus: {
+        status: org?.status,
+        status_description: org?.status_description,
+      },
+      orgHealth: {
+        status: health?.[0]?.status,
+        status_description: health?.[0]?.status_description,
+      },
     }
-  })
+  } catch (error) {
+    console.error(error)
+  }
+
+  return NextResponse.json(status)
 })
