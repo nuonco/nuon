ef5df5affa7bfe520567002ea2695f040dcbec1e
Jon Morehouse <MorehouseJ09@gmail.com>
2024-04-24 15:52:10 -0600

    feat: add domain for website (#4650)

--- infra/vercel/projects.tf
+++ infra/vercel/projects.tf
@@ -1,3 +1,7 @@
+locals {
+  website_domain = "nuon.co"
+}
+
 resource "vercel_project" "website" {
   name           = "website"
   framework      = "astro"
@@ -8,4 +12,17 @@ resource "vercel_project" "website" {
     type = "github"
     repo = "powertoolsdev/mono"
   }
+}
+
+resource "vercel_project_domain" "website" {
+  project_id = vercel_project.website.id
+  domain     = local.website_domain
+}
+
+resource "vercel_project_domain" "www-website" {
+  project_id = vercel_project.website.id
+  domain     = local.website_domain
+
+  redirect             = "www.${local.website_domain}"
+  redirect_status_code = 308
 }
