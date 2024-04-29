7ad1a421d56477430659076a339f41fb37987093
Nat Hamilton <info@nathamilton.com>
2024-04-29 12:36:56 -0400

    feat(ui): added remaining component configs, updated deploy error UI (#4671)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/components/components/Component.tsx
+++ services/dashboard-ui/src/components/components/Component.tsx
@@ -1,8 +1,6 @@
-import React, { type FC, } from 'react'
-import { FaDocker} from 'react-icons/fa'
-import {
-  GoQuestion,
-} from 'react-icons/go'
+import React, { type FC } from 'react'
+import { FaDocker } from 'react-icons/fa'
+import { GoQuestion } from 'react-icons/go'
 import {
   SiAwslambda,
   SiOpencontainersinitiative,
@@ -115,7 +113,9 @@ export const ComponentDependencies: FC<{ deps: Array<string> }> = ({
   return (
     <div className="flex flex-col gap-4">
       {deps.map((d) => (
-        <span key={d}>{d}</span>
+        <Text variant="overline" key={d}>
+          {d}
+        </Text>
       ))}
     </div>
   )
@@ -135,6 +135,15 @@ export const ComponentConfig: FC<{
       )) ||
         (componentType === 'terraform' && (
           <TerraformConfig {...{ cfg: config?.terraform_module, version }} />
+        )) ||
+        (componentType === 'job' && (
+          <JobComponentConfig {...{ cfg: config?.job, version }} />
+        )) ||
+        (componentType === 'external' && (
+          <OCIComponentConfig {...{ cfg: config?.external_image, version }} />
+        )) ||
+        (componentType === 'helm' && (
+          <HelmComponentConfig {...{ cfg: config?.helm, version }} />
         ))}
     </div>
   )
@@ -186,7 +195,7 @@ export const TerraformConfig: FC<{
         <VCS {...cfg} />
       </span>
 
-      <Heading variant="subheading">Terraform veriables</Heading>
+      <Heading variant="subheading">Terraform variables</Heading>
       <Code variant="preformated">
         {JSON.stringify(cfg?.variables, null, 2)}
       </Code>
@@ -199,6 +208,106 @@ export const TerraformConfig: FC<{
           </Code>
         </>
       )}
+    </>
+  )
+}
+
+export const HelmComponentConfig: FC<{
+  cfg: TComponentConfig['helm']
+  version?: number
+}> = ({ cfg, version }) => {
+  return (
+    <>
+      <span>
+        <Text variant="caption">
+          <b>Config version:</b> {version}
+        </Text>
+        <Text variant="caption">
+          <b>Chart:</b> {cfg?.chart_name}
+        </Text>
+        <VCS {...cfg} />
+      </span>
+
+      {cfg?.values ? (
+        <>
+          <Heading variant="subheading">Config values</Heading>
+          <Code variant="preformated">
+            {JSON.stringify(cfg?.values, null, 4)}
+          </Code>
+        </>
+      ) : null}
+    </>
+  )
+}
+
+export const OCIComponentConfig: FC<{
+  cfg: TComponentConfig['external_image']
+  version?: number
+}> = ({ cfg, version }) => {
+  return (
+    <>
+      <span>
+        <Text variant="caption">
+          <b>Config version:</b> {version}
+        </Text>
+        <Text variant="caption">
+          <b>Image:</b> {cfg?.image_url}
+        </Text>
+        <Text variant="caption">
+          <b>Tag:</b> {cfg?.tag}
+        </Text>
+      </span>
+
+      {cfg?.env_vars ? (
+        <>
+          <Heading variant="subheading">Enviornment variables</Heading>
+          <Code variant="preformated">
+            {JSON.stringify(cfg?.env_vars, null, 4)}
+          </Code>
+        </>
+      ) : null}
+    </>
+  )
+}
+
+export const JobComponentConfig: FC<{
+  cfg: TComponentConfig['job']
+  version?: number
+}> = ({ cfg, version }) => {
+  return (
+    <>
+      <span>
+        <Text variant="caption">
+          <b>Config version:</b> {version}
+        </Text>
+        <Text variant="caption">
+          <b>Image:</b> {cfg?.image_url}
+        </Text>
+        <Text variant="caption">
+          <b>Tag:</b> {cfg?.tag}
+        </Text>
+        <Text variant="caption">
+          <b>Command:</b> <span className="tx-code tx-small">{cfg?.cmd}</span>
+        </Text>
+      </span>
+
+      {cfg?.args ? (
+        <>
+          <Heading variant="subheading">Arguments</Heading>
+          <Code variant="preformated">
+            {JSON.stringify(cfg?.args, null, 4)}
+          </Code>
+        </>
+      ) : null}
+
+      {cfg?.env_vars ? (
+        <>
+          <Heading variant="subheading">Enviornment variables</Heading>
+          <Code variant="preformated">
+            {JSON.stringify(cfg?.env_vars, null, 4)}
+          </Code>
+        </>
+      ) : null}
     </>
   )
 }
