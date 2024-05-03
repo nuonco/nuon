109c5a6db2387ed72f661aea68a9c715c10c732d
Nat Hamilton <info@nathamilton.com>
2024-05-03 11:15:21 -0400

    refactor: UI install page refetch (#4698)
    * feat(ui): adds installs/:id proxy api route
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * refactor(ui): disables status refetching
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * refactor(ui): enables full install refecthing every 45 seconds
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/app/dashboard/[org-id]/[install-id]/page.tsx
+++ services/dashboard-ui/src/app/dashboard/[org-id]/[install-id]/page.tsx
@@ -1,15 +1,6 @@
 import { withPageAuthRequired } from '@auth0/nextjs-auth0'
-import {
-  Card,
-  EventsTimeline,
-  Heading,
-  InstallComponents,
-  InstallPageHeader,
-  CloudDetails,
-  Page,
-  SandboxDetails,
-} from '@/components'
 import { getInstall, getInstallEvents } from '@/lib'
+import { ClientRefechInstallPage } from './install'
 
 export default withPageAuthRequired(
   async function InstallDashboard({ params }) {
@@ -21,44 +12,7 @@ export default withPageAuthRequired(
       getInstallEvents({ installId, orgId }),
     ])
 
-    return (
-      <Page
-        header={<InstallPageHeader {...install} />}
-        links={[{ href: install?.org_id }, { href: install?.id }]}
-      >
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-full h-fit">
-          <div className="flex flex-col gap-6">
-            <Heading variant="subtitle">History</Heading>
-            <Card className="max-h-[40rem]">
-              <EventsTimeline
-                feedId={install?.id}
-                orgId={install?.org_id}
-                initEvents={events}
-              />
-            </Card>
-          </div>
-
-          <div className="flex flex-col gap-6">
-            <Heading variant="subtitle">Components</Heading>
-            <Card className="max-h-[40rem]">
-              <InstallComponents components={install?.install_components} />
-            </Card>
-          </div>
-
-          <div className="flex flex-col gap-6">
-            <Heading variant="subtitle">Details</Heading>
-
-            <Card>
-              <SandboxDetails {...install?.app_sandbox_config} />
-            </Card>
-
-            <Card>
-              <CloudDetails {...install} />
-            </Card>
-          </div>
-        </div>
-      </Page>
-    )
+    return <ClientRefechInstallPage {...{ install, events }} />
   },
   { returnTo: '/dashboard' }
 )
