8710d245b404e73b53ebeaa0672a04190808164d
Nat Hamilton <info@nathamilton.com>
2024-05-03 15:15:10 -0400

    chore(ui): set datadog env in init code (#4702)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/utils/datadog-logs.tsx
+++ services/dashboard-ui/src/utils/datadog-logs.tsx
@@ -4,15 +4,20 @@ import { datadogLogs } from '@datadog/browser-logs'
 import React, { type FC, useEffect } from 'react'
 
 export const InitDatadogLogs: FC = () => {
+   const ddEnv =
+        process?.env?.NEXT_PUBLIC_DATADOG_ENV ||
+        window.location?.host?.split('.')[1]
+  
   useEffect(() => {
     const initDDLogs = () => {
       datadogLogs.init({
-        clientToken: process?.env?.NEXT_PUBLIC_DATADOG_CLIENT_TOKEN,
-        site: process?.env?.NEXT_PUBLIC_DATADOG_SITE,
+        clientToken: process?.env?.NEXT_PUBLIC_DATADOG_CLIENT_TOKEN ||
+          'pub6fb6cfe0d2ec271a2456660e54ba5e08',
+        site: process?.env?.NEXT_PUBLIC_DATADOG_SITE || 'us5.datadoghq.com',
         forwardConsoleLogs: ['error', 'info'],
         forwardErrorsToLogs: true,
         sessionSampleRate: 100,
-        env: process?.env?.NEXT_PUBLIC_DATADOG_ENV,
+        env: ddEnv || 'local',
         service: 'dashboard',
       })
     }
