8497d1af8300b82828df134574d9f3b3a66079f7
Nat Hamilton <info@nathamilton.com>
2024-05-03 15:15:10 -0400

    chore(ui): set datadog env in init code (#4702)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/utils/datadog-rum.tsx
+++ services/dashboard-ui/src/utils/datadog-rum.tsx
@@ -6,11 +6,19 @@ import React, { type FC, useEffect } from 'react'
 export const InitDatadogRUM: FC = () => {
   useEffect(() => {
     const initDDLogs = () => {
+      const ddEnv =
+        process?.env?.NEXT_PUBLIC_DATADOG_ENV ||
+        window.location?.host?.split('.')[1]
+
       datadogRum.init({
-        applicationId: process?.env?.NEXT_PUBLIC_DATADOG_APP_ID,
-        clientToken: process?.env?.NEXT_PUBLIC_DATADOG_CLIENT_TOKEN,
-        site: process?.env?.NEXT_PUBLIC_DATADOG_SITE,
-        env: process?.env?.NEXT_PUBLIC_DATADOG_ENV,
+        applicationId:
+          process?.env?.NEXT_PUBLIC_DATADOG_APP_ID ||
+          '19376b57-b3fb-4ad2-b0e9-fcdf9c986069',
+        clientToken:
+          process?.env?.NEXT_PUBLIC_DATADOG_CLIENT_TOKEN ||
+          'pub6fb6cfe0d2ec271a2456660e54ba5e08',
+        site: process?.env?.NEXT_PUBLIC_DATADOG_SITE || 'us5.datadoghq.com',
+        env: ddEnv || 'local',
         service: 'dashboard',
 
         // collection settings
