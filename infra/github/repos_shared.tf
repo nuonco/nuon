bc198d678d70485661336e1e9500d7cb074f7f74
Jordan Acosta <jordan@nuon.co>
2024-05-09 07:42:28 -0700

    feat(infra/github): add Common Fate installer repo (#4729)

--- infra/github/repos_shared.tf
+++ infra/github/repos_shared.tf
@@ -195,6 +195,27 @@ module "shared-commonfate" {
   }
 }
 
+module "shared-common-fate-installer" {
+  source = "./modules/repository"
+
+  name                     = "common-fate-installer"
+  description              = "Customer installer for Common Fate."
+  required_checks          = []
+  owning_team_id           = github_team.nuonco-shared.id
+  is_private               = true
+  enable_branch_protection = false
+
+  collaborators = {
+    chrnorm           = "push"
+    shwethaumashanker = "push"
+    JoshuaWilkes      = "push"
+  }
+
+  providers = {
+    github = github.nuonco-shared
+  }
+}
+
 module "shared-streamkap" {
   source = "./modules/repository"
 
