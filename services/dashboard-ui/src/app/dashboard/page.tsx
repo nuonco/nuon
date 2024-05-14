09980a183bfe1b9f4814f3a9d5c1b70565f4ac31
Nat Hamilton <info@nathamilton.com>
2024-05-14 09:19:28 -0400

    feat(ui): added org creation flow (#4740)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/app/dashboard/page.tsx
+++ services/dashboard-ui/src/app/dashboard/page.tsx
@@ -1,10 +1,12 @@
 import { withPageAuthRequired } from '@auth0/nextjs-auth0'
 import { Heading, Grid, OrgCard, Page, PageHeader } from '@/components'
 import { getOrgs } from '@/lib'
+import { createOrg } from './actions'
 
 export default withPageAuthRequired(
   async function OrgDashboard() {
     const orgs = await getOrgs()
+    const hasOrgs = Boolean(orgs)
 
     return (
       <Page
@@ -12,13 +14,31 @@ export default withPageAuthRequired(
           <PageHeader
             title={
               <Heading level={1} variant="title">
-                Your orgs
+                {hasOrgs ? 'Your organizations' : 'Create your organization'}
               </Heading>
             }
           />
         }
       >
-        <Grid>{orgs?.map((o) => <OrgCard key={o?.id} {...o} />)}</Grid>
+        {hasOrgs ? (
+          <Grid>{orgs?.map((o) => <OrgCard key={o?.id} {...o} />)}</Grid>
+        ) : (
+          <form className="flex flex-col gap-4 max-w-md" action={createOrg}>
+            <label className="flex flex-col flex-auto gap-2">
+              <span className="font-semibold">Organization name</span>
+              <input
+                className="border bg-inherit rounded px-4 py-1.5 shadow-inner"
+                name="name"
+                type="text"
+                required
+              />
+            </label>
+
+            <button className="rounded text-sm text-gray-50 bg-fuchsia-600 hover:bg-fuchsia-700 focus:bg-fuchsia-700 active:bg-fuchsia-800 px-4 py-1.5 w-fit">
+              Create organization
+            </button>
+          </form>
+        )}
       </Page>
     )
   },
---
+++ services/dashboard-ui/src/lib/post-org.test.ts
@@ -0,0 +1,71 @@
+import { afterAll, expect, test, vi } from 'vitest'
+import { postOrg } from './post-org'
+
+const orgName = 'test org'
+const orgId = 'org-id'
+const org = {
+  id: orgId,
+  name: orgName,
+}
+
+global.fetch = vi
+  .fn()
+  .mockResolvedValueOnce({
+    ok: true,
+    json: () => new Promise((resolve) => resolve(org)),
+  })
+  .mockResolvedValueOnce({
+    ok: false,
+    json: () => new Promise((resolve) => resolve('error')),
+  })
+
+vi.mock('../utils', async (og) => {
+  const mod = await og<typeof import('../utils')>()
+  return {
+    ...mod,
+    getFetchOpts: vi.fn().mockResolvedValue({
+      cache: 'no-store',
+      headers: {
+        Authorization: 'Bearer test-token',
+        'Content-Type': 'application/json',
+      },
+    }),
+  }
+})
+
+afterAll(() => {
+  vi.restoreAllMocks()
+})
+
+test('postOrg should return an array of install object', async () => {
+  const spec = await postOrg({ name: orgName })
+
+  expect(spec).toEqual(org)
+  expect(fetch).toBeCalledWith(
+    'https://ctl.prod.nuon.co/v1/orgs',
+    expect.objectContaining({
+      headers: expect.objectContaining({
+        Authorization: 'Bearer test-token',
+      }),
+      method: 'POST',
+      body: JSON.stringify({ name: orgName }),
+    })
+  )
+})
+
+test('postOrg should throw an error when it can not find orgs', async () => {
+  try {
+    await postOrg({ name: orgName })
+  } catch (error) {
+    expect(error).toMatchInlineSnapshot(`[Error: Failed to fetch data]`)
+  }
+
+  expect(fetch).toBeCalledWith(
+    'https://ctl.prod.nuon.co/v1/orgs',
+    expect.objectContaining({
+      headers: expect.objectContaining({
+        Authorization: 'Bearer test-token',
+      }),
+    })
+  )
+})
