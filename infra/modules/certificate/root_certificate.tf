e2c33d3dea2c6377338facf5b84d911b77e4ae21
Jon Morehouse <MorehouseJ09@gmail.com>
2024-05-16 14:49:15 -0600

    chore: update how we build root cert (#4765)

--- infra/modules/certificate/root_certificate.tf
+++ infra/modules/certificate/root_certificate.tf
@@ -15,8 +15,9 @@ module "root-certificate" {
   version = "~> 4.0"
 
   domain_name            = local.root_fqdn
-  wait_for_validation    = true
+  validation_method = "DNS"
   create_route53_records = false
+  validation_record_fqdns = module.root-certificate-validation[0].validation_route53_record_fqdns
 }
 
 module "root-certificate-validation" {
@@ -25,12 +26,13 @@ module "root-certificate-validation" {
   source  = "terraform-aws-modules/acm/aws"
   version = "~> 4.0"
 
-  domain_name                               = local.root_fqdn
-  zone_id                                   = data.aws_route53_zone.root.zone_id
-  wait_for_validation                       = true
+  create_certificate = false
   create_route53_records_only               = true
+  validation_method = "DNS"
+
+  zone_id                                   = data.aws_route53_zone.root.zone_id
   acm_certificate_domain_validation_options = module.root-certificate[0].acm_certificate_domain_validation_options
-  distinct_domain_names                     = [local.root_fqdn]
+  distinct_domain_names = module.root-certificate[0].distinct_domain_names
 
   providers = {
     aws = aws.root
