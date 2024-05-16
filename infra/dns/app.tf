327b97dbbb19256e1b6815df0fdf5ab1d09a054f
Jon Morehouse <MorehouseJ09@gmail.com>
2024-05-16 15:18:12 -0600

    chore: dns change for app (#4767)

--- infra/dns/app.tf
+++ infra/dns/app.tf
@@ -1,10 +1,10 @@
 // naked app dns
-#resource "aws_route53_record" "app" {
-  #zone_id = aws_route53_zone.main.zone_id
-  #name    = "app"
-  #type    = "CNAME"
-  #ttl     = 300
-  #records = [
-    #"app.${data.aws_route53_zone.prod.name}"
-  #]
-#}
+resource "aws_route53_record" "app" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = "app"
+  type    = "A"
+  ttl     = 300
+  records = [
+    "k8s-default-dashboar-fdcb6bc9e1-2132532861.us-west-2.elb.amazonaws.com"
+  ]
+}
