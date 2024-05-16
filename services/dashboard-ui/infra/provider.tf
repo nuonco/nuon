bfc9c0744a81cf03c65ff89d1ff576604e7fd1de
Jon Morehouse <MorehouseJ09@gmail.com>
2024-05-16 13:55:09 -0600

    feat: add support for alias domains, alias dashboard (#4760)
    * feat: add `certificate` module
    
    * chore: add wiki page on DNS
    
    * feat: update dashboard terraform
    
    * chore: update root certificate

--- services/dashboard-ui/infra/provider.tf
+++ services/dashboard-ui/infra/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = local.region
+  region = local.vars.region
   alias  = "mgmt"
   default_tags {
     tags = local.tags
@@ -7,33 +7,7 @@ provider "aws" {
 }
 
 provider "aws" {
-  region = local.region
-  alias  = "canary"
-
-  assume_role {
-    role_arn = "arn:aws:iam::${local.accounts.canary.id}:role/terraform"
-  }
-
-  default_tags {
-    tags = local.tags
-  }
-}
-
-provider "aws" {
-  region = local.region
-  alias  = "infra-shared-prod"
-
-  assume_role {
-    role_arn = "arn:aws:iam::${local.accounts.infra-shared-prod.id}:role/terraform"
-  }
-
-  default_tags {
-    tags = local.tags
-  }
-}
-
-provider "aws" {
-  region = local.region
+  region = local.vars.region
 
   assume_role {
     role_arn = "arn:aws:iam::${local.accounts[var.env].id}:role/terraform"
