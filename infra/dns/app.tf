1c3a8ce38d882aa9cdaf95f9cc49254418f1af34
Jon Morehouse <MorehouseJ09@gmail.com>
2024-05-16 15:48:34 -0600

    fix: use cname for alb (#4768)

--- infra/dns/app.tf
+++ infra/dns/app.tf
@@ -2,7 +2,7 @@
 resource "aws_route53_record" "app" {
   zone_id = aws_route53_zone.main.zone_id
   name    = "app"
-  type    = "A"
+  type    = "CNAME"
   ttl     = 300
   records = [
     "k8s-default-dashboar-fdcb6bc9e1-2132532861.us-west-2.elb.amazonaws.com"
