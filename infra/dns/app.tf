ee111e188ff69b236e087d3f9c8fd4d848136ae8
Jon Morehouse <MorehouseJ09@gmail.com>
2024-05-16 14:00:15 -0600

    chore: remove fly cnames (#4761)

--- infra/dns/app.tf
+++ infra/dns/app.tf
@@ -1,37 +1,10 @@
 // naked app dns
-resource "aws_route53_record" "app" {
-  zone_id = aws_route53_zone.main.zone_id
-  name    = "app"
-  type    = "CNAME"
-  ttl     = 300
-  records = [
-    "app.${data.aws_route53_zone.prod.name}"
-  ]
-}
-
-// prod app dns
-resource "aws_route53_record" "app-prod" {
-  provider = aws.prod
-
-  zone_id = data.aws_route53_zone.prod.zone_id
-  name    = "app"
-  type    = "CNAME"
-  ttl     = 300
-  records = [
-    "nuon-ui.fly.dev"
-  ]
-}
-
-
-// stage app dns
-resource "aws_route53_record" "app-stage" {
-  provider = aws.stage
-
-  zone_id = data.aws_route53_zone.stage.zone_id
-  name    = "app"
-  type    = "CNAME"
-  ttl     = 300
-  records = [
-    "nuon-ui-stage.fly.dev"
-  ]
-}
+#resource "aws_route53_record" "app" {
+  #zone_id = aws_route53_zone.main.zone_id
+  #name    = "app"
+  #type    = "CNAME"
+  #ttl     = 300
+  #records = [
+    #"app.${data.aws_route53_zone.prod.name}"
+  #]
+#}
