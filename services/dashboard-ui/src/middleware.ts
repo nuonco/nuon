0a42c8775d5b7a1c168eb0d4240e1b86b2be0f7d
Nat Hamilton <info@nathamilton.com>
2024-05-17 08:54:56 -0400

    fix(ui): redirect to dashboard if user still has valid session (#4777)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/middleware.ts
+++ services/dashboard-ui/src/middleware.ts
@@ -1,12 +1,14 @@
+import { getSession } from '@auth0/nextjs-auth0/edge'
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 // This function can be marked `async` if using `await` inside
-export async function middleware(request: NextRequest) {
-  return NextResponse.next()
-}
+export default async function middleware(request: NextRequest) {
+  const session = await getSession()
 
-// See "Matching Paths" below to learn more
-export const config = {
-  // matcher: '*',
+  if (session && new URL(request.url).pathname === '/') {
+    return NextResponse.redirect(new URL('/dashboard', request.url))
+  }
+
+  return NextResponse.next()
 }
