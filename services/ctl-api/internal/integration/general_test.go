457458c06f4238fa7851dde89144ca987fd5d60a
Jon Morehouse <MorehouseJ09@gmail.com>
2024-05-20 14:43:16 -0600

    fix: cloud platform regions (#4809)
    * fix: cloud platform regions
    
    * chore: add tests for cloud platform

--- services/ctl-api/internal/integration/general_test.go
+++ services/ctl-api/internal/integration/general_test.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"testing"
 
+	"github.com/powertoolsdev/mono/pkg/generics"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 )
@@ -29,5 +30,47 @@ func (s *generalIntegrationTestSuite) TestGetCurrentUser() {
 		require.NoError(t, err)
 		require.NotNil(t, user)
 		require.NotEmpty(t, user.Subject)
+	})
+}
+
+func (s *generalIntegrationTestSuite) TestGetCloudPlatforms() {
+	s.T().Run("azure", func(t *testing.T) {
+		regions, err := s.apiClient.GetCloudPlatformRegions(s.ctx, "azure")
+		require.NoError(t, err)
+		require.NotNil(t, regions)
+		require.NotEmpty(t, regions)
+
+		// verify eastus is in there
+		found := false
+		for _, region := range regions {
+			if region.Value == "eastus" {
+				found = true
+				break
+			}
+		}
+		require.True(t, found)
+	})
+
+	s.T().Run("aws", func(t *testing.T) {
+		regions, err := s.apiClient.GetCloudPlatformRegions(s.ctx, "aws")
+		require.NoError(t, err)
+		require.NotNil(t, regions)
+		require.NotEmpty(t, regions)
+
+		// verify us-east-1 is in there
+		found := false
+		for _, region := range regions {
+			if region.Value == "us-east-1" {
+				found = true
+				break
+			}
+		}
+		require.True(t, found)
+	})
+
+	s.T().Run("invalid", func(t *testing.T) {
+		regions, err := s.apiClient.GetCloudPlatformRegions(s.ctx, generics.GetFakeObj[string]())
+		require.Error(t, err)
+		require.Nil(t, regions)
 	})
 }
