0b60964872c823a1626319ba4c817e2d0694a78d
Jon Morehouse <MorehouseJ09@gmail.com>
2024-05-27 11:46:31 -0600

    feat: add notifications configs for all orgs and apps (#4850)
    * feat: add migration for all notifications configs
    
    * add notif config to app

---
+++ services/ctl-api/internal/pkg/db/migrations/migration_046_notifications_existing_apps.go
@@ -0,0 +1,54 @@
+package migrations
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
+)
+
+func (m *Migrations) migration046NotifcationsExistingApps(ctx context.Context) error {
+	var apps []app.App
+
+	res := m.db.WithContext(ctx).
+		Preload("NotificationsConfig").
+		Preload("CreatedBy").
+		Find(&apps)
+	if res.Error != nil {
+		return fmt.Errorf("unable to get apps: %w", res.Error)
+	}
+
+	for _, currentApp := range apps {
+		if currentApp.NotificationsConfigID != "" {
+			m.l.Info("org already has notifications config")
+			continue
+		}
+
+		notificationsCfg := app.NotificationsConfig{
+			CreatedByID:              currentApp.CreatedByID,
+			OrgID:                    currentApp.ID,
+			EnableSlackNotifications: currentApp.CreatedBy.TokenType == app.TokenTypeAuth0,
+			EnableEmailNotifications: currentApp.CreatedBy.TokenType == app.TokenTypeAuth0,
+			InternalSlackWebhookURL:  m.cfg.InternalSlackWebhookURL,
+			OwnerID:                  currentApp.ID,
+			OwnerType:                "apps",
+		}
+
+		res := m.db.WithContext(ctx).
+			Create(&notificationsCfg)
+		if res.Error != nil {
+			return fmt.Errorf("unable to create notifications config: %w", res.Error)
+		}
+
+		// update app
+		res = m.db.WithContext(ctx).Model(&app.App{
+			ID: currentApp.ID,
+		}).Updates(app.Org{
+			NotificationsConfigID: notificationsCfg.ID,
+		})
+		if res.Error != nil {
+			return fmt.Errorf("unable to update app with notifications config: %w", res.Error)
+		}
+	}
+	return nil
+}
