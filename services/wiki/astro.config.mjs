e41479bb03583b82157edd7e76a88066952ebdc4
Jordan Acosta <jordan@nuon.co>
2024-05-29 12:18:06 -0700

    feat(services/wiki): Ja/wiki setup (#4867)
    * feat(wiki): update all files to match starlight schema
    
    Added headmatter with quoted title.
    
    * fix(services/wiki): generate sidebar from wiki files
    
    * feat(services/wiki): organize wiki files
    
    * fix(wiki): add wiki earthfile
    
    * feat(wiki): reorganize based on feedback
    
    * update
    
    * fix(wiki): fix build errors
    
    ---------
    
    Co-authored-by: Jon Morehouse <morehousej09@gmail.com>
    Co-authored-by: nuonbot <bot@nuon.co>

--- services/wiki/astro.config.mjs
+++ services/wiki/astro.config.mjs
@@ -1,47 +1,66 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 import tailwind from "@astrojs/tailwind";
 
 import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [starlight({
-    title: 'Nuon Wiki',
-    social: {
-      github: 'https://github.com/nuonco',
-      twitter: 'https://twitter.com/nuoninc',
-      youtube: 'https://www.youtube.com/channel/UC5zHWPIfIIfgpPMNo_gonPw',
-      linkedin: 'https://www.linkedin.com/company/nuonco/'
+  vite: {
+    resolve: {
+      preserveSymlinks: true,
     },
-    customCss: ['./src/tailwind.css'],
-    pagination: false,
-    sidebar: [{
-      label: 'Company',
-      autogenerate: {
-        directory: 'company'
-      }
-    }, {
-      label: 'Sales',
-      autogenerate: {
-        directory: 'sales'
-      }
-    }, {
-      label: 'Marketing',
-      autogenerate: {
-        directory: 'marketing'
-      }
-    }, {
-      label: 'Product',
-      autogenerate: {
-        directory: 'product'
-      }
-    }]
-  }), tailwind({
-    applyBaseStyles: false
-  })],
+  },
+  integrations: [
+    starlight({
+      title: "Nuon Wiki",
+      social: {
+        github: "https://github.com/nuonco",
+        twitter: "https://twitter.com/nuoninc",
+        youtube: "https://www.youtube.com/channel/UC5zHWPIfIIfgpPMNo_gonPw",
+        linkedin: "https://www.linkedin.com/company/nuonco/",
+      },
+      customCss: ["./src/tailwind.css"],
+      pagination: false,
+      // sidebar: [
+      //   {
+      //     label: "Company",
+      //     autogenerate: {
+      //       directory: "company",
+      //     },
+      //   },
+      //   {
+      //     label: "Sales",
+      //     autogenerate: {
+      //       directory: "sales",
+      //     },
+      //   },
+      //   {
+      //     label: "Marketing",
+      //     autogenerate: {
+      //       directory: "marketing",
+      //     },
+      //   },
+      //   {
+      //     label: "Product",
+      //     autogenerate: {
+      //       directory: "product",
+      //     },
+      //   },
+      //   {
+      //     label: "Legacy Wiki",
+      //     autogenerate: {
+      //       directory: "../../wiki",
+      //     },
+      //   },
+      // ],
+    }),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
   output: "server",
   adapter: node({
-    mode: "standalone"
-  })
+    mode: "standalone",
+  }),
 });
