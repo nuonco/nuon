00065fc11d4a56320a15b8fd1f03f6ad1ec7b073
Nat Hamilton <info@nathamilton.com>
2024-06-04 15:58:53 -0400

    test(ui): generate mock rest api (#4914)
    test(ui): added msw + msw-auto-mock to generate mock rest api, updated all lib test
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/vitest.setup.ts
+++ services/dashboard-ui/vitest.setup.ts
@@ -1,1 +1,7 @@
+import { beforeAll, afterEach, afterAll } from 'vitest'
 import '@testing-library/jest-dom/vitest'
+import { server } from './test/mock-api-server'
+ 
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
