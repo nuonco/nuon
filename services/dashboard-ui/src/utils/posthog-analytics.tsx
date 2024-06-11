67621039dd850ca4005fd33c3ca098b1b7694000
Nat Hamilton <info@nathamilton.com>
2024-06-11 12:43:04 -0400

    feat: UI posthog poc (#4948)
    * fix(ui): removed a rouge console log
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * chore(ui): added github as avatar source in nextjs cfg
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * chore(ui): updated tailwind safelist
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat(ui): added posthog analytics to dashboard
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * chore(ui): oapi types updated
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat(website): added posthog tracking to website
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat(docs): added posthog tracking to docs
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * style(ui): lint obey
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * refactor(docs): removed sign-in button from header
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

---
+++ services/dashboard-ui/src/utils/posthog-analytics.tsx
@@ -0,0 +1,85 @@
+'use client'
+
+import { useUser } from '@auth0/nextjs-auth0/client'
+import React, { type FC, useEffect, useState } from 'react'
+import { GoX } from 'react-icons/go'
+import posthog from 'posthog-js'
+import { Button, Heading, Text } from '@/components'
+
+export const InitPosthogAnalytics: FC = () => {
+  const { user, error, isLoading } = useUser()
+  const [showOptOut, setShowOptOut] = useState<boolean>(true)
+
+  useEffect(() => {
+    posthog.init(process?.env?.NEXT_PUBLIC_POSTHOG_TOKEN, {
+      api_host: process?.env?.NEXT_PUBLIC_POSTHOG_HOST,
+      person_profiles: 'identified_only',
+    })
+
+    posthog.opt_out_capturing()
+  }, [])
+
+  useEffect(() => {
+    if (user && !isLoading && !posthog.has_opted_out_capturing()) {
+      posthog.identify(user.sub, {
+        email: user.email,
+        nuon_id: user.sub,
+        name: user.name,
+      })
+    }
+
+    if (error || (!user && !isLoading)) {
+      posthog.reset()
+    }
+  }, [user, error, isLoading])
+
+  return (
+    <>
+      {showOptOut && (
+        <div className="absolute bottom-0 bg-opacity-95 bg-slate-50 dark:bg-slate-950 z-20 border-b shadow-sm w-full flex items-start p-6">
+          <div className="flex flex-col gap-4 items-start p-6 max-w-xl m-auto">
+            <div>
+              <Heading>Enhance your experience with Nuon</Heading>
+              <Text variant="caption">
+                We use cookies to improve your experience and enhance our
+                app&apos;s performance. By clicking &quot;Accept,&quot; you
+                consent to the use of cookies for product analytics, helping us
+                understand how you use our app and make it even better for you.
+              </Text>
+            </div>
+            <div className="flex gap-4">
+              <Button
+                variant="primary"
+                onClick={() => {
+                  posthog.opt_in_capturing()
+                  setShowOptOut(false)
+                }}
+              >
+                Accept
+              </Button>
+              <Button
+                onClick={() => {
+                  posthog.opt_out_capturing()
+                  setShowOptOut(false)
+                }}
+              >
+                Deny
+              </Button>
+            </div>
+          </div>
+          <Button
+            className="rounded-full"
+            variant="ghost"
+            title="Opt out and close"
+            onClick={() => {
+              posthog.opt_out_capturing()
+              setShowOptOut(false)
+            }}
+          >
+            <GoX />
+          </Button>
+        </div>
+      )}
+    </>
+  )
+}
--- services/dashboard-ui/tailwind.config.ts
+++ services/dashboard-ui/tailwind.config.ts
@@ -1,21 +1,44 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  safelist: [
+    {
+      pattern: /(bg|text)-(red|green|yellow)-(500|600)/,
+    },
+    {
+      pattern: /(bg|text)-(slate)-(50|950)/,
+    },
+    {
+      pattern: /bg-opacity-(25|50|75|85|95)/,
+    },
+    {
+      pattern: /z-(10|20|30|40|50)/,
+    },
+    {
+      pattern: /(bottom|top)-(0)/,
+    },
+    {
+      pattern: /(m|p)-(auto)/,
+    },
+    {
+      pattern: /(max|min)-(w|h)-(xs|sm|md|lg|xl|2xl)/,
+    },
   ],
   theme: {
     extend: {
-      gridAutoRows:{
-        "auto": "1fr"
+      gridAutoRows: {
+        auto: '1fr',
       },
       gridTemplateColumns: {
-        "auto": "repeat(auto-fill, minmax(18rem, 1fr))"
-      }
-    }
+        auto: 'repeat(auto-fill, minmax(18rem, 1fr))',
+      },
+    },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
