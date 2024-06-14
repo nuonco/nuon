571f8d71737ac3d75b615ef077272b970a9e09e4
Nat Hamilton <info@nathamilton.com>
2024-06-14 14:18:32 -0400

    test: UI context testing (#4971)
    * test(ui): added msw handler for org proxy api
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * test(ui): added org-context test
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * chore(ui): api types and mocks now respect the NUON_API_URL
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * chore(ui): set stage api as default in ci and added pretest mock gen
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * test(ui): added install-context test and procy api mock
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * test(ui): added install-component-context test and proxy api mock
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

---
+++ services/dashboard-ui/src/context/install-context.test.tsx
@@ -0,0 +1,77 @@
+import { afterAll, expect, test, vi } from 'vitest'
+import { renderHook, waitFor } from '@testing-library/react'
+import { getGetInstall200Response } from '@test/mock-api-handlers'
+import { useInstallContext, InstallProvider } from './install-context'
+
+const mockInstall = getGetInstall200Response()
+vi.mock('../utils', async (og) => {
+  const mod = await og<typeof import('../utils')>()
+  return {
+    ...mod,
+    POLL_DURATION: 2000,
+  }
+})
+
+import { POLL_DURATION } from '../utils'
+
+afterAll(() => {
+  vi.resetAllMocks()
+})
+
+test('useInstallContext should throw error when used outside of InstallProvider', () => {
+  try {
+    renderHook(() => useInstallContext())
+  } catch (error) {
+    expect(error).toMatchInlineSnapshot(
+      `[Error: useInstallContext() may only be used in the context of a <InstallProvider> component.]`
+    )
+  }
+})
+
+test('install context should render with init state', () => {
+  const { result } = renderHook(() => useInstallContext(), {
+    wrapper: ({ children }) => (
+      <InstallProvider initInstall={mockInstall}>{children}</InstallProvider>
+    ),
+  })
+  expect(result.current.install).toHaveProperty('id', mockInstall.id)
+  expect(result.current.install).toHaveProperty('name', mockInstall.name)
+  expect(result.current.isFetching).toBeFalsy()
+})
+
+test(
+  'install context should refetch it state from api if provider has polling enabled',
+  async () => {
+    const { result } = renderHook(() => useInstallContext(), {
+      wrapper: ({ children }) => (
+        <InstallProvider initInstall={mockInstall} shouldPoll>
+          {children}
+        </InstallProvider>
+      ),
+    })
+    expect(result.current.install).toHaveProperty('id', mockInstall.id)
+    expect(result.current.install).toHaveProperty('name', mockInstall.name)
+    expect(result.current.isFetching).toBeFalsy()
+
+    // starting install refetch
+    await waitFor(
+      () => {
+        return expect(result.current.isFetching).toBeTruthy()
+      },
+      {
+        timeout: POLL_DURATION + 1000,
+      }
+    )
+
+    // finish install refetch
+    await waitFor(
+      () => {
+        return expect(result.current.isFetching).toBeFalsy()
+      },
+      {
+        timeout: POLL_DURATION + 1001,
+      }
+    )
+  },
+  POLL_DURATION * 1002
+)
