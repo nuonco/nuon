5bab94997d347bf5f35620307f9857477ae4b5e6
Jordan Acosta <jordan@nuon.co>
2024-06-17 23:22:14 -0700

    fix(infra/vercel): common fate installer was already deleted (#5012)

--- infra/vercel/common-fate.tf
+++
@@ -1,9 +0,0 @@
-resource "vercel_project" "common_fate_installer" {
-  name      = "common-fate-installer"
-  framework = "nextjs"
-
-  git_repository = {
-    type = "github"
-    repo = "nuonco-shared/common-fate-installer"
-  }
-}
