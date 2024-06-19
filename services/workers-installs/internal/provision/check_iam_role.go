e5910f0ea9262e5a52edab782cf1e1ef8361cce8
Jon Morehouse <MorehouseJ09@gmail.com>
2024-06-19 13:45:23 -0600

    chore: check iam access uses two-step config (#5034)
    fix: add two step access config
    
    Co-authored-by: nuonbot <bot@nuon.co>

--- services/workers-installs/internal/provision/check_iam_role.go
+++ services/workers-installs/internal/provision/check_iam_role.go
@@ -2,18 +2,27 @@ package provision
 
 import (
 	"context"
+	"fmt"
 
-	"github.com/powertoolsdev/mono/pkg/aws/credentials"
 	"go.temporal.io/sdk/temporal"
+
+	assumerole "github.com/powertoolsdev/mono/pkg/aws/assume-role"
+	"github.com/powertoolsdev/mono/pkg/aws/credentials"
 )
 
 type CheckIAMRoleRequest struct {
-	RoleARN string
+	RoleARN string `validate:"required"`
+
+	TwoStepConfig *assumerole.TwoStepConfig `validate:"required"`
 }
 
 type CheckIAMRoleResponse struct{}
 
 func (a *Activities) CheckIAMRole(ctx context.Context, req CheckIAMRoleRequest) (CheckIAMRoleResponse, error) {
+	if err := a.v.Struct(req); err != nil {
+		return CheckIAMRoleResponse{}, fmt.Errorf("invalid request: %w", err)
+	}
+
 	var resp CheckIAMRoleResponse
 	cfg := &credentials.Config{
 		AssumeRole: &credentials.AssumeRoleConfig{
