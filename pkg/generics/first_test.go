3a6cb4321dcea81904f097cc2cdcc296ce522385
sam boyer <sam@nuon.co>
2024-06-28 12:56:23 -0400

    chore: Clarify behavior of generics.First() (#5109)

--- pkg/generics/first_test.go
+++ pkg/generics/first_test.go
@@ -7,6 +7,7 @@ import (
 )
 
 func TestFirst(t *testing.T) {
+	var emptystr string
 	tests := map[string]struct {
 		fn       func() interface{}
 		expected interface{}
@@ -23,11 +24,29 @@ func TestFirst(t *testing.T) {
 			},
 			expected: true,
 		},
+		"ptrs": {
+			// Empty string value is returned because First relies on simple equality checks
+			// that compare pointer addresses
+			fn: func() any {
+				return First(&emptystr, ToPtr("foo"))
+			},
+			expected: &emptystr,
+		},
+		"nilptrs": {
+			// But a nil pointer in the zero index is skipped
+			fn: func() any {
+				return First(nil, &emptystr)
+			},
+			expected: &emptystr,
+		},
 	}
 	for name, tt := range tests {
 		t.Run(name, func(t *testing.T) {
 			res := tt.fn()
 			require.Equal(t, tt.expected, res)
+			if res != tt.expected {
+				t.Fatal("require.Equal succeeds but basic equality check fails")
+			}
 		})
 	}
 }
