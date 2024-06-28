ec0e74e67ebca114f1e62f5cc032cc1f5600c115
sam boyer <sam@nuon.co>
2024-06-28 12:56:23 -0400

    chore: Clarify behavior of generics.First() (#5109)

--- pkg/generics/first.go
+++ pkg/generics/first.go
@@ -1,5 +1,9 @@
 package generics
 
+// First returns the first non-empty value in the provided slice.
+//
+// If the type of T is a pointer, the first non-nil value is returned, even
+// if it is a zero value.
 func First[T comparable](vals ...T) T {
 	empty := *new(T)
 	for _, v := range vals {
