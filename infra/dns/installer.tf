d2c7ec609f085e13f3c096c93957278c54864979
Dat Boi Diego <fidiego@users.noreply.github.com>
2024-07-19 14:32:06 -0500

    feat: add cname entry for dev.installer-hosted (#5277)

---
+++ infra/dns/installer.tf
@@ -0,0 +1,10 @@
+// api alias
+resource "aws_route53_record" "dev_installer_hosted" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = "dev.installer-hosted"
+  type    = "CNAME"
+  ttl     = 300
+  records = [
+    "cname.vercel-dns.com."
+  ]
+}
