66f5441f890ee0dd66d92260e5c636d699cf7da3
Dat Boi Diego <fidiego@users.noreply.github.com>
2024-07-19 14:43:34 -0500

    feat: entries for *.dev.installer-hosted (#5278)

--- infra/dns/installer.tf
+++ infra/dns/installer.tf
@@ -8,3 +8,13 @@ resource "aws_route53_record" "dev_installer_hosted" {
     "cname.vercel-dns.com."
   ]
 }
+
+resource "aws_route53_record" "wildcard_dev_installer_hosted" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = "*.dev.installer-hosted"
+  type    = "CNAME"
+  ttl     = 300
+  records = [
+    "cname.vercel-dns.com."
+  ]
+}
