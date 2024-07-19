1ac7feda7db781f35f24efe65bde033834e41848
Dat Boi Diego <fidiego@users.noreply.github.com>
2024-07-19 14:58:32 -0500

    feat: ns records for wildcard subdomain workaround (#5279)
    https://vercel.com/guides/wildcard-domain-without-vercel-nameservers

--- infra/dns/installer.tf
+++ infra/dns/installer.tf
@@ -1,4 +1,6 @@
-// api alias
+// dev.installer-hosted alias
+// ns records for wildcard subdomain
+// txt records are in ./txt.tf
 resource "aws_route53_record" "dev_installer_hosted" {
   zone_id = aws_route53_zone.main.zone_id
   name    = "dev.installer-hosted"
@@ -16,5 +18,16 @@ resource "aws_route53_record" "wildcard_dev_installer_hosted" {
   ttl     = 300
   records = [
     "cname.vercel-dns.com."
+  ]
+}
+
+resource "aws_route53_record" "ns_wildcard_dev_installer_hosted" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = "_acme-challenge"
+  type    = "NS"
+  ttl     = 300
+  records = [
+    "ns1.vercel-dns.com.",
+    "ns2.vercel-dns.com."
   ]
 }
