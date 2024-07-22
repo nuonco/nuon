27ebee04f5d7782340fef26f16ae944797687f77
Nat Hamilton <info@nathamilton.com>
2024-07-22 14:03:47 -0400

    refactor(ui): removed unused install status utils code (#5289)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/utils/index.ts
+++ services/dashboard-ui/src/utils/index.ts
@@ -1,5 +1,3 @@
-import { TInstall, TInstallComponent, TSandboxRun } from '@/types'
-
 // general utils
 export const API_URL =
   (process?.env?.NEXT_PUBLIC_API_URL || process?.env?.NUON_API_URL) || 'https://api.nuon.co'
@@ -20,98 +18,6 @@ export function getFlagEmoji(countryCode = 'us') {
     .split('')
     .map((char) => 127397 + char.charCodeAt(0))
   return String.fromCodePoint(...codePoints)
-}
-
-// install status helpers
-type TStatus = { status: string; status_description: string }
-
-export function getSandboxStatus(runs: TSandboxRun[]): TStatus {
-  return {
-    status: runs?.[0]?.status || 'error',
-    status_description:
-      runs?.[0]?.status_description || "Sandbox isn't provisioned",
-  }
-}
-
-export function getInstallComponentStatus(
-  components: TInstallComponent[]
-): TStatus {
-  let status = {
-    status: 'waiting',
-    status_description: 'Waiting on components to deploy',
-  }
-
-  if (
-    components.some(
-      (c) =>
-        c?.install_deploys?.[0]?.status === 'failed' ||
-        c?.install_deploys?.[0]?.status === 'error'
-    )
-  ) {
-    status = {
-      status: 'failed',
-      status_description: 'Some components have failed to deploy',
-    }
-  }
-
-  if (components.every((c) => c?.install_deploys?.[0]?.status === 'active')) {
-    status = {
-      status: 'active',
-      status_description: 'All components are active',
-    }
-  }
-
-  if (components?.every((c) => c?.install_deploys?.length === 0)) {
-    status = {
-      status: 'waiting',
-      status_description: 'Components are not deployed',
-    }
-  }
-
-  return status
-}
-
-export function getInstallStatus(statuses: TStatus[]): TStatus {
-  let status: TStatus = {
-    status: 'waiting',
-    status_description: 'Install is waiting for something',
-  }
-
-  if (statuses.some((s) => s?.status === 'failed' || s?.status === 'error')) {
-    status = {
-      status: 'error',
-      status_description: 'Something has gone wrong',
-    }
-  }
-
-  if (statuses.every((s) => s?.status === 'active')) {
-    status = {
-      status: 'active',
-      status_description: 'Everything is working',
-    }
-  }
-
-  return status
-}
-
-export type TFullInstallStatus = Record<
-  'componentStatus' | 'installStatus' | 'sandboxStatus',
-  TStatus
->
-
-export function getFullInstallStatus(install: TInstall): TFullInstallStatus {
-  const sandboxStatus = getSandboxStatus(
-    (install?.install_sandbox_runs as Array<TSandboxRun>) || []
-  )
-  const componentStatus = getInstallComponentStatus(
-    (install?.install_components as Array<TInstallComponent>) || []
-  )
-
-  return {
-    componentStatus,
-    installStatus: getInstallStatus([sandboxStatus, componentStatus]),
-    sandboxStatus,
-  }
 }
 
 export * from './install-regions'
