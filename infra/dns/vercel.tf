ef9776ad3eb8f6d9ecb7aa06f82d220da49ab964
Dat Boi Diego <fidiego@users.noreply.github.com>
2024-07-22 14:57:37 -0600

    feat: ns records for *.stage.installers (#5292)

--- infra/dns/vercel.tf
+++ infra/dns/vercel.tf
@@ -7,6 +7,7 @@ resource "aws_route53_record" "vercel" {
     "vc-domain-verify=www.nuon.co,eb46ec2a8f783d42b56a",
     "vc-domain-verify=nuon.co,accce88f274d635e2146",
     "vc-domain-verify=docs.nuon.co,d405ffa1f9db2909d3bd",
-    "vc-domain-verify=stage.installers.nuon.co,f6c421d292fc664dd508"
+    "vc-domain-verify=stage.installers.nuon.co,f6c421d292fc664dd508",
+    "vc-domain-verify=*.stage.installers.nuon.co,19c51a544f53a328fb9b"
   ]
 }
