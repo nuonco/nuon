9a7f2acd32eb8b2d683dcf8ff46ae719ef587e30
Nat Hamilton <info@nathamilton.com>
2024-07-22 12:48:09 -0400

    feat(ui): added simple xterm and code editor components (#5265)
    * feat(ui): added simple xterm and code editor components
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat(ui): editable codeviewer
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat(ui): prevent codeeditor text wrapping
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat(ui): adds first iteration of otel log viewer
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat(ui): fix codeviewer width when empty
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    Co-authored-by: nuonbot <bot@nuon.co>

--- services/dashboard-ui/src/components/Logs.tsx
+++ services/dashboard-ui/src/components/Logs.tsx
@@ -1,4 +1,5 @@
 import classNames from 'classnames'
+import { DateTime } from 'luxon'
 import React, { type FC } from 'react'
 import { Code } from '@/components'
 import type { TWaypointLog } from '@/types'
@@ -15,7 +16,7 @@ export const Logs: FC<ILogs> = ({ logs }) => {
             // handle complete state
 
             return (
-              <span  key={ii}>
+              <span key={ii}>
                 {term?.Terminal?.events?.length
                   ? term?.Terminal?.events?.map((l, i) => {
                       let line = null
@@ -75,6 +76,55 @@ export const Logs: FC<ILogs> = ({ logs }) => {
             )
           })
         : 'no logs to show'}
+    </Code>
+  )
+}
+
+const LogLine: FC<{ line: any }> = ({ line }) => {
+  const lineStyle = 'tracking-wider text-[10px] leading-none'
+
+  return (
+    <span className="flex items-center justify-start gap-6 w-max">
+      <span
+        className={classNames('flex w-1.5 h-4', {
+          'bg-blue-300': line.ServityNumber <= 4,
+          'bg-gray-500': line?.SeverityNumber >= 5 && line?.SeverityNumber <= 8,
+          'bg-fuchsia-500':
+            line?.SeverityNumber >= 9 && line?.SeverityNumber <= 12,
+          'bg-yellow-500':
+            line?.SeverityNumber >= 13 && line?.SeverityNumber <= 16,
+          'bg-red-400':
+            line?.SeverityNumber >= 17 && line?.SeverityNumber <= 20,
+          'bg-red-600':
+            line?.SeverityNumber >= 21 && line?.SeverityNumber <= 24,
+        })}
+      />
+
+      <span className={lineStyle}>
+        {DateTime.fromMillis(line?.Timestamp).toLocaleString(
+          DateTime.DATETIME_MED_WITH_SECONDS
+        )}
+      </span>
+
+      <span className={lineStyle}>{line?.Resource?.['service.name']}</span>
+
+      <span className={lineStyle + ' font-semibold uppercase'}>
+        {line?.SeverityText || 'UNKOWN'}
+      </span>
+
+      <span className={lineStyle + ' text-gray-100/75'}>{line?.Body}</span>
+    </span>
+  )
+}
+
+export const OTELLogs: FC<{ logs?: Array<Record<string, unknown>> }> = ({
+  logs = [],
+}) => {
+  return (
+    <Code className="gap-4">
+      {logs.map((line) => (
+        <LogLine key={line?.timestamp?.toString()} line={line} />
+      ))}
     </Code>
   )
 }
