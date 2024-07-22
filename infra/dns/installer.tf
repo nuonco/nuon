bcad32c75677f99316c338dc64edd7774f9e7e2b
Dat Boi Diego <fidiego@users.noreply.github.com>
2024-07-22 14:52:35 -0600

    feat: cname for stage installer (#5291)

--- infra/dns/installer.tf
+++ infra/dns/installer.tf
@@ -1,9 +1,9 @@
 resource "aws_route53_record" "installers-stage-txt-records" {
   zone_id = aws_route53_zone.main.zone_id
-  name    = ""
-  type    = "TXT"
+  name    = "stage.installers"
+  type    = "CNAME"
   ttl     = 300
   records = [
-    "vc-domain-verify=stage.installers.nuon.co,f6c421d292fc664dd508"
+    "cname.vercel-dns.com."
   ]
 }
