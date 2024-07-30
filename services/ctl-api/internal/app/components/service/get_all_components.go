0d10237a507c75fa8f21944d32df21e2c55935e7
Rob <alphavibe@gmail.com>
2024-07-30 15:37:26 -0700

    fix: preload component dependencies (#5389)

--- services/ctl-api/internal/app/components/service/get_all_components.go
+++ services/ctl-api/internal/app/components/service/get_all_components.go
@@ -31,6 +31,7 @@ func (s *service) getAllComponents(ctx context.Context) ([]*app.Component, error
 	var components []*app.Component
 	res := s.db.WithContext(ctx).
 		Order("created_at desc").
+		Preload("Dependencies").
 		Find(&components)
 	if res.Error != nil {
 		return nil, fmt.Errorf("unable to get all components: %w", res.Error)
