b1ee6457aceb3c741569d22924dbb4032d55ab70
Jon Morehouse <MorehouseJ09@gmail.com>
2024-07-31 14:46:28 -0600

    fix: sandbox canaries (#5407)
    Co-authored-by: nuonbot <bot@nuon.co>

--- services/workers-canary/install-cli.sh
+++ services/workers-canary/install-cli.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+if [[ "${NUON_LOCAL_CLI}" ]]; then
+  echo "skipping installing nuon, and using local instance."
+  exit 0
+fi
+
 echo "y" | /bin/bash -c "$(curl -fsSL https://nuon-artifacts.s3.us-west-2.amazonaws.com/cli/install.sh)"
