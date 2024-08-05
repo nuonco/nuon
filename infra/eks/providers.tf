2ad1c674ed561e25570ad3eae9113f0b55d6d9d9
Dat Boi Diego <fidiego@users.noreply.github.com>
2024-08-05 16:00:49 -0500

    chore: disable debug (#5477)

--- infra/eks/providers.tf
+++ infra/eks/providers.tf
@@ -64,7 +64,6 @@ provider "kubernetes" {
 }
 
 provider "helm" {
-  debug = true
   kubernetes {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
