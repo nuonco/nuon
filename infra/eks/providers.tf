68971f03f27634cd4639ea904e50a8b372c0da5b
Dat Boi Diego <fidiego@users.noreply.github.com>
2024-08-05 15:15:53 -0500

    chore: enable debug for helm for a hot sec (#5475)

--- infra/eks/providers.tf
+++ infra/eks/providers.tf
@@ -64,6 +64,7 @@ provider "kubernetes" {
 }
 
 provider "helm" {
+  debug = true
   kubernetes {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
