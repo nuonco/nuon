fb6f07402a6c65ed82835694334d6d6a3a625603
Rob <alphavibe@gmail.com>
2024-08-07 06:46:16 -0700

    feat: use anyof to validate against source schema or resource schema (#5506)
    Co-authored-by: nuonbot <bot@nuon.co>

--- pkg/config/terraform_module_component.go
+++ pkg/config/terraform_module_component.go
@@ -4,7 +4,7 @@ package config
 type TerraformModuleComponentConfig struct {
 	TerraformVersion string `mapstructure:"terraform_version" jsonschema:"required"`
 
-	EnvVarMap map[string]string `mapstructure:"env_vars,omitempty" jsonschema:"required"`
+	EnvVarMap map[string]string `mapstructure:"env_vars,omitempty"`
 	VarsMap   map[string]string `mapstructure:"vars,omitempty" jsonschema:"required"`
 
 	PublicRepo    *PublicRepoConfig    `mapstructure:"public_repo,omitempty" jsonschema:"oneof_required=connected_repo"`
