f9710da466ca83f528e87abd4c9e5172d3494f3c
Jon Morehouse <MorehouseJ09@gmail.com>
2024-08-09 11:31:05 -0600

    fix: sync dependencies (#5534)
    Co-authored-by: nuonbot <bot@nuon.co>

--- pkg/config/sync/steps.go
+++ pkg/config/sync/steps.go
@@ -43,12 +43,12 @@ func (s *sync) syncSteps() ([]syncStep, error) {
 	for _, comp := range s.cfg.Components {
 		// thanks russ cox
 		obj := comp
-		obj.Dependencies = deps
 
 		resourceName := fmt.Sprintf("component-%s", obj.Name)
 		steps = append(steps, syncStep{
 			Resource: resourceName,
 			Method: func(ctx context.Context) error {
+				obj.Dependencies = deps
 				compID, err := s.syncComponent(ctx, resourceName, obj)
 				if err != nil {
 					return err
