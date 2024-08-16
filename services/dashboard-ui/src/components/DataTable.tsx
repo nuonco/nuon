b446186c331b2c41a87f68f67fea6c4b0858d154
Nat Hamilton <info@nathamilton.com>
2024-08-16 13:02:52 -0400

    feat: UI data table (#5640)
    * feat(ui): Added basic DataTable react component
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat(ui): Added datatable to installs/apps view
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    Co-authored-by: nuonbot <bot@nuon.co>

---
+++ services/dashboard-ui/src/components/DataTable.tsx
@@ -0,0 +1,71 @@
+'use client'
+
+import React, { type FC, useState } from 'react'
+import { FiMoreVertical } from 'react-icons/fi'
+import { Link, Text } from '@/components'
+
+export interface IDataTable {
+  headers: Array<string>
+  initData: Array<Array<string>>
+}
+
+export const DataTable: FC<IDataTable> = ({ headers, initData }) => {
+  const [data, setData] = useState(initData)
+
+  return (
+    <div className="flex flex-col gap-8">
+      <div className="flex-auto">
+        <input
+          className="rounded bg-transparent border px-2 py-1"
+          onInput={(e): void => {
+            const val = e.currentTarget.value
+            const filteredData = initData.reduce(
+              (acc: Array<Array<string>>, row) => {
+                const match = row.some((item) => item.includes(val))
+                if (match) {
+                  acc.push(row)
+                }
+                return acc
+              },
+              []
+            )
+
+            setData(filteredData.length ? filteredData : initData)
+          }}
+          placeholder="Search"
+          type="search"
+        />
+      </div>
+
+      <table className="table-auto w-full">
+        <thead>
+          <tr className="border-b text-left">
+            {headers.map((header, i) => (
+              <th className="text-sm" key={`header-${i}`}>
+                {header}
+              </th>
+            ))}
+            <th></th>
+          </tr>
+        </thead>
+        <tbody className="divide-y">
+          {data.map((row, i) => (
+            <tr key={`row-${i}`}>
+              {row.map((td, i) => (
+                <td className="py-2" key={`cell-${i}`}>
+                  {i + 1 !== row.length ? (
+                    <Text variant="caption">{td}</Text>
+                  ) : (
+                    <Link className="text-gray-950 dark:text-gray-50" href={td}>
+                      <FiMoreVertical />
+                    </Link>
+                  )}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
