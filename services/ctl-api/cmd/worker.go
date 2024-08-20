1bad3f730404309582f7ab2bd468206c819be27e
Dat Boi Diego <fidiego@users.noreply.github.com>
2024-08-20 12:58:03 -0500

    feat: parameterize the worker namespaces (#5679)

--- services/ctl-api/cmd/worker.go
+++ services/ctl-api/cmd/worker.go
@@ -16,6 +16,8 @@ import (
 	"gorm.io/gorm"
 )
 
+var namespace string
+
 func (c *cli) registerWorker() error {
 	cmd := &cobra.Command{
 		Use:   "worker",
@@ -23,48 +25,64 @@ func (c *cli) registerWorker() error {
 		Run:   c.runWorker,
 	}
 	rootCmd.AddCommand(cmd)
+	rootCmd.PersistentFlags().StringVarP(&namespace, "namespace", "n", "all", "namespace defines the namespace whose workers to run. e.g. all, orgs, apps, components, installs, releases.")
 	return nil
 }
 
 func (c *cli) runWorker(cmd *cobra.Command, _ []string) {
 	providers := []fx.Option{
 		fx.Invoke(func(*gorm.DB) {}),
-
-		// orgs worker
-		fx.Provide(orgsactivities.New),
-		fx.Provide(orgsworker.NewWorkflows),
-		fx.Provide(orgsworker.New),
-		fx.Invoke(func(*orgsworker.Worker) {
-		}),
-
-		// apps worker
-		fx.Provide(appsactivities.New),
-		fx.Provide(appsworker.NewWorkflows),
-		fx.Provide(appsworker.New),
-		fx.Invoke(func(*appsworker.Worker) {
-		}),
-
-		// components worker
-		fx.Provide(componentsactivities.New),
-		fx.Provide(componentsworker.NewWorkflows),
-		fx.Provide(componentsworker.New),
-		fx.Invoke(func(*componentsworker.Worker) {
-		}),
-
-		// installs worker
-		fx.Provide(installsactivities.New),
-		fx.Provide(installsworker.NewWorkflows),
-		fx.Provide(installsworker.New),
-		fx.Invoke(func(*installsworker.Worker) {
-		}),
-
-		// releases worker
-		fx.Provide(releasesactivities.New),
-		fx.Provide(releasesworker.NewWorkflows),
-		fx.Provide(releasesworker.New),
-		fx.Invoke(func(*releasesworker.Worker) {
-		}),
 	}
+
+	// orgs worker
+	if namespace == "all" || namespace == "orgs" {
+		providers = append(providers,
+			fx.Provide(orgsactivities.New),
+			fx.Provide(orgsworker.NewWorkflows),
+			fx.Provide(orgsworker.New),
+			fx.Invoke(func(*orgsworker.Worker) {}),
+		)
+	}
+
+	// apps worker
+	if namespace == "all" || namespace == "apps" {
+		providers = append(providers,
+			fx.Provide(appsactivities.New),
+			fx.Provide(appsworker.NewWorkflows),
+			fx.Provide(appsworker.New),
+			fx.Invoke(func(*appsworker.Worker) {}),
+		)
+	}
+
+	// components worker
+	if namespace == "all" || namespace == "components" {
+		providers = append(providers,
+			fx.Provide(componentsactivities.New),
+			fx.Provide(componentsworker.NewWorkflows),
+			fx.Provide(componentsworker.New),
+			fx.Invoke(func(*componentsworker.Worker) {}),
+		)
+	}
+
+	// installs worker
+	if namespace == "all" || namespace == "installs" {
+		providers = append(providers,
+			fx.Provide(installsactivities.New),
+			fx.Provide(installsworker.NewWorkflows),
+			fx.Provide(installsworker.New),
+			fx.Invoke(func(*installsworker.Worker) {}),
+		)
+	}
+
+	if namespace == "all" || namespace == "releases" {
+		providers = append(providers,
+			fx.Provide(releasesactivities.New),
+			fx.Provide(releasesworker.NewWorkflows),
+			fx.Provide(releasesworker.New),
+			fx.Invoke(func(*releasesworker.Worker) {}),
+		)
+	}
+
 	providers = append(providers, c.providers()...)
 	fx.New(providers...).Run()
 }
