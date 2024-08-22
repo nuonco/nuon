8e8b3dd9e30a836aaf1f19016ef69939914a7908
Rob <alphavibe@gmail.com>
2024-08-22 13:14:14 -0700

    feat: healthz endpoint (#5739)
    feat: healtz endpoint

--- services/ctl-api/internal/health/health.go
+++ services/ctl-api/internal/health/health.go
@@ -3,16 +3,19 @@ package health
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/powertoolsdev/mono/services/ctl-api/internal"
+	"gorm.io/gorm"
 )
 
 type Service struct {
 	cfg *internal.Config
+	db  *gorm.DB
 }
 
 func (s *Service) RegisterRoutes(api *gin.Engine) error {
 	api.GET("/livez", s.GetLivezHandler)
 	api.GET("/readyz", s.GetReadyzHandler)
 	api.GET("/version", s.GetVersionHandler)
+	api.GET("/healthz", s.GetHealthzHandler)
 
 	return nil
 }
@@ -21,12 +24,14 @@ func (s *Service) RegisterInternalRoutes(api *gin.Engine) error {
 	api.GET("/livez", s.GetLivezHandler)
 	api.GET("/readyz", s.GetReadyzHandler)
 	api.GET("/version", s.GetVersionHandler)
+	api.GET("/healthz", s.GetHealthzHandler)
 
 	return nil
 }
 
-func New(cfg *internal.Config) (*Service, error) {
+func New(cfg *internal.Config, db *gorm.DB) (*Service, error) {
 	return &Service{
 		cfg: cfg,
+		db:  db,
 	}, nil
 }
