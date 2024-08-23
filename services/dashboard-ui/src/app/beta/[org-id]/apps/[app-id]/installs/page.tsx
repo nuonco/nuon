7e461e76ce93eccfcb19b794ab156b2aec321a34
Nat Hamilton <info@nathamilton.com>
2024-08-23 11:13:31 -0400

    feat(ui): basic table for app components and app installs (#5744)
    feat(ui): app installs and components basic table
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/app/beta/[org-id]/apps/[app-id]/installs/page.tsx
+++ services/dashboard-ui/src/app/beta/[org-id]/apps/[app-id]/installs/page.tsx
@@ -1,4 +1,4 @@
-import { Card, Heading, Text, Link } from '@/components'
+import { DataTable } from '@/components'
 import { getAppInstalls } from '@/lib'
 
 export default async function AppInstalls({ params }) {
@@ -6,13 +6,25 @@ export default async function AppInstalls({ params }) {
   const orgId = params?.['org-id'] as string
   const installs = await getAppInstalls({ appId, orgId })
 
+  const tableData = installs.reduce((acc, install) => {
+    acc.push([
+      install.id,
+      install.name,
+      install.azure_account?.location ? 'AWS' : 'Azure',
+      install.sandbox_status,
+      install.runner_status,
+      `/beta/${orgId}/installs/${install.id}`,
+    ])
+
+    return acc
+  }, [])
+
   return (
-    <>
-      {installs.map((install) => (
-        <Link key={install.id} href={`/beta/${orgId}/installs/${install.id}`}>
-          {install.name}
-        </Link>
-      ))}
-    </>
+    <section className="px-6 py-8">
+      <DataTable
+        headers={['ID', 'Name', 'Platform', 'Sandbox', 'Runner']}
+        initData={tableData}
+      />
+    </section>
   )
 }
