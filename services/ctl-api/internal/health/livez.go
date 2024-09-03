89d0b50c986f368617441b4d7a6d58afecedad95
Rob <alphavibe@gmail.com>
2024-09-03 12:43:31 -0700

    feat: livez endpoint checks for temporal reachability (#5830)
    * feat: livez endpoint checks for temporal reachability
    
    * feat: revert artifact version pin

--- services/ctl-api/internal/health/livez.go
+++ services/ctl-api/internal/health/livez.go
@@ -1,13 +1,31 @@
 package health
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
+	"go.temporal.io/sdk/client"
 )
 
 func (s *Service) GetLivezHandler(ctx *gin.Context) {
+	// NOTE: didn't find a ping command with gorm. For now will try fetching from org.
+	org := app.Org{}
+	dbRes := s.db.WithContext(ctx).First(&org)
+	if dbRes.Error != nil {
+		ctx.Error(fmt.Errorf("unable to query database: %w", dbRes.Error))
+		return
+	}
+
+	_, err := s.tclient.CheckHealth(ctx, &client.CheckHealthRequest{})
+	if err != nil {
+		ctx.Error(fmt.Errorf("unable to check temporal health: %w", err))
+		return
+	}
+
 	ctx.JSON(http.StatusOK, map[string]interface{}{
 		"status": "ok",
 	})
+
 }
