dc93ac8023ae8bff5e907abb3571294b45798f13
Nat Hamilton <info@nathamilton.com>
2024-09-10 11:15:17 -0400

    feat: UI nav typography (#5895)
    * feat(ui): added new icon pack
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat(ui): updated typography and nav styles
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/app/page.tsx
+++ services/dashboard-ui/src/app/page.tsx
@@ -6,14 +6,14 @@ export default function Home() {
     <Dashboard>
       <main className="flex h-full gap-6">
         <div className="flex flex-col gap-6 p-0 md:p-12 lg:p-24 lg:max-w-2xl">
-          <Heading variant="title">BYOC for everyone.</Heading>
-          <Text variant="base">
+          <Heading variant="title" level={1}>BYOC for everyone.</Heading>
+          <Text className="text-lg leading-relaxed">
             Offer Bring Your Own Cloud in minutes, unlocking new customers,
             product capabilities and revenue.
           </Text>
           <a
             className="flex flex-initial items-center w-fit gap-1 bg-gradient-to-r
-hover:scale-105 focus:scale-105 active:scale-95 from-indigo-500 via-purple-500 to-fuchsia-400 text-slate-50 dark:text-slate-950 drop-shadow-sm px-4 py-2 rounded-sm text-xl transition-transform duration-75"
+hover:scale-105 focus:scale-105 active:scale-95 from-indigo-500 via-purple-500 to-fuchsia-400 text-slate-50 dark:text-slate-950 drop-shadow-sm px-4 py-2 rounded-md text-lg transition-transform duration-75"
             href="/api/auth/login?returnTo=/dashboard"
           >
             Login to get started <GoArrowRight />
--- services/dashboard-ui/src/components/Link.tsx
+++ services/dashboard-ui/src/components/Link.tsx
@@ -1,20 +1,35 @@
 import classNames from 'classnames'
 import NextLink from 'next/link'
-import  { type FC, HTMLAttributes } from 'react'
+import { type FC, HTMLAttributes } from 'react'
 
 export interface ILink extends HTMLAttributes<HTMLAnchorElement> {
   href: string
   target?: string
+  isActive?: boolean
   isExternal?: boolean
+  variant?: 'default' | 'breadcrumb'
 }
 
-export const Link: FC<ILink> = ({ className, children, href, ...props }) => {
+export const Link: FC<ILink> = ({
+  className,
+  children,
+  href,
+  isActive = false,
+  isExternal = false,
+  variant = 'default',
+  ...props
+}) => {
   return (
     <NextLink
-      className={classNames(
-        'flex flex-initial items-center w-fit gap-1 text-primary-700 hover:text-primary-600 focus:text-primary-600 active:text-primary-800 dark:text-primary-500 dark:hover:text-primary-400 dark:focus:text-primary-400 dark:active:text-primary-600',
-        { [`${className}`]: Boolean(className) }
-      )}
+      className={classNames('flex flex-initial items-center w-fit gap-1', {
+        'text-primary-700 hover:text-primary-600 focus:text-primary-600 active:text-primary-800 dark:text-primary-500 dark:hover:text-primary-400 dark:focus:text-primary-400 dark:active:text-primary-600':
+          variant === 'default',
+        'text-cool-grey-600 hover:text-primary-600 focus:text-primary-600 active:text-primary-800 dark:text-cool-grey-400 dark:hover:text-primary-400 dark:focus:text-primary-400 dark:active:text-primary-600':
+          variant === 'breadcrumb',
+        'text-cool-grey-950 dark:text-white':
+          variant === 'breadcrumb' && isActive,
+        [`${className}`]: Boolean(className),
+      })}
       href={href}
       {...props}
     >
