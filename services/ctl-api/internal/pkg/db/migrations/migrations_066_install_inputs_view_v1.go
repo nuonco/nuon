4a3ef9b4149f65c7cb5aa7505c6f279c4c26d6cb
Dat Boi Diego <fidiego@users.noreply.github.com>
2024-09-10 19:50:33 -0500

    fix: mask sensitive values (#5860)
    feat: return redacted values through the use of a view
    
    Co-authored-by: nuonbot <bot@nuon.co>

---
+++ services/ctl-api/internal/pkg/db/migrations/migrations_066_install_inputs_view_v1.go
@@ -0,0 +1,17 @@
+package migrations
+
+import (
+	"context"
+	_ "embed"
+)
+
+//go:embed install_inputs_view_v1.sql
+var installInputsViewV1 string
+
+func (a *Migrations) migration066InstallInputsViewV1(ctx context.Context) error {
+	if res := a.db.WithContext(ctx).Exec(installInputsViewV1); res.Error != nil {
+		return res.Error
+	}
+
+	return nil
+}
