450840b40a01016ece78cc63fdd1b667d92f5ed9
Dat Boi Diego <fidiego@users.noreply.github.com>
2024-09-13 13:16:34 -0500

    feat: bring gorm/driver/clickhouse deps into mono (#5909)
    * feat: mono: vendor gorm.io/driver/clickhouse
    
    we need to make adjustments for the automigrator to support nested
    fields so we'll have to vendor it, patch it, test it, then send our
    changes back out.
    
    * feat: ctl-api: use our vendored gorm clickhouse driver
    
    * feat: clickhouse driver: add support for nested columns
    
    the goal is to be able to AutoMigrate our clickhouse models that use
    nested columns. this functionality was missing.

---
+++ pkg/gorm/clickhouse/create_test.go
@@ -0,0 +1,87 @@
+package clickhouse_test
+
+import (
+	"slices"
+	"testing"
+
+	"gorm.io/gorm/utils/tests"
+)
+
+func TestCreate(t *testing.T) {
+	user := User{ID: 1, Name: "create", FirstName: "zhang", LastName: "jinzhu", Age: 18, Active: true, Salary: 8.8888, Attrs: map[string]string{
+		"a": "a",
+		"b": "b",
+	}}
+
+	if err := DB.Create(&user).Error; err != nil {
+		t.Fatalf("failed to create user, got error %v", err)
+	}
+
+	var result User
+	if err := DB.Find(&result, user.ID).Error; err != nil {
+		t.Fatalf("failed to query user, got error %v", err)
+	}
+
+	tests.AssertEqual(t, result, user)
+
+	type partialUser struct {
+		Name string
+	}
+	var partialResult partialUser
+	if err := DB.Raw("select * from users where id = ?", user.ID).Scan(&partialResult).Error; err != nil {
+		t.Fatalf("failed to query partial, got error %v", err)
+	}
+
+	var names []string
+	if err := DB.Select("name").Model(&User{}).Find(&names).Error; err != nil {
+		t.Fatalf("failed to query user, got error %v", err)
+	}
+
+	if !slices.Contains(names, user.Name) {
+		t.Errorf("name should be included in the result")
+	}
+}
+
+func TestBatchCreate(t *testing.T) {
+	users := []User{
+		{ID: 11, Name: "batch_create_1", FirstName: "zhang", LastName: "jinzhu", Age: 18, Active: true, Salary: 6},
+		{ID: 12, Name: "batch_create_2", FirstName: "zhang", LastName: "jinzhu", Age: 18, Active: false, Salary: 6.12},
+		{ID: 13, Name: "batch_create_3", FirstName: "zhang", LastName: "jinzhu", Age: 18, Active: true, Salary: 6.1234},
+		{ID: 14, Name: "batch_create_4", FirstName: "zhang", LastName: "jinzhu", Age: 18, Active: false, Salary: 6.123456},
+	}
+
+	if err := DB.Create(&users).Error; err != nil {
+		t.Fatalf("failed to create users, got error %v", err)
+	}
+
+	var results []User
+	DB.Find(&results)
+
+	for _, u := range users {
+		var result User
+		if err := DB.Find(&result, u.ID).Error; err != nil {
+			t.Fatalf("failed to query user, got error %v", err)
+		}
+
+		tests.AssertEqual(t, result, u)
+	}
+}
+
+func TestCreateWithMap(t *testing.T) {
+	user := User{ID: 122, Name: "create2", FirstName: "zhang", LastName: "jinzhu", Age: 18, Active: true, Salary: 6.6666}
+
+	if err := DB.Table("users").Create(&map[string]interface{}{
+		"id": user.ID, "name": user.Name, "first_name": user.FirstName, "last_name": user.LastName, "age": user.Age, "active": user.Active, "salary": user.Salary,
+	}).Error; err != nil {
+		t.Fatalf("failed to create user, got error %v", err)
+	}
+
+	var result User
+	if err := DB.Find(&result, user.ID).Error; err != nil {
+		t.Fatalf("failed to query user, got error %v", err)
+	}
+
+	user.CreatedAt = result.CreatedAt
+	user.UpdatedAt = result.UpdatedAt
+	tests.AssertEqual(t, result, user)
+}
