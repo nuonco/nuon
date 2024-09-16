fdefd6ceb8f1057bf6164f384fe78a2446b1e180
Nat Hamilton <info@nathamilton.com>
2024-09-16 10:04:45 -0400

    feat(ui): auth redirects for new pages (#5945)
    * feat(ui): added auth redirects for new pages
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat(ui): install inputs use redacted values now
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/app/beta/[org-id]/apps/[app-id]/components/page.tsx
+++ services/dashboard-ui/src/app/beta/[org-id]/apps/[app-id]/components/page.tsx
@@ -1,3 +1,4 @@
+import { withPageAuthRequired } from '@auth0/nextjs-auth0'
 import {
   AppComponentsTable,
   DashboardContent,
@@ -6,51 +7,56 @@ import {
 } from '@/components'
 import { getApp, getAppComponents, getComponentConfig, getOrg } from '@/lib'
 
-export default async function AppComponents({ params }) {
-  const appId = params?.['app-id'] as string
-  const orgId = params?.['org-id'] as string
-  const subNavLinks: Array<TLink> = [
-    { href: `/beta/${orgId}/apps/${appId}`, text: 'Config' },
-    { href: `/beta/${orgId}/apps/${appId}/components`, text: 'Components' },
-    { href: `/beta/${orgId}/apps/${appId}/installs`, text: 'Installs' },
-  ]
+export default withPageAuthRequired(
+  async function AppComponents({ params }) {
+    const appId = params?.['app-id'] as string
+    const orgId = params?.['org-id'] as string
+    const subNavLinks: Array<TLink> = [
+      { href: `/beta/${orgId}/apps/${appId}`, text: 'Config' },
+      { href: `/beta/${orgId}/apps/${appId}/components`, text: 'Components' },
+      { href: `/beta/${orgId}/apps/${appId}/installs`, text: 'Installs' },
+    ]
 
-  const [app, components, org] = await Promise.all([
-    getApp({ appId, orgId }),
-    getAppComponents({ appId, orgId }),
-    getOrg({ orgId }),
-  ])
-  const hydratedComponents = await Promise.all(
-    components.map(async (comp, _, arr) => {
-      const config = await getComponentConfig({ componentId: comp.id, orgId })
-      const deps = arr.filter((c) => comp.dependencies?.some((d) => d === c.id))
+    const [app, components, org] = await Promise.all([
+      getApp({ appId, orgId }),
+      getAppComponents({ appId, orgId }),
+      getOrg({ orgId }),
+    ])
+    const hydratedComponents = await Promise.all(
+      components.map(async (comp, _, arr) => {
+        const config = await getComponentConfig({ componentId: comp.id, orgId })
+        const deps = arr.filter((c) =>
+          comp.dependencies?.some((d) => d === c.id)
+        )
 
-      return {
-        ...comp,
-        config,
-        deps,
-      }
-    })
-  )
+        return {
+          ...comp,
+          config,
+          deps,
+        }
+      })
+    )
 
-  return (
-    <DashboardContent
-      breadcrumb={[
-        { href: `/beta/${org.id}`, text: org.name },
-        { href: `/beta/${org.id}/apps`, text: 'Apps' },
-        { href: `/beta/${org.id}/apps/${app.id}`, text: app.name },
-      ]}
-      heading={app.name}
-      headingUnderline={app.id}
-      meta={<SubNav links={subNavLinks} />}
-    >
-      <section className="px-6 py-8">
-        <AppComponentsTable
-          components={hydratedComponents}
-          appId={appId}
-          orgId={orgId}
-        />
-      </section>
-    </DashboardContent>
-  )
-}
+    return (
+      <DashboardContent
+        breadcrumb={[
+          { href: `/beta/${org.id}`, text: org.name },
+          { href: `/beta/${org.id}/apps`, text: 'Apps' },
+          { href: `/beta/${org.id}/apps/${app.id}`, text: app.name },
+        ]}
+        heading={app.name}
+        headingUnderline={app.id}
+        meta={<SubNav links={subNavLinks} />}
+      >
+        <section className="px-6 py-8">
+          <AppComponentsTable
+            components={hydratedComponents}
+            appId={appId}
+            orgId={orgId}
+          />
+        </section>
+      </DashboardContent>
+    )
+  },
+  { returnTo: '/dashboard' }
+)
--- services/dashboard-ui/src/app/beta/[org-id]/installs/[install-id]/components/page.tsx
+++ services/dashboard-ui/src/app/beta/[org-id]/installs/[install-id]/components/page.tsx
@@ -1,3 +1,4 @@
+import { withPageAuthRequired } from '@auth0/nextjs-auth0'
 import {
   DashboardContent,
   InstallStatus,
@@ -15,77 +16,83 @@ import {
   getOrg,
 } from '@/lib'
 
-export default async function InstallComponents({ params }) {
-  const orgId = params?.['org-id'] as string
-  const installId = params?.['install-id'] as string
-  const subNavLinks: Array<TLink> = [
-    { href: `/beta/${orgId}/installs/${installId}`, text: 'Status' },
-    {
-      href: `/beta/${orgId}/installs/${installId}/components`,
-      text: 'Components',
-    },
-  ]
+export default withPageAuthRequired(
+  async function InstallComponents({ params }) {
+    const orgId = params?.['org-id'] as string
+    const installId = params?.['install-id'] as string
+    const subNavLinks: Array<TLink> = [
+      { href: `/beta/${orgId}/installs/${installId}`, text: 'Status' },
+      {
+        href: `/beta/${orgId}/installs/${installId}/components`,
+        text: 'Components',
+      },
+    ]
 
-  const [install, org] = await Promise.all([
-    getInstall({ orgId, installId }),
-    getOrg({ orgId }),
-  ])
+    const [install, org] = await Promise.all([
+      getInstall({ orgId, installId }),
+      getOrg({ orgId }),
+    ])
 
-  const hydratedInstallComponents = await Promise.all(
-    install.install_components.map(async (comp, _, arr) => {
-      const build = await getBuild({
-        buildId: comp.install_deploys?.[0]?.build_id,
-        orgId,
+    const hydratedInstallComponents = await Promise.all(
+      install.install_components.map(async (comp, _, arr) => {
+        const build = await getBuild({
+          buildId: comp.install_deploys?.[0]?.build_id,
+          orgId,
+        })
+        const config = await getComponentConfig({
+          componentId: comp.component.id,
+          componentConfigId: build.component_config_connection_id,
+          orgId,
+        })
+        const appComponent = await getComponent({
+          componentId: comp.component_id,
+          orgId,
+        })
+        const deps = arr.filter((c) =>
+          appComponent.dependencies?.some((d) => d === c.component_id)
+        )
+
+        return {
+          ...comp,
+          build,
+          config,
+          deps,
+        }
       })
-      const config = await getComponentConfig({
-        componentId: comp.component.id,
-        componentConfigId: build.component_config_connection_id,
-        orgId,
-      })
-      const appComponent = await getComponent({
-        componentId: comp.component_id,
-        orgId,
-      })
-      const deps = arr.filter((c) =>
-        appComponent.dependencies?.some((d) => d === c.component_id)
-      )
+    )
 
-      return {
-        ...comp,
-        build,
-        config,
-        deps,
-      }
-    })
-  )
-
-  return (
-    <DashboardContent
-      breadcrumb={[
-        { href: `/beta/${org.id}`, text: org.name },
-        { href: `/beta/${org.id}/installs`, text: 'Installs' },
-        { href: `/beta/${org.id}/installs/${install.id}`, text: install.name },
-      ]}
-      heading={install.name}
-      headingUnderline={install.id}
-      statues={
-        <div>
-          <InstallProvider initInstall={install}>
-            <InstallStatus />
-          </InstallProvider>
-        </div>
-      }
-      meta={<SubNav links={subNavLinks} />}
-    >
-      <section className="px-6 py-8">
-        <InstallComponentsTable
-          installComponents={
-            hydratedInstallComponents as Array<TDataInstallComponent>
-          }
-          installId={installId}
-          orgId={orgId}
-        />
-      </section>
-    </DashboardContent>
-  )
-}
+    return (
+      <DashboardContent
+        breadcrumb={[
+          { href: `/beta/${org.id}`, text: org.name },
+          { href: `/beta/${org.id}/installs`, text: 'Installs' },
+          {
+            href: `/beta/${org.id}/installs/${install.id}`,
+            text: install.name,
+          },
+        ]}
+        heading={install.name}
+        headingUnderline={install.id}
+        statues={
+          <div>
+            <InstallProvider initInstall={install}>
+              <InstallStatus />
+            </InstallProvider>
+          </div>
+        }
+        meta={<SubNav links={subNavLinks} />}
+      >
+        <section className="px-6 py-8">
+          <InstallComponentsTable
+            installComponents={
+              hydratedInstallComponents as Array<TDataInstallComponent>
+            }
+            installId={installId}
+            orgId={orgId}
+          />
+        </section>
+      </DashboardContent>
+    )
+  },
+  { returnTo: '/dashboard' }
+)
