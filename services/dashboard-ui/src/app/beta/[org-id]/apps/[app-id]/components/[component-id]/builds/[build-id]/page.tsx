2d348c69540275a4fdff29692ad5f27c05a83de9
Nat Hamilton <info@nathamilton.com>
2024-09-16 10:04:45 -0400

    feat(ui): auth redirects for new pages (#5945)
    * feat(ui): added auth redirects for new pages
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat(ui): install inputs use redacted values now
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/app/beta/[org-id]/apps/[app-id]/components/[component-id]/builds/[build-id]/page.tsx
+++ services/dashboard-ui/src/app/beta/[org-id]/apps/[app-id]/components/[component-id]/builds/[build-id]/page.tsx
@@ -1,3 +1,4 @@
+import { withPageAuthRequired } from '@auth0/nextjs-auth0'
 import { FiCloud, FiClock } from 'react-icons/fi'
 import {
   DashboardContent,
@@ -17,126 +18,129 @@ import {
   getOrg,
 } from '@/lib'
 
-export default async function AppComponent({ params }) {
-  const appId = params?.['app-id'] as string
-  const buildId = params?.['build-id'] as string
-  const componentId = params?.['component-id'] as string
-  const orgId = params?.['org-id'] as string
+export default withPageAuthRequired(
+  async function AppComponent({ params }) {
+    const appId = params?.['app-id'] as string
+    const buildId = params?.['build-id'] as string
+    const componentId = params?.['component-id'] as string
+    const orgId = params?.['org-id'] as string
 
-  const build = await getBuild({ buildId, orgId })
-  const [app, component, componentConfig, org] = await Promise.all([
-    getApp({ appId, orgId }),
-    getComponent({ componentId, orgId }),
-    getComponentConfig({
-      componentId,
-      componentConfigId: build.component_config_connection_id,
-      orgId,
-    }),
-    getOrg({ orgId }),
-  ])
+    const build = await getBuild({ buildId, orgId })
+    const [app, component, componentConfig, org] = await Promise.all([
+      getApp({ appId, orgId }),
+      getComponent({ componentId, orgId }),
+      getComponentConfig({
+        componentId,
+        componentConfigId: build.component_config_connection_id,
+        orgId,
+      }),
+      getOrg({ orgId }),
+    ])
 
-  return (
-    <DashboardContent
-      breadcrumb={[
-        { href: `/beta/${org.id}`, text: org.name },
-        { href: `/beta/${org.id}/apps`, text: 'Apps' },
-        { href: `/beta/${org.id}/apps/${app.id}`, text: app.name },
-        {
-          href: `/beta/${org.id}/apps/${app.id}/components/${build.component_id}`,
-          text: component.name,
-        },
-        {
-          href: `/beta/${org.id}/apps/${app.id}/components/${build.component_id}/builds/${build.id}`,
-          text: `${component.name} build`,
-        },
-      ]}
-      heading={`${component.name} build`}
-      headingUnderline={build.id}
-      meta={
-        <div className="flex gap-8 items-center justify-start pb-6">
-          <Text variant="caption">
-            <FiCloud />
-            <Time time={build.created_at} variant="caption" />
-          </Text>
-          <Text variant="caption">
-            <FiClock />
-            <Duration
-              beginTime={build.created_at}
-              endTime={build.updated_at}
-              variant="caption"
-            />
-          </Text>
-        </div>
-      }
-      statues={
-        <div className="flex gap-6 items-start justify-start">
-          <span className="flex flex-col gap-2">
-            <Text variant="overline">Status:</Text>
-            <ToolTip tipContent={build.status_description}>
-              <Status status={build.status} />
-            </ToolTip>
-          </span>
+    return (
+      <DashboardContent
+        breadcrumb={[
+          { href: `/beta/${org.id}`, text: org.name },
+          { href: `/beta/${org.id}/apps`, text: 'Apps' },
+          { href: `/beta/${org.id}/apps/${app.id}`, text: app.name },
+          {
+            href: `/beta/${org.id}/apps/${app.id}/components/${build.component_id}`,
+            text: component.name,
+          },
+          {
+            href: `/beta/${org.id}/apps/${app.id}/components/${build.component_id}/builds/${build.id}`,
+            text: `${component.name} build`,
+          },
+        ]}
+        heading={`${component.name} build`}
+        headingUnderline={build.id}
+        meta={
+          <div className="flex gap-8 items-center justify-start pb-6">
+            <Text variant="caption">
+              <FiCloud />
+              <Time time={build.created_at} variant="caption" />
+            </Text>
+            <Text variant="caption">
+              <FiClock />
+              <Duration
+                beginTime={build.created_at}
+                endTime={build.updated_at}
+                variant="caption"
+              />
+            </Text>
+          </div>
+        }
+        statues={
+          <div className="flex gap-6 items-start justify-start">
+            <span className="flex flex-col gap-2">
+              <Text variant="overline">Status:</Text>
+              <ToolTip tipContent={build.status_description}>
+                <Status status={build.status} />
+              </ToolTip>
+            </span>
 
-          <span className="flex flex-col gap-2">
-            <Text variant="overline">Component:</Text>
-            <Text variant="label">{component.name}</Text>
-            <Text variant="caption">{build.component_id}</Text>
-          </span>
-        </div>
-      }
-    >
-      <div className="flex flex-col lg:flex-row flex-auto">
-        <section className="flex flex-auto flex-col gap-4 px-6 py-8 border-r overflow-auto">
-          <Heading>Build details</Heading>
+            <span className="flex flex-col gap-2">
+              <Text variant="overline">Component:</Text>
+              <Text variant="label">{component.name}</Text>
+              <Text variant="caption">{build.component_id}</Text>
+            </span>
+          </div>
+        }
+      >
+        <div className="flex flex-col lg:flex-row flex-auto">
+          <section className="flex flex-auto flex-col gap-4 px-6 py-8 border-r overflow-auto">
+            <Heading>Build details</Heading>
 
-          <Text>New runner logs here</Text>
-        </section>
-        <div
-          className="divide-y flex flex-col lg:min-w-[550px]
+            <Text>New runner logs here</Text>
+          </section>
+          <div
+            className="divide-y flex flex-col lg:min-w-[550px]
 lg:max-w-[550px]"
-        >
-          {build.vcs_connection_commit && (
-            <section className="flex flex-col gap-6 px-6 py-8">
-              <Heading>Commit details</Heading>
+          >
+            {build.vcs_connection_commit && (
+              <section className="flex flex-col gap-6 px-6 py-8">
+                <Heading>Commit details</Heading>
 
-              <div className="flex gap-6 items-start justify-start">
-                <span className="flex flex-col gap-2">
-                  <Text variant="overline">SHA:</Text>
-                  <ToolTip tipContent={build.vcs_connection_commit?.sha}>
-                    <Text
-                      className="truncate text-ellipsis w-16"
-                      variant="caption"
-                    >
-                      {build.vcs_connection_commit?.sha}
-                    </Text>
-                  </ToolTip>
-                </span>
-
-                {build.vcs_connection_commit?.author_name !== '' && (
+                <div className="flex gap-6 items-start justify-start">
                   <span className="flex flex-col gap-2">
-                    <Text variant="overline">Author:</Text>
+                    <Text variant="overline">SHA:</Text>
+                    <ToolTip tipContent={build.vcs_connection_commit?.sha}>
+                      <Text
+                        className="truncate text-ellipsis w-16"
+                        variant="caption"
+                      >
+                        {build.vcs_connection_commit?.sha}
+                      </Text>
+                    </ToolTip>
+                  </span>
+
+                  {build.vcs_connection_commit?.author_name !== '' && (
+                    <span className="flex flex-col gap-2">
+                      <Text variant="overline">Author:</Text>
+                      <Text variant="caption">
+                        {build.vcs_connection_commit?.author_name}
+                      </Text>
+                    </span>
+                  )}
+
+                  <span className="flex flex-col gap-2">
+                    <Text variant="overline">Message:</Text>
                     <Text variant="caption">
-                      {build.vcs_connection_commit?.author_name}
+                      {build.vcs_connection_commit?.message}
                     </Text>
                   </span>
-                )}
+                </div>
+              </section>
+            )}
+            <section className="flex flex-col gap-6 px-6 py-8">
+              <Heading>Component configuration</Heading>
 
-                <span className="flex flex-col gap-2">
-                  <Text variant="overline">Message:</Text>
-                  <Text variant="caption">
-                    {build.vcs_connection_commit?.message}
-                  </Text>
-                </span>
-              </div>
+              <ComponentConfiguration config={componentConfig} />
             </section>
-          )}
-          <section className="flex flex-col gap-6 px-6 py-8">
-            <Heading>Component configuration</Heading>
-
-            <ComponentConfiguration config={componentConfig} />
-          </section>
+          </div>
         </div>
-      </div>
-    </DashboardContent>
-  )
-}
+      </DashboardContent>
+    )
+  },
+  { returnTo: '/dashboard' }
+)
--- services/dashboard-ui/src/app/beta/[org-id]/apps/[app-id]/page.tsx
+++ services/dashboard-ui/src/app/beta/[org-id]/apps/[app-id]/page.tsx
@@ -1,3 +1,4 @@
+import { withPageAuthRequired } from '@auth0/nextjs-auth0'
 import {
   AppInputConfig,
   AppRunnerConfig,
@@ -16,54 +17,57 @@ import {
   getOrg,
 } from '@/lib'
 
-export default async function App({ params }) {
-  const appId = params?.['app-id'] as string
-  const orgId = params?.['org-id'] as string
-  const subNavLinks: Array<TLink> = [
-    { href: `/beta/${orgId}/apps/${appId}`, text: 'Config' },
-    { href: `/beta/${orgId}/apps/${appId}/components`, text: 'Components' },
-    { href: `/beta/${orgId}/apps/${appId}/installs`, text: 'Installs' },
-  ]
+export default withPageAuthRequired(
+  async function App({ params }) {
+    const appId = params?.['app-id'] as string
+    const orgId = params?.['org-id'] as string
+    const subNavLinks: Array<TLink> = [
+      { href: `/beta/${orgId}/apps/${appId}`, text: 'Config' },
+      { href: `/beta/${orgId}/apps/${appId}/components`, text: 'Components' },
+      { href: `/beta/${orgId}/apps/${appId}/installs`, text: 'Installs' },
+    ]
 
-  const [org, app, inputCfg, runnerCfg, sandboxCfg] = await Promise.all([
-    getOrg({ orgId }),
-    getApp({ appId, orgId }),
-    getAppInputLatestConfig({ appId, orgId }),
-    getAppRunnerLatestConfig({ appId, orgId }),
-    getAppSandboxLatestConfig({ appId, orgId }),
-  ])
+    const [org, app, inputCfg, runnerCfg, sandboxCfg] = await Promise.all([
+      getOrg({ orgId }),
+      getApp({ appId, orgId }),
+      getAppInputLatestConfig({ appId, orgId }),
+      getAppRunnerLatestConfig({ appId, orgId }),
+      getAppSandboxLatestConfig({ appId, orgId }),
+    ])
 
-  return (
-    <DashboardContent
-      breadcrumb={[
-        { href: `/beta/${org.id}`, text: org.name },
-        { href: `/beta/${org.id}/apps`, text: 'Apps' },
-        { href: `/beta/${org.id}/apps/${app.id}`, text: app.name },
-      ]}
-      heading={app.name}
-      headingUnderline={app.id}
-      meta={<SubNav links={subNavLinks} />}
-    >
-      <div className="flex flex-col md:flex-row flex-auto">
-        <section className="flex flex-col gap-4 px-6 py-8 border-r">
-          <Heading variant="subheading">Inputs</Heading>
-          <AppInputConfig inputConfig={inputCfg} />
-        </section>
-
-        <div className="flex flex-col lg:min-w-[510px]">
-          <section className="flex flex-col gap-4 px-6 py-8 border-b">
-            <Heading variant="subheading">Sandbox</Heading>
-            <AppSandboxConfig sandboxConfig={sandboxCfg} />
-            <AppSandboxVariables variables={sandboxCfg.variables} />
+    return (
+      <DashboardContent
+        breadcrumb={[
+          { href: `/beta/${org.id}`, text: org.name },
+          { href: `/beta/${org.id}/apps`, text: 'Apps' },
+          { href: `/beta/${org.id}/apps/${app.id}`, text: app.name },
+        ]}
+        heading={app.name}
+        headingUnderline={app.id}
+        meta={<SubNav links={subNavLinks} />}
+      >
+        <div className="flex flex-col md:flex-row flex-auto">
+          <section className="flex flex-col gap-4 px-6 py-8 border-r">
+            <Heading variant="subheading">Inputs</Heading>
+            <AppInputConfig inputConfig={inputCfg} />
           </section>
 
-          <section className="flex flex-col gap-4 px-6 py-8">
-            <Heading variant="subheading">Runner</Heading>
+          <div className="flex flex-col lg:min-w-[510px]">
+            <section className="flex flex-col gap-4 px-6 py-8 border-b">
+              <Heading variant="subheading">Sandbox</Heading>
+              <AppSandboxConfig sandboxConfig={sandboxCfg} />
+              <AppSandboxVariables variables={sandboxCfg.variables} />
+            </section>
 
-            <AppRunnerConfig runnerConfig={runnerCfg} />
-          </section>
+            <section className="flex flex-col gap-4 px-6 py-8">
+              <Heading variant="subheading">Runner</Heading>
+
+              <AppRunnerConfig runnerConfig={runnerCfg} />
+            </section>
+          </div>
         </div>
-      </div>
-    </DashboardContent>
-  )
-}
+      </DashboardContent>
+    )
+  },
+  { returnTo: '/dashboard' }
+)
