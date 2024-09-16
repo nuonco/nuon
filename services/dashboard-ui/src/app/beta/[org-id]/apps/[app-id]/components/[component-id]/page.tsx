536e254ef4cc65021a0451234c929727703b3712
Nat Hamilton <info@nathamilton.com>
2024-09-16 10:04:45 -0400

    feat(ui): auth redirects for new pages (#5945)
    * feat(ui): added auth redirects for new pages
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat(ui): install inputs use redacted values now
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/app/beta/[org-id]/apps/[app-id]/components/[component-id]/page.tsx
+++ services/dashboard-ui/src/app/beta/[org-id]/apps/[app-id]/components/[component-id]/page.tsx
@@ -1,3 +1,4 @@
+import { withPageAuthRequired } from '@auth0/nextjs-auth0'
 import {
   ComponentBuildHistory,
   ComponentConfiguration,
@@ -15,71 +16,74 @@ import {
   getOrg,
 } from '@/lib'
 
-export default async function AppComponent({ params }) {
-  const appId = params?.['app-id'] as string
-  const componentId = params?.['component-id'] as string
-  const orgId = params?.['org-id'] as string
+export default withPageAuthRequired(
+  async function AppComponent({ params }) {
+    const appId = params?.['app-id'] as string
+    const componentId = params?.['component-id'] as string
+    const orgId = params?.['org-id'] as string
 
-  const [app, appComponents, builds, component, componentConfig, org] =
-    await Promise.all([
-      getApp({ appId, orgId }),
-      getAppComponents({ appId, orgId }),
-      getComponentBuilds({ componentId, orgId }),
-      getComponent({ componentId, orgId }),
-      getComponentConfig({ componentId, orgId }),
-      getOrg({ orgId }),
-    ])
+    const [app, appComponents, builds, component, componentConfig, org] =
+      await Promise.all([
+        getApp({ appId, orgId }),
+        getAppComponents({ appId, orgId }),
+        getComponentBuilds({ componentId, orgId }),
+        getComponent({ componentId, orgId }),
+        getComponentConfig({ componentId, orgId }),
+        getOrg({ orgId }),
+      ])
 
-  return (
-    <DashboardContent
-      breadcrumb={[
-        { href: `/beta/${org.id}`, text: org.name },
-        { href: `/beta/${org.id}/apps`, text: 'Apps' },
-        { href: `/beta/${org.id}/apps/${app.id}`, text: app.name },
-        {
-          href: `/beta/${org.id}/apps/${app.id}/components/${component.id}`,
-          text: component.name,
-        },
-      ]}
-      heading={component.name}
-      headingUnderline={component.id}
-    >
-      <div className="flex flex-col lg:flex-row flex-auto">
-        <div className="divide-y flex flex-col flex-auto">
-          {component.dependencies && (
+    return (
+      <DashboardContent
+        breadcrumb={[
+          { href: `/beta/${org.id}`, text: org.name },
+          { href: `/beta/${org.id}/apps`, text: 'Apps' },
+          { href: `/beta/${org.id}/apps/${app.id}`, text: app.name },
+          {
+            href: `/beta/${org.id}/apps/${app.id}/components/${component.id}`,
+            text: component.name,
+          },
+        ]}
+        heading={component.name}
+        headingUnderline={component.id}
+      >
+        <div className="flex flex-col lg:flex-row flex-auto">
+          <div className="divide-y flex flex-col flex-auto">
+            {component.dependencies && (
+              <section className="flex flex-col gap-6 px-6 py-8">
+                <Heading>Dependencies</Heading>
+
+                <DependentComponents
+                  appId={appId}
+                  appComponents={appComponents}
+                  dependentIds={component.dependencies}
+                  orgId={orgId}
+                />
+              </section>
+            )}
+
             <section className="flex flex-col gap-6 px-6 py-8">
-              <Heading>Dependencies</Heading>
+              <Heading>Latest config</Heading>
 
-              <DependentComponents
-                appId={appId}
-                appComponents={appComponents}
-                dependentIds={component.dependencies}
-                orgId={orgId}
-              />
+              <ComponentConfiguration config={componentConfig} />
             </section>
-          )}
+          </div>
+          <section
+            className="flex flex-col gap-4 px-6 py-8 border-l overflow-auto lg:min-w-[500px]
+lg:max-w-[500px]"
+          >
+            <Heading>Build history</Heading>
 
-          <section className="flex flex-col gap-6 px-6 py-8">
-            <Heading>Latest config</Heading>
-
-            <ComponentConfiguration config={componentConfig} />
+            <ComponentBuildHistory
+              appId={appId}
+              componentId={componentId}
+              initBuilds={builds}
+              orgId={orgId}
+              shouldPoll
+            />
           </section>
         </div>
-        <section
-          className="flex flex-col gap-4 px-6 py-8 border-l overflow-auto lg:min-w-[500px]
-lg:max-w-[500px]"
-        >
-          <Heading>Build history</Heading>
-
-          <ComponentBuildHistory
-            appId={appId}
-            componentId={componentId}
-            initBuilds={builds}
-            orgId={orgId}
-            shouldPoll
-          />
-        </section>
-      </div>
-    </DashboardContent>
-  )
-}
+      </DashboardContent>
+    )
+  },
+  { returnTo: '/dashboard' }
+)
