03999f81255e693f42df770ba7e972252c5905b4
Dat Boi Diego <fidiego@users.noreply.github.com>
2024-09-16 11:53:54 -0500

    fd/otel/trace writer (#5893)
    * feat: use our clone please
    
    * feat: ctl-api/otel: logs: clean up and add json tags
    
    * feat: ctl-api/otel: write traces

--- services/ctl-api/internal/app/runners/utils/attributes_to_map.go
+++ services/ctl-api/internal/app/runners/utils/attributes_to_map.go
@@ -1,7 +1,10 @@
 package utils
 
 import (
+	"time"
+
 	"go.opentelemetry.io/collector/pdata/pcommon"
+	"go.opentelemetry.io/collector/pdata/ptrace"
 )
 
 // src: https://github.com/open-telemetry/opentelemetry-collector-contrib/blob/main/exporter/clickhouseexporter/exporter_logs.go#L132-L139
@@ -12,4 +15,38 @@ func AttributesToMap(attributes pcommon.Map) map[string]string {
 		return true
 	})
 	return m
+}
+
+// src: https://github.com/open-telemetry/opentelemetry-collector-contrib/blob/main/exporter/clickhouseexporter/exporter_traces.go#L130
+func ConvertEvents(events ptrace.SpanEventSlice) ([]time.Time, []string, []map[string]string) {
+	var (
+		times []time.Time
+		names []string
+		attrs []map[string]string
+	)
+	for i := 0; i < events.Len(); i++ {
+		event := events.At(i)
+		times = append(times, event.Timestamp().AsTime())
+		names = append(names, event.Name())
+		attrs = append(attrs, AttributesToMap(event.Attributes()))
+	}
+	return times, names, attrs
+}
+
+// src: https://github.com/open-telemetry/opentelemetry-collector-contrib/blob/main/exporter/clickhouseexporter/exporter_traces.go#L145
+func ConvertLinks(links ptrace.SpanLinkSlice) ([]string, []string, []string, []map[string]string) {
+	var (
+		traceIDs []string
+		spanIDs  []string
+		states   []string
+		attrs    []map[string]string
+	)
+	for i := 0; i < links.Len(); i++ {
+		link := links.At(i)
+		traceIDs = append(traceIDs, TraceIDToHexOrEmptyString(link.TraceID()))
+		spanIDs = append(spanIDs, SpanIDToHexOrEmptyString(link.SpanID()))
+		states = append(states, link.TraceState().AsRaw())
+		attrs = append(attrs, AttributesToMap(link.Attributes()))
+	}
+	return traceIDs, spanIDs, states, attrs
 }
