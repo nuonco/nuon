97d4656c255236e665d2128987e2c99b2bb3f413
Nat Hamilton <info@nathamilton.com>
2024-09-16 10:04:45 -0400

    feat(ui): auth redirects for new pages (#5945)
    * feat(ui): added auth redirects for new pages
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat(ui): install inputs use redacted values now
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/app/beta/[org-id]/apps/[app-id]/installs/page.tsx
+++ services/dashboard-ui/src/app/beta/[org-id]/apps/[app-id]/installs/page.tsx
@@ -1,3 +1,4 @@
+import { withPageAuthRequired } from '@auth0/nextjs-auth0'
 import {
   AppInstallsTable,
   DashboardContent,
@@ -6,37 +7,40 @@ import {
 } from '@/components'
 import { getApp, getAppInstalls, getOrg } from '@/lib'
 
-export default async function AppInstalls({ params }) {
-  const appId = params?.['app-id'] as string
-  const orgId = params?.['org-id'] as string
-  const subNavLinks: Array<TLink> = [
-    { href: `/beta/${orgId}/apps/${appId}`, text: 'Config' },
-    { href: `/beta/${orgId}/apps/${appId}/components`, text: 'Components' },
-    { href: `/beta/${orgId}/apps/${appId}/installs`, text: 'Installs' },
-  ]
-  const [app, installs, org] = await Promise.all([
-    getApp({ appId, orgId }),
-    getAppInstalls({ appId, orgId }),
-    getOrg({ orgId }),
-  ])
+export default withPageAuthRequired(
+  async function AppInstalls({ params }) {
+    const appId = params?.['app-id'] as string
+    const orgId = params?.['org-id'] as string
+    const subNavLinks: Array<TLink> = [
+      { href: `/beta/${orgId}/apps/${appId}`, text: 'Config' },
+      { href: `/beta/${orgId}/apps/${appId}/components`, text: 'Components' },
+      { href: `/beta/${orgId}/apps/${appId}/installs`, text: 'Installs' },
+    ]
+    const [app, installs, org] = await Promise.all([
+      getApp({ appId, orgId }),
+      getAppInstalls({ appId, orgId }),
+      getOrg({ orgId }),
+    ])
 
-  return (
-    <DashboardContent
-      breadcrumb={[
-        { href: `/beta/${org.id}`, text: org.name },
-        { href: `/beta/${org.id}/apps`, text: 'Apps' },
-        { href: `/beta/${org.id}/apps/${app.id}`, text: app.name },
-      ]}
-      heading={app.name}
-      headingUnderline={app.id}
-      meta={<SubNav links={subNavLinks} />}
-    >
-      <section className="px-6 py-8">
-        <AppInstallsTable
-          installs={installs.map((install) => ({ ...install, app }))}
-          orgId={orgId}
-        />
-      </section>
-    </DashboardContent>
-  )
-}
+    return (
+      <DashboardContent
+        breadcrumb={[
+          { href: `/beta/${org.id}`, text: org.name },
+          { href: `/beta/${org.id}/apps`, text: 'Apps' },
+          { href: `/beta/${org.id}/apps/${app.id}`, text: app.name },
+        ]}
+        heading={app.name}
+        headingUnderline={app.id}
+        meta={<SubNav links={subNavLinks} />}
+      >
+        <section className="px-6 py-8">
+          <AppInstallsTable
+            installs={installs.map((install) => ({ ...install, app }))}
+            orgId={orgId}
+          />
+        </section>
+      </DashboardContent>
+    )
+  },
+  { returnTo: '/dashboard' }
+)
--- services/dashboard-ui/src/app/beta/[org-id]/installs/[install-id]/runs/[run-id]/page.tsx
+++ services/dashboard-ui/src/app/beta/[org-id]/installs/[install-id]/runs/[run-id]/page.tsx
@@ -1,3 +1,4 @@
+import { withPageAuthRequired } from '@auth0/nextjs-auth0'
 import { FiCloud, FiClock } from 'react-icons/fi'
 import {
   AppSandboxConfig,
@@ -13,91 +14,94 @@ import {
 import { getInstall, getSandboxRun, getOrg } from '@/lib'
 import { sentanceCase } from '@/utils'
 
-export default async function SandboxRuns({ params }) {
-  const installId = params?.['install-id'] as string
-  const orgId = params?.['org-id'] as string
-  const runId = params?.['run-id'] as string
-  const [install, org, sandboxRun] = await Promise.all([
-    getInstall({ installId, orgId }),
-    getOrg({ orgId }),
-    getSandboxRun({ installId, orgId, runId }),
-  ])
+export default withPageAuthRequired(
+  async function SandboxRuns({ params }) {
+    const installId = params?.['install-id'] as string
+    const orgId = params?.['org-id'] as string
+    const runId = params?.['run-id'] as string
+    const [install, org, sandboxRun] = await Promise.all([
+      getInstall({ installId, orgId }),
+      getOrg({ orgId }),
+      getSandboxRun({ installId, orgId, runId }),
+    ])
 
-  return (
-    <DashboardContent
-      breadcrumb={[
-        { href: `/beta/${org.id}`, text: org.name },
-        { href: `/beta/${org.id}/installs`, text: 'Installs' },
-        {
-          href: `/beta/${org.id}/installs/${install.id}`,
-          text: install.name,
-        },
-        {
-          href: `/beta/${org.id}/installs/${install.id}/runs/${sandboxRun.id}`,
-          text: `${install.name} ${sandboxRun.run_type}`,
-        },
-      ]}
-      heading={`${install.name} ${sandboxRun.run_type}`}
-      headingUnderline={sandboxRun.id}
-      meta={
-        <div className="flex gap-8 items-center justify-start pb-6">
-          <Text variant="caption">
-            <FiCloud />
-            <Time time={sandboxRun.created_at} variant="caption" />
-          </Text>
-          <Text variant="caption">
-            <FiClock />
-            <Duration
-              beginTime={sandboxRun.created_at}
-              endTime={sandboxRun.updated_at}
-              variant="caption"
-            />
-          </Text>
-        </div>
-      }
-      statues={
-        <div className="flex gap-6 items-start justify-start">
-          <span className="flex flex-col gap-2">
-            <Text variant="overline">Status:</Text>
-            <ToolTip tipContent={sandboxRun.status_description}>
-              <Status status={sandboxRun.status} />
-            </ToolTip>
-          </span>
+    return (
+      <DashboardContent
+        breadcrumb={[
+          { href: `/beta/${org.id}`, text: org.name },
+          { href: `/beta/${org.id}/installs`, text: 'Installs' },
+          {
+            href: `/beta/${org.id}/installs/${install.id}`,
+            text: install.name,
+          },
+          {
+            href: `/beta/${org.id}/installs/${install.id}/runs/${sandboxRun.id}`,
+            text: `${install.name} ${sandboxRun.run_type}`,
+          },
+        ]}
+        heading={`${install.name} ${sandboxRun.run_type}`}
+        headingUnderline={sandboxRun.id}
+        meta={
+          <div className="flex gap-8 items-center justify-start pb-6">
+            <Text variant="caption">
+              <FiCloud />
+              <Time time={sandboxRun.created_at} variant="caption" />
+            </Text>
+            <Text variant="caption">
+              <FiClock />
+              <Duration
+                beginTime={sandboxRun.created_at}
+                endTime={sandboxRun.updated_at}
+                variant="caption"
+              />
+            </Text>
+          </div>
+        }
+        statues={
+          <div className="flex gap-6 items-start justify-start">
+            <span className="flex flex-col gap-2">
+              <Text variant="overline">Status:</Text>
+              <ToolTip tipContent={sandboxRun.status_description}>
+                <Status status={sandboxRun.status} />
+              </ToolTip>
+            </span>
 
-          <span className="flex flex-col gap-2">
-            <Text variant="overline">Type:</Text>
-            <Text variant="caption">{sandboxRun.run_type}</Text>
-          </span>
+            <span className="flex flex-col gap-2">
+              <Text variant="overline">Type:</Text>
+              <Text variant="caption">{sandboxRun.run_type}</Text>
+            </span>
 
-          <span className="flex flex-col gap-2">
-            <Text variant="overline">Install:</Text>
-            <Text variant="label">{install.name}</Text>
-            <Text variant="caption">{install.id}</Text>
-          </span>
-        </div>
-      }
-    >
-      <div className="flex flex-col lg:flex-row flex-auto">
-        <section className="flex flex-auto flex-col gap-4 px-6 py-8 border-r overflow-auto">
-          <Heading>{sentanceCase(sandboxRun.run_type)} details</Heading>
+            <span className="flex flex-col gap-2">
+              <Text variant="overline">Install:</Text>
+              <Text variant="label">{install.name}</Text>
+              <Text variant="caption">{install.id}</Text>
+            </span>
+          </div>
+        }
+      >
+        <div className="flex flex-col lg:flex-row flex-auto">
+          <section className="flex flex-auto flex-col gap-4 px-6 py-8 border-r overflow-auto">
+            <Heading>{sentanceCase(sandboxRun.run_type)} details</Heading>
 
-          <Text>New runner logs here</Text>
-        </section>
-
-        <div
-          className="divide-y flex flex-col lg:min-w-[550px]
-lg:max-w-[550px]"
-        >
-          <section className="flex flex-col gap-6 px-6 py-8">
-            <Heading>Sandbox</Heading>
-
-            <AppSandboxConfig sandboxConfig={sandboxRun.app_sandbox_config} />
-            <AppSandboxVariables
-              variables={sandboxRun.app_sandbox_config?.variables}
-            />
+            <Text>New runner logs here</Text>
           </section>
+
+          <div
+            className="divide-y flex flex-col lg:min-w-[550px]
+lg:max-w-[550px]"
+          >
+            <section className="flex flex-col gap-6 px-6 py-8">
+              <Heading>Sandbox</Heading>
+
+              <AppSandboxConfig sandboxConfig={sandboxRun.app_sandbox_config} />
+              <AppSandboxVariables
+                variables={sandboxRun.app_sandbox_config?.variables}
+              />
+            </section>
+          </div>
         </div>
-      </div>
-    </DashboardContent>
-  )
-}
+      </DashboardContent>
+    )
+  },
+  { returnTo: '/dashboard' }
+)
