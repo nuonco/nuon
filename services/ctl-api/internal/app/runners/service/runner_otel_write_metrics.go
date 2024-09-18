908564f3da7559926fd6812afce4352760e8bb54
Dat Boi Diego <fidiego@users.noreply.github.com>
2024-09-18 09:48:56 -0500

    feat: ctl-api/otel: metrics writer (#5960)
    * feat: write metrics sums
    
    * feat: write metrics gauges
    
    * feat: write metrics histogram
    
    * feat: write metrics exponential histogram
    
    * feat: write metrics summary
    
    * fix: conditionally create metrics, if list is not empty
    
    * feat: rename column: s/Examplar/Exemplar/g
    
    * chore: remove stray comment
    
    * chore: specify correct type although not used in migration
    
    ---------
    
    Co-authored-by: nuonbot <bot@nuon.co>

--- services/ctl-api/internal/app/runners/service/runner_otel_write_metrics.go
+++ services/ctl-api/internal/app/runners/service/runner_otel_write_metrics.go
@@ -3,13 +3,15 @@ package service
 import (
 	"context"
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-
-	"go.opentelemetry.io/collector/pdata/pmetric/pmetricotlp"
-
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app/runners/utils"
+
+	"go.opentelemetry.io/collector/pdata/pmetric"
+	"go.opentelemetry.io/collector/pdata/pmetric/pmetricotlp"
 )
 
 // NOTE(jm): we have to define this here, because the `pmetricotlp.ExportRequest` type is actually a hidden type and
@@ -72,21 +74,319 @@ type OTLPMetricExportRequest struct {
 func (s *service) OtelWriteMetrics(ctx *gin.Context) {
 	runnerID := ctx.Param("runner_id")
 
-	var req *pmetricotlp.ExportRequest
-	if err := ctx.BindJSON(&req); err != nil {
+	// read data into bytes
+	jsonData, err := io.ReadAll(ctx.Request.Body)
+	if err != nil {
 		ctx.Error(fmt.Errorf("unable to parse request: %w", err))
 		return
 	}
-
-	_, err := s.writeRunnerMetrics(ctx, runnerID, req)
-	if err != nil {
-		ctx.Error(fmt.Errorf("unable to write runner metrics: %w", err))
+	// unmarshal bytes into ExportRequest
+	// NOTE(fd): this is essentially our validation step. we do not use this object directly otherwise.
+	expreq := pmetricotlp.NewExportRequest()
+	if err := expreq.UnmarshalJSON(jsonData); err != nil {
+		ctx.Error(fmt.Errorf("unable to marshal request: %w", err))
 		return
 	}
 
-	ctx.JSON(http.StatusOK, "ok")
+	writerrs := s.writeRunnerMetrics(ctx, runnerID, &expreq)
+	if writerrs != nil {
+		for _, err := range writerrs {
+			ctx.Error(fmt.Errorf("unable to write runner metrics: %w", err))
+		}
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, "ok")
 }
 
-func (s *service) writeRunnerMetrics(ctx context.Context, runnerID string, logs *pmetricotlp.ExportRequest) ([]*app.RunnerJob, error) {
-	return nil, nil
+func (s *service) writeRunnerMetrics(ctx context.Context, runnerID string, req *pmetricotlp.ExportRequest) []error {
+
+	// a list of errors to be returned
+	errors := []error{}
+
+	// list of metric objects to be created
+	sumMetrics := []app.OtelMetricSumIngestion{}
+	gaugeMetrics := []app.OtelMetricGaugeIngestion{}
+	histogramMetrics := []app.OtelMetricHistogramIngestion{}
+	exponentialHistogramMetrics := []app.OtelMetricExponentialHistogramIngestion{}
+	summaryMetrics := []app.OtelMetricSummaryIngestion{}
+
+	metricsSlice := req.Metrics().ResourceMetrics()
+	for i := 0; i < metricsSlice.Len(); i++ {
+		metrics := metricsSlice.At(i)
+
+		resAttr := metrics.Resource().Attributes()
+		resourceSchemaUrl := metrics.SchemaUrl()
+
+		// NOTE(fd): this is a well established convention.
+		var serviceName string
+		snVal, ok := resAttr.Get("service.name")
+		if ok {
+			serviceName = snVal.AsString()
+		}
+
+		// NOTE(fd): this is a nuon convention.
+		var jobId string
+		jobIdVal, ok := resAttr.Get("job.id")
+		if ok {
+			jobId = jobIdVal.AsString()
+		}
+
+		scopeMetrics := metrics.ScopeMetrics()
+		for j := 0; j < scopeMetrics.Len(); j++ {
+			rs := scopeMetrics.At(j).Metrics()
+			scopeInstr := scopeMetrics.At(j).Scope()
+			scopeURL := scopeMetrics.At(j).SchemaUrl()
+
+			for k := 0; k < rs.Len(); k++ {
+				r := rs.At(k)
+
+				switch r.Type() {
+				case pmetric.MetricTypeGauge:
+					for l := 0; l < r.Gauge().DataPoints().Len(); l++ {
+						dp := r.Gauge().DataPoints().At(i)
+						attrs, times, values, traceIDs, spanIDs := utils.ConvertExemplars(dp.Exemplars())
+						gaugeMetrics = append(gaugeMetrics, app.OtelMetricGaugeIngestion{
+							RunnerID:    runnerID,
+							RunnerJobID: jobId,
+
+							ResourceAttributes: utils.AttributesToMap(resAttr),
+							ResourceSchemaURL:  resourceSchemaUrl,
+
+							ScopeName:             scopeInstr.Name(),
+							ScopeVersion:          scopeInstr.Version(),
+							ScopeAttributes:       utils.AttributesToMap(scopeInstr.Attributes()),
+							ScopeDroppedAttrCount: int(scopeInstr.DroppedAttributesCount()),
+							ScopeSchemaURL:        scopeURL,
+
+							ServiceName: serviceName,
+
+							MetricName:        r.Name(),
+							MetricDescription: r.Description(),
+							MetricUnit:        r.Unit(),
+
+							StartTimeUnix: dp.StartTimestamp().AsTime(),
+							TimeUnix:      dp.Timestamp().AsTime(),
+							Value:         utils.GetValue(dp.IntValue(), dp.DoubleValue(), dp.ValueType()),
+							Flags:         uint32(dp.Flags()),
+
+							ExemplarsFilteredAttributes: attrs,
+							ExemplarsTimeUnix:           times,
+							ExemplarsValue:              values,
+							ExemplarsTraceID:            traceIDs,
+							ExemplarsSpanID:             spanIDs,
+						})
+					}
+				case pmetric.MetricTypeSum:
+					for l := 0; l < r.Sum().DataPoints().Len(); l++ {
+						dp := r.Sum().DataPoints().At(i)
+						attrs, times, values, traceIDs, spanIDs := utils.ConvertExemplars(dp.Exemplars())
+						sumMetrics = append(sumMetrics, app.OtelMetricSumIngestion{
+							RunnerID:    runnerID,
+							RunnerJobID: jobId,
+
+							ResourceAttributes: utils.AttributesToMap(resAttr),
+							ResourceSchemaURL:  resourceSchemaUrl,
+
+							ScopeName:             scopeInstr.Name(),
+							ScopeVersion:          scopeInstr.Version(),
+							ScopeAttributes:       utils.AttributesToMap(scopeInstr.Attributes()),
+							ScopeDroppedAttrCount: int(scopeInstr.DroppedAttributesCount()),
+							ScopeSchemaURL:        scopeURL,
+
+							ServiceName: serviceName,
+
+							MetricName:        r.Name(),
+							MetricDescription: r.Description(),
+							MetricUnit:        r.Unit(),
+
+							StartTimeUnix: dp.StartTimestamp().AsTime(),
+							TimeUnix:      dp.Timestamp().AsTime(),
+							Value:         utils.GetValue(dp.IntValue(), dp.DoubleValue(), dp.ValueType()),
+							Flags:         uint32(dp.Flags()),
+
+							IsMonotonic: r.Sum().IsMonotonic(),
+
+							AggregationTemporality:      int32(r.Sum().AggregationTemporality()),
+							ExemplarsFilteredAttributes: attrs,
+							ExemplarsTimeUnix:           times,
+							ExemplarsValue:              values,
+							ExemplarsTraceID:            traceIDs,
+							ExemplarsSpanID:             spanIDs,
+						})
+					}
+				case pmetric.MetricTypeHistogram:
+					for l := 0; l < r.Histogram().DataPoints().Len(); l++ {
+						dp := r.Histogram().DataPoints().At(i)
+						attrs, times, values, traceIDs, spanIDs := utils.ConvertExemplars(dp.Exemplars())
+						histogramMetrics = append(histogramMetrics, app.OtelMetricHistogramIngestion{
+							RunnerID:    runnerID,
+							RunnerJobID: jobId,
+
+							ResourceAttributes: utils.AttributesToMap(resAttr),
+							ResourceSchemaURL:  resourceSchemaUrl,
+
+							ScopeName:             scopeInstr.Name(),
+							ScopeVersion:          scopeInstr.Version(),
+							ScopeAttributes:       utils.AttributesToMap(scopeInstr.Attributes()),
+							ScopeDroppedAttrCount: uint32(scopeInstr.DroppedAttributesCount()),
+							ScopeSchemaURL:        scopeURL,
+
+							ServiceName: serviceName,
+
+							MetricName:        r.Name(),
+							MetricDescription: r.Description(),
+							MetricUnit:        r.Unit(),
+
+							StartTimeUnix: dp.StartTimestamp().AsTime(),
+							TimeUnix:      dp.Timestamp().AsTime(),
+
+							Count:          dp.Count(),
+							Sum:            dp.Sum(),
+							BucketsCount:   utils.ConvertSliceToArraySet(dp.BucketCounts().AsRaw()),
+							ExplicitBounds: utils.ConvertSliceToArraySet(dp.ExplicitBounds().AsRaw()),
+
+							Flags: uint32(dp.Flags()),
+							Min:   dp.Min(),
+							Max:   dp.Max(),
+
+							AggregationTemporality: int32(r.Histogram().AggregationTemporality()),
+
+							ExemplarsFilteredAttributes: attrs,
+							ExemplarsTimeUnix:           times,
+							ExemplarsValue:              values,
+							ExemplarsTraceID:            traceIDs,
+							ExemplarsSpanID:             spanIDs,
+						})
+					}
+				case pmetric.MetricTypeExponentialHistogram:
+					dp := r.ExponentialHistogram().DataPoints().At(i)
+					attrs, times, values, traceIDs, spanIDs := utils.ConvertExemplars(dp.Exemplars())
+					exponentialHistogramMetrics = append(exponentialHistogramMetrics, app.OtelMetricExponentialHistogramIngestion{
+						RunnerID:    runnerID,
+						RunnerJobID: jobId,
+
+						ResourceAttributes: utils.AttributesToMap(resAttr),
+						ResourceSchemaURL:  resourceSchemaUrl,
+
+						ScopeName:             scopeInstr.Name(),
+						ScopeVersion:          scopeInstr.Version(),
+						ScopeAttributes:       utils.AttributesToMap(scopeInstr.Attributes()),
+						ScopeDroppedAttrCount: uint32(scopeInstr.DroppedAttributesCount()),
+						ScopeSchemaURL:        scopeURL,
+
+						ServiceName: serviceName,
+
+						MetricName:        r.Name(),
+						MetricDescription: r.Description(),
+						MetricUnit:        r.Unit(),
+
+						StartTimeUnix: dp.StartTimestamp().AsTime(),
+						TimeUnix:      dp.Timestamp().AsTime(),
+
+						Count: dp.Count(),
+						Sum:   dp.Sum(),
+
+						Scale:                dp.Scale(),
+						ZeroCount:            dp.ZeroCount(),
+						PositiveOffset:       dp.Positive().Offset(),
+						PositiveBucketCounts: utils.ConvertSliceToArraySet(dp.Positive().BucketCounts().AsRaw()),
+						NegativeOffset:       dp.Negative().Offset(),
+						NegativeBucketCounts: utils.ConvertSliceToArraySet(dp.Negative().BucketCounts().AsRaw()),
+
+						Flags: uint32(dp.Flags()),
+						Min:   dp.Min(),
+						Max:   dp.Max(),
+
+						AggregationTemporality: int32(r.ExponentialHistogram().AggregationTemporality()),
+
+						ExemplarsFilteredAttributes: attrs,
+						ExemplarsTimeUnix:           times,
+						ExemplarsValue:              values,
+						ExemplarsTraceID:            traceIDs,
+						ExemplarsSpanID:             spanIDs,
+					})
+				case pmetric.MetricTypeSummary:
+					dp := r.Summary().DataPoints().At(i)
+					quantiles, values := utils.ConvertValueAtQuantile(dp.QuantileValues())
+					summaryMetrics = append(summaryMetrics, app.OtelMetricSummaryIngestion{
+						RunnerID:    runnerID,
+						RunnerJobID: jobId,
+
+						ResourceAttributes: utils.AttributesToMap(resAttr),
+						ResourceSchemaURL:  resourceSchemaUrl,
+
+						ScopeName:             scopeInstr.Name(),
+						ScopeVersion:          scopeInstr.Version(),
+						ScopeAttributes:       utils.AttributesToMap(scopeInstr.Attributes()),
+						ScopeDroppedAttrCount: int(scopeInstr.DroppedAttributesCount()),
+						ScopeSchemaURL:        scopeURL,
+
+						ServiceName: serviceName,
+
+						MetricName:        r.Name(),
+						MetricDescription: r.Description(),
+						MetricUnit:        r.Unit(),
+
+						StartTimeUnix: dp.StartTimestamp().AsTime(),
+						TimeUnix:      dp.Timestamp().AsTime(),
+
+						Count:                    dp.Count(),
+						Sum:                      dp.Sum(),
+						Flags:                    uint32(dp.Flags()),
+						ValueAtQuantilesQuantile: quantiles,
+						ValueAtQuantilesValue:    values,
+					})
+				case pmetric.MetricTypeEmpty:
+					// TODO: log and consider returning early (or ... validate these up top)
+					errors = append(errors, fmt.Errorf("metrics type is unset"))
+
+				default:
+					// TODO: log and consider returning early
+					errors = append(errors, fmt.Errorf("unsupported metrics type"))
+				}
+			}
+		}
+	}
+
+	// TODO: collect all errors in a list and return the list of errors
+	if len(sumMetrics) > 0 {
+		sumRes := s.chDB.WithContext(ctx).Create(&sumMetrics)
+		if sumRes.Error != nil {
+			errors = append(errors, fmt.Errorf("unable to ingest sum metrics: %w", sumRes.Error))
+		}
+	}
+
+	if len(gaugeMetrics) > 0 {
+		gaugeRes := s.chDB.WithContext(ctx).Create(&gaugeMetrics)
+		if gaugeRes.Error != nil {
+			errors = append(errors, fmt.Errorf("unable to ingest gauge metrics: %w", gaugeRes.Error))
+		}
+	}
+
+	if len(histogramMetrics) > 0 {
+		histogramRes := s.chDB.WithContext(ctx).Create(&histogramMetrics)
+		if histogramRes.Error != nil {
+			errors = append(errors, fmt.Errorf("unable to ingest histogram metrics: %w", histogramRes.Error))
+		}
+	}
+
+	if len(exponentialHistogramMetrics) > 0 {
+		exponentialHistogramRes := s.chDB.WithContext(ctx).Create(&exponentialHistogramMetrics)
+		if exponentialHistogramRes.Error != nil {
+			errors = append(errors, fmt.Errorf("unable to ingest exponential histogram metrics: %w", exponentialHistogramRes.Error))
+		}
+	}
+
+	if len(summaryMetrics) > 0 {
+		summaryRes := s.chDB.WithContext(ctx).Create(&summaryMetrics)
+		if summaryRes.Error != nil {
+			errors = append(errors, fmt.Errorf("unable to ingest summary metrics: %w", summaryRes.Error))
+		}
+	}
+
+	if len(errors) > 0 {
+		return errors
+	}
+	return nil
 }
