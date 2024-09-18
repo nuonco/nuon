e9d1893be287a901b21a9c04418fbc363ec1bcf2
Dat Boi Diego <fidiego@users.noreply.github.com>
2024-09-18 09:48:56 -0500

    feat: ctl-api/otel: metrics writer (#5960)
    * feat: write metrics sums
    
    * feat: write metrics gauges
    
    * feat: write metrics histogram
    
    * feat: write metrics exponential histogram
    
    * feat: write metrics summary
    
    * fix: conditionally create metrics, if list is not empty
    
    * feat: rename column: s/Examplar/Exemplar/g
    
    * chore: remove stray comment
    
    * chore: specify correct type although not used in migration
    
    ---------
    
    Co-authored-by: nuonbot <bot@nuon.co>

--- services/ctl-api/internal/app/otel_metric_gauge.go
+++ services/ctl-api/internal/app/otel_metric_gauge.go
@@ -3,10 +3,92 @@ package app
 import (
 	"time"
 
+	"github.com/nuonco/clickhouse-go/v2"
+	"github.com/powertoolsdev/mono/pkg/shortid/domains"
+	"gorm.io/gorm"
 	"gorm.io/plugin/soft_delete"
 )
 
+type OtelMetricGaugeExemplar struct {
+	FilteredAttributes map[string]string `json:"filtered_attributes"`
+	TimesUnix          string            `json:"times_unix"`
+	Value              string            `json:"value"`
+	SpanID             string            `json:"span_id"`
+	TraceID            string            `json:"trace_id"`
+}
+
 type OtelMetricGauge struct {
+	ID          string `gorm:"primary_key" json:"id"`
+	CreatedByID string `json:"created_by_id" gorm:"notnull"`
+
+	CreatedAt time.Time             `json:"created_at" gorm:"notnull"`
+	UpdatedAt time.Time             `json:"updated_at" gorm:"notnull"`
+	DeletedAt soft_delete.DeletedAt `json:"-"`
+
+	// internal log attributes
+	RunnerID             string `json:"runner_id" `
+	RunnerJobID          string `json:"runner_job_id"`
+	RunnerJobExecutionID string `json:"runner_job_execution_id"`
+
+	// OTEL attributes
+	ResourceAttributes map[string]string `json:"resource_attributes" gorm:"type:Map(LowCardinality(String),String);codec:ZSTD(1); index:idx_res_attr_key,expression:mapKeys(resource_attributes),type:bloom_filter(0.1),granularity:1; index:idx_res_attr_value,expression:mapKeys(resource_attributes),type:bloom_filter(0.1),granularity:1"`
+	ResourceSchemaURL  string            `json:"resource_schema_url" gorm:"type:LowCardinality(String);codec:ZSTD(1)"`
+
+	ScopeName             string            `json:"scope_name" gorm:"codec:ZSTD(1)"`
+	ScopeVersion          string            `json:"scope_version" gorm:"type:LowCardinality(String);codec:ZSTD(1)"`
+	ScopeAttributes       map[string]string `json:"scope_attributes" gorm:"type:Map(LowCardinality(String), String);codec:ZSTD(1);index:idx_scope_attr_key,expression:mapKeys(resource_attributes),type:bloom_filter(0.1),granularity:1; index:idx_scope_attr_value,expression:mapKeys(resource_attributes),type:bloom_filter(0.1),granularity:1"`
+	ScopeDroppedAttrCount int               `json:"scope_dropped_attr_count" gorm:"type:UInt32;codec:ZSTD(1)"`
+	ScopeSchemaURL        string            `json:"scope_schema_url" gorm:"type:LowCardinality(String);codec:ZSTD(1)"`
+
+	ServiceName string `json:"service_name" gorm:"type:LowCardinality(String);codec:ZSTD(1)"`
+
+	MetricName        string `json:"metric_name" gorm:"codec:ZSTD(1)"`
+	MetricDescription string `json:"metric_description" gorm:"codec:ZSTD(1)"`
+	MetricUnit        string `json:"metric_unit" gorm:"codec:ZSTD(1)"`
+
+	Attributes map[string]string `json:"attributes" gorm:"type:Map(LowCardinality(String),String);codec:ZSTD(1); index:idx_attr_key,expression:mapKeys(resource_attributes),type:bloom_filter(0.1),granularity:1; index:idx_attr_value,expression:mapKeys(resource_attributes),type:bloom_filter(0.1),granularity:1"`
+
+	StartTimeUnix time.Time `json:"start_time_unix" gorm:"type:DateTime64(9); codec:Delta, ZSTD(1)"`
+	TimeUnix      time.Time `json:"time_unix" gorm:"type:DateTime64(9);codec:ZSTD(1)"`
+	Value         float64   `json:"value" gorm:"type:Float64;codec:ZSTD(1)"`
+	Flags         uint32    `json:"flags" gorm:"type:UInt32;codec:ZSTD(1)"`
+
+	Exemplars []OtelMetricGaugeExemplar `json:"-" gorm:"type:Nested(filtered_attributes Map(LowCardinality(String), String), time_unix DateTime64(9), value Float64, span_id String, trace_id String); codec:ZSTD(1);"`
+}
+
+func (m OtelMetricGauge) GetTableOptions() (string, bool) {
+	opts := `ENGINE = MergeTree()
+	TTL toDateTime("time_unix") + toIntervalDay(180)
+	PARTITION BY toDate(time_unix)
+	ORDER BY (service_name, metric_name, attributes, toUnixTimestamp64Nano(time_unix))
+	SETTINGS index_granularity=8192, ttl_only_drop_parts = 1;`
+	return opts, true
+}
+
+func (m OtelMetricGauge) TableName() string {
+	return "otel_metrics_gauge"
+}
+
+func (m OtelMetricGauge) MigrateDB(db *gorm.DB) *gorm.DB {
+	opts, hasOpts := m.GetTableOptions()
+	if !hasOpts {
+		return db
+	}
+	return db.Set("gorm:table_options", opts)
+}
+
+func (m *OtelMetricGauge) BeforeCreate(tx *gorm.DB) error {
+	if m.ID == "" {
+		m.ID = domains.NewOtelMetricGaugeID()
+	}
+	if m.CreatedByID == "" {
+		m.CreatedByID = createdByIDFromContext(tx.Statement.Context)
+	}
+	return nil
+}
+
+// DO NOT MIGRATE
+type OtelMetricGaugeIngestion struct {
 	ID          string `gorm:"primary_key" json:"id"`
 	CreatedByID string `json:"created_by_id" gorm:"not null;default:null"`
 
@@ -20,20 +102,46 @@ type OtelMetricGauge struct {
 	RunnerJobExecutionID string `json:"runner_job_execution_id"`
 
 	// OTEL log message attributes
-	ResourceAttributes    map[string]string
-	ResourceSchemaURL     string
-	ScopeName             string
-	ScopeVersion          string
-	ScopeAttributes       map[string]string
-	ScopeDroppedAttrCount int
-	ScopeSchemaURL        string
-	ServiceName           string
-	MetricName            string
-	MetricDescription     string
-	MetricUnit            string
-	Attributes            map[string]string
-	StartTimeUnix         time.Time
-	TimeUnix              time.Time
-	Value                 float64
-	Flags                 int
+	ResourceSchemaURL  string            `json:"resource_schema_url" gorm:"type:LowCardinality(String);codec:ZSTD(1)"`
+	ResourceAttributes map[string]string `json:"resource_attributes" gorm:"type:Map(LowCardinality(String),String);codec:ZSTD(1); index:idx_res_attr_key,expression:mapKeys(resource_attributes),type:bloom_filter(0.1),granularity:1; index:idx_res_attr_value,expression:mapKeys(resource_attributes),type:bloom_filter(0.1),granularity:1"`
+
+	ScopeName             string            `json:"scope_name" gorm:"codec:ZSTD(1)"`
+	ScopeVersion          string            `json:"scope_version" gorm:"type:LowCardinality(String);codec:ZSTD(1)"`
+	ScopeAttributes       map[string]string `json:"scope_attributes" gorm:"type:Map(LowCardinality(String), String);codec:ZSTD(1);index:idx_scope_attr_key,expression:mapKeys(resource_attributes),type:bloom_filter(0.1),granularity:1; index:idx_scope_attr_value,expression:mapKeys(resource_attributes),type:bloom_filter(0.1),granularity:1"`
+	ScopeDroppedAttrCount int               `json:"scope_dropped_attr_count" gorm:"type:UInt32;codec:ZSTD(1)"`
+	ScopeSchemaURL        string            `json:"scope_schema_url" gorm:"type:LowCardinality(String);codec:ZSTD(1)"`
+
+	ServiceName string `json:"service_name" gorm:"type:LowCardinality(String);codec:ZSTD(1)"`
+
+	MetricName        string `json:"metric_name" gorm:"codec:ZSTD(1)"`
+	MetricDescription string `json:"metric_description" gorm:"codec:ZSTD(1)"`
+	MetricUnit        string `json:"metric_unit" gorm:"codec:ZSTD(1)"`
+
+	Attributes map[string]string `json:"attributes" gorm:"type:Map(LowCardinality(String),String);codec:ZSTD(1); index:idx_attr_key,expression:mapKeys(resource_attributes),type:bloom_filter(0.1),granularity:1; index:idx_attr_value,expression:mapKeys(resource_attributes),type:bloom_filter(0.1),granularity:1"`
+
+	StartTimeUnix time.Time `json:"start_time_unix" gorm:"type:DateTime64(9); codec:Delta, ZSTD(1)"`
+	TimeUnix      time.Time `json:"time_unix" gorm:"type:DateTime64(9);codec:ZSTD(1)"`
+	Value         float64   `json:"value" gorm:"type:Float64;codec:ZSTD(1)"`
+	Flags         uint32    `json:"flags" gorm:"type:UInt32;codec:ZSTD(1)"`
+
+	// Exemplars []OtelMetricGaugeExemplar `json:"-" gorm:"type:Nested(filtered_attributes Map(LowCardinality(String), String), time_unix DateTime64(9), value Float64, span_id String, trace_id String); codec:ZSTD(1);"`
+	ExemplarsFilteredAttributes clickhouse.ArraySet `json:"-" gorm:"type:Array(Map(LowCardinality(String), String));column:exemplars.filtered_attributes"`
+	ExemplarsTimeUnix           clickhouse.ArraySet `json:"-" gorm:"type:Array(DateTime64(9));column:exemplars.time_unix"`
+	ExemplarsValue              clickhouse.ArraySet `json:"-" gorm:"type:Array(Float64);column:exemplars.value"`
+	ExemplarsSpanID             clickhouse.ArraySet `json:"-" gorm:"type:Array(String);column:exemplars.span_id"`
+	ExemplarsTraceID            clickhouse.ArraySet `json:"-" gorm:"type:Array(String);column:exemplars.trace_id"`
+}
+
+func (m *OtelMetricGaugeIngestion) BeforeCreate(tx *gorm.DB) error {
+	if m.ID == "" {
+		m.ID = domains.NewOtelMetricGaugeID()
+	}
+	if m.CreatedByID == "" {
+		m.CreatedByID = createdByIDFromContext(tx.Statement.Context)
+	}
+	return nil
+}
+
+func (m OtelMetricGaugeIngestion) TableName() string {
+	return "otel_metrics_gauge"
 }
