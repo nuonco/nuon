3faf990e852b125f351955822cdbdb43934b8ecb
Dat Boi Diego <fidiego@users.noreply.github.com>
2024-09-18 12:30:30 -0500

    chore: update team (#5988)

--- infra/vantage/team.tf
+++ infra/vantage/team.tf
@@ -1,6 +1,7 @@
 resource "vantage_team" "nuon_engineering" {
   name = "Nuon Engineering"
   description = "Everyone on the nuon engineering team."
-  user_emails = ["jon@nuon.co", "nat@nuon.co", "jordan@nuon.co", "fred@nuon.com", "rob@nuon.co"]
+  # adding jon@nuon.co is giving us grief - contact support
+  user_emails = ["nat@nuon.co", "jordan@nuon.co", "sam@nuon.co", "fred@nuon.com", "rob@nuon.co"]
   workspace_tokens = ["wrkspc_217372b8dbd3cc61"]
 }
