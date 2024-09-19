db17d3db88ea776e65819326e4d5538cfb68bfd4
Dat Boi Diego <fidiego@users.noreply.github.com>
2024-09-19 10:27:32 -0500

    chore: disable team mng an tag resources (#6011)

--- infra/vantage/integration.tf
+++ infra/vantage/integration.tf
@@ -3,4 +3,6 @@ module "vantage-integration" {
   source  = "vantage-sh/vantage-integration/aws"
   # provisioned with private acl's and only accessed by Vantage via the provisioned cross account role.
   cur_bucket_name = "nuon-vantage-cur"
+
+  tags = local.tags
 }
--- infra/vantage/team.tf
+++ infra/vantage/team.tf
@@ -1,7 +1,9 @@
-resource "vantage_team" "nuon_engineering" {
-  name = "Nuon Engineering"
-  description = "Everyone on the nuon engineering team."
-  # adding jon@nuon.co is giving us grief - contact support
-  user_emails = ["nat@nuon.co", "jordan@nuon.co", "sam@nuon.co", "fred@nuon.com", "rob@nuon.co"]
-  workspace_tokens = ["wrkspc_217372b8dbd3cc61"]
-}
+# user management is currently not supported so we're commenting this out until
+# we hear back from the maintainers and can add users via tf
+
+# resource "vantage_team" "nuon_engineering" {
+#   name = "Nuon Engineering"
+#   description = "Everyone on the nuon engineering team."
+#   user_emails = ["jon@nuon.co", "nat@nuon.co", "jordan@nuon.co", "sam@nuon.co", "fred@nuon.com", "rob@nuon.co"]
+#   workspace_tokens = ["wrkspc_217372b8dbd3cc61"]
+# }
