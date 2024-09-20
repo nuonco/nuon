cc16d6ab2c04ceb7a64fc98e063fa59cfeba62fe
Dat Boi Diego <fidiego@users.noreply.github.com>
2024-09-19 21:21:50 -0500

    feat: kickstart read logs endpoint (#6021)
    * feat: increase ttl on otel tables
    
    * fix: do NOT trim off the nano seconds
    
    * feat: implement simple paginated runner otel log GET endpoint
    
    * fix: ttl was using the wrong granularity

--- services/ctl-api/internal/app/otel_metric_sum.go
+++ services/ctl-api/internal/app/otel_metric_sum.go
@@ -63,7 +63,7 @@ type OtelMetricSum struct {
 
 func (m OtelMetricSum) GetTableOptions() (string, bool) {
 	opts := `ENGINE = MergeTree()
-	TTL toDateTime("time_unix") + toIntervalDay(180)
+	TTL toDateTime("time_unix") + toIntervalDay(720)
 	PARTITION BY toDate(time_unix)
 	ORDER BY (service_name, metric_name, attributes, toUnixTimestamp64Nano(time_unix))
 	SETTINGS index_granularity=8192, ttl_only_drop_parts = 1;`
