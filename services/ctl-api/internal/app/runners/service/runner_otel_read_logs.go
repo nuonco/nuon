50e656026f0e1b36dc3e05eb7099b9c0a11b3863
Dat Boi Diego <fidiego@users.noreply.github.com>
2024-09-22 21:58:44 -0500

    feat: ctl-api/otel: small featch logs endpoint w/ filters  (#6036)
    * fix: ensure we pass time.Time to timestamps fields
    
    to avoid any potential for losing nanoseconds
    
    * feat: ctl-api/otel: read logs endpoint w/ query params
    
    job_id and job_execution_id accepted as query params
    
    * fix: ctl-api/otel: ensure we pass a time.Time to all time.Time fields
    
    otherwise they are not NULL, they are ZERO! becuse of golang/gorm struct
    default values. this is highly problematic because the timestamp_date is
    used for TTLs and the zero is a date back in 1970!
    
    * feat: all nuon well-known runner facts follow same runner_* convention
    
    we use well-known resource attributes w/ specific names as conventions
    for facts about the objects doing the logging. `job.id` was the first
    one we implemented and we omitted the `runner_*` prefix. in this pr, we
    update it to use it across the board.

--- services/ctl-api/internal/app/runners/service/runner_otel_read_logs.go
+++ services/ctl-api/internal/app/runners/service/runner_otel_read_logs.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"strconv"
+	"time"
 
 	"net/http"
 
@@ -37,37 +38,45 @@ type Page struct {
 // @Success		200				{object}	[]app.OtelLogRecord
 // @Router			/v1/runners/{runner_id}/logs [GET]
 func (s *service) OtelReadLogs(ctx *gin.Context) {
-	// get the otel logs in order of their timstamp (asc)
-	// TODO(fd): add [cursor-based?] pagination
-	// NOTE(fd): the pagination is mocked out but not implemented
+	// get the otel logs in order of their timstamp (desc)
+	// returns pagination details in the header
+	// X-Nuon-API-Next   returns a value, a unix time in nanoseconds rn
+	// X-Nuon-API-Offset accepts a value, a unix time in nanoseconds rn
 
-	// NOTE(fd): consider checking if the runner exists in the given org after looking for logs
+	// NOTE(fd): consider checking if the runner exists in the given org
 	runnerID := ctx.Param("runner_id")
 
-	// pagination facts
-	// we expect a header w/ the value for next in the header. we choose how to handle it internally
-	// the current implemention will use after in a `next` header
-	var after int64
-	afterStr := ctx.GetHeader("X-Nuon-API-Offset")
-	if afterStr == "" {
+	// Pagination Facts
+	// parse the offset
+	var before int64
+	beforeStr := ctx.GetHeader("X-Nuon-API-Offset")
+	if beforeStr == "" {
 		// set default value
-		after = 0
+		before = time.Now().UTC().UnixNano()
+		fmt.Printf("before :: %+v (default)\n", before)
 	} else {
-		afterVal, err := strconv.ParseInt(afterStr, 10, 64)
+		beforeVal, err := strconv.ParseInt(beforeStr, 10, 64)
 		if err != nil {
 			ctx.Error(fmt.Errorf("unable to parse pagination query params: %w", err))
 			return
 		}
-		after = afterVal
+		before = beforeVal
 	}
 
-	// write the logs to the db
-	logs, headers, readerr := s.getRunnerLogs(ctx, runnerID, after)
+	// parse query params
+	job_id := ctx.DefaultQuery("job_id", "")
+	job_execution_id := ctx.DefaultQuery("job_execution_id", "")
+
+	fmt.Printf("job_id=%s job_execution_id=%s\n", job_id, job_execution_id)
+
+	// read logs from chDB
+	logs, headers, readerr := s.getRunnerLogs(ctx, runnerID, before, job_id, job_execution_id)
 	if readerr != nil {
 		ctx.Error(fmt.Errorf("unable to read runner logs: %w", readerr))
 		return
 	}
 
+	// set the header
 	for key, value := range headers {
 		ctx.Header(key, value)
 	}
@@ -75,23 +84,35 @@ func (s *service) OtelReadLogs(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, logs)
 }
 
-func (s *service) getRunnerLogs(ctx context.Context, runnerID string, after int64) ([]app.OtelLogRecord, map[string]string, error) {
+func (s *service) getRunnerLogs(ctx context.Context, runnerID string, before int64, job_id string, job_execution_id string) ([]app.OtelLogRecord, map[string]string, error) {
 	// headers
 	headers := map[string]string{"Range-Units": "items"}
+
 	// prepare a slice to hold all of the record we will be writing
 	otelLogRecords := []app.OtelLogRecord{}
 
 	// get count
 	var count int64
-	countres := s.chDB.WithContext(ctx).Find(&otelLogRecords).Where("runner_id = ?", runnerID).Count(&count)
-	if countres.Error != nil {
-		return nil, headers, fmt.Errorf("unable to retrieve logs: %w", countres.Error)
+	query := s.chDB.WithContext(ctx).Where("runner_id = ?", runnerID)
+	// compose query from values from query params
+
+	if job_id != "" {
+		query = query.Where("runner_job_id = ?", job_id)
+	}
+	if job_execution_id != "" {
+		query = query.Where("runner_job_execution_id = ?", job_execution_id)
 	}
 
-	// get records
-	res := s.chDB.WithContext(ctx).Limit(PageSize).Find(&otelLogRecords).Where("runner_id = ? AND timestamp > ?", runnerID, after).Order("timestamp asc")
+	// Query: get records
+	res := s.chDB.WithContext(ctx).Order("timestamp asc").Limit(PageSize).Where(query).Where("toUnixTimestamp64Nano(timestamp) < ?", before).Find(&otelLogRecords)
 	if res.Error != nil {
 		return nil, headers, fmt.Errorf("unable to retrieve logs: %w", res.Error)
+	}
+
+	// Query: get record count
+	countres := s.chDB.WithContext(ctx).Find(&app.OtelLogRecord{}).Select("id").Where(query).Count(&count)
+	if countres.Error != nil {
+		return nil, headers, fmt.Errorf("unable to retrieve logs: %w", countres.Error)
 	}
 
 	// determine next
@@ -102,10 +123,10 @@ func (s *service) getRunnerLogs(ctx context.Context, runnerID string, after int6
 		last := otelLogRecords[len(otelLogRecords)-1]
 		next = fmt.Sprintf("%d", last.Timestamp.UnixNano())
 	}
+
+	// add headers
 	headers["X-Nuon-API-Next"] = next
 	headers["count"] = strconv.FormatInt(count, 10)
-
-	// parse from json
 
 	return otelLogRecords, headers, nil
 }
