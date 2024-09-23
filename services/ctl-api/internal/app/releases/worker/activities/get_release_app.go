5059ddad314ef910037e25b9252b711b5cea8f60
sam boyer <sam@nuon.co>
2024-09-23 13:13:29 -0400

    feat: Additional slack/email notifications (#6031)
    Added vendor notifications for release success, deploy failure and
    component build failure.
    
    Co-authored-by: nuonbot <bot@nuon.co>

---
+++ services/ctl-api/internal/app/releases/worker/activities/get_release_app.go
@@ -0,0 +1,28 @@
+package activities
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
+)
+
+type GetReleaseAppRequest struct {
+	ReleaseID string `validate:"required"`
+}
+
+// @temporal-gen activity
+// @by-id ReleaseID
+func (a *Activities) GetReleaseApp(ctx context.Context, req *GetReleaseAppRequest) (*app.App, error) {
+	app := &app.App{}
+	res := a.db.WithContext(ctx).
+		Joins("JOIN installs ON installs.app_id = apps.id").
+		Joins("JOIN component_release_steps ON install.id = ANY (component_release_steps.requested_install_ids)").
+		Where("component_release_steps.release_id = ?", req.ReleaseID).
+		First(app)
+	if res.Error != nil {
+		return nil, fmt.Errorf("unable to get app: %w", res.Error)
+	}
+
+	return app, nil
+}
