95a0722a885d5300a84494468c8a8546675f17fc
Jon Morehouse <MorehouseJ09@gmail.com>
2024-09-25 18:45:45 +0100

    feat: add `run-local` for runner + various fixes into core job loops (#6092)
    * chore: add helper admin commands
    
    * fix: runner admin jobs
    
    * fix: waypoint config parsing for sandbox terraform
    
    * chore: fixes to various lifecycle things
    
    * feat: support `run-local` for `runner`
    
    * remove debugging
    
    * update docs
    
    * update nuon-runner-go
    
    ---------
    
    Co-authored-by: nuonbot <bot@nuon.co>

---
+++ services/ctl-api/internal/app/runners/service/admin_get_job.go
@@ -0,0 +1,29 @@
+package service
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// @ID AdminGetRunnerJob
+// @Summary	get a runner job
+// @Description.markdown admin_get_runner_job.md
+// @Param			runner_job_id	path	string					true	"runner ID"
+// @Tags runners/admin
+// @Accept			json
+// @Produce		json
+// @Success		200				{object}	app.RunnerJob
+// @Router			/v1/runner-jobs/{runner_job_id} [GET]
+func (s *service) AdminGetRunnerJob(ctx *gin.Context) {
+	runnerJobID := ctx.Param("runner_job_id")
+
+	runnerJob, err := s.getRunnerJob(ctx, runnerJobID)
+	if err != nil {
+		ctx.Error(fmt.Errorf("unable to get runner job: %w", err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, runnerJob)
+}
--- services/ctl-api/internal/app/runners/worker/activities/get_runner_status.go
+++ services/ctl-api/internal/app/runners/worker/activities/get_runner_status.go
@@ -8,16 +8,17 @@ import (
 )
 
 type GetRunnerStatusRequest struct {
-	RunnerID string `validate:"required"`
+	ID string `validate:"required"`
 }
 
 // @temporal-gen activity
 // @schedule-to-close-timeout 5s
+// @by-id ID
 func (a *Activities) GetRunnerStatus(ctx context.Context, req GetRunnerStatusRequest) (app.RunnerStatus, error) {
 	// NOTE(jm): remove this once the runner health checks are added
 	return app.RunnerStatusActive, nil
 
-	runner, err := a.getRunner(ctx, req.RunnerID)
+	runner, err := a.getRunner(ctx, req.ID)
 	if err != nil {
 		return app.RunnerStatusUnknown, fmt.Errorf("unable to get runner status: %w", err)
 	}
