b7acd23dac467207e2da2e175f551b5bd14ff45e
Jon Morehouse <MorehouseJ09@gmail.com>
2024-09-29 17:34:04 -0600

    feat: embed registry vs running separately (#6134)
    * chore: remove registry from docker-compose
    
    * feat: port registry to run inside of the runner
    
    * chore: `go mod tidy`

---
+++ bins/runner/internal/registry/lifecycle.go
@@ -0,0 +1,30 @@
+package registry
+
+import (
+	"context"
+	"fmt"
+
+	"go.uber.org/fx"
+)
+
+func (s *Registry) LifecycleHook() fx.Hook {
+	return fx.Hook{
+		// start the background loop to update the settings
+		OnStart: func(context.Context) error {
+			go func() {
+				s.ListenAndServe()
+			}()
+
+			return nil
+		},
+
+		// stop the loop and wait for the background goroutine to return
+		OnStop: func(ctx context.Context) error {
+			if err := s.Shutdown(ctx); err != nil {
+				return fmt.Errorf("unable to shut down registry: %w", err)
+			}
+
+			return nil
+		},
+	}
+}
