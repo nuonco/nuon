dcca19fc5b0bae3b650931f1697fd5ab22d9a8db
Dat Boi Diego <fidiego@users.noreply.github.com>
2024-09-29 21:03:00 -0500

    feat: infra/datadog: synthetic monitors for installers (#6132)

--- infra/datadog/synthetics.tf
+++ infra/datadog/synthetics.tf
@@ -22,7 +22,6 @@ resource "datadog_synthetics_test" "main" {
     target   = each.value.assertion.target
   }
 
-
   options_list {
     tick_every = each.value.options_list.tick_every
     retry {
@@ -33,5 +32,43 @@ resource "datadog_synthetics_test" "main" {
       renotify_interval = each.value.options_list.monitor_options.renotify_interval
     }
     monitor_priority = each.value.options_list.monitor_priority
+  }
+}
+
+resource "datadog_synthetics_test" "installers" {
+  for_each = local.vars.installer_synthetics.installers
+
+  name      = "[${var.env}] Installer Uptime - ${each.key}"
+  type      = local.vars.installer_synthetics.config.type
+  subtype   = local.vars.installer_synthetics.config.subtype
+  status    = local.vars.installer_synthetics.config.status
+  message   = local.vars.installer_synthetics.config.message
+  locations = local.vars.installer_synthetics.config.locations
+  tags      = local.vars.installer_synthetics.config.tags
+
+  request_definition {
+    method = local.vars.installer_synthetics.config.request_definition.method
+    url    = "${each.value}"
+  }
+
+  request_headers = local.vars.installer_synthetics.config.request_headers
+
+  assertion {
+    type     = local.vars.installer_synthetics.config.assertion.type
+    operator = local.vars.installer_synthetics.config.assertion.operator
+    target   = local.vars.installer_synthetics.config.assertion.target
+  }
+
+
+  options_list {
+    tick_every = local.vars.installer_synthetics.config.options_list.tick_every
+    retry {
+      count    = local.vars.installer_synthetics.config.options_list.retry.count
+      interval = local.vars.installer_synthetics.config.options_list.retry.interval
+    }
+    monitor_options {
+      renotify_interval = local.vars.installer_synthetics.config.options_list.monitor_options.renotify_interval
+    }
+    monitor_priority = local.vars.installer_synthetics.config.options_list.monitor_priority
   }
 }
