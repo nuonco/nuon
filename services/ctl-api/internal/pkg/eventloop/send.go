6d5d18eb9bd4ced576f3dbea150cd68f5c104ed5
Jon Morehouse <MorehouseJ09@gmail.com>
2024-09-29 19:35:44 -0600

    fix: various issues with component build workflow (#6137)

--- services/ctl-api/internal/pkg/eventloop/send.go
+++ services/ctl-api/internal/pkg/eventloop/send.go
@@ -4,10 +4,18 @@ import (
 	"context"
 	"fmt"
 
+	"go.uber.org/zap"
+
 	"github.com/powertoolsdev/mono/pkg/metrics"
 )
 
 func (a *evClient) Send(ctx context.Context, id string, signal Signal) {
+	if err := a.v.Struct(signal); err != nil {
+		a.mw.Incr("event_loop.signal", metrics.ToStatusTag("invalid signal"))
+		a.l.Error("invalid signal", zap.Error(err))
+		return
+	}
+
 	if signal.Start() {
 		if err := a.startEventLoop(ctx, id, signal); err != nil {
 			a.mw.Incr("event_loop_signal", metrics.ToStatusTag("unable_to_start_event_loop"))
