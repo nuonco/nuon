6894afd2d7da589bdd06a8051b26c6bd61169901
sam boyer <sam@nuon.co>
2024-10-03 06:43:25 -0400

    feat: Runner error taxonomy, begin emitting to sentry (#6108)
    * Initial working impl of runner sentry errs
    
    * Framework error in place too
    
    * Take env info from API

--- bins/runner/internal/pkg/errs/record.go
+++ bins/runner/internal/pkg/errs/record.go
@@ -1,9 +1,18 @@
 package errs
 
-import "go.uber.org/zap"
+import (
+	"errors"
+
+	"github.com/powertoolsdev/mono/bins/runner/internal/pkg/settings"
+	"github.com/powertoolsdev/mono/pkg/errs"
+	"go.uber.org/fx"
+	"go.uber.org/zap"
+)
 
 type Recorder struct {
-	l *zap.Logger
+	l             *zap.Logger
+	sentryEnabled bool
+	settings      *settings.Settings
 }
 
 // Record is used to record errors that can not other wise be handled, such as when doing cleanup work, where we only
@@ -12,8 +21,27 @@ func (r *Recorder) Record(msg string, err error) {
 	r.l.Error(msg, zap.Error(err))
 }
 
-func NewRecorder(l *zap.Logger) *Recorder {
-	return &Recorder{
-		l: l,
+func (r *Recorder) ToSentry(err error) {
+	if r.sentryEnabled {
+		switch {
+		// this is probably the right way - unwrap errors all the way down to see if they're one of our types,
+		// and only rewrap it if it's not one of those types
+		case errors.Is(err, &RunnerHandlerError{}):
+			errs.ReportToSentry(err)
+		case errors.Is(err, &RunnerFrameworkError{}):
+			errs.ReportToSentry(err)
+		default:
+			errs.ReportToSentry(WithFrameworkError(err, ""))
+		}
 	}
+}
+
+func NewRecorder(l *zap.Logger, s *settings.Settings, lc fx.Lifecycle) *Recorder {
+	r := &Recorder{
+		l:        l,
+		settings: s,
+	}
+
+	lc.Append(r.LifecycleHook())
+	return r
 }
