8c665a1d21caf12e0a332cd29b017c54e650b3d7
Jon Morehouse <MorehouseJ09@gmail.com>
2024-10-03 15:05:56 -0600

    fix: set up role for correct namespace (#6191)

--- services/ctl-api/infra/service.tf
+++ services/ctl-api/infra/service.tf
@@ -1,8 +1,13 @@
+locals {
+  name = "ctl-api"
+}
+
 module "service" {
   source = "../../../infra/modules/service"
 
-  name = "ctl-api"
-  env  = var.env
+  name      = local.name
+  env       = var.env
+  namespace = local.namespace
   additional_iam_policies = [
     aws_iam_policy.additional_permissions.arn
   ]
