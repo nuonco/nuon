d3e3972647aad0d9fe3acced48b00613faaf4607
Jon Morehouse <MorehouseJ09@gmail.com>
2024-10-03 13:24:34 -0600

    chore: remove legacy org static token, add service account endpoint (#6175)
    * chore: remove legacy org static token, add service account endpoint
    
    * update
    
    * fix: allow looking up accounts by ID
    
    ---------
    
    Co-authored-by: nuonbot <bot@nuon.co>

--- services/ctl-api/internal/pkg/authz/account.go
+++ services/ctl-api/internal/pkg/authz/account.go
@@ -22,21 +22,24 @@ func (m *Client) FetchAccount(ctx context.Context, acctID string) (*app.Account,
 	return &acct, nil
 }
 
-func (m *Client) FindAccount(ctx context.Context, emailOrSubject string) (*app.Account, error) {
+func (m *Client) FindAccount(ctx context.Context, emailOrSubjectOrID string) (*app.Account, error) {
 	acct := app.Account{}
 	res := m.db.WithContext(ctx).
 		Preload("Roles").
 		Preload("Roles.Org").
 		Preload("Roles.Policies").
 		Where(app.Account{
-			Email: emailOrSubject,
+			Email: emailOrSubjectOrID,
 		}).
 		Or(app.Account{
-			Subject: emailOrSubject,
+			Subject: emailOrSubjectOrID,
+		}).
+		Or(app.Account{
+			ID: emailOrSubjectOrID,
 		}).
 		First(&acct)
 	if res.Error != nil {
-		return nil, fmt.Errorf("unable to find account %s: %w", emailOrSubject, res.Error)
+		return nil, fmt.Errorf("unable to find account %s: %w", emailOrSubjectOrID, res.Error)
 	}
 
 	return &acct, nil
