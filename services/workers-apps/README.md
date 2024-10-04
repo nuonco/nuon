dc426355546bb8bf12a76e30e5996feeb2b3abbe
Jon Morehouse <MorehouseJ09@gmail.com>
2024-10-03 21:36:13 -0600

    chore: remove legacy workers (#6214)

--- services/workers-apps/README.md
+++
@@ -1,4 +0,0 @@
-# workers-apps
-
-Temporal workers for provisioning infra for an app (such as ECR repo, waypoint project etc).
-
--- services/workers-orgs/infra/data.tf
+++
@@ -1,8 +0,0 @@
-data "aws_organizations_organization" "orgs" {
-  provider = aws.mgmt
-}
-
-data "aws_ecr_repository" "ecr_repository" {
-  provider = aws.infra-shared-prod
-  name     = "mono/${local.name}"
-}
