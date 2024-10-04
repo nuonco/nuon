73a60c1e9fefa04881c5fbf775ff670871f19892
Jon Morehouse <MorehouseJ09@gmail.com>
2024-10-03 21:56:21 -0600

    fix: remove additional e2e modules + fix copy script (#6215)
    * fix: remove additional e2e modules + fix copy script
    
    * chore: disconnect worker terraform from repos
    
    ---------
    
    Co-authored-by: nuonbot <bot@nuon.co>

--- infra/terraform/workspaces_services.tf
+++ infra/terraform/workspaces_services.tf
@@ -153,38 +153,6 @@ module "workers-canary-prod" {
   trigger_workspaces              = [module.infra-eks-orgs-prod-main.workspace_id]
 }
 
-module "workers-apps-prod" {
-  source = "./modules/workspace"
-
-  name       = "workers-apps-prod"
-  repo       = "powertoolsdev/mono"
-  auto_apply = true
-  dir        = "services/workers-apps/infra"
-  vars = {
-    env = "prod"
-  }
-  variable_sets                   = ["aws-environment-credentials"]
-  project_id                      = tfe_project.services.id
-  slack_notifications_webhook_url = var.default_slack_notifications_webhook_url
-  trigger_workspaces              = [module.infra-eks-orgs-prod-main.workspace_id]
-}
-
-module "workers-apps-stage" {
-  source = "./modules/workspace"
-
-  name       = "workers-apps-stage"
-  repo       = "powertoolsdev/mono"
-  auto_apply = true
-  dir        = "services/workers-apps/infra"
-  vars = {
-    env = "stage"
-  }
-  variable_sets                   = ["aws-environment-credentials"]
-  project_id                      = tfe_project.services.id
-  slack_notifications_webhook_url = var.default_slack_notifications_webhook_url
-  trigger_workspaces              = [module.infra-eks-orgs-stage-main.workspace_id]
-}
-
 module "workers-executors-prod" {
   source = "./modules/workspace"
 
@@ -217,66 +185,3 @@ module "workers-executors-stage" {
   trigger_workspaces              = [module.infra-eks-orgs-stage-main.workspace_id]
 }
 
-module "workers-installs-prod" {
-  source = "./modules/workspace"
-
-  name       = "workers-installs-prod"
-  repo       = "powertoolsdev/mono"
-  auto_apply = true
-  dir        = "services/workers-installs/infra"
-  vars = {
-    env = "prod"
-  }
-  variable_sets                   = ["aws-environment-credentials"]
-  project_id                      = tfe_project.services.id
-  slack_notifications_webhook_url = var.default_slack_notifications_webhook_url
-  trigger_workspaces              = [module.infra-eks-orgs-prod-main.workspace_id]
-}
-
-module "workers-installs-stage" {
-  source = "./modules/workspace"
-
-  name       = "workers-installs-stage"
-  repo       = "powertoolsdev/mono"
-  auto_apply = true
-  dir        = "services/workers-installs/infra"
-  vars = {
-    env = "stage"
-  }
-  variable_sets                   = ["aws-environment-credentials"]
-  project_id                      = tfe_project.services.id
-  slack_notifications_webhook_url = var.default_slack_notifications_webhook_url
-  trigger_workspaces              = [module.infra-eks-orgs-stage-main.workspace_id]
-}
-
-module "workers-orgs-prod" {
-  source = "./modules/workspace"
-
-  name       = "workers-orgs-prod"
-  repo       = "powertoolsdev/mono"
-  auto_apply = true
-  dir        = "services/workers-orgs/infra"
-  vars = {
-    env = "prod"
-  }
-  variable_sets                   = ["aws-environment-credentials"]
-  project_id                      = tfe_project.services.id
-  slack_notifications_webhook_url = var.default_slack_notifications_webhook_url
-  trigger_workspaces              = [module.infra-eks-orgs-prod-main.workspace_id]
-}
-
-module "workers-orgs-stage" {
-  source = "./modules/workspace"
-
-  name       = "workers-orgs-stage"
-  repo       = "powertoolsdev/mono"
-  auto_apply = true
-  dir        = "services/workers-orgs/infra"
-  vars = {
-    env = "stage"
-  }
-  variable_sets                   = ["aws-environment-credentials"]
-  project_id                      = tfe_project.services.id
-  slack_notifications_webhook_url = var.default_slack_notifications_webhook_url
-  trigger_workspaces              = [module.infra-eks-orgs-stage-main.workspace_id]
-}
