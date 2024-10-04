430edacce87d0cd62c9ae79dcab94683104f690f
Jon Morehouse <MorehouseJ09@gmail.com>
2024-10-03 18:59:56 -0600

    chore: remove legacy api domain (#6208)

--- services/ctl-api/infra/outputs_service.tf
+++ services/ctl-api/infra/outputs_service.tf
@@ -1,11 +1,3 @@
-output "certificate_arn_legacy" {
-  value = module.certificate.acm_certificate_arn
-}
-
-output "public_domain_legacy" {
-  value = "ctl.${local.root_domain}"
-}
-
 output "internal_domain" {
   value = "ctl.${data.aws_route53_zone.private.name}"
 }
