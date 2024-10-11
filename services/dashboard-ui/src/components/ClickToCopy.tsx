143afc1bff670040ac2a68a443fbb7b3d51f27c5
Nat Hamilton <info@nathamilton.com>
2024-10-11 10:04:15 -0400

    fix(ui): copy icon actually copies child text (#6321)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/components/ClickToCopy.tsx
+++ services/dashboard-ui/src/components/ClickToCopy.tsx
@@ -9,13 +9,15 @@ export const ClickToCopy: FC<React.HTMLAttributes<HTMLSpanElement>> = ({
   children,
   ...props
 }) => {
+  const text = children?.valueOf()?.['props']?.children
+
   return (
     <span
       className={classNames('flex items-center gap-2 cursor-pointer', {
         [`${className}`]: Boolean(className),
       })}
-      onClick={(e) => {
-        navigator.clipboard.writeText((e.target as HTMLElement)?.textContent)
+      onClick={() => {
+        navigator.clipboard.writeText(text)
       }}
       title="Click to copy"
       {...props}
