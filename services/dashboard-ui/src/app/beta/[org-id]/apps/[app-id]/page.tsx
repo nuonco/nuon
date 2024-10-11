d4a078c7ee20ee7f560b97d494e20196d4dd1e9b
Nat Hamilton <info@nathamilton.com>
2024-10-11 09:00:01 -0400

    fix(ui): correct orgs page route in breadcrumb (#6320)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/app/beta/[org-id]/apps/[app-id]/page.tsx
+++ services/dashboard-ui/src/app/beta/[org-id]/apps/[app-id]/page.tsx
@@ -43,7 +43,7 @@ export default withPageAuthRequired(
     return (
       <DashboardContent
         breadcrumb={[
-          { href: `/beta/${org.id}`, text: org.name },
+          { href: `/beta/${org.id}/apps`, text: org.name },
           { href: `/beta/${org.id}/apps`, text: 'Apps' },
           { href: `/beta/${org.id}/apps/${app.id}`, text: app.name },
         ]}
