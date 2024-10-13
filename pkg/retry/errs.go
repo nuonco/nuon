a9044ffff9d2edac9defb81e556c07fbf3019ed6
Jon Morehouse <MorehouseJ09@gmail.com>
2024-10-13 12:55:19 -0700

    feat: add pkg/retry and refactor run_local (#6347)
    * feat: add pkg/retry and refactor run_local
    
    * fix: retry package
    
    * more fixes

---
+++ pkg/retry/errs.go
@@ -0,0 +1,26 @@
+package retry
+
+import "errors"
+
+type nonRetryableErr struct {
+	err error
+}
+
+func (e nonRetryableErr) Error() string {
+	return e.err.Error()
+}
+
+func (e nonRetryableErr) Unwrap() error {
+	return e.err
+}
+
+func AsNonRetryable(err error) error {
+	return &nonRetryableErr{
+		err: err,
+	}
+}
+
+func IsNonRetryable(err error) bool {
+	var nre nonRetryableErr
+	return errors.As(err, &nre)
+}
---
+++ pkg/retry/retryer.go
@@ -0,0 +1,55 @@
+package retry
+
+import (
+	"context"
+	"time"
+
+	smithytime "github.com/aws/smithy-go/time"
+	"github.com/pkg/errors"
+)
+
+type retryer struct {
+	maxAttempts int
+	sleep       time.Duration
+
+	fn          RetryFn
+	retryCBHook RetryCBHook
+
+	timeout time.Duration
+}
+
+func (r *retryer) exec(ctx context.Context) error {
+	attempt := 0
+
+	if r.timeout > time.Duration(0) {
+		var cancelFn context.CancelFunc
+		ctx, cancelFn = context.WithTimeout(ctx, r.timeout)
+		defer cancelFn()
+	}
+
+	var err error
+	for attempt <= r.maxAttempts || r.maxAttempts < 0 {
+		if attempt > 0 {
+			if err = r.retryCBHook(attempt); err != nil {
+				return errors.Wrap(err, "retry callback hook failed")
+			}
+
+			smithytime.SleepWithContext(ctx, r.sleep)
+		}
+		if err = ctx.Err(); err != nil {
+			return errors.Wrap(err, "parent context closed")
+		}
+
+		if err = r.fn(ctx); err == nil {
+			return nil
+		}
+
+		if IsNonRetryable(err) {
+			return errors.Wrap(err, "not retryable")
+		}
+
+		attempt += 1
+	}
+
+	return errors.Wrap(err, "maximum attempts reached")
+}
