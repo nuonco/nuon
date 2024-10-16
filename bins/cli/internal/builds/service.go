b6157298307bd78a4cbae1f32572c539d95eefd1
Rob <alphavibe@gmail.com>
2024-10-16 10:19:58 -0700

    fix: cli opens browser (#6256)

--- bins/cli/internal/builds/service.go
+++ bins/cli/internal/builds/service.go
@@ -1,13 +1,27 @@
 package builds
 
-import "github.com/nuonco/nuon-go"
+import (
+	"github.com/nuonco/nuon-go"
+	"github.com/powertoolsdev/mono/bins/cli/internal/config"
+	"github.com/pterm/pterm"
+)
 
 type Service struct {
 	api nuon.Client
+	cfg *config.Config
 }
 
-func New(apiClient nuon.Client) *Service {
+func New(apiClient nuon.Client, cfg *config.Config) *Service {
 	return &Service{
 		api: apiClient,
+		cfg: cfg,
 	}
+}
+
+func (s *Service) printAppNotSetMsg() {
+	pterm.DefaultBasicText.Printfln("current app is not set, use %s to set one", pterm.LightMagenta("apps select"))
+}
+
+func (s *Service) printOrgNotSetMsg() {
+	pterm.DefaultBasicText.Printfln("current org is not set, use %s to set one", pterm.LightMagenta("orgs select"))
 }
