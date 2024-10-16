179df09dd59c5c552cafd84ac8d265f395de277d
Rob <alphavibe@gmail.com>
2024-10-16 15:59:59 -0700

    feat: switch to browser pkg for cli (#6448)

--- bins/cli/internal/installs/deploy_logs.go
+++ bins/cli/internal/installs/deploy_logs.go
@@ -3,9 +3,8 @@ package installs
 import (
 	"context"
 	"fmt"
-	"os/exec"
-	"runtime"
 
+	"github.com/pkg/browser"
 	"github.com/powertoolsdev/mono/bins/cli/internal/lookup"
 	"github.com/powertoolsdev/mono/bins/cli/internal/ui"
 )
@@ -22,22 +21,7 @@ func (s *Service) DeployLogs(ctx context.Context, installID, deployID, installCo
 	}
 
 	url := fmt.Sprintf("%s/%s/installs/%s/components/%s/deploys/%s", cfg.DashboardURL, s.cfg.OrgID, installID, installComponentID, deployID)
-	var cmd *exec.Cmd
+	browser.OpenURL(url)
 
-	// Determine the OS and set the command accordingly
-	switch runtime.GOOS {
-	case "linux":
-		cmd = exec.Command("xdg-open", url)
-	case "darwin": // macOS
-		cmd = exec.Command("open", url)
-	case "windows":
-		cmd = exec.Command("rundll32", "url.dll,FileProtocolHandler", url)
-	default:
-		// If the OS is not supported, print the URL
-		ui.PrintLn("Use the following URL to view the logs")
-		ui.PrintLn(url)
-		return nil
-	}
-
-	return cmd.Start()
+	return nil
 }
