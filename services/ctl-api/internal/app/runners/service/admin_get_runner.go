d9b88acf50d09d14f26df090f018dd905c9c1c9d
Jon Morehouse <MorehouseJ09@gmail.com>
2024-10-15 17:32:06 -0700

    fix: update runner version + add admin endpoints (#6395)
    * fix: update runner version + add admin endpoints
    
    * fix: support optional propagator
    
    * fix endpoint
    
    ---------
    
    Co-authored-by: nuonbot <bot@nuon.co>

---
+++ services/ctl-api/internal/app/runners/service/admin_get_runner.go
@@ -0,0 +1,28 @@
+package service
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// @ID AdminGetRunner
+// @Summary	get a runner
+// @Description.markdown get_runner.md
+// @Tags runners/admin
+// @Accept			json
+// @Param	runner_id	path	string	true	"runner ID to fetc"
+// @Produce		json
+// @Success		200				{object}	app.Runner
+// @Router			/v1/runners/{runner_id} [GET]
+func (s *service) AdminGetRunner(ctx *gin.Context) {
+	runnerID := ctx.Param("runner_id")
+	runner, err := s.getRunner(ctx, runnerID)
+	if err != nil {
+		ctx.Error(fmt.Errorf("unable to get runner: %w", err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, runner)
+}
---
+++ services/ctl-api/internal/app/runners/service/admin_get_runner_settings.go
@@ -0,0 +1,29 @@
+package service
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// @ID AdminGetRunnerSettings
+// @Summary	get a runner settings
+// @Description.markdown admin_get_runner_settings.md
+// @Param			runner_id	path	string					true	"runner ID"
+// @Tags runners/admin
+// @Accept			json
+// @Produce		json
+// @Success		200				{object}	app.RunnerGroupSettings
+// @Router			/v1/runners/{runner_id}/settings [GET]
+func (s *service) AdminGetRunnerSettings(ctx *gin.Context) {
+	runnerID := ctx.Param("runner_id")
+
+	runner, err := s.getRunner(ctx, runnerID)
+	if err != nil {
+		ctx.Error(fmt.Errorf("unable to get runner job: %w", err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, runner.RunnerGroup.Settings)
+}
