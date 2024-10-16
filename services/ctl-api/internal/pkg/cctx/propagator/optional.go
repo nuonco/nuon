70b9af20aee63703fa6c47969799f65bb109284d
Jon Morehouse <MorehouseJ09@gmail.com>
2024-10-15 17:32:06 -0700

    fix: update runner version + add admin endpoints (#6395)
    * fix: update runner version + add admin endpoints
    
    * fix: support optional propagator
    
    * fix endpoint
    
    ---------
    
    Co-authored-by: nuonbot <bot@nuon.co>

---
+++ services/ctl-api/internal/pkg/cctx/propagator/optional.go
@@ -0,0 +1,51 @@
+package propagator
+
+import (
+	"context"
+
+	"go.temporal.io/sdk/workflow"
+	"go.uber.org/zap"
+)
+
+var _ workflow.ContextPropagator = (*optionalPropagator)(nil)
+
+type optionalPropagator struct {
+	l          *zap.Logger
+	propagator workflow.ContextPropagator
+}
+
+func (s *optionalPropagator) Inject(ctx context.Context, writer workflow.HeaderWriter) error {
+	if err := s.propagator.Inject(ctx, writer); err != nil {
+		s.l.Error("propagator inject failed", zap.Error(err))
+	}
+
+	return nil
+}
+
+func (s *optionalPropagator) InjectFromWorkflow(ctx workflow.Context, writer workflow.HeaderWriter) error {
+	if err := s.propagator.InjectFromWorkflow(ctx, writer); err != nil {
+		s.l.Error("propagator inject from workflow failed", zap.Error(err))
+	}
+
+	return nil
+}
+
+func (s *optionalPropagator) Extract(ctx context.Context, reader workflow.HeaderReader) (context.Context, error) {
+	newCtx, err := s.propagator.Extract(ctx, reader)
+	if err != nil {
+		s.l.Error("propagator extract failed", zap.Error(err))
+		return ctx, nil
+	}
+
+	return newCtx, nil
+}
+
+func (s *optionalPropagator) ExtractToWorkflow(ctx workflow.Context, reader workflow.HeaderReader) (workflow.Context, error) {
+	newCtx, err := s.propagator.ExtractToWorkflow(ctx, reader)
+	if err != nil {
+		s.l.Error("propagator extract to workflow failed", zap.Error(err))
+		return ctx, nil
+	}
+
+	return newCtx, nil
+}
