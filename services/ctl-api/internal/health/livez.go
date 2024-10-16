29ea01b46fc81c774f841094e603030455a525ad
Jon Morehouse <MorehouseJ09@gmail.com>
2024-10-16 10:41:27 -0700

    chore: prevent clickhouse from failing api (#6434)

--- services/ctl-api/internal/health/livez.go
+++ services/ctl-api/internal/health/livez.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"go.temporal.io/sdk/client"
 
+	"github.com/powertoolsdev/mono/pkg/metrics"
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/middlewares/stderr"
 )
 
@@ -20,41 +21,63 @@ func (s *Service) GetLivezHandler(ctx *gin.Context) {
 		return
 	}
 	if err := sqlDB.PingContext(ctx); err != nil {
+		s.mw.Incr("healthcheck.check", metrics.ToTags(map[string]string{
+			"system": "psql",
+			"status": "unable_to_ping",
+		}))
 		ctx.Error(stderr.ErrSystem{
 			Err:         err,
 			Description: "unable to ping psql db",
 		})
 		return
 	}
+	s.mw.Incr("healthcheck.check", metrics.ToTags(map[string]string{
+		"system": "psql",
+		"status": "ok",
+	}))
+
+	degraded := make([]string, 0)
 
 	// ping ch
 	chDB, err := s.chDB.DB()
 	if err != nil {
-		ctx.Error(stderr.ErrSystem{
-			Err:         err,
-			Description: "unable to get clickhouse connection",
-		})
-		return
-	}
-	if err := chDB.PingContext(ctx); err != nil {
-		ctx.Error(stderr.ErrSystem{
-			Err:         err,
-			Description: "unable to ping clickhouse db",
-		})
-		return
+		degraded = append(degraded, "ch")
+		s.mw.Incr("healthcheck.check", metrics.ToTags(map[string]string{
+			"system": "psql",
+			"status": "unable_to_connect",
+		}))
+	} else {
+		// attempt to ping clickhouse, if we get a connection
+		if err := chDB.PingContext(ctx); err != nil {
+			degraded = append(degraded, "ch")
+			s.mw.Incr("healthcheck.check", metrics.ToTags(map[string]string{
+				"system": "ch",
+				"status": "unable_to_ping",
+			}))
+		}
+
+		s.mw.Incr("healthcheck.check", metrics.ToTags(map[string]string{
+			"system": "ch",
+			"status": "ok",
+		}))
 	}
 
 	// ping temporal
 	_, err = s.tclient.CheckHealth(ctx, &client.CheckHealthRequest{})
 	if err != nil {
-		ctx.Error(stderr.ErrSystem{
-			Err:         err,
-			Description: "unable to check temporal health",
-		})
-		return
+		degraded = append(degraded, "ch")
+		s.mw.Incr("healthcheck.check", metrics.ToTags(map[string]string{
+			"system": "temporal",
+			"status": "unable_to_ping",
+		}))
 	}
+	s.mw.Incr("healthcheck.check", metrics.ToTags(map[string]string{
+		"system": "temporal",
+		"status": "ok",
+	}))
 
 	ctx.JSON(http.StatusOK, map[string]interface{}{
-		"status": "ok",
+		"status":   "ok",
+		"degraded": degraded,
 	})
 }
