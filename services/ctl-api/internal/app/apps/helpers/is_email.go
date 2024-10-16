7391062622bac2460a29fe861d4dd472124eb11b
Rob <alphavibe@gmail.com>
2024-10-16 13:53:56 -0700

    feat: require email on invite (#6375)
    Co-authored-by: Dat Boi Diego <fidiego@users.noreply.github.com>

---
+++ services/ctl-api/internal/app/apps/helpers/is_email.go
@@ -0,0 +1,11 @@
+package helpers
+
+import (
+	"regexp"
+)
+
+func IsEmail(email string) bool {
+	regex := `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
+	re := regexp.MustCompile(regex)
+	return re.MatchString(email)
+}
