112057bbd0d98535b4e39921db1503ed9ef8c2af
Jon Morehouse <MorehouseJ09@gmail.com>
2024-10-17 17:08:35 -0600

    feat: double output logs for runner in dev (#6492)
    * feat: double output logs for runner in dev
    
    * chore: ensure seed-from-env pins version too
    
    * chore: update versions
    
    * update

--- bins/runner/internal/pkg/log/otel_job.go
+++ bins/runner/internal/pkg/log/otel_job.go
@@ -2,13 +2,32 @@ package log
 
 import (
 	"go.opentelemetry.io/contrib/bridges/otelzap"
-	"go.opentelemetry.io/otel/log"
+	"go.opentelemetry.io/otel/sdk/log"
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+
+	"github.com/pkg/errors"
+
+	"github.com/powertoolsdev/mono/bins/runner/internal"
 )
 
-func NewOTELJobLogger(lp log.LoggerProvider) *zap.Logger {
+func NewOTELJobLogger(cfg *internal.Config, lp *log.LoggerProvider) (*zap.Logger, error) {
 	zapCore := otelzap.NewCore("oteljob",
 		otelzap.WithLoggerProvider(lp))
-	l := zap.New(zapCore)
-	return l
+
+	dev, err := NewDev(cfg)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to get dev logger")
+	}
+
+	// if running inside of nuonctl, we automatically print all logs to stdout as well
+	if cfg.IsNuonctl {
+		double := zap.WrapCore(func(c zapcore.Core) zapcore.Core {
+			return dev.Core()
+		})
+
+		return zap.New(zapCore, double), nil
+	}
+
+	return zap.New(zapCore), nil
 }
