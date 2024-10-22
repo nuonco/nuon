cbea58d2b07d922c0dddef7bffdb99b895df8a5d
Nat Hamilton <info@nathamilton.com>
2024-10-22 16:55:26 -0400

    chore(ui): revert nctl cmds (#6626)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/next.config.mjs
+++ services/dashboard-ui/next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  swcMinify: true,
+  fastRefresh: true,
+  concurrentFeatures: true,
+  optimizeFonts: false,
   images: {
     remotePatterns: [
       {
