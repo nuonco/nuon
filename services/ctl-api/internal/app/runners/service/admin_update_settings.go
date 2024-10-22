f1d14cb500861f966ed135769af7d71e256dcb98
Jordan Acosta <jordan@nuon.co>
2024-10-22 15:15:34 -0700

    fix(services/ctl-api): update runner settings description (#6449)
    Co-authored-by: nuonbot <bot@nuon.co>

--- services/ctl-api/internal/app/runners/service/admin_update_settings.go
+++ services/ctl-api/internal/app/runners/service/admin_update_settings.go
@@ -20,8 +20,8 @@ type AdminUpdateRunnerSettingsRequest struct {
 }
 
 // @ID AdminUpdateRunnerSettings
-// @Summary	create a job
-// @Description.markdown create_job.md
+// @Summary	update a runner's settings
+// @Description.markdown update_runner_settings.md
 // @Param		runner_id	path	string						true	"runner ID"
 // @Param		req				body	AdminUpdateRunnerSettingsRequest	true	"Input"
 // @Tags runners/admin
