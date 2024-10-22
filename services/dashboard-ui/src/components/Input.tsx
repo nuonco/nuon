8f9a2b8401165c9d93eca738257ce2764b1493ab
Nat Hamilton <info@nathamilton.com>
2024-10-22 14:11:09 -0400

    feat(ui): clear log filtering (#6616)
    * feat(ui): added checkbox input
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * refactor(ui): stop passing columnFilters to logs preview
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat(ui): clear log filters, show all attr text
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/components/Input.tsx
+++ services/dashboard-ui/src/components/Input.tsx
@@ -26,3 +26,29 @@ export const RadioInput: FC<IRadioInput> = ({
     </label>
   )
 }
+
+export interface ICheckboxInput extends React.HTMLAttributes<HTMLInputElement> {
+  checked?: boolean
+  name: string
+  labelText: React.ReactNode
+  value: string
+}
+
+export const CheckboxInput: FC<ICheckboxInput> = ({
+  className,
+  labelText,
+  ...props
+}) => {
+  return (
+    <label className="flex gap-4 items-center w-full px-4 py-2 cursor-pointer hover:bg-black/5 focus:bg-black/5 active:bg-black/10 dark:hover:bg-white/5 dark:focus:bg-white/5 dark:active:bg-white/10">
+      <input
+        className={classNames('accent-primary-600 w-auto h-[14px]', {
+          [`${className}`]: Boolean(className),
+        })}
+        {...props}
+        type="checkbox"
+      />
+      <span className="font-medium text-sm">{labelText}</span>
+    </label>
+  )
+}
