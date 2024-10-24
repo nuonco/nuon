22a545d08f8449e3d97c268cb5cc15ae4141d85a
Nat Hamilton <info@nathamilton.com>
2024-10-24 08:07:13 -0400

    feat(ui): empty states for apps, installs, components (#6667)
    * refactor(ui): show empty inputs message on app page
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat(ui): empty apps, installs, components messages
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat(ui): added empty messages to apps, installs & components pages
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * chore(ui): removed console.log
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * style(ui): lint obey
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    Co-authored-by: nuonbot <bot@nuon.co>

---
+++ services/dashboard-ui/src/components/NoApps.tsx
@@ -0,0 +1,43 @@
+import React, { type FC } from 'react'
+import { Link } from '@/components/Link'
+import { Heading, Text, Code } from '@/components/Typography'
+
+export const NoApps: FC = () => {
+  return (
+    <div className="max-w-xl flex flex-col gap-8">
+      <div className="flex flex-col gap-2">
+        <Heading variant="title">No apps configured</Heading>
+        <Text className="!text-lg !leading-loose">
+          Package your existing application code and infrastructure to create
+          bring-your-own-cloud installable versions of your product. Nuon apps
+          are versions of your application code and infrastructure that can be
+          deployed into a customer cloud account. <br />
+          <Link
+            className="!inline"
+            href="https://docs.nuon.co/concepts/apps"
+            target="_blank"
+          >
+            Learn more
+          </Link>{' '}
+          about apps.
+        </Text>
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <Heading>Create an app with the Nuon CLI</Heading>
+        <Text>
+          Create your first app using the Nuon CLI with the{' '}
+          <Code variant="inline">nuon apps create -n YOUR-APP-NAME</Code>{' '}
+          command. Follow our{' '}
+          <Link
+            href="https://docs.nuon.co/tutorials/aws-ecs-app-tutorial"
+            target="_blank"
+          >
+            app creation guide
+          </Link>{' '}
+          to get started.
+        </Text>
+      </div>
+    </div>
+  )
+}
---
+++ services/dashboard-ui/src/components/NoInstalls.tsx
@@ -0,0 +1,36 @@
+import React, { type FC } from 'react';
+import { Link } from '@/components/Link'
+import { Heading, Text, Code } from '@/components/Typography'
+
+export const NoInstalls: FC = () => {
+ return (
+    <div className="max-w-xl flex flex-col gap-8">
+      <div className="flex flex-col gap-2">
+        <Heading variant="title">No installs created</Heading>
+        <Text className="!text-lg !leading-loose">
+         An install is an instance of an application running in a customer cloud account. When an install is created, each component in your application will be deployed in the correct order, alongside a runner which is responsible for managing the software. <br />
+          <Link className="!inline" href="https://docs.nuon.co/concepts/installs" target="_blank">
+            Learn more
+          </Link>{' '}
+          about installs.
+        </Text>
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <Heading>Create an install with the Nuon CLI</Heading>
+        <Text>
+          If you&apos;ve already setup an AWS account with Nuon access create an install with the{' '}
+          <Code variant="inline">nuon installs create -n YOUR-INSTALL-NAME -r AWS-REGION -o ARN-AWS-IAM</Code>{' '}
+          command. Or follow our{' '}
+          <Link
+            href="https://docs.nuon.co/guides/install-access-permissions#install-access-permissions"
+            target="_blank"
+          >
+            install access permissions guide
+          </Link>{' '}
+          to get started.
+        </Text>
+      </div>
+    </div>
+  )
+}
