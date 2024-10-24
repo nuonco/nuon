908684d53f36ee6d71dba5ffe83ca2b8ba26ff3d
Dat Boi Diego <fidiego@users.noreply.github.com>
2024-10-24 19:36:12 -0400

    chore: infra/eks: karpenter - update to 0.37.3 (#6703)

--- infra/eks/karpenter.tf
+++ infra/eks/karpenter.tf
@@ -50,7 +50,7 @@ resource "helm_release" "karpenter" {
   chart      = "karpenter"
   name       = "karpenter"
   repository = "oci://public.ecr.aws/karpenter"
-  version    = "0.37.2"
+  version    = "0.37.3"
 
   values = [
     # https://github.com/aws/karpenter-provider-aws/blob/release-v0.37.x/charts/karpenter/values.yaml
