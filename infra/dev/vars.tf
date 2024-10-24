2adeec5699c984d36824b92faf36df49efc376b2
Dat Boi Diego <fidiego@users.noreply.github.com>
2024-10-24 12:19:28 -0400

    feat: infra/dev: add a vpc for testing byovpc (#6674)
    uses the new module

--- infra/dev/vars.tf
+++ infra/dev/vars.tf
@@ -8,7 +8,16 @@ locals {
   region      = "us-west-2"
 
   tags = {
-    service     = local.name
-    terraform   = "${local.name}"
+    service   = local.name
+    terraform = local.name
+  }
+
+  networks = {
+    sandbox = {
+      cidr             = "10.128.0.0/16"
+      public_subnets   = ["10.128.0.0/26", "10.128.0.64/26", "10.128.0.128/26"]
+      private_subnets  = ["10.128.128.0/24", "10.128.129.0/24", "10.128.130.0/24"]
+      database_subnets = ["10.128.131.0/24", "10.128.132.0/24"]
+    }
   }
 }
---
+++ infra/dev/vpc.tf
@@ -0,0 +1,12 @@
+module "noun-vpc" {
+
+  source  = "nuonco/vpc/aws"
+  version = "~> 1.1.0"
+
+  name = "byovpc"
+  cidr = local.networks["sandbox"]["cidr"]
+
+  private_subnets  = local.networks["sandbox"]["private_subnets"]
+  public_subnets   = local.networks["sandbox"]["public_subnets"]
+  database_subnets = local.networks["sandbox"]["database_subnets"]
+}
