4a3625cbbbab196ddca5856ca43b3acdccf8d3f9
Jon Morehouse <MorehouseJ09@gmail.com>
2024-10-24 16:54:05 -0600

    feat: improve terraform log parsing (#6701)
    improve logs

--- pkg/zaphclog/terraform.go
+++ pkg/zaphclog/terraform.go
@@ -3,27 +3,43 @@ package zaphclog
 import (
 	"encoding/json"
 
+	"github.com/mitchellh/mapstructure"
 	"github.com/pkg/errors"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 type terraformLineOutput struct {
-	Level string `json:"@level"`
-	Msg   string `json:"@message"`
+	Level     string `mapstructure:"@level"`
+	Msg       string `mapstructure:"@message"`
+	Timestamp string `mapsturcture:"@timestamp"`
+
+	Other map[string]interface{} `mapstructure:",remain"`
 }
 
 func (z *zaphclogWriter) writeTerraform(byts []byte) error {
+	var obj map[string]interface{}
+	if err := json.Unmarshal(byts, &obj); err != nil {
+		return errors.Wrap(err, "unable to parse to attrs")
+	}
+
 	var tfLine terraformLineOutput
-	if err := json.Unmarshal(byts, &tfLine); err != nil {
-		return errors.Wrap(err, "assuming not terraform json")
+	if err := mapstructure.Decode(obj, &tfLine); err != nil {
+		return errors.Wrap(err, "unable to decode to terraform output")
+	}
+
+	attrs := make([]zapcore.Field, 0, len(tfLine.Other))
+	for k, v := range tfLine.Other {
+		attrs = append(attrs, zap.Any(k, v))
 	}
 
 	switch tfLine.Level {
 	case "info":
-		z.l.Info(tfLine.Msg)
+		z.zl.Info(tfLine.Msg, attrs...)
 	case "error":
-		z.l.Error(tfLine.Msg)
+		z.zl.Error(tfLine.Msg, attrs...)
 	case "warning", "warn":
-		z.l.Warn(tfLine.Msg)
+		z.zl.Warn(tfLine.Msg, attrs...)
 	}
 
 	return nil
