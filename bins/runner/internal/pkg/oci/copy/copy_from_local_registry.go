923aa57a4c38327f273f94b0fa55f9ffb6a1c876
Jon Morehouse <MorehouseJ09@gmail.com>
2024-10-24 23:50:52 -0600

    chore: fixes for docker builds + local dev (#6714)
    * fix: update
    
    * more fixes
    
    * fix: org IAM grants worker access
    
    * sigh, at least kaniko works
    
    * refactor local runs

--- bins/runner/internal/pkg/oci/copy/copy_from_local_registry.go
+++ bins/runner/internal/pkg/oci/copy/copy_from_local_registry.go
@@ -4,27 +4,21 @@ import (
 	"context"
 
 	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
+	"github.com/pkg/errors"
 	"oras.land/oras-go/v2"
+	"oras.land/oras-go/v2/registry/remote"
 
 	"github.com/powertoolsdev/mono/bins/runner/internal/pkg/oci"
+	"github.com/powertoolsdev/mono/bins/runner/internal/pkg/registry/local"
 	"github.com/powertoolsdev/mono/pkg/plugins/configs"
 )
 
-const (
-	localRegistryRepositoryName string = "runner"
-	localRegistryAddress        string = "http://localhost:5000"
-)
-
 func (c *copier) CopyFromLocalRegistry(ctx context.Context, srcTag string, dstCfg *configs.OCIRegistryRepository, dstTag string) (*ocispec.Descriptor, error) {
-	srcCfg := &configs.OCIRegistryRepository{
-		RegistryType: configs.OCIRegistryTypePublicOCI,
-		Repository:   localRegistryRepositoryName,
-		LoginServer:  localRegistryAddress,
-	}
-
-	srcRepo, err := oci.GetRepo(ctx, srcCfg)
+	localRepo := local.GetCopyRepo()
+	repo, err := remote.NewRepository(localRepo)
+	repo.PlainHTTP = true
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, "unable to get local repo")
 	}
 
 	dstRepo, err := oci.GetRepo(ctx, dstCfg)
@@ -32,7 +26,7 @@ func (c *copier) CopyFromLocalRegistry(ctx context.Context, srcTag string, dstCf
 		return nil, err
 	}
 
-	res, err := oras.Copy(ctx, srcRepo, srcTag, dstRepo, dstTag, oras.DefaultCopyOptions)
+	res, err := oras.Copy(ctx, repo, srcTag, dstRepo, dstTag, oras.DefaultCopyOptions)
 	if err != nil {
 		return nil, err
 	}
---
+++ bins/runner/internal/pkg/registry/local/tag.go
@@ -0,0 +1,36 @@
+package local
+
+import (
+	"fmt"
+)
+
+// When we are building and pushing images locally, we have to do things _slightly_ different, to ensure we interoperate
+// with the docker vm correctly.
+//
+// When using `nctl run-local`, we need to tag and push the image as `host.containers.internal`, because the docker
+// build is happening _inside_ the vm. This will push it to our local registry running on port 5000 (ultimately, on the
+// host)
+//
+// When we copy the image from the local registry to ECR, we can always access it as localhost:5000/runner.
+//
+// For running builds inside kaniko, we can simply use localhost:5000 for everything.
+
+// Return a tag that can be used to build+push from the run-local environment
+func GetLocalTag(version string) string {
+	return fmt.Sprintf("host.containers.internal:5000/runner:%s", version)
+}
+
+// Return a tag that can be used inside kaniko
+func GetKanikoTag(version string) string {
+	return fmt.Sprintf("localhost:5000/runner:%s", version)
+}
+
+// Return the tag we _always_ use for copying
+func GetCopyTag(version string) string {
+	return fmt.Sprintf("localhost:5000/runner:%s", version)
+}
+
+// Return the repo we _always_ use for copying
+func GetCopyRepo() string {
+	return "localhost:5000/runner"
+}
