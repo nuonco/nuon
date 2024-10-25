4103aad37807bd77e45ce3e0e465a3c0586dc482
Dat Boi Diego <fidiego@users.noreply.github.com>
2024-10-25 13:38:11 -0400

    feat: infra/eks/break out nodepools (#6727)
    * chore: infra/eks: break out nodepool into its own file
    
    * feat: infra/eks: refactor and add new ec2nodeclasses

---
+++ infra/eks/ec2nodeclasses.tf
@@ -0,0 +1,193 @@
+# uses locals from karpenter.tf
+
+# "randomize" node TTLs so that all nodes across all clusters
+# aren't going down simultaneously
+resource "random_integer" "node_ttl" {
+  min = 60 * 60 * 11 # 11 hours
+  max = 60 * 60 * 17 # 17 hours
+
+  seed = local.karpenter.cluster_name
+  keepers = {
+    cluster_version = local.cluster_version
+  }
+}
+
+# docs: https://karpenter.sh/v0.37/getting-started/getting-started-with-karpenter/#5-create-nodepool
+resource "kubectl_manifest" "karpenter_ec2nodeclass" {
+  yaml_body = yamlencode({
+    apiVersion = "karpenter.k8s.aws/v1beta1"
+    kind       = "EC2NodeClass"
+    metadata = {
+      name = "default"
+    }
+    spec = {
+      amiFamily       = "AL2"
+      instanceProfile = aws_iam_instance_profile.karpenter.name # https://karpenter.sh/v0.32/concepts/nodeclasses/#specinstanceprofile
+      subnetSelectorTerms = [
+        {
+          tags = {
+            "karpenter.sh/discovery" = local.karpenter.discovery_value
+          }
+        }
+      ]
+      securityGroupSelectorTerms = [
+        {
+          tags = {
+            "karpenter.sh/discovery" = local.karpenter.discovery_value
+          }
+        }
+      ]
+      tags = {
+        "karpenter.sh/discovery" = local.karpenter.discovery_value
+      }
+    }
+  })
+
+  depends_on = [
+    helm_release.karpenter
+  ]
+}
+
+# docs: https://karpenter.sh/v0.37/getting-started/getting-started-with-karpenter/#5-create-nodepool
+resource "kubectl_manifest" "clickhouse_installation_ec2nodeclass" {
+  yaml_body = yamlencode({
+    apiVersion = "karpenter.k8s.aws/v1beta1"
+    kind       = "EC2NodeClass"
+    metadata = {
+      name = "clickhouse-installation"
+    }
+    spec = {
+      amiFamily       = "AL2"
+      instanceProfile = aws_iam_instance_profile.karpenter.name # https://karpenter.sh/v0.32/concepts/nodeclasses/#specinstanceprofile
+      subnetSelectorTerms = [
+        {
+          tags = {
+            "karpenter.sh/discovery" = local.karpenter.discovery_value
+          }
+        }
+      ]
+      securityGroupSelectorTerms = [
+        {
+          tags = {
+            "karpenter.sh/discovery" = local.karpenter.discovery_value
+          }
+        }
+      ]
+      tags = {
+        "karpenter.sh/discovery" = local.karpenter.discovery_value
+      }
+    }
+  })
+
+  depends_on = [
+    helm_release.karpenter
+  ]
+}
+
+# docs: https://karpenter.sh/v0.37/getting-started/getting-started-with-karpenter/#5-create-nodepool
+resource "kubectl_manifest" "clickhouse_keeper_ec2nodeclass" {
+  yaml_body = yamlencode({
+    apiVersion = "karpenter.k8s.aws/v1beta1"
+    kind       = "EC2NodeClass"
+    metadata = {
+      name = "clickhouse-keeper"
+    }
+    spec = {
+      amiFamily       = "AL2"
+      instanceProfile = aws_iam_instance_profile.karpenter.name # https://karpenter.sh/v0.32/concepts/nodeclasses/#specinstanceprofile
+      subnetSelectorTerms = [
+        {
+          tags = {
+            "karpenter.sh/discovery" = local.karpenter.discovery_value
+          }
+        }
+      ]
+      securityGroupSelectorTerms = [
+        {
+          tags = {
+            "karpenter.sh/discovery" = local.karpenter.discovery_value
+          }
+        }
+      ]
+      tags = {
+        "karpenter.sh/discovery" = local.karpenter.discovery_value
+      }
+    }
+  })
+
+  depends_on = [
+    helm_release.karpenter
+  ]
+}
+
+# docs: https://karpenter.sh/v0.37/getting-started/getting-started-with-karpenter/#5-create-nodepool
+resource "kubectl_manifest" "ctl_api_api_ec2nodeclass" {
+  yaml_body = yamlencode({
+    apiVersion = "karpenter.k8s.aws/v1beta1"
+    kind       = "EC2NodeClass"
+    metadata = {
+      name = "ctl-api-api"
+    }
+    spec = {
+      amiFamily       = "AL2"
+      instanceProfile = aws_iam_instance_profile.karpenter.name # https://karpenter.sh/v0.32/concepts/nodeclasses/#specinstanceprofile
+      subnetSelectorTerms = [
+        {
+          tags = {
+            "karpenter.sh/discovery" = local.karpenter.discovery_value
+          }
+        }
+      ]
+      securityGroupSelectorTerms = [
+        {
+          tags = {
+            "karpenter.sh/discovery" = local.karpenter.discovery_value
+          }
+        }
+      ]
+      tags = {
+        "karpenter.sh/discovery" = local.karpenter.discovery_value
+      }
+    }
+  })
+
+  depends_on = [
+    helm_release.karpenter
+  ]
+}
+
+# docs: https://karpenter.sh/v0.37/getting-started/getting-started-with-karpenter/#5-create-nodepool
+resource "kubectl_manifest" "ctl_api_worker_ec2nodeclass" {
+  yaml_body = yamlencode({
+    apiVersion = "karpenter.k8s.aws/v1beta1"
+    kind       = "EC2NodeClass"
+    metadata = {
+      name = "ctl-api-worker"
+    }
+    spec = {
+      amiFamily       = "AL2"
+      instanceProfile = aws_iam_instance_profile.karpenter.name # https://karpenter.sh/v0.32/concepts/nodeclasses/#specinstanceprofile
+      subnetSelectorTerms = [
+        {
+          tags = {
+            "karpenter.sh/discovery" = local.karpenter.discovery_value
+          }
+        }
+      ]
+      securityGroupSelectorTerms = [
+        {
+          tags = {
+            "karpenter.sh/discovery" = local.karpenter.discovery_value
+          }
+        }
+      ]
+      tags = {
+        "karpenter.sh/discovery" = local.karpenter.discovery_value
+      }
+    }
+  })
+
+  depends_on = [
+    helm_release.karpenter
+  ]
+}
