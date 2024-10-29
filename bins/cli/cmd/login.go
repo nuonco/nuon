24f9f0dfb6d6a749f4b9062f8815d6a8adff0682
sam boyer <sam@nuon.co>
2024-10-29 12:48:47 -0400

    chore: Compute cli event name dynamically (#6832)
    Co-authored-by: nuonbot <bot@nuon.co>

--- bins/cli/cmd/login.go
+++ bins/cli/cmd/login.go
@@ -11,7 +11,7 @@ func (c *cli) loginCmd() *cobra.Command {
 		Use:               "login",
 		Short:             "Login to Nuon",
 		PersistentPreRunE: c.persistentPreRunE,
-		Run: c.wrapCmd(LoginEvent, func(cmd *cobra.Command, args []string) error {
+		Run: c.wrapCmd(func(cmd *cobra.Command, args []string) error {
 			svc := auth.New(c.apiClient)
 			return svc.Login(cmd.Context(), c.cfg)
 		}),
