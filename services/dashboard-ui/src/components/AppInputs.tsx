c230e025bc25479f63d10126b95b42417b35f305
Nat Hamilton <info@nathamilton.com>
2024-10-30 12:50:15 -0400

    feat(ui): input expand and other updates (#6842)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    Co-authored-by: nuonbot <bot@nuon.co>

--- services/dashboard-ui/src/components/AppInputs.tsx
+++ services/dashboard-ui/src/components/AppInputs.tsx
@@ -1,4 +1,5 @@
 import React, { type FC } from 'react'
+import { Expand } from '@/components/Expand'
 import { Text } from '@/components/Typography'
 import type { TAppInputConfig } from '@/types'
 
@@ -17,44 +18,54 @@ export const AppInputConfig: FC<IAppInputConfig> = ({ inputConfig }) => {
         <Text className="!font-medium">Sensitive</Text>
       </div>
       {inputConfig.input_groups.map((group, i) => (
-        <div className="divide-y" key={`${group.id}-${i}`}>
-          <div className="px-3 py-2 bg-cool-grey-50 dark:bg-dark-grey-200 text-cool-grey-600 dark:text-cool-grey-500 text-base border-t grid grid-cols-8 gap-4 items-start">
-            <Text className="col-span-2 !font-medium">
-              {group.display_name}
-            </Text>
-            <Text className="col-span-2 text-sm">{group.description}</Text>
-          </div>
+        <Expand
+          className="w-full"
+          key={`${group.id}-${i}`}
+          hasHeadingStyle
+          heading={
+            <div className="px-3 py-2 text-base grid grid-cols-8 gap-4 items-start">
+              <Text className="col-span-2 !font-medium">
+                {group.display_name}
+              </Text>
+              <Text className="col-span-2 text-sm">{group.description}</Text>
+            </div>
+          }
+          id={`${group.id}-${i}`}
+          isOpen
+          expandContent={
+            <div className="divide-y">
+              {group.app_inputs.map((input, ii) => (
+                <div
+                  key={`${input.id}-${ii}`}
+                  className="grid grid-cols-8 gap-4 px-3 py-4 items-start"
+                >
+                  <div className="col-span-2 gap-2 flex flex-col items-start justify-start">
+                    <Text className="!font-medium">{input.display_name}</Text>{' '}
+                    <Text className="font-mono text-cool-grey-600 dark:text-cool-grey-500 text-sm break-all !inline truncate max-w-[150px]">
+                      {input.name}
+                    </Text>
+                  </div>
 
-          <div className="divide-y">
-            {group.app_inputs.map((input, ii) => (
-              <div
-                key={`${input.id}-${ii}`}
-                className="grid grid-cols-8 gap-4 px-3 py-4 items-start"
-              >
-                <div className="col-span-2 gap-2 flex flex-col items-start justify-start">
-                  <Text className="!font-medium">{input.display_name}</Text>{' '}
-                  <Text className="font-mono text-cool-grey-600 dark:text-cool-grey-500 text-sm break-all !inline truncate max-w-[150px]">
-                    {input.name}
+                  <Text className="col-span-2 text-sm">
+                    {input.description}
+                  </Text>
+
+                  <Text className="col-span-2 text-sm">
+                    {input.default || 'No default'}
+                  </Text>
+
+                  <Text className="text-sm">
+                    {input.required ? 'True' : 'False'}
+                  </Text>
+
+                  <Text className="text-sm">
+                    {input.sensitive ? 'True' : 'False'}
                   </Text>
                 </div>
-
-                <Text className="col-span-2 text-sm">{input.description}</Text>
-
-                <Text className="col-span-2 text-sm">
-                  {input.default || 'No default'}
-                </Text>
-
-                <Text className="text-sm">
-                  {input.required ? 'True' : 'False'}
-                </Text>
-
-                <Text className="text-sm">
-                  {input.sensitive ? 'True' : 'False'}
-                </Text>
-              </div>
-            ))}
-          </div>
-        </div>
+              ))}
+            </div>
+          }
+        />
       ))}
     </div>
   ) : (
