63538df0958c0d68f64624fad239bde57d8aa2df
Nat Hamilton <info@nathamilton.com>
2024-10-31 08:54:04 -0400

    feat(ui): polling org status during provision (#6909)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

---
+++ services/dashboard-ui/src/components/OrgStatus.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import React, { type FC, useEffect, useState } from 'react'
+import { StatusBadge } from '@/components/Status'
+import type { TOrg } from '@/types'
+import { SHORT_POLL_DURATION } from '@/utils'
+
+export interface IOrgStatus {
+  descriptionAlignment?: 'center' | 'left' | 'right'
+  descriptionPosition?: 'bottom' | 'top'
+  initOrg: TOrg
+  shouldPoll?: boolean
+}
+
+export const OrgStatus: FC<IOrgStatus> = ({
+  initOrg,
+  shouldPoll = false,
+  ...props
+}) => {
+  const [org, updateOrg] = useState<TOrg>(initOrg)
+
+  useEffect(() => {
+    const fetchOrg = () => {
+      fetch(`/api/${org.id}`)
+        .then((res) =>
+          res.json().then((o) => {
+            updateOrg(o)
+          })
+        )
+        .catch(console.error)
+    }
+    if (shouldPoll) {
+      const pollOrg = setInterval(fetchOrg, SHORT_POLL_DURATION)
+
+      if (
+        org?.status === 'active' ||
+        org?.status === 'error' ||
+        org?.status === 'failed' ||
+        org?.status === 'noop'
+      ) {
+        clearInterval(pollOrg)
+      }
+
+      return () => clearInterval(pollOrg)
+    }
+  }, [org, shouldPoll])
+
+  return (
+    <StatusBadge
+      description={org?.status_description}
+      status={org?.status}
+      {...props}
+      isWithoutBorder
+    />
+  )
+}
