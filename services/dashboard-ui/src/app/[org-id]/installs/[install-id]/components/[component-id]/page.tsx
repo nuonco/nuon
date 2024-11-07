9109c662c59926405de37d6f8d2d2ad4bfe55a85
Nat Hamilton <info@nathamilton.com>
2024-11-07 11:13:18 -0500

    fix(ui): correct auth redirect (#7048)
    * refactor(ui): set origin url in middleware headers
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * refactor(ui): set origin url as auth returnTo
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * refactor(ui): removed unneeded route auth returnTo
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/app/[org-id]/installs/[install-id]/components/[component-id]/page.tsx
+++ services/dashboard-ui/src/app/[org-id]/installs/[install-id]/components/[component-id]/page.tsx
@@ -15,84 +15,83 @@ import {
 } from '@/lib'
 import type { TInstallComponent } from '@/types'
 
-export default withPageAuthRequired(
-  async function InstallComponent({ params }) {
-    const installComponentId = params?.['component-id'] as string
-    const installId = params?.['install-id'] as string
-    const orgId = params?.['org-id'] as string
+export default withPageAuthRequired(async function InstallComponent({
+  params,
+}) {
+  const installComponentId = params?.['component-id'] as string
+  const installId = params?.['install-id'] as string
+  const orgId = params?.['org-id'] as string
 
-    const [install, installComponent, org] = await Promise.all([
-      getInstall({ installId, orgId }),
-      getInstallComponent({
-        installComponentId,
-        installId,
-        orgId,
-      }),
-      getOrg({ orgId }),
-    ])
+  const [install, installComponent, org] = await Promise.all([
+    getInstall({ installId, orgId }),
+    getInstallComponent({
+      installComponentId,
+      installId,
+      orgId,
+    }),
+    getOrg({ orgId }),
+  ])
 
-    const [component, componentConfig] = await Promise.all([
-      getComponent({ componentId: installComponent.component_id, orgId }),
-      getComponentConfig({
-        componentId: installComponent?.component_id,
-        orgId,
-      }),
-    ])
+  const [component, componentConfig] = await Promise.all([
+    getComponent({ componentId: installComponent.component_id, orgId }),
+    getComponentConfig({
+      componentId: installComponent?.component_id,
+      orgId,
+    }),
+  ])
 
-    return (
-      <DashboardContent
-        breadcrumb={[
-          { href: `/${org.id}/apps`, text: org.name },
-          { href: `/${org.id}/installs`, text: 'Installs' },
-          {
-            href: `/${org.id}/installs/${install.id}/components`,
-            text: install.name,
-          },
-          {
-            href: `/${org.id}/installs/${install.id}/components/${installComponent.id}`,
-            text: component.name,
-          },
-        ]}
-        heading={component.name}
-        headingUnderline={installComponent.id}
-      >
-        <div className="flex flex-col lg:flex-row flex-auto">
-          <div className="divide-y flex-auto  flex flex-col overlfow-auto">
-            {component.dependencies && (
-              <section className="flex flex-col gap-6 px-6 py-8">
-                <Heading>Dependencies</Heading>
-
-                <DependentComponents
-                  dependentIds={component.dependencies}
-                  installComponents={
-                    install?.install_components as Array<TInstallComponent>
-                  }
-                  installId={installId}
-                  orgId={orgId}
-                />
-              </section>
-            )}
-
+  return (
+    <DashboardContent
+      breadcrumb={[
+        { href: `/${org.id}/apps`, text: org.name },
+        { href: `/${org.id}/installs`, text: 'Installs' },
+        {
+          href: `/${org.id}/installs/${install.id}/components`,
+          text: install.name,
+        },
+        {
+          href: `/${org.id}/installs/${install.id}/components/${installComponent.id}`,
+          text: component.name,
+        },
+      ]}
+      heading={component.name}
+      headingUnderline={installComponent.id}
+    >
+      <div className="flex flex-col lg:flex-row flex-auto">
+        <div className="divide-y flex-auto  flex flex-col overlfow-auto">
+          {component.dependencies && (
             <section className="flex flex-col gap-6 px-6 py-8">
-              <Heading>Component config</Heading>
+              <Heading>Dependencies</Heading>
 
-              <ComponentConfiguration config={componentConfig} />
+              <DependentComponents
+                dependentIds={component.dependencies}
+                installComponents={
+                  install?.install_components as Array<TInstallComponent>
+                }
+                installId={installId}
+                orgId={orgId}
+              />
             </section>
-          </div>
-          <section className="flex flex-col gap-4 px-6 py-8 border-l overflow-auto lg:min-w-[450px] lg:max-w-[450px]">
-            <Heading>Deploy history</Heading>
-            <InstallComponentDeploys
-              component={component}
-              initDeploys={installComponent.install_deploys}
-              installId={installId}
-              installComponentId={installComponent.id}
-              orgId={orgId}
-              shouldPoll
-            />
+          )}
+
+          <section className="flex flex-col gap-6 px-6 py-8">
+            <Heading>Component config</Heading>
+
+            <ComponentConfiguration config={componentConfig} />
           </section>
         </div>
-      </DashboardContent>
-    )
-  },
-  { returnTo: '/' }
-)
+        <section className="flex flex-col gap-4 px-6 py-8 border-l overflow-auto lg:min-w-[450px] lg:max-w-[450px]">
+          <Heading>Deploy history</Heading>
+          <InstallComponentDeploys
+            component={component}
+            initDeploys={installComponent.install_deploys}
+            installId={installId}
+            installComponentId={installComponent.id}
+            orgId={orgId}
+            shouldPoll
+          />
+        </section>
+      </div>
+    </DashboardContent>
+  )
+})
