2b9bb391f1eb34b31bdb28e27e0367260b950f8c
Rob <alphavibe@gmail.com>
2024-11-18 20:37:26 -0800

    feat: api endpoint restart runners (#7153)
    feat: restart runners

---
+++ services/ctl-api/docs/admin/descriptions/restart_runners.md
@@ -0,0 +1,1 @@
+Restart all org and install runners where org type is default.
---
+++ services/ctl-api/internal/app/runners/service/admin_restart_runners.go
@@ -0,0 +1,90 @@
+package service
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app/runners/signals"
+)
+
+type AdminRestartRunnersRequest struct {
+}
+
+type AdminRestartRunnersResponse struct {
+	OrgID    string `json:"org_id"`
+	RunnerID string `json:"runner_id"`
+}
+
+// @ID AdminRestartRunners
+// @Summary	Restarts all non sandbox org and install runners
+// @Description.markdown restart_runners.md
+// @Param		req				body	AdminRestartRunnersRequest	true "Input"
+// @Tags runners/admin
+// @Security AdminEmail
+// @Accept			json
+// @Produce		json
+// @Success		200	{array} AdminRestartRunnersResponse
+// @Router		/v1/runners/restart [POST]
+func (s *service) AdminRestartRunners(ctx *gin.Context) {
+	var req AdminRestartRunnersRequest
+	if err := ctx.BindJSON(&req); err != nil {
+		ctx.Error(fmt.Errorf("unable to parse request: %w", err))
+		return
+	}
+
+	updatesResponse, err := s.bulkRestartRunners(ctx)
+	if err != nil {
+		ctx.Error(fmt.Errorf("unable to restart runners: %w", err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, updatesResponse)
+}
+
+func (s *service) bulkRestartRunners(ctx context.Context) ([]AdminRestartRunnersResponse, error) {
+	updatesResponse := []AdminRestartRunnersResponse{}
+	batchSize := 50
+	var runners []app.Runner
+	offset := 0
+
+	for {
+		result := s.db.
+			Joins("JOIN orgs ON runners.org_id = orgs.id AND orgs.org_type = ?", app.OrgTypeDefault).
+			Offset(offset).
+			Limit(batchSize).
+			Find(&runners).
+			Order("created_at ASC")
+
+		if result.Error != nil {
+			return nil, fmt.Errorf("unable to fetch runners: %w", result.Error)
+		}
+
+		if len(runners) == 0 {
+			break
+		}
+
+		for _, runner := range runners {
+			updatesResponse = append(updatesResponse, AdminRestartRunnersResponse{
+				OrgID:    runner.OrgID,
+				RunnerID: runner.ID,
+			})
+		}
+
+		offset += batchSize
+	}
+
+	runnerVisited := make(map[string]bool)
+	for _, response := range updatesResponse {
+		if _, ok := runnerVisited[response.RunnerID]; !ok {
+			s.evClient.Send(ctx, response.RunnerID, &signals.Signal{
+				Type: signals.OperationRestart,
+			})
+			runnerVisited[response.RunnerID] = true
+		}
+	}
+
+	return updatesResponse, nil
+}
