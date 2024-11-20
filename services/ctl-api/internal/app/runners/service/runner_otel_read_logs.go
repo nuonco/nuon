82b3be15681fb570ec5896dc76992b42d3cec3b2
Dat Boi Diego <fidiego@users.noreply.github.com>
2024-11-20 16:52:32 -0600

    feat: ctl-api: revert change to otel log record page size (#7193)

--- services/ctl-api/internal/app/runners/service/runner_otel_read_logs.go
+++ services/ctl-api/internal/app/runners/service/runner_otel_read_logs.go
@@ -13,7 +13,7 @@ import (
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
 )
 
-const PageSize int = 2500
+const PageSize int = 10000
 
 type Page struct {
 	Data  []app.OtelLogRecord `json:"data"`
