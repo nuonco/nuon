872d33159c5316faba6882345b41f66c8994ff82
Jon Morehouse <MorehouseJ09@gmail.com>
2024-11-20 07:48:26 -0800

    feat: improve sandbox job logging (#7183)
    * feat: improve sandbox job logging
    
    * fix: output to correct logger

--- bins/runner/internal/pkg/jobloop/job_step_validate.go
+++ bins/runner/internal/pkg/jobloop/job_step_validate.go
@@ -5,14 +5,13 @@ import (
 	"fmt"
 
 	"github.com/nuonco/nuon-runner-go/models"
-	"go.uber.org/zap"
 
 	"github.com/powertoolsdev/mono/bins/runner/internal/jobs"
 )
 
 func (j *jobLoop) executeValidateJobStep(ctx context.Context, handler jobs.JobHandler, job *models.AppRunnerJob, jobExecution *models.AppRunnerJobExecution) error {
 	if j.settings.SandboxMode {
-		j.l.Info("sandbox mode enabled, skipping", zap.String("step", "validate"))
+		j.execSandboxStep(ctx)
 		return nil
 	}
 
