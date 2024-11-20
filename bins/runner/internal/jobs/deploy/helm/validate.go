d72ef4c47eaa37d24f7e33c4eb5dd746e74feaa4
Dat Boi Diego <fidiego@users.noreply.github.com>
2024-11-19 18:17:13 -0600

    feat: runner: helm plan updates and region in oci and helm archive store (#7173)
    * fix: runner: pkg/oci: introduce a BasePath
    
    * feat: runner: helm: use BasePath
    
    * fix: runner: tf workspace - use BasePath
    
    * fix: runner: helm - update waypoint config
    
    * fix: runner/terraform: update waypoint config
    
    * chore: runner/helm: add path to log attrs
    
    * fix: workers-executors: add region to ecr auth
    
    * fix: api seed configs: fix input
    
    ---------
    
    Co-authored-by: nuonbot <bot@nuon.co>

--- bins/runner/internal/jobs/deploy/helm/validate.go
+++ bins/runner/internal/jobs/deploy/helm/validate.go
@@ -8,6 +8,7 @@ import (
 	"go.uber.org/zap"
 
 	pkgctx "github.com/powertoolsdev/mono/bins/runner/internal/pkg/ctx"
+	ociarchive "github.com/powertoolsdev/mono/bins/runner/internal/pkg/oci/archive"
 	"github.com/powertoolsdev/mono/bins/runner/internal/pkg/plan"
 )
 
@@ -16,19 +17,24 @@ const (
 )
 
 func (h *handler) Validate(ctx context.Context, job *models.AppRunnerJob, jobExecution *models.AppRunnerJobExecution) error {
-        l, err := pkgctx.Logger(ctx)
-        if err != nil {
-                return err
-        }
+	l, err := pkgctx.Logger(ctx)
+	if err != nil {
+		return err
+	}
 
 	cfg, err := plan.ParseConfig[WaypointConfig](h.state.plan)
 	if err != nil {
 		return fmt.Errorf("unable to parse plan: %w", err)
 	}
 
-	h.state.cfg = &cfg.Deploy
-	h.state.srcCfg = cfg.Deploy.ArtifactRepo
-	h.state.srcTag = cfg.Deploy.ArtifactTag
+	h.state.cfg = &cfg.App.Deploy.Use
+	h.state.srcCfg = cfg.App.Deploy.Use.ArtifactRepo
+	h.state.srcTag = cfg.App.Deploy.Use.ArtifactTag
+	arch := ociarchive.New()
+	if err := arch.Initialize(ctx); err != nil {
+		return fmt.Errorf("unable to initialize archive: %w", err)
+	}
+	h.state.arch = arch
 
 	if h.state.cfg.Namespace == "" {
 		l.Info("no namespace set, using default", zap.String("namespace", defaultNamespace))
