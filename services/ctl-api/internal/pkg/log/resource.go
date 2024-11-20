943313f15a3c9990aa11940bb2f82ff3d838ae32
Jon Morehouse <MorehouseJ09@gmail.com>
2024-11-20 12:25:04 -0800

    feat: add api tag to loggers (#7187)
    feat: add `service.name` tag to executor/api loggers
    
    Co-authored-by: nuonbot <bot@nuon.co>

--- services/ctl-api/internal/pkg/log/resource.go
+++ services/ctl-api/internal/pkg/log/resource.go
@@ -13,10 +13,16 @@ func getResource(logStreamID string, kvs map[string]string) *resource.Resource {
 			Value: attribute.StringValue(v),
 		})
 	}
-	attrs = append(attrs, attribute.KeyValue{
-		Key:   "log_stream.id",
-		Value: attribute.StringValue(logStreamID),
-	})
+	attrs = append(attrs,
+		attribute.KeyValue{
+			Key:   "log_stream.id",
+			Value: attribute.StringValue(logStreamID),
+		},
+		attribute.KeyValue{
+			Key:   "service.name",
+			Value: attribute.StringValue("api"),
+		},
+	)
 
 	return resource.NewSchemaless(attrs...)
 }
