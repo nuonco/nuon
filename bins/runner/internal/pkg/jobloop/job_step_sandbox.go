6dd006c3b3ad4838c3aa5bd71be0446f51806765
Jon Morehouse <MorehouseJ09@gmail.com>
2024-11-20 07:48:26 -0800

    feat: improve sandbox job logging (#7183)
    * feat: improve sandbox job logging
    
    * fix: output to correct logger

---
+++ bins/runner/internal/pkg/jobloop/job_step_sandbox.go
@@ -0,0 +1,48 @@
+package jobloop
+
+import (
+	"context"
+	"time"
+
+	pkgctx "github.com/powertoolsdev/mono/bins/runner/internal/pkg/ctx"
+	"go.uber.org/zap"
+)
+
+const (
+	logPeriod  time.Duration = time.Second / 4
+	totalSteps               = 6
+)
+
+func (j *jobLoop) execSandboxStep(ctx context.Context) error {
+	l, err := pkgctx.Logger(ctx)
+	if err != nil {
+		return err
+	}
+
+	duration := j.cfg.SandboxJobDuration / totalSteps
+	l.Info("sandbox mode enabled, faking job output", zap.String("step", "initialize"))
+
+	timeout := time.NewTimer(duration)
+	ticker := time.NewTicker(logPeriod)
+	defer ticker.Stop()
+	defer timeout.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			l.Info("sandbox job log",
+				zap.String("key", "value"),
+				zap.Any("obj", map[string]interface{}{}),
+			)
+		case <-timeout.C:
+			goto BREAK
+		}
+	}
+BREAK:
+	l.Info("sandbox job log ending",
+		zap.String("key", "value"),
+		zap.Any("obj", map[string]interface{}{}),
+	)
+
+	return nil
+}
