8e179f0ed79710ea8633479578876d7172561037
Nat Hamilton <info@nathamilton.com>
2024-11-21 11:26:04 -0500

    feat(ui): display  api & ui version (#7196)
    * feat(ui): add getAPIVersion func and version config val
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat(ui): add NuonVersion react component
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat(ui): add NuonVersion to dashboard layout
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat(ui): NuonVersion bigger text
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

---
+++ services/dashboard-ui/src/lib/get-api-version.ts
@@ -0,0 +1,14 @@
+import { API_URL, getFetchOpts } from '@/utils'
+
+export async function getAPIVersion(): Promise<{
+  git_ref: string
+  version: string
+}> {
+  const data = await fetch(`${API_URL}/version`, await getFetchOpts())
+
+  if (!data.ok) {
+    throw new Error('Failed to fetch api version')
+  }
+
+  return data.json()
+}
