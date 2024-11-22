775336a0a9a2c5d986e686e7e8751d5eaf5aff20
Rob <alphavibe@gmail.com>
2024-11-22 14:37:51 -0800

    feat: action endpoints cleanup (#7219)

--- services/ctl-api/internal/app/action_workflow_step_config.go
+++ services/ctl-api/internal/app/action_workflow_step_config.go
@@ -22,7 +22,7 @@ type ActionWorkflowStepConfig struct {
 	OrgID string `json:"org_id" gorm:"notnull" swaggerignore:"true"`
 	Org   Org    `json:"-" faker:"-"`
 
-	App   App    `swaggerignore:"true" json:"app"`
+	App   App    `json:"-" swaggerignore:"true"`
 	AppID string `json:"app_id" gorm:"notnull;index:idx_app_install_name,unique"`
 
 	// this belongs to an app config id
