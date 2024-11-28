b1e3651afa849222a4a27a8d45b75127342ad138
Rob <alphavibe@gmail.com>
2024-11-27 19:50:22 -0800

    feat: action workflow cli sync (#7245)

---
+++ pkg/config/action_config.go
@@ -0,0 +1,46 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/mitchellh/mapstructure"
+	"github.com/powertoolsdev/mono/pkg/config/source"
+)
+
+type ActionConfig struct {
+	Source   string                 `mapstructure:"source,omitempty"`
+	Name     string                 `mapstructure:"name" jsonschema:"required"`
+	Triggers []*ActionTriggerConfig `mapstructure:"triggers" jsonschema:"required"`
+	Steps    []*ActionStepConfig    `mapstructure:"steps" jsonschema:"required"`
+}
+
+type ActionTriggerConfig struct {
+	Type         string `mapstructure:"type" jsonschema:"required"`
+	CronSchedule string `mapstructure:"cron_schedule,omitempty"`
+}
+
+type ActionStepConfig struct {
+	Name          string               `mapstructure:"name" jsonschema:"required"`
+	EnvVarMap     map[string]string    `mapstructure:"env_vars,omitempty"`
+	PublicRepo    *PublicRepoConfig    `mapstructure:"public_repo,omitempty" jsonschema:"oneof_required=public_repo"`
+	ConnectedRepo *ConnectedRepoConfig `mapstructure:"connected_repo,omitempty"  jsonschema:"oneof_required=connected_repo"`
+}
+
+func (a *ActionConfig) parse() error {
+	if a.Source == "" {
+		return nil
+	}
+
+	obj, err := source.LoadSource(a.Source)
+	if err != nil {
+		return ErrConfig{
+			Description: fmt.Sprintf("unable to load source %s", a.Source),
+			Err:         err,
+		}
+	}
+
+	if err := mapstructure.Decode(obj, &a); err != nil {
+		return err
+	}
+	return nil
+}
