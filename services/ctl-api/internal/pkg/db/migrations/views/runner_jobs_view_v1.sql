e0837e93a1818e417d7d5bf54b14631772bd7883
Jon Morehouse <MorehouseJ09@gmail.com>
2024-12-03 20:07:04 -0600

    chore: prevent runaway runner jobs (#7318)
    * feat: add runner job view
    
    Add a view that has the runner job execution count included.
    
    * feat: prevent runaway jobs
    
    * update

---
+++ services/ctl-api/internal/pkg/db/migrations/views/runner_jobs_view_v1.sql
@@ -0,0 +1,20 @@
+CREATE OR REPLACE VIEW runner_jobs_view_v1 AS
+  /* Runner job execution counts */
+  WITH runner_job_execution_counts AS (
+    SELECT
+        rje.runner_job_id,
+        count(*) as execution_count
+    FROM 
+        runner_job_executions rje
+    GROUP BY runner_job_id  
+  )
+
+  SELECT 
+    rj.*,
+    rjec.execution_count
+  FROM    
+    runner_jobs AS rj
+  FULL OUTER JOIN
+    runner_job_execution_counts AS rjec
+  ON 
+    rjec.runner_job_id = rj.id
