3222e7b7b24177ea549049ddbd717e30d23d6198
Jon Morehouse <MorehouseJ09@gmail.com>
2024-12-04 15:40:15 -0600

    fix: remove some uses of the runner ctx helper (#7329)
    * wip changes
    
    * fix nctl api seed
    
    ---------
    
    Co-authored-by: nuonbot <bot@nuon.co>

--- services/ctl-api/internal/app/runners/worker/activities/save_runner_job_plan.go
+++ services/ctl-api/internal/app/runners/worker/activities/save_runner_job_plan.go
@@ -13,11 +13,6 @@ type SaveRunnerJobPlanRequest struct {
 // @temporal-gen activity
 // @schedule-to-close-timeout 5s
 func (a *Activities) SaveRunnerJobPlan(ctx context.Context, req *SaveRunnerJobPlanRequest) error {
-	ctx, err := a.helpers.ContextFromJob(ctx, req.JobID)
-	if err != nil {
-		return fmt.Errorf("unable to create context: %w", err)
-	}
-
 	if err := a.helpers.WriteJobPlan(ctx, req.JobID, []byte(req.PlanJSON)); err != nil {
 		return fmt.Errorf("unable to write job plan: %w", err)
 	}
