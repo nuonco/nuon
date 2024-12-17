acc39199d24ebea3f285b230eb3242b8611032c6
Nat Hamilton <info@nathamilton.com>
2024-12-17 15:12:45 -0500

    feat(ui): timeline component (#7530)
    * feat(ui): added new timeline component
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat(ui): updated to the timeline component
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/components/ComponentBuildHistory.tsx
+++ services/dashboard-ui/src/components/ComponentBuildHistory.tsx
@@ -1,11 +1,7 @@
 'use client'
 
-import classNames from 'classnames'
 import React, { type FC, useEffect, useState } from 'react'
-import { CaretRight } from '@phosphor-icons/react'
-import { Link } from '@/components/Link'
-import { StatusBadge } from '@/components/Status'
-import { Time } from '@/components/Time'
+import { Timeline } from '@/components/Timeline'
 import { ToolTip } from '@/components/ToolTip'
 import { Text, Truncate } from '@/components/Typography'
 import type { TBuild } from '@/types'
@@ -20,16 +16,17 @@ export interface IComponentBuildHistory {
 }
 
 export const ComponentBuildHistory: FC<IComponentBuildHistory> = ({
+  appId,
   componentId,
   initBuilds,
   shouldPoll = false,
-  ...props
+  orgId,
 }) => {
   const [builds, setComponentBuilds] = useState(initBuilds)
 
   useEffect(() => {
     const fetchComponentBuilds = () => {
-      fetch(`/api/${props.orgId}/components/${componentId}/builds`)
+      fetch(`/api/${orgId}/components/${componentId}/builds`)
         .then((res) => res.json().then((b) => setComponentBuilds(b)))
         .catch(console.error)
     }
@@ -38,79 +35,37 @@ export const ComponentBuildHistory: FC<IComponentBuildHistory> = ({
       const pollBuilds = setInterval(fetchComponentBuilds, SHORT_POLL_DURATION)
       return () => clearInterval(pollBuilds)
     }
-  }, [builds, componentId, props.orgId, shouldPoll])
+  }, [builds, componentId, orgId, shouldPoll])
 
   return (
-    <div className="flex flex-col gap-2">
-      {builds.map((build, i) => (
-        <ComponentBuildEvent
-          key={`${build.id}-${i}`}
-          build={build}
-          isMostRecent={i === 0}
-          {...props}
-        />
-      ))}
-    </div>
-  )
-}
-
-interface IComponentBuildEvent {
-  appId: string
-  build: TBuild
-  isMostRecent?: boolean
-  orgId: string
-}
-
-const ComponentBuildEvent: FC<IComponentBuildEvent> = ({
-  appId,
-  build,
-  isMostRecent = false,
-  orgId,
-}) => {
-  return (
-    <Link
-      className="!block w-full !p-0"
-      href={`/${orgId}/apps/${appId}/components/${build.component_id}/builds/${build.id}`}
-      variant="ghost"
-    >
-      <div
-        className={classNames('flex items-center justify-between p-4', {
-          'border rounded-md shadow-sm': isMostRecent,
-        })}
-      >
-        <div className="flex flex-col">
-          <span className="flex items-center gap-2">
-            <StatusBadge
-              status={build.status}
-              isStatusTextHidden
-              isWithoutBorder
-            />
-          </span>
-
-          <Text className="flex items-center gap-2 ml-4">
-            <ToolTip tipContent={build.id}>
+    <Timeline
+      emptyMessage="No component builds to show"
+      events={builds.map((b, i) => ({
+        id: b.id,
+        status: b.status,
+        underline: (
+          <>
+            <ToolTip tipContent={b.id}>
               <Text className="truncate text-ellipsis w-16" variant="mono-12">
-                {build.id}
+                {b.id}
               </Text>
             </ToolTip>
             <>
               /{' '}
-              {build.component_name.length >= 12 ? (
-                <ToolTip tipContent={build.component_name} alignment="right">
-                  <Truncate variant="small">{build.component_name}</Truncate>
+              {b.component_name.length >= 12 ? (
+                <ToolTip tipContent={b.component_name} alignment="right">
+                  <Truncate variant="small">{b.component_name}</Truncate>
                 </ToolTip>
               ) : (
-                build.component_name
+                b.component_name
               )}
             </>
-          </Text>
-        </div>
-
-        <div className="flex items-center gap-2">
-          <Time time={build.updated_at} format="relative" />
-          <CaretRight />
-        </div>
-      </div>
-    </Link>
+          </>
+        ),
+        time: b.updated_at,
+        href: `/${orgId}/apps/${appId}/components/${b.component_id}/builds/${b.id}`,
+        isMostRecent: i === 0,
+      }))}
+    />
   )
 }
