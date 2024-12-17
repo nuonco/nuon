9db64209a116dffff733c8e9516c8567d4c3e51c
Nat Hamilton <info@nathamilton.com>
2024-12-17 15:12:45 -0500

    feat(ui): timeline component (#7530)
    * feat(ui): added new timeline component
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat(ui): updated to the timeline component
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/components/InstallHistory.tsx
+++ services/dashboard-ui/src/components/InstallHistory.tsx
@@ -1,14 +1,10 @@
 'use client'
 
-import classNames from 'classnames'
 import React, { type FC, useEffect, useState } from 'react'
-import { CaretRight } from '@phosphor-icons/react'
-import { Link } from '@/components/Link'
-import { Time } from '@/components/Time'
+import { Timeline } from '@/components/Timeline'
 import { ToolTip } from '@/components/ToolTip'
-import { Text } from '@/components/Typography'
 import type { TInstallEvent } from '@/types'
-import { SHORT_POLL_DURATION, sentanceCase } from '@/utils'
+import { SHORT_POLL_DURATION } from '@/utils'
 
 function parseInstallEventPayload(
   event: TInstallEvent
@@ -105,94 +101,36 @@ export const InstallHistory: FC<IInstallHistory> = ({
   }, [events, installId, orgId, shouldPoll])
 
   return (
-    <div className="flex flex-col gap-2">
-      {events.length ? (
-        events.map((event, i) => (
-          <InstallEvent
-            key={`${event.payload_id}-${i}`}
-            event={event}
-            isMostRecent={i === 0}
-          />
-        ))
-      ) : (
-        <Text className="text-sm">No install events found</Text>
-      )}
-    </div>
-  )
-}
-
-interface IInstallEvent {
-  event: TInstallHistoryEvent
-  isMostRecent?: boolean
-}
-
-const InstallEvent: FC<IInstallEvent> = ({ event, isMostRecent = false }) => {
-  const href =
-    (event?.operation === 'deploy' &&
-      `/${event.org_id}/installs/${event.install_id}/components/${event?.component_id}/deploys/${event.payload_id}`) ||
-    ((event?.operation === 'provision' ||
-      event?.operation === 'reprovision' ||
-      event?.operation === 'deprovision') &&
-      `/${event.org_id}/installs/${event.install_id}/runs/${event.payload_id}`) ||
-    null
-
-  return (
-    <Link className="!block w-full !p-0" href={href} variant="ghost">
-      <div
-        className={classNames('flex items-center justify-between p-4', {
-          'border rounded-md shadow-sm': isMostRecent,
-        })}
-      >
-        <div className="flex flex-col">
-          <span className="flex items-center gap-2">
-            <InstallEventStatus status={event.operation_status} />
-            <Text variant="med-12">{sentanceCase(event.operation_status)}</Text>
-          </span>
-
-          <Text className="flex items-center gap-2 ml-3.5" variant="reg-12">
-            <span>{event.operation_name}</span>
-            {event.operation === 'deploy' && (
+    <Timeline
+      emptyMessage="No install events to show"
+      events={events.map((e) => ({
+        id: e.payload_id,
+        status: e.operation_status,
+        underline: (
+          <>
+            <span>{e.operation_name}</span>
+            {e.operation === 'deploy' && (
               <>
                 /{' '}
-                <ToolTip tipContent={event.component_name}>
+                <ToolTip tipContent={e.component_name}>
                   <span className="!inline truncate max-w-[100px]">
-                    {event.component_name}
+                    {e.component_name}
                   </span>
                 </ToolTip>
               </>
             )}
-          </Text>
-        </div>
-
-        <div className="flex items-center gap-2">
-          <Time
-            time={event.updated_at}
-            format="relative"
-            variant="reg-12"
-            className={classNames({
-              'text-black/60 dark:text-white/60': !Boolean(
-                event.operation_status === 'finished' ||
-                  event.operation_status === 'failed'
-              ),
-            })}
-          />
-          {href && <CaretRight />}
-        </div>
-      </div>
-    </Link>
+          </>
+        ),
+        time: e.updated_at,
+        href:
+          (e?.operation === 'deploy' &&
+            `/${e.org_id}/installs/${e.install_id}/components/${e?.component_id}/deploys/${e.payload_id}`) ||
+          ((e?.operation === 'provision' ||
+            e?.operation === 'reprovision' ||
+            e?.operation === 'deprovision') &&
+            `/${e.org_id}/installs/${e.install_id}/runs/${e.payload_id}`) ||
+          null,
+      }))}
+    />
   )
 }
-
-const InstallEventStatus: FC<{ status?: string }> = ({
-  status = 'waiting',
-}) => (
-  <span
-    className={classNames('w-1.5 h-1.5 rounded-full', {
-      'bg-green-800 dark:bg-green-500': status === 'finished',
-      'bg-red-800 dark:bg-red-500': status === 'failed',
-      'bg-cool-grey-600 dark:bg-cool-grey-500': status === 'noop',
-      'bg-orange-800 dark:bg-orange-500':
-        status === 'waiting' || status === 'started',
-    })}
-  />
-)
