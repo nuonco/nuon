91ba49ad8948086b01cb7d8970a26e95488208d9
Rob <alphavibe@gmail.com>
2024-12-20 09:44:14 -0800

    feat: app readme (#7551)
    * feat: app config readme
    
    * fd: update direct references to view
    
    * fd: move RenderString into pkg/render
    
    * fd: ctl-api: get install readme
    
    * chore: s/ReadMe/Readme/g
    
    * chore: seed-deafult: readme
    
    * chore: bump nuon-go version
    
    * fix: readme nuon-go generation
    
    ---------
    
    Co-authored-by: fidiego <sayheytofred@gmail.com>

---
+++ pkg/render/render.go
@@ -0,0 +1,42 @@
+package render
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"text/template"
+
+	"google.golang.org/protobuf/types/known/structpb"
+)
+
+const (
+	defaultPrefix string = "nuon"
+)
+
+func RenderString(ctx context.Context, inputVal string, intermediateData *structpb.Struct) (string, error) {
+	if inputVal == "" {
+		return "", nil
+	}
+
+	data := intermediateData.AsMap()
+
+	temp, err := template.New("input").Option("missingkey=zero").Parse(inputVal)
+	if err != nil {
+		return inputVal, nil
+	}
+
+	buf := new(bytes.Buffer)
+	if err := temp.Execute(buf, data); err != nil {
+		return inputVal, fmt.Errorf("unable to execute template: %w", err)
+	}
+
+	outputVal := buf.String()
+	if outputVal == "" {
+		return "", fmt.Errorf("rendered value was empty, this usually means a bad interpolation config: %s", inputVal)
+	}
+
+	if outputVal == "<no value>" {
+		return "", fmt.Errorf("rendered value was empty, which usually means a bad interpolation config: %s", inputVal)
+	}
+	return outputVal, nil
+}
