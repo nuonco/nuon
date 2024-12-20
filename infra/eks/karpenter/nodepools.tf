f7818dc0c3daef8cd8ab1b607ff39a6669dec516
Dat Boi Diego <fidiego@users.noreply.github.com>
2024-12-20 11:49:09 -0800

    feat: infra/eks: t3a.large public nodepool (#7558)

--- infra/eks/karpenter/nodepools.tf
+++ infra/eks/karpenter/nodepools.tf
@@ -71,3 +71,87 @@ resource "kubectl_manifest" "karpenter_nodepool_default" {
     helm_release.karpenter,
   ]
 }
+
+# https://karpenter.sh/v1.0/concepts/nodepools/
+# this nodepool is created on all clusters but is only required by prod and stage
+resource "kubectl_manifest" "karpenter_nodepool_public" {
+  yaml_body = yamlencode({
+    apiVersion = "karpenter.sh/v1" # we are on v1 now
+    kind       = "NodePool"
+    metadata = {
+      name = "public"
+    }
+    spec = {
+      limits = {
+        cpu    = 1000
+        memory = "1000Gi"
+      }
+      template = {
+        spec = {
+          # expires 7 days after creation
+          expireAfter = "168h"
+          nodeClassRef = {
+            group = "karpenter.k8s.aws"
+            kind  = "EC2NodeClass"
+            name  = "public"
+          }
+          requirements = [
+            {
+              key      = "karpenter.sh/capacity-type"
+              operator = "In"
+              values = [
+                "on-demand",
+              ]
+            },
+            {
+              "key"      = "node.kubernetes.io/instance-type"
+              "operator" = "In"
+              "values"   = "t3a.large" # notice: this is hardcoded for this nodepool
+            },
+            {
+              key      = "topology.kubernetes.io/zone"
+              operator = "In"
+              values = [
+                "us-west-2a",
+                "us-west-2b",
+                "us-west-2c",
+              ]
+            },
+          ]
+        }
+      }
+      # https://karpenter.sh/v1.0/concepts/disruption/
+      disruption = {
+        consolidationPolicy = "WhenEmptyOrUnderutilized"
+        consolidateAfter    = "120s"
+        budgets = [
+          {
+            # NOTE(fd): 1. do not rotate during the work week.
+            nodes    = "0",
+            schedule = "0 0 * * 1,2,3,4,5" # https://crontab.guru/#0_0_*_*_1,2,3,4,5
+            duration = "24h"
+            reasons = [
+              "Drifted",
+              "Underutilized"
+            ]
+          },
+          {
+            // NOTE(fd): 2. rotate during a four hour window during the weekend.
+            nodes    = "1",
+            schedule = "0 9 * * 6, 7" # https://crontab.guru/#0_9_*_*_6,7
+            duration = "20h"
+            reasons = [
+              "Empty",
+              "Drifted",
+              "Underutilized"
+            ]
+          },
+        ]
+      }
+    }
+  })
+  depends_on = [
+    kubectl_manifest.ec2nodeclass,
+    helm_release.karpenter,
+  ]
+}
