6c8f3de0b1d4ccb0f7fef548861f3718d58046b0
Dat Boi Diego <fidiego@users.noreply.github.com>
2025-01-08 09:48:41 -0600

    chore: infra/vantage: update vantage integration (#7601)

--- infra/vantage/cloudformation.tf
+++ infra/vantage/cloudformation.tf
@@ -64,3 +64,57 @@ resource "aws_cloudformation_stack_set_instance" "vantage-integrations-stack-set
     organizational_unit_ids = ["r-p4e3"]
   }
 }
+
+#
+# upgrade vantage: Wed Jan 8, 2025
+#
+# NOTE(fd): do we have to do anything to remove the old cloudformation stacks? i think we should delete them after this is done.
+# aws cloudformation create-stack --stack-name ConnectToVantage15519-1736350314 \
+#   --template-url https://vantage-public.s3.amazonaws.com/vantage-integration-update-latest.json \
+#   --parameters ParameterKey=VantageCrossAccountRole,ParameterValue='ConnectToVantage15519-1726781115-CrossAccountRole-13HVDF0Jjh9K' \
+#   --capabilities CAPABILITY_IAM \
+#   --region us-east-1
+resource "aws_cloudformation_stack" "update-vantage" {
+  name         = "ConnectToVantage15519-1736347644"
+  template_url = "https://vantage-public.s3.amazonaws.com/vantage-integration-update-latest.json"
+
+  parameters = {
+    VantageCrossAccountRole = "ConnectToVantage15519-1726781115-CrossAccountRole-13HVDF0Jjh9K"
+  }
+
+  capabilities = ["CAPABILITY_IAM"]
+}
+
+# per resource costs
+# aws cloudformation create-stack --stack-name VantageSetupCUR30834-1736350314 \
+#   --template-url https://vantage-public.s3.amazonaws.com/cur-setup-latest.json \
+#   --parameters ParameterKey=VantageID,ParameterValue='O1XMAv_xX3ZE_wvwyHtrsg' \
+#     ParameterKey=VantageDomain,ParameterValue='https://console.vantage.sh' \
+#     ParameterKey=VantagePingbackArn,ParameterValue='arn:aws:sns:us-east-1:630399649041:cost-and-usage-report-connector' \
+#     ParameterKey=VantageIamRole,ParameterValue='ConnectToVantage15519-1726781115-CrossAccountRole-13HVDF0Jjh9K' \
+#     ParameterKey=VantageIamRoleArn,ParameterValue='arn:aws:iam::491187160246:role/ConnectToVantage15519-1726781115-CrossAccountRole-13HVDF0Jjh9K' \
+#     ParameterKey=VantageNotificationTopicArn,ParameterValue='arn:aws:sns:us-east-1:630399649041:cost-and-usage-report-uploaded' \
+#     ParameterKey=ReportName,ParameterValue='VantageReport-2549feb6a9' \
+#     ParameterKey=BucketName,ParameterValue='vantage-cur-o1xmavxx3zewvwyhtrsg-2549feb6a9' \
+#   --capabilities CAPABILITY_IAM \
+#   --region us-east-1
+resource "aws_cloudformation_stack" "per-resource-costs" {
+  name         = "VantageSetupCUR30834-1736347644"
+  template_url = "https://vantage-public.s3.amazonaws.com/cur-setup-latest.json"
+
+  parameters = {
+    VantageCrossAccountRole     = "ConnectToVantage15519-1726781115-CrossAccountRole-13HVDF0Jjh9K"
+    VantageID                   = "O1XMAv_xX3ZE_wvwyHtrsg"
+    VantageDomain               = "https://console.vantage.sh"
+    VantagePingbackArn          = "arn:aws:sns:us-east-1:630399649041:cost-and-usage-report-connector"
+    VantageIamRole              = "ConnectToVantage15519-1726781115-CrossAccountRole-13HVDF0Jjh9K"
+    VantageIamRoleArn           = "arn:aws:iam::491187160246:role/ConnectToVantage15519-1726781115-CrossAccountRole-13HVDF0Jjh9K"
+    VantageNotificationTopicArn = "arn:aws:sns:us-east-1:630399649041:cost-and-usage-report-uploaded"
+    ReportName                  = "VantageReport-88e7dfd029"
+    BucketName                  = "vantage-cur-o1xmavxx3zewvwyhtrsg-88e7dfd029"
+  }
+
+  capabilities = ["CAPABILITY_IAM"]
+
+  depends_on = [aws_cloudformation_stack.update-vantage]
+}
