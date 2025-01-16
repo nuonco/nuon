7a651943050a8803d0ef1daf6859e25f10da11f7
Jon Morehouse <MorehouseJ09@gmail.com>
2025-01-16 02:03:49 -0600

    fix: admin get job supports finding jobs by owner (#7672)
    Co-authored-by: nuonbot <bot@nuon.co>

--- services/ctl-api/internal/app/runners/service/admin_get_job.go
+++ services/ctl-api/internal/app/runners/service/admin_get_job.go
@@ -1,10 +1,14 @@
 package service
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
 )
 
 // @ID AdminGetRunnerJob
@@ -20,11 +24,27 @@ import (
 func (s *service) AdminGetRunnerJob(ctx *gin.Context) {
 	runnerJobID := ctx.Param("runner_job_id")
 
-	runnerJob, err := s.getRunnerJob(ctx, runnerJobID)
+	runnerJob, err := s.adminGetRunnerJob(ctx, runnerJobID)
 	if err != nil {
 		ctx.Error(fmt.Errorf("unable to get runner job: %w", err))
 		return
 	}
 
 	ctx.JSON(http.StatusOK, runnerJob)
+}
+
+func (s *service) adminGetRunnerJob(ctx context.Context, runnerJobID string) (*app.RunnerJob, error) {
+	runnerJob := app.RunnerJob{}
+	res := s.db.WithContext(ctx).
+		Preload("Executions", func(db *gorm.DB) *gorm.DB {
+			return db.Order("runner_job_executions.created_at DESC").Limit(1)
+		}).
+		Where("owner_id = ?", runnerJobID).
+		Or("id = ?", runnerJobID).
+		First(&runnerJob)
+	if res.Error != nil {
+		return nil, fmt.Errorf("unable to get runner job: %w", res.Error)
+	}
+
+	return &runnerJob, nil
 }
