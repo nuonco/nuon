f3aaf70966cf5f93cf3d119b4298147850780975
Jon Morehouse <MorehouseJ09@gmail.com>
2025-01-21 10:57:17 -0800

    fix: different preloads in installs (#7716)
    * fix: properly preload
    
    * update

--- services/ctl-api/internal/app/installs/service/get_install_deploy.go
+++ services/ctl-api/internal/app/installs/service/get_install_deploy.go
@@ -45,7 +45,7 @@ func (s *service) getInstallDeploy(ctx context.Context, installID, deployID stri
 	res := s.db.WithContext(ctx).
 		Joins("JOIN install_components ON install_components.id=install_deploys.install_component_id").
 		Preload("InstallComponent").
-		Preload("RunnerJob").
+		Preload("RunnerJobs").
 		Preload("LogStream").
 		Preload("ComponentBuild").
 		Preload("ComponentBuild.ComponentConfigConnection").
