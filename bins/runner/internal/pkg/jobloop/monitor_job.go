4ecdc63388d235848d476018c50a74241019ee9b
Jon Morehouse <MorehouseJ09@gmail.com>
2025-01-21 21:54:46 -0800

    chore: cancel jobs in runner when marked failed in API (#7733)
    * chore: cancel jobs in runner when marked failed in API
    
    * update mod tidy
    
    ---------
    
    Co-authored-by: nuonbot <bot@nuon.co>

---
+++ bins/runner/internal/pkg/jobloop/monitor_job.go
@@ -0,0 +1,42 @@
+package jobloop
+
+import (
+	"context"
+	"time"
+
+	"github.com/nuonco/nuon-runner-go/models"
+	"go.uber.org/zap"
+)
+
+func (j *jobLoop) monitorJob(ctx context.Context, cancel func(), doneCh chan struct{}, jobID string, l *zap.Logger) {
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+	for {
+		select {
+		case <-ticker.C:
+		case <-doneCh:
+			return
+		}
+
+		job, err := j.apiClient.GetJob(ctx, jobID)
+		if err != nil {
+			l.Warn("unable to fetch job cancellation status", zap.Error(err))
+			continue
+		}
+
+		if job.Status == models.AppRunnerJobStatusCancelled {
+			l.Error("job was cancelled via API, attempting to cancel execution")
+			cancel()
+		}
+
+		if job.Status == models.AppRunnerJobStatusTimedDashOut {
+			l.Error("job was timed out via API, attempting to cancel execution")
+			cancel()
+		}
+
+		if job.Status == models.AppRunnerJobStatusFailed {
+			l.Error("job was failed via API, attempting to cancel execution")
+			cancel()
+		}
+	}
+}
