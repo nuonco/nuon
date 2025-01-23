c98c1fadb4cbcd8fe1e11c9550f2a71bf50d07ad
Rob <alphavibe@gmail.com>
2025-01-23 14:31:33 -0800

    fix: update-input accepts list of kv inputs (#7754)

--- bins/cli/internal/installs/update_input.go
+++ bins/cli/internal/installs/update_input.go
@@ -2,16 +2,21 @@ package installs
 
 import (
 	"context"
+	"strings"
 
 	"github.com/nuonco/nuon-go/models"
 
 	"github.com/powertoolsdev/mono/bins/cli/internal/ui"
 )
 
-func (s *Service) UpdateInput(ctx context.Context, installID, name, value string) error {
+func (s *Service) UpdateInput(ctx context.Context, installID string, inputs []string, printJSON bool) error {
+	inputsMap := make(map[string]string)
+	for _, kv := range inputs {
+		kvT := strings.Split(kv, "=")
+		inputsMap[kvT[0]] = kvT[1]
+	}
 	installInput, err := s.api.UpdateInstallInput(ctx, installID, &models.ServiceUpdateInstallInputRequest{
-		Name:  &name,
-		Value: &value,
+		Inputs: inputsMap,
 	})
 	if err != nil {
 		return ui.PrintJSONError(err)
