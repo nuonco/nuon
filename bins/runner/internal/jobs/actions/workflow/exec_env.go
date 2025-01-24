c8d3ab991a79f66c7afe5f52534cfc33f4c66ebf
Jon Morehouse <MorehouseJ09@gmail.com>
2025-01-24 09:42:40 -0800

    feat: support outputs (#7759)
    Co-authored-by: nuonbot <bot@nuon.co>

--- bins/runner/internal/jobs/actions/workflow/exec_env.go
+++ bins/runner/internal/jobs/actions/workflow/exec_env.go
@@ -2,16 +2,25 @@ package workflow
 
 import (
 	"context"
+	"os"
 
 	"github.com/pkg/errors"
-	"github.com/powertoolsdev/mono/bins/runner/internal/pkg/git"
 	"go.uber.org/zap"
+
+	"github.com/powertoolsdev/mono/bins/runner/internal/pkg/git"
 )
 
 func (h *handler) createExecEnv(ctx context.Context, l *zap.Logger, src *git.Source) error {
 	if err := git.Clone(ctx, h.state.workspace.Root(), src, l); err != nil {
 		return errors.Wrap(err, "unable to clone repository")
 	}
+
+	// create file for outputs
+	f, err := os.OpenFile(h.outputsFP(), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0o644)
+	if err != nil {
+		return errors.Wrap(err, "unable to open file")
+	}
+	f.Close()
 
 	return nil
 }
--- bins/runner/internal/jobs/actions/workflow/methods.go
+++ bins/runner/internal/jobs/actions/workflow/methods.go
@@ -1,10 +1,1 @@
 package workflow
-
-import (
-	"context"
-)
-
-func (h *handler) Outputs(ctx context.Context) (map[string]interface{}, error) {
-	// read outputs from the workspace file
-	return map[string]interface{}{}, nil
-}
