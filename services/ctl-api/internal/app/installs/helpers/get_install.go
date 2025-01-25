9109292bfa4d762abce0f9e89dba042b06f5ea99
Jordan Acosta <jordan@nuon.co>
2025-01-25 11:35:45 -0800

    feat(services/ctl-api): refactor getInstallState into a helper (#7778)
    Co-authored-by: nuonbot <bot@nuon.co>

--- services/ctl-api/internal/app/installs/helpers/get_install.go
+++ services/ctl-api/internal/app/installs/helpers/get_install.go
@@ -9,21 +9,34 @@ import (
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
 )
 
+// GetInstall reads an install from the DB.
 func (h *Helpers) GetInstall(ctx context.Context, installID string) (*app.Install, error) {
 	install := app.Install{}
 	res := h.db.WithContext(ctx).
-		Preload("AWSAccount").
-		Preload("AzureAccount").
-		Preload("AppSandboxConfig").
 		Preload("AppRunnerConfig").
 		Preload("InstallInputs", func(db *gorm.DB) *gorm.DB {
 			return db.Order("install_inputs_view_v1.created_at DESC")
 		}).
-		Preload("App").
-		Preload("App.Org").
 		Preload("RunnerGroup").
 		Preload("RunnerGroup.Runners").
-		First(&install, "id = ?", installID)
+		Preload("AWSAccount").
+		Preload("AzureAccount").
+		Preload("App").
+		Preload("App.AppInputConfigs", func(db *gorm.DB) *gorm.DB {
+			return db.Order("app_input_configs.created_at DESC")
+		}).
+		Preload("App.AppSecrets", func(db *gorm.DB) *gorm.DB {
+			return db.Order("app_secrets.created_at DESC")
+		}).
+		Preload("InstallSandboxRuns", func(db *gorm.DB) *gorm.DB {
+			return db.Order("install_sandbox_runs.created_at DESC")
+		}).
+		Preload("InstallSandboxRuns.AppSandboxConfig").
+		Preload("App.Org").
+		Preload("AppSandboxConfig").
+		Where("name = ?", installID).
+		Or("id = ?", installID).
+		First(&install)
 	if res.Error != nil {
 		return nil, fmt.Errorf("unable to get install: %w", res.Error)
 	}
---
+++ services/ctl-api/internal/app/installs/helpers/get_install_sandbox_runs.go
@@ -0,0 +1,29 @@
+package helpers
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
+)
+
+// getInstallSandboxRuns reads an install's sandbox runs from the DB.
+func (h *Helpers) getInstallSandboxRuns(ctx context.Context, installID string) ([]app.InstallSandboxRun, error) {
+	var installSandboxRuns []app.InstallSandboxRun
+	res := h.db.WithContext(ctx).
+		Preload("AppSandboxConfig").
+		Preload("AppSandboxConfig").
+		Preload("AppSandboxConfig.PublicGitVCSConfig").
+		Preload("AppSandboxConfig.ConnectedGithubVCSConfig").
+		Preload("AppSandboxConfig.ConnectedGithubVCSConfig.VCSConnection").
+		Preload("RunnerJob").
+		Preload("LogStream").
+		Where("install_id = ?", installID).
+		Order("created_at desc").
+		Find(&installSandboxRuns)
+	if res.Error != nil {
+		return nil, fmt.Errorf("unable to get install sandbox runs: %w", res.Error)
+	}
+
+	return installSandboxRuns, nil
+}
