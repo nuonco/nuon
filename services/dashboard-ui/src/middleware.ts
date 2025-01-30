437c4f8fe9209fa2cdc1296b29995a8b18290f6b
Nat Hamilton <info@nathamilton.com>
2025-01-30 09:06:17 -0500

    fix(ui): redirect to apps route form org root (#7821)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/middleware.ts
+++ services/dashboard-ui/src/middleware.ts
@@ -5,30 +5,28 @@ import { API_URL } from '@/utils/configs'
 import type { TOrg } from '@/types'
 
 export default async function middleware(request: NextRequest) {
+  const pathname = new URL(request.url).pathname
   const headers = new Headers(request.headers)
   const session = await getSession()
   // set origin url encase of login redirect
-  headers.set('x-origin-path', new URL(request.url).pathname)
+  headers.set('x-origin-path', pathname)
 
   if (session) {
-    if (
-      new URL(request.url).pathname === '/' ||
-      new URL(request.url).pathname.split('/')[1] === 'beta'
-    ) {
-      let redirectPath = '/request-access'
-      const orgSession = request.cookies.get('org-session')
+    let redirectPath = '/request-access'
+    const orgSession = request.cookies.get('org-session')
 
-      const orgs: Array<TOrg> = await (
-        await fetch(`${API_URL}/v1/orgs`, {
-          cache: 'no-store',
-          headers: {
-            Authorization: `Bearer ${session?.accessToken}`,
-            'Content-Type': 'application/json',
-            'X-Nuon-Org-ID': '',
-          },
-        })
-      ).json()
+    const orgs: Array<TOrg> = await (
+      await fetch(`${API_URL}/v1/orgs`, {
+        cache: 'no-store',
+        headers: {
+          Authorization: `Bearer ${session?.accessToken}`,
+          'Content-Type': 'application/json',
+          'X-Nuon-Org-ID': '',
+        },
+      })
+    ).json()
 
+    if (/^\/[^\/]+$/.test(pathname)) {
       if (
         orgSession &&
         orgs.length > 0 &&
@@ -42,6 +40,22 @@ export default async function middleware(request: NextRequest) {
       return NextResponse.redirect(new URL(redirectPath, request.url), {
         headers,
       })
+    } else {
+      if (pathname === '/' || pathname.split('/')[1] === 'beta') {
+        if (
+          orgSession &&
+          orgs.length > 0 &&
+          orgs.some((org) => org.id === orgSession?.value)
+        ) {
+          redirectPath = `/${orgSession?.value}/apps`
+        } else if (orgs.length > 0) {
+          redirectPath = `/${orgs[0].id}/apps`
+        }
+
+        return NextResponse.redirect(new URL(redirectPath, request.url), {
+          headers,
+        })
+      }
     }
   }
 
