1b15e61db703f325edf7d51f88b08c16f94da036
Nat Hamilton <info@nathamilton.com>
2025-02-04 17:14:22 -0500

    feat(ui): runner last seen & chart updates (#7856)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/components/Grid.tsx
+++ services/dashboard-ui/src/components/Grid.tsx
@@ -3,18 +3,23 @@ import classNames from 'classnames'
 
 export interface IGrid extends React.HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode
-  variant?: 'default' | '3-cols'
+  variant?: 'default' | '3-cols' | '2-cols'
 }
 
-export const Grid: FC<IGrid> = ({ className, children, variant = 'default' }) => {
+export const Grid: FC<IGrid> = ({
+  className,
+  children,
+  variant = 'default',
+}) => {
   return (
     <div
       className={classNames('grid gap-6 w-full', {
-        'auto-rows-auto grid-cols-auto':
-          variant === 'default',
+        'auto-rows-auto grid-cols-auto': variant === 'default',
         'grid-cols-1 md:grid-cols-2 lg:grid-cols-3  h-fit overflow-hidden':
-        variant === '3-cols',
-         [`${className}`]: Boolean(className),
+          variant === '3-cols',
+        'grid-cols-1 md:grid-cols-2  h-fit overflow-hidden':
+          variant === '2-cols',
+        [`${className}`]: Boolean(className),
       })}
     >
       {children}
--- services/dashboard-ui/src/components/RunnerHeartbeatChart.tsx
+++ services/dashboard-ui/src/components/RunnerHeartbeatChart.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import {
+  BarElement,
   Chart as ChartJS,
   CategoryScale,
   LinearScale,
@@ -10,9 +11,12 @@ import {
   Tooltip,
   Legend,
 } from 'chart.js'
+import classNames from 'classnames'
 import { DateTime } from 'luxon'
 import { type FC } from 'react'
-import { Line } from 'react-chartjs-2'
+import { Bar } from 'react-chartjs-2'
+import { ToolTip } from '@/components/ToolTip'
+import { Text } from '@/components/Typography'
 import type { TRunnerRecentHeartbeat } from '@/types'
 
 ChartJS.register(
@@ -20,80 +24,87 @@ ChartJS.register(
   LinearScale,
   PointElement,
   LineElement,
+  BarElement,
   Title,
   Tooltip,
   Legend
 )
 
-export const options = {}
+function getMinutesOfCurrentHour() {
+  const now = new Date()
+  const currentHour = now.getHours()
+  const minutesArray = []
 
-export const RunnerHeartbeatChart: FC<{
-  heartBeats: Array<TRunnerRecentHeartbeat>
-}> = ({ heartBeats }) => {
-  const parsedData = heartBeats?.reduce(
-    (acc, item) => {
-      acc['labels'].push(
-        DateTime.fromISO(item?.truncated_time).toLocaleString(
-          DateTime.TIME_SIMPLE
-        )
+  for (let minute = 0; minute < 60; minute++) {
+    minutesArray.push(
+      new Date(
+        now.getFullYear(),
+        now.getMonth(),
+        now.getDate(),
+        currentHour,
+        minute
       )
-      acc['data'].push(item?.record_count)
-
-      return acc
-    },
-    { labels: [], data: [] }
-  )
-
-  const data = {
-    labels: parsedData['labels'],
-    datasets: [
-      {
-        label: 'Heartbeat count',
-        data: parsedData['data'],
-        borderColor: '#8040BF',
-        backgroundColor: '#F2E5FF',
-        stepped: true,
-      },
-    ],
+    )
   }
 
-  return (
-    <Line
-      options={{
-        responsive: true,
-        plugins: {
-          legend: {
-            position: 'top' as const,
-            display: false,
-          },
-          title: {
-            display: false,
-            text: 'Heartbeat records',
-          },
-        },
+  return minutesArray
+}
 
-        scales: {
-          x: {
-            ticks: {
-              //              maxTicksLimit: 30,
-            },
-            grid: {
-              color: '#CFD6DD66',
-            },
-          },
-          y: {
-            ticks: {
-              // maxTicksLimit: 2,
-            },
-            grid: {
-              color: '#CFD6DD66',
-            },
-            max: 12,
-            min: 0,
-          },
-        },
-      }}
-      data={data}
-    />
+export const RunnerHeartbeatChart: FC = () => {
+  // Sample data for service availability
+  // 1 means the service was available, 0 means it was unavailable
+  const uptimeData = [
+    1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+  ] // 60 data points for 60 minutes
+
+  const labels = getMinutesOfCurrentHour()
+
+  return (
+    <div className="flex flex-col gap-6 w-full">
+      <div className="flex items-center gap-0.5">
+        {uptimeData?.map((ut, i) => (
+          <ToolTip
+            key={`${ut}-${i}`}
+            alignment={i <= 9 ? 'left' : i >= 49 ? 'right' : 'center'}
+            parentClassName="flex-auto heartbeat-item-parent"
+            tipContent={
+              ut === 1 ? (
+                <>
+                  <Text variant="med-12">Available</Text>
+                  <Text variant="reg-12">{labels[i].toLocaleString()}</Text>
+                </>
+              ) : (
+                <>
+                  <Text variant="med-12">Unavailable</Text>
+                  <Text variant="reg-12">{labels[i].toLocaleString()}</Text>
+                </>
+              )
+            }
+            isIconHidden
+          >
+            <div
+              className={classNames(
+                'flex-auto max-w-[16px] h-[46px] border rounded-sm heartbeat-item',
+                {
+                  'border-green-600 bg-green-500': ut === 1,
+                  'border-red-600 bg-red-500': ut === 0,
+                }
+              )}
+            />
+          </ToolTip>
+        ))}
+      </div>
+      <div className="flex items-center justify-between bg-black/5 dark:bg-white/5 px-4 py-1">
+        {labels
+          ?.filter((_, i) => (i + 1) % 12 === 0)
+          ?.map((label) => (
+            <Text key={label.toSring()} className="rotate-0" variant="med-12">
+              {label.toLocaleString([], { hour: '2-digit', minute: '2-digit' })}
+            </Text>
+          ))}
+      </div>
+    </div>
   )
 }
