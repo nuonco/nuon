7e96ffcfc49557bf4308e791aa1ef033b42b6777
Jon Morehouse <MorehouseJ09@gmail.com>
2025-02-05 07:48:07 -0800

    feat: reset handlers between runs (#7868)

--- bins/runner/internal/jobs/deploy/job/exec.go
+++ bins/runner/internal/jobs/deploy/job/exec.go
@@ -4,8 +4,16 @@ import (
 	"context"
 
 	"github.com/nuonco/nuon-runner-go/models"
+
+	pkgctx "github.com/powertoolsdev/mono/bins/runner/internal/pkg/ctx"
 )
 
 func (h *handler) Exec(ctx context.Context, job *models.AppRunnerJob, jobExecution *models.AppRunnerJobExecution) error {
+	l, err := pkgctx.Logger(ctx)
+	if err != nil {
+		return err
+	}
+
+	l.Warn("job components are no longer supported, please use an action instead")
 	return nil
 }
---
+++ bins/runner/internal/jobs/deploy/job/reset.go
@@ -0,0 +1,11 @@
+package job
+
+import (
+	"context"
+
+	"github.com/nuonco/nuon-runner-go/models"
+)
+
+func (h *handler) Reset(ctx context.Context, job *models.AppRunnerJob, jobExecution *models.AppRunnerJobExecution) error {
+	return nil
+}
