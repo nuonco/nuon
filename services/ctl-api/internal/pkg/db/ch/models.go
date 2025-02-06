cc611f57af7b0d99e5e361ba70902d94f89af7cb
Jon Morehouse <MorehouseJ09@gmail.com>
2025-02-05 22:30:07 -0800

    fix: get ch views working 🙌 (#7883)

--- services/ctl-api/internal/pkg/db/ch/models.go
+++ services/ctl-api/internal/pkg/db/ch/models.go
@@ -1,8 +1,9 @@
 package ch
 
 import (
-	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
 	"gorm.io/gorm"
+
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
 )
 
 type ChModel interface {
@@ -11,17 +12,16 @@ type ChModel interface {
 }
 
 func AllChModels() []ChModel {
-	// NOTE(fd): i have to make this manually because i don't understand interface slices enough
-	models := make([]ChModel, 8)
-	models[0] = app.RunnerHeartBeat{}
-	models[1] = app.RunnerHealthCheck{}
-	models[2] = app.OtelLogRecord{}
-	models[3] = app.OtelTrace{}
-	models[4] = app.OtelMetricSum{}
-	models[5] = app.OtelMetricGauge{}
-	models[6] = app.OtelMetricHistogram{}
-	models[7] = app.OtelMetricExponentialHistogram{}
-	return models
+	return []ChModel{
+		&app.RunnerHeartBeat{},
+		&app.RunnerHealthCheck{},
+		&app.OtelLogRecord{},
+		&app.OtelTrace{},
+		&app.OtelMetricSum{},
+		&app.OtelMetricGauge{},
+		&app.OtelMetricHistogram{},
+		&app.OtelMetricExponentialHistogram{},
+	}
 }
 
 func AllModels() []interface{} {
