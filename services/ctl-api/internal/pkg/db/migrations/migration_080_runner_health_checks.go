01039720a3b16ce4bd28aef83dad2b681f10d555
Jon Morehouse <MorehouseJ09@gmail.com>
2025-02-06 10:57:02 -0800

    fix: fast follows to migrations (#7891)
    Co-authored-by: nuonbot <bot@nuon.co>

--- services/ctl-api/internal/pkg/db/migrations/migration_080_runner_health_checks.go
+++ services/ctl-api/internal/pkg/db/migrations/migration_080_runner_health_checks.go
@@ -9,7 +9,7 @@ import (
 var runnerHealthChecksViewV1 string
 
 func (a *Migrations) migration080RunnerHealthChecks(ctx context.Context) error {
-	dropSQL := `DROP VIEW IF EXISTS runner_health_checks_view_v1`
+	dropSQL := `DROP VIEW IF EXISTS runner_health_checks_view_v1 ON CLUSTER simple`
 	if res := a.chDB.WithContext(ctx).
 		Exec(dropSQL); res.Error != nil {
 		return res.Error
