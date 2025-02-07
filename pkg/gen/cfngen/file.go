8954998023a0a172c6c8a8333d08150bbf52c37b
sam boyer <sam@nuon.co>
2025-02-06 23:01:06 -0500

    chore: Move cfngen back into exp (#7908)

--- pkg/gen/cfngen/file.go
+++
@@ -1,8 +0,0 @@
-package cfngen
-
-// CfgFile represents the temporary prototype configuration file we're using to iterate on generator inputs and config values.
-// Remove this once that work is done.
-type CfgFile struct {
-	Internal  InternalValues  `toml:"internal"`
-	VendorCfg AppConfigValues `toml:"vendorcfg"`
-}
