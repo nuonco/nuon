7df46f0b1f57bf4bbcbf376f72a209475290f5a4
Jon Morehouse <MorehouseJ09@gmail.com>
2025-02-09 01:06:15 -0600

    fix: crons for actions (#7927)
    * feat: update cron workflows
    
    * fix: cron workflows

--- services/ctl-api/internal/app/installs/worker/cron_action_workflow.go
+++ services/ctl-api/internal/app/installs/worker/cron_action_workflow.go
@@ -4,39 +4,40 @@ import (
 	"github.com/pkg/errors"
 	"go.temporal.io/sdk/workflow"
 
+	"github.com/powertoolsdev/mono/pkg/generics"
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/app/installs/worker/activities"
 )
 
 // Run an action workflow config from a cron trigger
 type CronActionWorkflowRequest struct {
-	InstallID        string             `validate:"required" json:"install_id"`
-	ActionWorkflowID string             `validate:"required" json:"action_workflow_id"`
-	RunEnvVars       map[string]*string `validate:"required" json:"run_env_vars"`
+	ID string `validate:"required" json:"id"`
 }
 
 func (w *Workflows) CronActionWorkflow(ctx workflow.Context, req *CronActionWorkflowRequest) error {
-	installActionWorkflow, err := activities.AwaitGetInstallActionWorkflow(ctx, activities.GetInstallActionWorkflowRequest{
-		InstallID:        req.InstallID,
-		ActionWorkflowID: req.ActionWorkflowID,
-	})
+	installActionWorkflow, err := activities.AwaitGetInstallActionWorkflowByID(ctx, req.ID)
 	if err != nil {
 		return errors.Wrap(err, "unable to get install action workflow")
 	}
 
+	runEnvVars := map[string]*string{
+		"TRIGGER_TYPE": generics.ToPtr("cron"),
+	}
+
 	actionWorkflowRun, err := activities.AwaitCreateActionWorkflowRun(ctx, &activities.CreateActionWorkflowRunRequest{
 		InstallActionWorkflowID: installActionWorkflow.ID,
-		ActionWorkflowID:        req.ActionWorkflowID,
+		ActionWorkflowID:        installActionWorkflow.ActionWorkflowID,
+		InstallID:               installActionWorkflow.InstallID,
 		TriggerType:             app.ActionWorkflowTriggerTypeCron,
-		TriggeredByID:           req.ActionWorkflowID,
+		TriggeredByID:           installActionWorkflow.ActionWorkflowID,
 		TriggeredByType:         "action_workflows",
-		RunEnvVars:              req.RunEnvVars,
+		RunEnvVars:              runEnvVars,
 	})
 	if err != nil {
 		return errors.Wrap(err, "unable to create action workflow config")
 	}
 
-	if err := w.actionWorkflowRun(ctx, req.InstallID, actionWorkflowRun.ID); err != nil {
+	if err := w.actionWorkflowRun(ctx, installActionWorkflow.InstallID, actionWorkflowRun.ID); err != nil {
 		return errors.Wrap(err, "unable to create action workflow run")
 	}
 
