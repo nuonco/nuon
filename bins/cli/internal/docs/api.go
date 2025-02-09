e169cd69b9f4f76281f8f228efa79b08e697a183
Jon Morehouse <MorehouseJ09@gmail.com>
2025-02-08 18:13:25 -0600

    feat: add `docs` subcommand (#7924)
    * feat: add `docs` subcommands
    
    * update
    
    * fix

---
+++ bins/cli/internal/docs/api.go
@@ -0,0 +1,29 @@
+package docs
+
+import (
+	"context"
+	"fmt"
+	"net/url"
+
+	"github.com/pkg/browser"
+
+	"github.com/powertoolsdev/mono/pkg/ui"
+)
+
+func (s *Service) BrowseAPI(ctx context.Context, asJSON bool) error {
+	ui.Line(ctx, "opening up api docs with local api-key and org-id preauthorized")
+
+	params := url.Values{}
+	params.Add("org_id", s.cfg.OrgID)
+	params.Add("api_key", "Bearer "+s.cfg.APIToken)
+
+	url := fmt.Sprintf("%s/docs/index.html?%s", s.cfg.APIURL, params.Encode())
+
+	if asJSON {
+		ui.Line(ctx, url)
+	} else {
+		browser.OpenURL(url)
+	}
+
+	return nil
+}
