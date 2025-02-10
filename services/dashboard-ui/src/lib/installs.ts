6b1139e6fe5cd8f61181009e65f1916ba097a51a
Nat Hamilton <info@nathamilton.com>
2025-02-10 16:17:20 -0500

    feat(ui): custom env_vars for action runs (#7937)
    * fix(ui): install overview layout
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * fix(ui): runner ui updates
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat(ui): custom env_vars for action workflow runs
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/lib/installs.ts
+++ services/dashboard-ui/src/lib/installs.ts
@@ -200,15 +200,20 @@ export async function reprovisionInstall({
 
 export interface IRunInstallActionWorkflow extends IGetInstall {
   actionWorkflowConfigId: string
+  options?: {
+    run_env_vars: Record<string, string>
+  }
 }
 
 export async function runInstallActionWorkflow({
   actionWorkflowConfigId,
   installId,
   orgId,
+  options,
 }: IRunInstallActionWorkflow) {
+  
   return mutateData({
-    data: { action_workflow_config_id: actionWorkflowConfigId },
+    data: { action_workflow_config_id: actionWorkflowConfigId, ...options },
     errorMessage: 'Unable to run action workflow on this install.',
     orgId,
     path: `installs/${installId}/action-workflows/runs`,
