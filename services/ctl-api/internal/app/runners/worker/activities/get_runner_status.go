fa4d44c4fb2a0b75afaf1b69b3f5b6ffb34f9b7a
Jon Morehouse <MorehouseJ09@gmail.com>
2025-02-10 14:27:40 -0600

    fix: various runner health check + actions things (#7936)

--- services/ctl-api/internal/app/runners/worker/activities/get_runner_status.go
+++ services/ctl-api/internal/app/runners/worker/activities/get_runner_status.go
@@ -14,9 +14,6 @@ type GetRunnerStatusRequest struct {
 // @temporal-gen activity
 // @by-id ID
 func (a *Activities) GetRunnerStatus(ctx context.Context, req GetRunnerStatusRequest) (app.RunnerStatus, error) {
-	// NOTE(jm): remove this once the runner health checks are added
-	return app.RunnerStatusActive, nil
-
 	runner, err := a.getRunner(ctx, req.ID)
 	if err != nil {
 		return app.RunnerStatusUnknown, fmt.Errorf("unable to get runner status: %w", err)
