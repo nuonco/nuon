7d48eb559e9f45ddc79575b7ab12172892b6a685
Jon Morehouse <MorehouseJ09@gmail.com>
2025-02-14 20:00:55 -0800

    feat: add addtl metrics (#7999)
    * feat: add addtl metrics
    
    * fix

--- services/ctl-api/internal/pkg/workflows/job/workflow.go
+++ services/ctl-api/internal/pkg/workflows/job/workflow.go
@@ -1,9 +1,13 @@
 package job
 
 import (
+	"fmt"
+
 	"github.com/go-playground/validator/v10"
 	"go.uber.org/fx"
 
+	"github.com/powertoolsdev/mono/pkg/metrics"
+	tmetrics "github.com/powertoolsdev/mono/pkg/temporal/metrics"
 	teventloop "github.com/powertoolsdev/mono/services/ctl-api/internal/pkg/eventloop/temporal"
 )
 
@@ -12,17 +16,29 @@ import (
 // effectively.
 type Workflows struct {
 	evClient teventloop.Client
+	mw       tmetrics.Writer
 }
 
 type Params struct {
 	fx.In
 
-	V        *validator.Validate
-	EVClient teventloop.Client
+	V             *validator.Validate
+	EVClient      teventloop.Client
+	MetricsWriter metrics.Writer
 }
 
 func New(params Params) (*Workflows, error) {
+	tmw, err := tmetrics.New(params.V,
+		tmetrics.WithMetricsWriter(params.MetricsWriter),
+		tmetrics.WithTags(map[string]string{
+			"context": "worker",
+		}))
+	if err != nil {
+		return nil, fmt.Errorf("unable to create temporal metrics writer: %w", err)
+	}
+
 	return &Workflows{
 		evClient: params.EVClient,
+		mw:       tmw,
 	}, nil
 }
