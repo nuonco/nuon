7401f49c1ed2b89f8fc461ce1fbf79650bd9997f
Jon Morehouse <MorehouseJ09@gmail.com>
2025-02-20 21:10:25 -0800

    chore: add retries + more robust runner unhealthy handling (#8059)
    * chore: add retries + more robust runner unhealthy handling
    
    * update wiki docs
    
    * fix
    
    * update
    
    ---------
    
    Co-authored-by: nuonbot <bot@nuon.co>

--- bins/runner/internal/pkg/log/otel_system.go
+++ bins/runner/internal/pkg/log/otel_system.go
@@ -8,7 +8,6 @@ import (
 	"go.uber.org/zap/zapcore"
 
 	"github.com/powertoolsdev/mono/bins/runner/internal"
-	"github.com/powertoolsdev/mono/bins/runner/internal/pkg/settings"
 )
 
 // the system logger is used to log all things that should not be sent to our API via OTEL
@@ -17,8 +16,7 @@ type OTELSystemParams struct {
 
 	Cfg       *internal.Config
 	LP        *log.LoggerProvider `name:"otel"`
-	Settings  *settings.Settings
-	DevLogger *zap.Logger `name:"dev"`
+	DevLogger *zap.Logger         `name:"dev"`
 }
 
 func NewOTELSystem(params OTELSystemParams) (*zap.Logger, error) {
