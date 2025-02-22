ef7e1f3495eaaaaa70f0d31604165a0192120b9c
Jon Morehouse <MorehouseJ09@gmail.com>
2025-02-21 16:00:57 -0800

    fix: extra logs (#8088)
    * fix
    
    * Revert "chore: remove datadog extra costs (#8087)"
    
    This reverts commit e9c06c143c3724f31091b5f7828b4717c395e4ff.
    
    * fix

--- services/ctl-api/internal/pkg/log/otel_zap.go
+++ services/ctl-api/internal/pkg/log/otel_zap.go
@@ -1,8 +1,6 @@
 package log
 
 import (
-	"os"
-
 	"go.opentelemetry.io/contrib/bridges/otelzap"
 	"go.opentelemetry.io/otel/sdk/log"
 	"go.uber.org/zap"
@@ -15,15 +13,11 @@ func NewLogStreamLogger(logStream *app.LogStream, lp *log.LoggerProvider, sysLog
 	zapCore := otelzap.NewCore("workflow",
 		otelzap.WithLoggerProvider(lp))
 
-	opts := []zap.Option{}
+	double := zap.WrapCore(func(c zapcore.Core) zapcore.Core {
+		return zapcore.NewTee(sysLog.Core(), zapCore)
+	})
 
-	if os.Getenv("IS_NUONCTL") == "true" {
-		opts = append(opts, zap.WrapCore(func(c zapcore.Core) zapcore.Core {
-			return zapcore.NewTee(sysLog.Core(), zapCore)
-		}))
-	}
-
-	l := zap.New(zapCore, opts...)
+	l := zap.New(zapCore, double)
 	for _, attrs := range allAttrs {
 		for k, v := range attrs {
 			l = l.With(zap.String(k, v))
