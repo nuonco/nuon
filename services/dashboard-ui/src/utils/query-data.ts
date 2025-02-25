dd3819107f9cd41b2941abaeaf8a4caf55d78fa8
Nat Hamilton <info@nathamilton.com>
2025-02-25 15:21:59 -0500

    refactor(ui): longer timeout for readme fetch & design fixes (#8126)
    refactro(ui): longer timeout for readme fetch & design fixes
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    Co-authored-by: nuonbot <bot@nuon.co>

--- services/dashboard-ui/src/utils/query-data.ts
+++ services/dashboard-ui/src/utils/query-data.ts
@@ -1,11 +1,24 @@
 import { API_URL } from '@/utils/configs'
 import { getFetchOpts } from '@/utils/get-fetch-opts'
 
+export type TResponseError = {
+  description: string
+  error: string
+  user_error: boolean
+}
+
+export type TQueryError = {
+  errorMessage: string
+  status: Response['status']
+  error: TResponseError
+}
+
 export interface IQueryData {
   errorMessage?: string
   orgId?: string
   path: string
   pathVersion?: 'v1'
+  abortTimeout?: number
 }
 
 export async function queryData<T>({
@@ -13,14 +26,26 @@ export async function queryData<T>({
   orgId,
   path,
   pathVersion = 'v1',
+  abortTimeout = 10000,
 }: IQueryData): Promise<T> {
   const res = await fetch(
     `${API_URL}/${pathVersion}/${path}`,
-    await getFetchOpts(orgId, {}, 10000)
+    await getFetchOpts(orgId, {}, abortTimeout)
   )
 
   if (!res.ok) {
     throw new Error(errorMessage)
+    // const error = await res.json()
+
+    // if (res.status >= 500) {
+
+    // } else {
+    //   return {
+    //     status: res.status,
+    //     error,
+    //     errorMessage,
+    //   }
+    // }
   }
 
   return res.json()
