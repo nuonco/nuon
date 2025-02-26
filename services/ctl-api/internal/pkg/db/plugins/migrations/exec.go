66d8bc4e6dd06a47d7f8c534b0e0d7e4bdd38c8d
Jon Morehouse <MorehouseJ09@gmail.com>
2025-02-25 17:12:14 -0800

    chore: add visibility into startup script (#8127)
    * chore: add visibility into startup script
    
    * update
    
    * update
    
    * udpate

--- services/ctl-api/internal/pkg/db/plugins/migrations/exec.go
+++ services/ctl-api/internal/pkg/db/plugins/migrations/exec.go
@@ -2,29 +2,55 @@ package migrations
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/pkg/errors"
+	"go.uber.org/zap"
 )
 
-func (m *Migrator) Exec(ctx context.Context) error {
-	methods := []func(context.Context, any) error{
-		// join tables must be declared _before_ the auto-migrate due to constraints
-		m.applyJoinTables,
-		m.applyGormMigrations,
+type step struct {
+	name string
 
-		// custom migrations
-		m.applyIndexes,
-		m.applyViews,
-		m.applyMigrations,
+	objMethod func(context.Context, any) error
+}
+
+func (m *Migrator) Exec(ctx context.Context) error {
+	methods := []step{
+		{
+			"join-tables",
+			m.applyJoinTables,
+		},
+		{
+			"gorm-migrations",
+			m.applyGormMigrations,
+		},
+		{
+			"indexes",
+			m.applyIndexes,
+		},
+		{
+			"views",
+			m.applyViews,
+		},
+		{
+			"custom-migrations",
+			m.applyMigrations,
+		},
 	}
+
 	for _, method := range methods {
+		m.l.Info(fmt.Sprintf("executing %s migration method", method.name),
+			zap.String("db_type", m.dbType))
+
 		for _, model := range m.models {
-			if err := method(ctx, model); err != nil {
+			if err := method.objMethod(ctx, model); err != nil {
 				return err
 			}
 		}
 	}
 
+	m.l.Info("applying global migrations",
+		zap.String("db_type", m.dbType))
 	if err := m.applyGlobalMigrations(ctx); err != nil {
 		return errors.Wrap(err, "unable to execute global migrations")
 	}
