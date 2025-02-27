cff356721ab401f3310c441ea139a0c660559f39
Nat Hamilton <info@nathamilton.com>
2025-02-26 19:22:06 -0500

    refactor(ui): loading list pages via ssr streaming (#8150)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/app/[org-id]/installs/page.tsx
+++ services/dashboard-ui/src/app/[org-id]/installs/page.tsx
@@ -1,22 +1,42 @@
+import { Suspense, type FC } from 'react'
+import { ErrorBoundary } from 'react-error-boundary'
 import { withPageAuthRequired } from '@auth0/nextjs-auth0'
-import { DashboardContent, NoInstalls, OrgInstallsTable } from '@/components'
+import {
+  DashboardContent,
+  ErrorFallback,
+  NoInstalls,
+  OrgInstallsTable,
+  Loading,
+  Section,
+} from '@/components'
 import { getInstalls } from '@/lib'
 
 export default withPageAuthRequired(async function Installs({ params }) {
   const orgId = params?.['org-id'] as string
-  const [installs] = await Promise.all([getInstalls({ orgId })])
-
   return (
     <DashboardContent
       breadcrumb={[{ href: `/${orgId}/installs`, text: 'Installs' }]}
     >
-      <section className="px-6 py-8">
-        {installs.length ? (
-          <OrgInstallsTable orgId={orgId} installs={installs} />
-        ) : (
-          <NoInstalls />
-        )}
-      </section>
+      <Section>
+        <ErrorBoundary fallbackRender={ErrorFallback}>
+          <Suspense
+            fallback={
+              <Loading variant="page" loadingText="Loading installs..." />
+            }
+          >
+            <LoadInstalls orgId={orgId} />
+          </Suspense>
+        </ErrorBoundary>
+      </Section>
     </DashboardContent>
   )
 })
+
+const LoadInstalls: FC<{ orgId: string }> = async ({ orgId }) => {
+  const installs = await getInstalls({ orgId })
+  return installs?.length ? (
+    <OrgInstallsTable orgId={orgId} installs={installs} />
+  ) : (
+    <NoInstalls />
+  )
+}
