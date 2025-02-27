a58a4263f2975dac1ca660fcf73cfb7991717470
Dat Boi Diego <fidiego@users.noreply.github.com>
2025-02-26 21:05:46 -0600

    chore: reduce the ttl on runner_health_checks (#8158)

--- services/ctl-api/internal/pkg/db/viewsql/runner_health_checks_view_v1.sql
+++ services/ctl-api/internal/pkg/db/viewsql/runner_health_checks_view_v1.sql
@@ -1,14 +1,19 @@
 WITH ranked_health_checks AS (
-SELECT 
-	rhc.*,
-	toDateTime64(toStartOfMinute(rhc.created_at), 3) as minute_bucket,
-     ROW_NUMBER() OVER (
-     	PARTITION BY rhc.runner_id, toDateTime64(toStartOfMinute(rhc.created_at), 3)
-        ORDER BY rhc.created_at DESC
-     ) AS row_num
-FROM runner_health_checks as rhc
+    SELECT
+        rhc.*,
+        toDateTime64(toStartOfMinute(rhc.created_at), 3) AS minute_bucket,
+        ROW_NUMBER() OVER (
+            PARTITION BY rhc.runner_id,
+            toDateTime64(toStartOfMinute(rhc.created_at), 3)
+            ORDER BY
+                rhc.created_at DESC
+        ) AS row_num
+    FROM
+        runner_health_checks AS rhc
 )
-SELECT 
+SELECT
     *
-FROM ranked_health_checks
-WHERE row_num = 1;
+FROM
+    ranked_health_checks
+WHERE
+    row_num = 1;
