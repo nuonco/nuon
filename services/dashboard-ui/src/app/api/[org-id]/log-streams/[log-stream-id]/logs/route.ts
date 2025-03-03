ef9a394182fed72c0b0504c742c29109d48e9704
Nat Hamilton <info@nathamilton.com>
2025-03-03 18:40:12 -0500

    refactor(ui): increate logs endpoint abort timeout & tooltip z-index fix (#8183)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/app/api/[org-id]/log-streams/[log-stream-id]/logs/route.ts
+++ services/dashboard-ui/src/app/api/[org-id]/log-streams/[log-stream-id]/logs/route.ts
@@ -5,7 +5,11 @@ import { API_URL, getFetchOpts } from '@/utils'
 export const GET = withApiAuthRequired(async (req: NextRequest) => {
   const [orgId, _, logStreamId] = req.url.split('/').slice(4, 7)
   const offset = req?.headers?.get('x-nuon-api-offset') || '0'
-  const fetchOpts = await getFetchOpts(orgId, { 'X-Nuon-API-Offset': offset })
+  const fetchOpts = await getFetchOpts(
+    orgId,
+    { 'X-Nuon-API-Offset': offset },
+    10000
+  )
 
   return fetch(`${API_URL}/v1/log-streams/${logStreamId}/logs`, fetchOpts).then(
     (res) => {
