12a79c49c2a1c8d958a7f5023cf3b0796fba8f9d
Nat Hamilton <info@nathamilton.com>
2025-03-04 11:19:33 -0500

    refactor(ui): only ping api once for org status (#8189)
    refactor(ui): only ping api once for org status, updated snapshots
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/components/OrgStatus.tsx
+++ services/dashboard-ui/src/components/OrgStatus.tsx
@@ -1,9 +1,8 @@
 'use client'
 
-import React, { type FC, useEffect, useState } from 'react'
+import React, { type FC } from 'react'
 import { StatusBadge } from '@/components/Status'
 import type { TOrg } from '@/types'
-import { POLL_DURATION } from '@/utils'
 
 export interface IOrgStatus {
   descriptionAlignment?: 'center' | 'left' | 'right'
@@ -13,38 +12,10 @@ export interface IOrgStatus {
 }
 
 export const OrgStatus: FC<IOrgStatus> = ({
-  initOrg,
+  initOrg: org,
   shouldPoll = false,
   ...props
 }) => {
-  const [org, updateOrg] = useState<TOrg>(initOrg)
-
-  useEffect(() => {
-    const fetchOrg = () => {
-      fetch(`/api/${initOrg.id}`)
-        .then((res) =>
-          res.json().then((o) => {
-            updateOrg(o)
-          })
-        )
-        .catch(console.error)
-    }
-    if (shouldPoll) {
-      const pollOrg = setInterval(fetchOrg, POLL_DURATION)
-
-      /* if (
-       *   org?.status === 'active' ||
-       *   org?.status === 'error' ||
-       *   org?.status === 'failed' ||
-       *   org?.status === 'noop'
-       * ) {
-       *   clearInterval(pollOrg)
-       * } */
-
-      return () => clearInterval(pollOrg)
-    }
-  }, [org, shouldPoll])
-
   return (
     <StatusBadge
       description={org?.status_description}
