23709f74eef3dd4e7db8fff01a5a2d6460cd319c
sam boyer <sam@nuon.co>
2025-03-11 10:17:49 -0400

    chore: Auto shutdown zombie event loops (#8226)
    * chore: Auto shutdown zombie event loops
    
    * Document eventloop.Loop and rename type constraints
    
    * Dodge panic, rename activity, remove print
    
    * Remove Dummy model, split event loop type
    
    * Fix call-side impl with reflection
    
    * Move to non-abstracted approach
    
    * Trim comment
    
    * Disable check exists check in all event loops

---
+++ services/ctl-api/internal/app/actions/worker/activities/exists.go
@@ -0,0 +1,20 @@
+package activities
+
+import (
+	"context"
+
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/pkg/eventloop/loop"
+)
+
+type CheckExistsRequest struct {
+	ID string
+}
+
+// @temporal-gen activity
+// @schedule-to-close-timeout 1m
+// @start-to-close-timeout 10s
+// @by-id ID
+func (a *Activities) CheckExists(ctx context.Context, req CheckExistsRequest) (bool, error) {
+	return loop.CheckExists[*app.ActionWorkflow](ctx, a.db, req.ID)
+}
