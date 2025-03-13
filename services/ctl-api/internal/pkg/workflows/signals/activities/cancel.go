ab7e0d8fc47f0af5d765c1032285a6d634c8a483
Jon Morehouse <MorehouseJ09@gmail.com>
2025-03-13 09:36:30 -0600

    feat: trigger signals with activities, not side-effects (#8275)
    * feat: trigger signals via activities, not sideeffects
    
    * update
    
    * feat: add ability to restart all orgs
    
    ---------
    
    Co-authored-by: nuonbot <bot@nuon.co>

---
+++ services/ctl-api/internal/pkg/workflows/signals/activities/cancel.go
@@ -0,0 +1,15 @@
+package activities
+
+import (
+	"context"
+)
+
+type CancelRequest struct {
+	ID        string `validate:"required"`
+	Namespace string `validate:"required"`
+}
+
+// @temporal-gen activity
+func (a *Activities) PkgSignalsCancel(ctx context.Context, req *CancelRequest) error {
+	return a.evClient.Cancel(ctx, req.Namespace, req.ID)
+}
