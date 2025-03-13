fcdd3f452ed29c2b3a4fa4492b333ef04e688603
Jon Morehouse <MorehouseJ09@gmail.com>
2025-03-13 09:21:55 -0600

    feat: make org/install runner toggleable (#8281)
    * fix: run install runner locally
    
    * feat: make install/org runner toggle'able

---
+++ bins/runner/internal/pkg/dev/disable.go
@@ -0,0 +1,15 @@
+package dev
+
+import "os"
+
+func (d *devver) Disabled() bool {
+	switch d.runnerType {
+	case "org":
+		return os.Getenv("DISABLE_ORG_RUNNER") == "true"
+	case "install":
+		return os.Getenv("DISABLE_INSTALL_RUNNER") == "true"
+	default:
+	}
+
+	return false
+}
