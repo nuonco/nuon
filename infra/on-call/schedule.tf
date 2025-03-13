7d3b192f57b37c3c913973e1e9abcb2981a2b96c
Harsh Thakur <harsh@nuon.co>
2025-03-13 20:14:45 +0530

    feat: add pagerduty schedule (#8278)
    * add pagerduty schedule
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    * remove output field rendered_percent
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    ---------
    
    Signed-off-by: harsh <harsh@nuon.co>

---
+++ infra/on-call/schedule.tf
@@ -0,0 +1,23 @@
+resource "pagerduty_schedule" "schedule" {
+    name           = "Engineering"
+    teams          = [
+       pagerduty_team.engineering.id,
+    ]
+
+    time_zone      = "America/Los_Angeles"
+    layer {
+        end                          = null
+        name                         = "On-Call-0"
+        rotation_turn_length_seconds = 604800
+        rotation_virtual_start       = "2025-03-11T12:30:00-07:00"
+        start                        = "2025-03-11T12:57:03-07:00"
+        users                        = [
+            pagerduty_user.fred.id,
+            pagerduty_user.harsh.id,
+            pagerduty_user.jon.id,
+            pagerduty_user.nat.id,
+            pagerduty_user.rob.id,
+            pagerduty_user.sam.id,
+        ]
+    }
+}
