32bc44215f3aa0802f9c0e6804284aaa30d1b3b9
Nat Hamilton <info@nathamilton.com>
2025-03-13 10:20:34 -0400

    feat(ui): start break glass & team UI (#8276)
    * feat(ui): added account type and start of team members page
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat(ui): show operation jobs, style and type updates for runner stuff
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * refactor(ui): style updates, show error message in cancel job button
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat(ui): start of break glass form UI
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * refactor(ui): clean up install management dropdown styles
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat(api): add break-glass org feature
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat(ui): add break glass pages and cloudformation svgf
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

---
+++ services/dashboard-ui/src/app/[org-id]/installs/[install-id]/break-glass/generate-stack/page.tsx
@@ -0,0 +1,106 @@
+import Image from 'next/image'
+import { type FC, Suspense } from 'react'
+import { ErrorBoundary } from 'react-error-boundary'
+import { withPageAuthRequired } from '@auth0/nextjs-auth0'
+import {
+  AppSandboxConfig,
+  AppSandboxVariables,
+  Config,
+  ConfigContent,
+  DashboardContent,
+  Duration,
+  ErrorFallback,
+  ID,
+  InstallCloudPlatform,
+  InstallInputs,
+  InstallInputsModal,
+  InstallPageSubNav,
+  InstallStatuses,
+  Loading,
+  Notice,
+  StatusBadge,
+  Section,
+  SectionHeader,
+  Text,
+  Time,
+  Markdown,
+} from '@/components'
+import {
+  InstallManagementDropdown,
+  BreakGlassForm,
+} from '@/components/Installs'
+import {
+  getInstall,
+  getInstallCurrentInputs,
+  getInstallReadme,
+  getInstallRunnerGroup,
+  getRunnerLatestHeartbeat,
+} from '@/lib'
+import { RUNNERS, USER_REPROVISION } from '@/utils'
+
+export default withPageAuthRequired(async function InstallBreakGlass({
+  params,
+}) {
+  const orgId = params?.['org-id'] as string
+  const installId = params?.['install-id'] as string
+  const install = await getInstall({ installId, orgId })
+
+  return (
+    <DashboardContent
+      breadcrumb={[
+        {
+          href: `/${orgId}/installs`,
+          text: 'Installs',
+        },
+        { href: `/${orgId}/installs/${installId}`, text: install.name },
+        {
+          href: `/${orgId}/installs/${installId}/break-glass`,
+          text: 'Access permissions',
+        },
+      ]}
+    >
+      <>
+        <header className="px-6 py-8 flex flex-col border-b">
+          <hgroup className="flex flex-col gap-2">
+            <Text level={1} role="heading" variant="semi-18">
+              Break glass permissions
+            </Text>
+            <Text
+              variant="reg-12"
+              className="text-cool-grey-600 dark:text-white/70"
+            >
+              Develop robust CloudFormation templates to streamline AWS
+              infrastructure deployment for clients.
+            </Text>
+          </hgroup>
+        </header>
+
+        <Section
+          heading={
+            <div className="flex flex-col gap-2">
+              <Text variant="semi-18">
+                <Image
+                  className=""
+                  src={`/aws-cloudformation.svg`}
+                  alt=""
+                  height={24}
+                  width={24}
+                />
+                CloudFormation
+              </Text>
+              <Text
+                variant="reg-14"
+                className="text-cool-grey-600 dark:text-white/70"
+              >
+                Note: Review access permissions thorougly before implementing
+                modifications.
+              </Text>
+            </div>
+          }
+        >
+          <BreakGlassForm install={install} />
+        </Section>
+      </>
+    </DashboardContent>
+  )
+})
--- services/dashboard-ui/src/app/not-found.tsx
+++ services/dashboard-ui/src/app/not-found.tsx
@@ -1,10 +1,13 @@
-import { Dashboard, Link, Text } from '@/components'
+import { Dashboard, EmptyStateGraphic, Link, Text } from '@/components'
 
 export default function NotFound() {
   return (
     <Dashboard>
       <main className="flex h-full gap-6">
         <div className="flex flex-col gap-6 py-6 md:py-12 lg:py-24 lg:max-w-2xl">
+          <div>
+            <EmptyStateGraphic />
+          </div>
           <Text variant="semi-18" level={1}>
             Nuon data not found
           </Text>
