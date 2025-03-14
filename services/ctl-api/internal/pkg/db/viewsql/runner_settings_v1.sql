d726fac54e8a2d3d4e6b1390e2f7791b24bc9473
Dat Boi Diego <fidiego@users.noreply.github.com>
2025-03-14 11:30:28 -0500

    feat: ctl-api: job loop groups in runner settings (#8287)
    * chore: ctl-api: re-organize the columns to avoid collision w/ new column
    
    * feat: ctl-api: Groups in RunnerGroupSettings
    
    to determine which loops a runner should run

--- services/ctl-api/internal/pkg/db/viewsql/runner_settings_v1.sql
+++ services/ctl-api/internal/pkg/db/viewsql/runner_settings_v1.sql
@@ -1,3 +1,6 @@
-  SELECT runner_group_settings.*, runners.id as runner_id from runner_group_settings 
-	JOIN runners ON 
-	runners.runner_group_id=runner_group_settings.runner_group_id 
+SELECT
+    runners.id AS runner_id,
+    runner_group_settings.*
+FROM
+    runner_group_settings
+    JOIN runners ON runners.runner_group_id = runner_group_settings.runner_group_id
