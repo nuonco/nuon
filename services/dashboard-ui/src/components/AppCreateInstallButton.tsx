57707b99a3687981a6d90850929069d2250f68d5
Nat Hamilton <info@nathamilton.com>
2025-03-14 13:34:20 -0400

    refactor(ui): cf link in install form & break glass updates (#8291)
    * refactor(ui): click to copy button and style updates
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * refactor(ui): cloudformation link in create install form
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * refactor(ui): update breakglass form to show CF link
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/components/AppCreateInstallButton.tsx
+++ services/dashboard-ui/src/components/AppCreateInstallButton.tsx
@@ -1,14 +1,17 @@
 'use client'
 
 import { useRouter } from 'next/navigation'
-import React, { type FC, useState } from 'react'
+import React, { type FC, useEffect, useState } from 'react'
 import { Cube } from '@phosphor-icons/react'
 import { Button } from '@/components/Button'
+import { composeCloudFormationQuickCreateUrl } from '@/components/Installs/helpers'
 import { InstallForm } from '@/components/InstallForm'
+import { Loading } from '@/components/Loading'
 import { Modal } from '@/components/Modal'
+import { Notice } from '@/components/Notice'
 import { Text } from '@/components/Typography'
 import { createAppInstall } from './app-actions'
-import type { TAppInputConfig } from '@/types'
+import type { TAppInputConfig, TAppSandboxConfig } from '@/types'
 
 interface IAppCreateInstallButton {
   appId: string
@@ -24,7 +27,28 @@ export const AppCreateInstallButton: FC<IAppCreateInstallButton> = ({
   platform,
 }) => {
   const [isOpen, setIsOpen] = useState(false)
+  const [isLoading, setIsLoading] = useState(true)
+  const [sandboxConfig, setSandboxConfig] = useState<
+    TAppSandboxConfig | undefined
+  >()
+  const [error, setError] = useState<string>()
   const router = useRouter()
+
+  useEffect(() => {
+    if (isOpen) {
+      fetch(`/api/${orgId}/apps/${appId}/sandbox-configs/latest`)
+        .then((res) =>
+          res.json().then((sandbox) => {
+            setSandboxConfig(sandbox as TAppSandboxConfig)
+            setIsLoading(false)
+          })
+        )
+        .catch((err) => {
+          setIsLoading(false)
+          setError(err?.message || 'Unable to fetch latest sandbox config')
+        })
+    }
+  }, [isOpen])
 
   return (
     <>
@@ -44,24 +68,35 @@ export const AppCreateInstallButton: FC<IAppCreateInstallButton> = ({
         }}
         contentClassName="px-0 py-0"
       >
-        <InstallForm
-          onSubmit={(formData) => {
-            return createAppInstall({
-              appId,
-              orgId,
-              formData,
-              platform,
-            })
-          }}
-          onSuccess={(install) => {
-            router.push(`/${orgId}/installs/${install.id}/history`)
-          }}
-          onCancel={() => {
-            setIsOpen(false)
-          }}
-          platform={platform}
-          inputConfig={inputConfig}
-        />
+        {isLoading ? (
+          <div className="p-6">
+            <Loading loadingText="Loading configs..." variant="stack" />
+          </div>
+        ) : error ? (
+          <div className="p-6">
+            <Notice>{error}</Notice>
+          </div>
+        ) : (
+          <InstallForm
+            onSubmit={(formData) => {
+              return createAppInstall({
+                appId,
+                orgId,
+                formData,
+                platform,
+              })
+            }}
+            onSuccess={(install) => {
+              router.push(`/${orgId}/installs/${install.id}/history`)
+            }}
+            onCancel={() => {
+              setIsOpen(false)
+            }}
+            platform={platform}
+            inputConfig={inputConfig}
+            cfLink={composeCloudFormationQuickCreateUrl(sandboxConfig)}
+          />
+        )}
       </Modal>
       <Button
         className="flex items-center gap-2 text-sm font-medium"
