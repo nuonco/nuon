37655c7a1551b98334f0cb7238bfe13057257b75
Nat Hamilton <info@nathamilton.com>
2025-03-17 14:41:26 -0400

    feat(ui): team page MVP (#8315)
    * refactor(ui): set user data on datadog RUM
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat(ui): team page MVP
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * refactor(ui): correct env for datadog integrations
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/utils/datadog-logs.tsx
+++ services/dashboard-ui/src/utils/datadog-logs.tsx
@@ -3,21 +3,20 @@
 import { datadogLogs } from '@datadog/browser-logs'
 import React, { type FC, useEffect } from 'react'
 
-export const InitDatadogLogs: FC = () => {
-   const ddEnv =
-        process?.env?.NEXT_PUBLIC_DATADOG_ENV ||
-        window.location?.host?.split('.')[1]
-  
+export const InitDatadogLogs: FC<{ env?: 'local' | 'stage' | 'prod' }> = ({
+  env = 'local',
+}) => {
   useEffect(() => {
     const initDDLogs = () => {
       datadogLogs.init({
-        clientToken: process?.env?.NEXT_PUBLIC_DATADOG_CLIENT_TOKEN ||
+        clientToken:
+          process?.env?.NEXT_PUBLIC_DATADOG_CLIENT_TOKEN ||
           'pub6fb6cfe0d2ec271a2456660e54ba5e08',
         site: process?.env?.NEXT_PUBLIC_DATADOG_SITE || 'us5.datadoghq.com',
         forwardConsoleLogs: ['error', 'info'],
         forwardErrorsToLogs: true,
         sessionSampleRate: 100,
-        env: ddEnv || 'local',
+        env,
         service: 'dashboard',
       })
     }
