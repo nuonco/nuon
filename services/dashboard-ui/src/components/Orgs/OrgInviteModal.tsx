1a4a3c4be9e76e9e3959be16783c2d2d599b3916
Nat Hamilton <info@nathamilton.com>
2025-03-17 14:41:26 -0400

    feat(ui): team page MVP (#8315)
    * refactor(ui): set user data on datadog RUM
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat(ui): team page MVP
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * refactor(ui): correct env for datadog integrations
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

---
+++ services/dashboard-ui/src/components/Orgs/OrgInviteModal.tsx
@@ -0,0 +1,106 @@
+'use client'
+
+import React, { type FC, useState } from 'react'
+import { UserPlus } from '@phosphor-icons/react'
+import { Button } from '@/components/Button'
+import { Input } from '@/components/Input'
+import { SpinnerSVG } from '@/components/Loading'
+import { Modal } from '@/components/Modal'
+import { Notice } from '@/components/Notice'
+import { Text } from '@/components/Typography'
+import { inviteUserToOrg } from '@/components/org-actions'
+import { useOrg } from './org-context'
+
+interface IOrgInviteModal {}
+
+export const OrgInviteModal: FC<IOrgInviteModal> = ({}) => {
+  const { org } = useOrg()
+  const [isOpen, setIsOpen] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState<string>()
+
+  return (
+    <>
+      <Modal
+        className="!max-w-2xl"
+        contentClassName="!p-0"
+        heading="Invite team member"
+        isOpen={isOpen}
+        onClose={() => {
+          setIsOpen(false)
+        }}
+      >
+        <form
+          onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
+            e.preventDefault()
+            setIsLoading(true)
+            const formData = new FormData(e.currentTarget)
+
+            inviteUserToOrg(formData, org.id)
+              .then(() => {
+                setIsLoading(false)
+                setIsOpen(false)
+              })
+              .catch((err) => {
+                console.error(err)
+                setIsLoading(false)
+                setError('Unable to invite user, refresh page and try again.')
+              })
+          }}
+        >
+          <div className="p-6 flex flex-col gap-4">
+            {error ? <Notice>{error}</Notice> : null}
+            <label className="w-full flex flex-col gap-2">
+              <Text variant="med-14">
+                Email address of the user you want to invite
+              </Text>
+              <Input
+                placeholder="user@email.com"
+                type="email"
+                name="email"
+                required
+              />
+            </label>
+          </div>
+          <div className="p-6 border-t flex gap-3 justify-end">
+            <Button
+              className="text-sm"
+              onClick={() => {
+                setError(undefined)
+                setIsLoading(false)
+                setIsOpen(false)
+              }}
+              type="button"
+            >
+              Cancel
+            </Button>
+            <Button
+              className="text-sm flex items-center gap-2 font-medium"
+              disabled={isLoading}
+              variant="primary"
+            >
+              {isLoading ? (
+                <>
+                  <SpinnerSVG /> Inviting...
+                </>
+              ) : (
+                <>
+                  <UserPlus size="16" /> Invite user
+                </>
+              )}
+            </Button>
+          </div>
+        </form>
+      </Modal>
+      <Button
+        className="text-sm flex items-center gap-2"
+        onClick={() => {
+          setIsOpen(true)
+        }}
+      >
+        <UserPlus size="16" />
+        Invite team member
+      </Button>
+    </>
+  )
+}
--- services/dashboard-ui/src/components/org-actions.ts
+++ services/dashboard-ui/src/components/org-actions.ts
@@ -1,7 +1,9 @@
 'use server'
 
+import { revalidatePath } from 'next/cache'
 import { joinWaitlist, type IJoinWaitlist } from '@/lib'
-import type { TWaitlist } from '@/types'
+import type { TInvite, TWaitlist } from '@/types'
+import { mutateData } from '@/utils'
 
 export async function requestWaitlistAccess(
   formData: FormData
@@ -9,4 +11,21 @@ export async function requestWaitlistAccess(
   const data = Object.fromEntries(formData) as IJoinWaitlist
 
   return joinWaitlist(data)
+}
+
+export async function inviteUserToOrg(
+  formData: FormData,
+  orgId: string
+): Promise<TInvite> {
+  const data = Object.fromEntries(formData)
+
+  return mutateData({
+    errorMessage: 'Unable to invite user',
+    data,
+    path: `orgs/current/invites`,
+    orgId,
+  }).then((invite) => {
+    revalidatePath(`/${orgId}/team`)
+    return invite
+  })
 }
