9a908d9432a6fc9b1ab0324f07afa7615f08291d
Nat Hamilton <info@nathamilton.com>
2025-03-18 10:24:07 -0400

    refactor(ui): page loading improvements (#8341)
    * fix(ui): tooltip z-index again
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * refactor(ui): click-to-copy button styles
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * fix(ui): correct component ids in install comp table
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * refactor(ui): streaming to load deploy page
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * refactor(ui): parallel fetch on build page
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * refactor(ui): load builds in client for deploy build dropdown
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * refactor(ui): use streaming to load install component page
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * chore(ui): remove old install dropdown code
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/app/[org-id]/installs/[install-id]/components/[component-id]/deploys/[deploy-id]/page.tsx
+++ services/dashboard-ui/src/app/[org-id]/installs/[install-id]/components/[component-id]/deploys/[deploy-id]/page.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from 'next'
+import { type FC, Suspense } from 'react'
+import { ErrorBoundary } from 'react-error-boundary'
 import { withPageAuthRequired } from '@auth0/nextjs-auth0'
 import {
   CalendarBlank,
@@ -12,8 +14,10 @@ import {
   DashboardContent,
   DeployStatus,
   Duration,
+  ErrorFallback,
   InstallDeployIntermediateData,
   Link,
+  Loading,
   LogStreamProvider,
   OperationLogsSection,
   StatusBadge,
@@ -31,14 +35,14 @@ import {
   getInstallDeploy,
   getInstallDeployPlan,
 } from '@/lib'
-import type { TInstallDeployPlan } from '@/types'
+import type { TInstallDeployPlan, TInstall } from '@/types'
 import { CANCEL_RUNNER_JOBS, DEPLOY_INTERMEDIATE_DATA } from '@/utils'
 
 export async function generateMetadata({ params }): Promise<Metadata> {
+  const componentId = params?.['component-id'] as string
   const deployId = params?.['deploy-id'] as string
   const installId = params?.['install-id'] as string
   const orgId = params?.['org-id'] as string
-  const componentId = params?.['component-id'] as string
   const [deploy, component] = await Promise.all([
     getInstallDeploy({
       installDeployId: deployId,
@@ -56,24 +60,21 @@ export async function generateMetadata({ params }): Promise<Metadata> {
 export default withPageAuthRequired(async function InstallComponentDeploy({
   params,
 }) {
+  const componentId = params?.['component-id'] as string
   const deployId = params?.['deploy-id'] as string
   const installId = params?.['install-id'] as string
   const orgId = params?.['org-id'] as string
-  const deploy = await getInstallDeploy({
-    installDeployId: deployId,
-    installId,
-    orgId,
-  })
-  const build = await getComponentBuild({ orgId, buildId: deploy.build_id })
-  const [component, componentConfig, install, deployPlan] = await Promise.all([
-    getComponent({ componentId: build.component_id, orgId }),
-    getComponentConfig({
-      componentId: build.component_id,
-      componentConfigId: build.component_config_connection_id,
+  const [component, deploy, install] = await Promise.all([
+    getComponent({
+      componentId,
+      orgId,
+    }),
+    getInstallDeploy({
+      installDeployId: deployId,
+      installId,
       orgId,
     }),
     getInstall({ installId, orgId }),
-    getInstallDeployPlan({ deployId, installId, orgId }).catch(console.error),
   ])
 
   return (
@@ -132,9 +133,9 @@ export default withPageAuthRequired(async function InstallComponentDeploy({
               Build
             </Text>
             <Text variant="mono-12">
-              <ToolTip alignment="right" tipContent={build.id}>
+              <ToolTip alignment="right" tipContent={deploy.build_id}>
                 <ClickToCopy>
-                  <Truncate variant="small">{build.id}</Truncate>
+                  <Truncate variant="small">{deploy.build_id}</Truncate>
                 </ClickToCopy>
               </ToolTip>
             </Text>
@@ -194,7 +195,7 @@ export default withPageAuthRequired(async function InstallComponentDeploy({
             actions={
               <Text>
                 <Link
-                  href={`/${orgId}/apps/${component.app_id}/components/${component.id}/builds/${build.id}`}
+                  href={`/${orgId}/apps/${component.app_id}/components/${component.id}/builds/${deploy.build_id}`}
                 >
                   Details
                   <CaretRight />
@@ -203,51 +204,162 @@ export default withPageAuthRequired(async function InstallComponentDeploy({
             }
             heading="Component build"
           >
-            <div className="flex items-start justify-start gap-6">
-              <span className="flex flex-col gap-2">
-                <StatusBadge
-                  description={build.status_description}
-                  status={build.status}
-                  label="Status"
-                />
-              </span>
-
-              <span className="flex flex-col gap-2">
-                <Text className="text-cool-grey-600 dark:text-cool-grey-500">
-                  Build date
-                </Text>
-                <Time time={build.created_at} />
-              </span>
-
-              <span className="flex flex-col gap-2">
-                <Text className="text-cool-grey-600 dark:text-cool-grey-500">
-                  Build duration
-                </Text>
-                <Duration
-                  beginTime={build.created_at}
-                  endTime={build.updated_at}
-                />
-              </span>
-            </div>
+            <ErrorBoundary fallbackRender={ErrorFallback}>
+              <Suspense
+                fallback={
+                  <Loading
+                    loadingText="Loading component build..."
+                    variant="stack"
+                  />
+                }
+              >
+                <LoadComponentBuild buildId={deploy.build_id} orgId={orgId} />
+              </Suspense>
+            </ErrorBoundary>
           </Section>
 
-          {DEPLOY_INTERMEDIATE_DATA &&
-          (deployPlan as TInstallDeployPlan)?.actual?.waypoint_plan?.variables
-            ?.intermediate_data?.nuon ? (
-            <InstallDeployIntermediateData
-              install={install}
-              data={
-                (deployPlan as TInstallDeployPlan)?.actual?.waypoint_plan
-                  ?.variables?.intermediate_data
-              }
-            />
+          <Section
+            className="flex-initial"
+            actions={
+              <Text>
+                <Link
+                  href={`/${orgId}/apps/${component.app_id}/components/${component.id}`}
+                >
+                  Details
+                  <CaretRight />
+                </Link>
+              </Text>
+            }
+            heading="Component config"
+          >
+            <ErrorBoundary fallbackRender={ErrorFallback}>
+              <Suspense
+                fallback={
+                  <Loading
+                    loadingText="Loading component config..."
+                    variant="stack"
+                  />
+                }
+              >
+                <LoadComponentConfig
+                  buildId={deploy.build_id}
+                  componentId={deploy.component_id}
+                  orgId={orgId}
+                />
+              </Suspense>
+            </ErrorBoundary>
+          </Section>
+
+          {DEPLOY_INTERMEDIATE_DATA ? (
+            <ErrorBoundary fallbackRender={ErrorFallback}>
+              <Suspense
+                fallback={
+                  <Section>
+                    <Loading
+                      loadingText="Loading intermediate data..."
+                      variant="stack"
+                    />
+                  </Section>
+                }
+              >
+                <LoadIntermediateData
+                  deployId={deploy.id}
+                  install={install}
+                  orgId={orgId}
+                />
+              </Suspense>
+            </ErrorBoundary>
           ) : null}
-
-          <Section heading="Component config">
-            <ComponentConfiguration config={componentConfig} />
-          </Section>
         </div>
       </div>
     </DashboardContent>
   )
 })
+
+// load log stream
+
+// load component build
+const LoadComponentBuild: FC<{ buildId: string; orgId: string }> = async ({
+  buildId,
+  orgId,
+}) => {
+  const build = await getComponentBuild({ buildId, orgId }).catch(console.error)
+
+  return build ? (
+    <div className="flex items-start justify-start gap-6">
+      <span className="flex flex-col gap-2">
+        <StatusBadge
+          description={build.status_description}
+          status={build.status}
+          label="Status"
+        />
+      </span>
+
+      <span className="flex flex-col gap-2">
+        <Text className="text-cool-grey-600 dark:text-cool-grey-500">
+          Build date
+        </Text>
+        <Time time={build.created_at} />
+      </span>
+
+      <span className="flex flex-col gap-2">
+        <Text className="text-cool-grey-600 dark:text-cool-grey-500">
+          Build duration
+        </Text>
+        <Duration beginTime={build.created_at} endTime={build.updated_at} />
+      </span>
+    </div>
+  ) : (
+    <Text>No component build found.</Text>
+  )
+}
+
+// load component config
+const LoadComponentConfig: FC<{
+  componentId: string
+  buildId: string
+  orgId: string
+}> = async ({ componentId, buildId, orgId }) => {
+  const build = await getComponentBuild({ buildId, orgId })
+  const componentConfig = await getComponentConfig({
+    componentId,
+    componentConfigId: build.component_config_connection_id,
+    orgId,
+  }).catch(console.error)
+  return componentConfig ? (
+    <ComponentConfiguration config={componentConfig} />
+  ) : (
+    <Text>No component config found.</Text>
+  )
+}
+
+// load intermediate data
+const LoadIntermediateData: FC<{
+  deployId: string
+  install: TInstall
+  orgId: string
+}> = async ({ deployId, install, orgId }) => {
+  const deployPlan = await getInstallDeployPlan({
+    deployId,
+    installId: install.id,
+    orgId,
+  }).catch(console.error)
+
+  return deployPlan &&
+    (deployPlan as TInstallDeployPlan)?.actual?.waypoint_plan?.variables
+      ?.intermediate_data?.nuon ? (
+    <Section
+      childrenClassName="flex flex-col gap-8"
+      heading="Rendered intermediate data"
+      className="flex-initial"
+    >
+      <InstallDeployIntermediateData
+        install={install}
+        data={
+          (deployPlan as TInstallDeployPlan)?.actual?.waypoint_plan?.variables
+            ?.intermediate_data
+        }
+      />
+    </Section>
+  ) : null
+}
