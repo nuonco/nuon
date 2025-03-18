436bc1c26560ee67ea3e737817f5475145f7a49f
Nat Hamilton <info@nathamilton.com>
2025-03-18 10:24:07 -0400

    refactor(ui): page loading improvements (#8341)
    * fix(ui): tooltip z-index again
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * refactor(ui): click-to-copy button styles
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * fix(ui): correct component ids in install comp table
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * refactor(ui): streaming to load deploy page
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * refactor(ui): parallel fetch on build page
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * refactor(ui): load builds in client for deploy build dropdown
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * refactor(ui): use streaming to load install component page
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * chore(ui): remove old install dropdown code
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/components/InstallDeployComponentsButton.tsx
+++ services/dashboard-ui/src/components/InstallDeployComponentsButton.tsx
@@ -5,66 +5,19 @@ import { CloudCheck, CloudArrowUp } from '@phosphor-icons/react'
 import { Button } from '@/components/Button'
 import { Dropdown } from '@/components/Dropdown'
 import { RadioInput } from '@/components/Input'
-import { SpinnerSVG } from '@/components/Loading'
+import { SpinnerSVG, Loading } from '@/components/Loading'
 import { Modal } from '@/components/Modal'
+import { Notice } from '@/components/Notice'
 import { Time } from '@/components/Time'
 import { Text } from '@/components/Typography'
-import {
-  deployComponents,
-  deployComponentBuild,
-} from '@/components/install-actions'
+import { deployComponentBuild } from '@/components/install-actions'
 import type { TBuild } from '@/types'
 
-export const InstallDeployComponentButton: FC<{
-  installId: string
-  orgId: string
-  onComplete: () => void
-}> = ({ installId, orgId, ...props }) => {
-  const [isLoading, setIsLoading] = useState(false)
-  const [isKickedOff, setIsKickedOff] = useState(false)
-
-  useEffect(() => {
-    const kickoff = () => setIsKickedOff(false)
-
-    if (isKickedOff) {
-      const displayNotice = setTimeout(kickoff, 15000)
-
-      return () => {
-        clearTimeout(displayNotice)
-      }
-    }
-  }, [isKickedOff])
-
-  return (
-    <Button
-      className="text-base flex items-center gap-1"
-      onClick={() => {
-        setIsLoading(true)
-        deployComponents({ installId, orgId }).then(() => {
-          setIsLoading(false)
-          setIsKickedOff(true)
-          props.onComplete()
-        })
-      }}
-      variant="primary"
-    >
-      {isKickedOff ? (
-        <CloudCheck size="18" />
-      ) : isLoading ? (
-        <SpinnerSVG />
-      ) : (
-        <CloudArrowUp size="18" />
-      )}{' '}
-      Deploy components
-    </Button>
-  )
-}
-
 export const InstallDeployLatestBuildButton: FC<{
-  builds: Array<TBuild>
+  componentId: string
   installId: string
   orgId: string
-}> = ({ builds, installId, orgId }) => {
+}> = ({ componentId, installId, orgId }) => {
   const [isDeploymentOpen, setIsDeploymentOpen] = useState(false)
   const [buildId, setBuildId] = useState<string>()
   const [isLoading, setIsLoading] = useState(false)
@@ -140,33 +93,11 @@ export const InstallDeployLatestBuildButton: FC<{
         wrapperClassName="z-20"
       >
         <div className="min-w-[180px] rounded-md overflow-hidden">
-          <div className="w-full max-h-[250px] overflow-y-auto">
-            <Text className="px-3 pt-2 pb-1 text-cool-grey-600 dark:text-cool-grey-400">
-              Recent builds
-            </Text>
-            {builds.map((build) =>
-              build.status === 'active' ? (
-                <RadioInput
-                  key={build?.id}
-                  name="build-id"
-                  value={build?.id}
-                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                    setBuildId(e.target?.value)
-                  }}
-                  labelText={
-                    <span>
-                      <Text variant="med-12">{build?.id}</Text>
-                      <Time
-                        className="!font-normal"
-                        variant="reg-12"
-                        time={build.created_at}
-                      />
-                    </span>
-                  }
-                />
-              ) : null
-            )}
-          </div>
+          <BuildOptions
+            componentId={componentId}
+            orgId={orgId}
+            setBuildId={setBuildId}
+          />
           <hr />
           <Button
             disabled={!buildId}
@@ -181,5 +112,72 @@ export const InstallDeployLatestBuildButton: FC<{
         </div>
       </Dropdown>
     </>
+  )
+}
+
+const BuildOptions: FC<{
+  componentId: string
+  orgId: string
+  setBuildId: (id: string) => void
+}> = ({ componentId, orgId, ...props }) => {
+  const [builds, setBuilds] = useState<Array<TBuild>>()
+  const [isLoading, setIsLoading] = useState(true)
+  const [error, setError] = useState<string>()
+
+  useEffect(() => {
+    fetch(`/api/${orgId}/components/${componentId}/builds`)
+      .then((res) =>
+        res.json().then((blds) => {
+          setBuilds(
+            (blds as Array<TBuild>).filter((b) => b.status === 'active')
+          )
+          setIsLoading(false)
+        })
+      )
+      .catch((err) => {
+        console.error(err?.message)
+        setIsLoading(false)
+        setError('Unable to load component builds')
+      })
+  }, [])
+
+  return (
+    <div className="w-full max-h-[250px] overflow-y-auto">
+      <Text className="px-3 pt-2 pb-1 text-cool-grey-600 dark:text-cool-grey-400">
+        Recent builds
+      </Text>
+      {error ? (
+        <div className="p-3">
+          <Notice>{error}</Notice>
+        </div>
+      ) : isLoading ? (
+        <div className="p-3 text-sm">
+          <Loading loadingText="Loading builds..." />
+        </div>
+      ) : builds && builds?.length ? (
+        builds.map((build) => (
+          <RadioInput
+            key={build?.id}
+            name="build-id"
+            value={build?.id}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              props.setBuildId(e.target?.value)
+            }}
+            labelText={
+              <span>
+                <Text variant="med-12">{build?.id}</Text>
+                <Time
+                  className="!font-normal"
+                  variant="reg-12"
+                  time={build.created_at}
+                />
+              </span>
+            }
+          />
+        ))
+      ) : (
+        <Text className="text-sm px-3 pb-2">No active builds found</Text>
+      )}
+    </div>
   )
 }
