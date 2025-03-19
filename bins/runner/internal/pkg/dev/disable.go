d866fedf362147776364c17b2751110d896cd8d9
Jon Morehouse <MorehouseJ09@gmail.com>
2025-03-18 23:31:57 -0700

    fix: disable runner type in local dev (#8358)
    * fix: disable does not work locally
    
    * fix: dev disabling one runner type

--- bins/runner/internal/pkg/dev/disable.go
+++ bins/runner/internal/pkg/dev/disable.go
@@ -1,9 +1,11 @@
 package dev
 
-import "os"
+import (
+	"os"
+)
 
 func (d *devver) Disabled() bool {
-	switch d.runnerType {
+	switch d.watchRunnerType {
 	case "org":
 		return os.Getenv("DISABLE_ORG_RUNNER") == "true"
 	case "install":
