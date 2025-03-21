22006df3b6dc9b6e94e5d552b9bcf202598d47ae
Jon Morehouse <MorehouseJ09@gmail.com>
2025-03-20 20:19:42 -0700

    chore: formalize forceful/graceful shutdowns (#8375)
    * chore: consolidate shut down into signals
    
    * rename to graceful shut down
    
    * chore: update
    
    * update
    
    * fixing up the shutdown behavior
    
    * updates
    
    * feat(ui): graceful and force shutdown btns in admin controls
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * update
    
    * fix: graceful shutdown created by
    
    * feat: update nuon-runner-go
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    Co-authored-by: Nat Hamilton <info@nathamilton.com>

--- bins/runner/internal/pkg/jobloop/sandbox.go
+++ bins/runner/internal/pkg/jobloop/sandbox.go
@@ -9,6 +9,10 @@ var ignoreSandboxMode []models.AppRunnerJobType = []models.AppRunnerJobType{
 }
 
 func (j *jobLoop) isSandbox(job *models.AppRunnerJob) bool {
+	if job.Type == models.AppRunnerJobTypeShutDashDown {
+		return false
+	}
+
 	if job.Type == models.AppRunnerJobTypeActionsDashWorkflow {
 		return false
 	}
---
+++ services/ctl-api/docs/runner/descriptions/update_runner_job.md
@@ -0,0 +1,3 @@
+Update a runner job, and mark it's status.
+
+This is only used for shutdown jobs to mark that a forced shutdown job was completed.
