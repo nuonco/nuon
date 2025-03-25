a818f1060acc540ced8e1f0995d68b74bd80aae4
Nat Hamilton <info@nathamilton.com>
2025-03-25 12:32:23 -0400

    refactor(ui): use params for next route handlers  (#8419)
    refactor(ui): use next params for proxy api
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/app/api/[org-id]/apps/[app-id]/sandbox-configs/[config-id]/route.ts
+++ services/dashboard-ui/src/app/api/[org-id]/apps/[app-id]/sandbox-configs/[config-id]/route.ts
@@ -1,22 +1,30 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { withApiAuthRequired } from '@auth0/nextjs-auth0'
 import { API_URL, getFetchOpts } from '@/utils'
+import { TRouteRes } from '@/app/api/[org-id]/types'
 
-export const GET = withApiAuthRequired(async (req: NextRequest) => {
-  const [orgId, _, appId, __, configId] = req.url.split('/').slice(4, 9)
+export const GET = withApiAuthRequired(
+  async (
+    req: NextRequest,
+    { params }: TRouteRes<'org-id' | 'app-id' | 'config-id'>
+  ) => {
+    const orgId = params?.['org-id']
+    const appId = params?.['app-id']
+    const configId = params?.['config-id']
 
-  return fetch(
-    `${API_URL}/v1/apps/${appId}/sandbox-configs`,
-    await getFetchOpts(orgId, 10000)
-  ).then((res) => {
-    return res.json().then((sandboxs) => {
-      const sandbox =
-        configId === 'latest'
-          ? sandboxs[0]
-          : sandboxs?.find((sbx) => sbx?.id === configId)
-      return NextResponse.json(sandbox, {
-        status: 200,
+    return fetch(
+      `${API_URL}/v1/apps/${appId}/sandbox-configs`,
+      await getFetchOpts(orgId, 10000)
+    ).then((res) => {
+      return res.json().then((sandboxs) => {
+        const sandbox =
+          configId === 'latest'
+            ? sandboxs[0]
+            : sandboxs?.find((sbx) => sbx?.id === configId)
+        return NextResponse.json(sandbox, {
+          status: 200,
+        })
       })
     })
-  })
-})
+  }
+)
