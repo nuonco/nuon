df4ef40eb2c0e84d18f93ae6683bdb3f5ba9d0cd
Nat Hamilton <info@nathamilton.com>
2025-03-25 12:32:23 -0400

    refactor(ui): use params for next route handlers  (#8419)
    refactor(ui): use next params for proxy api
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/app/api/[org-id]/installs/[install-id]/events/route.ts
+++ services/dashboard-ui/src/app/api/[org-id]/installs/[install-id]/events/route.ts
@@ -1,16 +1,20 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { withApiAuthRequired } from '@auth0/nextjs-auth0'
 import { getInstallEvents } from '@/lib'
+import { TRouteRes } from '@/app/api/[org-id]/types'
 
-export const GET = withApiAuthRequired(async (req: NextRequest) => {
-  const [orgId, _, installId] = req.url.split('/').slice(4, 7)
+export const GET = withApiAuthRequired(
+  async (req: NextRequest, { params }: TRouteRes<'org-id' | 'install-id'>) => {
+    const orgId = params?.['org-id']
+    const installId = params?.['install-id']
 
-  let events = []
-  try {
-    events = await getInstallEvents({ orgId, installId })
-  } catch (error) {
-    console.error(error)
+    let events = []
+    try {
+      events = await getInstallEvents({ orgId, installId })
+    } catch (error) {
+      console.error(error)
+    }
+
+    return NextResponse.json(events)
   }
-
-  return NextResponse.json(events)
-})
+)
