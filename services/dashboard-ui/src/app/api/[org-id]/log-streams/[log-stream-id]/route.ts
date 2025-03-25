a17e5abfc440183d906cfd7cd6c33789ae027a67
Nat Hamilton <info@nathamilton.com>
2025-03-25 12:32:23 -0400

    refactor(ui): use params for next route handlers  (#8419)
    refactor(ui): use next params for proxy api
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/app/api/[org-id]/log-streams/[log-stream-id]/route.ts
+++ services/dashboard-ui/src/app/api/[org-id]/log-streams/[log-stream-id]/route.ts
@@ -1,16 +1,23 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { withApiAuthRequired } from '@auth0/nextjs-auth0'
 import { getLogStream } from '@/lib'
+import { TRouteRes } from '@/app/api/[org-id]/types'
 
-export const GET = withApiAuthRequired(async (req: NextRequest) => {
-  const [orgId, _, logStreamId] = req.url.split('/').slice(4, 7)
+export const GET = withApiAuthRequired(
+  async (
+    req: NextRequest,
+    { params }: TRouteRes<'org-id' | 'log-stream-id'>
+  ) => {
+    const orgId = params?.['org-id']
+    const logStreamId = params?.['log-stream-id']
 
-  let logStream = {}
-  try {
-    logStream = await getLogStream({ orgId, logStreamId })
-  } catch (error) {
-    console.error(error)
+    let logStream = {}
+    try {
+      logStream = await getLogStream({ orgId, logStreamId })
+    } catch (error) {
+      console.error(error)
+    }
+
+    return NextResponse.json(logStream)
   }
-
-  return NextResponse.json(logStream)
-})
+)
