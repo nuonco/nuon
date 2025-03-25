ceb00cd9ad71c777b2c88465cfde88408c9dc607
Nat Hamilton <info@nathamilton.com>
2025-03-25 10:45:55 -0400

    refactor(ui): install mgmt dropdown (#8416)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/components/Installs/ManagementDropdown.tsx
+++ services/dashboard-ui/src/components/Installs/ManagementDropdown.tsx
@@ -1,4 +1,5 @@
 import React, { type FC } from 'react'
+import { Gear } from '@phosphor-icons/react/dist/ssr'
 import { BreakGlassLink } from './BreakGlassLink'
 import { DeployComponentsModal } from './DeployComponentsModal'
 import { EditModal } from './EditModal'
@@ -25,16 +26,27 @@ export const InstallManagementDropdown: FC<IInstallManagementDropdown> = ({
       className="text-sm !font-medium !p-2 h-[32px]"
       alignment="right"
       id="mgmt-install"
-      text="Admin"
+      text={
+        <>
+          <Gear />
+          Manage
+        </>
+      }
       isDownIcon
       wrapperClassName="z-20"
     >
       <div className="min-w-[256px] rounded-md overflow-hidden p-2 flex flex-col gap-1">
         <Text className="px-2 pt-2 pb-1 text-cool-grey-600 dark:text-cool-grey-400">
-          Controls
+          Settings
         </Text>
 
         <EditModal install={install} orgId={orgId} />
+        <BreakGlassLink installId={install.id} />
+
+        <hr className="my-2" />
+        <Text className="px-2 pt-2 pb-1 text-cool-grey-600 dark:text-cool-grey-400">
+          Controls
+        </Text>
         <ReprovisionModal installId={install.id} orgId={orgId} />
         {hasInstallComponents ? (
           <DeployComponentsModal installId={install.id} orgId={orgId} />
@@ -45,7 +57,9 @@ export const InstallManagementDropdown: FC<IInstallManagementDropdown> = ({
 
         <>
           <hr className="my-2" />
-          <BreakGlassLink installId={install.id} />
+          <Text className="px-2 pt-2 pb-1 text-cool-grey-600 dark:text-cool-grey-400">
+            Remove
+          </Text>
           <ForgetModal install={install} orgId={orgId} />
         </>
       </div>
