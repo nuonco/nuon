7feb4425954ad1412eb9cc3437f3a72f6ef82a01
Nat Hamilton <info@nathamilton.com>
2025-03-27 15:42:00 -0400

    feat(ui): install components delete (#8458)
    * refactor(ui): org admin has graceful and force runner shutdowns
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * refactor(ui): handles offline statue
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * refactor(ui): updated install page header
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat(ui): added install component delete flows
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * chore(ui): rename from teardown to delete components
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

---
+++ services/dashboard-ui/src/components/Installs/ManageComponentsDropdown.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import { useParams } from 'next/navigation'
+import React, { type FC } from 'react'
+import { SlidersHorizontal } from '@phosphor-icons/react/dist/ssr'
+import { DeployComponentsModal } from './DeployComponentsModal'
+import { DeleteComponentsModal } from './DeleteComponentsModal'
+import { Dropdown } from '@/components/Dropdown'
+import { useOrg } from '@/components/Orgs'
+
+interface IInstallComponentsManagementDropdown {}
+
+export const InstallComponentsManagementDropdown: FC<
+  IInstallComponentsManagementDropdown
+> = ({}) => {
+  const params =
+    useParams<Record<'org-id' | 'install-id' | 'component-id', string>>()
+  const { org } = useOrg()
+  const installId = params['install-id']
+
+  return (
+    <Dropdown
+      className="text-sm !font-medium !p-2 h-[32px]"
+      alignment="right"
+      id="mgmt-install"
+      text={
+        <>
+          <SlidersHorizontal size="16" />
+          Manage
+        </>
+      }
+      isDownIcon
+      wrapperClassName="z-18"
+    >
+      <div className="min-w-[256px] rounded-md overflow-hidden p-2 flex flex-col gap-1">
+        <DeployComponentsModal installId={installId} orgId={org?.id} />
+
+        {org?.features?.['install-delete-components'] ? (
+          <DeleteComponentsModal installId={installId} orgId={org?.id} />
+        ) : null}
+      </div>
+    </Dropdown>
+  )
+}
