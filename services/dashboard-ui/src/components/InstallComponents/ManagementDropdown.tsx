7372b07bee5820feeaa425147ea3c985fc6fb614
Nat Hamilton <info@nathamilton.com>
2025-03-28 09:07:07 -0400

    feat(ui): delete components flow (#8465)
    * refactor(ui): org admin has graceful and force runner shutdowns
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * refactor(ui): handles offline statue
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * refactor(ui): updated install page header
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat(ui): added install component delete flows
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * chore(ui): rename from teardown to delete components
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * refactor(ui): removed install table polling
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

---
+++ services/dashboard-ui/src/components/InstallComponents/ManagementDropdown.tsx
@@ -0,0 +1,49 @@
+'use client'
+
+import React, { type FC } from 'react'
+import { GearFine } from '@phosphor-icons/react'
+import { Dropdown } from '@/components/Dropdown'
+import { useOrg } from '@/components/Orgs'
+import { Text } from '@/components/Typography'
+import type { TComponent } from '@/types'
+import { DeleteComponentModal } from './DeleteComponentModal'
+import { InstallDeployBuildModal } from './DeployBuildModal'
+
+interface IInstallComponentManagementDropdown {
+  component: TComponent
+}
+
+export const InstallComponentManagementDropdown: FC<
+  IInstallComponentManagementDropdown
+> = ({ component }) => {
+  const { org } = useOrg()
+  return org?.features?.['install-delete-components'] ? (
+    <Dropdown
+      className="text-sm !font-medium !p-2 h-[32px]"
+      alignment="right"
+      id="mgmt-install"
+      text={
+        <>
+          <GearFine />
+          Manage
+        </>
+      }
+      isDownIcon
+      wrapperClassName="z-20"
+    >
+      <div className="min-w-[256px] rounded-md overflow-hidden p-2 flex flex-col gap-1">
+        <Text className="px-2 pt-2 pb-1 text-cool-grey-600 dark:text-cool-grey-400">
+          Controls
+        </Text>
+        <InstallDeployBuildModal />
+        <>
+          <hr className="my-2" />
+          <Text className="px-2 pt-2 pb-1 text-cool-grey-600 dark:text-cool-grey-400">
+            Remove
+          </Text>
+          <DeleteComponentModal component={component} />
+        </>
+      </div>
+    </Dropdown>
+  ) : null
+}
