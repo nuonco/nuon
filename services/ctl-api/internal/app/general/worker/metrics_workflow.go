37d36b14f5978e4b7d28b780103e2dbd116a3722
sam boyer <sam@nuon.co>
2025-03-28 10:50:41 -0400

    chore: Switch to metric for deadman snitch (#8466)

--- services/ctl-api/internal/app/general/worker/metrics_workflow.go
+++ services/ctl-api/internal/app/general/worker/metrics_workflow.go
@@ -37,7 +37,7 @@ func (w *Workflows) Metrics(ctx workflow.Context) error {
 		return err
 	}
 
-	l.Info("deadman.snitch", zap.String("status", "running"))
+	w.mw.Gauge(ctx, "deadman.snitch", 1.0, metrics.ToTags(map[string]string{"snitchfor": "general-eloop-metrics"})...)
 
 	methods := map[string]func(workflow.Context) error{
 		"psql_tables": func(ctx workflow.Context) error {
