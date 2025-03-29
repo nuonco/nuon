3c8173dfd4f7c51811c1636f542bbe2cbc34d16e
Jon Morehouse <MorehouseJ09@gmail.com>
2025-03-29 11:44:40 -0700

    chore: fix error for component builds (#8477)
    * fix: component build error should be a `stderr.ErrUser`
    
    * fix: bug where author name would not exist for bot user

--- services/ctl-api/internal/app/vcs/helpers/commit.go
+++ services/ctl-api/internal/app/vcs/helpers/commit.go
@@ -5,18 +5,26 @@ import (
 	"fmt"
 
 	"github.com/google/go-github/v50/github"
+
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/middlewares/stderr"
 )
 
 func (h *Helpers) GetVCSConfigLatestCommit(ctx context.Context, vcsCfg *app.ConnectedGithubVCSConfig) (*github.RepositoryCommit, error) {
 	client, err := h.GetVCSConnectionClient(ctx, &vcsCfg.VCSConnection)
 	if err != nil {
-		return nil, fmt.Errorf("unable to get vcs connection client: %w", err)
+		return nil, stderr.ErrUser{
+			Err:         err,
+			Description: "invalid VCS connection, unable to get access token",
+		}
 	}
 
 	commitResp, _, err := client.Repositories.GetCommit(ctx, vcsCfg.RepoOwner, vcsCfg.RepoName, vcsCfg.Branch, &github.ListOptions{})
 	if err != nil {
-		return nil, fmt.Errorf("unable to get latest commit: %w", err)
+		return nil, stderr.ErrUser{
+			Err:         fmt.Errorf("unable to get latest commit: %w", err),
+			Description: "unable to get error",
+		}
 	}
 
 	return commitResp, nil
