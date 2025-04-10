718566d499208b42c57aa28aa54de14c96c4829f
Dat Boi Diego <fidiego@users.noreply.github.com>
2025-04-10 13:42:02 -0500

    chore: infra/github: new aws eks sandbox (#8622)

--- infra/github/repos_sandboxes.tf
+++ infra/github/repos_sandboxes.tf
@@ -95,3 +95,17 @@ module "nuon-azure-aks-byopn-sandbox" {
     github = github.nuon
   }
 }
+
+module "nuon-aws-eks-sandbox-m1" {
+  source           = "./modules/repository"
+  name             = "aws-eks-sandbox"
+  description      = "AWS EKS sandbox for Nuon apps."
+  required_checks  = []
+  is_public        = true
+  owning_team_id   = github_team.nuon.id
+  owning_team_name = "nuonco/${github_team.nuon.name}"
+
+  providers = {
+    github = github.nuon
+  }
+}
