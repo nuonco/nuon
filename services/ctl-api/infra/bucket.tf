3d1a62d7b980b1a5d63b5a745c172c745f8e01ef
Jon Morehouse <MorehouseJ09@gmail.com>
2025-04-14 14:48:44 -0700

    feat: create templates bucket (#8642)
    feat: create bucket

---
+++ services/ctl-api/infra/bucket.tf
@@ -0,0 +1,103 @@
+locals {
+  org_id     = data.aws_organizations_organization.orgs.id
+  account_id = local.accounts["public"]
+  bucket_name = local.vars.install_template_bucket_name
+  public_prefixes = [
+    "templates/*",
+  ]
+}
+
+data "aws_iam_policy_document" "s3_bucket_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:ListBucket",
+    ]
+    resources = ["arn:aws:s3:::${local.bucket_name}", ]
+    principals {
+      type        = "AWS"
+      identifiers = ["*", ]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "aws:PrincipalOrgID"
+      values   = [local.org_id, ]
+    }
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:*Object",
+    ]
+    resources = ["arn:aws:s3:::${local.bucket_name}/*", ]
+    principals {
+      type        = "AWS"
+      identifiers = ["*", ]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "aws:PrincipalOrgID"
+      values   = [local.org_id]
+    }
+  }
+
+  // allow a few select public paths in the artifacts bucket
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:GetObject",
+    ]
+    resources = formatlist("arn:aws:s3:::${local.bucket_name}/%s", local.public_prefixes)
+    principals {
+      type        = "*"
+      identifiers = ["*", ]
+    }
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:ListBucket",
+    ]
+    resources = [
+      "arn:aws:s3:::${local.bucket_name}",
+    ]
+    principals {
+      type        = "*"
+      identifiers = ["*", ]
+    }
+    condition {
+      test     = "StringLike"
+      variable = "s3:prefix"
+      values   = local.public_prefixes
+    }
+  }
+}
+
+module "bucket" {
+  providers = {
+    aws = aws.public
+  }
+
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = ">= v3.2.4"
+
+  bucket = local.bucket_name
+  versioning = {
+    enabled = true
+  }
+
+  attach_deny_insecure_transport_policy = true
+  attach_require_latest_tls_policy      = true
+
+  attach_public_policy = false
+  block_public_acls    = false
+  block_public_policy  = false
+
+  control_object_ownership = true
+  object_ownership         = "BucketOwnerEnforced"
+
+  attach_policy = true
+  policy        = data.aws_iam_policy_document.s3_bucket_policy.json
+}
