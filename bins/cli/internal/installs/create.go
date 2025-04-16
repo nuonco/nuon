17e37758a9829e285c63f71f478370ad6a87ffec
Rob <alphavibe@gmail.com>
2025-04-17 00:26:21 +0200

    feat: cli install create opens dashboard install page (#8665)

--- bins/cli/internal/installs/create.go
+++ bins/cli/internal/installs/create.go
@@ -4,13 +4,12 @@ import (
 	"context"
 	"fmt"
 	"strings"
-	"time"
 
 	"github.com/nuonco/nuon-go/models"
+	"github.com/pkg/browser"
 
 	"github.com/powertoolsdev/mono/bins/cli/internal/lookup"
 	"github.com/powertoolsdev/mono/bins/cli/internal/ui"
-	"github.com/powertoolsdev/mono/pkg/errs"
 )
 
 const (
@@ -62,30 +61,13 @@ func (s *Service) Create(ctx context.Context, appID, name, region, arn string, i
 		return view.Fail(err)
 	}
 
-	for {
-		ins, err := s.api.GetInstall(ctx, install.ID)
-		switch {
-		case err != nil:
-			view.Fail(err)
-		case ins.SandboxStatus == statusAccessError:
-			return view.Fail(fmt.Errorf("failed to create install due to access error: %s", ins.StatusDescription))
-		case ins.SandboxStatus == statusError:
-			return view.Fail(fmt.Errorf("failed to create install: %s", ins.StatusDescription))
-		case ins.SandboxStatus == statusActive:
-			view.Success(fmt.Sprintf("successfully created install %s", ins.ID))
-			if !noSelect {
-				if err := s.setInstallID(ctx, ins.ID); err == nil {
-					s.printInstallSetMsg(name, ins.ID)
-				} else {
-					view.Fail(errs.NewUserFacing("failed to set install as current: %s", err))
-				}
-				return nil
-			}
-			return nil
-		default:
-			view.Update(fmt.Sprintf("%s install", ins.SandboxStatus))
-		}
-
-		time.Sleep(5 * time.Second)
+	cfg, err := s.api.GetCLIConfig(ctx)
+	if err != nil {
+		return ui.PrintError(fmt.Errorf("couldn't get cli config: %w", err))
 	}
+
+	url := fmt.Sprintf("%s/%s/installs/%s", cfg.DashboardURL, s.cfg.OrgID, install.ID)
+	browser.OpenURL(url)
+
+	return nil
 }
