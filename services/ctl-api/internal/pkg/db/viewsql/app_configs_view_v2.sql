6cccc816041c22b3af3369e9ecf94f3f44d73ce3
Dat Boi Diego <fidiego@users.noreply.github.com>
2025-04-17 01:38:16 -0500

    fix: ctl-api: force the recreation of some views (#8669)
    it seems some fields have been added to the tables backing the views and
    this is messing with the automatic sql-only re-creation. the short term
    solution is to promote this with `ApplyRecreate` once. we can (should)
    remove it after.

--- services/ctl-api/internal/pkg/db/viewsql/app_configs_view_v2.sql
+++ services/ctl-api/internal/pkg/db/viewsql/app_configs_view_v2.sql
@@ -1,3 +1,9 @@
-SELECT *, row_number() OVER (PARTITION BY app_id
-                               ORDER BY created_at) AS version
-FROM app_configs;
+SELECT
+    row_number() OVER (
+        PARTITION BY app_id
+        ORDER BY
+            created_at
+    ) AS version,
+    *
+FROM
+    app_configs;
