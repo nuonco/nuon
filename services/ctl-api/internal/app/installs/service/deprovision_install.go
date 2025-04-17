bd99f1f1bc253d7f26afa6f400adc588279ca4fc
Rob <alphavibe@gmail.com>
2025-04-17 10:49:54 -0700

    feat: return workflow id as a header (#8680)
    * feat: return workflow id as a header
    
    * feat: return workflow id as a header

--- services/ctl-api/internal/app/installs/service/deprovision_install.go
+++ services/ctl-api/internal/app/installs/service/deprovision_install.go
@@ -82,5 +82,7 @@ func (s *service) DeprovisionInstall(ctx *gin.Context) {
 		InstallWorkflowID: workflow.ID,
 	})
 
+	ctx.Header(app.HeaderInstallWorkflowID, workflow.ID)
+
 	ctx.JSON(http.StatusCreated, "ok")
 }
