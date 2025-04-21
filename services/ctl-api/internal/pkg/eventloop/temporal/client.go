841763c99d8208a95fd94e48d56e800350773d25
sam boyer <sam@nuon.co>
2025-04-21 13:33:22 -0400

    feat: Add intra-workflow sync and async signal communications (#8694)
    * Initial skeleton checkin
    
    * Tidy up signatures and names
    
    * Add send test and fix implementation
    
    * Port changes from test to actual eventloop
    
    * Fix method receivers
    
    * Add cross-ns test case
    
    * Convert to a matrix of tests
    
    * Separate await and async paths, and dataconverter
    
    * Slicify test matrix
    
    * Comment out failing test
    
    * Hacky fix to make wire errors sorta work
    
    * Fix DI params
    
    ---------
    
    Co-authored-by: nuonbot <bot@nuon.co>

--- services/ctl-api/internal/pkg/eventloop/temporal/client.go
+++ services/ctl-api/internal/pkg/eventloop/temporal/client.go
@@ -1,9 +1,12 @@
 package temporal
 
 import (
+	"github.com/go-playground/validator/v10"
 	"go.temporal.io/sdk/workflow"
 	"go.uber.org/fx"
 	"go.uber.org/zap"
+
+	"github.com/powertoolsdev/mono/pkg/metrics"
 
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/pkg/eventloop"
 )
@@ -11,22 +14,45 @@ import (
 type Params struct {
 	fx.In
 
-	L *zap.Logger
+	L  *zap.Logger
+	MW metrics.Writer
+	V  *validator.Validate
 }
 
 type Client interface {
+	// Send sends a signal to an event loop for asynchronous processing.
+	//
+	// If knowing when the signal is done processing is a requirement, use SendAndWait or SendAsync.
 	Send(ctx workflow.Context, id string, signal eventloop.Signal)
-	Cancel(ctx workflow.Context, namespace, id string)
+
+	// SendAsync is the same as Send, but it returns a future that allows you to decide when to
+	// wait for a result.
+	//
+	// Prefer Send for fire-and-forget use cases, as creating the future has some overhead.
+	//
+	// The value and error of the returned future will be the return type of the signal handler.
+	// For signal handlers with only an error return type, a successful return will be nil, nil.
+	SendAsync(ctx workflow.Context, id string, signal eventloop.Signal) (workflow.Future, error)
+
+	// SendAndWait sends a signal to an event loop, then waits for the signal to be processed
+	// before returning.
+	//
+	// It is the same as calling SendAsync and then immediately waiting for the future to complete.
+	SendAndWait(ctx workflow.Context, id string, signal eventloop.Signal) error
 }
 
 var _ Client = (*evClient)(nil)
 
 type evClient struct {
-	l *zap.Logger
+	l  *zap.Logger
+	mw metrics.Writer
+	v  *validator.Validate
 }
 
 func New(params Params) Client {
 	return &evClient{
-		l: params.L,
+		l:  params.L,
+		mw: params.MW,
+		v:  params.V,
 	}
 }
