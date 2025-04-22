ac32508bc4805894f83cb796e654115b06653946
Harsh Thakur <harsh@nuon.co>
2025-04-23 00:40:23 +0530

    feat: terraform http backend (#8659)
    * draft- terraform http backend
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    * move to separate files
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    * Fix TF input & output params along with passing the right stateID
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    * return terraform state data as json in get endpoint
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    * manage lock resource properly
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    * refactor data model to append state changes rather than upserts
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    * Fix reading install_id & handling empty id
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    * add tf workspace endpoints to runner api
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    * add org_id to terraform workspace
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    * create workspaces post install component & sandbox run
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    * fix swagger generation
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    * Add http backend in pkg/terraform/backend
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    ---------
    
    Signed-off-by: harsh <harsh@nuon.co>
    Co-authored-by: nuonbot <bot@nuon.co>

--- services/ctl-api/internal/app/runners/service/create_runner_job_execution.go
+++ services/ctl-api/internal/app/runners/service/create_runner_job_execution.go
@@ -14,23 +14,23 @@ import (
 
 type CreateRunnerJobExecutionRequest struct{}
 
-//	@ID						CreateRunnerJobExecution
-//	@Summary				create runner job execution
-//	@Description.markdown	create_runner_job_execution.md
-//	@Param					req				body	CreateRunnerJobExecutionRequest	true	"Input"
-//	@Param					runner_job_id	path	string							true	"runner job ID"
-//	@Tags					runners/runner
-//	@Accept					json
-//	@Produce				json
-//	@Security				APIKey
-//	@Security				OrgID
-//	@Failure				400	{object}	stderr.ErrResponse
-//	@Failure				401	{object}	stderr.ErrResponse
-//	@Failure				403	{object}	stderr.ErrResponse
-//	@Failure				404	{object}	stderr.ErrResponse
-//	@Failure				500	{object}	stderr.ErrResponse
-//	@Success				201	{object}	app.RunnerJobExecution
-//	@Router					/v1/runner-jobs/{runner_job_id}/executions [POST]
+// @ID						CreateRunnerJobExecution
+// @Summary				create runner job execution
+// @Description.markdown	create_runner_job_execution.md
+// @Param					req				body	CreateRunnerJobExecutionRequest	true	"Input"
+// @Param					runner_job_id	path	string							true	"runner job ID"
+// @Tags					runners/runner
+// @Accept					json
+// @Produce				json
+// @Security				APIKey
+// @Security				OrgID
+// @Failure				400	{object}	stderr.ErrResponse
+// @Failure				401	{object}	stderr.ErrResponse
+// @Failure				403	{object}	stderr.ErrResponse
+// @Failure				404	{object}	stderr.ErrResponse
+// @Failure				500	{object}	stderr.ErrResponse
+// @Success				201	{object}	app.RunnerJobExecution
+// @Router					/v1/runner-jobs/{runner_job_id}/executions [POST]
 func (s *service) CreateRunnerJobExecution(ctx *gin.Context) {
 	runnerJobID := ctx.Param("runner_job_id")
 
