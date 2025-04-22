c1300d4b05fd11bccc9067224289bbd544151351
Dat Boi Diego <fidiego@users.noreply.github.com>
2025-04-22 12:15:19 -0500

    chore: ctl-api: enable helm client in sync app runner (#8711)
    * chore: update nuon-go in go.mod
    
    * chore: ctl-api: enable helm client in sync app runner
    
    requires updated nuon-go

--- pkg/config/sync/runner_sync.go
+++ pkg/config/sync/runner_sync.go
@@ -10,10 +10,8 @@ func (s sync) syncAppRunner(ctx context.Context, resource string) error {
 	cfg, err := s.apiClient.CreateAppRunnerConfig(ctx, s.appID, &models.ServiceCreateAppRunnerConfigRequest{
 		AppConfigID: s.appConfigID,
 		EnvVars:     s.cfg.Runner.EnvVarMap,
-		// TODO: after this PR is merged, update nuon-go, then enable this again
-		// HelmDriver:  models.AppAppRunnerConfigHelmDriverType(s.cfg.Runner.HelmDriver),
-		// Type:    models.AppAppRunnerType(s.cfg.Runner.RunnerType),
-		Type: models.NewAppAppRunnerType(models.AppAppRunnerType(s.cfg.Runner.RunnerType)),
+		HelmDriver:  models.AppAppRunnerConfigHelmDriverType(s.cfg.Runner.HelmDriver),
+		Type:        models.NewAppAppRunnerType(models.AppAppRunnerType(s.cfg.Runner.RunnerType)),
 	})
 	if err != nil {
 		return SyncAPIErr{
