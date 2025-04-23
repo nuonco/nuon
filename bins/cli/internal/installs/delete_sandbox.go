c979f754b24fe819ffc95d7e7d6688916c931394
Rob <alphavibe@gmail.com>
2025-04-23 15:50:27 -0700

    feat: deprovision install sandbox (#8724)
    * feat: deprovision install sandbox
    
    * feat: update nuon-go to v0.54.0
    
    * feat: update

---
+++ bins/cli/internal/installs/delete_sandbox.go
@@ -0,0 +1,23 @@
+package installs
+
+import (
+	"context"
+
+	"github.com/powertoolsdev/mono/bins/cli/internal/lookup"
+	"github.com/powertoolsdev/mono/bins/cli/internal/ui"
+)
+
+func (s *Service) DeprovisionSandbox(ctx context.Context, installID string, asJSON bool) error {
+	installID, err := lookup.InstallID(ctx, s.api, installID)
+	if err != nil {
+		return ui.PrintError(err)
+	}
+
+	err = s.api.DeprovisionInstallSandbox(ctx, installID)
+	if err != nil {
+		return ui.PrintJSONError(err)
+	}
+
+	ui.PrintLn("successfully scheduled deprovision of install sandbox")
+	return nil
+}
