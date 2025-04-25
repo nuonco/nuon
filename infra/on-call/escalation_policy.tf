ee9d3207d65e42feb7d078faa1001e50942b19a5
Jordan Acosta <jordan@nuon.co>
2025-04-25 13:18:46 -0700

    feat(infra/on-call): alert Jon and Engineering at the same time (#8751)

--- infra/on-call/escalation_policy.tf
+++ infra/on-call/escalation_policy.tf
@@ -17,14 +17,6 @@ resource "pagerduty_escalation_policy" "support" {
       id   = pagerduty_user.jon.id
       type = "user_reference"
     }
-  }
-
-  rule {
-    escalation_delay_in_minutes = 30
-
-    escalation_rule_assignment_strategy {
-      type = "assign_to_everyone"
-    }
 
     target {
       id   = pagerduty_schedule.schedule.id
