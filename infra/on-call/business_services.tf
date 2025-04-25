c7d07040354abf74e76044a121466c68f40bad5b
Jordan Acosta <jordan@nuon.co>
2025-04-25 10:24:57 -0700

    fix(infra/on-call): use correct team ID for business services (#8748)

--- infra/on-call/business_services.tf
+++ infra/on-call/business_services.tf
@@ -1,28 +1,28 @@
 resource "pagerduty_business_service" "controlplane" {
   name = "Control Plane API"
-  team = "PG5IHQW"
+  team = pagerduty_team.engineering.id
 }
 
 
 resource "pagerduty_business_service" "vendor-dashboard" {
   name = "Vendor dashboard"
-  team = "PG5IHQW"
+  team = pagerduty_team.engineering.id
 }
 
 
 resource "pagerduty_business_service" "installers" {
   name = "Installers"
-  team = "PG5IHQW"
+  team = pagerduty_team.engineering.id
 }
 
 
 resource "pagerduty_business_service" "website" {
   name = "website"
-  team = "PG5IHQW"
+  team = pagerduty_team.engineering.id
 }
 
 
 resource "pagerduty_business_service" "runners" {
   name = "Runners"
-  team = "PG5IHQW"
+  team = pagerduty_team.engineering.id
 }
