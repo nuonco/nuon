7a4fd825d6d36b7b5dc5e7415977ffe34ca5243b
Dat Boi Diego <fidiego@users.noreply.github.com>
2025-04-25 00:55:27 -0500

    feat: ctl-api: add very important tag to runner sg (#8744)
    fix: ctl-api: add very important tag to runner sg

--- services/ctl-api/internal/pkg/cloudformation/resource_runner_security_group.go
+++ services/ctl-api/internal/pkg/cloudformation/resource_runner_security_group.go
@@ -3,9 +3,17 @@ package cloudformation
 import (
 	"github.com/awslabs/goformation/v7/cloudformation"
 	"github.com/awslabs/goformation/v7/cloudformation/ec2"
+
+	"github.com/awslabs/goformation/v7/cloudformation/tags"
 )
 
 func (a *Templates) getRunnerSecurityGroup(inp *TemplateInput, t tagBuilder) *ec2.SecurityGroup {
+	// NOTE: this tag is REQUIRED. the sandboxes use it to identify the runner group which
+	// needs to be added to the eks node sg additional rules. this is a VIP tag. w/out it the
+	// runner won't be allowed to apply heml/kubectl to the cluster.
+	tags := []tags.Tag{
+		{Key: "networking.nuon.co/domain", Value: "runner"},
+	}
 	return &ec2.SecurityGroup{
 		GroupDescription: "Egress security group for the runner - allow all outbound traffic",
 		VpcId:            ptr(cloudformation.GetAtt("VPC", "Outputs.VPC")),
@@ -17,6 +25,6 @@ func (a *Templates) getRunnerSecurityGroup(inp *TemplateInput, t tagBuilder) *ec
 				IpProtocol: "-1",
 			},
 		},
-		Tags: t.apply(nil, "egress-sg"),
+		Tags: t.apply(tags, "egress-sg"),
 	}
 }
