c5c6b47c65362ea7bb2380b288b096393577259d
Jon Morehouse <MorehouseJ09@gmail.com>
2025-04-27 22:51:13 -0700

    chore: fix up sandbox mode for local dev (#8763)
    * fix: various sandbox mode deploys
    
    * fix: reset namespaces
    
    * feat: force delete
    
    * fix: null string parsing, sandbox modes

--- pkg/plans/types/sandbox_run.go
+++ pkg/plans/types/sandbox_run.go
@@ -32,7 +32,7 @@ type SandboxRunPlan struct {
 	AppID       string `json:"app_id"`
 	AppConfigID string `json:"app_config_id"`
 
-	Vars             map[string]any           `json:"vars"`
+	Vars             map[string]any           `json:"vars" faker:"-"`
 	EnvVars          map[string]string        `json:"env_vars"`
 	VarsFiles        []string                 `json:"vars_files"`
 	GitSource        *GitSource               `json:"git_source"`
