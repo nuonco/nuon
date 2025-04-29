e89f5ac82f6e5e04e571ff76ff7c61bf959986ed
Nat Hamilton <info@nathamilton.com>
2025-04-29 10:10:34 -0400

    refactor(ui) edit install inputs & removed continue on error (#8789)
    * refactor(ui): use latest app input config when editing install inputs
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * refactor(ui): removed continue on error for non delete actions
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

---
+++ services/dashboard-ui/src/app/api/[org-id]/apps/[app-id]/input-configs/latest/route.ts
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { withApiAuthRequired } from '@auth0/nextjs-auth0'
+import { API_URL, getFetchOpts } from '@/utils'
+import { TRouteRes } from '@/app/api/[org-id]/types'
+
+export const GET = withApiAuthRequired(
+  async (
+    req: NextRequest,
+    { params }: TRouteRes<'org-id' | 'app-id' | 'input-id'>
+  ) => {
+    const orgId = params?.['org-id']
+    const appId = params?.['app-id']
+
+    return fetch(
+      `${API_URL}/v1/apps/${appId}/input-latest-config`,
+      await getFetchOpts(orgId, 10000)
+    ).then((res) => {
+      return res.json().then((input) => {
+        return NextResponse.json(input, {
+          status: 200,
+        })
+      })
+    })
+  }
+)
