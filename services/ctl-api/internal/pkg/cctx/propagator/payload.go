06c3367aa437e7a1b60f483e3288e4ce860ba2bc
Rob <alphavibe@gmail.com>
2025-04-28 21:14:04 -0700

    feat: workflowID references for ui links (#8773)
    * feat: workflowID references for ui links
    
    * feat: workflowID references for ui links
    
    * feat: workflowID references for ui links
    
    * feat: workflowID references for ui links
    
    * feat: workflowID references for ui links

--- services/ctl-api/internal/pkg/cctx/propagator/payload.go
+++ services/ctl-api/internal/pkg/cctx/propagator/payload.go
@@ -6,19 +6,22 @@ import (
 )
 
 type Payload struct {
-	OrgID     string         `json:"org_id"`
-	AccountID string         `json:"account_id"`
-	LogStream *app.LogStream `json:"log_stream"`
+	OrgID           string                      `json:"org_id"`
+	AccountID       string                      `json:"account_id"`
+	LogStream       *app.LogStream              `json:"log_stream,omitempty"`
+	InstallWorkflow *app.InstallWorkflowContext `json:"install_workflow,omitempty"`
 }
 
 func FetchPayload(ctx cctx.ValueContext) (*Payload, error) {
 	acctID, _ := cctx.AccountIDFromContext(ctx)
 	orgID, _ := cctx.OrgIDFromContext(ctx)
 	logStream, _ := cctx.GetLogStreamContext(ctx)
+	workflow, _ := cctx.GetInstallWorkflowContext(ctx)
 
 	return &Payload{
-		OrgID:     orgID,
-		AccountID: acctID,
-		LogStream: logStream,
+		OrgID:           orgID,
+		AccountID:       acctID,
+		LogStream:       logStream,
+		InstallWorkflow: workflow,
 	}, nil
 }
