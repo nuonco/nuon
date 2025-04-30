1a27646dcd50ad5e8cc70fc6b2fb45e7a6915bfe
Nat Hamilton <info@nathamilton.com>
2025-04-30 09:57:14 -0400

    feat(ui): add ability to cancel install workflows from UI (#8799)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

---
+++ services/dashboard-ui/src/components/InstallWorkflows/InstallWorkflowCancelModal.tsx
@@ -0,0 +1,140 @@
+'use client'
+
+import classNames from 'classnames'
+import { useParams } from 'next/navigation'
+import React, { type FC, useState } from 'react'
+import { createPortal } from 'react-dom'
+import { useUser } from '@auth0/nextjs-auth0/client'
+import { Check, XSquare } from '@phosphor-icons/react'
+import { Button } from '@/components/Button'
+import { SpinnerSVG } from '@/components/Loading'
+import { Modal } from '@/components/Modal'
+import { Notice } from '@/components/Notice'
+import { useOrg } from '@/components/Orgs'
+import { Text } from '@/components/Typography'
+import { cancelInstallWorkflow } from '@/components/workflow-actions'
+import type { TInstallWorkflow } from '@/types'
+import { trackEvent, removeSnakeCase } from '@/utils'
+
+interface IInstallWorkflowCancelModal {
+  installWorkflow: TInstallWorkflow
+}
+
+export const InstallWorkflowCancelModal: FC<IInstallWorkflowCancelModal> = ({
+  installWorkflow,
+}) => {
+  const { user } = useUser()
+  const { org } = useOrg()
+  const params =
+    useParams<Record<'org-id' | 'install-id' | 'workflow-id', string>>()
+  const orgId = params?.['org-id']
+  const installWorkflowId = params?.['workflow-id']
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+  const [isLoading, setIsLoading] = useState(false)
+  const [isKickedOff, setIsKickedOff] = useState(false)
+  const [hasBeenCanceled, setHasBeenCanceled] = useState(false)
+  const [error, setError] = useState<string>()
+
+  const workflowType = removeSnakeCase(installWorkflow?.type)
+
+  return (
+    <>
+      {isOpen
+        ? createPortal(
+            <Modal
+              className="!max-w-lg"
+              isOpen={isOpen}
+              heading={`Cancel ${workflowType}?`}
+              onClose={() => {
+                setIsOpen(false)
+              }}
+            >
+              <div className="flex flex-col gap-3 mb-6">
+                {error ? <Notice>{error}</Notice> : null}
+                <Text>
+                  Are you sure you want to cancel this {workflowType} workflow?
+                </Text>
+              </div>
+              <div className="flex gap-3 justify-end">
+                <Button
+                  onClick={() => {
+                    setIsOpen(false)
+                  }}
+                  className="text-base"
+                >
+                  Cancel
+                </Button>
+                <Button
+                  disabled={Boolean(error)}
+                  className="text-sm flex items-center gap-1"
+                  onClick={() => {
+                    setIsLoading(true)
+                    cancelInstallWorkflow({ orgId, installWorkflowId }).then(
+                      (res) => {
+                        if (res?.error) {
+                          trackEvent({
+                            event: 'install_workflow_cancel',
+                            status: 'error',
+                            user,
+                            props: {
+                              workflowType: installWorkflow?.type,
+                              orgId: org.id,
+                              installWorkflowId,
+                            },
+                          })
+                          console.error(res?.error)
+                          setIsLoading(false)
+                          setError(
+                            res?.error?.error ||
+                              'Error occured, please refresh page and try again.'
+                          )
+                        } else {
+                          trackEvent({
+                            event: 'install_workflow_cancel',
+                            status: 'ok',
+                            user,
+                            props: {
+                              workflowType: installWorkflow?.type,
+                              orgId: org.id,
+                              installWorkflowId,
+                            },
+                          })
+                          setIsLoading(false)
+                          setIsKickedOff(true)
+                          setIsOpen(false)
+                          setHasBeenCanceled(true)
+                        }
+                      }
+                    )
+                  }}
+                  variant="danger"
+                >
+                  {isKickedOff ? (
+                    <Check size="18" />
+                  ) : isLoading ? (
+                    <SpinnerSVG />
+                  ) : (
+                    <XSquare size="18" />
+                  )}{' '}
+                  Cancel {workflowType}
+                </Button>
+              </div>
+            </Modal>,
+            document.body
+          )
+        : null}
+      <Button
+        disabled={hasBeenCanceled}
+        className={classNames('text-sm !font-medium', {
+          'text-red-800 dark:text-red-500': !hasBeenCanceled,
+          'text-red-800/50 dark:text-red-500/50': hasBeenCanceled,
+        })}
+        onClick={() => {
+          setIsOpen(true)
+        }}
+      >
+        Cancel {workflowType}
+      </Button>
+    </>
+  )
+}
