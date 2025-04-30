fe6c99f8a7e3d88feacc9b09cabac0e3a3f95a8e
Rob <alphavibe@gmail.com>
2025-04-30 13:00:34 -0700

    feat: action manual trigger with workspace (#8795)
    feat: actions manual trigger with workspace id

--- services/ctl-api/internal/app/actions/service/create_install_action_workflow_run.go
+++ services/ctl-api/internal/app/actions/service/create_install_action_workflow_run.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 
@@ -9,7 +10,7 @@ import (
 	"github.com/pkg/errors"
 
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
-	installsignals "github.com/powertoolsdev/mono/services/ctl-api/internal/app/installs/signals"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app/installs/signals"
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/middlewares/stderr"
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/pkg/db/generics"
 )
@@ -27,29 +28,29 @@ func (c *CreateInstallActionWorkflowRunRequest) Validate(v *validator.Validate) 
 	return nil
 }
 
-//	@ID						CreateInstallActionWorkflowRun
-//	@Summary				create an action workflow run for an install
-//	@Description.markdown	create_install_action_workflow_run.md
-//	@Tags					actions
-//	@Accept					json
-//	@Param					install_id	path	string									true	"install ID"
-//	@Param					req			body	CreateInstallActionWorkflowRunRequest	true	"Input"
-//	@Produce				json
-//	@Security				APIKey
-//	@Security				OrgID
-//	@Failure				400	{object}	stderr.ErrResponse
-//	@Failure				401	{object}	stderr.ErrResponse
-//	@Failure				403	{object}	stderr.ErrResponse
-//	@Failure				404	{object}	stderr.ErrResponse
-//	@Failure				500	{object}	stderr.ErrResponse
-//	@Success				201	{object}	app.InstallActionWorkflowRun
-//	@Router					/v1/installs/{install_id}/action-workflows/runs [post]
+// @ID						CreateInstallActionWorkflowRun
+// @Summary				create an action workflow run for an install
+// @Description.markdown	create_install_action_workflow_run.md
+// @Tags					actions
+// @Accept					json
+// @Param					install_id	path	string									true	"install ID"
+// @Param					req			body	CreateInstallActionWorkflowRunRequest	true	"Input"
+// @Produce				json
+// @Security				APIKey
+// @Security				OrgID
+// @Failure				400	{object}	stderr.ErrResponse
+// @Failure				401	{object}	stderr.ErrResponse
+// @Failure				403	{object}	stderr.ErrResponse
+// @Failure				404	{object}	stderr.ErrResponse
+// @Failure				500	{object}	stderr.ErrResponse
+// @Success				201	{string}	ok
+// @Router				/v1/installs/{install_id}/action-workflows/runs [post]
 func (s *service) CreateInstallActionWorkflowRun(ctx *gin.Context) {
 	installID := ctx.Param("install_id")
 
 	var req CreateInstallActionWorkflowRunRequest
 	if err := ctx.BindJSON(&req); err != nil {
-		ctx.Error(fmt.Errorf("unable to parse request: %w", err))
+		ctx.Error(fmt.Errorf("unable toq parse request: %w", err))
 		return
 	}
 
@@ -72,54 +73,61 @@ func (s *service) CreateInstallActionWorkflowRun(ctx *gin.Context) {
 		return
 	}
 
-	//
 	installActionWorkflow, err := s.getInstallActionWorkflow(ctx, installID, awc.ActionWorkflowID)
 	if err != nil {
 		ctx.Error(errors.Wrap(err, "unable to get install action workflow"))
 		return
 	}
 
-	run, err := s.createActionWorkflowRun(ctx, installID, installActionWorkflow.ID, awc, req.RunEnvVars)
+	prependRunEnvVars := PrependRunEnvPrefix(req.RunEnvVars)
+
+	workflow, err := s.CreateInstallWorkflow(ctx,
+		installActionWorkflow.InstallID,
+		app.InstallWorkflowTypeActionWorkflowRun,
+		prependRunEnvVars,
+		app.StepErrorBehaviorAbort,
+	)
 	if err != nil {
-		ctx.Error(fmt.Errorf("unable to create app: %w", err))
+		ctx.Error(err)
 		return
 	}
 
-	s.evClient.Send(ctx, installID, &installsignals.Signal{
-		ActionWorkflowRunID: run.ID,
-		Type:                installsignals.OperationActionWorkflowRun,
+	s.evClient.Send(ctx, installActionWorkflow.InstallID, &signals.Signal{
+		Type:              signals.OperationExecuteWorkflow,
+		InstallWorkflowID: workflow.ID,
 	})
 
-	ctx.JSON(http.StatusCreated, run)
+	ctx.Header(app.HeaderInstallWorkflowID, workflow.ID)
+
+	ctx.JSON(http.StatusOK, "ok")
 }
 
-func (s *service) createActionWorkflowRun(ctx *gin.Context, installID, installActionWorkflowID string, cfg *app.ActionWorkflowConfig, runEnvVars map[string]string) (*app.InstallActionWorkflowRun, error) {
-	steps := make([]app.InstallActionWorkflowRunStep, 0)
-	for _, step := range cfg.Steps {
-		steps = append(steps, app.InstallActionWorkflowRunStep{
-			Status: app.InstallActionWorkflowRunStepStatusPending,
-			StepID: step.ID,
-		})
+// PrependRunEnvPrefix modifies the keys in the provided RunEnvVars map
+// by prepending "RUNENV_" to each key.
+func PrependRunEnvPrefix(runEnvVars map[string]string) map[string]string {
+	result := make(map[string]string, len(runEnvVars))
+
+	for key, value := range runEnvVars {
+		newKey := "RUNENV_" + key
+		result[newKey] = value
 	}
 
-	trigger := app.InstallActionWorkflowManualTrigger{
-		InstallActionWorkflowRun: app.InstallActionWorkflowRun{
-			InstallID:               installID,
-			InstallActionWorkflowID: installActionWorkflowID,
-			ActionWorkflowConfigID:  cfg.ID,
-			TriggerType:             app.ActionWorkflowTriggerTypeManual,
-			Status:                  app.InstallActionRunStatusQueued,
-			StatusDescription:       "Queued",
-			Steps:                   steps,
-			RunEnvVars:              generics.ToHstore(runEnvVars),
-		},
+	return result
+}
+
+func (s *service) CreateInstallWorkflow(ctx context.Context, installID string, workflowType app.InstallWorkflowType, metadata map[string]string, errBehavior app.StepErrorBehavior) (*app.InstallWorkflow, error) {
+	installWorkflow := app.InstallWorkflow{
+		Type:              workflowType,
+		InstallID:         installID,
+		Metadata:          generics.ToHstore(metadata),
+		Status:            app.NewCompositeStatus(ctx, app.StatusPending),
+		StepErrorBehavior: errBehavior,
 	}
 
-	res := s.db.WithContext(ctx).
-		Create(&trigger)
+	res := s.db.WithContext(ctx).Create(&installWorkflow)
 	if res.Error != nil {
-		return nil, fmt.Errorf("unable to create action workflow: %w", res.Error)
+		return nil, errors.Wrap(res.Error, "unable to create install workflow")
 	}
 
-	return &trigger.InstallActionWorkflowRun, nil
+	return &installWorkflow, nil
 }
