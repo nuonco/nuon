56409cbf94274899c376372e020297e5fae5e0fb
Jordan Acosta <jordan@nuon.co>
2025-04-30 10:27:06 -0700

    feat(services/ctl-api): install runner tab updates (#8782)
    * feat(services/dashboard-ui): RunnerMeta component
    
    Since we're showing more than just runner heartbeat info, and we want to
    reduce the number of concepts the user needs to understand, this commit
    replaces the RunnerHeartbeat component with a RunnerMeta component.
    
    The main differences are:
    - "Alive time" is replaced with "Started at"
    - "Last hearbeat seen" is removed. We just want the status.
    - "Platform" is added.
    - Runner name and ID are removed.
    
    * feat(services/dashboard-ui): update install runner page
    
    - Use the RunnerMeta component
    - Remove "Recent job".
    - "Health status" replaced with just "Health"
    - "Job run history" replaced with "Completed jobs"
    
    * feat(services/dashboard-ui): org runner page updates
    
    - Use the RunnerMeta component
    - Remove "Recent job".
    - "Health status" replaced with just "Health"
    - "Job run history" replaced with "Completed jobs"
    
    * refactor(services/dashboard-ui): move data fetching into RunnerMeta
    
    ---------
    
    Co-authored-by: nuonbot <bot@nuon.co>

--- services/dashboard-ui/src/components/Runners/index.ts
+++ services/dashboard-ui/src/components/Runners/index.ts
@@ -3,4 +3,5 @@ export * from './RunnerHeartbeat'
 export * from './RunnerPastJobs'
 export * from './RunnerRecentJob'
 export * from './RunnerUpcomingJobs'
+export * from './RunnerMeta'
 // cancel runner job button
