791c1366c4c681665825dab0aa12fcbf31af98ea
Rob <alphavibe@gmail.com>
2025-05-02 14:35:19 -0700

    feat: migrate leftover endpoints to workflows (#8824)

--- services/ctl-api/internal/app/installs/service/delete_install.go
+++ services/ctl-api/internal/app/installs/service/delete_install.go
@@ -1,71 +1,73 @@
 package service
 
 import (
-	"context"
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"gorm.io/gorm"
 
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/app/installs/signals"
 )
 
-//	@ID						DeleteInstall
-//	@Summary				delete an install
-//	@Description.markdown	delete_install.md
-//	@Param					install_id	path	string	true	"install ID"
-//	@Tags					installs
-//	@Accept					json
-//	@Produce				json
-//	@Security				APIKey
-//	@Security				OrgID
-//	@Failure				400	{object}	stderr.ErrResponse
-//	@Failure				401	{object}	stderr.ErrResponse
-//	@Failure				403	{object}	stderr.ErrResponse
-//	@Failure				404	{object}	stderr.ErrResponse
-//	@Failure				500	{object}	stderr.ErrResponse
-//	@Success				200	{boolean}	true
-//	@Router					/v1/installs/{install_id} [DELETE]
+// DEPRECATED: This endpoint is deprecated and will be removed in a future release.
+
+// @ID						DeleteInstall
+// @Summary				delete an install
+// @Description.markdown	delete_install.md
+// @Param					install_id	path	string	true	"install ID"
+// @Tags					installs
+// @Accept					json
+// @Produce				json
+// @Security				APIKey
+// @Security				OrgID
+// @Failure				400	{object}	stderr.ErrResponse
+// @Failure				401	{object}	stderr.ErrResponse
+// @Failure				403	{object}	stderr.ErrResponse
+// @Failure				404	{object}	stderr.ErrResponse
+// @Failure				500	{object}	stderr.ErrResponse
+// @Success				200	{boolean}	true
+// @Router					/v1/installs/{install_id} [DELETE]
 func (s *service) DeleteInstall(ctx *gin.Context) {
 	installID := ctx.Param("install_id")
-
-	err := s.deleteInstall(ctx, installID)
+	install, err := s.getInstall(ctx, installID)
 	if err != nil {
 		ctx.Error(err)
 		return
 	}
 
-	s.evClient.Send(ctx, installID, &signals.Signal{
-		Type: signals.OperationTeardownComponents,
+	// TODO(jm): remove this once the legacy install flow is deprecated
+	enabled, err := s.featuresClient.FeatureEnabled(ctx, app.OrgFeatureIndependentRunner)
+	if err != nil {
+		ctx.Error(err)
+		return
+	}
+	if !enabled {
+		s.evClient.Send(ctx, install.ID, &signals.Signal{
+			Type: signals.OperationDeprovision,
+		})
+		ctx.JSON(http.StatusOK, true)
+		return
+	}
+
+	workflow, err := s.helpers.CreateInstallWorkflow(ctx,
+		install.ID,
+		app.InstallWorkflowTypeDeprovision,
+		map[string]string{},
+		app.StepErrorBehaviorAbort)
+	if err != nil {
+		ctx.Error(err)
+		return
+	}
+	s.evClient.Send(ctx, install.ID, &signals.Signal{
+		Type:              signals.OperationExecuteWorkflow,
+		InstallWorkflowID: workflow.ID,
 	})
-	s.evClient.Send(ctx, installID, &signals.Signal{
-		Type: signals.OperationDeprovision,
+
+	s.evClient.Send(ctx, install.ID, &signals.Signal{
+		Type: signals.OperationForget,
 	})
-	s.evClient.Send(ctx, installID, &signals.Signal{
-		Type: signals.OperationDelete,
-	})
+
+	ctx.Header(app.HeaderInstallWorkflowID, workflow.ID)
 
 	ctx.JSON(http.StatusOK, true)
-}
-
-func (s *service) deleteInstall(ctx context.Context, installID string) error {
-	install := app.Install{
-		ID: installID,
-	}
-
-	res := s.db.WithContext(ctx).Model(&install).Updates(app.Install{
-		Status:            "delete_queued",
-		StatusDescription: "delete has been queued and waiting",
-	})
-	if res.Error != nil {
-		return fmt.Errorf("unable to update install: %w", res.Error)
-	}
-
-	if res.RowsAffected < 1 {
-		return fmt.Errorf("install not found %s: %w", installID, gorm.ErrRecordNotFound)
-	}
-
-	return nil
 }
