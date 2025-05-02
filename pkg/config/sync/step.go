21d1884c3cd6e1a0d91c7e59cb08db879058aeca
Rob <alphavibe@gmail.com>
2025-05-02 12:20:46 -0700

    fix: move reconcile states (#8836)

--- pkg/config/sync/step.go
+++ pkg/config/sync/step.go
@@ -8,11 +8,12 @@ import (
 	"github.com/nuonco/nuon-go/models"
 )
 
-func (s *sync) syncStep(ctx context.Context, step syncStep) (error) {
+func (s *sync) syncStep(ctx context.Context, step syncStep) error {
 	stepErr := step.Method(ctx)
 	if stepErr == nil {
 		return nil
 	}
+	s.reconcileStates()
 
 	stateJSON, err := json.Marshal(s.state)
 	if err != nil {
