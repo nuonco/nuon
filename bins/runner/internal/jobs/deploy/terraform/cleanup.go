979e3ab75aef6d49ec8ab88a3eba8c55d1291900
Dat Boi Diego <fidiego@users.noreply.github.com>
2025-05-03 11:42:20 -0500

    fix: runner: ensure the tf workspace is cleaned up (#8843)

--- bins/runner/internal/jobs/deploy/terraform/cleanup.go
+++ bins/runner/internal/jobs/deploy/terraform/cleanup.go
@@ -2,14 +2,22 @@ package terraform
 
 import (
 	"context"
+	"os"
 
 	"github.com/nuonco/nuon-runner-go/models"
+	pkgctx "github.com/powertoolsdev/mono/bins/runner/internal/pkg/ctx"
+	"go.uber.org/zap"
 )
 
 func (h *handler) Cleanup(ctx context.Context, job *models.AppRunnerJob, jobExecution *models.AppRunnerJobExecution) error {
+	l, _ := pkgctx.Logger(ctx)
+	l.Info("cleanup: archive")
 	if err := h.state.arch.Cleanup(ctx); err != nil {
 		h.errRecorder.Record("unable to cleanup archive", err)
 	}
-
+	l.Info("cleanup: terraform - removing workspace", zap.String("workspace.root", h.state.tfWorkspace.Root()))
+	if err := os.RemoveAll(h.state.tfWorkspace.Root()); err != nil {
+		l.Error("cleanup: terraform cleanup failed", zap.String("workspace.root", h.state.tfWorkspace.Root()))
+	}
 	return nil
 }
