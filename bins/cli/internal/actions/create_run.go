c670cfd226b0b59e5984c36b810761c9a67fe682
Rob <alphavibe@gmail.com>
2025-05-05 15:11:29 -0700

    fix: cli action runs & teardown component (#8852)

--- bins/cli/internal/actions/create_run.go
+++ bins/cli/internal/actions/create_run.go
@@ -2,6 +2,7 @@ package actions
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/nuonco/nuon-go/models"
 	"github.com/powertoolsdev/mono/bins/cli/internal/ui"
@@ -9,26 +10,23 @@ import (
 )
 
 func (s *Service) CreateRun(ctx context.Context, installID, actionWorkflowID string, asJSON bool) error {
-	view := ui.NewCreateView("action run", asJSON)
-	view.Start()
-
 	awc, err := s.api.GetActionWorkflowLatestConfig(ctx, actionWorkflowID)
-
 	if err != nil {
-		return view.Fail(err)
+		ui.PrintError(fmt.Errorf("error getting action workflow config: %w", err))
+		return err
 	}
 
-	view.Update("creating action run")
 	req := &models.ServiceCreateInstallActionWorkflowRunRequest{
 		ActionWorkflowConfigID: generics.ToPtr(awc.ID),
 	}
 
-	run, err := s.api.CreateInstallActionWorkflowRun(ctx, installID, req)
+	err = s.api.CreateInstallActionWorkflowRun(ctx, installID, req)
 	if err != nil {
-		return view.Fail(err)
+		ui.PrintError(fmt.Errorf("error creating action workflow run: %w", err))
+		return err
 	}
 
-	view.Success(run.ID)
+	ui.PrintLn(fmt.Sprintf("action triggered for action %s", actionWorkflowID))
 
 	return nil
 }
