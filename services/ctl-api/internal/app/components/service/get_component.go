3c0717e1e2a30ae02655dbc8b1b3e8a1563bfd64
Jon Morehouse <MorehouseJ09@gmail.com>
2025-05-07 09:03:56 -0700

    fix: set `ComponentType` on component properly (#8881)
    * chore: add latest config on get-component
    
    * fix scope
    
    * update
    
    ---------
    
    Co-authored-by: nuonbot <bot@nuon.co>

--- services/ctl-api/internal/app/components/service/get_component.go
+++ services/ctl-api/internal/app/components/service/get_component.go
@@ -8,25 +8,26 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app/components/helpers"
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/pkg/cctx"
 )
 
-//	@ID						GetComponent
-//	@Summary				get a component
-//	@Description.markdown	get_component.md
-//	@Param					component_id	path	string	true	"component ID"
-//	@Tags					components
-//	@Accept					json
-//	@Produce				json
-//	@Security				APIKey
-//	@Security				OrgID
-//	@Failure				400	{object}	stderr.ErrResponse
-//	@Failure				401	{object}	stderr.ErrResponse
-//	@Failure				403	{object}	stderr.ErrResponse
-//	@Failure				404	{object}	stderr.ErrResponse
-//	@Failure				500	{object}	stderr.ErrResponse
-//	@Success				200	{object}	app.Component
-//	@Router					/v1/components/{component_id} [get]
+// @ID						GetComponent
+// @Summary				get a component
+// @Description.markdown	get_component.md
+// @Param					component_id	path	string	true	"component ID"
+// @Tags					components
+// @Accept					json
+// @Produce				json
+// @Security				APIKey
+// @Security				OrgID
+// @Failure				400	{object}	stderr.ErrResponse
+// @Failure				401	{object}	stderr.ErrResponse
+// @Failure				403	{object}	stderr.ErrResponse
+// @Failure				404	{object}	stderr.ErrResponse
+// @Failure				500	{object}	stderr.ErrResponse
+// @Success				200	{object}	app.Component
+// @Router					/v1/components/{component_id} [get]
 func (s *service) GetComponent(ctx *gin.Context) {
 	org, err := cctx.OrgFromContext(ctx)
 	if err != nil {
@@ -54,6 +55,7 @@ func (s *service) findComponent(ctx context.Context, orgID, componentID string) 
 		Preload("Dependencies").
 		Preload("App").
 		Preload("App.Org").
+		Scopes(helpers.PreloadLatestConfig).
 		First(&component)
 	if res.Error != nil {
 		return nil, fmt.Errorf("unable to get component: %w", res.Error)
@@ -69,6 +71,7 @@ func (s *service) getComponentWithParents(ctx context.Context, cmpID string) (*a
 		Preload("App").
 		Preload("App.Org").
 		Preload("App.Org.VCSConnections").
+		Scopes(helpers.PreloadLatestConfig).
 		First(&parentCmp, "id = ?", cmpID)
 	if res.Error != nil {
 		return nil, fmt.Errorf("unable to get component: %w", res.Error)
