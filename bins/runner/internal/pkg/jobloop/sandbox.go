e428e7acd96777ee7ad29f427b2e8c436893a929
Jon Morehouse <MorehouseJ09@gmail.com>
2025-05-07 22:06:48 -0700

    chore: enable sandbox actions (#8902)

--- bins/runner/internal/pkg/jobloop/sandbox.go
+++ bins/runner/internal/pkg/jobloop/sandbox.go
@@ -4,16 +4,8 @@ import (
 	"github.com/nuonco/nuon-runner-go/models"
 )
 
-var ignoreSandboxMode []models.AppRunnerJobType = []models.AppRunnerJobType{
-	models.AppRunnerJobTypeActionsDashWorkflow,
-}
-
 func (j *jobLoop) isSandbox(job *models.AppRunnerJob) bool {
 	if job.Type == models.AppRunnerJobTypeShutDashDown {
-		return false
-	}
-
-	if job.Type == models.AppRunnerJobTypeActionsDashWorkflow {
 		return false
 	}
 
