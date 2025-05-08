95d87bf7a62facbaf2761b0bfc8e17992cafb56e
Nat Hamilton <info@nathamilton.com>
2025-05-08 15:24:20 -0400

    refactor(ui): workflow step auto scroll, skipped steps and handle no … (#8908)
    * refactor(ui): workflow step auto scroll, skipped steps and handle no TF state
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * refactor(ui): enter install name to deprovision or forget
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * refactor(ui): show skipped step reason
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/components/InstallSandbox/TerraformWorkspace.tsx
+++ services/dashboard-ui/src/components/InstallSandbox/TerraformWorkspace.tsx
@@ -5,6 +5,7 @@ import {
   getWorkspaceStateResources,
 } from '@/lib'
 import { Section } from '@/components/Card'
+import { Empty } from '@/components/Empty'
 import { Time } from '@/components/Time'
 import { WorkspaceManagementDropdown } from '@/components/InstallSandbox/WorkspaceManagementDropdown'
 import { DataTable } from '@/components/InstallSandbox/Table'
@@ -33,92 +34,96 @@ export const TerraformWorkspace: FC<ITerraformWorkspace> = async ({
       orgId,
       workspaceId: workspace?.id,
       stateId: states[0]?.id,
-    }),
+    }).catch(console.error),
     getWorkspaceStateResources({
       workspaceId: workspace?.id,
       stateId: states[0]?.id,
       orgId,
-    }),
+    }).catch(console.error),
     getToken(),
   ])
 
   const token = tokenRes.result.accessToken
 
-  const revisions = states.map((state: any, idx: number) => {
-    return [
-      <Text key={idx}>{state.revision}</Text>,
-      <Time key={idx} time={state.created_at} />,
-    ]
-  })
-
-  const resourceList = resources.map((resource, idx) => {
-    if (resource.mode == 'managed') {
+  if (states && resources && currentRevision) {
+    const revisions = states.map((state: any, idx: number) => {
       return [
-        <Text key={idx}>{resource.type}</Text>,
-        <Text key={idx}>{resource.name}</Text>,
-        <Text key={idx}>{resource.instances.length}</Text>,
+        <Text key={idx}>{state.revision}</Text>,
+        <Time key={idx} time={state.created_at} />,
       ]
-    }
-  })
+    })
 
-  const datasourceList = resources.map((datasource, idx) => {
-    if (datasource.mode == 'data') {
-      return [
-        <Text key={idx}>{datasource.type}</Text>,
-        <Text key={idx}>{datasource.name}</Text>,
-        <Text key={idx}>{datasource.instances.length}</Text>,
-      ]
-    }
-  })
+    const resourceList = resources.map((resource, idx) => {
+      if (resource.mode == 'managed') {
+        return [
+          <Text key={idx}>{resource.type}</Text>,
+          <Text key={idx}>{resource.name}</Text>,
+          <Text key={idx}>{resource.instances.length}</Text>,
+        ]
+      }
+    })
 
-  const outputs = currentRevision.data.outputs
-  const outputList = Object.keys(outputs).map((key, idx) => [
-    <Text key={idx}>{key}</Text>,
-    <Text key={idx}>{outputs[key].type[0]}</Text>,
-    <Code key={idx}>{JSON.stringify(outputs[key].value)}</Code>,
-  ])
+    const datasourceList = resources.map((datasource, idx) => {
+      if (datasource.mode == 'data') {
+        return [
+          <Text key={idx}>{datasource.type}</Text>,
+          <Text key={idx}>{datasource.name}</Text>,
+          <Text key={idx}>{datasource.instances.length}</Text>,
+        ]
+      }
+    })
 
-  return (
-    <>
-      <Section
-        className="flex-initial"
-        heading="Terraform state"
-        childrenClassName="flex flex-col gap-4"
-        actions={
-          <WorkspaceManagementDropdown
-            orgId={orgId}
-            workspace={workspace}
-            token={token}
-          />
-        }
-      >
-        <Tabs>
-          <Tab title="Resources">
-            <DataTable
-              headers={['Type', 'Name', 'Count']}
-              initData={resourceList}
+    const outputs = currentRevision.data.outputs
+    const outputList = Object.keys(outputs).map((key, idx) => [
+      <Text key={idx}>{key}</Text>,
+      <Text key={idx}>{outputs[key].type[0]}</Text>,
+      <Code key={idx}>{JSON.stringify(outputs[key].value)}</Code>,
+    ])
+
+    return (
+      <>
+        <Section
+          className="flex-initial"
+          heading="Terraform state"
+          childrenClassName="flex flex-col gap-4"
+          actions={
+            <WorkspaceManagementDropdown
+              orgId={orgId}
+              workspace={workspace}
+              token={token}
             />
-          </Tab>
-          <Tab title="Data Sources">
-            <DataTable
-              headers={['Type', 'Name', 'Count']}
-              initData={datasourceList}
-            />
-          </Tab>
-          <Tab title="Outputs">
-            <DataTable
-              headers={['Name', 'Type', 'Value']}
-              initData={outputList}
-            />
-          </Tab>
-          <Tab title="History">
-            <DataTable
-              headers={['Revision', 'Created at']}
-              initData={revisions}
-            />
-          </Tab>
-        </Tabs>
-      </Section>
-    </>
-  )
+          }
+        >
+          <Tabs>
+            <Tab title="Resources">
+              <DataTable
+                headers={['Type', 'Name', 'Count']}
+                initData={resourceList}
+              />
+            </Tab>
+            <Tab title="Data Sources">
+              <DataTable
+                headers={['Type', 'Name', 'Count']}
+                initData={datasourceList}
+              />
+            </Tab>
+            <Tab title="Outputs">
+              <DataTable
+                headers={['Name', 'Type', 'Value']}
+                initData={outputList}
+              />
+            </Tab>
+            <Tab title="History">
+              <DataTable
+                headers={['Revision', 'Created at']}
+                initData={revisions}
+              />
+            </Tab>
+          </Tabs>
+        </Section>
+      </>
+    )
+  } else {
+    return null
+  }
 }
