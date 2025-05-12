a3a0001dd9e4338f8078bfd0ea4ed98d5d88e88f
Jon Morehouse <MorehouseJ09@gmail.com>
2025-05-12 00:04:10 -0700

    chore: point-in-time configs (#8930)
    chore: update

--- services/ctl-api/internal/pkg/db/viewsql/component_config_connections_v1.sql
+++ services/ctl-api/internal/pkg/db/viewsql/component_config_connections_v1.sql
@@ -13,6 +13,10 @@ WITH component_config_connections_with_count AS (
      component_config_connections rje
 )
 
-SELECT *
-FROM component_config_connections_with_count
-ORDER BY version DESC
+SELECT
+  ccc.*,
+  ac.version as app_config_version
+FROM component_config_connections_with_count ccc
+JOIN 
+  app_configs_view_v2 ac on ac.id = ccc.app_config_id
+ORDER BY ccc.version DESC
