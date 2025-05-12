2bfc92215f84e8a7bbccb001a845abde02e0be9f
Jon Morehouse <MorehouseJ09@gmail.com>
2025-05-12 00:04:10 -0700

    chore: point-in-time configs (#8930)
    chore: update

---
+++ services/ctl-api/internal/app/apps/helpers/get_components_by_app_config_version.go
@@ -0,0 +1,30 @@
+package helpers
+
+import (
+	"context"
+
+	"github.com/pkg/errors"
+	"gorm.io/gorm"
+
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
+)
+
+// This returns all app components for a point in time
+func (h *Helpers) GetAppComponentsAtConfigVersion(ctx context.Context, appID string, appCfgVersion int) ([]app.Component, error) {
+	var components []app.Component
+
+	res := h.db.WithContext(ctx).
+		Where(&app.Component{
+			AppID: appID,
+		}).
+		Preload("ComponentConfigs", func(db *gorm.DB) *gorm.DB {
+			return db.Where("version <= ?", appCfgVersion)
+		}).
+		Scopes(PreloadComponentConfigConnections).
+		Find(&components)
+	if res.Error != nil {
+		return nil, errors.Wrap(res.Error, "unable to get install components")
+	}
+
+	return components, nil
+}
