463f4d34f741ef6f6cd8a4031dd876635e42162b
Harsh Thakur <harsh@nuon.co>
2025-05-13 00:42:50 +0530

    revert: "feat: update component type when creating component config (#… (#8938)
    fix: revert
    
    This reverts commit cf5b83a9bb16bf0cf5d015b2de428393788d4388.

--- services/ctl-api/internal/app/components/service/create_docker_build_component_config.go
+++ services/ctl-api/internal/app/components/service/create_docker_build_component_config.go
@@ -124,10 +124,5 @@ func (s *service) createDockerBuildComponentConfig(ctx context.Context, cmpID st
 		return nil, fmt.Errorf("unable to create docker build component config connection: %w", res.Error)
 	}
 
-	err = s.helpers.UpdateComponentType(ctx, cmpID, app.ComponentTypeDockerBuild)
-	if err != nil {
-		return nil, fmt.Errorf("unable to update component type: %w", err)
-	}
-
 	return &cfg, nil
 }
