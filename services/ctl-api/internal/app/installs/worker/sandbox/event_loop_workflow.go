411463c88c0e370c600713f1bf89a3f06bd97ba3
sam boyer <sam@nuon.co>
2025-05-13 02:24:53 -0400

    chore: Split install workflows into subpackages (#8934)
    * Split sandbox into subpackage
    
    * Split stack into subpackage
    
    * Split components into subpackage
    
    * Split actions into subpackage
    
    * Add the actions workflows enumeration
    
    ---------
    
    Co-authored-by: nuonbot <bot@nuon.co>

---
+++ services/ctl-api/internal/app/installs/worker/sandbox/event_loop_workflow.go
@@ -0,0 +1,43 @@
+package sandbox
+
+import (
+	"go.temporal.io/sdk/workflow"
+
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app/installs/signals"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/pkg/eventloop"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/pkg/eventloop/loop"
+)
+
+func (w *Workflows) getHandlers() map[eventloop.SignalType]func(workflow.Context, signals.RequestSignal) error {
+	return map[eventloop.SignalType]func(workflow.Context, signals.RequestSignal) error{
+		signals.OperationDeprovisionSandbox: w.AwaitDeprovisionSandbox,
+		signals.OperationReprovisionSandbox: w.AwaitReprovisionSandbox,
+		signals.OperationProvisionSandbox:   w.AwaitProvisionSandbox,
+		// signals.OperationRestart:            w.AwaitRestarted, // TODO(sdboyer) cant reregister, need namespacing
+	}
+}
+
+func (w *Workflows) EventLoop(ctx workflow.Context, req eventloop.EventLoopRequest, pendingSignals []*signals.Signal) error {
+	handlers := w.getHandlers()
+	l := loop.Loop[*signals.Signal, signals.RequestSignal]{
+		Cfg:              w.cfg,
+		V:                w.v,
+		MW:               w.mw,
+		Handlers:         handlers,
+		NewRequestSignal: signals.NewRequestSignal,
+		// StartupHook: func(ctx workflow.Context, req eventloop.EventLoopRequest) error {
+		// 	w.handleSyncActionWorkflowTriggers(ctx, signals.RequestSignal{
+		// 		Signal: &signals.Signal{
+		// 			Type: signals.OperationSyncActionWorkflowTriggers,
+		// 		},
+		// 		EventLoopRequest: req,
+		// 	})
+		// 	return nil
+		// },
+		// ExistsHook: func(ctx workflow.Context, req eventloop.EventLoopRequest) (bool, error) {
+		// 	return activities.AwaitCheckExistsByID(ctx, req.ID)
+		// },
+	}
+
+	return l.Run(ctx, req, pendingSignals)
+}
