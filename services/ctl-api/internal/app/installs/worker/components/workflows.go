8f2ca41972c3a4eab195d449080d220929796a52
sam boyer <sam@nuon.co>
2025-05-13 02:24:53 -0400

    chore: Split install workflows into subpackages (#8934)
    * Split sandbox into subpackage
    
    * Split stack into subpackage
    
    * Split components into subpackage
    
    * Split actions into subpackage
    
    * Add the actions workflows enumeration
    
    ---------
    
    Co-authored-by: nuonbot <bot@nuon.co>

---
+++ services/ctl-api/internal/app/installs/worker/components/workflows.go
@@ -0,0 +1,74 @@
+package components
+
+import (
+	"fmt"
+
+	"github.com/go-playground/validator/v10"
+	"go.uber.org/fx"
+	"gorm.io/gorm"
+
+	temporalanalytics "github.com/powertoolsdev/mono/pkg/analytics/temporal"
+	"github.com/powertoolsdev/mono/pkg/metrics"
+	tmetrics "github.com/powertoolsdev/mono/pkg/temporal/metrics"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app/installs/worker/actions"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/pkg/cloudformation"
+	teventloop "github.com/powertoolsdev/mono/services/ctl-api/internal/pkg/eventloop/temporal"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/pkg/protos"
+)
+
+type Params struct {
+	fx.In
+
+	Cfg       *internal.Config
+	DB        *gorm.DB `name:"psql"`
+	V         *validator.Validate
+	Protos    *protos.Adapter
+	MW        metrics.Writer
+	EVClient  teventloop.Client
+	Analytics temporalanalytics.Writer
+	Templates *cloudformation.Templates
+	// FIXME(sdboyer) remove ASAP, once lifecycle workflows are deprecated
+	ActionsWorkflows *actions.Workflows
+}
+
+type Workflows struct {
+	cfg       *internal.Config
+	v         *validator.Validate
+	protos    *protos.Adapter
+	mw        tmetrics.Writer
+	evClient  teventloop.Client
+	analytics temporalanalytics.Writer
+	templates *cloudformation.Templates
+	db        *gorm.DB
+	// FIXME(sdboyer) remove ASAP, once lifecycle workflows are deprecated
+	wfActions *actions.Workflows
+}
+
+func (w *Workflows) All() []any {
+	return w.ListWorkflowFns()
+}
+
+func NewWorkflows(params Params) (*Workflows, error) {
+	tmw, err := tmetrics.New(params.V,
+		tmetrics.WithMetricsWriter(params.MW),
+		tmetrics.WithTags(map[string]string{
+			"namespace": defaultNamespace,
+			"context":   "worker",
+			"actor-object": "install-component",
+		}))
+	if err != nil {
+		return nil, fmt.Errorf("unable to create temporal metrics writer: %w", err)
+	}
+
+	return &Workflows{
+		cfg:       params.Cfg,
+		v:         params.V,
+		protos:    params.Protos,
+		evClient:  params.EVClient,
+		mw:        tmw,
+		analytics: params.Analytics,
+		templates: params.Templates,
+		db:        params.DB,
+	}, nil
+}
