02780ec673c6542b8f2f0132505b622e288725c4
Jon Morehouse <MorehouseJ09@gmail.com>
2025-05-12 22:04:25 -0700

    fix: create namespace for secret before sync (#8939)
    * fix: create namespace for secret before sync
    
    * fix: validation for secrets

--- pkg/kube/secret/create.go
+++ pkg/kube/secret/create.go
@@ -3,6 +3,7 @@ package secret
 import (
 	"context"
 
+	"github.com/pkg/errors"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -13,6 +14,22 @@ func (k *k8sSecretManager) Upsert(ctx context.Context, value []byte) error {
 		return err
 	}
 	client := kubeClient.CoreV1().Secrets(k.Namespace)
+
+	// Check if namespace exists
+	nsClient := kubeClient.CoreV1().Namespaces()
+	_, err = nsClient.Get(ctx, k.Namespace, metav1.GetOptions{})
+	if err != nil {
+		// Create the namespace if it doesn't exist
+		namespace := &v1.Namespace{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: k.Namespace,
+			},
+		}
+		_, err = nsClient.Create(ctx, namespace, metav1.CreateOptions{})
+		if err != nil {
+			return errors.Wrap(err, "unable to create a namespace")
+		}
+	}
 
 	kubeSecret := &v1.Secret{
 		ObjectMeta: metav1.ObjectMeta{
