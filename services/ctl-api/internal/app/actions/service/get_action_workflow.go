7ecdf0324fc8e9d13e1103eabeed8d73e2609846
Rob <alphavibe@gmail.com>
2025-05-13 20:13:23 -0700

    fix: set view for workflow gets (#8970)

--- services/ctl-api/internal/app/actions/service/get_action_workflow.go
+++ services/ctl-api/internal/app/actions/service/get_action_workflow.go
@@ -10,24 +10,25 @@ import (
 
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/pkg/cctx"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/pkg/db/scopes"
 )
 
-//	@ID						GetActionWorkflow
-//	@Summary				get an app action workflow
-//	@Description.markdown	get_app_action_workflow.md
-//	@Param					action_workflow_id	path	string	true	"action workflow ID or name"
-//	@Tags					actions
-//	@Accept					json
-//	@Produce				json
-//	@Security				APIKey
-//	@Security				OrgID
-//	@Failure				400	{object}	stderr.ErrResponse
-//	@Failure				401	{object}	stderr.ErrResponse
-//	@Failure				403	{object}	stderr.ErrResponse
-//	@Failure				404	{object}	stderr.ErrResponse
-//	@Failure				500	{object}	stderr.ErrResponse
-//	@Success				200	{object}	app.ActionWorkflow
-//	@Router					/v1/action-workflows/{action_workflow_id} [get]
+// @ID						GetActionWorkflow
+// @Summary				get an app action workflow
+// @Description.markdown	get_app_action_workflow.md
+// @Param					action_workflow_id	path	string	true	"action workflow ID or name"
+// @Tags					actions
+// @Accept					json
+// @Produce				json
+// @Security				APIKey
+// @Security				OrgID
+// @Failure				400	{object}	stderr.ErrResponse
+// @Failure				401	{object}	stderr.ErrResponse
+// @Failure				403	{object}	stderr.ErrResponse
+// @Failure				404	{object}	stderr.ErrResponse
+// @Failure				500	{object}	stderr.ErrResponse
+// @Success				200	{object}	app.ActionWorkflow
+// @Router					/v1/action-workflows/{action_workflow_id} [get]
 func (s *service) GetActionWorkflow(ctx *gin.Context) {
 	org, err := cctx.OrgFromContext(ctx)
 	if err != nil {
@@ -49,7 +50,7 @@ func (s *service) findActionWorkflow(ctx context.Context, orgID, awID string) (*
 	aw := app.ActionWorkflow{}
 	res := s.db.WithContext(ctx).
 		Preload("Configs", func(db *gorm.DB) *gorm.DB {
-			return db.Limit(10).Order("action_workflow_configs.created_at DESC")
+			return db.Scopes(scopes.WithOverrideTable("action_workflow_configs_latest_view_v1"))
 		}).
 		Preload("Configs.Triggers").
 		Preload("Configs.Steps").
