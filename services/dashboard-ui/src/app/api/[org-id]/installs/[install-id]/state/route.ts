cc901ed563d205cea38a8841f1495c568a078d16
Nat Hamilton <info@nathamilton.com>
2025-05-15 16:13:53 -0400

    feat(ui): added ability to view install state in modal (#8999)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

---
+++ services/dashboard-ui/src/app/api/[org-id]/installs/[install-id]/state/route.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { withApiAuthRequired } from '@auth0/nextjs-auth0'
+import { nueQueryData } from '@/utils'
+import { TRouteRes } from '@/app/api/[org-id]/types'
+
+export const GET = withApiAuthRequired(
+  async (
+    req: NextRequest,
+    { params }: TRouteRes<'org-id' | 'install-id'>
+  ) => {
+    const orgId = params?.['org-id']
+    const installId = params?.['install-id']
+
+    const res = await nueQueryData({
+      orgId,
+      path: `installs/${installId}/state`,
+    })
+
+    return NextResponse.json(res)
+  }
+)
