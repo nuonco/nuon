c85c7942bf16a688034999a2137015f980ae6171
Jordan Acosta <jordan@nuon.co>
2025-05-15 11:29:10 -0700

    feat(bins/cli): dev command (#8861)
    * feat: `nuon dev` command
    
    New CLi command to make developing your app easier. The user should be
    able to sync, build, and deploy with a single command.
    
    This is probably a short-term solution, but we'll see what users think of it.
    
    Basically, this is going to:
    
    1. Prompt the user to create a dev branch, use the same branches for all
       components, and select an install to deploy to.
    1. Sync and build, like `nuon apps sync-dir` does.
    1. Build components if their source code has changed.
    1. Deploy new builds to the dev install.
    
    * feat(bins/cli): trigger builds for source code updates
    
    * feat(services/ctl-api): separate app config installs endpoint
    
    * feat(bins/cli): use new app config installs endpoint
    
    * feat(bins/cli): clean up dev command prompts
    
    * fix: app config installs endpoint type error
    
    * feat: clean up deployment output
    
    * feat: update seed data to test git status checking
    
    * fix: correctly message deploys failing
    
    * feat(bins/cli): dev command auto-approve flag
    
    * refactor(bins/cli): move app validattion to pre-flight section
    
    * feat(bins/cli): de-scope component source builds
    
    * feat: add dev-command feature toggle
    
    * refactor: clean up function and file names, dev command copy
    
    * feat: can override component branches with cli dev command
    
    * fix: reset seed data component branches
    
    * fix: unused var in fmt.Errorf call
    
    * fix: corrently check for unstaged changes

---
+++ services/ctl-api/internal/app/apps/service/update_app_config_installs.go
@@ -0,0 +1,88 @@
+package service
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"github.com/pkg/errors"
+
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
+)
+
+type UpdateAppConfigInstallsRequest struct {
+	UpdateAll  bool
+	InstallIDs []string
+}
+
+func (c *UpdateAppConfigInstallsRequest) Validate(v *validator.Validate) error {
+	if err := v.Struct(c); err != nil {
+		return fmt.Errorf("invalid request: %w", err)
+	}
+
+	return nil
+}
+
+// @ID						UpdateAppConfigInstalls
+// @Description.markdown	update_app_config_installs.md
+// @Tags					apps
+// @Accept					json
+// @Param					req	body	UpdateAppConfigInstallsRequest	true	"Input"
+// @Produce				json
+// @Param					app_id			path	string	true	"app ID"
+// @Param					app_config_id	path	string	true	"app config ID"
+// @Security				APIKey
+// @Security				OrgID
+// @Failure				400	{object}	stderr.ErrResponse
+// @Failure				401	{object}	stderr.ErrResponse
+// @Failure				403	{object}	stderr.ErrResponse
+// @Failure				404	{object}	stderr.ErrResponse
+// @Failure				500	{object}	stderr.ErrResponse
+// @Success				200	{string}	ok
+// @Router					/v1/apps/{app_id}/config/{app_config_id}/update-installs [POST]
+func (s *service) UpdateAppConfigInstalls(ctx *gin.Context) {
+	appID := ctx.Param("app_id")
+	appConfigID := ctx.Param("app_config_id")
+
+	var req UpdateAppConfigInstallsRequest
+	if err := ctx.BindJSON(&req); err != nil {
+		ctx.Error(fmt.Errorf("unable to parse request: %w", err))
+		return
+	}
+	if err := req.Validate(s.v); err != nil {
+		ctx.Error(fmt.Errorf("invalid request: %w", err))
+		return
+	}
+
+	err := s.updateAppConfigInstalls(ctx, appID, appConfigID, &req)
+	if err != nil {
+		ctx.Error(fmt.Errorf("unable to update app config installs: %w", err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, "ok")
+}
+
+func (s *service) updateAppConfigInstalls(ctx context.Context, appID, appConfigID string, req *UpdateAppConfigInstallsRequest) error {
+	res := s.db.WithContext(ctx).
+		Model(&app.Install{}).
+		Where(app.Install{
+			AppID: appID,
+		})
+
+	// if "all" is false, filter by the provided install IDs
+	if !req.UpdateAll {
+		res.Where("id in ?", req.InstallIDs)
+	}
+
+	res.Updates(app.Install{
+		AppConfigID: appConfigID,
+	})
+	if res.Error != nil {
+		return errors.Wrap(res.Error, "unable to update installations with new config")
+	}
+
+	return nil
+}
