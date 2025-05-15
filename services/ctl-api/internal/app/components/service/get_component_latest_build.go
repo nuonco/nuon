0716a79c49197463aa405ad8f1a2d6acb3bbc864
Harsh Thakur <harsh@nuon.co>
2025-05-16 01:59:28 +0530

    feat: install summary endpoint (#8656)
    * install summary endpoint
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    * fetch dependencies via app config
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    * correlate installs's component config with install component
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    * preload app config & component configurations
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    * fetch all app components by <= app config version
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    * fix building dep comps field
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    * preload component configs from component builds
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    ---------
    
    Signed-off-by: harsh <harsh@nuon.co>

--- services/ctl-api/internal/app/components/service/get_component_latest_build.go
+++ services/ctl-api/internal/app/components/service/get_component_latest_build.go
@@ -10,22 +10,22 @@ import (
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
 )
 
-//	@ID						GetComponentLatestBuild
-//	@Summary				get latest build for a component
-//	@Description.markdown	get_component_latest_build.md
-//	@Param					component_id	path	string	true	"component ID"
-//	@Tags					components
-//	@Accept					json
-//	@Produce				json
-//	@Security				APIKey
-//	@Security				OrgID
-//	@Failure				400	{object}	stderr.ErrResponse
-//	@Failure				401	{object}	stderr.ErrResponse
-//	@Failure				403	{object}	stderr.ErrResponse
-//	@Failure				404	{object}	stderr.ErrResponse
-//	@Failure				500	{object}	stderr.ErrResponse
-//	@Success				200	{object}	app.ComponentBuild
-//	@Router					/v1/components/{component_id}/builds/latest [GET]
+// @ID						GetComponentLatestBuild
+// @Summary				get latest build for a component
+// @Description.markdown	get_component_latest_build.md
+// @Param					component_id	path	string	true	"component ID"
+// @Tags					components
+// @Accept					json
+// @Produce				json
+// @Security				APIKey
+// @Security				OrgID
+// @Failure				400	{object}	stderr.ErrResponse
+// @Failure				401	{object}	stderr.ErrResponse
+// @Failure				403	{object}	stderr.ErrResponse
+// @Failure				404	{object}	stderr.ErrResponse
+// @Failure				500	{object}	stderr.ErrResponse
+// @Success				200	{object}	app.ComponentBuild
+// @Router					/v1/components/{component_id}/builds/latest [GET]
 func (s *service) GetComponentLatestBuild(ctx *gin.Context) {
 	cmpID := ctx.Param("component_id")
 
