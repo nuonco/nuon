9abda1be90062a905b281218190244d33e745673
Jon Morehouse <MorehouseJ09@gmail.com>
2025-05-16 23:51:06 -0700

    fix: sandbox-configs not respected, cancellation (#9021)
    * fix: disable feature flag for syncing
    
    * chore: more updates
    
    * chore: update

--- services/ctl-api/internal/app/installs/service/cancel_install_workflow.go
+++ services/ctl-api/internal/app/installs/service/cancel_install_workflow.go
@@ -1,11 +1,15 @@
 package service
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/pkg/errors"
 
+	"github.com/powertoolsdev/mono/pkg/generics"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/app/installs/signals"
 )
 
@@ -34,12 +38,19 @@ func (s *service) CancelInstallWorkflow(ctx *gin.Context) {
 		return
 	}
 
-	// if wf.Status != app.InstallWorkflowStatusRunning {
-	// 	ctx.Error(fmt.Errorf("install workflow is not running"))
-	// 	return
-	// }
+	if !generics.SliceContains(wf.Status.Status, []app.Status{
+		app.StatusInProgress,
+		app.StatusPending,
+	}) {
+		ctx.Error(fmt.Errorf("install workflow is not cancelable"))
+		return
+	}
 
 	id := fmt.Sprintf("event-loop-%s-execute-workflow-steps", wf.InstallID)
+	if err := s.cancelInstallWorkflow(ctx, wf.ID); err != nil {
+		ctx.Error(errors.Wrap(err, "unable to cancel workflow"))
+		return
+	}
 
 	err = s.evClient.Cancel(ctx, signals.TemporalNamespace, id)
 	if err != nil {
@@ -48,4 +59,24 @@ func (s *service) CancelInstallWorkflow(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusAccepted, true)
+}
+
+func (s *service) cancelInstallWorkflow(ctx context.Context, installWorkflowID string) error {
+	obj := app.InstallWorkflow{
+		ID: installWorkflowID,
+	}
+
+	status := app.NewCompositeStatus(ctx, app.StatusCancelled)
+	res := s.db.WithContext(ctx).Model(obj).Updates(
+		map[string]any{
+			"status": status,
+		})
+	if res.Error != nil {
+		return errors.Wrap(res.Error, "unable to update")
+	}
+	if res.RowsAffected < 1 {
+		return errors.New("no object found to update")
+	}
+
+	return nil
 }
