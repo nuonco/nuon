bdf52bbccb3b62a199b5d367cf5a7d08b8395f47
Jon Morehouse <MorehouseJ09@gmail.com>
2025-05-18 14:40:48 -0700

    chore: add outputs (#9028)
    * chore: update terraform outputs
    
    * chore: update

---
+++ services/ctl-api/infra/org_management.tf
@@ -0,0 +1,85 @@
+data "aws_iam_policy_document" "orgs_account_iam_access" {
+  provider = aws.orgs
+
+  // allow management of IAM roles for an org
+  statement {
+    effect = "Allow"
+    actions = [
+      "iam:CreatePolicy",
+      "iam:CreateRole",
+      "iam:GetRole",
+      "iam:TagRole",
+      "iam:TagPolicy",
+      "iam:AttachRolePolicy",
+      "iam:DeletePolicy",
+      "iam:DeleteRole",
+      "iam:DetachRolePolicy",
+    ]
+    resources = ["*", ]
+  }
+
+  // allow management of ecr repos
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecr:*",
+    ]
+    resources = ["*", ]
+  }
+
+}
+
+data "aws_iam_policy_document" "orgs_account_iam_access_trust" {
+  provider = aws.orgs
+
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole", ]
+
+    principals {
+      type = "AWS"
+      identifiers = [
+        module.service.eks_role_arn,
+        data.tfe_outputs.infra-orgs.values.iam_roles.support.arn,
+      ]
+    }
+  }
+
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole", ]
+
+    principals {
+      type = "AWS"
+      identifiers = [
+        data.tfe_outputs.infra-orgs.values.iam_roles.support.arn,
+      ]
+    }
+  }
+}
+
+resource "aws_iam_policy" "orgs_account_iam_access_policy" {
+  provider = aws.orgs
+
+  name   = "${local.name}-orgs-account-iam-access"
+  policy = data.aws_iam_policy_document.orgs_account_iam_access.json
+}
+
+module "management_access_role" {
+  providers = {
+    aws = aws.orgs
+  }
+
+
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
+  version = ">= 5.1.0"
+
+  create_role       = true
+  role_requires_mfa = false
+
+  role_name                       = "${local.name}-orgs-account-iam-access"
+  create_custom_role_trust_policy = true
+  custom_role_trust_policy        = data.aws_iam_policy_document.orgs_account_iam_access_trust.json
+  custom_role_policy_arns         = [aws_iam_policy.orgs_account_iam_access_policy.arn, ]
+}
+
--- services/ctl-api/infra/outputs_service.tf
+++ services/ctl-api/infra/outputs_service.tf
@@ -29,3 +29,40 @@ output "install_templates_bucket" {
 output "install_templates_bucket_base_url" {
   value = "https://${module.bucket.s3_bucket_bucket_regional_domain_name}/"
 }
+
+// management for working with k8s
+output "org_runner" {
+  description = "configuration to talk to the runner k8s cluster"
+  value = {
+    k8s_ca_data           = nonsensitive(data.tfe_outputs.infra-orgs.values.runner_k8s.ca_data)
+    k8s_public_endpoint   = nonsensitive(data.tfe_outputs.infra-orgs.values.runner_k8s.public_endpoint)
+    k8s_cluster_id        = nonsensitive(data.tfe_outputs.infra-orgs.values.runner_k8s.cluster_id)
+    k8s_iam_role_arn  = nonsensitive(data.tfe_outputs.infra-orgs.values.runner_k8s.access_role_arns["eks-ctl-api"])
+    oidc_provider_arn = nonsensitive(data.tfe_outputs.infra-orgs.values.runner_k8s.oidc_provider_arn)
+    oidc_provider_url = nonsensitive(data.tfe_outputs.infra-orgs.values.runner_k8s.oidc_provider_url)
+    region = local.vars.region
+    support_role_arn = nonsensitive(data.tfe_outputs.infra-orgs.values.iam_roles.support.arn)
+  }
+}
+
+output "dns" {
+  description = "dns management"
+
+  value = {
+    enabled = true,
+    management_iam_role_arn = module.public_dns_access_role.iam_role_arn,
+    zone_id = nonsensitive(data.tfe_outputs.infra-orgs.values.public_domain.zone_id),
+    root_domain =  nonsensitive(data.tfe_outputs.infra-orgs.values.public_domain.domain),
+  }
+}
+
+output "management" {
+  description = "management account for iam, ecr and more"
+
+  value = {
+    iam_role_arn = module.management_access_role.iam_role_arn,
+    account_id =  nonsensitive(data.tfe_outputs.infra-orgs.values.ecr.account_id),
+    ecr_registry_id =  nonsensitive(data.tfe_outputs.infra-orgs.values.ecr.registry_id),
+    ecr_registry_arn =  nonsensitive(data.tfe_outputs.infra-orgs.values.ecr.registry_arn),
+  }
+}
