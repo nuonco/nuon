daf322653306207d92725659af79f92655207372
Jon Morehouse <MorehouseJ09@gmail.com>
2025-05-21 08:11:44 -0700

    fix: org runner provisioning (#9065)
    fix: runner provisioning
    
    Co-authored-by: nuonbot <bot@nuon.co>

--- services/ctl-api/internal/app/runners/worker/kuberunner/deprovision.go
+++ services/ctl-api/internal/app/runners/worker/kuberunner/deprovision.go
@@ -8,13 +8,23 @@ import (
 	"github.com/powertoolsdev/mono/pkg/workflows/types/executors"
 )
 
-func (w wkflow) DeprovisionRunner(ctx workflow.Context, req executors.DeprovisionRunnerRequest) (*executors.DeprovisionRunnerResponse, error) {
+type DeprovisionRunnerRequest struct {
+	RunnerID string `validate:"required"`
+}
+
+type DeprovisionRunnerResponse struct{}
+
+// @temporal-gen workflow
+// @execution-timeout 30m
+// @task-timeout 15m
+// @id-template {{ .CallerID }}-deprovision-runner
+func (w Wkflow) DeprovisionRunner(ctx workflow.Context, req DeprovisionRunnerRequest) (*executors.DeprovisionRunnerResponse, error) {
 	clusterInfo := w.getClusterInfo()
 
 	if _, err := AwaitUninstall(ctx, &UninstallRequest{
 		ClusterInfo: clusterInfo,
 		Namespace:   req.RunnerID,
-                RunnerID: req.RunnerID,
+		RunnerID:    req.RunnerID,
 	}); err != nil {
 		return nil, fmt.Errorf("unable to uninstall runner: %w", err)
 	}
