996bb884789458afceda661f6c0cc4b0223c6640
Harsh Thakur <harsh@nuon.co>
2025-05-21 14:41:41 +0530

    fix: helm backend fixes (#8946)
    * Create helm chart if it doesn't exist
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    * skip validation when component type is unknown
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    * Revert "fix: Revert "feat: implement helm http backend " (#8942)"
    
    This reverts commit 334024d0a9756695ee2c50e88d3bf339b694fe9e.
    
    * create helm chart if it doesn't exist and driver is nuon
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    * fix helm client & fix component type
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    ---------
    
    Signed-off-by: harsh <harsh@nuon.co>

---
+++ services/ctl-api/internal/app/helm_chart.go
@@ -0,0 +1,44 @@
+package app
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+	"gorm.io/plugin/soft_delete"
+
+	"github.com/powertoolsdev/mono/pkg/shortid/domains"
+)
+
+type HelmChart struct {
+	ID          string                `gorm:"primary_key:true;check:id_checker,char_length(id)=26" json:"id"`
+	CreatedByID string                `json:"created_by_id" gorm:"not null;default:null" `
+	CreatedBy   Account               `json:"-"`
+	CreatedAt   time.Time             `json:"created_at" gorm:"notnull" `
+	UpdatedAt   time.Time             `json:"updated_at" gorm:"notnull" `
+	DeletedAt   soft_delete.DeletedAt `json:"-" `
+
+	OrgID string `json:"org_id" `
+	Org   Org    `json:"-" `
+
+	OwnerID   string `json:"owner_id" gorm:"type:text;check:owner_id_checker,char_length(id)=26;uniqueIndex:idx_owner" `
+	OwnerType string `json:"owner_type" gorm:"type:text;uniqueIndex:idx_owner"`
+
+	HelmReleases []HelmRelease `faker:"-"  gorm:"constraint:OnDelete:CASCADE;"`
+}
+
+func (h *HelmChart) BeforeCreate(tx *gorm.DB) (err error) {
+	if h.ID == "" {
+		h.ID = domains.NewHelmChartID()
+	}
+
+	if h.CreatedByID == "" {
+		h.CreatedByID = createdByIDFromContext(tx.Statement.Context)
+	}
+
+	if h.OrgID == "" {
+		h.OrgID = orgIDFromContext(tx.Statement.Context)
+	}
+
+	return nil
+
+}
---
+++ services/ctl-api/internal/app/installs/worker/activities/get_helm_chart.go
@@ -0,0 +1,46 @@
+package activities
+
+import (
+	"context"
+
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
+	"gorm.io/gorm"
+)
+
+type GetHelmChartRequest struct {
+	OwnerID string `validate:"required"`
+}
+
+// @temporal-gen activity
+// @by-id OwnerID
+func (a *Activities) GetHelmChart(ctx context.Context, req GetHelmChartRequest) (*app.HelmChart, error) {
+	return a.getOrCreateHelmChart(ctx, req.OwnerID)
+}
+
+func (a *Activities) getOrCreateHelmChart(ctx context.Context, ownerID string) (*app.HelmChart, error) {
+	helmChart := app.HelmChart{}
+	res := a.db.WithContext(ctx).Model(&app.HelmChart{}).
+		First(&helmChart, "owner_id = ?", ownerID)
+
+	if res.Error == nil {
+		return &helmChart, nil
+	}
+
+	if res.Error != gorm.ErrRecordNotFound {
+		return nil, res.Error
+	}
+
+	helmChart = app.HelmChart{
+		OwnerID:   ownerID,
+		OwnerType: "install_components",
+	}
+	if err := a.db.WithContext(ctx).Create(&helmChart).Error; err != nil {
+		return nil, err
+	}
+
+	if err := a.db.WithContext(ctx).First(&helmChart, "owner_id = ?", ownerID).Error; err != nil {
+		return nil, err
+	}
+
+	return &helmChart, nil
+}
