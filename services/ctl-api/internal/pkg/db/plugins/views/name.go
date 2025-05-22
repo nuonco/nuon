edda9c0b9441d2dfa4936a593dcd351e05162b53
Rob <alphavibe@gmail.com>
2025-05-22 08:06:51 -0700

    feat: views get current view name (#9092)
    feat: views current name

--- services/ctl-api/internal/pkg/db/plugins/views/name.go
+++ services/ctl-api/internal/pkg/db/plugins/views/name.go
@@ -8,6 +8,16 @@ import (
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/pkg/db/plugins"
 )
 
+func TableOrViewName(db *gorm.DB, obj ViewModel, appendStr string) string {
+	tableName := plugins.TableName(db, obj)
+
+	if !obj.UseView() {
+		return fmt.Sprintf("%s_%s%s", tableName, obj.ViewVersion(), appendStr)
+	}
+
+	return fmt.Sprintf("%s_view_%s%s", tableName, obj.ViewVersion(), appendStr)
+}
+
 func DefaultViewName(db *gorm.DB, obj any, version int) string {
 	tableName := plugins.TableName(db, obj)
 	return fmt.Sprintf("%s_view_v%d", tableName, version)
