a23b098abd14ca92975ef7529b4783759f2f31a8
Nat Hamilton <info@nathamilton.com>
2025-05-22 08:55:59 -0400

    fix(ui): use JsonViewer for runner job plans, fixed sync / deploy pla… (#9094)
    fix(ui): use JsonViewer for runner job plans, fixed sync / deploy plan buttons
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/components/Runners/RunnerJobPlanModal.tsx
+++ services/dashboard-ui/src/components/Runners/RunnerJobPlanModal.tsx
@@ -5,7 +5,7 @@ import { createPortal } from 'react-dom'
 import { CodeBlock } from '@phosphor-icons/react'
 import { Button } from '@/components/Button'
 import { Modal } from '@/components/Modal'
-import { CodeViewer } from '@/components/Code'
+import { CodeViewer, JsonView } from '@/components/Code'
 
 interface IRunnerJobPlanModal {
   buttonText?: string
@@ -33,10 +33,7 @@ export const RunnerJobPlanModal: FC<IRunnerJobPlanModal> = ({
               }}
             >
               <div className="flex flex-col gap-4">
-                <CodeViewer
-                  initCodeSource={JSON.stringify(plan, null, 2)}
-                  language="json"
-                />
+                <JsonView data={plan} />
               </div>
               <div className="mt-4 flex gap-3 justify-end">
                 <Button
