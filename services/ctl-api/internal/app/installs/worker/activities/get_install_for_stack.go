dfc4faa9c3b7c585c06b0db1bf9e261d64086acc
sam boyer <sam@nuon.co>
2025-05-22 12:57:50 -0400

    feat: Handle workflow steps on subloops, redux (#9099)
    * Reapply "feat: Handle workflow steps on subloops (#8980)"
    
    This reverts commit 085f93402686050ff53bfc5e8d9062b43ca42804.
    
    * feat: Handle workflow steps on subloops, redux

---
+++ services/ctl-api/internal/app/installs/worker/activities/get_install_for_stack.go
@@ -0,0 +1,29 @@
+package activities
+
+import (
+	"context"
+
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/pkg/db/generics"
+)
+
+type GetInstallForStackRequest struct {
+	StackID string `json:"stack_id"`
+}
+
+// @temporal-gen activity
+// @by-id StackID
+func (a *Activities) GetInstallForStack(ctx context.Context, req GetInstallForStackRequest) (*app.Install, error) {
+	var stack app.InstallStack
+
+	res := a.db.WithContext(ctx).
+		Where(app.InstallStack{
+			ID: req.StackID,
+		}).
+		First(&stack)
+	if res.Error != nil {
+		return nil, generics.TemporalGormError(res.Error, "unable to get install stack")
+	}
+
+	return a.getInstall(ctx, stack.InstallID)
+}
