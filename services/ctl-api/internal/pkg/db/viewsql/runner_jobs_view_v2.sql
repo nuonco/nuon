8bf7f186b3770a493d486e71aad5abb2c44c7d64
Rob <alphavibe@gmail.com>
2025-05-22 10:23:26 -0700

    feat: revert fix: Revert feat: runner jobs a quarter mile at a time (#9093)
    Revert "fix: Revert "feat: runner jobs a quarter mile at a time" (#9091)"
    
    This reverts commit 3e1ed82e496fda89f70854db0cf8aa021b492147.

---
+++ services/ctl-api/internal/pkg/db/viewsql/runner_jobs_view_v2.sql
@@ -0,0 +1,29 @@
+SELECT 
+    rj.*,
+    -- Execution count subquery with NULL if 0
+    NULLIF((SELECT count(*) 
+            FROM public.runner_job_executions rje 
+            WHERE rje.runner_job_id = rj.id), 0) AS execution_count,
+    -- Final execution ID subquery
+    (SELECT rjeo.runner_job_execution_id
+     FROM runner_job_execution_outputs rjeo
+     JOIN (
+         SELECT rje.id
+         FROM public.runner_job_executions rje
+         WHERE rje.runner_job_id = rj.id
+         ORDER BY rje.created_at
+         LIMIT 1
+     ) first_exec ON first_exec.id = rjeo.runner_job_execution_id
+     LIMIT 1) AS final_runner_job_execution_id,
+    -- Outputs subquery
+    (SELECT rjeo.outputs
+     FROM runner_job_execution_outputs rjeo
+     JOIN (
+         SELECT rje.id
+         FROM public.runner_job_executions rje
+         WHERE rje.runner_job_id = rj.id
+         ORDER BY rje.created_at
+         LIMIT 1
+     ) first_exec ON first_exec.id = rjeo.runner_job_execution_id
+     LIMIT 1) AS outputs
+FROM runner_jobs rj;
