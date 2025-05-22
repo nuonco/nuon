81c197ee435a0701ed65a00765326fd9b363b9d0
sam boyer <sam@nuon.co>
2025-05-22 12:57:50 -0400

    feat: Handle workflow steps on subloops, redux (#9099)
    * Reapply "feat: Handle workflow steps on subloops (#8980)"
    
    This reverts commit 085f93402686050ff53bfc5e8d9062b43ca42804.
    
    * feat: Handle workflow steps on subloops, redux

--- services/ctl-api/internal/app/installs/worker/components/event_loop_workflow.go
+++ services/ctl-api/internal/app/installs/worker/components/event_loop_workflow.go
@@ -8,30 +8,30 @@ import (
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/pkg/eventloop/loop"
 )
 
-func (w *Workflows) getHandlers() map[eventloop.SignalType]func(workflow.Context, signals.RequestSignal) error {
+func (w *Workflows) GetHandlers() map[eventloop.SignalType]func(workflow.Context, signals.RequestSignal) error {
 	return map[eventloop.SignalType]func(workflow.Context, signals.RequestSignal) error{
-			signals.OperationExecuteDeployComponent:   AwaitExecuteDeployComponent,
-			signals.OperationExecuteTeardownComponent: AwaitExecuteTeardownComponent,
+		signals.OperationExecuteDeployComponent:   AwaitExecuteDeployComponent,
+		signals.OperationExecuteTeardownComponent: AwaitExecuteTeardownComponent,
 	}
 }
 
 func (w *Workflows) ComponentEventLoop(ctx workflow.Context, req eventloop.EventLoopRequest, pendingSignals []*signals.Signal) error {
-	handlers := w.getHandlers()
+	handlers := w.GetHandlers()
 	l := loop.Loop[*signals.Signal, signals.RequestSignal]{
-			Cfg:              w.cfg,
-			V:                w.v,
-			MW:               w.mw,
-			Handlers:         handlers,
-			NewRequestSignal: signals.NewRequestSignal,
-			// StartupHook: func(ctx workflow.Context, req eventloop.EventLoopRequest) error {
-			//         w.handleSyncActionWorkflowTriggers(ctx, signals.RequestSignal{
-			//                 Signal: &signals.Signal{
-			//                         Type: signals.OperationSyncActionWorkflowTriggers,
-			//                 },
-			//                 EventLoopRequest: req,
-			//         })
-			//         return nil
-			// },
+		Cfg:              w.cfg,
+		V:                w.v,
+		MW:               w.mw,
+		Handlers:         handlers,
+		NewRequestSignal: signals.NewRequestSignal,
+		// StartupHook: func(ctx workflow.Context, req eventloop.EventLoopRequest) error {
+		//         w.handleSyncActionWorkflowTriggers(ctx, signals.RequestSignal{
+		//                 Signal: &signals.Signal{
+		//                         Type: signals.OperationSyncActionWorkflowTriggers,
+		//                 },
+		//                 EventLoopRequest: req,
+		//         })
+		//         return nil
+		// },
 	}
 
 	return l.Run(ctx, req, pendingSignals)
