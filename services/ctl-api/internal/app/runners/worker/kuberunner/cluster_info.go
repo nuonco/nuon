0f3dee52d4f4e97eb53aa8c183259390b1889b56
Jon Morehouse <MorehouseJ09@gmail.com>
2025-05-23 18:33:36 -0700

    fix: support default role (#9136)

--- services/ctl-api/internal/app/runners/worker/kuberunner/cluster_info.go
+++ services/ctl-api/internal/app/runners/worker/kuberunner/cluster_info.go
@@ -6,18 +6,27 @@ import (
 )
 
 func (w *Wkflow) getClusterInfo() *kube.ClusterInfo {
-	return &kube.ClusterInfo{
+	clusterInfo := &kube.ClusterInfo{
 		ID:       w.cfg.OrgRunnerK8sClusterID,
 		Endpoint: w.cfg.OrgRunnerK8sPublicEndpoint,
 		CAData:   w.cfg.OrgRunnerK8sCAData,
+	}
 
-		AWSAuth: &awscredentials.Config{
+	if w.cfg.OrgRunnerK8sUseDefaultCreds {
+		clusterInfo.AWSAuth = &awscredentials.Config{
+			Region:     w.cfg.OrgRunnerRegion,
+			UseDefault: true,
+		}
+	} else {
+		clusterInfo.AWSAuth = &awscredentials.Config{
 			Region: w.cfg.OrgRunnerRegion,
 			AssumeRole: &awscredentials.AssumeRoleConfig{
 				RoleARN:                w.cfg.OrgRunnerK8sIAMRoleARN,
 				SessionName:            "ctl-api-runner-install",
 				SessionDurationSeconds: 60 * 60,
 			},
-		},
+		}
 	}
+
+	return clusterInfo
 }
