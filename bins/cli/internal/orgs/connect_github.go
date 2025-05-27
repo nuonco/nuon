9f43b6fd4dbaaf81b3a4512a777eaff017c9529a
Jordan Acosta <jordan@nuon.co>
2025-05-27 14:50:26 -0700

    fix: connect to github via cli in byoc installs (#9157)
    The CLI uses our own github apps by default. You can configure it to use
    a different app, just like you can configure it to use a different api,
    but that's not a good UX.
    
    This PR adds a `/connect-github` endpoint to the dashboard, and updates
    the CLI to send users there. The dashboard will already be configured
    with the correct github app for the byoc install the user is workin
    with.

--- bins/cli/internal/orgs/connect_github.go
+++ bins/cli/internal/orgs/connect_github.go
@@ -3,9 +3,7 @@ package orgs
 import (
 	"context"
 	"fmt"
-	"time"
 
-	"github.com/nuonco/nuon-go/models"
 	"github.com/pkg/browser"
 	"github.com/powertoolsdev/mono/bins/cli/internal/ui"
 )
@@ -16,69 +14,11 @@ func (s *Service) ConnectGithub(ctx context.Context) error {
 		return nil
 	}
 
-	var v *models.AppVCSConnection
-	connection := "started"
-	view := ui.NewGetView()
-
-	ogVcs, _, err := s.api.GetVCSConnections(ctx, &models.GetVCSConnectionsQuery{
-		Offset:            0,
-		Limit:             50,
-		PaginationEnabled: s.cfg.PaginationEnabled,
-	})
+	cfg, err := s.api.GetCLIConfig(ctx)
 	if err != nil {
-		view.Error(err)
-		connection = "failed"
+		return ui.PrintError(fmt.Errorf("couldn't get cli config: %w", err))
 	}
 
-	timeout := 1
-	var connected bool
-	for !connected {
-		fmt.Printf("connection: %s\n", connection)
-
-		if connection == "started" {
-			connection = "pending"
-			browser.OpenURL("https://github.com/apps/" + s.cfg.GitHubAppName + "/installations/new?state=" + s.cfg.OrgID)
-		}
-
-		vcs, _, err := s.api.GetVCSConnections(ctx, &models.GetVCSConnectionsQuery{
-			Offset:            0,
-			Limit:             50,
-			PaginationEnabled: s.cfg.PaginationEnabled,
-		})
-		if err != nil {
-			view.Error(err)
-			connection = "failed"
-		}
-
-		if len(vcs) > len(ogVcs) || connection == "failed" {
-			v = vcs[len(vcs)-1]
-			fmt.Println("connection: connected")
-			connection = "connected"
-			connected = true
-		} else {
-			time.Sleep(5 * time.Second)
-		}
-
-		if timeout == 18 {
-			fmt.Println("connection: time out")
-			connection = "time out"
-			connected = true
-		}
-
-		timeout++
-	}
-
-	if connection == "connected" {
-		fmt.Println("")
-		fmt.Println("new vcs connection")
-		fmt.Println("-------------------------------------------------")
-		view.Render([][]string{
-			{"id", v.ID},
-			{"github install id", v.GithubInstallID},
-		})
-	} else {
-		fmt.Println("")
-		fmt.Println("please try again")
-	}
-	return nil
+	url := fmt.Sprintf("%s/api/connect-github?org_id=%s", cfg.DashboardURL, s.cfg.OrgID)
+	return browser.OpenURL(url)
 }
