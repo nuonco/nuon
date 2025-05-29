767a808d617436bc58475062edbeb15c688af632
Nat Hamilton <info@nathamilton.com>
2025-05-29 16:51:25 -0400

    feat(ui): show runner connected status, updated runner step Ui, fix c… (#9194)
    feat(ui): show runner connected status, updated runner step Ui, fix comp filter layout
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/components/Runners/RunnerMeta.tsx
+++ services/dashboard-ui/src/components/Runners/RunnerMeta.tsx
@@ -1,4 +1,5 @@
 import React, { type FC } from 'react'
+import { StatusBadge } from '@/components/Status'
 import { Time } from '@/components/Time'
 import { ID, Text } from '@/components/Typography'
 import {
@@ -6,32 +7,64 @@ import {
   getInstallRunnerGroup,
   getOrgRunnerGroup,
 } from '@/lib'
+import type { TRunner } from '@/types'
 
 interface IRunnerMeta {
   orgId: string
-  runnerId: string
+  runner: TRunner
   installId?: string
 }
 
 export const RunnerMeta: FC<IRunnerMeta> = async ({
   orgId,
-  runnerId,
+  runner,
   installId = '',
 }) => {
-  const [runnerHeartbeat] = await Promise.all([
+  const getRunnerGroup = () =>
+    installId === ''
+      ? getOrgRunnerGroup({ orgId })
+      : getInstallRunnerGroup({ orgId, installId })
+
+  const [runnerHeartbeat, runnerGroup] = await Promise.all([
     getRunnerLatestHeartbeat({
       orgId,
-      runnerId,
+      runnerId: runner.id,
     }).catch(console.error),
+    getRunnerGroup().catch(console.error),
   ])
 
-  const runnerGroup =
-    installId === ''
-      ? await getOrgRunnerGroup({ orgId })
-      : await getInstallRunnerGroup({ orgId, installId })
-
   return (
-    <>
+    <div className="flex gap-8 items-start justify-start flex-wrap">
+      <span className="flex flex-col gap-2">
+        <Text className="text-cool-grey-600 dark:text-cool-grey-500">
+          Status
+        </Text>
+
+        {runnerHeartbeat && runnerHeartbeat?.alive_time > 15 * 1_000_000_000 ? (
+          <StatusBadge
+            status="connected"
+            description="Connected to runner"
+            descriptionAlignment="left"
+            shouldPoll
+            isWithoutBorder
+          />
+        ) : (
+          <StatusBadge
+            status="not-connected"
+            description="Not connected to runner"
+            descriptionAlignment="left"
+            shouldPoll
+            isWithoutBorder
+          />
+        )}
+        <StatusBadge
+          status={runner?.status === 'active' ? 'healthy' : 'unhealthy'}
+          description={runner?.status_description}
+          descriptionAlignment="left"
+          shouldPoll
+          isWithoutBorder
+        />
+      </span>
       {runnerHeartbeat ? (
         <>
           <span className="flex flex-col gap-2">
@@ -39,31 +72,29 @@ export const RunnerMeta: FC<IRunnerMeta> = async ({
               Started at
             </Text>
             <Text>
-              <Time
-                time={runnerHeartbeat?.started_at}
-                format="default"
-                variant="med-12"
-              />
+              <Time time={runnerHeartbeat?.started_at} format="default" />
             </Text>
           </span>
           <span className="flex flex-col gap-2">
             <Text className="text-cool-grey-600 dark:text-cool-grey-500">
               Version
             </Text>
-            <Text variant="med-12">{runnerHeartbeat?.version}</Text>
+            <Text>{runnerHeartbeat?.version}</Text>
           </span>
         </>
       ) : null}
-      <span className="flex flex-col gap-2">
-        <Text className="text-cool-grey-600 dark:text-cool-grey-500">
-          Platform
-        </Text>
-        <Text variant="med-12">{runnerGroup?.platform}</Text>
-      </span>
+      {runnerGroup ? (
+        <span className="flex flex-col gap-2">
+          <Text className="text-cool-grey-600 dark:text-cool-grey-500">
+            Platform
+          </Text>
+          <Text>{runnerGroup?.platform}</Text>
+        </span>
+      ) : null}
       <span className="flex flex-col gap-2">
         <Text className="text-cool-grey-600 dark:text-cool-grey-500">ID</Text>
-        <ID id={runnerId} />
+        <ID id={runner?.id} />
       </span>
-    </>
+    </div>
   )
 }
