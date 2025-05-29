28f453da0483709a278752af70dd63cc7a2c9231
Nat Hamilton <info@nathamilton.com>
2025-05-29 09:54:20 -0400

    feat(ui): show stack outputs form stack version table (#9185)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/components/Installs/StacksTable.tsx
+++ services/dashboard-ui/src/components/Installs/StacksTable.tsx
@@ -2,107 +2,115 @@
 
 import React, { type FC, useMemo, useState } from 'react'
 import { type ColumnDef } from '@tanstack/react-table'
-import { CaretRight, CalendarBlank, Minus } from '@phosphor-icons/react'
-import { ClickToCopyButton } from '@/components/ClickToCopy'
+import { CalendarBlank, Minus, DotsThreeVertical } from '@phosphor-icons/react'
 import { DataTableSearch, Table } from '@/components/DataTable'
-import { Link } from '@/components/Link'
+import { Dropdown } from '@/components/Dropdown'
 import { StatusBadge } from '@/components/Status'
 import { Time } from '@/components/Time'
 import { ID, Text } from '@/components/Typography'
+import { StackLinksModal } from './StackLinksModal'
+import { StackOutputsModal } from './StackOutputsModal'
 // eslint-disable-next-line import/no-cycle
-import type { TInstallStack } from '@/types'
+import type {
+  TInstallStack,
+  TInstallStackVersion,
+  TInstallStackVersionRun,
+} from '@/types'
 
 export interface IStacksTable {
-  installId: string
-  orgId: string
   stack: TInstallStack
 }
 
-export const StacksTable: FC<IStacksTable> = ({ installId, orgId, stack }) => {
+export const StacksTable: FC<IStacksTable> = ({ stack }) => {
   const [data, _] = useState(stack?.versions)
   const [columnFilters, __] = useState([])
   const [globalFilter, setGlobalFilter] = useState('')
 
-  const columns: Array<ColumnDef<TInstallStack['versions'] & { id: string }>> =
-    useMemo(
-      () => [
-        {
-          header: 'Stack version',
-          accessorKey: 'id',
-          cell: (props) => (
-            <div className="flex flex-col gap-2">
-              <ID className="mt-1" id={props.getValue<string>()} />
+  const columns: Array<ColumnDef<TInstallStackVersion>> = useMemo(
+    () => [
+      {
+        header: 'Stack version',
+        accessorKey: 'id',
+        cell: (props) => <ID className="mt-1" id={props.getValue<string>()} />,
+      },
+      {
+        header: 'App version',
+        accessorKey: 'app_config_id',
+        cell: (props) => <ID className="mt-1" id={props.getValue<string>()} />,
+      },
+      {
+        header: 'Status',
+        accessorKey: 'composite_status.status',
+        cell: (props) => {
+          const status = props.getValue<string>()
+          return status ? <StatusBadge status={status} /> : <Minus />
+        },
+      },
+      {
+        header: 'Runs',
+        accessorKey: 'runs',
+        id: 'run_count',
+        cell: (props) => {
+          const runs = props.getValue<Array<any>>()
+          return <Text>{runs?.length}</Text>
+        },
+      },
+      {
+        header: 'Created',
+        accessorKey: 'created_at',
+        cell: (props) => (
+          <Text className="inline truncate">
+            <CalendarBlank size="16" />
+            <Time format="relative" time={props.getValue<string>()} />
+          </Text>
+        ),
+      },
+      /* {
+       *   accessorKey: 'quick_link_url',
+       *   header: 'Quick link',
+       *   cell: (props) => (
+       *     <Text className="flex flex-nowrap max-w-[160px] items-center gap-2">
+       *       <span className="truncate">{props.getValue<string>()}</span>{' '}
+       *       <ClickToCopyButton textToCopy={props.getValue<string>()} />
+       *     </Text>
+       *   ),
+       * },
+       * {
+       *   accessorKey: 'template_url',
+       *   header: 'Template link',
+       *   cell: (props) => (
+       *     <Text className="flex flex-nowrap max-w-[160px] items-center gap-2">
+       *       <span className="truncate">{props.getValue<string>()}</span>{' '}
+       *       <ClickToCopyButton textToCopy={props.getValue<string>()} />
+       *     </Text>
+       *   ),
+       * }, */
+      {
+        id: 'runs',
+        enableSorting: false,
+        cell: (props) => {
+          return (
+            <div className="flex justify-start">
+              <Dropdown
+                className="!p-1"
+                id="more-stack"
+                text={<DotsThreeVertical size="14" />}
+                noIcon
+                alignment="right"
+              >
+                <StackLinksModal
+                  template_url={props?.row.original.template_url}
+                  quick_link_url={props.row.original.quick_link_url}
+                />
+                <StackOutputsModal runs={props.row.original.runs} />
+              </Dropdown>
             </div>
-          ),
+          )
         },
-        {
-          header: 'App version',
-          accessorKey: 'app_config_id',
-          cell: (props) => (
-            <ID className="mt-1" id={props.getValue<string>()} />
-          ),
-        },
-        {
-          header: 'Status',
-          accessorKey: 'composite_status.status',
-          cell: (props) => {
-            const status = props.getValue<string>()
-            return status ? <StatusBadge status={status} /> : <Minus />
-          },
-        },
-        {
-          header: 'Runs',
-          accessorKey: 'runs',
-          cell: (props) => {
-            const runs = props.getValue<Array<any>>()
-            return <Text>{runs?.length}</Text>
-          },
-        },
-        {
-          header: 'Created',
-          accessorKey: 'created_at',
-          cell: (props) => (
-            <Text className="!items-center">
-              <CalendarBlank size="18" />
-              <Time format="relative" time={props.getValue<string>()} />
-            </Text>
-          ),
-        },
-        {
-          accessorKey: 'quick_link_url',
-          header: 'Quick link',
-          cell: (props) => (
-            <Text className="flex flex-nowrap max-w-[160px] items-center gap-2">
-              <span className="truncate">{props.getValue<string>()}</span>{' '}
-              <ClickToCopyButton textToCopy={props.getValue<string>()} />
-            </Text>
-          ),
-        },
-        {
-          accessorKey: 'template_url',
-          header: 'Template link',
-          cell: (props) => (
-            <Text className="flex flex-nowrap max-w-[160px] items-center gap-2">
-              <span className="truncate">{props.getValue<string>()}</span>{' '}
-              <ClickToCopyButton textToCopy={props.getValue<string>()} />
-            </Text>
-          ),
-        },
-        /* {
-         *   id: 'test',
-         *   enableSorting: false,
-         *   cell: (props) => (
-         *     <Link
-         *       href={`/${orgId}/installs/${installId}/stacks/${props.row.original.id}`}
-         *       variant="ghost"
-         *     >
-         *       <CaretRight />
-         *     </Link>
-         *   ),
-         * }, */
-      ],
-      []
-    )
+      },
+    ],
+    []
+  )
 
   const handleGlobleFilter = (e: React.ChangeEvent<HTMLInputElement>) => {
     setGlobalFilter(e.target.value || '')
