dd108628d3ff907af2f0a8b17f14aae1c225da1a
Harsh Thakur <harsh@nuon.co>
2025-05-30 19:39:34 +0530

    feat: introduce job graceful shutdown primtive (#9200)
    * introduce job graceful shutdowns
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    * implement terraform sandbox graceful shutdown
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    * update component deploy with graceful shutdowns for terraform runs
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    ---------
    
    Signed-off-by: harsh <harsh@nuon.co>
    Co-authored-by: nuonbot <bot@nuon.co>

--- bins/runner/internal/jobs/deploy/job/handler.go
+++ bins/runner/internal/jobs/deploy/job/handler.go
@@ -1,10 +1,13 @@
 package job
 
 import (
+	"context"
+
 	"github.com/go-playground/validator/v10"
 	nuonrunner "github.com/nuonco/nuon-runner-go"
 	"github.com/nuonco/nuon-runner-go/models"
 	"go.uber.org/fx"
+	"go.uber.org/zap"
 
 	"github.com/powertoolsdev/mono/bins/runner/internal"
 	"github.com/powertoolsdev/mono/bins/runner/internal/jobs"
@@ -25,7 +28,7 @@ type handler struct {
 var _ jobs.JobHandler = (*handler)(nil)
 
 type HandlerParams struct {
-        fx.In
+	fx.In
 
 	V         *validator.Validate
 	APIClient nuonrunner.Client
@@ -36,4 +39,8 @@ func New(params HandlerParams) (*handler, error) {
 	return &handler{
 		v: params.V,
 	}, nil
+}
+
+func (h *handler) GracefulShutdown(ctx context.Context, job *models.AppRunnerJob, l *zap.Logger) error {
+	return nil
 }
---
+++ bins/runner/internal/jobs/deploy/terraform/graceful_shutdown.go
@@ -0,0 +1,38 @@
+package terraform
+
+import (
+	"context"
+
+	"github.com/nuonco/nuon-runner-go/models"
+	"github.com/powertoolsdev/mono/bins/runner/internal/pkg/log"
+	"go.uber.org/zap"
+)
+
+func (p *handler) GracefulShutdown(ctx context.Context, job *models.AppRunnerJob, l *zap.Logger) error {
+	hlog := log.NewHClog(l)
+	if p.state == nil {
+		return nil
+	}
+
+	if p.state.tfWorkspace != nil {
+		l.Info("attempting to update terraform state before shutdown")
+		err := p.updateTerraformState(ctx, p.state.tfWorkspace, hlog)
+		if err != nil {
+			p.writeErrorResult(ctx, "update terraform state", err)
+			// we don't return an error here because we want to allow the graceful shutdown to complete even when state update fails
+		}
+		l.Info("terraform state updated during  graceful shutdown")
+	}
+
+	if p.state.plan != nil && p.state.plan.TerraformDeployPlan != nil {
+		l.Info("attempting to unlock terraform workspace before shutdown")
+		err := p.apiClient.UnlockTerraformWorkspace(ctx, p.state.plan.TerraformDeployPlan.TerraformBackend.WorkspaceID)
+		if err != nil {
+			p.writeErrorResult(ctx, "unlock terraform workspace", err)
+			// we don't return an error here because we want to allow the graceful shutdown to complete even when unlock fails
+		}
+		l.Info("terraform workspace unlocked during graceful shutdown")
+	}
+
+	return nil
+}
