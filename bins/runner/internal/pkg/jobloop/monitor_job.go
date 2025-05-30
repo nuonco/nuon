4822cd640e4ee1e46b9d68ff8a9f32c5c5bc1e6a
Harsh Thakur <harsh@nuon.co>
2025-05-30 19:39:34 +0530

    feat: introduce job graceful shutdown primtive (#9200)
    * introduce job graceful shutdowns
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    * implement terraform sandbox graceful shutdown
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    * update component deploy with graceful shutdowns for terraform runs
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    ---------
    
    Signed-off-by: harsh <harsh@nuon.co>
    Co-authored-by: nuonbot <bot@nuon.co>

--- bins/runner/internal/pkg/jobloop/monitor_job.go
+++ bins/runner/internal/pkg/jobloop/monitor_job.go
@@ -5,10 +5,11 @@ import (
 	"time"
 
 	"github.com/nuonco/nuon-runner-go/models"
+	"github.com/powertoolsdev/mono/bins/runner/internal/jobs"
 	"go.uber.org/zap"
 )
 
-func (j *jobLoop) monitorJob(ctx context.Context, cancel func(), doneCh chan struct{}, jobID string, l *zap.Logger) {
+func (j *jobLoop) monitorJob(ctx context.Context, cancel func(), doneCh chan struct{}, jobID string, l *zap.Logger, jh jobs.JobHandler) {
 	ticker := time.NewTicker(time.Second)
 	defer ticker.Stop()
 	for {
@@ -26,22 +27,29 @@ func (j *jobLoop) monitorJob(ctx context.Context, cancel func(), doneCh chan str
 			continue
 		}
 
-		if job.Status == models.AppRunnerJobStatusCancelled {
-			l.Error("job was cancelled via API, attempting to cancel execution")
-			cancel()
-			return
-		}
-
-		if job.Status == models.AppRunnerJobStatusTimedDashOut {
-			l.Error("job was timed out via API, attempting to cancel execution")
-			cancel()
-			return
-		}
-
-		if job.Status == models.AppRunnerJobStatusFailed {
-			l.Error("job was failed via API, attempting to cancel execution")
-			cancel()
+		if j.handleJobStatus(ctx, job, jh, cancel, l) {
 			return
 		}
 	}
+}
+
+func (j *jobLoop) handleJobStatus(ctx context.Context, job *models.AppRunnerJob, jh jobs.JobHandler, cancel func(), l *zap.Logger) bool {
+	switch job.Status {
+	case models.AppRunnerJobStatusCancelled:
+		l.Error("job was cancelled via API, attempting to cancel execution")
+	case models.AppRunnerJobStatusTimedDashOut:
+		l.Error("job was timed out via API, attempting to cancel execution")
+	case models.AppRunnerJobStatusFailed:
+		l.Error("job was failed via API, attempting to cancel execution")
+	default:
+		return false
+	}
+
+	l.Info("attempting graceful shutdown")
+	err := jh.GracefulShutdown(ctx, job, l)
+	if err != nil {
+		l.Error("unable to gracefully shutdown job", zap.Error(err))
+	}
+	cancel()
+	return true
 }
