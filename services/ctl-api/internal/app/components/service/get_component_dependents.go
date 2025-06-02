8eb5d331cc7b00a1eff9b9f3df07f1f65edf600a
Somesh Koli <somesh@nuon.co>
2025-06-02 15:17:46 +0530

    feat: api for getting components dependent on particular component (#9215)
    * feat: add new api for getting all the components dependent on any given component id for a particular app id
    
    * fix: typo
    
    * fix: rename contract
    
    * fix: lint
    
    ---------
    
    Co-authored-by: nuonbot <bot@nuon.co>

---
+++ services/ctl-api/internal/app/components/service/get_component_dependents.go
@@ -0,0 +1,52 @@
+package service
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
+)
+
+type ComponentChildren struct {
+	Children []app.Component `json:"children"`
+}
+
+// @ID						GetComponentDependents
+// @Summary					get a component's children
+// @Description.markdown	get_component_dependents.md
+// @Param					component_id	path	string	true	"component ID"
+// @Tags					components
+// @Accept					json
+// @Produce					json
+// @Security				APIKey
+// @Security				OrgID
+// @Failure					400	{object}	stderr.ErrResponse
+// @Failure					401	{object}	stderr.ErrResponse
+// @Failure					403	{object}	stderr.ErrResponse
+// @Failure					404	{object}	stderr.ErrResponse
+// @Failure					500	{object}	stderr.ErrResponse
+// @Success					200	{object}	ComponentChildren
+// @Router					/v1/components/{component_id}/dependents [get]
+func (s *service) GetComponentDependents(ctx *gin.Context) {
+	componentID := ctx.Param("component_id")
+
+	component, err := s.getComponent(ctx, componentID)
+	if component == nil {
+		ctx.Error(fmt.Errorf("component %s not found", componentID))
+		return
+	}
+
+	appID := component.AppID
+	components, err := s.getAppComponents(ctx, appID)
+	if err != nil {
+		ctx.Error(fmt.Errorf("unable to get components for app: %s", appID))
+		return
+	}
+
+	cdo := s.appsHelpers.GetComponentsDependents(componentID, components)
+
+	ctx.JSON(http.StatusOK, ComponentChildren{
+		Children: cdo,
+	})
+}
