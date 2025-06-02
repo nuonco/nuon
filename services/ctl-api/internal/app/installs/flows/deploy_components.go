b553ca40d8a9451a1ddc9465922a82bcc6610237
sam boyer <sam@nuon.co>
2025-06-02 16:17:50 -0700

    feat: Abstract install workflows to be domain-agnostic (#9187)
    * Copy over and trivial refactors
    
    * WIP WIP
    
    * WIP many bits, including userflow name
    
    * Add userflows subpkg
    
    * Add pkg/userflow, and more bits
    
    * WIP - almost to just fixing type errors
    
    * Directly invoke step iterator
    
    * s/Userflow/Flow/g
    
    * it compiles
    
    * re-enable generate, whoops
    
    * Two-model approach, relying on dual writes and transforms back and forth
    
    * Get dual write working
    
    * Everything uses InstallWorkflow for now
    
    * Final bits for minimally functional

---
+++ services/ctl-api/internal/app/installs/flows/deploy_components.go
@@ -0,0 +1,45 @@
+package flows
+
+import (
+	"github.com/pkg/errors"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
+	"go.temporal.io/sdk/workflow"
+
+	"github.com/jackc/pgx/v5/pgtype"
+
+	"github.com/powertoolsdev/mono/pkg/generics"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app/installs/signals"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app/installs/worker/activities"
+)
+
+func (w *Flows) DeployAllComponents(ctx workflow.Context, flw *app.Flow) ([]*app.FlowStep, error) {
+	installID := generics.FromPtrStr(flw.Metadata["install_id"])
+	install, err := activities.AwaitGetByInstallID(ctx, installID)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to get install")
+	}
+
+	componentIDs, err := activities.AwaitGetAppGraph(ctx, activities.GetAppGraphRequest{
+		InstallID: install.ID,
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to get install graph")
+	}
+
+	steps := make([]*app.FlowStep, 0)
+	step, err := w.installSignalStep(ctx, installID, "await runner healthy", pgtype.Hstore{}, &signals.Signal{
+		Type: signals.OperationAwaitRunnerHealthy,
+	})
+	if err != nil {
+		return nil, err
+	}
+	steps = append(steps, step)
+
+	deploySteps, err := w.getComponentDeploySteps(ctx, installID, flw, componentIDs)
+	if err != nil {
+		return nil, err
+	}
+
+	steps = append(steps, deploySteps...)
+	return steps, nil
+}
---
+++ services/ctl-api/internal/app/installs/flows/deprovision_sandbox.go
@@ -0,0 +1,33 @@
+package flows
+
+import (
+	"github.com/jackc/pgx/v5/pgtype"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
+	"go.temporal.io/sdk/workflow"
+
+	"github.com/powertoolsdev/mono/pkg/generics"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app/installs/signals"
+)
+
+func (w *Flows) DeprovisionSandbox(ctx workflow.Context, flw *app.Flow) ([]*app.FlowStep, error) {
+	installID := generics.FromPtrStr(flw.Metadata["install_id"])
+	steps := make([]*app.FlowStep, 0)
+
+	step, err := w.installSignalStep(ctx, installID, "await runner healthy", pgtype.Hstore{}, &signals.Signal{
+		Type: signals.OperationAwaitRunnerHealthy,
+	})
+	if err != nil {
+		return nil, err
+	}
+	steps = append(steps, step)
+
+	step, err = w.installSignalStep(ctx, installID, "deprovision sandbox", pgtype.Hstore{}, &signals.Signal{
+		Type: signals.OperationDeprovisionSandbox,
+	})
+	if err != nil {
+		return nil, err
+	}
+	steps = append(steps, step)
+
+	return steps, nil
+}
