14648a7f7d182e700aa63c1ea589a76030ab458c
Dat Boi Diego <fidiego@users.noreply.github.com>
2025-06-01 23:54:29 -0500

    chore: default to prod values (#9229)

--- services/dashboard-ui/next.config.mjs
+++ services/dashboard-ui/next.config.mjs
@@ -16,11 +16,11 @@ const nextConfig = {
     return [
       {
         source: '/admin/temporal/:path*',
-        destination: `${process.env.NUON_TEMPORAL_UI_URL || 'http://localhost:8234'}/admin/temporal/:path*`,
+        destination: `${process.env.NUON_TEMPORAL_UI_URL || 'http://temporal-web.temporal.svc.cluster.local:8080'}/admin/temporal/:path*`,
       },
       {
         source: '/admin/swagger/docs/:path*',
-        destination: `${process.env.NUON_CTL_API_ADMIN_URL || 'http://localhost:8082'}/docs/:path*`,
+        destination: `${process.env.NUON_CTL_API_ADMIN_URL || 'http://ctl-api-admin.ctl-api.svc.cluster.local:8082'}/docs/:path*`,
       },
     ]
   },
