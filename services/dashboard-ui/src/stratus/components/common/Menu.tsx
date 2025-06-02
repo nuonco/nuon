8808a3f2266a718cebad61d130331d7725709ccf
Nat Hamilton <info@nathamilton.com>
2025-06-02 18:14:51 -0400

    feat(ui): new stratus dropdowns (#9236)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/stratus/components/common/Menu.tsx
+++ services/dashboard-ui/src/stratus/components/common/Menu.tsx
@@ -1,6 +1,7 @@
 import classNames from 'classnames'
 import React, { type FC } from 'react'
 import { Button, type IButton } from './Button'
+import { Dropdown, type IDropdown } from "./Dropdown"
 import { Link, type ILink } from './Link'
 import './Menu.css'
 
@@ -16,8 +17,8 @@ export const Menu: FC<IMenu> = ({ className, children, ...props }) => {
       {...props}
     >
       {React.Children.map(children, (c) =>
-        React.isValidElement(c) && (c.type === Button || c.type === Link)
-          ? React.cloneElement<IButton | ILink>(c, {
+        React.isValidElement(c) && (c.type === Button || c.type === Dropdown || c.type === Link)
+          ? React.cloneElement<IButton | IDropdown | ILink>(c, {
               variant: 'ghost',
             })
           : c
