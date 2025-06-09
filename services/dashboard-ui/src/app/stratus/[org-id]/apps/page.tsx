174f67a7f916d4f69c4cbde6dd4b527268831342
Nat Hamilton <info@nathamilton.com>
2025-06-09 13:26:22 -0400

    feat(ui): stratus page layout components (#9281)
    * feat(ui): stratus page layout components
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat(ui): stratus root redirect and dashbaord user name
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/app/stratus/[org-id]/apps/page.tsx
+++ services/dashboard-ui/src/app/stratus/[org-id]/apps/page.tsx
@@ -1,5 +1,50 @@
-const StratusApps = () => {
-  return <div>Apps page</div>
+import { type FC, Suspense } from 'react'
+import { ErrorBoundary } from 'react-error-boundary'
+import {
+  Header,
+  HeaderGroup,
+  Page,
+  Section,
+  ScrollableContent,
+  Text,
+} from '@/stratus/components'
+import type { IPageProps } from '@/types'
+
+const AppsPage: FC<IPageProps<'org-id'>> = ({ params }) => {
+  const orgId = params?.['org-id']
+
+  return (
+    <Page
+      breadcrumb={{
+        baseCrumbs: [
+          {
+            path: `/stratus/${orgId}`,
+            text: 'Home',
+          },
+          {
+            path: `/stratus/${orgId}/apps`,
+            text: 'Apps',
+          },
+        ],
+      }}
+    >
+      <ScrollableContent>
+        <Header>
+          <HeaderGroup>
+            <Text variant="h3" weight="strong" level={1}>
+              Installs
+            </Text>
+            <Text theme="muted">View your installs here.</Text>
+          </HeaderGroup>
+        </Header>
+        <Section className="divide-y">
+          <ErrorBoundary fallback="An error happened while loading installs">
+            <Suspense fallback={'Loading...'}>Apps: TKTKTK</Suspense>
+          </ErrorBoundary>
+        </Section>
+      </ScrollableContent>
+    </Page>
+  )
 }
 
-export default StratusApps
+export default AppsPage
