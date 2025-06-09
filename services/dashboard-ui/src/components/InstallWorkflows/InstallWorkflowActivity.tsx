7e87d47208a1225d9b76d85be83f42d67bccb42e
Nat Hamilton <info@nathamilton.com>
2025-06-09 15:07:37 -0400

    fix(ui): check for steps with approved status in step count (#9283)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/components/InstallWorkflows/InstallWorkflowActivity.tsx
+++ services/dashboard-ui/src/components/InstallWorkflows/InstallWorkflowActivity.tsx
@@ -50,7 +50,8 @@ export const InstallWorkflowActivity: FC<IInstallWorkflowActivity> = ({
                   (s) =>
                     s?.status?.status === 'success' ||
                     s?.status?.status === 'active' ||
-                    s?.status?.status === 'error'
+                    s?.status?.status === 'error' ||
+                    s?.status?.status === 'approved'
                 ).length
               }
             />
