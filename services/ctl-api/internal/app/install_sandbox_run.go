ef9edb1996f93b71cb92684c7d097fef12521939
Harsh Thakur <harsh@nuon.co>
2025-06-11 21:49:39 +0530

    refactor: composite status v2  (#9308)
    * fix build status v2
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    * refactor to use status activities
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    * fix bugs
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    ---------
    
    Signed-off-by: harsh <harsh@nuon.co>
    Co-authored-by: nuonbot <bot@nuon.co>

--- services/ctl-api/internal/app/install_sandbox_run.go
+++ services/ctl-api/internal/app/install_sandbox_run.go
@@ -98,3 +98,10 @@ func (i *InstallSandboxRun) Views(db *gorm.DB) []migrations.View {
 		},
 	}
 }
+
+func (i *InstallSandboxRun) AfterQuery(tx *gorm.DB) error {
+	i.Status = SandboxRunStatus(i.StatusV2.Status)
+	i.StatusDescription = i.StatusV2.StatusHumanDescription
+
+	return nil
+}
--- services/ctl-api/internal/pkg/workflows/status/activities/update.go
+++ services/ctl-api/internal/pkg/workflows/status/activities/update.go
@@ -2,9 +2,11 @@ package statusactivities
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/pkg/errors"
+	"gorm.io/gorm"
 
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/pkg/cctx"
@@ -83,6 +85,14 @@ func (a *Activities) PkgStatusUpdateInstallStackVersionStatus(ctx context.Contex
 }
 
 func (a *Activities) updateStatus(ctx context.Context, obj any, status app.CompositeStatus, statusGetter func(ctx context.Context) (app.CompositeStatus, error)) error {
+	return a.updateStatusCommon(ctx, obj, status, statusGetter, "status")
+}
+
+func (a *Activities) updateStatusV2(ctx context.Context, obj any, status app.CompositeStatus, statusGetter func(ctx context.Context) (app.CompositeStatus, error)) error {
+	return a.updateStatusCommon(ctx, obj, status, statusGetter, "status_v2")
+}
+
+func (a *Activities) updateStatusCommon(ctx context.Context, obj any, status app.CompositeStatus, statusGetter func(ctx context.Context) (app.CompositeStatus, error), statusField string) error {
 	createdBy, err := cctx.AccountIDFromContext(ctx)
 	if err != nil {
 		return errors.Wrap(err, "unable to get created by")
@@ -112,7 +122,7 @@ func (a *Activities) updateStatus(ctx context.Context, obj any, status app.Compo
 
 	res := a.db.WithContext(ctx).Model(obj).Updates(
 		map[string]any{
-			"status": status,
+			statusField: status,
 		})
 	if res.Error != nil {
 		return errors.Wrap(res.Error, "unable to update")
@@ -157,4 +167,184 @@ func (a *Activities) PkgStatusUpdateFlowStepStatus(ctx context.Context, req Upda
 	}
 
 	return a.updateStatus(ctx, &obj, req.Status, getter)
+}
+
+type UpdateBuildStatusV2 struct {
+	BuildID           string                   `validate:"required"`
+	Status            app.ComponentBuildStatus `validate:"required"`
+	StatusDescription string                   `validate:"required"`
+}
+
+// @temporal-gen activity
+func (a *Activities) UpdateBuildStatusV2(ctx context.Context, req UpdateBuildStatusV2) error {
+	obj := app.ComponentBuild{
+		ID: req.BuildID,
+	}
+
+	getter := func(ctx context.Context) (app.CompositeStatus, error) {
+		var obj app.ComponentBuild
+		if err := a.getStatus(ctx, &obj, req.BuildID); err != nil {
+			return app.CompositeStatus{}, err
+		}
+
+		return obj.StatusV2, nil
+	}
+
+	status := app.NewCompositeStatus(ctx, app.Status(req.Status))
+	status.StatusHumanDescription = req.StatusDescription
+	return a.updateStatusV2(ctx, &obj, status, getter)
+}
+
+type UpdateInstallWorkflowRunStatusV2Request struct {
+	RunID             string                             `validate:"required"`
+	Status            app.InstallActionWorkflowRunStatus `validate:"required"`
+	StatusDescription string                             `validate:"required"`
+}
+
+// @temporal-gen activity
+// @by-id RunID
+func (a *Activities) UpdateInstallWorkflowRunStatusV2(ctx context.Context, req UpdateInstallWorkflowRunStatusV2Request) error {
+	install := app.InstallActionWorkflowRun{
+		ID: req.RunID,
+	}
+
+	getter := func(ctx context.Context) (app.CompositeStatus, error) {
+		var install app.InstallActionWorkflowRun
+		if err := a.getStatus(ctx, &install, req.RunID); err != nil {
+			return app.CompositeStatus{}, err
+		}
+		return install.StatusV2, nil
+	}
+
+	status := app.NewCompositeStatus(ctx, app.Status(req.Status))
+	status.StatusHumanDescription = req.StatusDescription
+	return a.updateStatusV2(ctx, &install, status, getter)
+}
+
+type UpdateRunStatusV2Request struct {
+	RunID             string               `validate:"required"`
+	Status            app.SandboxRunStatus `validate:"required"`
+	StatusDescription string               `validate:"required"`
+	SkipStatusSync    bool
+}
+
+// @temporal-gen activity
+func (a *Activities) UpdateRunStatusV2(ctx context.Context, req UpdateRunStatusV2Request) error {
+	install := app.InstallSandboxRun{
+		ID: req.RunID,
+	}
+
+	getter := func(ctx context.Context) (app.CompositeStatus, error) {
+		var install app.InstallSandboxRun
+		if err := a.getStatus(ctx, &install, req.RunID); err != nil {
+			return app.CompositeStatus{}, err
+		}
+		return install.StatusV2, nil
+	}
+
+	compStatus := app.NewCompositeStatus(ctx, app.Status(req.Status))
+	compStatus.StatusHumanDescription = req.StatusDescription
+
+	err := a.updateStatusV2(ctx, &install, compStatus, getter)
+	if err != nil {
+		return fmt.Errorf("unable to update run status: %w", err)
+	}
+
+	// update InstallSandbox
+	if !req.SkipStatusSync {
+		run := app.InstallSandboxRun{}
+		res := a.db.WithContext(ctx).
+			Where("id = ?", req.RunID).
+			First(&run)
+		if res.Error != nil && res.Error != gorm.ErrRecordNotFound {
+			return fmt.Errorf("unable to get install deploy: %w", res.Error)
+		}
+
+		if res.Error == gorm.ErrRecordNotFound {
+			return nil
+		}
+
+		if run.InstallSandboxID == nil {
+			return nil
+		}
+
+		installSandbox := app.InstallSandbox{
+			ID: *run.InstallSandboxID,
+		}
+
+		getter := func(ctx context.Context) (app.CompositeStatus, error) {
+			var installSandbox app.InstallSandbox
+			if err := a.getStatus(ctx, &installSandbox, *run.InstallSandboxID); err != nil {
+				return app.CompositeStatus{}, err
+			}
+			return installSandbox.StatusV2, nil
+		}
+
+		err = a.updateStatusV2(ctx, &installSandbox, compStatus, getter)
+		if err != nil {
+			return fmt.Errorf("unable to update install sandbox: %w", err)
+		}
+	}
+	return nil
+}
+
+type UpdateDeployStatusV2Request struct {
+	DeployID          string     `validate:"required"`
+	Status            app.Status `validate:"required"`
+	StatusDescription string     `validate:"required"`
+	SkipStatusSync    bool
+}
+
+// @temporal-gen activity
+func (a *Activities) UpdateDeployStatusV2(ctx context.Context, req UpdateDeployStatusV2Request) error {
+	installDeploy := app.InstallDeploy{
+		ID: req.DeployID,
+	}
+
+	getter := func(ctx context.Context) (app.CompositeStatus, error) {
+		var installDeploy app.InstallDeploy
+		if err := a.getStatus(ctx, &installDeploy, req.DeployID); err != nil {
+			return app.CompositeStatus{}, err
+		}
+		return installDeploy.StatusV2, nil
+	}
+
+	compositeStatus := app.NewCompositeStatus(ctx, req.Status)
+	compositeStatus.StatusHumanDescription = req.StatusDescription
+
+	err := a.updateStatusV2(ctx, &installDeploy, compositeStatus, getter)
+	if err != nil {
+		return fmt.Errorf("unable to update install deploy: %w", err)
+	}
+	if req.SkipStatusSync {
+		return nil
+	}
+
+	extantInstallDeploy := app.InstallDeploy{}
+	res := a.db.WithContext(ctx).
+		Preload("InstallComponent").
+		Where("id = ?", req.DeployID).
+		First(&extantInstallDeploy)
+	if res.Error != nil {
+		return fmt.Errorf("unable to get install deploy: %w", res.Error)
+	}
+
+	compositeStatus = app.NewCompositeStatus(ctx, req.Status)
+	compositeStatus.StatusHumanDescription = req.StatusDescription
+	installComponent := app.InstallComponent{
+		ID: extantInstallDeploy.InstallComponent.ID,
+	}
+
+	getInstallComponentStatus := func(ctx context.Context) (app.CompositeStatus, error) {
+		if err := a.getStatus(ctx, &installComponent, installComponent.ID); err != nil {
+			return app.CompositeStatus{}, err
+		}
+		return installComponent.StatusV2, nil
+	}
+
+	if err := a.updateStatusV2(ctx, &installComponent, compositeStatus, getInstallComponentStatus); err != nil {
+		return fmt.Errorf("unable to update install component: %w", err)
+	}
+
+	return nil
 }
