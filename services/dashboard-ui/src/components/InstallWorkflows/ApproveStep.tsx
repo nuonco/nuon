a9d871c83a56eb12b4f56430f7009632aabbba2a
Nat Hamilton <info@nathamilton.com>
2025-06-11 11:28:45 -0400

    refactor(ui): show diffs after approved (#9306)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/components/InstallWorkflows/ApproveStep.tsx
+++ services/dashboard-ui/src/components/InstallWorkflows/ApproveStep.tsx
@@ -6,7 +6,6 @@ import { X, Check } from '@phosphor-icons/react'
 import { Button } from '@/components/Button'
 import { SpinnerSVG } from '@/components/Loading'
 import { Notice } from '@/components/Notice'
-import { CodeViewer, JsonView } from '@/components/Code'
 import { Text } from '@/components/Typography'
 import { approveWorkflowStep } from '@/components/install-actions'
 import { CodeEditor, DiffEditor, splitYamlDiff } from '@/stratus/components/'
@@ -56,15 +55,26 @@ export const ApprovalStep: FC<IApprovalStep> = ({
 
   return (
     <>
-      <Notice className="!p-4 w-full" variant="warn">
-        <Text variant="med-14" className="mb-2">
-          Action needed: {removeSnakeCase(approval?.type)}
-        </Text>
-        <Text isMuted>
-          Approve or deny these changes included in this{' '}
-          {removeSnakeCase(approval?.type)}.
-        </Text>
-      </Notice>
+      {approval?.response ? (
+        <Notice className="!p-4 w-full" variant="success">
+          <Text variant="med-14" className="mb-2">
+            Step approved: {removeSnakeCase(approval?.type)}
+          </Text>
+          <Text isMuted>
+            These {removeSnakeCase(approval?.type)} changes have been approved.
+          </Text>
+        </Notice>
+      ) : (
+        <Notice className="!p-4 w-full" variant="warn">
+          <Text variant="med-14" className="mb-2">
+            Action needed: {removeSnakeCase(approval?.type)}
+          </Text>
+          <Text isMuted>
+            Approve or deny these changes included in this{' '}
+            {removeSnakeCase(approval?.type)}.
+          </Text>
+        </Notice>
+      )}
       <div className="flex flex-col gap-2 !w-full">
         <div className="flex flex-col gap-4 border rounded-md p-2">
           {error ? <Notice>{error}</Notice> : null}
@@ -74,50 +84,52 @@ export const ApprovalStep: FC<IApprovalStep> = ({
             <CodeEditor language="json" defaultValue={approval?.contents} />
           )}
         </div>
-        <div className="mt-4 flex gap-3 justify-end">
-          <Button
-            onClick={() => {
-              setIsDenyLoading(true)
-              approve('deny')
-            }}
-            className="text-sm font-sans flex items-center gap-2 h-[32px]"
-            disabled={isKickedOff}
-          >
-            {isDenyLoading ? (
-              <>
-                <SpinnerSVG />
-                Denying plan
-              </>
-            ) : (
-              <>
-                <X />
-                Deny plan
-              </>
-            )}
-          </Button>
+        {approval?.response ? null : (
+          <div className="mt-4 flex gap-3 justify-end">
+            <Button
+              onClick={() => {
+                setIsDenyLoading(true)
+                approve('deny')
+              }}
+              className="text-sm font-sans flex items-center gap-2 h-[32px]"
+              disabled={isKickedOff}
+            >
+              {isDenyLoading ? (
+                <>
+                  <SpinnerSVG />
+                  Denying plan
+                </>
+              ) : (
+                <>
+                  <X />
+                  Deny plan
+                </>
+              )}
+            </Button>
 
-          <Button
-            onClick={() => {
-              setIsApproveLoading(true)
-              approve('approve')
-            }}
-            className="text-sm font-sans flex items-center gap-2 h-[32px] !px-2"
-            disabled={isKickedOff}
-            variant="primary"
-          >
-            {isApproveLoading ? (
-              <>
-                <SpinnerSVG />
-                Approving plan
-              </>
-            ) : (
-              <>
-                <Check />
-                Approve plan
-              </>
-            )}
-          </Button>
-        </div>
+            <Button
+              onClick={() => {
+                setIsApproveLoading(true)
+                approve('approve')
+              }}
+              className="text-sm font-sans flex items-center gap-2 h-[32px] !px-2"
+              disabled={isKickedOff}
+              variant="primary"
+            >
+              {isApproveLoading ? (
+                <>
+                  <SpinnerSVG />
+                  Approving plan
+                </>
+              ) : (
+                <>
+                  <Check />
+                  Approve plan
+                </>
+              )}
+            </Button>
+          </div>
+        )}
       </div>
     </>
   )
