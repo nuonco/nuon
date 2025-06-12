a4bec75b618e906688bf08ae2853c76cfb589e8f
Nat Hamilton <info@nathamilton.com>
2025-06-12 08:54:06 -0400

    feat(ui): improved tooltips, back link & transition div (#9317)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/app/stratus/[org-id]/installs/[install-id]/template.tsx
+++ services/dashboard-ui/src/app/stratus/[org-id]/installs/[install-id]/template.tsx
@@ -2,7 +2,6 @@
 
 import { usePathname } from 'next/navigation'
 import {
-  CheckCircle,
   CalendarDots,
   HouseSimple,
   ShippingContainer,
@@ -11,21 +10,7 @@ import {
   Stack,
   TerminalWindow,
 } from '@phosphor-icons/react/dist/ssr'
-import {
-  Button,
-  Badge,
-  Header,
-  HeaderGroup,
-  InstallHeader,
-  Link,
-  PageLayout,
-  Page,
-  PageNav,
-  Status,
-  Tooltip,
-  Text,
-  Time,
-} from '@/stratus/components'
+import { InstallHeader, PageLayout, Page, PageNav } from '@/stratus/components'
 import { useInstall, useOrg } from '@/stratus/context'
 
 export default function Template({ children }: { children: React.ReactNode }) {
@@ -34,7 +19,7 @@ export default function Template({ children }: { children: React.ReactNode }) {
   const { install } = useInstall()
   const isThirdLevel = pathName.split('/').length > 6
 
-  return isThirdLevel ? (
+  return (
     <Page
       breadcrumb={{
         baseCrumbs: [
@@ -53,71 +38,57 @@ export default function Template({ children }: { children: React.ReactNode }) {
         ],
       }}
     >
-      {children}
-    </Page>
-  ) : (
-    <Page
-      breadcrumb={{
-        baseCrumbs: [
-          {
-            path: `/stratus/${org?.id}`,
-            text: 'Home',
-          },
-          {
-            path: `/stratus/${org?.id}/installs`,
-            text: 'Installs',
-          },
-          {
-            path: `/stratus/${org?.id}/installs/${install?.id}`,
-            text: install?.name || 'Install',
-          },
-        ],
-      }}
-    >
-      <InstallHeader />
-      <PageLayout>
-        <PageNav
-          basePath={`/stratus/${org?.id}/installs/${install?.id}`}
-          links={[
-            {
-              path: `/`,
-              icon: <HouseSimple />,
-              text: 'Overview',
-            },
-            {
-              path: `/runner`,
-              icon: <SneakerMove />,
-              text: 'Runner',
-            },
-            {
-              path: `/sandbox`,
-              icon: <ShippingContainer />,
-              text: 'Sandbox',
-            },
-            {
-              path: `/stacks`,
-              icon: <Stack />,
-              text: 'Stacks',
-            },
-            {
-              path: `/components`,
-              icon: <Cards />,
-              text: 'Components',
-            },
-            {
-              path: `/actions`,
-              icon: <TerminalWindow />,
-              text: 'Actions',
-            },
-            {
-              path: `/history`,
-              icon: <CalendarDots />,
-              text: 'History',
-            },
-          ]}
-        />
-        {children}
-      </PageLayout>
+      {isThirdLevel ? (
+        children
+      ) : (
+        <>
+          {' '}
+          <InstallHeader />
+          <PageLayout>
+            <PageNav
+              basePath={`/stratus/${org?.id}/installs/${install?.id}`}
+              links={[
+                {
+                  path: `/`,
+                  icon: <HouseSimple />,
+                  text: 'Overview',
+                },
+                {
+                  path: `/runner`,
+                  icon: <SneakerMove />,
+                  text: 'Runner',
+                },
+                {
+                  path: `/sandbox`,
+                  icon: <ShippingContainer />,
+                  text: 'Sandbox',
+                },
+                {
+                  path: `/stacks`,
+                  icon: <Stack />,
+                  text: 'Stacks',
+                },
+                {
+                  path: `/components`,
+                  icon: <Cards />,
+                  text: 'Components',
+                },
+                {
+                  path: `/actions`,
+                  icon: <TerminalWindow />,
+                  text: 'Actions',
+                },
+                {
+                  path: `/history`,
+                  icon: <CalendarDots />,
+                  text: 'History',
+                },
+              ]}
+            />
+            {children}
+          </PageLayout>
+        </>
+      )}
     </Page>
   )
 }
