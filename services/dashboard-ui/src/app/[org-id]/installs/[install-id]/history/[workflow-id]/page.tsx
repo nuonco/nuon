ca3f6b4c0bf3831b9552fc468e8467cf9621d149
Nat Hamilton <info@nathamilton.com>
2025-06-13 13:19:30 -0400

    feat(ui): approve all install toggle and workflow steps (#9327)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/app/[org-id]/installs/[install-id]/history/[workflow-id]/page.tsx
+++ services/dashboard-ui/src/app/[org-id]/installs/[install-id]/history/[workflow-id]/page.tsx
@@ -6,7 +6,9 @@ import {
   InstallWorkflowActivity,
   InstallWorkflowSteps,
   InstallWorkflowCancelModal,
+  WorkflowApproveAllModal,
   YAStatus,
+  Text,
 } from '@/components'
 import { getInstall, getInstallWorkflow } from '@/lib'
 
@@ -65,16 +67,79 @@ export default withPageAuthRequired(async function InstallWorkflow({ params }) {
         </span>
       }
       headingUnderline={install?.id}
+      meta={
+        <div className="pb-6 flex flex-col gap-4">
+          <div className="flex gap-8">
+            <div className="flex flex-col gap-1">
+              <Text variant="reg-12" isMuted>
+                Pending approvals
+              </Text>
+              <Text variant="med-18">
+                {
+                  installWorkflow?.steps?.filter(
+                    (s) =>
+                      s?.execution_type === 'approval' && !s?.approval?.response
+                  )?.length
+                }
+              </Text>
+            </div>
+
+            <div className="flex flex-col gap-1">
+              <Text variant="reg-12" isMuted>
+                Total steps
+              </Text>
+              <Text variant="med-18">{installWorkflow?.steps?.length}</Text>
+            </div>
+
+            <div className="flex flex-col gap-1">
+              <Text variant="reg-12" isMuted>
+                Completed steps
+              </Text>
+              <Text variant="med-18">
+                {
+                  installWorkflow?.steps?.filter(
+                    (s) =>
+                      s?.status?.status === 'success' ||
+                      s?.status?.status === 'active' ||
+                      s?.status?.status === 'error' ||
+                      s?.status?.status === 'approved'
+                  )?.length
+                }
+              </Text>
+            </div>
+          </div>
+          {installWorkflow?.steps?.some(
+            (s) => s?.execution_type === 'approval'
+          ) ? (
+            <div className="flex flex-col gap-3">
+              {installWorkflow?.approval_option === 'prompt' &&
+              !installWorkflow?.finished ? (
+                <>
+                  <Text>
+                    Automatically approve all changes waiting for approval
+                  </Text>
+                  <WorkflowApproveAllModal workflow={installWorkflow} />
+                </>
+              ) : (
+                <Text className="text-green-600 dark:text-green-400">
+                  All changes have been approved
+                </Text>
+              )}
+            </div>
+          ) : null}
+        </div>
+      }
       statues={
-        <div className="flex gap-8 items-center">
+        <div className="flex flex-col gap-3 items-end">
+          {!installWorkflow?.finished && installWorkflow?.steps?.length > 0 ? (
+            <InstallWorkflowCancelModal installWorkflow={installWorkflow} />
+          ) : null}
+
           <InstallWorkflowActivity
             installWorkflow={installWorkflow}
             shouldPoll
             pollDuration={3000}
           />
-          {!installWorkflow?.finished && installWorkflow?.steps?.length > 0 ? (
-            <InstallWorkflowCancelModal installWorkflow={installWorkflow} />
-          ) : null}
         </div>
       }
     >
