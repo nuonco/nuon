2ae97455e258b4292560599f1dd6898dc31e68dc
Jon Morehouse <MorehouseJ09@gmail.com>
2025-06-14 15:07:45 -0700

    feat: `nctl v2`, replace Earthly (#9300)
    * nuonctl: v2, CI support
    
    * chore: ci fixes
    
    * update
    
    * update
    
    * update
    
    * update
    
    * chore: update dockerfile
    
    * fix
    
    * chore: update
    
    * chore: artifacts base built out
    
    * chore: cli works
    
    * chore: update to support artifacts
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update triggers
    
    * update
    
    * chore: remove promotions
    
    * update
    
    * update
    
    * chore: update
    
    * update
    
    * update
    
    * chore: update
    
    * chore: update
    
    * update
    
    * fix
    
    * update
    
    * update
    
    * update
    
    * update

--- pkg/aws/assume-role/assume.go
+++ pkg/aws/assume-role/assume.go
@@ -9,6 +9,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/sts"
 	sts_types "github.com/aws/aws-sdk-go-v2/service/sts/types"
+	"github.com/pkg/errors"
 
 	"github.com/powertoolsdev/mono/pkg/generics"
 )
@@ -40,18 +41,34 @@ func (a *assumer) LoadConfigWithAssumedRole(ctx context.Context) (aws.Config, er
 	return cfg, nil
 }
 
-type awsClientIamRoleAssumer interface {
-	AssumeRole(ctx context.Context,
-		params *sts.AssumeRoleInput,
-		optFns ...func(*sts.Options)) (*sts.AssumeRoleOutput, error)
-}
+func (a *assumer) assumeIamRole(ctx context.Context, client *sts.Client, role string) (*sts_types.Credentials, error) {
+	if a.UseGithubOIDC {
+		token, err := a.getGithubOIDCToken(ctx)
+		if err != nil {
+			return nil, errors.Wrap(err, "unable to get token")
+		}
 
-func (a *assumer) assumeIamRole(ctx context.Context, client awsClientIamRoleAssumer, role string) (*sts_types.Credentials, error) {
+		params := &sts.AssumeRoleWithWebIdentityInput{
+			RoleArn:          generics.ToPtr(role),
+			RoleSessionName:  generics.ToPtr(a.RoleSessionName),
+			WebIdentityToken: generics.ToPtr(token),
+			DurationSeconds:  generics.ToPtr(int32(a.RoleSessionDuration.Seconds())),
+		}
+
+		resp, err := client.AssumeRoleWithWebIdentity(ctx, params)
+		if err != nil {
+			return nil, err
+		}
+
+		return resp.Credentials, nil
+	}
+
 	params := &sts.AssumeRoleInput{
 		RoleArn:         &role,
 		RoleSessionName: &a.RoleSessionName,
 		DurationSeconds: generics.ToPtr(int32(a.RoleSessionDuration.Seconds())),
 	}
+
 	resp, err := client.AssumeRole(ctx, params)
 	if err != nil {
 		return nil, err
