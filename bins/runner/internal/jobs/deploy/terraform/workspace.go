f274c1db05fef4f09fced5eaa2117506aa43bb16
Dat Boi Diego <fidiego@users.noreply.github.com>
2025-06-16 13:03:42 -0500

    feat: approvals in the runner (#9270)
    * chore: nctl: do not override if empty
    
    * feat: approvals for sandbox runs

--- bins/runner/internal/jobs/deploy/terraform/workspace.go
+++ bins/runner/internal/jobs/deploy/terraform/workspace.go
@@ -82,3 +82,71 @@ func (p *handler) GetWorkspace(ctx context.Context) (workspace.Workspace, error)
 
 	return wkspace, nil
 }
+
+// GetWorkspace returns a valid workspace for working with this plugin
+func (p *handler) GetWorkspaceWithPlan(ctx context.Context, planBytes []byte) (workspace.Workspace, error) {
+	arch, err := dirarchive.New(p.v,
+		dirarchive.WithPath(p.state.arch.BasePath()),
+		dirarchive.WithAddBackendFile("http"),
+		dirarchive.WithIgnoreTerraformStateFile(),
+		dirarchive.WithIgnoreDotTerraformDir(),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("unable to create local archive: %w", err)
+	}
+
+	back, err := httpbackend.New(p.v, httpbackend.WithNuonTerraformWorkspaceConfig(&httpbackend.NuonWorkspaceConfig{
+		APIEndpoint: p.cfg.RunnerAPIURL,
+		WorkspaceID: p.state.plan.TerraformDeployPlan.TerraformBackend.WorkspaceID,
+		Token:       p.cfg.RunnerAPIToken,
+		JobID:       p.state.jobID,
+	}))
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to get http backend")
+	}
+
+	bin, err := remotebinary.New(p.v,
+		remotebinary.WithVersion(p.state.terraformCfg.Version))
+	if err != nil {
+		return nil, fmt.Errorf("unable to create binary: %w", err)
+	}
+
+	extraEnvVars := make(map[string]string, 0)
+	if p.state.plan.TerraformDeployPlan.ClusterInfo != nil {
+		extraEnvVars[config.DefaultKubeConfigEnvVar] = config.DefaultKubeConfigFilename
+	}
+
+	vars, err := staticvars.New(p.v,
+		staticvars.WithFileVars(p.state.plan.TerraformDeployPlan.Vars),
+		staticvars.WithFiles(p.state.plan.TerraformDeployPlan.VarsFiles),
+		staticvars.WithEnvVars(p.state.plan.TerraformDeployPlan.EnvVars),
+		staticvars.WithEnvVars(extraEnvVars),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("unable to create variable set: %w", err)
+	}
+
+	authVars, err := authvars.New(p.v,
+		authvars.WithAWSAuth(p.state.plan.TerraformDeployPlan.AWSAuth),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("unable to create auth vars: %w", err)
+	}
+
+	hooks := noop.New()
+
+	wkspace, err := workspace.New(p.v,
+		workspace.WithHooks(hooks),
+		workspace.WithArchive(arch),
+		workspace.WithBackend(back),
+		workspace.WithBinary(bin),
+		workspace.WithVariables(vars),
+		workspace.WithVariables(authVars),
+		workspace.WithPlanBytes(planBytes),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("unable to create workspace: %w", err)
+	}
+
+	return wkspace, nil
+}
--- pkg/terraform/workspace/binary.go
+++ pkg/terraform/workspace/binary.go
@@ -3,7 +3,9 @@ package workspace
 import (
 	"context"
 	"fmt"
+	"io"
 	"os"
+	"os/exec"
 	"path/filepath"
 
 	"github.com/hashicorp/go-hclog"
@@ -27,4 +29,51 @@ func (w *workspace) LoadBinary(ctx context.Context, log hclog.Logger) error {
 	w.execPath = execPath
 
 	return nil
+}
+
+func (w *workspace) loadLocalBinary(ctx context.Context) {
+	// Find the terraform executable path (equivalent to `which terraform`)
+	terraformPath, err := exec.LookPath("terraform")
+	if err != nil {
+		panic(err)
+	}
+
+	// Ensure the bins directory exists
+	err = os.MkdirAll(filepath.Join(w.root, "bins"), 0755)
+	if err != nil {
+		panic(err)
+	}
+
+	// Copy the file
+	err = copyFile(terraformPath, filepath.Join(w.root, "/bins/terraform"))
+	if err != nil {
+		panic(err)
+	}
+}
+
+func copyFile(src, dst string) error {
+	srcFile, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer srcFile.Close()
+
+	dstFile, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer dstFile.Close()
+
+	_, err = io.Copy(dstFile, srcFile)
+	if err != nil {
+		return err
+	}
+
+	// Copy permissions
+	srcInfo, err := srcFile.Stat()
+	if err != nil {
+		return err
+	}
+
+	return os.Chmod(dst, srcInfo.Mode())
 }
