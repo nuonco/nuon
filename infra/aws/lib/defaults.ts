fcc02272172d4e78838aeecb69d9be9a83ee964c
Harsh Thakur <harsh@nuon.co>
2025-06-19 14:34:38 +0530

    chore: add ci aws account (#9403)
    * add ci aws account
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    * update with synth run
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    * npm run upgrade
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    * Bump up aws deps
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    * npm run synth
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    * npm run synth!
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    ---------
    
    Signed-off-by: harsh <harsh@nuon.co>

--- infra/aws/lib/defaults.ts
+++ infra/aws/lib/defaults.ts
@@ -75,6 +75,7 @@ export const desiredOrgStructure: orgStructure = [
     accounts: [
       { name: "prod" },
       { name: "stage" },
+      { name: "ci" },
       { additionalEnvTags: ["stage"], name: "infra-shared-stage" },
       { additionalEnvTags: ["prod"], name: "infra-shared-prod" },
       { additionalEnvTags: ["stage"], name: "govcloud-stage" },
