e486575bd52822999224ec39b4c37cf134a4b1d8
Somesh Koli <somesh@nuon.co>
2025-06-23 20:45:56 +0530

    feat: excute components only which are affected by input change (#9328)
    * feat: install input change, only build components who's inputs are changed
    
    * fix: remove redundant changes
    
    * fix: change the way component details are fetched
    
    * feat: steps are now topologically ordered
    
    * fix: remove redundant changes

--- services/ctl-api/internal/app/installs/worker/activities/get_component_dependents.go
+++ services/ctl-api/internal/app/installs/worker/activities/get_component_dependents.go
@@ -2,54 +2,23 @@ package activities
 
 import (
 	"context"
-	"fmt"
-	"slices"
 
-	"github.com/dominikbraun/graph"
+	"github.com/pkg/errors"
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
 )
 
 type GetComponentDependents struct {
 	AppID           string `json:"app_id"`
 	ComponentRootID string `json:"component_root_id"`
+	ConfigVersion   int    `json:"config_version"`
 }
 
 // @temporal-gen activity
 func (a *Activities) GetComponentDependents(ctx context.Context, req GetComponentDependents) ([]app.Component, error) {
-	g, cmps, err := a.appsHelpers.GetDependencyGraph(ctx, req.AppID)
+	comps, err := a.appsHelpers.GetInvertedDependentByComponentConfigVersion(ctx, req.AppID, req.ConfigVersion, req.ComponentRootID)
 	if err != nil {
-		return nil, fmt.Errorf("unable to get graph: %w", err)
+		return nil, errors.Wrap(err, "unable to get dependent components")
 	}
 
-	cmpsById := make(map[string]app.Component)
-	for _, c := range cmps {
-		cmpsById[c.ID] = c
-	}
-
-	depsCmpIds := make([]string, 0)
-	if err := graph.BFS(g, req.ComponentRootID, func(compID string) bool {
-		// warn: g.Vertex(compID) is not always returning the correct component
-		if compID == req.ComponentRootID {
-			return false
-		}
-
-		if !slices.Contains(depsCmpIds, compID) {
-			depsCmpIds = append(depsCmpIds, compID)
-		}
-
-		return false
-	}); err != nil {
-		return nil, fmt.Errorf("unable to build app graph: %w", err)
-	}
-
-	depCmps := make([]app.Component, 0)
-	for _, id := range depsCmpIds {
-		comp, ok := cmpsById[id]
-		if !ok {
-			return nil, fmt.Errorf("unable to get component: %w", err)
-		}
-		depCmps = append(depCmps, comp)
-	}
-
-	return depCmps, nil
+	return comps, nil
 }
