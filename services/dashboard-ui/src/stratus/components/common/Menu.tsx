a2b490d5bbd82369157af4014995446251087f5e
Nat Hamilton <info@nathamilton.com>
2025-06-25 11:16:39 -0400

    refactor(ui): stratus Icon, className helper & removed FC types (#9480)
    * refactor(ui): add Icon & classNames helper, removed FC typing
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * refactor(ui): add Icon & classNames helper, removed FC typing
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/stratus/components/common/Menu.tsx
+++ services/dashboard-ui/src/stratus/components/common/Menu.tsx
@@ -1,23 +1,18 @@
-import classNames from 'classnames'
-import React, { type FC } from 'react'
+import React from 'react'
+import { cn } from '@/stratus/components/helpers'
 import { Button, type IButton } from './Button'
-import { Dropdown, type IDropdown } from "./Dropdown"
+import { Dropdown, type IDropdown } from './Dropdown'
 import { Link, type ILink } from './Link'
 import './Menu.css'
 
 interface IMenu extends Omit<React.HTMLAttributes<HTMLDivElement>, 'role'> {}
 
-export const Menu: FC<IMenu> = ({ className, children, ...props }) => {
+export const Menu = ({ className, children, ...props }: IMenu) => {
   return (
-    <div
-      className={classNames('menu', {
-        [`${className}`]: Boolean(className),
-      })}
-      role="menu"
-      {...props}
-    >
+    <div className={cn('menu', className)} role="menu" {...props}>
       {React.Children.map(children, (c) =>
-        React.isValidElement(c) && (c.type === Button || c.type === Dropdown || c.type === Link)
+        React.isValidElement(c) &&
+        (c.type === Button || c.type === Dropdown || c.type === Link)
           ? React.cloneElement<IButton | IDropdown | ILink>(c, {
               variant: 'ghost',
             })
