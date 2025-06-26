77cef4dffef882c3061dafa619ab1e79e8a6396d
Nat Hamilton <info@nathamilton.com>
2025-06-26 09:21:47 -0400

    refactor(ui): add region to install tables (#9486)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/components/Installs/CreateInstallModal.tsx
+++ services/dashboard-ui/src/components/Installs/CreateInstallModal.tsx
@@ -206,7 +206,6 @@ const CreateInstallFromApp: FC<{
             }}
             platform={app.cloud_platform}
             inputConfig={inputConfig}
-            cfLink={''}
           />
         </>
       )}
