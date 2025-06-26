70f6175a8f9a4c961e5524d80e88afdc7b4a4583
Nat Hamilton <info@nathamilton.com>
2025-06-26 12:42:14 -0400

    feat(ui): wip stratus datepicker, removed old deps, larger next mem buffer (#9491)
    feat(ui): wip datepicker, removed old deps, larger next mem buffer
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/next.config.mjs
+++ services/dashboard-ui/next.config.mjs
@@ -24,6 +24,12 @@ const nextConfig = {
       },
     ]
   },
+  onDemandEntries: {
+    // period (in ms) where the server will keep pages in the buffer
+    maxInactiveAge: 15 * 60 * 1000, // 15 minutes
+    // number of pages that should be kept simultaneously without being disposed
+    pagesBufferLength: 4,
+  },
 }
 
 export default nextConfig
