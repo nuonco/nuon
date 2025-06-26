9eb043fdbb399646cc37d11909f3d954b2ad964c
Nat Hamilton <info@nathamilton.com>
2025-06-26 12:42:14 -0400

    feat(ui): wip stratus datepicker, removed old deps, larger next mem buffer (#9491)
    feat(ui): wip datepicker, removed old deps, larger next mem buffer
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/tailwind.config.ts
+++ services/dashboard-ui/tailwind.config.ts
@@ -8,6 +8,7 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     './src/stratus/**/*.{js,ts,jsx,tsx,mdx}',
     './node_modules/react-tailwindcss-select/dist/index.esm.js',
+    './node_modules/react-tailwindcss-datepicker/dist/index.esm.{js,ts}',
   ],
   safelist: [
     {
