05195afa48d50c13575f0237ad5cb419d6b66179
Dat Boi Diego <fidiego@users.noreply.github.com>
2025-06-27 17:25:02 -0500

    chore: add css to matcher (#9508)

--- services/dashboard-ui/src/middleware.ts
+++ services/dashboard-ui/src/middleware.ts
@@ -93,6 +93,6 @@ export default async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|livez|readyz|\\.js$).*)',
+    '/((?!_next/static|_next/image|favicon.ico|livez|readyz|\\.js|\\.css$).*)',
   ],
 }
