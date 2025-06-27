9062b6adcdef88a7d2e36f6a79a3eaebcadea92f
Rob <alphavibe@gmail.com>
2025-06-27 09:15:49 -0700

    feat: app branches models (#9459)

---
+++ services/ctl-api/internal/app/app_branch.go
@@ -0,0 +1,42 @@
+package app
+
+import (
+	"time"
+
+	"github.com/powertoolsdev/mono/pkg/shortid/domains"
+	"gorm.io/gorm"
+	"gorm.io/plugin/soft_delete"
+)
+
+type AppBranch struct {
+	ID          string                `gorm:"primarykey;check:id_checker,char_length(id)=26" json:"id,omitzero" temporaljson:"id,omitzero,omitempty"`
+	CreatedByID string                `json:"created_by_id,omitzero" gorm:"not null;default:null" temporaljson:"created_by_id,omitzero,omitempty"`
+	CreatedBy   Account               `json:"-" temporaljson:"created_by,omitzero,omitempty"`
+	CreatedAt   time.Time             `json:"created_at,omitzero" temporaljson:"created_at,omitzero,omitempty"`
+	UpdatedAt   time.Time             `json:"updated_at,omitzero" temporaljson:"updated_at,omitzero,omitempty"`
+	DeletedAt   soft_delete.DeletedAt `json:"-" temporaljson:"deleted_at,omitzero,omitempty"`
+
+	OrgID string `json:"org_id,omitzero" gorm:"notnull;default null" temporaljson:"org_id,omitzero,omitempty"`
+	Org   Org    `faker:"-" json:"-" temporaljson:"org,omitzero,omitempty"`
+
+	AppID string `json:"app_id,omitzero" gorm:"notnull;index:idx_app_app_branch" temporaljson:"app_id,omitzero,omitempty"`
+	App   App    `faker:"-" json:"-" temporaljson:"app,omitzero,omitempty"`
+
+	VCSConnectionBranchID string              `json:"vcs_connection_branch_id,omitzero" gorm:"notnull;unique" temporaljson:"vcs_connection_branch_id,omitzero,omitempty"`
+	VCSConnectionBranch   VCSConnectionBranch `json:"vcs_connection_branch,omitzero" temporaljson:"vcs_connection_branch,omitzero,omitempty"`
+}
+
+func (a *AppBranch) BeforeCreate(tx *gorm.DB) error {
+	if a.ID == "" {
+		a.ID = domains.NewAppBranchID()
+	}
+
+	if a.CreatedByID == "" {
+		a.CreatedByID = createdByIDFromContext(tx.Statement.Context)
+	}
+	if a.OrgID == "" {
+		a.OrgID = orgIDFromContext(tx.Statement.Context)
+	}
+
+	return nil
+}
