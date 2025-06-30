94c6f5d8a8c72574e36311b452faf55b9ca30842
sam boyer <sam@nuon.co>
2025-06-30 13:19:45 -0400

    feat: Introduce concurrency groups for signal handlers (#9280)
    * WIP
    
    * Finish initial impl
    
    * Use proper count var
    
    * Create concurrency groups on the fly instead of statically
    
    * Remove unneeded code
    
    * Test framework working
    
    * Just one off-by-one issue left
    
    * IT WORKS
    
    * Fix, refactor, isolate
    
    * Add first pass at concurrency doc
    
    * Allow setting conc group directly on BaseSignal
    
    ---------
    
    Co-authored-by: nuonbot <bot@nuon.co>

--- services/ctl-api/internal/pkg/eventloop/loop/loop.go
+++ services/ctl-api/internal/pkg/eventloop/loop/loop.go
@@ -1,19 +1,17 @@
 package loop
 
 import (
-	"fmt"
 	"strconv"
+
 	"strings"
 	"time"
 
 	"github.com/cockroachdb/errors"
 	"github.com/go-playground/validator/v10"
-	"go.temporal.io/sdk/temporal"
 	"go.temporal.io/sdk/workflow"
 	"go.uber.org/zap"
 
 	"github.com/powertoolsdev/mono/pkg/errs"
-	"github.com/powertoolsdev/mono/pkg/generics"
 	"github.com/powertoolsdev/mono/pkg/metrics"
 	tmetrics "github.com/powertoolsdev/mono/pkg/temporal/metrics"
 	"github.com/powertoolsdev/mono/services/ctl-api/internal"
@@ -43,138 +41,141 @@ type Loop[SignalType eventloop.Signal, ReqSig any] struct {
 	Handlers         map[eventloop.SignalType]func(workflow.Context, ReqSig) error
 	NewRequestSignal func(eventloop.EventLoopRequest, SignalType) ReqSig
 
-	// unprocessed cancellation signals
+	// the cancelbroker manages cancellations for child workflows, including holding signals for not-yet-received
+	// signals
 	cb *ntemporal.CancelBroker[SignalType]
+
+	// conc holds the active concurrency groups within which signals are queued.
+	conc map[string]*concgroup[SignalType]
+
+	// concwg is a WaitGroup that tracks the completion of all spawned coroutines
+	concwg workflow.WaitGroup
+
+	// Each channel is used to broadcast that a distinct stopping conditions for the loop has been reached
+	stopChan, restartChan, countChan workflow.Channel
+	// Bools representing stopping conditions for the loop
+	stop, restart, countExceeded, notexist bool
+
+	// existsErr is set if the ExistsHook returns an error
+	existsErr error
+
+	// pendingSignals holds signals to be passed back in to a ContinueAsNew instance of this event loop
+	pendingSignals []SignalType
+
+	// The number of signals that have been ingested by the event loop. When exceeded, the loop will restart and ContinueAsNew
+	sigcount int
 
 	// hooks
 
 	// StartupHook is called once, before the event loop starts in a call to Run
 	StartupHook func(workflow.Context, eventloop.EventLoopRequest) error
 
-	// checkExistsHook is called before each signal is processed to check if the
+	// ExistsHook is called before each signal is processed to check if the
 	// underlying object exists. The check is skipped if no implementation is
 	// provided.
 	ExistsHook func(workflow.Context, eventloop.EventLoopRequest) (bool, error)
 }
 
-func (w *Loop[SignalType, ReqSig]) handleSignal(pctx workflow.Context, wkflowReq eventloop.EventLoopRequest, signal SignalType, defaultTags map[string]string) error {
-	l, err := log.WorkflowLogger(pctx)
+func (l *Loop[SignalType, ReqSig]) RunWithConcurrency(ctx workflow.Context, req eventloop.EventLoopRequest, incomingPendingSignals []SignalType) error {
+	defaultTags := l.init(ctx, req)
+	selector := l.createSelector(ctx, req, defaultTags)
+	log, err := log.WorkflowLogger(ctx)
 	if err != nil {
 		return err
 	}
 
-	ctx := signal.GetWorkflowContext(w.cb.Join(pctx, func(other SignalType) bool {
-		return w.signalsMatch(signal, other)
-	}))
-
-	// NOTE(jm): this wrapper type is basically the result of two requirements:
-	// 1. - to pass state for the org/sandbox-mode into the signal handler
-	// 2. - generate a workflow id for the signal handler
-	//
-	// However, neither of these should be requirements because a.) it's preferable to be more explicit vs magically
-	// generating the signal workflow id, and b.) we use context propagation + state on the workflow type to pass
-	// the other information in.
-	//
-	// TODO(jm): temporal-gen does not currently support this, but I think the workflow-id should be an argument on
-	// the generated function, ie: these methods should be w.AwaitCreated(wkflow-id, signal) vs
-	// w.AwaitCreated(sigReq)
-
-	startTS := workflow.Now(ctx)
-	sigReq := w.NewRequestSignal(wkflowReq, signal)
-	handler, ok := w.Handlers[signal.SignalType()]
-	if !ok || handler == nil {
-		err = fmt.Errorf("unhandled signal %s", signal.SignalType())
-	} else {
-		err = handler(ctx, sigReq)
-	}
-
-	op := signal.Name()
-	status := "ok"
-	if err != nil {
-		status = "error"
-		if temporal.IsTimeoutError(err) {
-			status = "timeout"
-		} else if temporal.IsPanicError(err) {
-			status = "panic"
-		}
-		l.Error("signal handling failed", zap.Error(err), zap.String("signal", op))
-	} else {
-		l.Debug("signal handling succeeded", zap.String("signal", op))
-	}
-
-	// write out metrics
-	tags := generics.MergeMap(map[string]string{
-		"op":     op,
-		"status": status,
-	}, defaultTags)
-
-	dur := workflow.Now(ctx).Sub(startTS)
-
-	w.MW.Timing(ctx, "event_loop.signal_duration", dur, metrics.ToTags(tags)...)
-	w.MW.Incr(ctx, "event_loop.signal", metrics.ToTags(tags)...)
-
-	var listenErrs []error
-	for _, listener := range signal.Listeners() {
-		l.Debug("notifying signal listener",
-			zap.String("listener-workflow-id", listener.WorkflowID),
-			zap.String("listener-namespace", listener.Namespace),
-			zap.String("signal", op),
-		)
-
-		lctx := workflow.WithWorkflowNamespace(ctx, listener.Namespace)
-		lerr := workflow.SignalExternalWorkflow(
-			lctx,
-			listener.WorkflowID,
-			"",
-			listener.SignalName,
-			eventloop.SignalDoneMessage{
-				// CompletedSignal: signal,
-				Error:  err,
-				Result: nil,
-			},
-		).Get(lctx, nil) // TODO(sdboyer) double check that this future just waits for the signal to be created, not processed
-		if lerr != nil {
-			l.Error("error notifying signal listener",
-				zap.Error(lerr),
-				zap.String("listener-workflow-id", listener.WorkflowID),
-				zap.String("listener-namespace", listener.Namespace),
-				zap.String("signal", op),
-			)
-			listenErrs = append(listenErrs, lerr)
-
-			w.MW.Incr(ctx, "event_loop.signal.notify_errors", metrics.ToTags(tags)...)
+	if l.StartupHook != nil {
+		if err := l.StartupHook(ctx, req); err != nil {
+			log.Error("startup hook failed, restarting", zap.Error(err))
+			l.MW.Incr(ctx, "event_loop.restart", metrics.ToTags(defaultTags, "op", "restarted", "reason", "startup_hook_failure")...)
+			req.RestartCount += 1
+			return workflow.NewContinueAsNewError(ctx, workflow.GetInfo(ctx).WorkflowType.Name, req, incomingPendingSignals)
 		}
 	}
 
-	ldur := workflow.Now(ctx).Sub(startTS) - dur
-	w.MW.Timing(ctx, "event_loop.signal.notify_duration", ldur, metrics.ToTags(tags)...)
+	// handle any pending signals
+	l.handlePending(ctx, req, defaultTags, incomingPendingSignals)
 
-	if len(listenErrs) > 0 {
-		return errors.Wrap(errors.Join(listenErrs...), "error notifying signal listeners")
+	// Main loop. The select has several branches to unblock, primarily when a signal is received, but also
+	// if any of our four stopping conditions are met:
+	//
+	//  - A signal with Stop() = true is received. No new signals will be ingested, but ones already in their concgroup queues will be processed before stopping.
+	//  - The underlying object goes away. Everything shuts down right away, and both waiting and ingested signals are dropped.
+	//  - A signal is sent with Restart(). No new signals will be ingested. All signals currently in their concgroup queues will be assembled in order
+	//    (within their conc group) into pendingSignals. The workflow will restart with ContinueAsNew, and pending signals processed in their respective conc groups.
+	// 	  The Restart() signal itself is processed as part of pendingSignals on the refreshed loop.
+	//  - The maxSignals limit is exceeded. Behavior is identical to the Restart() case, but slightly different telemetry is emitted.
+	for !(l.stop || l.notexist) {
+		// fmt.Println(l.stop, l.notexist, l.restart, l.countExceeded)
+		selector.Select(ctx)
+
+		if errors.Is(ctx.Err(), workflow.ErrCanceled) {
+			l.MW.Incr(ctx, "event_loop.canceled", metrics.ToTags(defaultTags)...)
+			log.Info("workflow canceled")
+			break
+		}
+
+		if l.restart || l.countExceeded {
+			if l.restart {
+				l.MW.Incr(ctx, "event_loop.restarted", metrics.ToTags(defaultTags)...)
+				log.Info("workflow restarted")
+				// TODO(jm): this should be removed once we ensure all event loops have an active
+				// `WorkerVersion` in their request chain
+				req.Version = l.Cfg.Version
+			} else {
+				l.MW.Incr(ctx, "event_loop.max_signals", metrics.ToTags(defaultTags)...)
+				log.Info("workflow hit max signals")
+			}
+
+			l.restartChan.Close()
+			l.stopChan.Close()
+			l.countChan.Close()
+			l.concwg.Wait(ctx)
+
+			req.RestartCount += 1
+			// This will drain all pending signals from the main signal queue
+			for selector.HasPending() {
+				selector.Select(ctx)
+			}
+			// It's critical that we return immediately after draining, without doing anything that would cause
+			// this coroutine to yield. Otherwise, additional signals may be received, which would then be dropped
+			// in the ContinueAsNew transition.
+			return workflow.NewContinueAsNewError(ctx, workflow.GetInfo(ctx).WorkflowType.Name, req, l.pendingSignals)
+		}
 	}
 
+	l.MW.Incr(ctx, "event_loop.finish", metrics.ToTags(defaultTags)...)
 	return nil
 }
 
-func (w *Loop[SignalType, ReqSig]) signalsMatch(a, b SignalType) bool {
-	if a.Name() != b.Name() {
-		return false
-	}
+func (l *Loop[SignalType, ReqSig]) init(ctx workflow.Context, req eventloop.EventLoopRequest) map[string]string {
+	l.conc = make(map[string]*concgroup[SignalType])
+	l.concwg = workflow.NewWaitGroup(ctx)
+	l.cb = ntemporal.NewCancelBroker[SignalType](ctx)
+	l.pendingSignals = make([]SignalType, 0)
 
-	// Allow cancellation only if the signal name of the listener matches
-	//
-	// TODO(sdboyer) this is a bit of a hack, but it's an effective way of ensuring for now that ONLY the workflow that called 
-	// SendAsync can trigger cancellation this way, because SendAsync generates these listener ids on the fly.
-	// This may be more restrictive than the contract we eventually want, but right now, it has no false positives.
-	for _, alistener := range a.Listeners() {
-		for _, blistener := range b.Listeners() {
-			if alistener.SignalName == blistener.SignalName {
-				return true
-			}
-		}
+	wkflowInfo := workflow.GetInfo(ctx)
+	defaultTags := map[string]string{
+		"sandbox_mode": strconv.FormatBool(req.SandboxMode),
+		"namespace":    wkflowInfo.Namespace,
+		"task_queue":   wkflowInfo.TaskQueueName,
 	}
-	return false
+	l.MW.Incr(ctx, "event_loop.start", metrics.ToTags(defaultTags)...)
+	l.MW.Gauge(ctx, "event_loop.restart_count", float64(req.RestartCount), metrics.ToTags(defaultTags)...)
+	l.MW.Gauge(ctx, "event_loop.version_change_count", float64(req.VersionChangeCount), metrics.ToTags(defaultTags)...)
+
+	return defaultTags
 }
+
+func (l *Loop[SignalType, ReqSig]) logger(ctx workflow.Context) *zap.Logger {
+	log, err := log.WorkflowLogger(ctx)
+	if err != nil {
+		panic("unreachable - unable to get workflow logger after it was retrieved successfully in parent fn")
+	}
+	return log
+}
+
+// TODO(sdboyer) delete everything below here once we're ready to fully adopt concurrent event loops
 
 func (w *Loop[SignalType, ReqSig]) Run(ctx workflow.Context, req eventloop.EventLoopRequest, pendingSignals []SignalType) error {
 	signalChan := workflow.GetSignalChannel(ctx, req.ID)
@@ -360,6 +361,21 @@ func (w *Loop[SignalType, ReqSig]) validateSignal(ctx workflow.Context, info *wo
 
 	ret = err == nil
 	return ret // overridden by defer, no matter what
+}
+
+func (l *Loop[SignalType, ReqSig]) drainSignals(ch workflow.ReceiveChannel) []SignalType {
+	var signals []SignalType
+	for {
+		var signal SignalType
+		ok := ch.ReceiveAsync(&signal)
+		if !ok {
+			break
+		}
+
+		signals = append(signals, signal)
+	}
+
+	return signals
 }
 
 func (w *Loop[SignalType, ReqSig]) filterSignals(ctx workflow.Context, info *workflow.Info, sigs []SignalType) []SignalType {
