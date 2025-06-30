a1ab0bde0cc3a6b2451b5ad795a583a943b8b239
Rob <alphavibe@gmail.com>
2025-06-30 13:22:43 -0700

    feat: app branches endpoints (#9520)
    * feat: app branches endpoints
    
    * feat: app branches endpoints
    
    * feat: app branches endpoints
    
    * feat: app branches endpoints

---
+++ services/ctl-api/internal/app/apps/helpers/create_app_branch.go
@@ -0,0 +1,24 @@
+package helpers
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
+)
+
+func (h *Helpers) CreateAppBranch(ctx context.Context, orgID, appID, vcsConnectionBranchID string) (*app.AppBranch, error) {
+	branch := app.AppBranch{
+		OrgID:                 orgID,
+		AppID:                 appID,
+		VCSConnectionBranchID: vcsConnectionBranchID,
+	}
+
+	res := h.db.WithContext(ctx).Create(&branch)
+	if res.Error != nil {
+		return nil, fmt.Errorf("unable to create app branch: %w", res.Error)
+	}
+
+	return &branch, nil
+}
+
---
+++ services/ctl-api/internal/app/apps/service/create_app_branch.go
@@ -0,0 +1,72 @@
+package service
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/pkg/cctx"
+)
+
+type CreateAppBranchRequest struct {
+	VCSConnectionBranchID string `json:"vcs_connection_branch_id"`
+}
+
+func (c *CreateAppBranchRequest) Validate(v *validator.Validate) error {
+	if err := v.Struct(c); err != nil {
+		return fmt.Errorf("invalid request: %w", err)
+	}
+
+	return nil
+}
+
+// @ID						CreateAppBranch
+// @Description.markdown	create_app_branch.md
+// @Tags					apps
+// @Accept					json
+// @Param					req	body	CreateAppBranchRequest	true	"Input"
+// @Produce				json
+// @Param					app_id	path	string	true	"app ID"
+// @Security				APIKey
+// @Security				OrgID
+// @Failure				400	{object}	stderr.ErrResponse
+// @Failure				401	{object}	stderr.ErrResponse
+// @Failure				403	{object}	stderr.ErrResponse
+// @Failure				404	{object}	stderr.ErrResponse
+// @Failure				500	{object}	stderr.ErrResponse
+// @Success				201	{object}	app.AppBranch
+// @Router					/v1/apps/{app_id}/branches [post]
+func (s *service) CreateAppBranch(ctx *gin.Context) {
+	org, err := cctx.OrgFromContext(ctx)
+	if err != nil {
+		ctx.Error(err)
+		return
+	}
+
+	appID := ctx.Param("app_id")
+
+	var req CreateAppBranchRequest
+	if err := ctx.BindJSON(&req); err != nil {
+		ctx.Error(fmt.Errorf("unable to parse request: %w", err))
+		return
+	}
+	if err := req.Validate(s.v); err != nil {
+		ctx.Error(fmt.Errorf("invalid request: %w", err))
+		return
+	}
+
+	branch, err := s.helpers.CreateAppBranch(ctx, org.ID, appID, req.VCSConnectionBranchID)
+	if err != nil {
+		ctx.Error(fmt.Errorf("unable to create app branch: %w", err))
+		return
+	}
+
+	// s.evClient.Send(ctx, appID, &signals.Signal{
+	// 	Type:        signals.OperationCreateAppBranch,
+	// 	AppBranchID: branch.ID,
+	// })
+	//
+	ctx.JSON(http.StatusCreated, branch)
+}
