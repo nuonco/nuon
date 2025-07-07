88f93cd1ea742ad3eca4ca10680f1f69ef68a650
sam boyer <sam@nuon.co>
2025-07-07 12:46:01 -0400

    chore: Run temporal-gen concurrently (#9567)
    * chore: Run temporal-gen concurrently
    
    * Remove all individual dir gen.go files
    
    * Use err groups instead
    
    * Run all generators concurrently
    
    ---------
    
    Co-authored-by: nuonbot <bot@nuon.co>

--- services/ctl-api/cmd/gen/main.go
+++ services/ctl-api/cmd/gen/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"fmt"
+	"io/fs"
 	"log"
 	"os"
+	"path/filepath"
+	"runtime"
+	"sync"
 
 	"golang.org/x/sync/errgroup"
 
@@ -110,6 +115,130 @@ func generatePublicSchema(ctx context.Context) error {
 	return nil
 }
 
+func runTemporalGen(ctx context.Context) error {
+	// Build a binary to reuse per-directory
+	binpath, err := compileToTemp(ctx, "github.com/powertoolsdev/mono/pkg/gen/temporal-gen")
+	if err != nil {
+		return fmt.Errorf("unable to compile temporal-gen binary: %w", err)
+	}
+
+	wd, err := os.Getwd()
+	if err != nil {
+		return fmt.Errorf("unable to get current working directory: %w", err)
+	}
+
+	paths := make(chan string)
+	var pathmap sync.Map
+	eg, _ := errgroup.WithContext(ctx)
+	numWorkers := runtime.NumCPU()
+	var inerr error
+
+	for i := 0; i < numWorkers; i++ {
+		eg.Go(func() error {
+			for path := range paths {
+				dir := filepath.Dir(path)
+				if _, has := pathmap.Load(dir); has {
+					continue
+				}
+				byt, err := os.ReadFile(path)
+				if err != nil {
+					return fmt.Errorf("unable to read file %s: %w", path, err)
+				}
+
+				if bytes.Contains(byt, []byte("\n// @temporal-gen ")) {
+					pathmap.Store(dir, struct{}{})
+				}
+			}
+
+			return nil
+		})
+	}
+
+	err = filepath.WalkDir(wd, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if !d.IsDir() && d.Type().IsRegular() {
+			paths <- path
+		}
+		return nil
+	})
+	if err != nil {
+		return fmt.Errorf("unable to walk directory: %w", err)
+	}
+
+	close(paths)
+	eg.Wait()
+	if inerr != nil {
+		return inerr
+	}
+
+	eg, _ = errgroup.WithContext(ctx)
+	dirs := make(chan string)
+	for i := 0; i < numWorkers; i++ {
+		eg.Go(func() error {
+			for dir := range dirs {
+				cmd, err := command.New(v,
+					command.WithInheritedEnv(),
+					command.WithCmd(binpath),
+					command.WithCwd(dir),
+				)
+				if err != nil {
+					inerr = fmt.Errorf("unable to create command: %w", err)
+					continue
+				}
+				if err := cmd.Exec(ctx); err != nil {
+					inerr = fmt.Errorf("error running temporal-gen on %s: %w", dir, err)
+				}
+			}
+
+			return nil
+		})
+	}
+
+	pathmap.Range(func(k, _ any) bool {
+		dirs <- k.(string)
+		return true
+	})
+
+	close(dirs)
+	eg.Wait()
+
+	return inerr
+}
+
+func compileToTemp(ctx context.Context, path string) (string, error) {
+	// Compile the temporal-gen binary for the given path
+	// This is a placeholder function and should be implemented as needed
+	name := filepath.Base(path)
+
+	tmpdir, err := os.MkdirTemp(os.TempDir(), name)
+	if err != nil {
+		return "", fmt.Errorf("unable to create temporary directory: %w", err)
+	}
+
+	binpath := filepath.Join(tmpdir, name)
+
+	args := []string{
+		"build",
+		"-o", binpath,
+		path,
+	}
+
+	cmd, err := command.New(v,
+		command.WithInheritedEnv(),
+		command.WithCmd("go"),
+		command.WithArgs(args),
+	)
+	if err != nil {
+		return "", fmt.Errorf("unable to create command: %w", err)
+	}
+	if err := cmd.Exec(ctx); err != nil {
+		return "", fmt.Errorf("unable to execute command: %w", err)
+	}
+	return binpath, nil
+}
+
 func main() {
 	ctx := context.Background()
 	ctx, cancelFn := context.WithCancel(ctx)
@@ -120,17 +249,10 @@ func main() {
 		generateRunnerSchema,
 		generatePublicSchema,
 		generateAdminSchema,
+		runTemporalGen,
 	}
 
-	for idx, fn := range fns {
-		if err := fn(ctx); err != nil {
-			log.Fatal("failed on %d %s", idx, err.Error())
-		}
-
-		// NOTE(jm): this is not parallelized any more because docker builds seem to run out of resources when
-		// building. Since it's not a huge speedup, I'm just disabling it for now, and we can revisit increasing
-		// memory during builds later.
-		continue
+	for _, fn := range fns {
 		eg.Go(func() error {
 			return fn(ctx)
 		})
