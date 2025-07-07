cce438eff784585ce769ffe126cffe951b2d9032
sam boyer <sam@nuon.co>
2025-07-07 12:46:01 -0400

    chore: Run temporal-gen concurrently (#9567)
    * chore: Run temporal-gen concurrently
    
    * Remove all individual dir gen.go files
    
    * Use err groups instead
    
    * Run all generators concurrently
    
    ---------
    
    Co-authored-by: nuonbot <bot@nuon.co>

--- services/ctl-api/internal/app/general/worker/gen.go
+++
@@ -1,4 +0,0 @@
-package worker
-
-//go:generate -command temporal-gen go run github.com/powertoolsdev/mono/pkg/gen/temporal-gen
-//go:generate temporal-gen
--- services/ctl-api/internal/app/installs/worker/actions/gen.go
+++
@@ -1,4 +0,0 @@
-package actions
-
-//go:generate -command temporal-gen go run github.com/powertoolsdev/mono/pkg/gen/temporal-gen
-//go:generate temporal-gen
