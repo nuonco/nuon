4ab6626ba537b75a42d943781e30e838f133d1e4
Prem Saraswat <prmsrswt@gmail.com>
2025-07-07 19:51:33 +0530

    feat(ui): Add button and modal for downloading audit history of installs (#9535)
    * feat(ui): Add button and modal for downloading audit history of installs
    
    Signed-off-by: Prem Saraswat <prem@nuon.co>
    
    * (ui) Use proper icon for audit history download button
    
    Signed-off-by: Prem Saraswat <prem@nuon.co>
    
    ---------
    
    Signed-off-by: Prem Saraswat <prem@nuon.co>
    Co-authored-by: nuonbot <bot@nuon.co>

---
+++ services/dashboard-ui/src/app/api/[org-id]/installs/[install-id]/audit-logs/route.ts
@@ -0,0 +1,21 @@
+import { NextRequest } from 'next/server'
+import { TRouteRes } from '@/app/api/[org-id]/types'
+import { API_URL } from '@/utils/configs'
+import { getFetchOpts } from '@/utils/get-fetch-opts'
+
+export const GET = async (
+  req: NextRequest,
+  { params }: TRouteRes<'org-id' | 'install-id'>
+) => {
+  const { ['org-id']: orgId, ['install-id']: installId } = await params
+
+  const startTS = req.nextUrl.searchParams.get('start')
+  const endTS = req.nextUrl.searchParams.get('end')
+
+  var resp = await fetch(
+    `${API_URL}/v1/installs/${installId}/audit_logs?start=${startTS}&end=${endTS}`,
+    await getFetchOpts(orgId, {}, 30000)
+  )
+
+  return resp
+}
