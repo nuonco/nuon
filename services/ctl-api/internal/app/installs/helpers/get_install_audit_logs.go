ec9fe6769b9a9662b6406837b84f240df2c7d526
Prem Saraswat <prmsrswt@gmail.com>
2025-07-07 19:41:57 +0530

    feat: [ctl-api] Add endpoint to get install audit logs (#9490)
    * [ctl-api] Add endpoint to get install audit logs
    
    * [ctl-api] Add time range fields to audit logs endpoint
    
    Signed-off-by: Prem Saraswat <prem@nuon.co>
    
    * [ctl-api] Switch to text/csv response in audit logs endpoint
    
    Signed-off-by: Prem Saraswat <prem@nuon.co>
    
    * [ctl-api] Introduce default limit for install audit logs
    
    Signed-off-by: Prem Saraswat <prem@nuon.co>
    
    ---------
    
    Signed-off-by: Prem Saraswat <prem@nuon.co>
    Co-authored-by: nuonbot <bot@nuon.co>

---
+++ services/ctl-api/internal/app/installs/helpers/get_install_audit_logs.go
@@ -0,0 +1,39 @@
+package helpers
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"time"
+
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/pkg/db/plugins/views"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/pkg/db/scopes"
+)
+
+const (
+	// defaultInstallAuditLogsLimit is the default limit for install audit logs.
+	defaultInstallAuditLogsLimit = 10_000
+)
+
+// GetInstallAuditLogs gets the audit logs for an install from the DB.
+func (h *Helpers) GetInstallAuditLogs(ctx context.Context, installID string, startTS, endTS time.Time) ([]app.InstallAuditLog, error) {
+	var auditLogs []app.InstallAuditLog
+	res := h.db.WithContext(ctx).
+		Scopes(
+			scopes.WithOverrideTable(views.DefaultViewName(h.db, &app.InstallAuditLog{}, 1)),
+		).
+		Order("time_stamp ASC").
+		Limit(defaultInstallAuditLogsLimit).
+		Find(&auditLogs, "install_id = ? AND time_stamp BETWEEN ? AND ?", installID, startTS, endTS)
+
+	if res.Error != nil {
+		if res.Error == sql.ErrNoRows {
+			// No rows found, return an empty slice
+			return auditLogs, nil
+		}
+		return nil, fmt.Errorf("unable to get install audit logs: %w", res.Error)
+	}
+
+	return auditLogs, nil
+}
