1d49d4540f5c92f9a80d82f593137a7ada06e98e
Jon Morehouse <MorehouseJ09@gmail.com>
2025-07-07 17:40:21 -0700

    chore: update table name plugin to work with indexes (#9581)

--- services/ctl-api/internal/pkg/db/plugins/table_name.go
+++ services/ctl-api/internal/pkg/db/plugins/table_name.go
@@ -6,13 +6,22 @@ import (
 	"gorm.io/gorm"
 )
 
+type Tabler interface {
+	TableName() string
+}
+
 func TableName(db *gorm.DB, obj any) string {
 	value := reflect.ValueOf(obj)
 	if value.Kind() == reflect.Ptr && value.IsNil() {
 		value = reflect.New(value.Type().Elem())
 	}
 
-	modelType := reflect.Indirect(value).Type()
+	// Check if the object implements Tabler interface
+	if tabler, ok := obj.(Tabler); ok {
+		return tabler.TableName()
+	}
 
+	// Fall back to using the naming strategy
+	modelType := reflect.Indirect(value).Type()
 	return db.NamingStrategy.TableName(modelType.Name())
 }
