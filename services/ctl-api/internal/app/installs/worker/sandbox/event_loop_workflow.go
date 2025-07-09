1de3b4c5c4628ffb28630dd6657989977111171c
sam boyer <sam@nuon.co>
2025-07-09 12:43:16 -0400

    fix: Do not run sub event loops as child workflows (#9593)
    * fix: Do not run sub event loops as child workflows
    
    * Sandbox, not stack
    
    * Many small fixes
    
    * Handle orgs properly, clean up cruft
    
    ---------
    
    Co-authored-by: nuonbot <bot@nuon.co>

--- services/ctl-api/internal/app/installs/worker/sandbox/event_loop_workflow.go
+++ services/ctl-api/internal/app/installs/worker/sandbox/event_loop_workflow.go
@@ -8,6 +8,10 @@ import (
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/pkg/eventloop/loop"
 )
 
+func noop(ctx workflow.Context, sreq signals.RequestSignal) error {
+	return nil
+}
+
 func (w *Workflows) GetHandlers() map[eventloop.SignalType]func(workflow.Context, signals.RequestSignal) error {
 	return map[eventloop.SignalType]func(workflow.Context, signals.RequestSignal) error{
 		signals.OperationDeprovisionSandboxPlan:      AwaitDeprovisionSandboxPlan,
@@ -18,6 +22,9 @@ func (w *Workflows) GetHandlers() map[eventloop.SignalType]func(workflow.Context
 
 		signals.OperationProvisionSandboxPlan:      AwaitProvisionSandboxPlan,
 		signals.OperationProvisionSandboxApplyPlan: AwaitProvisionSandboxApplyPlan,
+
+		signals.OperationRestart: noop,
+		signals.OperationCreated: noop,
 	}
 }
 
