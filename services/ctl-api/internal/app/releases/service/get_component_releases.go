1de0dd18597de4f0abe1ec1ea0c6e738dea8f9c0
robisso <alphavibe@gmail.com>
2025-07-08 22:46:49 -0700

    feat: pagination, add header (#9604)

--- services/ctl-api/internal/app/releases/service/get_component_releases.go
+++ services/ctl-api/internal/app/releases/service/get_component_releases.go
@@ -17,6 +17,7 @@ import (
 // @Param					component_id				path	string	true	"component ID"
 // @Param					offset						query	int		false	"offset of results to return"	Default(0)
 // @Param					limit						query	int		false	"limit of results to return"	Default(10)
+// @Param					page						query	int		false	"page number of results to return"	Default(0)
 // @Param					x-nuon-pagination-enabled	header	bool	false	"Enable pagination"
 // @Tags					releases
 // @Accept					json
--- services/ctl-api/internal/app/vcs/service/get_vcs_branches.go
+++ services/ctl-api/internal/app/vcs/service/get_vcs_branches.go
@@ -18,6 +18,7 @@ import (
 // @Description.markdown	get_vcs_branches.md
 // @Param					offset						query	int		false	"offset of branches to return"	Default(0)
 // @Param					limit						query	int		false	"limit of branches to return"	Default(10)
+// @Param					page						query	int		false	"page number of results to return"	Default(0)
 // @Param					x-nuon-pagination-enabled	header	bool	false	"Enable pagination"
 // @Tags					vcs
 // @Accept					json
@@ -73,4 +74,3 @@ func (s *service) getAllVCSBranches(ctx *gin.Context, orgID string) ([]app.VCSCo
 
 	return branches, nil
 }
-
