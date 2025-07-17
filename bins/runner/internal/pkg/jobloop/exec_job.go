2a947f3f0b7679fb36a223a528385fbf2469c1e1
Jon Morehouse <MorehouseJ09@gmail.com>
2025-07-17 15:37:44 -0700

    chore: add `runner_job.type` attribute (#9704)
    Co-authored-by: nuonbot <bot@nuon.co>

--- bins/runner/internal/pkg/jobloop/exec_job.go
+++ bins/runner/internal/pkg/jobloop/exec_job.go
@@ -35,6 +35,7 @@ func (j *jobLoop) executeJob(ctx context.Context, job *models.AppRunnerJob) erro
 	}
 
 	l = l.With(zap.String("runner_job.id", job.ID))
+	l = l.With(zap.String("runner_job.type", string(job.Type)))
 	l = l.With(zap.String("log_stream.id", job.LogStreamID))
 
 	// create an execution in the API
