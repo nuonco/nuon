5348817a6f22a7b99debc6b63a4c4db9b2b65b57
Dat Boi Diego <fidiego@users.noreply.github.com>
2025-07-22 16:42:39 -0500

    feat: ctl-api: runner job group "management" + endpoints (#9751)
    * feat: ctl-api: runner job group "management" + endpoints
    
    * chore: go mod tidy

---
+++ services/ctl-api/internal/app/runners/service/mng_shutdown_vm.go
@@ -0,0 +1,55 @@
+package service
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app/runners/signals"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/pkg/cctx"
+)
+
+type MngVMShutDownRequest struct{}
+
+// @ID						MngVMShutDown
+// @Summary				shut down an install runner VM
+// @Param					runner_id	path	string						true	"runner ID"
+// @Param					req			body	MngVMShutDownRequest	true	"Input"
+// @Tags					runners
+// @Accept					json
+// @Produce				json
+// @Security				APIKey
+// @Security				OrgID
+// @Failure				400	{object}	stderr.ErrResponse
+// @Failure				401	{object}	stderr.ErrResponse
+// @Failure				403	{object}	stderr.ErrResponse
+// @Failure				404	{object}	stderr.ErrResponse
+// @Failure				500	{object}	stderr.ErrResponse
+// @Success				201	{boolean}	true
+// @Router					/v1/runners/{runner_id}/mng/shutdown-vm [POST]
+func (s *service) MngVMShutDown(ctx *gin.Context) {
+	org, err := cctx.OrgFromContext(ctx)
+	if err != nil {
+		ctx.Error(err)
+		return
+	}
+	runnerID := ctx.Param("runner_id")
+	runner, err := s.getOrgRunner(ctx, runnerID, org.ID)
+	if err != nil {
+		ctx.Error(fmt.Errorf("unable to get runner %s: %w", runnerID, err))
+		return
+	}
+
+	var req MngVMShutDownRequest
+	if err := ctx.BindJSON(&req); err != nil {
+		ctx.Error(fmt.Errorf("unable to parse request: %w", err))
+		return
+	}
+
+	s.evClient.Send(ctx, runner.ID, &signals.Signal{
+		Type: signals.OperationMngVMShutDown,
+	})
+
+	ctx.JSON(http.StatusCreated, true)
+}
