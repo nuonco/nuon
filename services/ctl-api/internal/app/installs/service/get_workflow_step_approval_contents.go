4593f00f420e288e29f4f629eec262a28ac27462
Jon Morehouse <MorehouseJ09@gmail.com>
2025-07-28 14:17:06 -0700

    chore: roll out custom dataconverter (#9805)
    * fix: nuonctl run does not work when no tmp dir is set
    
    * feat: create data converter that persists to DB
    
    * tweak interface
    
    * chore: update
    
    * 50kb limit
    
    * chore: set threshold to 128kb (same as ddog s3 codec)
    
    * fix: new endpoint to fetch contents directly
    
    * chore: update workflow step

---
+++ services/ctl-api/internal/app/installs/service/get_workflow_step_approval_contents.go
@@ -0,0 +1,78 @@
+package service
+
+import (
+	"bytes"
+	"compress/gzip"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/pkg/errors"
+
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/pkg/cctx"
+)
+
+// @ID GetWorkflowStepApprovalContents
+// @Summary			get a workflow step approval contents
+// @Description.markdown	 get_workflow_step_approval_contents.md
+// @Param	workflow_id			path	string	true	"workflow id"
+// @Param	workflow_step_id	path	string	true	"step id"
+// @Param	approval_id					path	string	true	"approval id"
+// @Tags								installs
+// @Accept								json
+// @Produce								json
+// @Security							APIKey
+// @Security							OrgID
+// @Failure								400	{object}	stderr.ErrResponse
+// @Failure								401	{object}	stderr.ErrResponse
+// @Failure								403	{object}	stderr.ErrResponse
+// @Failure								404	{object}	stderr.ErrResponse
+// @Failure								500	{object}	stderr.ErrResponse
+// @Success								200	{array} byte
+// @Router /v1/workflows/{workflow_id}/steps/{workflow_step_id}/approvals/{approval_id}/contents  [GET]
+func (s *service) GetWorkflowStepApprovalContents(ctx *gin.Context) {
+	org, err := cctx.OrgFromContext(ctx)
+	if err != nil {
+		ctx.Error(errors.Wrap(err, "unable to get org from context"))
+		return
+	}
+
+	workflowID := ctx.Param("workflow_id")
+	stepID := ctx.Param("workflow_step_id")
+	approvalID := ctx.Param("approval_id")
+
+	_, err = s.getWorkflowStep(ctx, workflowID, stepID)
+	if err != nil {
+		ctx.Error(errors.Wrap(err, "unable to get workflow step"))
+		return
+	}
+
+	approval, err := s.getWorkflowStepApproval(ctx, org.ID, approvalID)
+	if err != nil {
+		ctx.Error(errors.Wrap(err, "unable to get workflow step approval"))
+		return
+	}
+
+	// Create a buffer to hold the gzipped data
+	var buf bytes.Buffer
+	gzipWriter := gzip.NewWriter(&buf)
+
+	// Write the contents to the gzip writer
+	_, err = gzipWriter.Write([]byte(approval.Contents))
+	if err != nil {
+		ctx.Error(errors.Wrap(err, "unable to gzip approval contents"))
+		return
+	}
+
+	// Close the gzip writer to flush any remaining data
+	err = gzipWriter.Close()
+	if err != nil {
+		ctx.Error(errors.Wrap(err, "unable to close gzip writer"))
+		return
+	}
+
+	// Set the Content-Encoding header to indicate gzip compression
+	ctx.Header("Content-Encoding", "gzip")
+
+	// Return the gzipped bytes
+	ctx.Data(http.StatusOK, "application/json", buf.Bytes())
+}
--- services/ctl-api/internal/app/workflow_step_approval.go
+++ services/ctl-api/internal/app/workflow_step_approval.go
@@ -43,7 +43,7 @@ type WorkflowStepApproval struct {
 	OwnerID   string `json:"owner_id,omitzero" gorm:"type:text;check:owner_id_checker,char_length(id)=26;index:idx_runner_jobs_owner_id,priority:1" temporaljson:"owner_id,omitzero,omitempty"`
 	OwnerType string `json:"owner_type,omitzero" gorm:"type:text;" temporaljson:"owner_type,omitzero,omitempty"`
 
-	Contents string `json:"contents" temporaljson:"-"`
+	Contents string `json:"-" temporaljson:"-"`
 
 	Type WorkflowStepApprovalType `json:"type"`
 
