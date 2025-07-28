67f9d415c9facb43171f37b76e327fb56bcce1ad
Jon Morehouse <MorehouseJ09@gmail.com>
2025-07-28 14:17:06 -0700

    chore: roll out custom dataconverter (#9805)
    * fix: nuonctl run does not work when no tmp dir is set
    
    * feat: create data converter that persists to DB
    
    * tweak interface
    
    * chore: update
    
    * 50kb limit
    
    * chore: set threshold to 128kb (same as ddog s3 codec)
    
    * fix: new endpoint to fetch contents directly
    
    * chore: update workflow step

--- services/ctl-api/internal/pkg/temporal/temporal.go
+++ services/ctl-api/internal/pkg/temporal/temporal.go
@@ -4,34 +4,33 @@ import (
 	"fmt"
 
 	"github.com/go-playground/validator/v10"
+	"go.temporal.io/sdk/converter"
 	"go.temporal.io/sdk/workflow"
 	"go.uber.org/fx"
 	"go.uber.org/zap"
 
 	"github.com/powertoolsdev/mono/pkg/metrics"
 	temporalclient "github.com/powertoolsdev/mono/pkg/temporal/client"
-	"github.com/powertoolsdev/mono/pkg/temporal/dataconverter"
 	"github.com/powertoolsdev/mono/services/ctl-api/internal"
 )
 
 type Params struct {
 	fx.In
 
-	L          *zap.Logger
-	V          *validator.Validate
-	Cfg        *internal.Config
-	MW         metrics.Writer
-	Propagator workflow.ContextPropagator
+	L             *zap.Logger
+	V             *validator.Validate
+	Cfg           *internal.Config
+	MW            metrics.Writer
+	Propagator    workflow.ContextPropagator
+	DataConverter converter.DataConverter
 }
 
 func New(params Params) (temporalclient.Client, error) {
-	dataConverter := dataconverter.NewJSONConverter()
-
 	tc, err := temporalclient.New(params.V,
 		temporalclient.WithAddr(params.Cfg.TemporalHost),
 		temporalclient.WithLogger(params.L),
 		temporalclient.WithNamespace(params.Cfg.TemporalNamespace),
-		temporalclient.WithDataConverter(dataConverter),
+		temporalclient.WithDataConverter(params.DataConverter),
 		temporalclient.WithContextPropagator(params.Propagator),
 		temporalclient.WithMetricsWriter(params.MW),
 	)
