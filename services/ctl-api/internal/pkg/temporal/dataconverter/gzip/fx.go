48784baf1e00bfb9c67801ccd59517e99fb16f60
Jon Morehouse <MorehouseJ09@gmail.com>
2025-07-29 15:36:53 -0700

    feat: introduce gzip codec, refactor (#9817)
    * feat: introduce gzip codec, refactor
    
    * add codec server endpoint
    
    * feat: support dataconverter codecs via nuon api
    
    * update
    
    * fix
    
    * chore: update
    
    * update
    
    * chore: update data converter

---
+++ services/ctl-api/internal/pkg/temporal/dataconverter/gzip/fx.go
@@ -0,0 +1,30 @@
+package gzip
+
+import (
+	"go.temporal.io/sdk/converter"
+	"go.uber.org/fx"
+	"go.uber.org/zap"
+
+	"github.com/powertoolsdev/mono/services/ctl-api/internal"
+)
+
+type Params struct {
+	fx.In
+
+	Cfg *internal.Config
+	L   *zap.Logger
+}
+
+func New(params Params) converter.PayloadCodec {
+	return &dataConverter{
+		cfg: params.Cfg,
+		l:   params.L,
+	}
+}
+
+func AsGzip(f any) any {
+	return fx.Annotate(
+		f,
+		fx.ResultTags(`name:"gzip"`),
+	)
+}
