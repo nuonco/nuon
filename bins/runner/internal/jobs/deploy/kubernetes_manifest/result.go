c0e966197e98e3b5fe7700aded43acba159c0cbb
Dat Boi Diego <fidiego@users.noreply.github.com>
2025-07-30 15:38:38 -0500

    feat: deploy plans: compress (#9816)
    * feat: compressed approvals: use new compressed plan in sandbox and component plans
    
    * chore: update pkg/terraform to compress plan.json
    
    * feat: compressed plans: update runner to write and read compressed plans
    
    plans are written to the API as URLSafe base64 encoded strings of the
    GZipped contents and contents display.
    
    note: the tfplan is ALREADY gzipped so we only base64 encode it for
    transit.
    
    on the way in, we now have to base64 decode and then decompress the
    apply plan contents.

--- bins/runner/internal/jobs/deploy/kubernetes_manifest/result.go
+++ bins/runner/internal/jobs/deploy/kubernetes_manifest/result.go
@@ -1,16 +1,43 @@
 package kubernetes_manifest
 
 import (
+	"bytes"
+	"compress/gzip"
+	"encoding/base64"
+	"encoding/json"
+
 	"github.com/nuonco/nuon-runner-go/models"
+	types "github.com/powertoolsdev/mono/pkg/types/components/plan"
+
+	"github.com/pkg/errors"
+	"go.uber.org/zap"
 	release "helm.sh/helm/v4/pkg/release/v1"
 )
 
-func (h *handler) createAPIResult(rel *release.Release, contents string, display map[string]interface{}) (*models.ServiceCreateRunnerJobExecutionResultRequest, error) {
+// TODO(fd): encrypt and such
+func (h *handler) createAPIResultRequest(rel *release.Release, l *zap.Logger, planContents types.KubernetesManifestPlanContents) (*models.ServiceCreateRunnerJobExecutionResultRequest, error) {
 	req := &models.ServiceCreateRunnerJobExecutionResultRequest{
-		Success:         true,
-		Contents:        contents,
-		ContentsDisplay: display,
+		Success: true,
 	}
+
+	// read plan contents into json
+	byts, err := json.Marshal(planContents)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to marshal plan contents")
+	}
+
+	// gzip
+	l.Info("zipping kubernetes_manifest plan")
+	var zippedBytes bytes.Buffer
+	gzipWriter := gzip.NewWriter(&zippedBytes)
+	gzipWriter.Write(byts)
+	gzipWriter.Close()
+	l.Debug("zipped kubernetes_manifest plan", zap.Int("bytes.zipped", len(zippedBytes.Bytes())))
+
+	// base64 encrypt
+	encodedString := base64.URLEncoding.EncodeToString(zippedBytes.Bytes())
+	req.ContentsCompressed = encodedString
+	req.ContentsDisplayCompressed = encodedString
 
 	return req, nil
 }
--- pkg/terraform/workspace/show_plan.go
+++ pkg/terraform/workspace/show_plan.go
@@ -2,10 +2,7 @@ package workspace
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
-	"os"
-	"path"
 
 	"github.com/hashicorp/go-hclog"
 	tfjson "github.com/hashicorp/terraform-json"
@@ -20,24 +17,11 @@ func (w *workspace) ShowPlan(ctx context.Context, log hclog.Logger) (*tfjson.Pla
 	return w.showPlan(ctx, client)
 }
 
-// TODO: revisit and use a callback to write to local instead of writing to local directly
 func (w *workspace) showPlan(ctx context.Context, client Terraform) (*tfjson.Plan, error) {
+	// NOTE: takes the tf plan and returns a json serializable *struct
 	out, err := client.ShowPlanFile(ctx, "tfplan")
 	if err != nil {
 		return nil, fmt.Errorf("unable to execute show: %w", err)
 	}
-
-	planJSON, err := json.MarshalIndent(out, "", "  ")
-	if err != nil {
-		return nil, fmt.Errorf("unable to marshal plan to JSON: %w", err)
-	}
-
-	// TODO: this should be legible from the workspace root but something is wrong in
-	// the local file writer callback pkg/terraform
-	pathToPlan := path.Join(w.Root(), "plan.json")
-	if err := os.WriteFile(pathToPlan, planJSON, 0644); err != nil {
-		return nil, fmt.Errorf("unable to write plan to file: %w", err)
-	}
-
 	return out, nil
 }
