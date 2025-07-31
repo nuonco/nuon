ccce88c8dac079a1cbbd23f1a3964023ee9c2257
Prem Saraswat <prmsrswt@gmail.com>
2025-07-31 13:57:26 +0530

    feat: CLI: improvements to input handling for install configs (#9809)
    * feat: CLI: improvements to input handling for install configs
    
    This patch adds a few improvements to how inputs are handled
    in install configs in nuon CLI.
    
    - Before creating an install, fetch app input config to set
      defaults, and validate required options
    - Before updating install inputs, fetch the current inputs
      and use them as defaults.
    - Better error messages
    
    Signed-off-by: Prem Saraswat <prem@nuon.co>
    
    * improve install sync: better handle defaults for inputs
    
    Signed-off-by: Prem Saraswat <prem@nuon.co>
    
    ---------
    
    Signed-off-by: Prem Saraswat <prem@nuon.co>

--- pkg/config/sync/installs_sync.go
+++ pkg/config/sync/installs_sync.go
@@ -2,6 +2,7 @@ package sync
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/nuonco/nuon-go"
 	"github.com/nuonco/nuon-go/models"
@@ -14,12 +15,36 @@ func (s *sync) syncInstall(ctx context.Context, resource string, install *config
 	appInstall, err := s.apiClient.GetInstall(ctx, install.Name)
 	if err != nil {
 		if !nuon.IsNotFound(err) {
-			return "", err
+			return "", SyncAPIErr{
+				Resource: resource,
+				Err:      fmt.Errorf("error getting install %s: %w", install.Name, err),
+			}
 		}
 		isNew = true
 	}
 
 	if isNew {
+		// Use defaults for any missing inputs.
+		{
+			appInputCfg, err := s.apiClient.GetAppInputLatestConfig(ctx, s.appID)
+			if err != nil {
+				return "", SyncAPIErr{
+					Resource: resource,
+					Err:      fmt.Errorf("error getting latest input config for app %s: %w", s.appID, err),
+				}
+			}
+
+			for _, ic := range appInputCfg.Inputs {
+				val, ok := install.Inputs[ic.Name]
+				if ok && val != "" {
+					continue
+				}
+				if ic.Default != "" {
+					install.Inputs[ic.Name] = ic.Default
+				}
+			}
+		}
+
 		appInstall, err = s.apiClient.CreateInstall(ctx, s.appID, &models.ServiceCreateInstallRequest{
 			Name:   &install.Name,
 			Inputs: install.Inputs,
@@ -30,17 +55,47 @@ func (s *sync) syncInstall(ctx context.Context, resource string, install *config
 		if err != nil {
 			return "", SyncAPIErr{
 				Resource: resource,
-				Err:      err,
+				Err:      fmt.Errorf("error creating install %s: %w", install.Name, err),
 			}
 		}
 	} else {
-		_, err := s.apiClient.UpdateInstallInputs(ctx, appInstall.ID, &models.ServiceUpdateInstallInputsRequest{
-			Inputs: install.Inputs,
-		})
+		currInputs, err := s.apiClient.GetInstallCurrentInputs(ctx, appInstall.ID)
 		if err != nil {
 			return "", SyncAPIErr{
 				Resource: resource,
-				Err:      err,
+				Err:      fmt.Errorf("error getting current inputs for install %s: %w", appInstall.Name, err),
+			}
+		}
+		// Use the current inputs as defaults, for missing values in the current inputs.
+		for k, v := range currInputs.Values {
+			if _, ok := install.Inputs[k]; !ok {
+				install.Inputs[k] = v
+			}
+		}
+
+		hasChanged := false
+		if len(install.Inputs) != len(currInputs.Values) {
+			hasChanged = true
+		} else {
+			// length is same, go through each input to see if any have changed.
+			for k, v := range install.Inputs {
+				if currInputs.Values[k] != v {
+					hasChanged = true
+					break
+				}
+			}
+		}
+
+		// If inputs have divereged, update the install inputs.
+		if hasChanged {
+			_, err = s.apiClient.UpdateInstallInputs(ctx, appInstall.ID, &models.ServiceUpdateInstallInputsRequest{
+				Inputs: install.Inputs,
+			})
+			if err != nil {
+				return "", SyncAPIErr{
+					Resource: resource,
+					Err:      fmt.Errorf("error updating inputs for install %s: %w", appInstall.Name, err),
+				}
 			}
 		}
 	}
