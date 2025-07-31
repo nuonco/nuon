e4f0b9abebe5570bf2064f36f08d774d5837adb1
Prem Saraswat <prmsrswt@gmail.com>
2025-07-31 20:39:36 +0530

    feat(cli): Support approval option for an install in config files (#9781)
    * feat(cli): Support approval behavious for an install in config files
    
    This patch allows users to define `approval_option` per install if they are
    using nuon cli to manage the installs.
    
    Signed-off-by: Prem Saraswat <prem@nuon.co>
    
    * cli: install sync: Set approval option as part of install creation
    
    Signed-off-by: Prem Saraswat <prem@nuon.co>
    
    * upgrade nuon-go to latest
    
    Signed-off-by: Prem Saraswat <prem@nuon.co>
    
    ---------
    
    Signed-off-by: Prem Saraswat <prem@nuon.co>

--- pkg/config/install.go
+++ pkg/config/install.go
@@ -1,17 +1,42 @@
 package config
 
-import "github.com/invopop/jsonschema"
+import (
+	"github.com/invopop/jsonschema"
+	"github.com/nuonco/nuon-go/models"
+)
+
+type InstallApprovalOption string
+
+const (
+	InstallApprovalOptionApproveAll InstallApprovalOption = "approve-all"
+	InstallApprovalOptionPrompt     InstallApprovalOption = "prompt"
+	InstallApprovalOptionUnknown    InstallApprovalOption = ""
+)
+
+func (o InstallApprovalOption) APIType() models.AppInstallApprovalOption {
+	switch o {
+	case InstallApprovalOptionApproveAll:
+		return models.AppInstallApprovalOptionApproveDashAll
+	case InstallApprovalOptionPrompt:
+		return models.AppInstallApprovalOptionPrompt
+	default:
+		// In case for unknown options, default to prompting for approval.
+		return models.AppInstallApprovalOptionPrompt
+	}
+}
 
 // Install is a flattened configuration type that allows us to define installs for an app.
 type Install struct {
-	Name      string            `mapstructure:"name" jsonschema:"required"`
-	AWSRegion string            `mapstructure:"aws_region,omitempty"`
-	Inputs    map[string]string `mapstructure:"inputs,omitempty"`
+	Name           string                `mapstructure:"name" jsonschema:"required"`
+	AWSRegion      string                `mapstructure:"aws_region,omitempty"`
+	ApprovalOption InstallApprovalOption `mapstructure:"approval_option,omitempty"`
+	Inputs         map[string]string     `mapstructure:"inputs,omitempty"`
 }
 
 func (a Install) JSONSchemaExtend(schema *jsonschema.Schema) {
 	addDescription(schema, "name", "name of the install")
 	addDescription(schema, "aws_region", "AWS region to use for the install, if not set, the default region will be used")
+	addDescription(schema, "approval_option", "approval option for the install, can be 'approve_all' or 'prompt'")
 	addDescription(schema, "inputs", "list of inputs")
 }
 
@@ -24,5 +49,9 @@ func (i *Install) parse() error {
 }
 
 func (i *Install) Validate() error {
+	if i == nil {
+		return nil
+	}
+
 	return nil
 }
