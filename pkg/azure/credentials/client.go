3d78a37b4d6f42dff903e4edd36c4e29b8b66927
Jordan Acosta <jordan@nuon.co>
2025-08-06 13:57:15 -0700

    feat: Ja/production ready azure runner (#9934)
    * feat(bins/runner): runner uses rbac auth for container registry
    
    * feat(bins/runner): support oci and rbac auth for azure
    
    * feat(bins/runner): log details of azure auth

--- pkg/azure/credentials/client.go
+++ pkg/azure/credentials/client.go
@@ -3,13 +3,26 @@ package credentials
 import (
 	"context"
 
+	azlog "github.com/Azure/azure-sdk-for-go/sdk/azcore/log"
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"go.uber.org/zap"
 )
 
 // Fetch uses the Azure SDK method NewDefaultAzureCredential to walk the chain of authentication mechanisms to get a credential.
 // If running in an Azure VM, it will use the identity assigned to the VM.
 // If running locally, it will the identity you have logged into from your local environment.
 // For more information, see: https://learn.microsoft.com/en-us/azure/developer/go/sdk/authentication/authentication-overview
-func Fetch(_ context.Context) (*azidentity.DefaultAzureCredential, error) {
+func Fetch(ctx context.Context, logger *zap.Logger) (*azidentity.DefaultAzureCredential, error) {
+	// TODO(ja): use pkg/ctx instead
+	// l, err := pkgctx.Logger(ctx)
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	azlog.SetListener(func(event azlog.Event, msg string) {
+		logger.Info(msg)
+	})
+	azlog.SetEvents(azidentity.EventAuthentication)
+
 	return azidentity.NewDefaultAzureCredential(nil)
 }
