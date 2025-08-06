a7a200ba867ddc9ac6e9021068a5e39751ad55cd
Prem Saraswat <prmsrswt@gmail.com>
2025-08-06 20:37:09 +0530

    feat: [cli] Add command to generate install config for an install (#9917)
    * feat: [cli] Add command to generate install config for an install
    
    This patch adds a new command to generate an install config file
    from a pre-existing Install.
    
    Signed-off-by: Prem Saraswat <prem@nuon.co>
    
    * cli: Rename `installs import` to `installs generate-config`
    
    Signed-off-by: Prem Saraswat <prem@nuon.co>
    
    * cli: generate-config, remove option to save to file
    
    Signed-off-by: Prem Saraswat <prem@nuon.co>
    
    * Fix ParseInstall
    
    ---------
    
    Signed-off-by: Prem Saraswat <prem@nuon.co>

---
+++ bins/cli/internal/installs/generate_config.go
@@ -0,0 +1,34 @@
+package installs
+
+import (
+	"context"
+
+	"github.com/powertoolsdev/mono/bins/cli/internal/lookup"
+	"github.com/powertoolsdev/mono/bins/cli/internal/ui"
+	"github.com/powertoolsdev/mono/pkg/config"
+)
+
+func (s *Service) GenerateConfig(ctx context.Context, installID string) error {
+	installID, err := lookup.InstallID(ctx, s.api, installID)
+	if err != nil {
+		return ui.PrintError(err)
+	}
+	view := ui.NewGetView()
+
+	install, err := s.api.GetInstall(ctx, installID)
+	if err != nil {
+		return view.Error(err)
+	}
+
+	curInps, err := s.api.GetInstallCurrentInputs(ctx, installID)
+	if err != nil {
+		return view.Error(err)
+	}
+
+	var ins config.Install
+	ins.ParseInstall(install, curInps)
+
+	ui.PrintTOML(ins)
+
+	return nil
+}
