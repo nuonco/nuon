555334258cc581402e60c4cb8b3658ca10320eb4
Jordan Acosta <jordan@nuon.co>
2025-08-06 13:57:15 -0700

    feat: Ja/production ready azure runner (#9934)
    * feat(bins/runner): runner uses rbac auth for container registry
    
    * feat(bins/runner): support oci and rbac auth for azure
    
    * feat(bins/runner): log details of azure auth

--- bins/runner/internal/pkg/registry/acr/fetch.go
+++ bins/runner/internal/pkg/registry/acr/fetch.go
@@ -2,22 +2,44 @@ package acr
 
 import (
 	"context"
+	"fmt"
 
+	pkgctx "github.com/powertoolsdev/mono/bins/runner/internal/pkg/ctx"
 	"github.com/powertoolsdev/mono/bins/runner/internal/pkg/registry"
+	"github.com/powertoolsdev/mono/pkg/azure/acr"
 	"github.com/powertoolsdev/mono/pkg/plugins/configs"
 )
 
 func FetchAccessInfo(ctx context.Context, cfg *configs.OCIRegistryRepository) (*registry.AccessInfo, error) {
-	// token, err := acr.GetRepositoryToken(ctx, cfg.ACRAuth, cfg.LoginServer)
-	// if err != nil {
-	// 	return nil, fmt.Errorf("unable to get acr token: %w", err)
-	// }
+	l, err := pkgctx.Logger(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	username := ""
+	password := ""
+	if cfg.OCIAuth != nil {
+		l.Info("plan includes oci auth credentials")
+		l.Info("using provided oci registry user and token")
+		username = cfg.OCIAuth.Username
+		password = cfg.OCIAuth.Password
+	} else {
+		l.Info("plan does not include oci auth credentials")
+		l.Info("getting token using azure rbac...")
+		token, err := acr.GetRepositoryToken(ctx, cfg.ACRAuth, cfg.LoginServer, l)
+		if err != nil {
+			return nil, fmt.Errorf("unable to get acr token: %w", err)
+		}
+		l.Info("got token using auzre rbac")
+		username = acr.DefaultACRUsername
+		password = token
+	}
 
 	return &registry.AccessInfo{
 		Image: cfg.Repository,
 		Auth: &registry.AccessInfoAuth{
-			Username:      cfg.OCIAuth.Username,
-			Password:      cfg.OCIAuth.Password,
+			Username:      username,
+			Password:      password,
 			ServerAddress: cfg.LoginServer,
 		},
 	}, nil
