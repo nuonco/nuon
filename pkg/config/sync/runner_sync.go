c60044ae4003e67c5ac0116f2cfa07c28b676136
Dat Boi Diego <fidiego@users.noreply.github.com>
2025-08-06 14:39:41 -0500

    feat: make the runner init script configurable (#9921)
    feat: pkg/config: runner - configurable init script

--- pkg/config/sync/runner_sync.go
+++ pkg/config/sync/runner_sync.go
@@ -8,10 +8,11 @@ import (
 
 func (s sync) syncAppRunner(ctx context.Context, resource string) error {
 	cfg, err := s.apiClient.CreateAppRunnerConfig(ctx, s.appID, &models.ServiceCreateAppRunnerConfigRequest{
-		AppConfigID: s.appConfigID,
-		EnvVars:     s.cfg.Runner.EnvVarMap,
-		HelmDriver:  models.AppAppRunnerConfigHelmDriverType(s.cfg.Runner.HelmDriver),
-		Type:        models.NewAppAppRunnerType(models.AppAppRunnerType(s.cfg.Runner.RunnerType)),
+		AppConfigID:   s.appConfigID,
+		EnvVars:       s.cfg.Runner.EnvVarMap,
+		HelmDriver:    models.AppAppRunnerConfigHelmDriverType(s.cfg.Runner.HelmDriver),
+		InitScriptURL: s.cfg.Runner.InitScriptURL,
+		Type:          models.NewAppAppRunnerType(models.AppAppRunnerType(s.cfg.Runner.RunnerType)),
 	})
 	if err != nil {
 		return SyncAPIErr{
