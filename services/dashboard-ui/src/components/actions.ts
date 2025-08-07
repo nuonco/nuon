8d1ee6b88c4c56117b771a0c19ba84af7d137f8e
Nat Hamilton <info@nathamilton.com>
2025-08-07 09:53:38 -0400

    chore(ui): update to latest auth0, log error in useEffect, updated co… (#9945)
    * chore(ui): update to latest auth0, log error in useEffect, updated config
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * chore(ui): log error in ErrorFallback
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * refactor(ui): server cookie for sidebar state, update dev deps
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * chore(ui): update config
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * chore(ui): run test in CI
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * chore(ui): removed type: module form pkg.json
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * chore(ui): update docker to node 20
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/components/actions.ts
+++ services/dashboard-ui/src/components/actions.ts
@@ -1,6 +1,7 @@
 'use server'
 
 import { revalidatePath, revalidateTag } from 'next/cache'
+import { cookies } from 'next/headers'
 
 interface IRevalidateData {
   path?: string,
@@ -15,4 +16,9 @@ export async function revalidateData({ path, tag }: IRevalidateData) {
   if (tag && !path) {
     revalidateTag(tag)
   }
+}
+
+export async function setDashboardSidebarCookie(isOpen: boolean) {
+  const cookieStore = await cookies()
+  cookieStore.set('is-sidebar-open', Boolean(isOpen).toString())
 }
