9fc4c4aebf699e1767eec3324777d5cfa49a1f54
robisso <alphavibe@gmail.com>
2025-08-08 13:38:30 -0700

    feat: Revert feat app config to configs endpoint rename (#9968)
    Revert "feat: app config to configs endpoint rename (#9906)"
    
    This reverts commit d716d1e517f0f02a5d11a6cc49730c7a64c4fb42.

--- services/ctl-api/internal/app/apps/service/get_app_config_graph.go
+++ services/ctl-api/internal/app/apps/service/get_app_config_graph.go
@@ -18,7 +18,6 @@ import (
 // @Produce				json
 // @Security				APIKey
 // @Security				OrgID
-// @Deprecated
 // @Failure				400	{object}	stderr.ErrResponse
 // @Failure				401	{object}	stderr.ErrResponse
 // @Failure				403	{object}	stderr.ErrResponse
@@ -28,47 +27,6 @@ import (
 // @Router					/v1/apps/{app_id}/config/{app_config_id}/graph [get]
 func (s *service) GetAppConfigGraph(ctx *gin.Context) {
 	appConfigID := ctx.Param("app_config_id")
-
-	appConfig, err := s.helpers.GetFullAppConfig(ctx, appConfigID)
-	if err != nil {
-		ctx.Error(err)
-		return
-	}
-
-	graph, err := s.helpers.GetConfigGraph(ctx, appConfig)
-	if err != nil {
-		ctx.Error(err)
-		return
-	}
-
-	var buf bytes.Buffer
-	if err := draw.DOT(graph, &buf, draw.GraphAttribute("name", "name")); err != nil {
-		ctx.Error(err)
-		return
-	}
-
-	ctx.JSON(http.StatusOK, buf.String())
-}
-
-// @ID						GetAppAppConfigGraph
-// @Summary				get an app config graph
-// @Description.markdown	get_app_config_graph.md
-// @Param					app_id			path	string	true	"app ID"
-// @Param					config_id	path	string	true	"app config ID"
-// @Tags					apps
-// @Accept					json
-// @Produce				json
-// @Security				APIKey
-// @Security				OrgID
-// @Failure				400	{object}	stderr.ErrResponse
-// @Failure				401	{object}	stderr.ErrResponse
-// @Failure				403	{object}	stderr.ErrResponse
-// @Failure				404	{object}	stderr.ErrResponse
-// @Failure				500	{object}	stderr.ErrResponse
-// @Success				200	{object}	string
-// @Router					/v1/apps/{app_id}/config/{app_config_id}/graph [get]
-func (s *service) GetAppAppConfigGraph(ctx *gin.Context) {
-	appConfigID := ctx.Param("config_id")
 
 	appConfig, err := s.helpers.GetFullAppConfig(ctx, appConfigID)
 	if err != nil {
