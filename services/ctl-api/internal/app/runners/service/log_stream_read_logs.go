05f3ed5e56872a3d261c1f76385c0823c552583d
Dat Boi Diego <fidiego@users.noreply.github.com>
2025-08-08 16:52:45 -0500

    chore: log stream: remove filtering by resource and log attributes (#9970)

--- services/ctl-api/internal/app/runners/service/log_stream_read_logs.go
+++ services/ctl-api/internal/app/runners/service/log_stream_read_logs.go
@@ -4,16 +4,13 @@ import (
 	"context"
 	"fmt"
 	"net/http"
-	"regexp"
 	"strconv"
-	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/pkg/errors"
 
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/pkg/cctx"
-	chhelpers "github.com/powertoolsdev/mono/services/ctl-api/internal/pkg/ch/helpers"
 )
 
 const (
@@ -21,23 +18,23 @@ const (
 	nestedAttributeRegex string = `^(?:[a-zA-Z0-9_]+(?:\.[a-zA-Z0-9_]+)?)$` // https://regex101.com/r/179bxx/1
 )
 
-//	@ID						LogStreamReadLogs
-//	@Summary				read a log stream's logs
-//	@Description.markdown	log_stream_read_logs.md
-//	@Param					log_stream_id		path	string	true	"log stream ID"
-//	@Param					X-Nuon-API-Offset	header	string	true	"log stream offset"
-//	@Tags					runners
-//	@Accept					json
-//	@Produce				json
-//	@Security				APIKey
-//	@Security				OrgID
-//	@Failure				400	{object}	stderr.ErrResponse
-//	@Failure				401	{object}	stderr.ErrResponse
-//	@Failure				403	{object}	stderr.ErrResponse
-//	@Failure				404	{object}	stderr.ErrResponse
-//	@Failure				500	{object}	stderr.ErrResponse
-//	@Success				200	{object}	[]app.OtelLogRecord
-//	@Router					/v1/log-streams/{log_stream_id}/logs [GET]
+// @ID						LogStreamReadLogs
+// @Summary				read a log stream's logs
+// @Description.markdown	log_stream_read_logs.md
+// @Param					log_stream_id		path	string	true	"log stream ID"
+// @Param					X-Nuon-API-Offset	header	string	true	"log stream offset"
+// @Tags					runners
+// @Accept					json
+// @Produce				json
+// @Security				APIKey
+// @Security				OrgID
+// @Failure				400	{object}	stderr.ErrResponse
+// @Failure				401	{object}	stderr.ErrResponse
+// @Failure				403	{object}	stderr.ErrResponse
+// @Failure				404	{object}	stderr.ErrResponse
+// @Failure				500	{object}	stderr.ErrResponse
+// @Success				200	{object}	[]app.OtelLogRecord
+// @Router					/v1/log-streams/{log_stream_id}/logs [GET]
 func (s *service) LogStreamReadLogs(ctx *gin.Context) {
 	// get the otel logs in order of their timstamp (desc)
 	// returns pagination details in the header
@@ -68,24 +65,24 @@ func (s *service) LogStreamReadLogs(ctx *gin.Context) {
 	}
 
 	// grab any and all attribute query params
-	queryParams := ctx.Request.URL.Query()
-	resourceAttrQueryParams := map[string]string{}
-	logAttrQueryParams := map[string]string{}
-	for key, value := range queryParams {
-		if strings.HasPrefix(key, "resource.") {
-			newKey := strings.Replace(key, "resource.", "", 1)
-			match, _ := regexp.MatchString(nestedAttributeRegex, newKey)
-			if match {
-				resourceAttrQueryParams[newKey] = value[0]
-			}
-		} else if strings.HasPrefix(key, "log.") {
-			newKey := strings.Replace(key, "log.", "", 1)
-			match, _ := regexp.MatchString(nestedAttributeRegex, newKey)
-			if match {
-				logAttrQueryParams[newKey] = value[0]
-			}
-		}
-	}
+	// queryParams := ctx.Request.URL.Query()
+	// resourceAttrQueryParams := map[string]string{}
+	// logAttrQueryParams := map[string]string{}
+	// for key, value := range queryParams {
+	// 	if strings.HasPrefix(key, "resource.") {
+	// 		newKey := strings.Replace(key, "resource.", "", 1)
+	// 		match, _ := regexp.MatchString(nestedAttributeRegex, newKey)
+	// 		if match {
+	// 			resourceAttrQueryParams[newKey] = value[0]
+	// 		}
+	// 	} else if strings.HasPrefix(key, "log.") {
+	// 		newKey := strings.Replace(key, "log.", "", 1)
+	// 		match, _ := regexp.MatchString(nestedAttributeRegex, newKey)
+	// 		if match {
+	// 			logAttrQueryParams[newKey] = value[0]
+	// 		}
+	// 	}
+	// }
 
 	// read logs from chDB
 	orgID, err := cctx.OrgIDFromContext(ctx)
@@ -93,7 +90,7 @@ func (s *service) LogStreamReadLogs(ctx *gin.Context) {
 		ctx.Error(errors.Wrap(err, "unable to read org id from context"))
 		return
 	}
-	logs, headers, readErr := s.getLogStreamLogs(ctx, logStreamID, orgID, after, resourceAttrQueryParams, logAttrQueryParams)
+	logs, headers, readErr := s.getLogStreamLogs(ctx, logStreamID, orgID, after)
 	if readErr != nil {
 		ctx.Error(errors.Wrap(readErr, "unable to read runner logs"))
 		return
@@ -107,7 +104,7 @@ func (s *service) LogStreamReadLogs(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, logs)
 }
 
-func (s *service) getLogStreamLogs(ctx context.Context, logStreamID string, orgID string, after int64, resourceAttrQueryParams map[string]string, logAttrQueryParams map[string]string) ([]app.OtelLogRecord, map[string]string, error) {
+func (s *service) getLogStreamLogs(ctx context.Context, logStreamID string, orgID string, after int64) ([]app.OtelLogRecord, map[string]string, error) {
 	// headers
 	headers := map[string]string{"Range-Units": "items"}
 
@@ -120,16 +117,6 @@ func (s *service) getLogStreamLogs(ctx context.Context, logStreamID string, orgI
 
 	if after != 0 {
 		res.Where("toUnixTimestamp64Nano(timestamp) > ?", after)
-	}
-
-	for key, value := range resourceAttrQueryParams {
-		columnName := chhelpers.NestedColumnName("resource_attributes", key)
-		res.Where(fmt.Sprintf("%s = ?", res.Config.NamingStrategy.ColumnName("", columnName)), value)
-	}
-
-	for key, value := range logAttrQueryParams {
-		columnName := chhelpers.NestedColumnName("log_attributes", key)
-		res.Where(fmt.Sprintf("%s = ?", res.Config.NamingStrategy.ColumnName("", columnName)), value)
 	}
 
 	res.
