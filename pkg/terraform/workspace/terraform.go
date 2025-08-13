3c3b4607a9b7b1d1ff41998c48bb52c87560e4f2
Dat Boi Diego <fidiego@users.noreply.github.com>
2025-08-13 18:59:17 -0500

    chore: pkg/terraform: make plan easier to find (#10036)

--- pkg/terraform/workspace/terraform.go
+++ pkg/terraform/workspace/terraform.go
@@ -136,44 +136,6 @@ func (w *workspace) destroy(ctx context.Context, client Terraform, log hclog.Log
 	return out.Bytes()
 }
 
-func (w *workspace) Plan(ctx context.Context, log hclog.Logger) ([]byte, error) {
-	client, err := w.getClient(ctx, log)
-	if err != nil {
-		return nil, err
-	}
-
-	return w.plan(ctx, client, log)
-}
-
-func (w *workspace) plan(ctx context.Context, client Terraform, log hclog.Logger) ([]byte, error) {
-	out, err := output.New(w.v, output.WithLogger(log))
-	if err != nil {
-		return nil, fmt.Errorf("unable to get output: %w", err)
-	}
-
-	writer, err := out.Writer()
-	if err != nil {
-		return nil, fmt.Errorf("unable to get writer: %w", err)
-	}
-
-	opts := []tfexec.PlanOption{
-		tfexec.Refresh(true),
-		tfexec.Out("tfplan"), // NOTE: this should probably be configured w/ a WithPlanOut
-	}
-	for _, fp := range w.varsPaths {
-		opts = append(opts, tfexec.VarFile(fp))
-	}
-
-	if _, err := client.PlanJSON(ctx,
-		writer,
-		opts...,
-	); err != nil {
-		return nil, fmt.Errorf("unable to plan: %w", err)
-	}
-
-	return out.Bytes()
-}
-
 func (w *workspace) Refresh(ctx context.Context, log hclog.Logger) ([]byte, error) {
 	client, err := w.getClient(ctx, log)
 	if err != nil {
