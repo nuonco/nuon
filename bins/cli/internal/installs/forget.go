097ba782e211e007e0138faad8891610b9fe9699
robisso <alphavibe@gmail.com>
2025-08-12 23:06:54 -0700

    feat: cli forget install (#10023)
    * fix: cli forget install
    
    * fix: cli forget install
    
    * fix: cli forget install
    
    * fix: go mod tidy

---
+++ bins/cli/internal/installs/forget.go
@@ -0,0 +1,36 @@
+package installs
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/powertoolsdev/mono/bins/cli/internal/lookup"
+	"github.com/powertoolsdev/mono/bins/cli/internal/ui"
+)
+
+func (s *Service) Forget(ctx context.Context, installID string, asJSON bool) error {
+	fmt.Println("rb - forgetting install", installID)
+	installID, err := lookup.InstallID(ctx, s.api, installID)
+	if err != nil {
+		return ui.PrintError(err)
+	}
+
+	res, err := s.api.ForgetInstall(ctx, installID)
+	if err != nil {
+		return ui.PrintJSONError(err)
+	}
+
+	if asJSON {
+		type response struct {
+			ID        string `json:"id"`
+			Forgotten bool   `json:"forgotten"`
+		}
+		r := response{ID: installID, Forgotten: res}
+		ui.PrintJSON(r)
+		return nil
+	}
+
+	ui.PrintLn(fmt.Sprintf("forget install %s request sent.", installID))
+
+	return nil
+}
