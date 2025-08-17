4dbffc53b094efcbc61fe70625118e4d957d4ff0
Jordan Acosta <jordan@nuon.co>
2025-08-16 18:38:24 -0700

    fix(bins/cli): installs deploy all components supports plan-only (#10064)
    Co-authored-by: nuonbot <bot@nuon.co>

--- bins/cli/internal/installs/deploy_components.go
+++ bins/cli/internal/installs/deploy_components.go
@@ -2,19 +2,21 @@ package installs
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/powertoolsdev/mono/bins/cli/internal/lookup"
 	"github.com/powertoolsdev/mono/bins/cli/internal/ui"
 )
 
-func (s *Service) DeployComponents(ctx context.Context, installID string, asJSON bool) error {
+func (s *Service) DeployComponents(ctx context.Context, installID string, planOnly bool, asJSON bool) error {
 	installID, err := lookup.InstallID(ctx, s.api, installID)
 	if err != nil {
 		return ui.PrintError(err)
 	}
 
-	err = s.api.DeployInstallComponents(ctx, installID)
+	err = s.api.DeployInstallComponents(ctx, installID, planOnly)
 	if err != nil {
+		fmt.Printf("deploy components err: %+s\n", err)
 		return ui.PrintJSONError(err)
 	}
 
