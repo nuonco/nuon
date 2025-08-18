50c9d8d067c2a4444115516b60dac35c18518ec5
Dat Boi Diego <fidiego@users.noreply.github.com>
2025-08-18 18:08:08 -0500

    chore: runner mng mode: restart job to restart `nuon-runner.service`' (#10081)
    * chore: ctl-api: mng update job
    
    * chore: runner: mng update

---
+++ services/ctl-api/internal/app/runners/worker/mng_update.go
@@ -0,0 +1,34 @@
+package worker
+
+import (
+	"go.temporal.io/sdk/workflow"
+
+	"github.com/pkg/errors"
+
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app/runners/signals"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app/runners/worker/activities"
+)
+
+// @temporal-gen workflow
+// @execution-timeout 10m
+// @task-timeout 5m
+func (w *Workflows) MngUpdate(ctx workflow.Context, sreq signals.RequestSignal) error {
+	runnerJob, err := w.createMngJob(ctx, sreq.ID, app.RunnerJobTypeMngRunnerUpdateVersion, map[string]string{
+		"upgrade_type": "mng",
+	})
+	if err != nil {
+		return errors.Wrap(err, "unable to create runner job")
+	}
+
+	// automatically set the job status to available, so it is picked up, regardless of what else is in flight.
+	if err := activities.AwaitUpdateJobStatus(ctx, activities.UpdateJobStatusRequest{
+		JobID:             runnerJob.ID,
+		Status:            app.RunnerJobStatusAvailable,
+		StatusDescription: string(app.RunnerJobStatusAvailable),
+	}); err != nil {
+		return errors.Wrap(err, "unable to update job status")
+	}
+
+	return nil
+}
