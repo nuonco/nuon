44637325da135e925b6af253a22332ae5b70daad
Dat Boi Diego <fidiego@users.noreply.github.com>
2025-08-18 18:08:08 -0500

    chore: runner mng mode: restart job to restart `nuon-runner.service`' (#10081)
    * chore: ctl-api: mng update job
    
    * chore: runner: mng update

--- services/ctl-api/internal/app/runners/service/service.go
+++ services/ctl-api/internal/app/runners/service/service.go
@@ -58,6 +58,7 @@ func (s *service) RegisterPublicRoutes(api *gin.Engine) error {
 	api.POST("/v1/runners/:runner_id/force-shutdown", s.ForceShutDown)
 	api.POST("/v1/runners/:runner_id/mng/shutdown-vm", s.MngVMShutDown)
 	api.POST("/v1/runners/:runner_id/mng/shutdown", s.MngShutDown)
+	api.POST("/v1/runners/:runner_id/mng/update", s.MngUpdate)
 
 	// settings
 	api.GET("/v1/runners/:runner_id/settings", s.GetRunnerSettingsPublic)
