6ef7fa286c7bffe4c56859fa9be38edd26e20b31
Erick Yellott <yellott@nuon.co>
2025-08-21 13:34:03 -0500

    chore: random lint fixes (#10043)

--- pkg/helm/operations/template.go
+++ pkg/helm/operations/template.go
@@ -1,28 +1,27 @@
+// TODO: Is this file needed?
 package operations
-
-import "context"
 
 // note: releaseutil.SplitManifests is a thing
 
-func (o *operations) Template(ctx context.Context, *action.Install, ) error {
-	client := action.NewUpgrade(actionCfg)
-	client.DryRun = true
-	client.DisableHooks = false
-	client.Wait = true
-	client.WaitForJobs = false
-	client.Devel = true
-	client.DependencyUpdate = true
-	client.Timeout = h.state.timeout
-	client.Namespace = h.state.plan.HelmDeployPlan.Namespace
-	client.Atomic = false
-	client.SkipCRDs = false
-	client.SubNotes = true
-	client.DisableOpenAPIValidation = false
-	client.Description = ""
-	client.ResetValues = false
-	client.ReuseValues = false
-	client.Recreate = false
-	client.MaxHistory = 0
-	client.CleanupOnFail = false
-	client.Force = false
-}
+// func (o *operations) Template(ctx context.Context, *action.Install, ) error {
+// 	client := action.NewUpgrade(actionCfg)
+// 	client.DryRun = true
+// 	client.DisableHooks = false
+// 	client.Wait = true
+// 	client.WaitForJobs = false
+// 	client.Devel = true
+// 	client.DependencyUpdate = true
+// 	client.Timeout = h.state.timeout
+// 	client.Namespace = h.state.plan.HelmDeployPlan.Namespace
+// 	client.Atomic = false
+// 	client.SkipCRDs = false
+// 	client.SubNotes = true
+// 	client.DisableOpenAPIValidation = false
+// 	client.Description = ""
+// 	client.ResetValues = false
+// 	client.ReuseValues = false
+// 	client.Recreate = false
+// 	client.MaxHistory = 0
+// 	client.CleanupOnFail = false
+// 	client.Force = false
+// }
