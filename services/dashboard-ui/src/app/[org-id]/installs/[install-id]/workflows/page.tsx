c7facce581bdb6bbdb49c03ed5be16c055e7da4a
Prem Saraswat <prmsrswt@gmail.com>
2025-08-21 13:05:08 +0530

    feat: Show Install is managed by config file in UI (#10108)
    * feat: Show Install is managed by config file in UI
    
    This PR adds a tag to Installs that are created or updated
    with the help of Config files. Using this tag we show 'Managed
    by Config File' tag along with statuses on Install's page in UI.
    
    Signed-off-by: Prem Saraswat <prem@nuon.co>
    
    * Upgrade nuon-go to latest
    
    * better name for variable
    
    Signed-off-by: Prem Saraswat <prem@nuon.co>
    
    ---------
    
    Signed-off-by: Prem Saraswat <prem@nuon.co>

--- services/dashboard-ui/src/app/[org-id]/installs/[install-id]/workflows/page.tsx
+++ services/dashboard-ui/src/app/[org-id]/installs/[install-id]/workflows/page.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { type FC, Suspense } from 'react'
+import { FileCodeIcon } from '@phosphor-icons/react/dist/ssr'
 import {
   DashboardContent,
   Link,
@@ -57,6 +58,16 @@ export default async function Install({ params, searchParams }) {
       }
       statues={
         <div className="flex items-start gap-8">
+          {install?.metadata?.managed_by &&
+          install?.metadata?.managed_by === 'nuon/cli/install-config' ? (
+            <span className="flex flex-col gap-2">
+              <Text isMuted>Managed By</Text>
+              <Text>
+                <FileCodeIcon />
+                Config File
+              </Text>
+            </span>
+          ) : null}
           <span className="flex flex-col gap-2">
             <Text isMuted>App config</Text>
             <Text>
