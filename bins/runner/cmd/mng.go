59d2aa9774d00161e3fbb80cd7022ccc0ec9ea73
Dat Boi Diego <fidiego@users.noreply.github.com>
2025-08-25 16:39:18 -0500

    feat(runner): split run into two commands (#10162)
    * feat(runner): new standalone commands
    
    * chore(runner): mng command: add description
    
    * chore(runner): use the new commands during dev
    
    * chore(runner): add alternative run cmds to service.yml

--- bins/runner/cmd/mng.go
+++ bins/runner/cmd/mng.go
@@ -9,9 +9,11 @@ import (
 
 func (c *cli) registerMng() error {
 	mngCmd := &cobra.Command{
-		Use:  "mng",
-		Long: "mng executes the runner management mode to start and monitor a runner.",
-		Run:  c.runMng,
+		Use:     "mng",
+		Short:   "Run in management mode.",
+		Long:    "Run in management mode and oversee an install mode process in a standalone VM.",
+		Aliases: []string{"management"},
+		Run:     c.runMng,
 	}
 
 	rootCmd.AddCommand(mngCmd)
@@ -20,10 +22,7 @@ func (c *cli) registerMng() error {
 
 func (c *cli) runMng(cmd *cobra.Command, _ []string) {
 	providers := []fx.Option{fx.Provide(log.NewSystem)}
-
 	providers = append(c.commonProviders(), providers...)
-
 	providers = append(providers, management.GetJobs()...)
-
 	fx.New(providers...).Run()
 }
