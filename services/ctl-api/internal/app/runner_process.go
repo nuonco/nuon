e08fe64bc599ec18b8375326485ae252f3a13a92
Dat Boi Diego <fidiego@users.noreply.github.com>
2025-08-26 17:15:13 -0500

    feat(ctl-api): accept process in heartbeat & healthcheck endpoints' (#10187)
    * chore(ctl-api): add process to the hearbeat endpoint
    
    * chore(ctl-api): runner health check - add process
    
    * chore(ctl-apio): rename a const
    
    * chore(clt-api): better default value for process

---
+++ services/ctl-api/internal/app/runner_process.go
@@ -0,0 +1,39 @@
+package app
+
+import (
+	"database/sql/driver"
+	"fmt"
+)
+
+// Now that we run third type of runner process, we need to distinguish which process is sending the heartbear
+// NOTE(fd): we have to implement Scan/Value so the gorm ch plugin doesn't complain
+type RunnerProcess string
+
+func (rp RunnerProcess) Value() (driver.Value, error) {
+	return string(rp), nil
+}
+
+func (rp *RunnerProcess) Scan(value any) error {
+	if value == nil {
+		*rp = ""
+		return nil
+	}
+
+	switch v := value.(type) {
+	case string:
+		*rp = RunnerProcess(v)
+	case []byte:
+		*rp = RunnerProcess(v)
+	default:
+		return fmt.Errorf("cannot scan %T into RunnerProcess", value)
+	}
+
+	return nil
+}
+
+const (
+	RunnerProcessMng     RunnerProcess = "mng"
+	RunnerProcessInstall RunnerProcess = "install"
+	RunnerProcessOrg     RunnerProcess = "org"
+	RunnerProcessUknown  RunnerProcess = ""
+)
