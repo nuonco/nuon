8f0b019fa17f28ff26406cb655c26310a1d95c53
Nat Hamilton <info@nathamilton.com>
2025-08-28 14:30:15 -0400

    refactor(ui): api pagination and search to org switcher (#10212)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/components/SearchInput.tsx
+++ services/dashboard-ui/src/components/SearchInput.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import classNames from "classnames";
+import classNames from 'classnames'
 import React from 'react'
 import { MagnifyingGlass, XCircle } from '@phosphor-icons/react'
 import { Button } from './Button'
@@ -8,16 +8,18 @@ import { Button } from './Button'
 export interface ISearchInput
   extends Omit<
     React.HTMLAttributes<HTMLInputElement>,
-    "autoComplete" | "onChange" | "type"
+    'autoComplete' | 'onChange' | 'type'
   > {
-  placeholder?: string;
-  onChange: (val: string) => void;
-  onClear?: () => void;
-  value: string;
+  labelClassName?: string
+  placeholder?: string
+  onChange: (val: string) => void
+  onClear?: () => void
+  value: string
 }
 
 export const SearchInput = ({
   className,
+  labelClassName,
   placeholder,
   onChange,
   onClear,
@@ -25,15 +27,17 @@ export const SearchInput = ({
   ...props
 }: ISearchInput) => {
   return (
-    <label className="relative w-fit flex">
-      <MagnifyingGlass
-        className="text-cool-grey-500 dark:text-cool-grey-600 absolute top-2.5 left-2"
-      />
+    <label
+      className={classNames('relative w-fit flex', {
+        [`${labelClassName}`]: Boolean(labelClassName),
+      })}
+    >
+      <MagnifyingGlass className="text-cool-grey-500 dark:text-cool-grey-600 absolute top-2.5 left-2" />
       <input
         className={classNames(
-          "rounded-md pl-8 pr-3.5 py-1.5 h-[36px] font-sans md:min-w-80 border text-sm bg-white dark:bg-dark-grey-100 placeholder:text-cool-grey-500 dark:placeholder:text-cool-grey-700",
+          'rounded-md pl-8 pr-3.5 py-1.5 h-[36px] font-sans md:min-w-80 border text-sm bg-white dark:bg-dark-grey-100 placeholder:text-cool-grey-500 dark:placeholder:text-cool-grey-700',
           {
-            [`${className}`]: Boolean(className)
+            [`${className}`]: Boolean(className),
           }
         )}
         type="search"
@@ -48,11 +52,11 @@ export const SearchInput = ({
           className="!p-0.5 !h-fit absolute top-1/2 right-1.5 -translate-y-1/2"
           variant="ghost"
           title="clear search"
-          onClick={() => (onClear ? onClear() : onChange(""))}
+          onClick={() => (onClear ? onClear() : onChange(''))}
         >
           <XCircle />
         </Button>
       ) : null}
     </label>
-  );
-};
+  )
+}
