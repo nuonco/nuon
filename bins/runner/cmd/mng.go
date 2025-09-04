ff85a115b8685901758589003d77e37182be5324
Dat Boi Diego <fidiego@users.noreply.github.com>
2025-09-04 10:27:49 -0500

    chore: runner: provide a process to the heartbeater (#10284)
    * chore: runner: provide a process to the heartbeater
    
    so heartbeats can be sent out with a process for distinguishing between
    install, build, and mng processes
    
    * chore: add heartbeater to mng cmd
    
    * chore(runner): provide "process" to run cmd

--- bins/runner/cmd/mng.go
+++ bins/runner/cmd/mng.go
@@ -2,6 +2,8 @@ package cmd
 
 import (
 	"github.com/powertoolsdev/mono/bins/runner/internal/jobs/management"
+	"github.com/powertoolsdev/mono/bins/runner/internal/pkg/heartbeater"
+	"github.com/powertoolsdev/mono/bins/runner/internal/pkg/jobloop"
 	"github.com/powertoolsdev/mono/bins/runner/internal/pkg/log"
 	"github.com/spf13/cobra"
 	"go.uber.org/fx"
@@ -24,5 +26,18 @@ func (c *cli) runMng(cmd *cobra.Command, _ []string) {
 	providers := []fx.Option{fx.Provide(log.NewSystem)}
 	providers = append(c.commonProviders(), providers...)
 	providers = append(providers, management.GetJobs()...)
+	// add mng and heartbeater to the mng process
+	providers = append(providers,
+		[]fx.Option{
+			// provide process for the heartbeater
+			fx.Supply(fx.Annotate("mng", fx.ResultTags(`name:"process"`))),
+			// start all job loops
+			fx.Invoke(jobloop.WithJobLoops(func([]jobloop.JobLoop) {})),
+			// NOTE: we do not include the `operations` job loops here
+			// start registry and heartbeater
+			fx.Invoke(func(*heartbeater.HeartBeater) {}),
+		}...,
+	)
+	// run
 	fx.New(providers...).Run()
 }
