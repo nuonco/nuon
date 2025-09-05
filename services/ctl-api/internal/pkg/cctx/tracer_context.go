13a18ede9bbf70bbf5beab7b92a37b5e75c57141
robisso <alphavibe@gmail.com>
2025-09-05 11:56:48 -0700

    feat: refactor log middleware (#10294)
    * feat: refactor log middleware
    
    * feat: ctx api logger
    
    * feat: trace-id
    
    * feat: trace-id
    
    * feat: trace-id
    
    * feat: add trace-id to workflows
    
    * feat: clean up stuff
    
    * feat: add nctl log helper
    
    * feat: update
    
    * feat: update k8 middlewares

---
+++ services/ctl-api/internal/pkg/cctx/tracer_context.go
@@ -0,0 +1,32 @@
+package cctx
+
+import (
+	"context"
+
+	"github.com/gin-gonic/gin"
+	"go.temporal.io/sdk/workflow"
+
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/pkg/cctx/keys"
+)
+
+func TraceIDFromContext(ctx ValueContext) string {
+	traceID := ctx.Value(keys.TraceIDCtxKey)
+	if traceID == nil {
+		// Generate a new trace ID if not present
+		return ""
+	}
+
+	return traceID.(string)
+}
+
+func SetTraceIDGinContext(ctx *gin.Context, traceID string) {
+	ctx.Set(keys.TraceIDCtxKey, traceID)
+}
+
+func SetTraceIDContext(ctx context.Context, traceID string) context.Context {
+	return context.WithValue(ctx, keys.TraceIDCtxKey, traceID)
+}
+
+func SetTraceIDWorkflowContext(ctx workflow.Context, traceID string) workflow.Context {
+	return workflow.WithValue(ctx, keys.TraceIDCtxKey, traceID)
+}
