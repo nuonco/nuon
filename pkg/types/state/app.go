ef27b379863ef3cee7632bbe0f4ed893ed421442
Jordan Acosta <jordan@nuon.co>
2025-09-05 13:29:38 -0700

    feat: app variables (#10293)
    * feat(bins/cli) deprecate `secrets` add `apps inputs`
    
    - Deprecate the `nuon secrets` command
    - Add `nuon apps inputs` to replace it
    
    * feat(docs): document app inputs
    
    * feat(services/ctl-api): rename app secrets in install state to app
    inputs
    
    * fix(bins/cli): messed up a rebase
    
    * feat(bins/cli): `apps inputs` to `apps variables`
    
    * feat(docs): rename app inputs to app variables
    
    * feat: app inputs to app variables in install state
    
    * fix(docs): app variables will not be managed in config files
    
    * feat(docs): changelog 18

--- pkg/types/state/app.go
+++ pkg/types/state/app.go
@@ -2,15 +2,15 @@ package state
 
 func NewAppState() *AppState {
 	return &AppState{
-		Secrets: make(map[string]string, 0),
+		Variables: make(map[string]string, 0),
 	}
 }
 
 type AppState struct {
 	Populated bool `json:"populated"`
 
-	Status  string            `json:"status"`
-	ID      string            `json:"id"`
-	Name    string            `json:"name"`
-	Secrets map[string]string `json:"secrets"`
+	Status    string            `json:"status"`
+	ID        string            `json:"id"`
+	Name      string            `json:"name"`
+	Variables map[string]string `json:"variables"`
 }
