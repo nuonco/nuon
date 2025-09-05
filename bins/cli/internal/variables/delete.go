1d4173ca3d87b9ab9d1f71508cf3e7c591a77995
Jordan Acosta <jordan@nuon.co>
2025-09-05 13:29:38 -0700

    feat: app variables (#10293)
    * feat(bins/cli) deprecate `secrets` add `apps inputs`
    
    - Deprecate the `nuon secrets` command
    - Add `nuon apps inputs` to replace it
    
    * feat(docs): document app inputs
    
    * feat(services/ctl-api): rename app secrets in install state to app
    inputs
    
    * fix(bins/cli): messed up a rebase
    
    * feat(bins/cli): `apps inputs` to `apps variables`
    
    * feat(docs): rename app inputs to app variables
    
    * feat: app inputs to app variables in install state
    
    * fix(docs): app variables will not be managed in config files
    
    * feat(docs): changelog 18

---
+++ bins/cli/internal/variables/delete.go
@@ -0,0 +1,39 @@
+package variables
+
+import (
+	"context"
+
+	"github.com/powertoolsdev/mono/bins/cli/internal/lookup"
+	"github.com/powertoolsdev/mono/bins/cli/internal/ui"
+)
+
+func (s *Service) Delete(ctx context.Context, appID, variableID string, asJSON bool) error {
+	appID, err := lookup.AppID(ctx, s.api, appID)
+	if err != nil {
+		return ui.PrintError(err)
+	}
+
+	if asJSON {
+		res, err := s.api.DeleteAppSecret(ctx, appID, variableID)
+		if err != nil {
+			return ui.PrintJSONError(err)
+		}
+
+		type response struct {
+			ID      string `json:"id"`
+			Deleted bool   `json:"deleted"`
+		}
+		r := response{ID: variableID, Deleted: res}
+		ui.PrintJSON(r)
+		return nil
+	}
+
+	view := ui.NewDeleteView("variable", variableID)
+	view.Start()
+	_, err = s.api.DeleteAppSecret(ctx, appID, variableID)
+	if err != nil {
+		return view.Fail(err)
+	}
+	view.Success()
+	return nil
+}
