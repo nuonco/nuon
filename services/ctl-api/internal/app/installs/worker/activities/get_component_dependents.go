e3740d7ea35414150029c2cff07bef857a29bdfc
Jon Morehouse <MorehouseJ09@gmail.com>
2025-09-08 09:26:05 -0700

    fix: issue where deleted apps can show up and cause errors (#10344)
    Revert "fix: issue where deleted apps can show up and cause errors (#10333)"
    
    This reverts commit 24b9dc436debc9353cde9f977eff3ad4edfd91c5.

--- services/ctl-api/internal/app/installs/worker/activities/get_component_dependents.go
+++ services/ctl-api/internal/app/installs/worker/activities/get_component_dependents.go
@@ -2,14 +2,23 @@ package activities
 
 import (
 	"context"
+
+	"github.com/pkg/errors"
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
 )
 
-type GetComponentDependentsRequest struct {
-	AppConfigID string `validate:"required"`
-	ComponentID string `validate:"required"`
+type GetComponentDependents struct {
+	AppID           string `json:"app_id" validate:"required"`
+	ComponentRootID string `json:"component_root_id" validate:"required"`
+	ConfigVersion   int    `json:"config_version" validate:"required"`
 }
 
 // @temporal-gen activity
-func (a *Activities) GetComponentDependents(ctx context.Context, req *GetComponentDependentsRequest) ([]string, error) {
-	return a.appsHelpers.GetComponentDependents(ctx, req.AppConfigID, req.ComponentID)
+func (a *Activities) GetComponentDependents(ctx context.Context, req GetComponentDependents) ([]app.Component, error) {
+	comps, err := a.appsHelpers.GetInvertedDependentByComponentConfigVersion(ctx, req.AppID, req.ConfigVersion, req.ComponentRootID)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to get dependent components")
+	}
+
+	return comps, nil
 }
