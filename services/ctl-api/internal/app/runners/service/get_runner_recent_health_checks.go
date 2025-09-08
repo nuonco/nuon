e2cc432a00954000d450ae5086f1adf74eb4dce4
robisso <alphavibe@gmail.com>
2025-09-08 15:29:19 -0700

    feat: disable timeout middleware (#10351)
    * feat: disable timeout middleware
    
    * feat: disable timeout middleware

--- services/ctl-api/internal/app/runners/service/get_runner_recent_health_checks.go
+++ services/ctl-api/internal/app/runners/service/get_runner_recent_health_checks.go
@@ -20,7 +20,7 @@ import (
 // @Param					window						query	string	false	"window of health checks to return"	Default(1h)
 // @Param					offset						query	int		false	"offset of results to return"		Default(0)
 // @Param					limit						query	int		false	"limit of results to return"		Default(10)
-// @Param					page						query	int		false	"page number of results to return"	Default(0)
+// @Param					x-nuon-pagination-enabled	header	bool	false	"Enable pagination"
 // @Tags					runners
 // @Accept					json
 // @Produce				json
