04945d5730f9449b13219b6856c15922e9294480
Jon Morehouse <MorehouseJ09@gmail.com>
2025-09-08 09:24:48 -0700

    fix: issue where deleted apps can show up and cause errors (#10333)
    * fix: issue where deleted apps can show up and cause errors
    
    * chore: fix install component summary

--- services/ctl-api/internal/app/components/service/get_component_dependencies.go
+++ services/ctl-api/internal/app/components/service/get_component_dependencies.go
@@ -5,25 +5,26 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/pkg/errors"
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/pkg/cctx"
 )
 
-//	@ID						GetComponentDependencies
-//	@Summary				get a component's dependencies
-//	@Description.markdown	get_component_dependencies.md
-//	@Param					component_id	path	string	true	"component ID"
-//	@Tags					components
-//	@Accept					json
-//	@Produce				json
-//	@Security				APIKey
-//	@Security				OrgID
-//	@Failure				400	{object}	stderr.ErrResponse
-//	@Failure				401	{object}	stderr.ErrResponse
-//	@Failure				403	{object}	stderr.ErrResponse
-//	@Failure				404	{object}	stderr.ErrResponse
-//	@Failure				500	{object}	stderr.ErrResponse
-//	@Success				200	{array}		app.Component
-//	@Router					/v1/components/{component_id}/dependencies [get]
+// @ID						GetComponentDependencies
+// @Summary				get a component's dependencies
+// @Description.markdown	get_component_dependencies.md
+// @Param					component_id	path	string	true	"component ID"
+// @Tags					components
+// @Accept					json
+// @Produce				json
+// @Security				APIKey
+// @Security				OrgID
+// @Failure				400	{object}	stderr.ErrResponse
+// @Failure				401	{object}	stderr.ErrResponse
+// @Failure				403	{object}	stderr.ErrResponse
+// @Failure				404	{object}	stderr.ErrResponse
+// @Failure				500	{object}	stderr.ErrResponse
+// @Success				200	{array}		app.Component
+// @Router					/v1/components/{component_id}/dependencies [get]
 func (s *service) GetComponentDependencies(ctx *gin.Context) {
 	org, err := cctx.OrgFromContext(ctx)
 	if err != nil {
@@ -39,7 +40,13 @@ func (s *service) GetComponentDependencies(ctx *gin.Context) {
 		return
 	}
 
-	depCmps, err := s.appsHelpers.GetDependentComponents(ctx, component.AppID, component.ID)
+	appCfg, err := s.appsHelpers.GetAppLatestConfig(ctx, component.AppID)
+	if err != nil {
+		ctx.Error(errors.Wrap(err, "unable to get app config"))
+		return
+	}
+
+	depCmps, err := s.appsHelpers.GetComponentDependents(ctx, appCfg.ID, component.ID)
 	if err != nil {
 		ctx.Error(fmt.Errorf("unable to get component dependencies: %w", err))
 		return
