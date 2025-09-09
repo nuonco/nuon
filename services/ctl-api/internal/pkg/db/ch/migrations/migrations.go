1441f8bf87ea16ba1dca54cb330bae63d27265b0
Dat Boi Diego <fidiego@users.noreply.github.com>
2025-09-09 12:33:58 -0500

    feat(ctl-api): store latest heartbeats in a standalone table (#10355)
    * chore(ctl-api): rename file
    
    * feat(ctl-api): store latest heartbeats in standalone table
    
    and populate it with a ch materialized view
    
    ---------
    
    Co-authored-by: nuonbot <bot@nuon.co>

--- services/ctl-api/internal/pkg/db/ch/migrations/migrations.go
+++ services/ctl-api/internal/pkg/db/ch/migrations/migrations.go
@@ -1,13 +1,8 @@
 package migrations
 
 import (
-	"github.com/powertoolsdev/mono/services/ctl-api/internal/pkg/db/plugins/migrations"
 	"go.uber.org/fx"
 )
-
-func All() []migrations.Migration {
-	return []migrations.Migration{}
-}
 
 type Params struct {
 	fx.In
