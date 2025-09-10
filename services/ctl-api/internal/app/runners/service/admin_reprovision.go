38fd3b6ee239c1c44af893c0a62fde38efe5d3d8
Dat Boi Diego <fidiego@users.noreply.github.com>
2025-09-09 20:09:01 -0500

    chore(ctl-api/admin): update swagger docs (#10374)

--- services/ctl-api/internal/app/runners/service/admin_reprovision.go
+++ services/ctl-api/internal/app/runners/service/admin_reprovision.go
@@ -11,17 +11,18 @@ import (
 
 type AdminReprovisionRunnerRequest struct{}
 
-//	@ID						AdminReprovisionRunner
-//	@Summary				reprovision a runner, but keep it in the database
-//	@Description.markdown	reprovision_runner.md
-//	@Tags					runners/admin
-//	@Security				AdminEmail
-//	@Accept					json
-//	@Param					req			body	AdminReprovisionRunnerRequest	true	"Input"
-//	@Param					runner_id	path	string							true	"runner ID to reprovision"
-//	@Produce				json
-//	@Success				200	{string}	ok
-//	@Router					/v1/runners/{runner_id}/reprovision [POST]
+// @ID						AdminReprovisionRunner
+// @Summary					reprovision a runner, but keep it in the database
+// @Description.markdown	reprovision_runner.md
+// @Tags					runners/admin
+// @Security				AdminEmail
+// @Accept					json
+// @Param					req			body	AdminReprovisionRunnerRequest	true	"Input"
+// @Param					runner_id	path	string							true	"runner ID to reprovision"
+// @Produce					json
+// @Success					200	{string}	ok
+// @Router					/v1/runners/{runner_id}/reprovision [POST]
+// @Deprecated 				true
 func (s *service) AdminReprovisionRunner(ctx *gin.Context) {
 	runnerID := ctx.Param("runner_id")
 	runner, err := s.getRunner(ctx, runnerID)
