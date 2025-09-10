50c6c5fb316612c321c21a818e8e1677edbf9e31
Jon Morehouse <MorehouseJ09@gmail.com>
2025-09-10 13:17:36 -0700

    fix: issue where deleted apps can show up and cause errors (#10353)
    * Revert "fix: issue where deleted apps can show up and cause errors (#10344)"
    
    This reverts commit 1a9c7c7076445fbecc35ad5d0fd38b05442e80ca.
    
    * chore: fixes to different parts of the app graph
    
    * chore: fix app components
    
    ---------
    
    Co-authored-by: nuonbot <bot@nuon.co>

--- services/ctl-api/internal/app/apps/service/service.go
+++ services/ctl-api/internal/app/apps/service/service.go
@@ -116,7 +116,6 @@ func (s *service) RegisterInternalRoutes(api *gin.Engine) error {
 	api.POST("/v1/apps/:app_id/admin-reprovision", s.AdminReprovisionApp)
 	api.POST("/v1/apps/:app_id/admin-restart", s.RestartApp)
 	api.POST("/v1/apps/:app_id/admin-config-graph", s.AdminConfigGraph)
-	api.POST("/v1/apps/:app_id/admin-config-order", s.AdminConfigOrder)
 	api.POST("/v1/app-branches/:app_branch_id/admin-test-app-branch-workflow", s.AdminTestAppBranchWorkflow)
 	api.POST("/v1/app-branches/:app_branch_id/admin-restart", s.AdminRestartAppBranch)
 
