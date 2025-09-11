113d368864cd6b1d7faa064119f8740bd37b495c
Nat Hamilton <info@nathamilton.com>
2025-09-11 16:50:07 -0400

    refactor(ui): use polling for install acition pages (#10412)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/components/ActionWorkflowStatus.tsx
+++ services/dashboard-ui/src/components/ActionWorkflowStatus.tsx
@@ -1,51 +1,23 @@
 'use client'
 
-import { usePathname } from 'next/navigation'
-import React, { type FC, useEffect } from 'react'
 import { StatusBadge } from '@/components/Status'
-import { revalidateData } from '@/components/actions'
-import type { TInstallActionWorkflowRun } from '@/types'
-import { SHORT_POLL_DURATION } from '@/utils'
+import { useInstallActionRun } from '@/hooks/use-install-action-run'
 
 export interface IActionWorkflowStatus {
   descriptionAlignment?: 'center' | 'left' | 'right'
   descriptionPosition?: 'bottom' | 'top'
-  actionWorkflowRun: TInstallActionWorkflowRun
   shouldPoll?: boolean
 }
 
-export const ActionWorkflowStatus: FC<IActionWorkflowStatus> = ({
-  actionWorkflowRun,
+export const ActionWorkflowStatus = ({
   shouldPoll = false,
   ...props
-}) => {
-  const path = usePathname()
-  const status = actionWorkflowRun?.status_v2 || {
-    status: actionWorkflowRun?.status || 'Unknown',
-    status_human_description:
-      actionWorkflowRun?.status_description || undefined,
+}: IActionWorkflowStatus) => {
+  const { installActionRun } = useInstallActionRun()
+  const status = installActionRun?.status_v2 || {
+    status: installActionRun?.status || 'Unknown',
+    status_human_description: installActionRun?.status_description || undefined,
   }
-
-  useEffect(() => {
-    const refreshData = () => {
-      revalidateData({ path })
-    }
-    if (shouldPoll) {
-      const pollBuild = setInterval(refreshData, SHORT_POLL_DURATION)
-
-      if (
-        status.status === 'active' ||
-        status.status === 'error' ||
-        status.status === 'cancelled' ||
-        status.status === 'not-attempted' ||
-        status.status === 'noop'
-      ) {
-        clearInterval(pollBuild)
-      }
-
-      return () => clearInterval(pollBuild)
-    }
-  }, [actionWorkflowRun, shouldPoll])
 
   return (
     <StatusBadge
