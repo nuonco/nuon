c9efbb4a4b3b129845318b135e37309f3924e99f
Nat Hamilton <info@nathamilton.com>
2025-09-18 16:27:36 -0400

    refactor(UI): install lib functions (#10465)
    * refactor(ui): updating install lib functions
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * refactor(ui): install lib get functions
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/app/api/[org-id]/installs/[install-id]/components/[component-id]/route.ts
+++ services/dashboard-ui/src/app/api/[org-id]/installs/[install-id]/components/[component-id]/route.ts
@@ -1,27 +1,12 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { getInstallComponent } from '@/lib'
 import { TRouteRes } from '@/app/api/[org-id]/types'
 
 export const GET = async (
   req: NextRequest,
   { params }: TRouteRes<'org-id' | 'install-id' | 'component-id'>
 ) => {
-  const {
-    ['org-id']: orgId,
-    ['install-id']: installId,
-    ['component-id']: componentId,
-  } = await params
-
-  let installComponent = {}
-  try {
-    installComponent = await getInstallComponent({
-      orgId,
-      installId,
-      componentId,
-    })
-  } catch (error) {
-    console.error(error)
-  }
-
-  return NextResponse.json(installComponent)
+  return NextResponse.json({
+    deprecated:
+      'use /api/orgs/:orgId/installs/:installId/components/:componentId',
+  })
 }
---
+++ services/dashboard-ui/src/lib/ctl-api/installs/sandbox/get-install-sandbox-run-by-id.test.ts
@@ -0,0 +1,31 @@
+import '@test/mock-auth'
+import { badResponseCodes } from '@test/utils'
+import { describe, expect, test } from 'vitest'
+import { getInstallSandboxRunById } from './get-install-sandbox-run-by-id'
+
+describe('getInstallSandboxRunById should handle response status codes from GET installs/sandbox-runs/:runId endpoint', () => {
+  const runId = 'test-run-id'
+  const orgId = 'test-org-id'
+
+  test('200 status', async () => {
+    const { data: run, status } = await getInstallSandboxRunById({
+      runId,
+      orgId,
+    })
+    expect(status).toBe(200)
+    expect(run).toHaveProperty('id')
+  })
+
+  test.each(badResponseCodes)('%s status', async (code) => {
+    const { error, status } = await getInstallSandboxRunById({
+      runId,
+      orgId,
+    })
+    expect(status).toBe(code)
+    expect(error).toMatchSnapshot({
+      error: expect.any(String),
+      description: expect.any(String),
+      user_error: expect.any(Boolean),
+    })
+  })
+})
