eb6db13efd6df7137ef8d980477ae51cee9e7a16
Nat Hamilton <info@nathamilton.com>
2025-09-22 12:43:23 -0400

    refactor(ui): steps use polling (#10471)
    * refactor(ui): workflow step details usePolling hook
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * style(ui): fixed types
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/components/InstallWorkflows/SandboxStepDetails.tsx
+++ services/dashboard-ui/src/components/InstallWorkflows/SandboxStepDetails.tsx
@@ -1,64 +1,47 @@
 'use client'
 
-import { useParams } from 'next/navigation'
-import React, { type FC, useEffect, useState } from 'react'
-import { CaretRight } from '@phosphor-icons/react'
+import { CaretRightIcon } from '@phosphor-icons/react'
 import { Link } from '@/components/Link'
 import { Loading } from '@/components/Loading'
 import { Notice } from '@/components/Notice'
 import { StatusBadge } from '@/components/Status'
 import { Text } from '@/components/Typography'
+import { useOrg } from '@/hooks/use-org'
+import { usePolling } from '@/hooks/use-polling'
+
 import type { TSandboxRun } from '@/types'
 import type { IPollStepDetails } from './InstallWorkflowSteps'
 
-export const SandboxStepDetails: FC<IPollStepDetails> = ({
+export const SandboxStepDetails = ({
   step,
   shouldPoll = false,
-  pollDuration = 5000,
-}) => {
-  const params = useParams<Record<'org-id', string>>()
-  const orgId = params?.['org-id']
-  const [sandboxRun, setData] = useState<TSandboxRun>()
-  const [isLoading, setIsLoading] = useState(true)
-  const [error, setError] = useState<string>()
-
-  const fetchData = () => {
-    fetch(
-      `/api/${orgId}/installs/${step?.owner_id}/sandbox-runs/${step?.step_target_id}`
-    ).then((r) =>
-      r.json().then((res) => {
-        setIsLoading(false)
-        if (res?.error) {
-          setError(res?.error?.error)
-        } else {
-          setError(undefined)
-          setData(res.data)
-        }
-      })
-    )
-  }
-
-  useEffect(() => {
-    fetchData()
-  }, [])
-
-  useEffect(() => {
-    if (shouldPoll) {
-      const pollData = setInterval(fetchData, pollDuration)
-
-      return () => clearInterval(pollData)
-    }
-  }, [shouldPoll])
+  pollInterval = 5000,
+}: IPollStepDetails) => {
+  const { org } = useOrg()
+  const {
+    data: sandboxRun,
+    isLoading,
+    error,
+  } = usePolling<TSandboxRun>({
+    initIsLoading: true,
+    path: `/api/${org.id}/installs/${step?.owner_id}/sandbox-runs/${step?.step_target_id}`,
+    pollInterval,
+    shouldPoll,
+  })
 
   return (
-    <>   
-    {isLoading ? (
+    <>
+      {isLoading ? (
         <div className="border rounded-md p-6">
           <Loading loadingText="Loading sandobx details..." variant="stack" />
         </div>
       ) : (
         <>
-          {error ? <Notice>{error}</Notice> : null}        
+          {error?.error ? (
+            <Notice>
+              {error?.error || 'Unable to load sandbox run details.'}
+            </Notice>
+          ) : null}
           {sandboxRun ? (
             <div className="flex flex-col border rounded-md shadow">
               <div className="flex items-center justify-between p-3 border-b">
@@ -67,10 +50,10 @@ export const SandboxStepDetails: FC<IPollStepDetails> = ({
                 </Text>
                 <Link
                   className="text-sm gap-0"
-                  href={`/${orgId}/installs/${step?.owner_id}/sandbox/${sandboxRun?.id}`}
+                  href={`/${org.id}/installs/${step?.owner_id}/sandbox/${sandboxRun?.id}`}
                 >
                   View details
-                  <CaretRight />
+                  <CaretRightIcon />
                 </Link>
               </div>
               <div className="p-6">
