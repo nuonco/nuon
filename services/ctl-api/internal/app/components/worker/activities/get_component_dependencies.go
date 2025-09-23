49e602731fa94fd8bd39b77e4218b23da97ef00c
Prem Saraswat <prmsrswt@gmail.com>
2025-09-23 22:11:25 +0530

    feat: Delete App Components in order when an app is deleted (#10282)
    * feat: Delete App Components in order when an app is deleted
    
    Signed-off-by: Prem Saraswat <prem@nuon.co>
    
    * skip component deletion if not found
    
    Signed-off-by: Prem Saraswat <prem@nuon.co>
    
    * fix: Refactor Component Deletion flow
    
    - Component Deletion now sets the status correctly throughout the flow
    - We wait for dependent components to deprovision before proceeding for deletion
    - Wait for all dependent components to be marked for deletion, before marking a component for delete
    
    Signed-off-by: Prem Saraswat <prem@nuon.co>
    
    * Set status correctly while deleting an app
    
    Signed-off-by: Prem Saraswat <prem@nuon.co>
    
    * Add more validations before deleting an app or a component permanently
    
    Signed-off-by: Prem Saraswat <prem@nuon.co>
    
    * ctl-api: Allow deleing a component only when it's not a part of latest config
    
    Signed-off-by: Prem Saraswat <prem@nuon.co>
    
    ---------
    
    Signed-off-by: Prem Saraswat <prem@nuon.co>

---
+++ services/ctl-api/internal/app/components/worker/activities/get_component_dependencies.go
@@ -0,0 +1,21 @@
+package activities
+
+import (
+	"context"
+	"fmt"
+)
+
+type GetComponentDependentsRequest struct {
+	ComponentID string `validate:"required"`
+	AppConfigID string `validate:"required"`
+}
+
+// @temporal-gen activity
+func (a *Activities) GetComponentDependents(ctx context.Context, req GetComponentDependentsRequest) ([]string, error) {
+	deps, err := a.appsHelpers.GetComponentDependents(ctx, req.AppConfigID, req.ComponentID)
+	if err != nil {
+		return nil, fmt.Errorf("unable to get component dependents: %w", err)
+	}
+
+	return deps, nil
+}
---
+++ services/ctl-api/internal/app/components/worker/activities/get_component_installs.go
@@ -0,0 +1,46 @@
+package activities
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
+)
+
+type GetComponentInstallsRequest struct {
+	ComponentID string `validate:"required"`
+	AppID       string `validate:"required"`
+}
+
+// @temporal-gen activity
+func (a *Activities) GetComponentInstalls(ctx context.Context, req GetComponentInstallsRequest) ([]string, error) {
+	installs, err := a.appsHelpers.GetAppInstalls(ctx, req.AppID)
+	if err != nil {
+		return nil, fmt.Errorf("unable to get component dependents: %w", err)
+	}
+
+	activeInstalls := make([]string, 0)
+	for _, inst := range installs {
+		// if an install was never attempted, it does not need to be polled
+		if len(inst.InstallSandboxRuns) < 1 {
+			continue
+		}
+
+		if inst.InstallSandboxRuns[0].Status == app.SandboxRunStatusAccessError ||
+			inst.InstallSandboxRuns[0].Status == app.SandboxRunStatusDeprovisioned {
+			continue
+		}
+
+		for _, instComp := range inst.InstallComponents {
+			if instComp.ComponentID == req.ComponentID {
+				if instComp.Status == app.InstallComponentStatusInactive || instComp.Status == "" {
+					continue
+				}
+				activeInstalls = append(activeInstalls, inst.ID)
+				break
+			}
+		}
+	}
+
+	return activeInstalls, nil
+}
