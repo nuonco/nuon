faebe5c2dd3562e1eef44daf03b9180f747e6954
Prem Saraswat <prmsrswt@gmail.com>
2025-09-23 22:11:25 +0530

    feat: Delete App Components in order when an app is deleted (#10282)
    * feat: Delete App Components in order when an app is deleted
    
    Signed-off-by: Prem Saraswat <prem@nuon.co>
    
    * skip component deletion if not found
    
    Signed-off-by: Prem Saraswat <prem@nuon.co>
    
    * fix: Refactor Component Deletion flow
    
    - Component Deletion now sets the status correctly throughout the flow
    - We wait for dependent components to deprovision before proceeding for deletion
    - Wait for all dependent components to be marked for deletion, before marking a component for delete
    
    Signed-off-by: Prem Saraswat <prem@nuon.co>
    
    * Set status correctly while deleting an app
    
    Signed-off-by: Prem Saraswat <prem@nuon.co>
    
    * Add more validations before deleting an app or a component permanently
    
    Signed-off-by: Prem Saraswat <prem@nuon.co>
    
    * ctl-api: Allow deleing a component only when it's not a part of latest config
    
    Signed-off-by: Prem Saraswat <prem@nuon.co>
    
    ---------
    
    Signed-off-by: Prem Saraswat <prem@nuon.co>

--- services/ctl-api/internal/app/apps/service/delete_app.go
+++ services/ctl-api/internal/app/apps/service/delete_app.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/app/apps/signals"
+	componentssignals "github.com/powertoolsdev/mono/services/ctl-api/internal/app/components/signals"
 )
 
 // @ID						DeleteApp
@@ -31,7 +32,71 @@ import (
 func (s *service) DeleteApp(ctx *gin.Context) {
 	appID := ctx.Param("app_id")
 
-	err := s.deleteApp(ctx, appID)
+	// Check if there are any active installs for the app, if so, do not allow deletion.
+	{
+		installs, err := s.getAppInstalls(ctx, appID)
+		if err != nil {
+			ctx.Error(fmt.Errorf("error fetching app installs: %w", err))
+			return
+		}
+
+		activeInstalls := make([]string, 0)
+		for _, ins := range installs {
+			// if an install was never attempted, it does not need to be accounted for
+			if len(ins.InstallSandboxRuns) < 1 {
+				continue
+			}
+
+			if ins.InstallSandboxRuns[0].Status == app.SandboxRunStatusAccessError ||
+				ins.InstallSandboxRuns[0].Status == app.SandboxRunStatusDeprovisioned ||
+				ins.InstallSandboxRuns[0].Status == app.SandboxRunStatusDeprovisioning {
+				continue
+			}
+
+			activeInstalls = append(activeInstalls, ins.ID)
+		}
+		if len(activeInstalls) > 0 {
+			ctx.Status(http.StatusBadRequest)
+			ctx.Error(fmt.Errorf("app has %d active install(s), please deprovision them first", len(activeInstalls)))
+			return
+		}
+	}
+
+	appCfg, err := s.helpers.GetAppLatestConfig(ctx, appID)
+	if err != nil {
+		ctx.Error(err)
+		return
+	}
+
+	// Check if there are any active components for the app, if so, do not proceed for deletion.
+	if len(appCfg.ComponentIDs) > 0 {
+		ctx.Error(fmt.Errorf("app has %d active component(s) in it's latest config, please remove them first", len(appCfg.ComponentIDs)))
+		return
+	}
+
+	// Trigger deletion for all components associated with the app in reverse order of their dependencies.
+	{
+		// Get full app config to include all components including missing components in the latest config.
+		appComponents, err := s.helpers.GetAppComponents(ctx, appID)
+		if err != nil {
+			ctx.Error(err)
+			return
+		}
+
+		for _, comp := range appComponents {
+			err = s.helpers.DeleteAppComponent(ctx, comp.ID)
+			if err != nil {
+				ctx.Error(err)
+				return
+			}
+
+			s.evClient.Send(ctx, comp.ID, &componentssignals.Signal{
+				Type: componentssignals.OperationDelete,
+			})
+		}
+	}
+
+	err = s.deleteApp(ctx, appID)
 	if err != nil {
 		ctx.Error(err)
 		return
@@ -49,7 +114,7 @@ func (s *service) deleteApp(ctx context.Context, appID string) error {
 	}
 
 	res := s.db.WithContext(ctx).Model(&currentApp).Updates(app.App{
-		Status:            "delete_queued",
+		Status:            app.AppStatusDeleteQueued,
 		StatusDescription: "delete has been queued and waiting",
 	})
 	if res.Error != nil {
@@ -61,4 +126,18 @@ func (s *service) deleteApp(ctx context.Context, appID string) error {
 	}
 
 	return nil
+}
+
+func (s *service) getAppInstalls(ctx context.Context, appID string) ([]app.Install, error) {
+	app := app.App{}
+	res := s.db.WithContext(ctx).
+		Preload("Installs").
+		Preload("Installs.InstallSandboxRuns").
+		Where("id = ?", appID).
+		First(&app)
+	if res.Error != nil {
+		return nil, fmt.Errorf("unable to get app: %w", res.Error)
+	}
+
+	return app.Installs, nil
 }
