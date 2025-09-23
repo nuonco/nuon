8c44b11fe0379c57208aebac45cc649146e8fffd
Prem Saraswat <prmsrswt@gmail.com>
2025-09-23 22:11:25 +0530

    feat: Delete App Components in order when an app is deleted (#10282)
    * feat: Delete App Components in order when an app is deleted
    
    Signed-off-by: Prem Saraswat <prem@nuon.co>
    
    * skip component deletion if not found
    
    Signed-off-by: Prem Saraswat <prem@nuon.co>
    
    * fix: Refactor Component Deletion flow
    
    - Component Deletion now sets the status correctly throughout the flow
    - We wait for dependent components to deprovision before proceeding for deletion
    - Wait for all dependent components to be marked for deletion, before marking a component for delete
    
    Signed-off-by: Prem Saraswat <prem@nuon.co>
    
    * Set status correctly while deleting an app
    
    Signed-off-by: Prem Saraswat <prem@nuon.co>
    
    * Add more validations before deleting an app or a component permanently
    
    Signed-off-by: Prem Saraswat <prem@nuon.co>
    
    * ctl-api: Allow deleing a component only when it's not a part of latest config
    
    Signed-off-by: Prem Saraswat <prem@nuon.co>
    
    ---------
    
    Signed-off-by: Prem Saraswat <prem@nuon.co>

--- services/ctl-api/internal/app/components/service/delete_component.go
+++ services/ctl-api/internal/app/components/service/delete_component.go
@@ -1,14 +1,10 @@
 package service
 
 import (
-	"context"
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"gorm.io/gorm"
 
-	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/app/components/signals"
 )
 
@@ -31,7 +27,7 @@ import (
 func (s *service) DeleteComponent(ctx *gin.Context) {
 	componentID := ctx.Param("component_id")
 
-	err := s.deleteComponent(ctx, componentID)
+	err := s.appsHelpers.DeleteAppComponent(ctx, componentID)
 	if err != nil {
 		ctx.Error(err)
 		return
@@ -41,43 +37,4 @@ func (s *service) DeleteComponent(ctx *gin.Context) {
 		Type: signals.OperationDelete,
 	})
 	ctx.JSON(http.StatusOK, true)
-}
-
-func (s *service) deleteComponent(ctx context.Context, compID string) error {
-	comp := app.Component{
-		ID: compID,
-	}
-
-	res := s.db.WithContext(ctx).Model(&comp).First(&comp).Where("id = ?", compID)
-	if res.Error != nil {
-		return fmt.Errorf("unable to get component %s: %w", compID, res.Error)
-	}
-
-	dependentComponents, err := s.appsHelpers.GetComponentDependents(ctx, comp.AppID, comp.ID)
-	if err != nil {
-		return fmt.Errorf("unable to get dependents: %w", err)
-	}
-
-	if len(dependentComponents) > 0 {
-		componentsIds := make([]string, 0, len(dependentComponents))
-		for _, c := range dependentComponents {
-			componentsIds = append(componentsIds, c)
-		}
-		return fmt.Errorf("unable to delete component %s, components dependents exist Dependent IDs: %s", compID, componentsIds)
-	}
-
-	res = s.db.WithContext(ctx).Model(&comp).Updates(app.Component{
-		Status:            "delete_queued",
-		StatusDescription: "delete has been queued and waiting",
-	})
-
-	if res.Error != nil {
-		return fmt.Errorf("unable to update component: %w", res.Error)
-	}
-
-	if res.RowsAffected < 1 {
-		return fmt.Errorf("component not found %s: %w", compID, gorm.ErrRecordNotFound)
-	}
-
-	return nil
 }
