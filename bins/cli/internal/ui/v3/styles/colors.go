a23dcf0828ece096ab3207ca8e9de784bae50f7c
Dat Boi Diego <fidiego@users.noreply.github.com>
2025-09-24 16:13:53 -0500

    chore(tui): workflow: dyanmic focus (#10498)

--- bins/cli/internal/ui/v3/styles/colors.go
+++ bins/cli/internal/ui/v3/styles/colors.go
@@ -5,6 +5,13 @@ import (
 )
 
 var (
-	PrimaryColor = lipgloss.Color("63") // Bright Purple
-	Dim          = lipgloss.Color("97") // Dark Purple
+	PrimaryColor   = lipgloss.Color("63") // Bright Purple
+	SecondaryColor = lipgloss.Color("14") // Bright Cyan
+	AccentColor    = lipgloss.Color("11") // Bright Yellow
+
+	Dim = lipgloss.Color("97") // Dark Purple
+
+	TextColor   = lipgloss.Color("")  // Default foreground
+	SubtleColor = lipgloss.Color("8") // Bright Black (typically gray)
+	BorderColor = lipgloss.Color("8") // Bright Black (typically gray)
 )
