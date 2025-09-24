8ff3466e99f90ecef990bf985262d97ec841050b
Dat Boi Diego <fidiego@users.noreply.github.com>
2025-09-24 16:13:53 -0500

    chore(tui): workflow: dyanmic focus (#10498)

--- bins/cli/internal/ui/v3/common/full_page_dialog.go
+++ bins/cli/internal/ui/v3/common/full_page_dialog.go
@@ -5,25 +5,52 @@ import (
 )
 
 var subtle = lipgloss.AdaptiveColor{Light: "#D9DCCF", Dark: "#383838"}
+var warning = lipgloss.AdaptiveColor{Light: "209", Dark: "130"}
+var error = lipgloss.AdaptiveColor{Light: "160", Dark: "52"}
+var info = lipgloss.AdaptiveColor{Light: "27", Dark: "17"}
 var dialogBoxStyle = lipgloss.NewStyle().
-	Border(lipgloss.RoundedBorder()).
-	BorderForeground(lipgloss.Color("#874BFD")).
 	Padding(1, 0).
+	Border(lipgloss.RoundedBorder()).
 	BorderTop(true).
 	BorderLeft(true).
 	BorderRight(true).
+	BorderForeground(lipgloss.Color("#874BFD")).
 	BorderBottom(true)
 
-func FullPageDialog(w, h int, padding int, content string) string {
+var levelStyleMap = map[string]lipgloss.AdaptiveColor{
+	"default": subtle,
+	"warning": warning,
+	"error":   error,
+	"info":    info,
+}
+
+type FullPageDialogRequest struct {
+	Width   int
+	Height  int
+	Padding int
+	Content string
+	Level   string
+}
+
+func (r FullPageDialogRequest) getLevelStyle() lipgloss.AdaptiveColor {
+	style, ok := levelStyleMap[r.Level]
+	if ok {
+		return style
+	}
+	return subtle
+}
+
+func FullPageDialog(req FullPageDialogRequest) string {
 	// dialog that fits the w, h provided w/ content centered..
-	dialog := lipgloss.Place(w, h,
+	levelStyle := req.getLevelStyle()
+	dialog := lipgloss.Place(req.Width, req.Height,
 		lipgloss.Center, lipgloss.Center,
 		dialogBoxStyle.
-			Width(lipgloss.Width(content)).
-			Height(lipgloss.Height(content)).
-			Render(content),
+			Width(lipgloss.Width(req.Content)).
+			Height(lipgloss.Height(req.Content)).
+			Render(req.Content),
 		lipgloss.WithWhitespaceChars("猫咪"),
-		lipgloss.WithWhitespaceForeground(subtle),
+		lipgloss.WithWhitespaceForeground(levelStyle),
 	)
 	return dialog
 }
