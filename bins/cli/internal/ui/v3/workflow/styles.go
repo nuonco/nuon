f62628fd8afd638c6e66d3c716a53b08683879b7
Dat Boi Diego <fidiego@users.noreply.github.com>
2025-09-24 16:13:53 -0500

    chore(tui): workflow: dyanmic focus (#10498)

--- bins/cli/internal/ui/v3/workflow/styles.go
+++ bins/cli/internal/ui/v3/workflow/styles.go
@@ -5,3 +5,11 @@ import "github.com/charmbracelet/lipgloss"
 var appStyle = lipgloss.NewStyle().
 	BorderStyle(lipgloss.NormalBorder()).
 	BorderForeground(lipgloss.Color("#6133FF"))
+
+var appStyleBlur = lipgloss.NewStyle().
+	BorderStyle(lipgloss.NormalBorder()).
+	BorderForeground(lipgloss.AdaptiveColor{Dark: "237", Light: "237"})
+
+var appStyleFocus = lipgloss.NewStyle().
+	BorderStyle(lipgloss.NormalBorder()).
+	BorderForeground(lipgloss.Color("#6133FF"))
