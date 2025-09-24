2400f9d3dd6db1457b4f5a330766d3fd51f7be57
Dat Boi Diego <fidiego@users.noreply.github.com>
2025-09-24 14:50:14 -0500

    chore(tui): start centralizing styles (#10494)
    * chore(tui): remove unused attr from selector model
    
    * chore(cli): primary color moved to styles

---
+++ bins/cli/internal/ui/v3/styles/colors.go
@@ -0,0 +1,10 @@
+package styles
+
+import (
+	"github.com/charmbracelet/lipgloss"
+)
+
+var (
+	PrimaryColor = lipgloss.Color("63") // Bright Purple
+	Dim          = lipgloss.Color("97") // Dark Purple
+)
