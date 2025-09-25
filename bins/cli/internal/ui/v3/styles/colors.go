c7746203a1c9f89d50582ab7e4eea20d710c9e3c
Dat Boi Diego <fidiego@users.noreply.github.com>
2025-09-25 10:31:48 -0500

    chore(cli): palette file for style testing (#10502)
    Co-authored-by: nuonbot <bot@nuon.co>

--- bins/cli/internal/ui/v3/styles/colors.go
+++ bins/cli/internal/ui/v3/styles/colors.go
@@ -5,14 +5,20 @@ import (
 )
 
 var (
+	// for text or ui elements
 	PrimaryColor   = lipgloss.Color("63") // Bright Purple
 	SecondaryColor = lipgloss.Color("14") // Bright Cyan
 	AccentColor    = lipgloss.Color("11") // Bright Yellow
+	Dim            = lipgloss.Color("97") // Dark Purple
 
-	Dim = lipgloss.Color("97") // Dark Purple
+	// text colors
+	TextColor   = lipgloss.Color("")  // Default foreground
+	SubtleColor = lipgloss.Color("8") // Bright Black (typically gray)
 
-	TextColor   = lipgloss.Color("")                              // Default foreground
-	SubtleColor = lipgloss.Color("8")                             // Bright Black (typically gray)
-	BorderColor = lipgloss.Color("8")                             // Bright Black (typically gray)
-	Ghost       = lipgloss.AdaptiveColor{Light: "93", Dark: "17"} //
+	BorderColor      = lipgloss.Color("8") // Bright Black (typically gray)
+	BorderFocusColor = lipgloss.Color("8") // A purple (typically gray)
+	BorderBlurColor  = lipgloss.Color("8") // if not the default, a dark gray (typically gray)
+
+	// very close to light/dark background so it is barely visible
+	Ghost = lipgloss.AdaptiveColor{Light: "93", Dark: "17"} //
 )
