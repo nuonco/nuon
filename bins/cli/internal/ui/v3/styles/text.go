29729f14341108c6e5aca568d3273b0e69abc4a4
Dat Boi Diego <fidiego@users.noreply.github.com>
2025-09-25 16:39:02 -0500

    chore(tui): centralize styles (#10513)
    * chore(tui): left and right shift focus
    
    * chore(tui): diff placeholder
    
    * chore(tui): centralize colors
    
    * chore(tui): workflow should only use official colors

--- bins/cli/internal/ui/v3/styles/text.go
+++ bins/cli/internal/ui/v3/styles/text.go
@@ -2,55 +2,11 @@ package styles
 
 import (
 	"github.com/charmbracelet/lipgloss"
-
-	"github.com/nuonco/nuon-go/models"
 )
 
+// NOTE: use the colors from colors.go
 var Link = lipgloss.NewStyle().Foreground(lipgloss.Color("20")).Underline(true)
-var Text = lipgloss.NewStyle().Foreground(lipgloss.Color(""))
 
 var TextGhost = lipgloss.NewStyle().Italic(true).Foreground(Ghost)
 var TextBold = lipgloss.NewStyle().Bold(true)
 var TextDim = lipgloss.NewStyle().Foreground(Dim)
-var TextLight = lipgloss.NewStyle().Foreground(lipgloss.Color("7"))
-var TextSuccess = lipgloss.NewStyle().Foreground(lipgloss.Color("10"))
-var TextError = lipgloss.NewStyle().
-	Foreground(lipgloss.Color("#ffffff")).
-	Background(lipgloss.Color("1"))
-
-// for statuses
-var Pending = lipgloss.NewStyle().Foreground(lipgloss.Color("8"))
-var NotAttempted = TextDim
-var Approved = TextSuccess
-var ApprovalDenied = lipgloss.NewStyle().Foreground(lipgloss.Color("208"))
-var Cancelled = lipgloss.NewStyle().Foreground(lipgloss.Color("208"))
-var Error = lipgloss.NewStyle().Foreground(lipgloss.Color("1"))
-var Info = lipgloss.NewStyle().Foreground(lipgloss.Color("27"))
-
-var StatusStyleMap = map[models.AppStatus]lipgloss.Style{
-	models.AppStatusPending:              Pending,
-	models.AppStatusNotDashAttempted:     NotAttempted,
-	models.AppStatusApproved:             Approved,
-	models.AppStatusApprovalDashDenied:   ApprovalDenied,
-	models.AppStatusCancelled:            Cancelled,
-	models.AppStatusError:                Error,
-	models.AppStatusAutoDashSkipped:      Info,
-	models.AppStatusApprovalDashAwaiting: Cancelled,
-	models.AppStatusSuccess:              TextSuccess,
-}
-
-func GetStatusStyle(status models.AppStatus) lipgloss.Style {
-	style, ok := StatusStyleMap[status]
-	if ok {
-		return style
-	}
-	return TextDim
-}
-
-var ApprovalConfirmation = lipgloss.NewStyle().Padding(1).
-	Foreground(lipgloss.Color("17")).
-	Background(lipgloss.Color("11"))
-
-var SuccessBanner = lipgloss.NewStyle().Padding(1).
-	Foreground(lipgloss.Color("17")).
-	Background(lipgloss.Color("36"))
