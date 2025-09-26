f7ec6236e49c210191d92c196bc9677360cba975
Nat Hamilton <info@nathamilton.com>
2025-09-26 17:40:26 -0400

    refactor(ui): remaining install mgmt actions (#10524)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/lib/ctl-api/installs/index.ts
+++ services/dashboard-ui/src/lib/ctl-api/installs/index.ts
@@ -2,13 +2,9 @@ export * from './actions'
 export * from './components'
 export * from './sandbox'
 
-// NOTE: might not need this after stratus
-export * from './get-install-component-outputs'
-
 // install query
 export * from './get-installs'
 export * from './get-install-by-id'
-
 export * from './get-install-current-inputs'
 export * from './get-install-readme'
 
@@ -16,10 +12,13 @@ export * from './get-install-readme'
 export * from './get-install-workflows'
 
 // mutations
+export * from './create-install-config'
+export * from './deprovision-install'
+export * from './reprovision-install'
+export * from './sync-secrets'
+export * from './update-install-config'
 export * from './update-install-inputs'
 export * from './update-install'
 
 // TODO(nnnnat): to refactor
-// export * from './reprovision-install'
-// export * from './reprovision-sandbox'
 export * from './forget-install'
