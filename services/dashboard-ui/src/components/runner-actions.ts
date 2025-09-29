eca689748caca36412d377a7989399105dbb8a9c
Nat Hamilton <info@nathamilton.com>
2025-09-29 18:21:49 -0400

    refactor(ui): runner mgmt actions (#10534)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/components/runner-actions.ts
+++ services/dashboard-ui/src/components/runner-actions.ts
@@ -4,88 +4,88 @@ import { revalidatePath } from 'next/cache'
 import { cancelRunnerJob as cancelJob } from '@/lib'
 import { nueMutateData, mutateData } from '@/utils'
 
-interface ICancelRunnerJob {
-  orgId: string
-  path: string
-  runnerJobId: string
-}
+// interface ICancelRunnerJob {
+//   orgId: string
+//   path: string
+//   runnerJobId: string
+// }
 
-export async function cancelRunnerJob({
-  orgId,
-  path,
-  runnerJobId,
-}: ICancelRunnerJob) {
-  try {
-    await cancelJob({
-      runnerJobId,
-      orgId,
-    })
+// export async function cancelRunnerJob({
+//   orgId,
+//   path,
+//   runnerJobId,
+// }: ICancelRunnerJob) {
+//   try {
+//     await cancelJob({
+//       runnerJobId,
+//       orgId,
+//     })
 
-    revalidatePath(path)
-  } catch (error) {
-    console.error(error)
-    throw new Error(
-      `${error?.message || 'An error occured.'} Please refresh page and try again.`
-    )
-  }
-}
+//     revalidatePath(path)
+//   } catch (error) {
+//     console.error(error)
+//     throw new Error(
+//       `${error?.message || 'An error occured.'} Please refresh page and try again.`
+//     )
+//   }
+// }
 
-interface IShutdownRunner {
-  orgId: string
-  runnerId: string
-  path: string
-  force?: boolean
-}
+// interface IShutdownRunner {
+//   orgId: string
+//   runnerId: string
+//   path: string
+//   force?: boolean
+// }
 
-export async function shutdownRunner({
-  force = false,
-  orgId,
-  path,
-  runnerId,
-}: IShutdownRunner) {
-  const reqPath = force
-    ? `runners/${runnerId}/force-shutdown`
-    : `runners/${runnerId}/graceful-shutdown`
+// export async function shutdownRunner({
+//   force = false,
+//   orgId,
+//   path,
+//   runnerId,
+// }: IShutdownRunner) {
+//   const reqPath = force
+//     ? `runners/${runnerId}/force-shutdown`
+//     : `runners/${runnerId}/graceful-shutdown`
 
-  return nueMutateData({
-    orgId,
-    path: reqPath,
-    body: {},
-  }).then((r) => {
-    revalidatePath(path)
-    return r
-  })
-}
+//   return nueMutateData({
+//     orgId,
+//     path: reqPath,
+//     body: {},
+//   }).then((r) => {
+//     revalidatePath(path)
+//     return r
+//   })
+// }
 
-interface IUpdateRunner {
-  orgId: string
-  runnerId: string
-  path: string
-  body: {
-    container_image_tag: string
-    container_image_url: string
-    org_awsiam_role_arn: string
-    org_k8s_service_account_name: string
-    runner_api_url: string
-  }
-}
+// interface IUpdateRunner {
+//   orgId: string
+//   runnerId: string
+//   path: string
+//   body: {
+//     container_image_tag: string
+//     container_image_url: string
+//     org_awsiam_role_arn: string
+//     org_k8s_service_account_name: string
+//     runner_api_url: string
+//   }
+// }
 
-export async function updateRunner({
-  body,
-  orgId,
-  path,
-  runnerId,
-}: IUpdateRunner) {
-  return nueMutateData({
-    orgId,
-    method: 'PATCH',
-    path: `runners/${runnerId}/settings`,
-    body,
-  }).then((r) => {
-    revalidatePath(path)
-    return r
-  })
-}
+// export async function updateRunner({
+//   body,
+//   orgId,
+//   path,
+//   runnerId,
+// }: IUpdateRunner) {
+//   return nueMutateData({
+//     orgId,
+//     method: 'PATCH',
+//     path: `runners/${runnerId}/settings`,
+//     body,
+//   }).then((r) => {
+//     revalidatePath(path)
+//     return r
+//   })
+// }
 
 export interface IUnlockWorkspace {
   workspaceId: string
