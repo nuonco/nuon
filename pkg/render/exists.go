9a2b60b1966940de140193eac5e2c7d9f4106685
Jon Morehouse <MorehouseJ09@gmail.com>
2025-10-01 13:01:08 -0700

    chore: fix issue with leading dots for lookups (#10553)
    * chore: fix issue with leading dots for lookups
    
    * chore: update test
    
    ---------
    
    Co-authored-by: nuonbot <bot@nuon.co>

--- pkg/render/exists.go
+++ pkg/render/exists.go
@@ -1,10 +1,14 @@
 package render
 
 import (
+	"strings"
+
 	"github.com/Jeffail/gabs"
 )
 
 func Exists(path string, data map[string]interface{}) bool {
+	path = strings.TrimPrefix(path, ".")
+
 	json, _ := gabs.Consume(data)
 	return json.ExistsP(path)
 }
