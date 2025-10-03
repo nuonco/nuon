cd49c564dd6e2cf5e383048c0fff688c3e5d5772
Somesh Koli <somesh@nuon.co>
2025-10-03 23:02:53 +0530

    feat: add break glass to action config (#10562)
    * feat: add break glass roles to aws stack outputs
    
    * feat: add break glass role arn to action config and use same if available instate
    
    * fix: imports

--- pkg/config/action_config.go
+++ pkg/config/action_config.go
@@ -16,8 +16,9 @@ type ActionConfig struct {
 	Triggers []*ActionTriggerConfig `mapstructure:"triggers" jsonschema:"required"`
 	Steps    []*ActionStepConfig    `mapstructure:"steps" jsonschema:"required"`
 
-	References   []refs.Ref `mapstructure:"-" jsonschema:"-"`
-	Dependencies []string   `mapstructure:"dependencies,omitempty"`
+	References        []refs.Ref `mapstructure:"-" jsonschema:"-"`
+	Dependencies      []string   `mapstructure:"dependencies,omitempty"`
+	BreakGlassRoleARN string     `mapstructure:"break_glass_role_arn,omitempty"`
 }
 
 type ActionTriggerConfig struct {
