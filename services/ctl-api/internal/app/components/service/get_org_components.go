1f048775aedfed5ad2a5aa9428bc332679f539bf
robisso <alphavibe@gmail.com>
2025-10-08 11:13:52 -0700

    feat: component id filter get components (#10678)
    * feat: get app components component id filter
    
    * feat: get app components component id filter

--- services/ctl-api/internal/app/components/service/get_org_components.go
+++ services/ctl-api/internal/app/components/service/get_org_components.go
@@ -3,8 +3,10 @@ package service
 import (
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/pkg/cctx"
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/pkg/db"
@@ -14,6 +16,7 @@ import (
 // @ID						GetOrgComponents
 // @Summary				get all components for an org
 // @Description.markdown	get_org_components.md
+// @Param 				component_ids		query	string	false	"comma-separated list of component IDs to filter by"
 // @Param					offset						query	int		false	"offset of results to return"	Default(0)
 // @Param					limit						query	int		false	"limit of results to return"	Default(10)
 // @Param					page						query	int		false	"page number of results to return"	Default(0)
@@ -46,13 +49,27 @@ func (s *service) GetOrgComponents(ctx *gin.Context) {
 }
 
 func (s *service) getOrgComponents(ctx *gin.Context, orgID string) ([]app.Component, error) {
+	componentIDs := ctx.Query("component_ids")
+	var componentIDsSlice []string
+	if componentIDs != "" {
+		componentIDsSlice = pq.StringArray(strings.Split(strings.TrimSpace(componentIDs), ","))
+		for i, id := range componentIDsSlice {
+			componentIDsSlice[i] = strings.TrimSpace(id)
+		}
+	}
+
 	comps := []app.Component{}
 
 	res := s.db.WithContext(ctx).
 		Scopes(scopes.WithOffsetPagination).
 		Joins("JOIN apps on apps.id=components.app_id").
-		Where("apps.org_id = ?", orgID).
-		Order("created_at desc").
+		Where("apps.org_id = ?", orgID)
+
+	if len(componentIDsSlice) > 0 {
+		res = res.Where("components.id IN ?", componentIDsSlice)
+	}
+
+	res = res.Order("created_at desc").
 		Find(&comps)
 	if res.Error != nil {
 		return nil, fmt.Errorf("unable to get components: %w", res.Error)
