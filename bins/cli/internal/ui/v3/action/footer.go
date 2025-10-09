2ee0e9b742efd84057da8bee8ea79a63e61e1934
Dat Boi Diego <fidiego@users.noreply.github.com>
2025-10-08 22:31:46 -0500

    feat(tui): install actions uis (#10672)
    * chore(revisit): add app id to cfg
    
    * chore(tui): installs actions

---
+++ bins/cli/internal/ui/v3/action/footer.go
@@ -0,0 +1,52 @@
+package action
+
+import (
+	"github.com/charmbracelet/lipgloss"
+	"github.com/powertoolsdev/mono/bins/cli/internal/ui/v3/common"
+	"github.com/powertoolsdev/mono/pkg/cli/styles"
+)
+
+func (m model) logMessageView() string {
+	if m.status.Message == "" {
+		return ""
+	}
+	return common.StatusBar(m.status)
+}
+
+func (m model) footerView() string {
+	/*
+
+		renders two rows
+		1. log message
+		2. help footer
+
+		If the log message is empty, that row is omitted.
+
+	*/
+	// we have to handle this base case since the element widths are zero on init
+	if m.footer.Width == 0 {
+		content := "\n" + m.help.View(m.keys)
+		m.footer.SetContent(content)
+		return m.footer.View()
+	}
+	footerMaxContentWidth := m.footer.Width - 3
+	if footerMaxContentWidth < 0 {
+		content := "\n" + m.help.View(m.keys)
+		m.footer.SetContent(content)
+		return m.footer.View()
+	}
+
+	sections := []string{}
+
+	if m.status.Message != "" {
+		sections = append(sections, m.logMessageView())
+	}
+	sections = append(sections, styles.HelpStyle.Render(m.help.View(m.keys)))
+
+	// set contents
+	m.footer.SetContent(lipgloss.JoinVertical(
+		lipgloss.Top,
+		sections...,
+	))
+	return m.footer.View()
+}
