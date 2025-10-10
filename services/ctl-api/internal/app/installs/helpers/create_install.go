8e7a312f997253f288ad81172c7ba7fe106907b2
Dat Boi Diego <fidiego@users.noreply.github.com>
2025-10-10 09:08:30 -0500

    chore(ctl-api): remove "TEST" from error message (#10710)

--- services/ctl-api/internal/app/installs/helpers/create_install.go
+++ services/ctl-api/internal/app/installs/helpers/create_install.go
@@ -121,7 +121,7 @@ func (s *Helpers) CreateInstall(ctx context.Context, appID string, req *CreateIn
 
 	res = s.db.WithContext(ctx).Create(&install)
 	if res.Error != nil {
-		return nil, fmt.Errorf("unable to create install TEST: %w", res.Error)
+		return nil, fmt.Errorf("unable to create install: %w", res.Error)
 	}
 
 	if req.InstallConfig != nil {
