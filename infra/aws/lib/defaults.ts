92c6820ad8131d5aecfbf4f7bbe34748be7fc326
Jon Morehouse <MorehouseJ09@gmail.com>
2025-10-13 15:01:21 -0700

    chore: update stacks and wiki (#10746)

--- infra/aws/lib/defaults.ts
+++ infra/aws/lib/defaults.ts
@@ -1,8 +1,9 @@
-import { quota_code, service_code } from './enums';
+import { quota_code, service_code } from "./enums";
 
 const STAGE_REDUCED_EC2_QUOTAS = [
   {
-    quotaCode: quota_code.RUNNING_ON_DEMAND_STANDARD_A_C_D_H_I_M_R_T_Z_INSTANCES,
+    quotaCode:
+      quota_code.RUNNING_ON_DEMAND_STANDARD_A_C_D_H_I_M_R_T_Z_INSTANCES,
     serviceCode: service_code.EC2,
     value: 512,
   },
@@ -13,11 +14,16 @@ const STAGE_REDUCED_EC2_QUOTAS = [
   },
 ];
 
-export const allowedRegions = ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2'];
+export const allowedRegions = [
+  "us-east-1",
+  "us-east-2",
+  "us-west-1",
+  "us-west-2",
+];
 
-export const rootDomain = 'nuon.co';
+export const rootDomain = "nuon.co";
 
-export const defaultRegion = 'us-east-2';
+export const defaultRegion = "us-east-2";
 
 type quota = {
   quotaCode: string;
@@ -47,82 +53,83 @@ export const desiredOrgStructure: orgStructure = [
   {
     accounts: [],
     disableScp: true,
-    name: 'nuon-public',
+    name: "nuon-public",
   },
 
   {
     accounts: [
-      { name: 'demo' },
-      { name: 'demo-govcloud' },
-      { name: 'public' },
-      { name: 'test' },
-      { name: 'infra-tests-00' },
-      { name: 'infra-tests-01' },
-      { name: 'infra-tests-02' },
-      { name: 'infra-tests-03' },
-      { name: 'infra-tests-04' },
+      { name: "demo" },
+      { name: "demo-govcloud" },
+      { name: "public" },
+      { name: "test" },
+      { name: "infra-tests-00" },
+      { name: "infra-tests-01" },
+      { name: "infra-tests-02" },
+      { name: "infra-tests-03" },
+      { name: "infra-tests-04" },
     ],
     disableScp: true,
-    name: 'nuon-testing',
+    name: "nuon-testing",
   },
   {
     accounts: [
-      { name: 'prod' },
-      { name: 'stage' },
-      { name: 'infra-shared-ci' },
-      { additionalEnvTags: ['stage'], name: 'infra-shared-stage' },
-      { additionalEnvTags: ['prod'], name: 'infra-shared-prod' },
-      { additionalEnvTags: ['stage'], name: 'govcloud-stage' },
-      { additionalEnvTags: ['prod'], name: 'govcloud-prod' },
+      { name: "prod" },
+      { name: "stage" },
+      { name: "infra-shared-ci" },
+      { additionalEnvTags: ["stage"], name: "infra-shared-stage" },
+      { additionalEnvTags: ["prod"], name: "infra-shared-prod" },
+      { additionalEnvTags: ["stage"], name: "govcloud-stage" },
+      { additionalEnvTags: ["prod"], name: "govcloud-prod" },
       {
-        additionalEnvTags: ['stage'],
-        name: 'orgs-stage',
+        additionalEnvTags: ["stage"],
+        name: "orgs-stage",
         // NOTE(jdt): these somehow got set really high, have to set them here to match...
         quotas: STAGE_REDUCED_EC2_QUOTAS,
       },
-      { additionalEnvTags: ['prod'], name: 'orgs-prod' },
+      { additionalEnvTags: ["prod"], name: "orgs-prod" },
       // NOTE(fd): accounts for runners
-      { additionalEnvTags: ['stage'], name: 'runners-stage' },
-      { additionalEnvTags: ['prod'], name: 'runners-prod' },
+      { additionalEnvTags: ["stage"], name: "runners-stage" },
+      { additionalEnvTags: ["prod"], name: "runners-prod" },
     ],
     defaultQuotas: [
       {
-        quotaCode: quota_code.RUNNING_ON_DEMAND_STANDARD_A_C_D_H_I_M_R_T_Z_INSTANCES,
+        quotaCode:
+          quota_code.RUNNING_ON_DEMAND_STANDARD_A_C_D_H_I_M_R_T_Z_INSTANCES,
         serviceCode: service_code.EC2,
         value: 512,
       },
       {
-        quotaCode: quota_code.ALL_STANDARD_A_C_D_H_I_M_R_T_Z_SPOT_INSTANCE_REQUESTS,
+        quotaCode:
+          quota_code.ALL_STANDARD_A_C_D_H_I_M_R_T_Z_SPOT_INSTANCE_REQUESTS,
         serviceCode: service_code.EC2,
         value: 512,
       },
     ],
-    name: 'workloads',
+    name: "workloads",
   },
   {
     accounts: [
-      { name: 'sandbox-jm' },
-      { name: 'sandbox-ja' },
-      { name: 'sandbox-rb' },
-      { name: 'sandbox-fd' },
-      { name: 'sandbox-nh' },
-      { name: 'sandbox-ht' },
-      { name: 'sandbox-tw' },
-      { name: 'sandbox-cb' },
-      { name: 'sandbox-sk' },
-      { name: 'sandbox-mm' },
-      { name: 'sandbox-pk' },
-      { name: 'sandbox-ey' },
-      { name: 'sandbox-se' },
-      { name: 'sandbox-byoc' },
-      {name:'sandbox-am'},
+      { name: "sandbox-jm" },
+      { name: "sandbox-ja" },
+      { name: "sandbox-rb" },
+      { name: "sandbox-fd" },
+      { name: "sandbox-nh" },
+      { name: "sandbox-ht" },
+      { name: "sandbox-cb" },
+      { name: "sandbox-sk" },
+      { name: "sandbox-mm" },
+      { name: "sandbox-pk" },
+      { name: "sandbox-ey" },
+      { name: "sandbox-se" },
+      { name: "sandbox-byoc" },
+      { name: "sandbox-am" },
 
       // NOTE(jm): these accounts can be deprecated
-      { name: 'sandbox-retool' },
-      { name: 'sandbox-stardog' },
+      { name: "sandbox-retool" },
+      { name: "sandbox-stardog" },
     ],
     disableScp: true,
-    name: 'engineers',
+    name: "engineers",
   },
 ];
 
