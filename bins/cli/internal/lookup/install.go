0323b64e9b4b8a722aefd16db4dbfdc14d03fac2
Prem Saraswat <prmsrswt@gmail.com>
2025-10-15 22:49:45 +0530

    fix: CLI: Return proper error when install-id is set to `""` (#10783)
    This patch fixes all `nuon installs` commands failing when
    install id is set to `""` in the config file.
    
    Signed-off-by: Prem Saraswat <prem@nuon.co>

--- bins/cli/internal/lookup/install.go
+++ bins/cli/internal/lookup/install.go
@@ -4,9 +4,16 @@ import (
 	"context"
 
 	"github.com/nuonco/nuon-go"
+	"github.com/powertoolsdev/mono/bins/cli/internal/ui"
 )
 
 func InstallID(ctx context.Context, apiClient nuon.Client, installIDOrName string) (string, error) {
+	if installIDOrName == "" {
+		return "", &ui.CLIUserError{
+			Msg: "current install is not set, use installs select to set one or pass the --install-id flag",
+		}
+	}
+
 	install, err := apiClient.GetInstall(ctx, installIDOrName)
 	if err != nil {
 		return "", err
