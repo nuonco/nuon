c6d56d29fa1d44ddc57e0771a32ff4937d74c227
Jordan Acosta <jordan@nuon.co>
2025-10-16 10:03:12 -0700

    feat: Ja/onboarding fixes (#10808)
    * fix: don't allow closing onboarding until org has been created
    
    * fix: correct onboarding account step content
    
    * feat: add "review onboarding" to stratus UI
    
    * fix: lint errors
    
    * refactor: use useUserJourney for user journey

--- services/dashboard-ui/src/components/Apps/CreateAccountStepContent.tsx
+++ services/dashboard-ui/src/components/Apps/CreateAccountStepContent.tsx
@@ -1,37 +1,35 @@
 'use client'
 
 import React, { type FC } from 'react'
-import { Text } from '@/components/Typography'
+import { Text, Heading } from '@/components/Typography'
+import { TAccount } from '@/types/ctl-api.types'
+import { Profile } from '../Profile'
+import { Card, type ICard } from '@/components/common/Card'
 
 interface CreateAppStepContentProps {
   stepComplete: boolean
+  account: TAccount
 }
 
 export const CreateAccountStepContent: FC<CreateAppStepContentProps> = ({
   stepComplete,
+  account,
 }) => {
   return (
     <div className="space-y-6">
       <div className="space-y-3 pb-4 border-b border-gray-200 dark:border-gray-700">
-        <div className="flex items-center gap-2">
-          <div className="w-2 h-2 bg-green-500 rounded-full" />
-          <Text
-            variant="semi-14"
-            className="text-green-800 dark:text-green-200"
-          >
-            Welcome to Nuon!
-          </Text>
-        </div>
-        <Text>
-          Your account has been created. Click &quot;Continue&quot; to create
-          your trial organization and get started.
+        <Heading>Welcome to Nuon!</Heading>
+        <Text variant="med-14">
+          Your account has been created and you are ready to get started.
         </Text>
-        <Text>
-          Your account has been created. Click &quot;Continue&quot; to create
-          your trial organization and get started.
+        <Card className="max-w-80">
+          <Profile />
+        </Card>
+        <Text variant="med-14">
+          Next, we&apos;ll create an org you can use to manage apps, installs,
+          and team members.
         </Text>
       </div>
     </div>
   )
 }
-
