85e47f54c83e16f70c7dddf441002ba109df5621
Nat Hamilton <info@nathamilton.com>
2025-10-31 15:47:07 -0400

    feat(ui): add versions to stratus sidebar (#10975)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/configs/app.ts
+++ services/dashboard-ui/src/configs/app.ts
@@ -2,3 +2,4 @@ export const REFRESH_PAGE_INTERVAL =
   process.env.REFRESH_PAGE_INTERVAL || 10 * 60 * 1000 // 10 minutes
 export const REFRESH_PAGE_WARNING = process.env.REFRESH_PAGE_WARNING || false
 export const SF_TRIAL_ACCESS_ENDPOINT = process?.env?.SF_TRIAL_ACCESS_ENDPOINT
+export const VERSION = process?.env?.VERSION || '0.1.0'
