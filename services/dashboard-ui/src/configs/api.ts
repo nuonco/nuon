0970969026924b3c7df7160b683cab40f592f177
Amit Meena <29653282+im-Amitto@users.noreply.github.com>
2025-11-04 13:37:35 +0530

    feat: nuon.cloud deprecate (#10987)
    * feat: prod nuon cloud deprecate
    
    * fix: deprecate all nuon.cloud hosted zones
    
    * fix: complete nuon.cloud wipeout
    
    * chore: update docs
    
    * empty
    
    ---------
    
    Co-authored-by: nuonbot <bot@nuon.co>

--- services/dashboard-ui/src/configs/api.ts
+++ services/dashboard-ui/src/configs/api.ts
@@ -6,7 +6,7 @@ export const API_URL =
 export const ADMIN_API_URL =
   process?.env?.ADMIN_API_URL ||
   process?.env?.NUON_CTL_API_ADMIN_URL ||
-  'http://ctl.nuon.us-west-2.prod.nuon.cloud'
+  'http://ctl.nuon.us-west-2.prod.internal.nuon.co'
 export const ADMIN_TEMPORAL_UI_URL =
   process.env.NUON_TEMPORAL_UI_URL ||
   'http://temporal-web.temporal.svc.cluster.local:8080'
