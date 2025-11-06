b3b5cc55ba5d48369fbd57a17842e935610fd6b7
Somesh Koli <somesh@nuon.co>
2025-11-06 17:53:37 +0530

    fix: split notification activity into two to avoid retry on partial failure (#10965)
    * fix: split notification activity into two to avoid retry on partial failure
    
    * chore: fix other notification methods
    
    * fix: metrics
    
    * chore: trigger ci
    
    ---------
    
    Co-authored-by: nuonbot <bot@nuon.co>

--- services/ctl-api/internal/app/installs/worker/notification.go
+++ services/ctl-api/internal/app/installs/worker/notification.go
@@ -4,18 +4,31 @@ import (
 	"go.temporal.io/sdk/workflow"
 	"go.uber.org/zap"
 
-	sharedactivities "github.com/powertoolsdev/mono/services/ctl-api/internal/pkg/workflows/activities"
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/pkg/notifications"
+	sharedactivities "github.com/powertoolsdev/mono/services/ctl-api/internal/pkg/workflows/activities"
 )
 
 func (w *Workflows) sendNotification(ctx workflow.Context, typ notifications.Type, appID string, vars map[string]string) {
 	l := workflow.GetLogger(ctx)
-	if err := sharedactivities.AwaitSendNotification(ctx, sharedactivities.SendNotificationRequest{
+
+	// Send email
+	if err := sharedactivities.AwaitSendEmail(ctx, sharedactivities.SendNotificationRequest{
 		AppID: appID,
 		Type:  typ,
 		Vars:  vars,
 	}); err != nil {
-		l.Error("unable to send notification",
+		l.Error("unable to send email",
+			zap.Error(err),
+			zap.String("type", typ.String()))
+	}
+
+	// Send slack notification
+	if err := sharedactivities.AwaitSendSlack(ctx, sharedactivities.SendNotificationRequest{
+		AppID: appID,
+		Type:  typ,
+		Vars:  vars,
+	}); err != nil {
+		l.Error("unable to send slack notification",
 			zap.Error(err),
 			zap.String("type", typ.String()))
 	}
