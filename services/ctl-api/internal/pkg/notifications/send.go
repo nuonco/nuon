5252aa1330110e0119826eb135072e132493df07
Somesh Koli <somesh@nuon.co>
2025-11-06 17:53:37 +0530

    fix: split notification activity into two to avoid retry on partial failure (#10965)
    * fix: split notification activity into two to avoid retry on partial failure
    
    * chore: fix other notification methods
    
    * fix: metrics
    
    * chore: trigger ci
    
    ---------
    
    Co-authored-by: nuonbot <bot@nuon.co>

--- services/ctl-api/internal/pkg/notifications/send.go
+++ services/ctl-api/internal/pkg/notifications/send.go
@@ -10,41 +10,52 @@ import (
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
 )
 
-func (n *Notifications) Send(ctx context.Context, cfg *app.NotificationsConfig, typ Type, vars map[string]string) error {
+func (n *Notifications) SendEmail(ctx context.Context, cfg *app.NotificationsConfig, typ Type, vars map[string]string) error {
 	if n.Cfg.DisableNotifications {
 		n.MetricsWriter.Incr("notification", metrics.ToTags(map[string]string{
 			"status": "noop",
 		}))
-		n.L.Debug("skipping notification, disabled", zap.String("type", typ.String()))
+		n.L.Debug("skipping email notification, notifications disabled", zap.String("type", typ.String()))
 		return nil
 	}
 
-	// handle email notifications
-	if cfg.EnableEmailNotifications {
-		if err := n.sendEmailNotification(ctx, typ, vars); err != nil {
-			n.MetricsWriter.Incr("notification.email", metrics.ToStatusTag("err"))
-			return fmt.Errorf("unable to send email notification: %w", err)
-		}
-
-		n.MetricsWriter.Incr("notification.email", metrics.ToStatusTag("ok"))
-	}
 	if !cfg.EnableEmailNotifications {
 		n.MetricsWriter.Incr("notification.email", metrics.ToStatusTag("noop"))
+		n.L.Debug("email notifications disabled for this org", zap.String("type", typ.String()))
+		return nil
 	}
 
-	// handle slack notifications
-	if cfg.EnableSlackNotifications {
-		for _, slackWebhookURL := range cfg.SlackWebhookURLs {
-			if err := n.sendSlackNotification(ctx, typ, slackWebhookURL, vars); err != nil {
-				n.MetricsWriter.Incr("notification.slack", metrics.ToStatusTag("err"))
-				return fmt.Errorf("unable to send slack notification: %w", err)
-			}
-
-			n.MetricsWriter.Incr("notification.slack", metrics.ToStatusTag("ok"))
-		}
+	if err := n.sendEmailNotification(ctx, typ, vars); err != nil {
+		n.MetricsWriter.Incr("notification.email", metrics.ToStatusTag("err"))
+		return fmt.Errorf("unable to send email notification: %w", err)
 	}
+
+	n.MetricsWriter.Incr("notification.email", metrics.ToStatusTag("ok"))
+	return nil
+}
+
+func (n *Notifications) SendSlack(ctx context.Context, cfg *app.NotificationsConfig, typ Type, vars map[string]string) error {
+	if n.Cfg.DisableNotifications {
+		n.MetricsWriter.Incr("notification", metrics.ToTags(map[string]string{
+			"status": "noop",
+		}))
+		n.L.Debug("skipping slack notification, notifications disabled", zap.String("type", typ.String()))
+		return nil
+	}
+
 	if !cfg.EnableSlackNotifications {
 		n.MetricsWriter.Incr("notification.slack", metrics.ToStatusTag("noop"))
+		n.L.Debug("slack notifications disabled for this org", zap.String("type", typ.String()))
+		return nil
+	}
+
+	for _, slackWebhookURL := range cfg.SlackWebhookURLs {
+		if err := n.sendSlackNotification(ctx, typ, slackWebhookURL, vars); err != nil {
+			n.MetricsWriter.Incr("notification.slack", metrics.ToStatusTag("err"))
+			return fmt.Errorf("unable to send slack notification: %w", err)
+		}
+
+		n.MetricsWriter.Incr("notification.slack", metrics.ToStatusTag("ok"))
 	}
 
 	return nil
