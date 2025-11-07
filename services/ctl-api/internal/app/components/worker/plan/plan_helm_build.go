08946a56e3d45a895555aa551c5ba252c8282951
Harsh Thakur <harsh@nuon.co>
2025-11-08 03:59:36 +0530

    feat: support helm repos (#11039)
    * Support helm repos
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    * remove kube-state-metrics dir
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    * update go mod
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    * update go.sum
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    ---------
    
    Signed-off-by: harsh <harsh@nuon.co>

--- services/ctl-api/internal/app/components/worker/plan/plan_helm_build.go
+++ services/ctl-api/internal/app/components/worker/plan/plan_helm_build.go
@@ -3,15 +3,27 @@ package plan
 import (
 	"go.temporal.io/sdk/workflow"
 
+	"github.com/powertoolsdev/mono/pkg/config"
 	plantypes "github.com/powertoolsdev/mono/pkg/plans/types"
 	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
 )
 
-func (p *Planner) createHelmBuildPlan(ctx workflow.Context, bld *app.ComponentBuild) (*plantypes.HelmBuildPlan, error) {
+func (p *Planner) createHelmBuildPlan(ctx workflow.Context, bld *app.ComponentBuild, helmCompCfg *app.HelmComponentConfig) (*plantypes.HelmBuildPlan, error) {
+
+	var helmCfg *config.HelmRepoConfig
+	if helmCompCfg.HelmConfig.HelmRepoConfig != nil {
+		helmCfg = &config.HelmRepoConfig{
+			Chart:   helmCompCfg.HelmConfig.HelmRepoConfig.Chart,
+			RepoURL: helmCompCfg.HelmConfig.HelmRepoConfig.RepoURL,
+			Version: helmCompCfg.HelmConfig.HelmRepoConfig.Version,
+		}
+	}
+
 	return &plantypes.HelmBuildPlan{
 		Labels: map[string]string{
 			"component_id":       bld.ComponentID,
 			"component_build_id": bld.ID,
 		},
+		HelmRepoConfig: helmCfg,
 	}, nil
 }
