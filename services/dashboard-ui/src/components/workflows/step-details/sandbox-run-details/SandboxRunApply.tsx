3b8f2f1800c3551f760c970972176989788b18ea
Nat Hamilton <info@nathamilton.com>
2025-11-07 11:32:40 -0500

    feat(ui): stratus wrkflw logs (#11033)
    * feat(ui): sandbox logs in stratus workflow details, bigger orgs menu
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * refactor(ui): add __tnx cookie delete script back
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * refactor(ui): remove dashboard link in stratus main nav
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/components/workflows/step-details/sandbox-run-details/SandboxRunApply.tsx
+++ services/dashboard-ui/src/components/workflows/step-details/sandbox-run-details/SandboxRunApply.tsx
@@ -3,32 +3,63 @@
 import { LabeledStatus } from '@/components/common/LabeledStatus'
 import { LabeledValue } from '@/components/common/LabeledValue'
 import { Skeleton } from '@/components/common/Skeleton'
+import { Logs, LogsSkeleton } from '@/components/log-stream/Logs'
+import { LogStreamProvider } from '@/providers/log-stream-provider'
+import { LogsProvider } from '@/providers/logs-provider'
 import { useOrg } from '@/hooks/use-org'
 import { useQuery } from '@/hooks/use-query'
-import type { TWorkflowStep, TSandboxRun } from '@/types'
+import type { TOTELLog, TWorkflowStep, TSandboxRun } from '@/types'
 
-export const SandboxRunApply = ({ step }: { step: TWorkflowStep }) => {
+export const SandboxRunApply = ({
+  step,
+  sandboxRun,
+}: {
+  step: TWorkflowStep
+  sandboxRun: TSandboxRun
+}) => {
   const { org } = useOrg()
-  const { data: sandboxRun, isLoading } = useQuery<TSandboxRun>({
-    path: `/api/orgs/${org.id}/installs/${step?.owner_id}/sandbox/runs/${step?.step_target_id}`,
+
+  const { data: logs, isLoading: isLoadingLogs } = useQuery<TOTELLog[]>({
+    initData: [],
+    path: `/api/orgs/${org.id}/log-streams/${sandboxRun?.log_stream?.id}/logs`,
   })
 
   return (
     <>
-      {isLoading || !sandboxRun ? (
-        <SandboxRunApplySkeleton />
+      {!sandboxRun ? (
+        <div className="flex flex-col gap-4">
+          <SandboxRunApplySkeleton />
+          <SandboxRunLogsSkeleton />
+        </div>
       ) : (
-        <div className="flex items-start gap-6">
-          <LabeledStatus
-            label="Status"
-            statusProps={{
-              status: sandboxRun?.status_v2?.status,
-            }}
-            tooltipProps={{
-              position: 'top',
-              tipContent: sandboxRun?.status_v2?.status_human_description,
-            }}
-          />
+        <div className="flex flex-col gap-4">
+          <div className="flex items-start gap-6">
+            <LabeledStatus
+              label="Status"
+              statusProps={{
+                status: sandboxRun?.status_v2?.status,
+              }}
+              tooltipProps={{
+                position: 'top',
+                tipContent: sandboxRun?.status_v2?.status_human_description,
+              }}
+            />
+          </div>
+
+          {sandboxRun?.log_stream ? (
+            <LogStreamProvider
+              shouldPoll
+              initLogStream={sandboxRun?.log_stream}
+            >
+              {isLoadingLogs ? (
+                <SandboxRunLogsSkeleton />
+              ) : (
+                <LogsProvider initLogs={logs}>
+                  <Logs />
+                </LogsProvider>
+              )}
+            </LogStreamProvider>
+          ) : null}
         </div>
       )}
     </>
@@ -41,6 +72,28 @@ export const SandboxRunApplySkeleton = () => {
       <LabeledValue label={<Skeleton height="17px" width="34px" />}>
         <Skeleton height="23px" width="75px" />
       </LabeledValue>
+    </div>
+  )
+}
+
+export const SandboxRunLogsSkeleton = () => {
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-4">
+          <Skeleton height="36px" width="320px" />
+          <Skeleton height="17px" width="85px" />
+        </div>
+
+        <div className="flex items-center gap-4">
+          <Skeleton height="32px" width="86px" />
+          <Skeleton height="32px" width="135px" />
+          <Skeleton height="32px" width="140px" />
+        </div>
+      </div>
+      <div>
+        <LogsSkeleton />
+      </div>
     </div>
   )
 }
