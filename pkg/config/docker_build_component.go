b08fdef2e6726305448ee0938b727f8a5dfb6255
Harsh Thakur <harsh@nuon.co>
2025-11-12 04:36:53 +0530

    fix: enforce documentation of config structs.  (#11059)
    * extract type map to be global
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    * add schema builder pattern to use over struct tags
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    * use builder pattern for all config structs
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    * enforce all fields have jsonschema documentation via tests
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    * replace go-getter reference with practical examples based on prefixes we use in code
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    * fix jsonschema based on existing configs
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    * add docs for lsp
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    ---------
    
    Signed-off-by: harsh <harsh@nuon.co>

--- pkg/config/docker_build_component.go
+++ pkg/config/docker_build_component.go
@@ -1,5 +1,9 @@
 package config
 
+import (
+	"github.com/invopop/jsonschema"
+)
+
 // NOTE(jm): components are parsed using mapstructure. Please refer to the wiki entry for more.
 type DockerBuildComponentConfig struct {
 	Dockerfile string `mapstructure:"dockerfile" jsonschema:"required"`
@@ -12,6 +16,31 @@ type DockerBuildComponentConfig struct {
 	// NOTE: the following parameters are not supported in the provider
 	// Target	     string		   `mapstructure:"target" toml:"target"`
 	// BuildArgs []string		`mapstructure:"build_args" toml:"build_args"`
+}
+
+func (d DockerBuildComponentConfig) JSONSchemaExtend(schema *jsonschema.Schema) {
+	NewSchemaBuilder(schema).
+		Field("source").Short("source path or URL").
+		Long("Optional source path or URL for the component configuration. Supports HTTP(S) URLs, git repositories, file paths, and relative paths (./). Examples: https://example.com/config.yaml, git::https://github.com/org/repo//config.yaml, file:///path/to/config.yaml, ./local/config.yaml").
+		Field("type").Short("component type").
+		Field("name").Short("component name").
+		Field("var_name").Short("variable name for component output").
+		Long("Optional name to use when storing component outputs as variables. If not specified, uses the component name").
+		Field("dependencies").Short("component dependencies").
+		Long("List of other components that must be deployed before this component. Automatically extracted from template references").
+		Field("dockerfile").Short("path to the Dockerfile").Required().
+		Long("Path to the Dockerfile to build. Supports external file sources: HTTP(S) URLs (https://example.com/Dockerfile), git repositories (git::https://github.com/org/repo//Dockerfile), file paths (file:///path/to/Dockerfile), and relative paths (./Dockerfile)").
+		Example("Dockerfile").
+		Example("docker/Dockerfile.prod").
+		Example("https://github.com/myorg/myrepo/raw/main/Dockerfile").
+		Field("env_vars").Short("build environment variables").
+		Long("Map of environment variables to pass to the Docker build command. Available during the build process. Supports Go templating").
+		Example("GOLANG_VERSION").
+		Example("NODE_ENV").
+		Field("public_repo").Short("public repository containing Dockerfile").OneOfRequired("repository_source").
+		Long("Clone a public GitHub repository containing the Dockerfile and build context. Requires repo, branch, and optionally directory").
+		Field("connected_repo").Short("connected repository containing Dockerfile").OneOfRequired("repository_source").
+		Long("Use a Nuon-connected repository containing the Dockerfile and build context. Requires repo, branch, and optionally directory")
 }
 
 func (t *DockerBuildComponentConfig) Validate() error {
--- pkg/config/vcs.go
+++ pkg/config/vcs.go
@@ -1,13 +1,51 @@
 package config
 
+import (
+	"github.com/invopop/jsonschema"
+)
+
 type PublicRepoConfig struct {
 	Repo      string `mapstructure:"repo,omitempty" jsonschema:"required"`
 	Directory string `mapstructure:"directory,omitempty" jsonschema:"required"`
 	Branch    string `mapstructure:"branch,omitempty" jsonschema:"required"`
 }
 
+func (p PublicRepoConfig) JSONSchemaExtend(schema *jsonschema.Schema) {
+	NewSchemaBuilder(schema).
+		Field("repo").Short("repository URL").Required().
+		Long("HTTPS URL to the public Git repository").
+		Example("https://github.com/user/repo.git").
+		Example("https://github.com/user/terraform-modules.git").
+		Field("directory").Short("directory path").Required().
+		Long("Path within the repository to the configuration files").
+		Example("terraform").
+		Example("infra/terraform").
+		Field("branch").Short("Git branch").Required().
+		Long("Git branch to checkout and use for deployments").
+		Example("main").
+		Example("develop").
+		Example("production")
+}
+
 type ConnectedRepoConfig struct {
 	Repo      string `mapstructure:"repo,omitempty" jsonschema:"required"`
 	Directory string `mapstructure:"directory,omitempty" jsonschema:"required"`
 	Branch    string `mapstructure:"branch,omitempty" jsonschema:"required"`
+}
+
+func (c ConnectedRepoConfig) JSONSchemaExtend(schema *jsonschema.Schema) {
+	NewSchemaBuilder(schema).
+		Field("repo").Short("repository identifier").Required().
+		Long("Identifier of the connected repository configured in the Nuon platform").
+		Example("my-repo").
+		Example("production-infrastructure").
+		Field("directory").Short("directory path").Required().
+		Long("Path within the repository to the configuration files").
+		Example("terraform").
+		Example("infra/terraform").
+		Field("branch").Short("Git branch").Required().
+		Long("Git branch to checkout and use for deployments").
+		Example("main").
+		Example("develop").
+		Example("production")
 }
