aee2f98521c87ae7d47a0851ed28f1a1dbdcde24
robisso <alphavibe@gmail.com>
2025-11-11 20:54:23 -0800

    feat: purge stale data general worker (#11075)
    * feat: purge stale data general worker
    
    * feat: purge stale data general worker
    
    * feat: purge stale data general worker
    
    * feat: purge stale data general worker
    
    * feat: purge stale data general worker
    
    * feat: purge stale data general worker

---
+++ services/ctl-api/internal/app/general/worker/activities/purge_stale_temporal_payloads.go
@@ -0,0 +1,29 @@
+package activities
+
+import (
+	"context"
+	"time"
+
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/app"
+)
+
+type PurgeStaleTemporalPayloadsRequest struct {
+	DurationAgo time.Duration
+}
+
+// @temporal-gen activity
+func (a *Activities) PurgeStaleTemporalPayloads(ctx context.Context, req PurgeStaleTemporalPayloadsRequest) error {
+	cutoff := time.Now().Add(-req.DurationAgo)
+
+	res := a.db.WithContext(ctx).
+		Unscoped().
+		Where("created_at < ?", cutoff).
+		Delete(&app.TemporalPayload{})
+
+	if res.Error != nil {
+		return res.Error
+	}
+
+	a.mw.Count("event_loop.general.purge_stale_data", int64(res.RowsAffected), []string{})
+	return nil
+}
