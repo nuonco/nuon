e441b72284c3c2eb1a4d2a38134cdf825f60ee2e
Prem Saraswat <prmsrswt@gmail.com>
2025-11-18 03:51:53 +0530

    feat: CLI: Add `nuon installs toggle-sync` command (#11149)
    * feat: CLI: Add `nuon installs toggle-sync` command
    
    This patch adds a new command to cli, `nuon installs toggle-sync`.
    This command toggles config file syncing for an install (enable it
    if disabled, or vice-versa). For better ux in some cases, there are
    also two mutually exclusive optional flags `--enable` and `--disable`
    to explicitly enable or disable config file syncing for the install.
    
    Signed-off-by: Prem Saraswat <prem@nuon.co>
    
    * Update bins/cli/internal/services/installs/toggle_sync.go
    
    Co-authored-by: greptile-apps[bot] <165735046+greptile-apps[bot]@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Prem Saraswat <prem@nuon.co>
    Co-authored-by: greptile-apps[bot] <165735046+greptile-apps[bot]@users.noreply.github.com>
    Co-authored-by: nuonbot <bot@nuon.co>

---
+++ bins/cli/internal/services/installs/toggle_sync.go
@@ -0,0 +1,53 @@
+package installs
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/nuonco/nuon-go/models"
+
+	"github.com/powertoolsdev/mono/bins/cli/internal/lookup"
+	"github.com/powertoolsdev/mono/bins/cli/internal/ui"
+)
+
+func (s *Service) ToggleSync(ctx context.Context, installID string, enable, disable bool) error {
+
+	installID, err := lookup.InstallID(ctx, s.api, installID)
+	if err != nil {
+		return ui.PrintError(err)
+	}
+
+	appInstall, err := s.api.GetInstall(ctx, installID)
+	if err != nil {
+		return ui.PrintError(fmt.Errorf("error fetching install %s: %w", installID, err))
+	}
+
+	managedBy := ManagedByNuonCLIConfig
+	if appInstall.Metadata["managed_by"] == ManagedByNuonCLIConfig {
+		managedBy = ManagedByNuonDashboard
+	}
+	// Explicitly set managed_by based if overriding flags are set.
+	if enable {
+		managedBy = ManagedByNuonCLIConfig
+	} else if disable {
+		managedBy = ManagedByNuonDashboard
+	}
+
+	appInstall, err = s.api.UpdateInstall(ctx, appInstall.ID, &models.ServiceUpdateInstallRequest{
+		Name: appInstall.Name,
+		Metadata: &models.HelpersInstallMetadata{
+			ManagedBy: managedBy,
+		},
+	})
+	if err != nil {
+		return ui.PrintError(fmt.Errorf("error toggling install's config syncing: %w", err))
+	}
+
+	configuredState := "disabled"
+	if appInstall.Metadata["managed_by"] == ManagedByNuonCLIConfig {
+		configuredState = "enabled"
+	}
+
+	ui.PrintSuccess(fmt.Sprintf("config file syncing is now %s for install %s", configuredState, appInstall.Name))
+	return nil
+}
