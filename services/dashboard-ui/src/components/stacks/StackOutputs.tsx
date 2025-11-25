f9bcb66151f9df6316cb94148ca9e3bac61d087c
Nat Hamilton <info@nathamilton.com>
2025-11-25 13:46:10 -0500

    fix(ui): support azure stack outputs (#11259)
    feat(ui): support azure stack outputs
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/components/stacks/StackOutputs.tsx
+++ services/dashboard-ui/src/components/stacks/StackOutputs.tsx
@@ -13,11 +13,11 @@ export const StackOutputs = ({ runs }: { runs: TInstallStackVersionRun[] }) => {
         </Text>
         <ClickToCopyButton
           className="w-fit self-end"
-          textToCopy={JSON.stringify(run.data_contents)}
+          textToCopy={JSON.stringify(run?.data_contents || run?.data || {})}
         />
       </Text>
       <div className="overflow-auto max-h-[600px]">
-        <KeyValueList values={objectToKeyValueArray(run?.data)} />
+        <KeyValueList values={objectToKeyValueArray(run?.data || {})} />
       </div>
     </div>
   ))
