28d79b39ff734fdfcd0be89e0b789946953bd567
FI. Diego <fidiego@users.noreply.github.com>
2025-12-02 13:13:23 -0600

    chore: dns for gh pages (#11313)

---
+++ infra/dns/gh.tf
@@ -0,0 +1,10 @@
+// gh.nuon.co configs for github pages
+resource "aws_route53_record" "gh_txt" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = " _github-pages-challenge-nuonco.gh"
+  type    = "TXT"
+  ttl     = 300
+  records = [
+    "695830077d79f7b262b0083c146af8"
+  ]
+}
