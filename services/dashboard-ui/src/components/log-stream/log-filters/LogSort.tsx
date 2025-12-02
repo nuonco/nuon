50d2e451a83a868a7b7b3871c74b677e3ac98f4e
Nat Hamilton <info@nathamilton.com>
2025-12-02 12:49:24 -0500

    feat(ui): sse logs poc (#11262)
    * feat(ui): support SSE logs streams in dashboard
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat(ui): POC sse build logs
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat(ui): temp sse log viewer example
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat(ui): removed the sse only provider/hooks
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * chore(ui): lint obey
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * chore(ui): golint obey
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * fix(ui): log sort filter use raw timestamp
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    Co-authored-by: nuonbot <bot@nuon.co>

--- services/dashboard-ui/src/components/log-stream/log-filters/LogSort.tsx
+++ services/dashboard-ui/src/components/log-stream/log-filters/LogSort.tsx
@@ -1,11 +1,17 @@
 import { Button } from '@/components/common/Button'
 import { Icon } from '@/components/common/Icon'
-import { useLogs } from '@/hooks/use-logs'
+import type { TLogFiltersProps } from '@/hooks/use-log-filters'
 
-export const LogSort = () => {
-  const {
-    filters: { handleSortToggle, sortStats },
-  } = useLogs()
+interface LogSortProps {
+  filters: {
+    handleSortToggle: TLogFiltersProps['handleSortToggle']
+    sortStats: TLogFiltersProps['sortStats']
+  }
+}
+
+export const LogSort = ({ filters }: LogSortProps) => {
+  const { handleSortToggle, sortStats } = filters
+  
   return (
     <Button onClick={handleSortToggle} variant="ghost">
       {sortStats.isNewestFirst ? 'Latest' : 'Oldest'}
