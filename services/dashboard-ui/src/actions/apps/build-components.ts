4108a4c1d87e7506248a378a92c3c48e708939ff
Nat Hamilton <info@nathamilton.com>
2025-12-08 17:22:40 -0500

    feat(ui): stratus build all comps (#11385)
    * feat(ui): stratus build all components
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat(ui): build all components btn in stratus
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/actions/apps/build-components.ts
+++ services/dashboard-ui/src/actions/apps/build-components.ts
@@ -5,17 +5,12 @@ import {
   type IServerAction,
 } from '@/actions/execute-server-action'
 import { buildComponents as build } from '@/lib'
-import type { TComponent } from '@/types'
 
-// NOTE: The buildComponents lib function is special as
-// it maps over each component then creates a build.
-// This means the return type doesn't conform to the standard
-// TAPIResponse type and can not be used with the useServerAction hook
 export async function buildComponents({
   path,
   ...args
 }: {
-  components: TComponent[]
+  appId: string
 } & IServerAction) {
   return executeServerAction({
     action: build,
