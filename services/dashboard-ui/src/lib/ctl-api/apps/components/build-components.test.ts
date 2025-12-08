a8f4f32c8a2440083b2998ca1498f64ba5eff82e
Nat Hamilton <info@nathamilton.com>
2025-12-08 17:22:40 -0500

    feat(ui): stratus build all comps (#11385)
    * feat(ui): stratus build all components
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat(ui): build all components btn in stratus
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/lib/ctl-api/apps/components/build-components.test.ts
+++ services/dashboard-ui/src/lib/ctl-api/apps/components/build-components.test.ts
@@ -3,23 +3,26 @@ import { badResponseCodes } from '@test/utils'
 import { describe, expect, test } from 'vitest'
 import { buildComponents } from './build-components'
 
-// TODO(nnnnat): need to figure out a good way to test this
-describe.skip('buildComponents should handle response status codes from POST components/:componentId/build endpoint', () => {
-  const components = [{ id: 'test-component-id' }, { id: 'test-component-id' }]
+describe('buildComponents should handle response status codes from POST apps/:appId/components/build-all endpoint', () => {
+  const appId = 'test-app-id'
   const orgId = 'test-org-id'
 
   test('200 status with default body', async () => {
-    const [{ data: build }] = await buildComponents({
-      components,
+    const { data: builds } = await buildComponents({
+      appId,
       orgId,
     })
-    expect(build).toHaveProperty('id')
-    expect(build).toHaveProperty('status_v2')
+    expect(Array.isArray(builds)).toBe(true)
+    if (builds && builds.length > 0) {
+      expect(builds[0]).toHaveProperty('id')
+      expect(builds[0]).toHaveProperty('status_v2')
+    }
   })
 
+
   test.each(badResponseCodes)('%s status', async (code) => {
-    const [{ error, status }] = await buildComponents({
-      components,
+    const { error, status } = await buildComponents({
+      appId,
       orgId,
     })
     expect(status).toBe(code)
