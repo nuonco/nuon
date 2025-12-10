d955e30cb0b6193653e4848b6b7c0c49a6d32d2a
Nat Hamilton <info@nathamilton.com>
2025-12-10 11:43:03 -0500

    feat(ui): stratus install component mgmt (#11391)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

---
+++ services/dashboard-ui/src/components/install-components/management/ManagementDropdown.tsx
@@ -0,0 +1,48 @@
+'use client'
+
+import { Dropdown } from '@/components/common/Dropdown'
+import { Icon } from '@/components/common/Icon'
+import { Menu } from '@/components/common/Menu'
+import { DeployComponentButton } from '@/components/install-components/management/DeployComponent'
+import { DriftScanComponentButton } from '@/components/install-components/management/DriftScanComponent'
+import { TeardownComponentButton } from '@/components/install-components/management/TeardownComponent'
+import type { TComponent } from '@/types'
+
+export const ManagementDropdown = ({ 
+  component, 
+  currentBuildId 
+}: { 
+  component: TComponent
+  currentBuildId?: string 
+}) => {
+  return (
+    <Dropdown
+      id={`component-${component.id}-mgmt`}
+      variant="primary"
+      buttonText={
+        <>
+          <Icon variant="SlidersHorizontalIcon" /> Manage
+        </>
+      }
+      alignment="right"
+    >
+      <Menu>
+        <DriftScanComponentButton 
+          component={component} 
+          currentBuildId={currentBuildId}
+          isMenuButton 
+        />
+        <DeployComponentButton 
+          component={component} 
+          currentBuildId={currentBuildId}
+          isMenuButton 
+        />
+        <hr />
+        <TeardownComponentButton 
+          component={component}
+          isMenuButton 
+        />        
+      </Menu>
+    </Dropdown>
+  )
+}
