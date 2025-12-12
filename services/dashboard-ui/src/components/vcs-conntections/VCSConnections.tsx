bd9c21bab9b722144a32fce8858c1fcc3e30cc9c
Nat Hamilton <info@nathamilton.com>
2025-12-12 16:05:15 -0500

    feat(ui): stratus json viewer and better markdown rendering (#11419)
    * feat(ui): stratus json viewer, key value list json support
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * refactor(ui): better markdown rendering
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * refactor(ui): use old showdown render for now
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/components/vcs-conntections/VCSConnections.tsx
+++ services/dashboard-ui/src/components/vcs-conntections/VCSConnections.tsx
@@ -14,7 +14,7 @@ export const VCSConnections = ({
         vcsConnections?.map((vcs) => (
           <Text
             key={vcs?.id}
-            className="flex gap-2 py-2 items-center font-mono text-sm text-cool-grey-600 dark:text-cool-grey-500 w-full"
+            className="flex gap-2 items-center font-mono text-sm text-cool-grey-600 dark:text-cool-grey-500 w-full"
           >
             <FaGithub className="text-lg" />
             {vcs?.github_account_name || vcs?.github_install_id}
