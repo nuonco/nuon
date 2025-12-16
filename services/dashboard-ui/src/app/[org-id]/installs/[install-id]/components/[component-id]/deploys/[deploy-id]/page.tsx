6d182056a969afeff89119df416d206b44f5802e
Nat Hamilton <info@nathamilton.com>
2025-12-16 15:45:58 -0500

    feat(ui): stratus deploy / sandbox page headers (#11438)
    * feat(ui): updated deploy page to use stratus header, deploy & sandbox run providers
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat(ui): updated sandbox run to use stratus comps
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/app/[org-id]/installs/[install-id]/components/[component-id]/deploys/[deploy-id]/page.tsx
+++ services/dashboard-ui/src/app/[org-id]/installs/[install-id]/components/[component-id]/deploys/[deploy-id]/page.tsx
@@ -3,21 +3,13 @@ import { notFound } from 'next/navigation'
 import { Suspense } from 'react'
 import { ApprovalBanner } from '@/components/approvals/ApprovalBanner'
 import { Plan } from '@/components/approvals/Plan'
-import { BackLink } from '@/components/common/BackLink'
 import { BackToTop } from '@/components/common/BackToTop'
-import { HeadingGroup } from '@/components/common/HeadingGroup'
-import { ID } from '@/components/common/ID'
-import { Link } from '@/components/common/Link'
-import { Text } from '@/components/common/Text'
+import { DeployHeader } from '@/components/deploys/DeployHeader'
 import { PageSection } from '@/components/layout/PageSection'
 import { Breadcrumbs } from '@/components/navigation/Breadcrumb'
+import { DeployProvider } from '@/providers/deploy-provider'
 import { LogStreamProvider } from '@/providers/log-stream-provider'
-import {
-  getInstall,
-  getDeploy,
-  getWorkflow,
-  getOrg,
-} from '@/lib'
+import { getComponent, getInstall, getDeploy, getWorkflow, getOrg } from '@/lib'
 import { Build } from './build'
 import { ComponentConfig } from './config'
 import { Logs, LogsError, LogsSkeleton } from './logs'
@@ -77,16 +69,21 @@ export default async function InstallComponentDeploy({ params }) {
     ['component-id']: componentId,
     ['deploy-id']: deployId,
   } = await params
-  const [{ data: deploy, error, status }, { data: install }, { data: org }] =
-    await Promise.all([
-      getDeploy({
-        deployId,
-        installId,
-        orgId,
-      }),
-      getInstall({ installId, orgId }),
-      getOrg({ orgId }),
-    ])
+  const [
+    { data: deploy, error, status },
+    { data: install },
+    { data: org },
+    { data: component },
+  ] = await Promise.all([
+    getDeploy({
+      deployId,
+      installId,
+      orgId,
+    }),
+    getInstall({ installId, orgId }),
+    getOrg({ orgId }),
+    getComponent({ componentId, orgId }),
+  ])
 
   if (error) {
     console.error(
@@ -117,316 +114,94 @@ export default async function InstallComponentDeploy({ params }) {
     : null
 
   return org?.features?.['stratus-layout'] ? (
-    <PageSection className="!p-0 !gap-0" id={containerId} isScrollable>
-      <Breadcrumbs
-        breadcrumbs={[
-          {
-            path: `/${orgId}`,
-            text: org?.name,
-          },
-          {
-            path: `/${orgId}/installs`,
-            text: 'Installs',
-          },
-          {
-            path: `/${orgId}/installs/${installId}`,
-            text: install?.name,
-          },
-          {
-            path: `/${orgId}/installs/${installId}/components`,
-            text: 'Components',
-          },
-          {
-            path: `/${orgId}/installs/${installId}/components/${componentId}`,
-            text: deploy?.component_name,
-          },
-          {
-            path: `/${orgId}/installs/${installId}/components/${componentId}/deploys/${deployId}`,
-            text: 'Deploy',
-          },
-        ]}
-      />
-      {/* old page layout */}
-      <div className="p-6 border-b flex justify-between">
-        <HeadingGroup>
-          <BackLink className="mb-6" />
-          <Text variant="base" weight="strong">
-            {deploy?.component_name}
-          </Text>
-          <ID>{deployId}</ID>
-          <div className="flex gap-8 items-center justify-start mt-2">
-            <Text className="!flex items-center gap-1">
-              <CalendarBlankIcon />
-              <Time time={deploy?.created_at} />
-            </Text>
-            <Text className="!flex items-center gap-1">
-              <TimerIcon />
-              <Duration
-                beginTime={deploy?.created_at}
-                endTime={deploy?.updated_at}
-              />
-            </Text>
-          </div>
-          {deploy?.install_workflow_id ? (
-            <Link
-              className="text-xs mt-2"
-              href={`/${orgId}/installs/${installId}/workflows/${deploy?.install_workflow_id}?panel=${step?.id}`}
-            >
-              <CaretLeftIcon />
-              View workflow
-            </Link>
-          ) : null}
-        </HeadingGroup>
-
-        <div className="flex gap-6 items-start justify-end flex-wrap">
-          <span className="flex flex-col gap-2">
-            <DeployStatus
-              descriptionAlignment="right"
-              initDeploy={deploy}
-              shouldPoll
-            />
-          </span>
-
-          <span className="flex flex-col gap-2">
-            <OldText className="text-cool-grey-600 dark:text-cool-grey-500">
-              Type
-            </OldText>
-            <OldText>{deploy.install_deploy_type}</OldText>
-          </span>
-
-          <span className="flex flex-col gap-2">
-            <OldText className="text-cool-grey-600 dark:text-cool-grey-500">
-              Build
-            </OldText>
-            <OldText variant="mono-12">
-              <ToolTip alignment="right" tipContent={deploy.build_id}>
-                <ClickToCopy>
-                  <Truncate variant="small">{deploy.build_id}</Truncate>
-                </ClickToCopy>
-              </ToolTip>
-            </OldText>
-          </span>
-
-          <span className="flex flex-col gap-2">
-            <OldText className="text-cool-grey-600 dark:text-cool-grey-500">
-              Component
-            </OldText>
-            <OldText variant="med-12">{deploy?.component_name}</OldText>
-            <OldText variant="mono-12">
-              <ToolTip alignment="right" tipContent={deploy.component_id}>
-                <ClickToCopy>
-                  <Truncate variant="small">{deploy.component_id}</Truncate>
-                </ClickToCopy>
-              </ToolTip>
-            </OldText>
-          </span>
-
-          <span className="flex flex-col gap-2">
-            <OldText className="text-cool-grey-600 dark:text-cool-grey-500">
-              Install
-            </OldText>
-            <OldText variant="med-12">{install.name}</OldText>
-            <OldText variant="mono-12">
-              <ToolTip alignment="right" tipContent={install.id}>
-                <ClickToCopy>
-                  <Truncate variant="small">{install.id}</Truncate>
-                </ClickToCopy>
-              </ToolTip>
-            </OldText>
-          </span>
-
-          <div className="flex flex-wrap gap-4 items-center">
-            <ErrorBoundary
-              fallback={<OldText>Can&apso;t fetching sync plan</OldText>}
-            >
-              <Suspense
-                fallback={
-                  <Loading variant="stack" loadingText="Loading sync plan..." />
-                }
+    <>
+      <DeployProvider initDeploy={deploy}>
+        <Breadcrumbs
+          breadcrumbs={[
+            {
+              path: `/${orgId}`,
+              text: org?.name,
+            },
+            {
+              path: `/${orgId}/installs`,
+              text: 'Installs',
+            },
+            {
+              path: `/${orgId}/installs/${installId}`,
+              text: install?.name,
+            },
+            {
+              path: `/${orgId}/installs/${installId}/components`,
+              text: 'Components',
+            },
+            {
+              path: `/${orgId}/installs/${installId}/components/${componentId}`,
+              text: deploy?.component_name,
+            },
+            {
+              path: `/${orgId}/installs/${installId}/components/${componentId}/deploys/${deployId}`,
+              text: 'Deploy',
+            },
+          ]}
+        />
+        <DeployHeader
+          component={component}
+          workflow={workflow}
+          stepId={step?.id}
+        />
+        <PageSection className="!pb-12" id={containerId} isScrollable>
+          <div>
+            {workflow &&
+            step &&
+            step?.approval &&
+            !step?.approval?.response &&
+            step?.status?.status !== 'auto-skipped' ? (
+              <Section
+                className="border-b !px-0"
+                childrenClassName="flex flex-col gap-6"
+                heading="Approve change"
               >
-                <RunnerJobPlanModal
-                  buttonText="Sync plan"
-                  headingText="Component sync plan"
-                  runnerJobId={deploy?.runner_jobs?.at(-1)?.id}
-                />
-              </Suspense>
-            </ErrorBoundary>
-
-            {deploy?.install_deploy_type !== 'sync-image' ? (
-              <ErrorBoundary
-                fallback={<OldText>Can&apso;t fetching deploy plan</OldText>}
-              >
-                <Suspense
-                  fallback={
-                    <Loading
-                      variant="stack"
-                      loadingText="Loading deploy plan..."
-                    />
-                  }
-                >
-                  <RunnerJobPlanModal
-                    buttonText="Deploy plan"
-                    headingText="Component deploy plan"
-                    runnerJobId={deploy?.runner_jobs?.at(0)?.id}
-                  />
-                </Suspense>
-              </ErrorBoundary>
+                <ApprovalBanner step={step} />
+                <Plan step={step} />
+              </Section>
             ) : null}
 
-            {deploy?.component_name ? (
-              <InstallComponentManagementDropdown
-                componentId={deploy?.component_id}
-                componentName={deploy?.component_name}
-              />
-            ) : null}
-            {CANCEL_RUNNER_JOBS &&
-            deploy?.status !== 'active' &&
-            deploy?.status !== 'error' &&
-            deploy?.status !== 'inactive' &&
-            deploy?.runner_jobs?.length &&
-            workflow &&
-            !workflow?.finished ? (
-              <InstallWorkflowCancelModal installWorkflow={workflow} />
-            ) : null}
-          </div>
-        </div>
-      </div>
-
-      <div className="grid grid-cols-1 md:grid-cols-12 flex-auto divide-x">
-        <div className="md:col-span-8">
-          {workflow &&
-          step &&
-          step?.approval &&
-          !step?.approval?.response &&
-          step?.status?.status !== 'auto-skipped' ? (
-            <Section
-              className="border-b"
-              childrenClassName="flex flex-col gap-6"
-              heading="Approve change"
-            >
-              <ApprovalBanner step={step} />
-              <Plan step={step} />
-            </Section>
-          ) : null}
-
-          <Section childrenClassName="!flex-auto" className="flex-auto">
             <LogStreamProvider
               initLogStream={deploy?.log_stream}
               shouldPoll={deploy?.log_stream?.open}
             >
               <ErrorBoundary fallback={<LogsError />}>
                 <Suspense fallback={<LogsSkeleton />}>
-                  <Logs logStreamId={deploy?.log_stream?.id} orgId={orgId} logStreamOpen={deploy?.log_stream?.open} />
+                  <Logs
+                    logStreamId={deploy?.log_stream?.id}
+                    orgId={orgId}
+                    logStreamOpen={deploy?.log_stream?.open}
+                  />
                 </Suspense>
               </ErrorBoundary>
             </LogStreamProvider>
-          </Section>
 
-          {workflow &&
-          step &&
-          step?.approval &&
-          step?.approval?.response &&
-          step?.status?.status !== 'auto-skipped' ? (
-            <Section
-              className="border-t"
-              childrenClassName="flex flex-col gap-6"
-              heading="Approve change"
-            >
-              <ApprovalBanner step={step} />
-              <Plan step={step} />
-            </Section>
-          ) : null}
-        </div>
-
-        <div className="divide-y flex flex-col md:col-span-4">
-          <Section
-            className="flex-initial"
-            actions={
-              <OldText>
-                <OldLink
-                  href={`/${orgId}/apps/${install?.app_id}/components/${componentId}/builds/${deploy.build_id}`}
-                >
-                  Details
-                  <CaretRightIcon />
-                </OldLink>
-              </OldText>
-            }
-            heading="Component build"
-          >
-            <ErrorBoundary fallbackRender={ErrorFallback}>
-              <Suspense
-                fallback={
-                  <Loading
-                    loadingText="Loading component build..."
-                    variant="stack"
-                  />
-                }
+            {workflow &&
+            step &&
+            step?.approval &&
+            step?.approval?.response &&
+            step?.status?.status !== 'auto-skipped' ? (
+              <Section
+                className="border-t !px-0"
+                childrenClassName="flex flex-col gap-6"
+                heading="Approve change"
               >
-                <Build
-                  buildId={deploy.build_id}
-                  componentId={componentId}
-                  orgId={orgId}
-                />
-              </Suspense>
-            </ErrorBoundary>
-          </Section>
+                <ApprovalBanner step={step} />
+                <Plan step={step} />
+              </Section>
+            ) : null}
+          </div>
 
-          <Section
-            className="flex-initial"
-            actions={
-              <OldText>
-                <OldLink
-                  href={`/${orgId}/apps/${install?.app_id}/components/${componentId}`}
-                >
-                  Details
-                  <CaretRightIcon />
-                </OldLink>
-              </OldText>
-            }
-            heading="Component config"
-            childrenClassName="flex flex-col gap-4"
-          >
-            <ErrorBoundary fallbackRender={ErrorFallback}>
-              <Suspense
-                fallback={
-                  <Loading
-                    loadingText="Loading component config..."
-                    variant="stack"
-                  />
-                }
-              >
-                <ComponentConfig
-                  appConfigId={install?.app_config_id}
-                  appId={install?.app_id}
-                  componentId={deploy.component_id}
-                  orgId={orgId}
-                />
-              </Suspense>
-            </ErrorBoundary>
-          </Section>
-
-          {deploy?.oci_artifact ? (
-            <Section>
-              <ConfigurationVariables
-                heading="OCI artifacts"
-                headingVariant="semi-14"
-                isNotTruncated
-                variables={{
-                  tag: deploy?.oci_artifact?.tag,
-                  repository: deploy?.oci_artifact?.repository,
-                  digest: deploy?.oci_artifact?.digest,
-                  size: sizeToMbOrGB(deploy?.oci_artifact?.size),
-                  artifact_type: deploy?.oci_artifact?.artifact_type,
-                  urls: deploy?.oci_artifact?.urls as unknown as string,
-                }}
-              />
-            </Section>
-          ) : null}
-        </div>
-      </div>
-      {/* old page layout */}
-      <BackToTop containerId={containerId} />
-    </PageSection>
+          {/* old page layout */}
+          <BackToTop containerId={containerId} />
+        </PageSection>
+      </DeployProvider>
+    </>
   ) : (
     <DashboardContent
       breadcrumb={[
