759326f34cbd46ea07e70c455e77251984a9a22f
Harsh Thakur <harsh@nuon.co>
2025-12-16 23:46:51 +0530

    fix: fix missing org_id bugs (#11434)
    * fix missing org_id bugs
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    * fix context signature
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    * fix cross org id issues
    
    Signed-off-by: harsh <harsh@nuon.co>
    
    ---------
    
    Signed-off-by: harsh <harsh@nuon.co>

--- services/ctl-api/internal/app/components/service/get_component_latest_config.go
+++ services/ctl-api/internal/app/components/service/get_component_latest_config.go
@@ -6,6 +6,8 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
+
+	"github.com/powertoolsdev/mono/services/ctl-api/internal/pkg/cctx"
 )
 
 // @ID						GetAppComponentLatestConfig
@@ -26,11 +28,24 @@ import (
 // @Success				200	{object}	app.ComponentConfigConnection
 // @Router					/v1/apps/{app_id}/components/{component_id}/configs/latest [GET]
 func (s *service) GetAppComponentLatestConfig(ctx *gin.Context) {
+	org, err := cctx.OrgFromContext(ctx)
+	if err != nil {
+		ctx.Error(err)
+		return
+	}
+
+	appID := ctx.Param("app_id")
 	cmpID := ctx.Param("component_id")
 
 	comp, err := s.helpers.GetComponent(ctx, cmpID)
 	if err != nil {
 		ctx.Error(fmt.Errorf("unable to get component configs: %w", err))
+		return
+	}
+
+	// Validate component belongs to the requesting org AND the specified app
+	if comp.OrgID != org.ID || comp.AppID != appID {
+		ctx.Error(fmt.Errorf("component not found: %w", gorm.ErrRecordNotFound))
 		return
 	}
 
@@ -60,11 +75,23 @@ func (s *service) GetAppComponentLatestConfig(ctx *gin.Context) {
 // @Success				200	{object}	app.ComponentConfigConnection
 // @Router					/v1/components/{component_id}/configs/latest [GET]
 func (s *service) GetComponentLatestConfig(ctx *gin.Context) {
+	org, err := cctx.OrgFromContext(ctx)
+	if err != nil {
+		ctx.Error(err)
+		return
+	}
+
 	cmpID := ctx.Param("component_id")
 
 	comp, err := s.helpers.GetComponent(ctx, cmpID)
 	if err != nil {
 		ctx.Error(fmt.Errorf("unable to get component configs: %w", err))
+		return
+	}
+
+	// Validate component belongs to the requesting org
+	if comp.OrgID != org.ID {
+		ctx.Error(fmt.Errorf("component not found: %w", gorm.ErrRecordNotFound))
 		return
 	}
 
