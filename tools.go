080a20d5fbb44506737842ea18a82a5614ac8f39
im-Amitto <29653282+im-Amitto@users.noreply.github.com>
2025-12-23 02:36:55 +0530

    chore: add transitive deps for generate

--- tools.go
+++ tools.go
@@ -4,8 +4,11 @@
 package tools
 
 import (
-	_ "github.com/go-swagger/go-swagger/cmd/swagger"
-	_ "github.com/golang/mock/mockgen"
-	_ "github.com/swaggo/swag/cmd/swag"
-	_ "go.opentelemetry.io/collector/cmd/builder"
+        _ "github.com/dave/dst/decorator"
+        _ "github.com/go-swagger/go-swagger/cmd/swagger"
+        _ "github.com/go-toolsmith/astfmt"
+        _ "github.com/golang/mock/mockgen"
+        _ "github.com/grafana/codejen"
+        _ "github.com/swaggo/swag/cmd/swag"
+        _ "go.opentelemetry.io/collector/cmd/builder"
 )
