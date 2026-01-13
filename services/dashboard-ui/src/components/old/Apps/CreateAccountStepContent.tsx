cd71c1e35519b99bde60a229c9ef06b76f407dd7
Nat Hamilton <info@nathamilton.com>
2026-01-13 14:09:12 -0500

    feat(ui): enabled org name field with env var
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/src/components/old/Apps/CreateAccountStepContent.tsx
+++ services/dashboard-ui/src/components/old/Apps/CreateAccountStepContent.tsx
@@ -7,6 +7,7 @@ import { Profile } from '../Profile'
 import { Card } from '@/components/common/Card'
 import { Input } from '@/components/common/form/Input'
 import { Textarea } from '@/components/common/form/Textarea'
+import { useUserJourney } from '@/hooks/use-user-journey'
 
 interface CreateAppStepContentProps {
   stepComplete: boolean
@@ -19,6 +20,7 @@ export const CreateAccountStepContent: FC<CreateAppStepContentProps> = ({
   account,
   setSFData,
 }) => {
+  const { isBYOC } = useUserJourney()
   return (
     <div className="space-y-6">
       <div className="space-y-3 pb-4 border-b border-gray-200 dark:border-gray-700">
@@ -41,15 +43,17 @@ export const CreateAccountStepContent: FC<CreateAppStepContentProps> = ({
                 setSFData?.(formObject as Record<string, string>)
               }}
             >
-               <Input
-                className="font-sans"
-                labelProps={{
-                  labelText: 'Organization name',
-                }}
-                 name="name"
-                 defaultValue={`${account?.email}-trial`}
-                 placeholder="Organization name"
-              />
+              {isBYOC ? (
+                <Input
+                  className="font-sans"
+                  labelProps={{
+                    labelText: 'Organization name',
+                  }}
+                  name="name"
+                  defaultValue={`${account?.email}-trial`}
+                  placeholder="Organization name"
+                />
+              ) : null}
               <Input
                 className="font-sans"
                 labelProps={{
