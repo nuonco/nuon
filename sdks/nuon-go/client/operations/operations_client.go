cfb9cea244b5a19db6fec5d7997218d867ad8935
Harsh <harsh@nuon.co>
2026-01-22 20:46:36 +0530

    regen code
    Signed-off-by: harsh <harsh@nuon.co>

--- sdks/nuon-go/client/operations/operations_client.go
+++ sdks/nuon-go/client/operations/operations_client.go
@@ -7312,9 +7312,11 @@ func (a *Client) GetDriftedObjects(params *GetDriftedObjectsParams, authInfo run
 }
 
 /*
-GetInstall gets an install
+	GetInstall gets an install
 
-Return an install by id.
+	Forget an install that has been deleted outside of nuon.
+
+This should only be used in cases where an install was broken in an unordinary way and needs to be manually deleted so the parent resources can be deleted.
 */
 func (a *Client) GetInstall(params *GetInstallParams, authInfo runtime.ClientAuthInfoWriter, opts ...ClientOption) (*GetInstallOK, error) {
 	// NOTE: parameters are not validated before sending
