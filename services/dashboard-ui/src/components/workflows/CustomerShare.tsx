796554ed9bc17f4ac84793909571a0f429f34de5
Jordan Acosta <jordanjacosta@gmail.com>
2026-01-23 11:36:56 -0800

    feat: install stack copy to share with customers

--- services/dashboard-ui/src/components/workflows/CustomerShare.tsx
+++ services/dashboard-ui/src/components/workflows/CustomerShare.tsx
@@ -8,7 +8,6 @@ import { useInstall } from '@/hooks/use-install'
 import { useOrg } from '@/hooks/use-org'
 import { useSurfaces } from '@/hooks/use-surfaces'
 
-
 import { useWorkflowActions } from '@/hooks/use-workflow-actions'
 import type { TWorkflow } from '@/types'
 
@@ -21,7 +20,7 @@ export const CustomerShareModal = ({
   ...props
 }: ICustomerShare & IModal) => {
   const { install } = useInstall()
-  
+
   return (
     <Modal
       heading={
@@ -32,25 +31,40 @@ export const CustomerShareModal = ({
           theme="info"
         >
           <Icon variant="Export" size="24" />
-          {`Share this ${install?.app?.name} ${workflow?.type} with your customer`}
+          {`${install?.app?.name} install link`}
         </Text>
       }
-    
       {...props}
     >
       <div className="flex flex-col gap-1">
-        
         <Text variant="base" weight="stronger">
-         TKTK
+          {`Share the following message with your customer to install ${install?.app?.name} app in their cloud account.`}
+        </Text>
+        <Text variant="base">(install link goes here)</Text>
+        <Text variant="base">
+          {`This is a Cloudformation stack that will install ${install?.app?.name}. To use it, follow these steps:`}
         </Text>
         <Text variant="base">
-          TKTKTKTK
+          1. Log into the account you want to install the app in.
+        </Text>
+        <Text variant="base">
+          2. Click on the link. It will take you to the AWS Cloudformation
+          console.
+        </Text>
+        <Text variant="base">
+          3. Fill out the inputs and secrets, then follow the stack install
+          directions.
+        </Text>
+        <Text variant="base">
+          {`4. When the stack has provisioned, it will automatically install ${install?.app?.name}.`}
+        </Text>
+        <Text variant="base">
+          {`(name_of_vendor) will be automatically notified when the stack has provisioned, and will monitor the installation of ${install?.app?.name}.`}
         </Text>
       </div>
     </Modal>
   )
 }
-
 
 export const CustomerShareButton = ({
   workflow,
