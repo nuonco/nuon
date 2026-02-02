3222d414ae6f1989b6943a6d96c8b4d7f5437083
Somesh Koli <kolisomesh27@gmail.com>
2026-02-03 00:47:08 +0530

    feat: add roles to pending api endpoints and workflows to be passed into signals

--- services/ctl-api/internal/app/installs/service/reprovision_sandbox.go
+++ services/ctl-api/internal/app/installs/service/reprovision_sandbox.go
@@ -52,7 +52,9 @@ func (s *service) ReprovisionInstallSandbox(ctx *gin.Context) {
 	workflow, err := s.helpers.CreateWorkflow(ctx,
 		install.ID,
 		app.WorkflowTypeReprovisionSandbox,
-		map[string]string{},
+		map[string]string{
+			"role": req.Role,
+		},
 		req.PlanOnly,
 	)
 	if err != nil {
