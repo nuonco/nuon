ddf8c017527d836f55149c77ae1af93e7d378761
Somesh Koli <kolisomesh27@gmail.com>
2026-02-18 01:33:45 +0530

    fix: break glass role display on dashboard

--- services/dashboard-ui/src/app/[org-id]/installs/[install-id]/actions/[action-id]/page.tsx
+++ services/dashboard-ui/src/app/[org-id]/installs/[install-id]/actions/[action-id]/page.tsx
@@ -78,6 +78,9 @@ export default async function InstallActionPage({
 
   const containerId = 'install-action-page'
 
+  const installActionBreakGlassRole = installAction.action_workflow?.configs?.[0].break_glass_role_arn
+  const breakGlassRoleArns = installState?.install_stack?.outputs?.break_glass_role_arns
+
   return (
     <PageSection id={containerId} isScrollable className="!p-0 !gap-0">
       <Breadcrumbs
@@ -197,8 +200,7 @@ export default async function InstallActionPage({
         </Section>
 
         <div className="divide-y flex flex-col lg:min-w-[450px] lg:max-w-[450px]">
-          {installAction.action_workflow?.configs?.[0]
-            .break_glass_role_arn!! ? (
+          {installActionBreakGlassRole ? (
             <Section
               className="flex-initial"
               childrenClassName="flex flex-col gap-4"
@@ -207,18 +209,12 @@ export default async function InstallActionPage({
                   <span>Break glass role</span>
                   <StatusBadge
                     description={
-                      installState?.install_stack?.outputs?.break_glass_roles?.[
-                        installAction.action_workflow?.configs?.[0]
-                          ?.break_glass_role_arn
-                      ]
+                      breakGlassRoleArns[installActionBreakGlassRole]
                         ? 'Role provisioned'
                         : 'Role not provisioned'
                     }
                     status={
-                      installState?.install_stack?.outputs?.break_glass_roles?.[
-                        installAction.action_workflow?.configs?.[0]
-                          .break_glass_role_arn
-                      ]
+                      breakGlassRoleArns[installActionBreakGlassRole]
                         ? 'provisioned'
                         : 'not provisioned'
                     }
@@ -230,32 +226,25 @@ export default async function InstallActionPage({
                 Access Permissions
               </Text>
               <Text>
-                Break Glass Role{' '}
-                <Code variant="inline">
+                Break Glass Role must be enabled in install stack before running this action.
+                <Code variant="default">
                   {
                     installAction?.action_workflow?.configs?.[0]
                       ?.break_glass_role_arn
                   }
-                </Code>{' '}
-                must be enabled in install stack before running this action.
+                </Code>
               </Text>
               <br></br>
-              {installState?.install_stack?.outputs?.break_glass_roles?.[
-                installAction.action_workflow?.configs?.[0]
-                  ?.break_glass_role_arn
-              ]!! ? (
-                <div>
+              {breakGlassRoleArns[installActionBreakGlassRole]!! ? (
+                <div className="flex flex-col gap-2">
                   <Text variant="body" weight="strong">
                     Role ARN
                   </Text>
-                  <Text>
+                  <Code variant='default'>
                     {
-                      installState?.install_stack?.outputs?.break_glass_roles?.[
-                        installAction.action_workflow?.configs?.[0]
-                          ?.break_glass_role_arn
-                      ]
+                        breakGlassRoleArns[installActionBreakGlassRole]
                     }
-                  </Text>
+                  </Code>
                 </div>
               ) : null}
             </Section>
