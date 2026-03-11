23ec3615aab7d31991a5cb1b1169d9de758a2911
im-Amitto <29653282+im-Amitto@users.noreply.github.com>
2026-03-11 20:09:59 +0530

    fix: handle backward compatibility

--- pkg/gcp/credentials/config.go
+++ pkg/gcp/credentials/config.go
@@ -1,7 +1,19 @@
 package credentials
 
+import "encoding/json"
+
 type Config struct {
 	ProjectID                 string `json:"project_id" temporaljson:"project_id"`
 	Region                    string `json:"region" temporaljson:"region"`
 	ImpersonateServiceAccount string `json:"impersonate_service_account,omitempty" temporaljson:"impersonate_service_account,omitempty"`
+}
+
+func (c *Config) UnmarshalJSON(data []byte) error {
+	var b bool
+	if json.Unmarshal(data, &b) == nil {
+		return nil
+	}
+
+	type config Config
+	return json.Unmarshal(data, (*config)(c))
 }
