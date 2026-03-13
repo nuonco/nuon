36d26b8381b15779cfe23c8d8e359de69a6f8cd8
Jon Morehouse <jon@nuon.co>
2026-03-12 20:52:35 -0700

    feat: make apisyncer store component-ids and action-ids

--- services/ctl-api/internal/app/app_branch_run.go
+++ services/ctl-api/internal/app/app_branch_run.go
@@ -53,6 +53,10 @@ type AppBranchRun struct {
 	// AppConfigID is the app config that was created/synced during this run
 	AppConfigID string `json:"app_config_id,omitempty" temporaljson:"app_config_id,omitzero,omitempty"`
 
+	// LogStreamID is the log stream created during this run for event tracking
+	LogStreamID *string    `json:"log_stream_id,omitempty" temporaljson:"log_stream_id,omitzero,omitempty"`
+	LogStream   *LogStream `json:"log_stream,omitempty" temporaljson:"log_stream,omitzero,omitempty"`
+
 	// CommitSHA is the VCS commit that triggered or is associated with this run
 	// DEPRECATED: Use VCSConnectionCommit relationship instead
 	CommitSHA string `json:"commit_sha,omitzero" temporaljson:"commit_sha,omitzero,omitempty"`
--- services/ctl-api/internal/app/apps/signals/v2/branches/activities/activities.go
+++ services/ctl-api/internal/app/apps/signals/v2/branches/activities/activities.go
@@ -3,9 +3,13 @@ package activities
 import (
 	"github.com/go-playground/validator/v10"
 	"go.uber.org/fx"
+	"go.uber.org/zap"
 	"gorm.io/gorm"
 
+	"github.com/nuonco/nuon/services/ctl-api/internal"
 	"github.com/nuonco/nuon/services/ctl-api/internal/app/apps/helpers"
+	"github.com/nuonco/nuon/services/ctl-api/internal/pkg/account"
+	"github.com/nuonco/nuon/services/ctl-api/internal/pkg/authz"
 	"github.com/nuonco/nuon/services/ctl-api/internal/pkg/blobstore"
 )
 
@@ -16,20 +20,32 @@ type Params struct {
 	Helpers     *helpers.Helpers
 	DB          *gorm.DB `name:"psql"`
 	BlobService blobstore.Service
+	AcctClient  *account.Client
+	AuthzClient *authz.Client
+	Cfg         *internal.Config
+	L           *zap.Logger
 }
 
 type Activities struct {
-	v       *validator.Validate
-	db      *gorm.DB
-	helpers *helpers.Helpers
-	blobSvc blobstore.Service
+	v           *validator.Validate
+	db          *gorm.DB
+	helpers     *helpers.Helpers
+	blobSvc     blobstore.Service
+	acctClient  *account.Client
+	authzClient *authz.Client
+	cfg         *internal.Config
+	l           *zap.Logger
 }
 
 func New(params Params) (*Activities, error) {
 	return &Activities{
-		v:       params.V,
-		db:      params.DB,
-		helpers: params.Helpers,
-		blobSvc: params.BlobService,
+		v:           params.V,
+		db:          params.DB,
+		helpers:     params.Helpers,
+		blobSvc:     params.BlobService,
+		acctClient:  params.AcctClient,
+		authzClient: params.AuthzClient,
+		cfg:         params.Cfg,
+		l:           params.L,
 	}, nil
 }
