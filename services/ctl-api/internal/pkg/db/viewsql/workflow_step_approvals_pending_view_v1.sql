47d6d5f4ef3318402cab40ca676a3c464891f522
Nat Hamilton <info@nathamilton.com>
2026-03-25 16:17:27 -0400

    refactor(ui): better filters on active workflows and pending approvals
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/ctl-api/internal/pkg/db/viewsql/workflow_step_approvals_pending_view_v1.sql
+++ services/ctl-api/internal/pkg/db/viewsql/workflow_step_approvals_pending_view_v1.sql
@@ -6,6 +6,13 @@ LEFT JOIN install_workflow_step_approval_responses resp
 JOIN install_workflow_steps s
   ON s.id = a.install_workflow_step_id
   AND s.deleted_at = 0
+JOIN install_workflows w
+  ON w.id = s.install_workflow_id
+  AND w.deleted_at = 0
+  AND w.finished_at IS NULL
+JOIN installs i
+  ON i.id = w.owner_id
+  AND i.deleted_at = 0
 WHERE a.deleted_at = 0
   AND resp.id IS NULL
   AND (s.status->>'status') != 'auto-skipped'
