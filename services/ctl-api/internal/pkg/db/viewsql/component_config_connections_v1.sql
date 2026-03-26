753f8a176e70d0276572a783591f7154ccaf53bb
Robert Bruce <alphavibe@gmail.com>
2026-03-25 21:09:47 -0700

    fix: remove view usage for app-config

--- services/ctl-api/internal/pkg/db/viewsql/component_config_connections_v1.sql
+++ services/ctl-api/internal/pkg/db/viewsql/component_config_connections_v1.sql
@@ -15,11 +15,6 @@ WITH component_config_connections_with_count AS (
 
 app_configs_for_component_configs AS (
 SELECT
-    row_number() OVER (
-        PARTITION BY app_id
-        ORDER BY
-            created_at
-    ) AS version,
     *
 FROM
     app_configs
