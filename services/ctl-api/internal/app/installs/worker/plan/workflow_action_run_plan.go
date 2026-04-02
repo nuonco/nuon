3e9b691796a54ccc8660a8b16bd3f6b24223b082
Somesh Koli <somesh@nuon.co>
2026-04-02 14:27:22 +0530

    Update workflow_action_run_plan.go

--- services/ctl-api/internal/app/installs/worker/plan/workflow_action_run_plan.go
+++ services/ctl-api/internal/app/installs/worker/plan/workflow_action_run_plan.go
@@ -22,6 +22,7 @@ type CreateActionPlanResponse struct {
 // @execution-timeout 5m
 // @task-timeout 1m
 // @task-queue "api"
+// @id-template {{.Req.WorkflowID}}
 func CreateActionWorkflowRunPlan(ctx workflow.Context, req *CreateActionRunPlanRequest) (CreateActionPlanResponse, error) {
 	p := Planner{}
 	plan, roleSelection, err := p.createActionWorkflowRunPlan(ctx, req.ActionWorkflowRunID)
