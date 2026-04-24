b2dcf61c12a6c32d5613ac7f9b602abf6192eeec
Jon Morehouse <jon@nuon.co>
2026-04-24 10:50:58 -0700

    fix: use update-with start + add not_found tag

--- services/ctl-api/internal/pkg/queue/client/enqueue_signal.go
+++ services/ctl-api/internal/pkg/queue/client/enqueue_signal.go
@@ -60,7 +60,7 @@ func (c *Client) EnqueueSignal(ctx context.Context, req *EnqueueSignalRequest) (
 		UpdateOptions: tclient.UpdateWorkflowOptions{
 			WorkflowID:   q.Workflow.ID,
 			UpdateName:   queue.EnqueueUpdateName,
-			WaitForStage: tclient.WorkflowUpdateStageAccepted,
+			WaitForStage: tclient.WorkflowUpdateStageAdmitted,
 			Args: []any{
 				queue.EnqueueHandlerInput{
 					QueueSignalID: queueSignal.ID,
