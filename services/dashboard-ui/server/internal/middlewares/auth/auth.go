60360b02d2a71d617f696c9c86469774654857a4
Nat Hamilton <info@nathamilton.com>
2026-05-07 16:07:29 -0400

    fix(ui): serve static files before auth check
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/server/internal/middlewares/auth/auth.go
+++ services/dashboard-ui/server/internal/middlewares/auth/auth.go
@@ -63,6 +63,7 @@ func (m *middleware) Handler() gin.HandlerFunc {
 			return
 		}
 
+		m.l.Info("auth middleware calling GetAuthMe", zap.String("path", c.Request.URL.Path), zap.String("method", c.Request.Method))
 		if _, err := client.GetAuthMe(c.Request.Context()); err != nil {
 			m.l.Warn("auth check failed", zap.Error(err))
 			c.Redirect(http.StatusFound, loginURL)
