3e199e82427f1cbd426ad5f87ab53dd167e49e47
Jordan Acosta <jordanjacosta@gmail.com>
2026-06-03 21:45:39 -0700

    fix: cli handles  api urls with end slashes

--- sdks/nuon-go/client.go
+++ sdks/nuon-go/client.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
+	"strings"
 
 	httptransport "github.com/go-openapi/runtime/client"
 	"github.com/go-playground/validator/v10"
@@ -304,6 +305,8 @@ func New(opts ...clientOption) (*client, error) {
 	if c.v == nil {
 		c.v = validator.New()
 	}
+
+	c.APIURL = strings.TrimRight(c.APIURL, "/")
 
 	if err := c.v.Struct(c); err != nil {
 		return nil, err
