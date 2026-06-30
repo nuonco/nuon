99ebc2028535bed7fa604024f97617743c51a830
Nat Hamilton <info@nathamilton.com>
2026-06-30 13:52:43 -0400

    refactor(ui): better install group diff
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

---
+++ services/dashboard-ui/client/components/approvals/plan-diffs/config-diff-focus.tsx
@@ -0,0 +1,13 @@
+import { createContext, useContext } from 'react'
+
+export type TConfigDiffFocus = { sectionKey: string; entityName?: string; nonce: number }
+
+type TConfigDiffFocusValue = {
+  requestFocus: (sectionKey: string, entityName?: string) => void
+}
+
+export const ConfigDiffFocusContext = createContext<TConfigDiffFocusValue | undefined>(undefined)
+
+export function useConfigDiffFocus() {
+  return useContext(ConfigDiffFocusContext)
+}
