67a6561d6a8b21a606271241337dd1fdae2bca56
Nat Hamilton <info@nathamilton.com>
2026-07-06 11:29:47 -0400

    feat(ui): better app branch workflow loading states (#1810)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

---
+++ services/dashboard-ui/client/components/branches/WorkflowStepDetail/shared/StepStatePlaceholder.stories.tsx
@@ -0,0 +1,13 @@
+export default {
+  title: 'Branches/WorkflowStepDetail/StepStatePlaceholder',
+}
+
+import { StepStatePlaceholder } from './StepStatePlaceholder'
+
+export const Loading = () => (
+  <StepStatePlaceholder variant="loading">Starting component builds</StepStatePlaceholder>
+)
+
+export const Pending = () => (
+  <StepStatePlaceholder variant="pending">Waiting to start component builds</StepStatePlaceholder>
+)
