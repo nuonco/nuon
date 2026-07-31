9f76db0460f4c1d4f1d0237e204783cf380b396a
im-Amitto <29653282+im-Amitto@users.noreply.github.com>
2026-08-01 02:15:17 +0530

    fix: read component health latest state from its own table

--- services/ctl-api/internal/app/installs/service/get_install_resources.go
+++ services/ctl-api/internal/app/installs/service/get_install_resources.go
@@ -11,7 +11,6 @@ import (
 	"github.com/nuonco/nuon/services/ctl-api/internal/app"
 	"github.com/nuonco/nuon/services/ctl-api/internal/middlewares/stderr"
 	"github.com/nuonco/nuon/services/ctl-api/internal/pkg/cctx"
-	"github.com/nuonco/nuon/services/ctl-api/internal/pkg/db/plugins/views"
 	"github.com/nuonco/nuon/services/ctl-api/internal/pkg/db/scopes"
 )
 
@@ -87,7 +86,7 @@ func (s *service) getInstallResources(ctx context.Context, orgID, installID stri
 	resources := make([]app.InstallComponentResourceState, 0)
 
 	q := s.chDB.WithContext(ctx).
-		Scopes(scopes.WithOverrideTable(views.CurrentViewName(s.chDB, &app.InstallComponentResourceState{}))).
+		Scopes(scopes.WithOverrideTable(app.InstallComponentResourceStatesLatestView)).
 		Where(app.InstallComponentResourceState{
 			OrgID:     orgID,
 			InstallID: installID,
