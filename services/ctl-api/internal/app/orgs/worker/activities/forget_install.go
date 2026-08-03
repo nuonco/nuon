d0be53dacd8b4f32275fe93bdaa3c5f2345eb159
Somesh Koli <kolisomesh27@gmail.com>
2026-08-04 02:46:29 +0530

    fix: kill runner related resources as well on install deletion

--- services/ctl-api/internal/app/orgs/worker/activities/forget_install.go
+++ services/ctl-api/internal/app/orgs/worker/activities/forget_install.go
@@ -17,23 +17,74 @@ type ForgetInstallRequest struct {
 // @temporal-gen-v2 activity
 // @by-field InstallID
 func (a *Activities) ForgetInstall(ctx context.Context, req ForgetInstallRequest) error {
+	installOwnerType := plugins.TableName(a.db, app.Install{})
+
 	// must run before the cascade delete below soft-deletes the queues
 	var queueIDs []string
 	if res := a.db.WithContext(ctx).
 		Model(&app.Queue{}).
 		Where(app.Queue{
 			OwnerID:   req.InstallID,
-			OwnerType: plugins.TableName(a.db, app.Install{}),
+			OwnerType: installOwnerType,
 		}).
 		Pluck("id", &queueIDs); res.Error != nil {
 		return dbgenerics.TemporalGormError(res.Error, "unable to list install queues: %w")
 	}
 
-	if len(queueIDs) > 0 {
+	var runnerGroupIDs []string
+	if res := a.db.WithContext(ctx).
+		Model(&app.RunnerGroup{}).
+		Where(app.RunnerGroup{
+			OwnerID:   req.InstallID,
+			OwnerType: installOwnerType,
+		}).
+		Pluck("id", &runnerGroupIDs); res.Error != nil {
+		return dbgenerics.TemporalGormError(res.Error, "unable to list install runner groups: %w")
+	}
+
+	var runnerIDs []string
+	if len(runnerGroupIDs) > 0 {
 		if res := a.db.WithContext(ctx).
-			Where("queue_id IN ?", queueIDs).
+			Model(&app.Runner{}).
+			Where("runner_group_id IN ?", runnerGroupIDs).
+			Pluck("id", &runnerIDs); res.Error != nil {
+			return dbgenerics.TemporalGormError(res.Error, "unable to list install runners: %w")
+		}
+	}
+
+	var runnerQueueIDs []string
+	if len(runnerIDs) > 0 {
+		if res := a.db.WithContext(ctx).
+			Model(&app.Queue{}).
+			Where(app.Queue{OwnerType: plugins.TableName(a.db, app.Runner{})}).
+			Where("owner_id IN ?", runnerIDs).
+			Pluck("id", &runnerQueueIDs); res.Error != nil {
+			return dbgenerics.TemporalGormError(res.Error, "unable to list runner queues: %w")
+		}
+	}
+
+	allQueueIDs := append(queueIDs, runnerQueueIDs...)
+	if len(allQueueIDs) > 0 {
+		if res := a.db.WithContext(ctx).
+			Where("queue_id IN ?", allQueueIDs).
 			Delete(&app.QueueEmitter{}); res.Error != nil {
-			return dbgenerics.TemporalGormError(res.Error, "unable to delete install queue emitters: %w")
+			return dbgenerics.TemporalGormError(res.Error, "unable to delete queue emitters: %w")
+		}
+	}
+
+	if len(runnerQueueIDs) > 0 {
+		if res := a.db.WithContext(ctx).
+			Where("id IN ?", runnerQueueIDs).
+			Delete(&app.Queue{}); res.Error != nil {
+			return dbgenerics.TemporalGormError(res.Error, "unable to delete runner queues: %w")
+		}
+	}
+
+	if len(runnerIDs) > 0 {
+		if res := a.db.WithContext(ctx).
+			Where("id IN ?", runnerIDs).
+			Delete(&app.Runner{}); res.Error != nil {
+			return dbgenerics.TemporalGormError(res.Error, "unable to delete runners: %w")
 		}
 	}
 
