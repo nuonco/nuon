2f71208fb8d14d6f9ec9eb8f797b5ddec169390c
Nat Hamilton <info@nathamilton.com>
2026-08-03 13:06:44 -0400

    refactor(ui): cloud platform messaging for stack deprovision (#2059)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/client/components/installs/management/DeprovisionStack/DeprovisionStack.stories.tsx
+++ services/dashboard-ui/client/components/installs/management/DeprovisionStack/DeprovisionStack.stories.tsx
@@ -7,7 +7,25 @@ import { DeprovisionStackModal } from './DeprovisionStack'
 
 const noop = () => {}
 
-export const Default = () => (
+export const AWS = () => (
+  <ModalStory>
+    <DeprovisionStackModal installName="prod-acme" stackType="aws-cloudformation" onDismiss={noop} />
+  </ModalStory>
+)
+
+export const Azure = () => (
+  <ModalStory>
+    <DeprovisionStackModal installName="prod-acme" stackType="azure-bicep" onDismiss={noop} />
+  </ModalStory>
+)
+
+export const GCP = () => (
+  <ModalStory>
+    <DeprovisionStackModal installName="prod-acme" stackType="gcp-terraform" onDismiss={noop} />
+  </ModalStory>
+)
+
+export const UnknownProvider = () => (
   <ModalStory>
     <DeprovisionStackModal installName="prod-acme" onDismiss={noop} />
   </ModalStory>
