077307a015ffe69ee41973887e7ea1fa2fb76599
Nat Hamilton <info@nathamilton.com>
2026-08-13 15:36:08 -0400

    fix(ui): org switcher on mobile
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/client/components/orgs/OrgSwitcher/OrgSwitcher/OrgSwitcherContainer.tsx
+++ services/dashboard-ui/client/components/orgs/OrgSwitcher/OrgSwitcher/OrgSwitcherContainer.tsx
@@ -1,12 +1,14 @@
+import { useIsDesktop } from '@/hooks/use-is-desktop'
 import { useOrg } from '@/hooks/use-org'
 import { useSidebar } from '@/hooks/use-sidebar'
 import { OrgSwitcher } from './OrgSwitcher'
 
 export const OrgSwitcherContainer = () => {
   const { isSidebarOpen } = useSidebar()
+  const isDesktop = useIsDesktop()
   const { org } = useOrg()
 
   if (!org) return null
 
-  return <OrgSwitcher org={org} isSidebarOpen={isSidebarOpen} />
+  return <OrgSwitcher org={org} isSidebarOpen={!!isSidebarOpen || !isDesktop} />
 }
