a1ec46d6351beff63fa063eb6432b5c544762b70
Nat Hamilton <info@nathamilton.com>
2026-08-21 08:47:06 -0400

    feat(ui): plan summary stories (#2262)
    * feat(ui): add presentational WorkflowChangesSummary component + stories
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat(ui): correct badges
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    * feat(ui): better styles for no change summary
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    
    ---------
    
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

---
+++ services/dashboard-ui/client/components/workflows/WorkflowChangesSummary/index.ts
@@ -0,0 +1,14 @@
+export { WorkflowChangesSummary } from './WorkflowChangesSummary'
+export type {
+  IWorkflowChangesSummary,
+  IWorkflowChangesSummaryLoadingStep,
+} from './WorkflowChangesSummary'
+export { ChangesAggregate } from './ChangesAggregate'
+export {
+  PLAN_TYPE_META,
+  STATUS_META,
+  emptyCounts,
+  hasChanges,
+  sumCounts,
+  formatAggregate,
+} from './change-summary-utils'
