d34450c2aab08132cb14fcf75f7d90f7010719ea
Prem Saraswat <prem@nuon.co>
2026-08-27 19:42:38 +0530

    feat: (runner) reconcile systemd unit updates
    Keep embedded runner service definitions current on managed VM runners and activate changes through a crash-safe, non-blocking restart flow.
    
    Persist restart intent before replacing the unit, reload systemd, and verify a new invocation before clearing state. Transitional services are left alone during slow starts or stops, while ambiguous or failed restart requests are retried on a bounded interval.

--- bins/runner/internal/pkg/monitor/check_runner_service.go
+++ bins/runner/internal/pkg/monitor/check_runner_service.go
@@ -6,15 +6,12 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	"path/filepath"
+	"text/template"
 
 	fetchtoken "github.com/nuonco/nuon/bins/runner/internal/jobs/management/fetch_token"
 	"github.com/nuonco/nuon/pkg/runner/settings"
 	nuonrunner "github.com/nuonco/nuon/sdks/nuon-runner-go"
 
-	"text/template"
-
-	"github.com/fidiego/systemctl"
 	"github.com/pkg/errors"
 	"go.uber.org/zap"
 )
@@ -23,24 +20,10 @@ import (
 const (
 	ConfigDirectory     = "/opt/nuon/runner"
 	ImageConfigFilename = "/opt/nuon/runner/image"
-	// systemd
-	RunnerServiceDir  = "/etc/systemd/system"
-	RunnerServiceName = "nuon-runner.service"
 )
-
-var defaultSystemctlOpts = systemctl.Options{UserMode: false}
 
 //go:embed templates/image.env
 var imageConfigTemplate string
-
-//go:embed templates/runner-service.aws.service
-var runnerServiceAWS string
-
-//go:embed templates/runner-service.gcp.service
-var runnerServiceGCP string
-
-//go:embed templates/runner-service.azure.service
-var runnerServiceAzure string
 
 func (h *Monitor) checkRunnerService(ctx context.Context) error {
 	h.l.Info("checking runner service")
@@ -71,13 +54,7 @@ func (h *Monitor) checkRunnerService(ctx context.Context) error {
 		return err
 	}
 
-	// systemd stuff
-	err = h.ensureRunnerServiceDefinition(ctx)
-	if err != nil {
-		h.l.Error(err.Error())
-		return err
-	}
-	err = h.ensureRunnerServiceIsActive(ctx)
+	err = h.ensureRunnerSystemdService(ctx)
 	if err != nil {
 		h.l.Error(err.Error())
 		return err
@@ -167,102 +144,6 @@ func (h *Monitor) ensureRunnerTokenValid(ctx context.Context) error {
 	h.settings.Cfg.RunnerAPIToken = result.Token
 
 	h.l.Info(fmt.Sprintf("successfully refreshed runner token for runner %s", result.RunnerID))
-	return nil
-}
-
-func (h *Monitor) ensureRunnerServiceDefinition(ctx context.Context) error {
-	// NOTE(fd): we need to pivot on the runner platform to grab the right template
-	path := filepath.Join(RunnerServiceDir, RunnerServiceName)
-	h.l.Debug(fmt.Sprintf("ensuring runner unit file exists: %s", path))
-
-	// dynamically choose the template based on cloud platform
-	var serviceTemplate string
-	switch h.settings.Platform {
-	case "aws", "":
-		serviceTemplate = runnerServiceAWS
-	case "gcp":
-		serviceTemplate = runnerServiceGCP
-	case "azure":
-		serviceTemplate = runnerServiceAzure
-	default:
-		serviceTemplate = runnerServiceAWS
-	}
-	tmpl := template.Must(template.New("").Parse(serviceTemplate))
-
-	var shouldWrite bool
-	// check the nuon-runner.service file
-	// 1. if it does not exist, create it and write the template
-	info, err := os.Stat(path)
-	if err != nil {
-		if errors.Is(err, os.ErrNotExist) {
-			h.l.Warn(fmt.Sprintf("the file (%s) does not exist - will create it", path))
-			shouldWrite = true
-		} else {
-			return errors.Wrap(err, fmt.Sprintf("unable to stat %s", path))
-		}
-	}
-	// 2. if it exists, but it is empty, overwrite it w/ the template
-	if info != nil && info.Size() == 0 {
-		h.l.Info(fmt.Sprintf("the file (%s) exists, but it is empty - will overwrite it", path))
-		shouldWrite = true
-	}
-
-	// 3. otherwise, do nothing (consider checking for drift?)
-	// NOTE(fd): we really only ever write the noun-runner.service file once. we never need to
-	// overwrite it since the service definition is baked into the runner binary and doesn't change.
-	// if we need to modify the service definition we need to release new version of the runner and
-	// run instance refresh or equivalent.
-
-	if shouldWrite {
-		err := h.writeNuonRunnerService(tmpl, path)
-		if err != nil {
-			return err
-		}
-		err = systemctl.DaemonReload(ctx, systemctl.Options{})
-		if err != nil {
-			return errors.Wrap(err, "unabel to reload the sytemctl daemon")
-		}
-	} else {
-		h.l.Debug(fmt.Sprintf("%s - everything is in order", RunnerServiceName))
-	}
-
-	return nil
-}
-
-func (h *Monitor) writeNuonRunnerService(tmpl *template.Template, path string) error {
-	f, err := os.Create(path)
-	if err != nil {
-		return errors.Wrap(err, fmt.Sprintf("unable to create %s", path))
-	}
-	err = tmpl.Execute(f, h.settings)
-	if err != nil {
-		return errors.Wrap(err, fmt.Sprintf("unable to execute template for %s", path))
-	}
-	f.Close()
-	return nil
-}
-
-// this method encapsulates all of the logic to ensure the service is running.
-// NOTE: we use start instead of enable
-func (h *Monitor) ensureRunnerServiceIsActive(ctx context.Context) error {
-	h.l.Debug("ensuring runner service is active")
-	isActive, err := systemctl.IsActive(ctx, RunnerServiceName, defaultSystemctlOpts)
-	if err != nil {
-		return errors.Wrap(err, "unable to determine if unit is active")
-	}
-	if !isActive {
-		err = systemctl.Start(ctx, RunnerServiceName, defaultSystemctlOpts)
-		if err != nil {
-			return errors.Wrap(err, "unable to start unit")
-		}
-	} else {
-		time, err := systemctl.GetStartTime(ctx, RunnerServiceName, defaultSystemctlOpts)
-		if err != nil {
-			return errors.Wrap(err, "unable to determine start time")
-		}
-		h.l.Info(fmt.Sprintf("service is up and running - uptime: %s", time))
-	}
-
 	return nil
 }
 
