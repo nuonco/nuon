4022ed64f261a9b783a79db7d7c94271b1b5f2c6
im-Amitto <29653282+im-Amitto@users.noreply.github.com>
2026-09-10 07:48:13 +0530

    fix: resolve app branch run queue to the default queue

--- services/ctl-api/internal/app/apps/service/get_app_branch.go
+++ services/ctl-api/internal/app/apps/service/get_app_branch.go
@@ -69,7 +69,7 @@ func (s *service) getAppBranch(ctx context.Context, orgID, appID, appBranchID st
 			OrgID: orgID,
 			AppID: appID,
 		}).
-		Preload("Queue")
+		Preload("Queue", "name = ?", "")
 
 	if latestConfig {
 		// Only preload the latest config with its relationships
