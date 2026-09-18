c8ed499d75cfdd9c7f789575f7427cb3b2737f80
Nat Hamilton <info@nathamilton.com>
2026-09-18 09:20:24 -0400

    refactor(ui/api): better composite error message & treatment for workflows (#2547)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/ctl-api/internal/app/installs/signals/componentteardownapplyplan/signal.go
+++ services/ctl-api/internal/app/installs/signals/componentteardownapplyplan/signal.go
@@ -10,6 +10,7 @@ import (
 
 	plantypes "github.com/nuonco/nuon/pkg/plans/types"
 	"github.com/nuonco/nuon/services/ctl-api/internal/app"
+	"github.com/nuonco/nuon/services/ctl-api/internal/app/installs/deployerrors"
 	"github.com/nuonco/nuon/services/ctl-api/internal/app/installs/helpers/stategen"
 	"github.com/nuonco/nuon/services/ctl-api/internal/app/installs/signals/componentteardownsyncandplan"
 	"github.com/nuonco/nuon/services/ctl-api/internal/app/installs/worker/activities"
@@ -25,6 +26,8 @@ import (
 )
 
 const SignalType signal.SignalType = "component-teardown-apply-plan"
+
+const planCompositeErrorVersion = "teardown-apply-plan-composite-error-v1"
 
 type Signal struct {
 	signal.LifecycleBase
@@ -309,6 +312,13 @@ func (s *Signal) execApplyPlan(ctx workflow.Context, install *app.Install, insta
 	s.runnerJobID = runnerJob.ID
 
 	// NOTE(jm): this is probably going to need to be refactored
+	planCompositeErrorsEnabled := workflow.GetVersion(ctx, planCompositeErrorVersion, workflow.DefaultVersion, 1) != workflow.DefaultVersion
+	if planCompositeErrorsEnabled {
+		_ = activities.AwaitSetInstallDeployPlanCompositeError(ctx, activities.SetInstallDeployPlanCompositeErrorRequest{
+			InstallDeployID: installDeploy.ID,
+		})
+	}
+
 	deployPlan, err := plan.AwaitCreateDeployPlan(ctx, &plan.CreateDeployPlanRequest{
 		InstallDeployID: installDeploy.ID,
 		InstallID:       install.ID,
@@ -317,6 +327,14 @@ func (s *Signal) execApplyPlan(ctx workflow.Context, install *app.Install, insta
 	})
 	if err != nil {
 		s.updateDeployStatus(ctx, installDeploy.ID, app.InstallDeployStatusError, "unable to create deploy plan")
+		if planCompositeErrorsEnabled && deployerrors.IsDeployPlanRenderFailed(err) {
+			_ = activities.AwaitSetInstallDeployPlanCompositeError(ctx, activities.SetInstallDeployPlanCompositeErrorRequest{
+				InstallDeployID: installDeploy.ID,
+				ComponentName:   installDeploy.ComponentName,
+				Stage:           deployerrors.PlanRenderStage(err),
+				Detail:          deployerrors.PlanRenderDetail(err),
+			})
+		}
 		return errors.Wrap(err, "unable to create deploy plan")
 	}
 
