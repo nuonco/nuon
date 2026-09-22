02bed03faa9ada1d205469c483175946bd9c9ab2
Somesh Koli <somesh@nuon.co>
2026-09-22 21:23:30 +0530

    fix: issue with install app branch change failing (#2603)
    Co-authored-by: nuonbot <bot@nuon.co>

--- services/ctl-api/internal/app/apps/signals/branches/activities/ensure_install_app_branch.go
+++ services/ctl-api/internal/app/apps/signals/branches/activities/ensure_install_app_branch.go
@@ -2,9 +2,6 @@ package activities
 
 import (
 	"context"
-	"fmt"
-
-	"github.com/nuonco/nuon/services/ctl-api/internal/app"
 )
 
 type EnsureInstallAppBranchInput struct {
@@ -15,21 +12,5 @@ type EnsureInstallAppBranchInput struct {
 // @temporal-gen-v2 activity
 // @start-to-close-timeout 1m
 func (a *Activities) EnsureInstallAppBranch(ctx context.Context, input *EnsureInstallAppBranchInput) error {
-	var install app.Install
-	if err := a.db.WithContext(ctx).Where(app.Install{ID: input.InstallID}).First(&install).Error; err != nil {
-		return fmt.Errorf("unable to get install: %w", err)
-	}
-
-	var branch app.AppBranch
-	if err := a.db.WithContext(ctx).Where(app.AppBranch{ID: input.AppBranchID, AppID: install.AppID}).First(&branch).Error; err != nil {
-		return fmt.Errorf("unable to get app branch: %w", err)
-	}
-
-	if install.AppBranchID.Valid && install.AppBranchID.String == branch.ID {
-		return nil
-	}
-	if err := a.helpers.SetInstallAppBranch(ctx, install.ID, branch.ID); err != nil {
-		return fmt.Errorf("unable to move install to app branch: %w", err)
-	}
-	return nil
+	return a.installHelpers.EnsureInstallAppBranch(ctx, input.InstallID, input.AppBranchID)
 }
